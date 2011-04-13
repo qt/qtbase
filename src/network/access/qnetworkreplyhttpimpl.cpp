04f2c4a7a7c65ff36ad60a26b193fa9d681af45f
Peter Hartmann <peter.hartmann@nokia.com>
2011-04-13 11:45:00 +0200

    Revert "HTTP caching internals: fix logic for PreferNetwork and PreferCache"
    This reverts commit 11838861a23ace66307306cf9c96b3f1910e09a9.

--- src/network/access/qnetworkreplyhttpimpl.cpp
+++ src/network/access/qnetworkreplyhttpimpl.cpp
@@ -457,11 +457,6 @@ bool QNetworkReplyHttpImplPrivate::loadFromCacheIfAllowed(QHttpNetworkRequest &h
         httpRequest.setHeaderField("If-Modified-Since", QNetworkHeadersPrivate::toHttpDate(lastModified));
 
     if (CacheLoadControlAttribute == QNetworkRequest::PreferNetwork) {
-        // PreferNetwork == send request with "If-None-Match" and "If-Modified-Since" header,
-        // which will return a 304 Not Modifed if resource has not been changed.
-        // We might read from cache later, if receiving a 304.
-        return false;
-    } else if (CacheLoadControlAttribute == QNetworkRequest::PreferCache) {
         it = cacheHeaders.findRawHeader("Cache-Control");
         if (it != cacheHeaders.rawHeaders.constEnd()) {
             QHash<QByteArray, QByteArray> cacheControl = parseHttpOptionHeader(it->second);
@@ -1443,7 +1438,7 @@ QNetworkCacheMetaData QNetworkReplyHttpImplPrivate::fetchCacheMetaData(const QNe
         attributes.insert(QNetworkRequest::HttpStatusCodeAttribute, statusCode);
         attributes.insert(QNetworkRequest::HttpReasonPhraseAttribute, reasonPhrase);
     } else {
-        // this is the server telling us the resource has not changed, keep the attributes intact
+        // this is a redirection, keep the attributes intact
         attributes = oldMetaData.attributes();
     }
     metaData.setAttributes(attributes);
