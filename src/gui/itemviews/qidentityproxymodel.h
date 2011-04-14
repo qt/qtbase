1ce725cb60358a31b1107ab5c892abb7ceca8453
Gabriel de Dietrich <gabriel.dietrich-de@nokia.com>
2011-04-14 13:58:25 +0200

    Fix licence headers again for MR 900
    See commit b00089261eafbdf5f92ed94d7fb20b402bfcaeb2
    
    Reviewed-by: Trust me
    (cherry picked from commit 7b6a7f475119878681c9d0c06b29896ec3fe72c3)

--- src/gui/itemviews/qidentityproxymodel.h
+++ src/gui/itemviews/qidentityproxymodel.h
@@ -20,17 +20,17 @@
 ** rights.  These rights are described in the Nokia Qt LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU General
-** Public License version 3.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of this
-** file.  Please review the following information to ensure the GNU General
-** Public License version 3.0 requirements will be met:
-** http://www.gnu.org/copyleft/gpl.html.
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
 **
-** Other Usage
-** Alternatively, this file may be used in accordance with the terms and
-** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
 **
 ****************************************************************************/
 
