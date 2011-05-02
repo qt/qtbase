575410f466799e1c5841db3e1e9624ac20040d6e
Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
2011-05-02 15:37:33 +0200

    fix potential crash in QProcessEnvironment::systemEnvironment() on windows
    GetEnvironmentStrings() can theoretically return null
    (cherry picked from commit 443608952d7df9a5146317be992320ba232d2cf9)

--- src/corelib/io/qprocess_win.cpp
+++ src/corelib/io/qprocess_win.cpp
@@ -283,18 +283,19 @@ QProcessEnvironment QProcessEnvironment::systemEnvironment()
     QProcessEnvironment env;
     // Calls to setenv() affect the low-level environment as well.
     // This is not the case the other way round.
-    wchar_t *envStrings = GetEnvironmentStringsW();
-    for (const wchar_t *entry = envStrings; *entry; ) {
-        int entryLen = wcslen(entry);
-        if (const wchar_t *equal = wcschr(entry, L'=')) {
-            int nameLen = equal - entry;
-            QString name = QString::fromWCharArray(entry, nameLen);
-            QString value = QString::fromWCharArray(equal + 1, entryLen - nameLen - 1);
-            env.d->hash.insert(QProcessEnvironmentPrivate::Key(name), value);
+    if (wchar_t *envStrings = GetEnvironmentStringsW()) {
+        for (const wchar_t *entry = envStrings; *entry; ) {
+            int entryLen = wcslen(entry);
+            if (const wchar_t *equal = wcschr(entry, L'=')) {
+                int nameLen = equal - entry;
+                QString name = QString::fromWCharArray(entry, nameLen);
+                QString value = QString::fromWCharArray(equal + 1, entryLen - nameLen - 1);
+                env.d->hash.insert(QProcessEnvironmentPrivate::Key(name), value);
+            }
+            entry += entryLen + 1;
         }
-        entry += entryLen + 1;
+        FreeEnvironmentStrings(envStrings);
     }
-    FreeEnvironmentStrings(envStrings);
     return env;
 }
 
