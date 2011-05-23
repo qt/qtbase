qt/qt

Doc: Fixed qdoc warnings.

src/gui/text/qtextlayout.cpp
@@ -579,12 +579,12 @@ bool QTextLayout::cacheEnabled() const
 }
 
 /*!
-    Set the visual cursor movement style. If the QTextLayout is backed by
-    a document, you can ignore this and use the option in QTextDocument,
-    this option is for widgets like QLineEdit or custom widgets without
-    a QTextDocument. Default value is QTextCursor::Logical.
+    Set the visual cursor movement style to the given \a style. If the
+    QTextLayout is backed by a document, you can ignore this and use the option
+    in QTextDocument, this option is for widgets like QLineEdit or custom
+    widgets without a QTextDocument. Default value is QTextCursor::Logical.
 
-    \sa setCursorMoveStyle()
+    \sa cursorMoveStyle()
 */
 void QTextLayout::setCursorMoveStyle(QTextCursor::MoveStyle style)
 {

qt/qtbase

src/gui/text/qtextlayout.cpp
@@ -579,12 +579,12 @@ bool QTextLayout::cacheEnabled() const
 }
 
 /*!
-    Set the cursor movement style. If the QTextLayout is backed by
-    a document, you can ignore this and use the option in QTextDocument,
-    this option is for widgets like QLineEdit or custom widgets without
-    a QTextDocument. Default value is Qt::LogicalMoveStyle.
+    Sets the visual cursor movement style to the given \a style. If the
+    QTextLayout is backed by a document, you can ignore this and use the option
+    in QTextDocument, this option is for widgets like QLineEdit or custom
+    widgets without a QTextDocument. Default value is QTextCursor::Logical.
 
-    \sa setCursorMoveStyle()
+    \sa cursorMoveStyle()
 */
 void QTextLayout::setCursorMoveStyle(Qt::CursorMoveStyle style)
 {