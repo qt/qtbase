bb6be57b38808273b3d302a782d68f31b3076894
Jiang Jiang <jiang.jiang@nokia.com>
2011-06-27 10:15:39 +0200

    Only compare styleNames if they are not empty
    Task-number: QTBUG-19366
    (cherry picked from commit 15e6ac8f4d9e7a419cd0c10405954bde78559fac)
    
    Change-Id: I5f06bb5133f23dbc8ad6d745d44ca0a85ef49686
    Reviewed-on: http://codereview.qt.nokia.com/760
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@nokia.com>

--- src/gui/text/qfont.cpp
+++ src/gui/text/qfont.cpp
@@ -147,7 +147,7 @@ bool QFontDef::exactMatch(const QFontDef &other) const
             && weight        == other.weight
             && style        == other.style
             && this_family   == other_family
-            && styleName     == other.styleName
+            && (styleName.isEmpty() || other.styleName.isEmpty() || styleName == other.styleName)
             && (this_foundry.isEmpty()
                 || other_foundry.isEmpty()
                 || this_foundry == other_foundry)
