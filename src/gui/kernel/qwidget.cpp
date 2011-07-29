5dd208b1f4e1780acf06e58291103adab0117f16
José Millán Soto <fid@gpul.org>
2011-07-29 10:35:54 -0700

    Call QAccessible::updateAccessibility when a widget is deleted
    Merge-request: 1310
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@nokia.com>
    (cherry picked from commit df3f763920b1450733817596148e087d11c0c543)
    
    Change-Id: I74fb08104c5dc527f9e9ac88776e4aa2623a3385
    Reviewed-on: http://codereview.qt.nokia.com/3041
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@nokia.com>

--- src/gui/kernel/qwidget.cpp
+++ src/gui/kernel/qwidget.cpp
@@ -1680,6 +1680,10 @@ QWidget::~QWidget()
     if (!d->children.isEmpty())
         d->deleteChildren();
 
+#ifndef QT_NOACCESSIBILITY
+    QAccessible::updateAccessibility(this, 0, QAccessible::ObjectDestroyed);
+#endif
+
     QApplication::removePostedEvents(this);
 
     QT_TRY {
