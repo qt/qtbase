9a652ed802d5ec1114ba97ad86eca8d2f25c53d9
Andy Nichols <andy.nichols@nokia.com>
2011-08-02 15:07:50 +0200

    Initial support for Qt using Kernel Mode-setting (KMS)
    This platform plugin supports fullscreen OpenGLES2 windows on devices
    that support Kernel Mode-setting.
    
    Change-Id: I7d4077319654c13851f0ff7ec0228b7b13936382
    Reviewed-on: http://codereview.qt.nokia.com/2516
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Jørgen Lind <jorgen.lind@nokia.com>

---
+++ src/plugins/platforms/kms/qkmsdevice.h
@@ -0,0 +1,87 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the plugins of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QKMSDEVICE_H
+#define QKMSDEVICE_H
+
+extern "C" {
+#include <gbm.h>
+}
+#include <EGL/egl.h>
+
+#include <QObject>
+
+QT_BEGIN_NAMESPACE
+
+class gbm_device;
+class QKmsIntegration;
+
+class QKmsDevice : public QObject
+{
+    Q_OBJECT
+public:
+    explicit QKmsDevice(const QString &path, QKmsIntegration *parent);
+    ~QKmsDevice();
+
+    EGLDisplay eglDisplay() { return m_eglDisplay; }
+    gbm_device *gbmDevice() { return m_graphicsBufferManager; }
+    EGLContext eglContext() { return m_eglContext; }
+    int fd() const { return m_fd; }
+
+    static void pageFlipHandler(int fd, unsigned int frame, unsigned int sec,
+                                unsigned int usec, void *data);
+
+public slots:
+    void handlePageFlipCompleted();
+private:
+    void createScreens();
+
+    QKmsIntegration *m_integration;
+
+    EGLDisplay m_eglDisplay;
+    EGLContext m_eglContext;
+    gbm_device *m_graphicsBufferManager;
+    int m_fd;
+};
+
+QT_END_NAMESPACE
+
+#endif // QKMSDEVICE_H
---
+++ src/plugins/platforms/kms/qkmsscreen.h
@@ -0,0 +1,97 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the plugins of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QKMSSCREEN_H
+#define QKMSSCREEN_H
+
+#include <QtGui/QPlatformScreen>
+#include "qkmsbuffermanager.h"
+
+QT_BEGIN_NAMESPACE
+
+class QKmsCursor;
+class QKmsDevice;
+class QKmsContext;
+
+class QKmsScreen : public QPlatformScreen
+{
+public:
+    QKmsScreen(QKmsDevice *device, int connectorId);
+    ~QKmsScreen();
+
+    QRect geometry() const;
+    int depth() const;
+    QImage::Format format() const;
+    QSize physicalSize() const;
+
+    GLuint framebufferObject() const;
+    quint32 crtcId() const { return m_crtcId; }
+    QKmsDevice *device() const;
+
+    //Called by context for each screen
+    void bindFramebuffer();
+    void swapBuffers();
+    void setFlipReady(unsigned int time);
+
+private:
+    void performPageFlip();
+    void initializeScreenMode();
+    void waitForPageFlipComplete();
+
+    QKmsDevice *m_device;
+    bool m_flipReady;
+    quint32 m_connectorId;
+
+    quint32 m_crtcId;
+    drmModeModeInfo m_mode;
+    QRect m_geometry;
+    QSize m_physicalSize;
+    int m_depth;
+    QImage::Format m_format;
+
+    QKmsCursor *m_cursor;
+    QKmsBufferManager m_bufferManager;
+    unsigned int m_refreshTime;
+};
+
+QT_END_NAMESPACE
+
+#endif // QKMSSCREEN_H
