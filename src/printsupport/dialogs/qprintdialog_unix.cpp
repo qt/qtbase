57bc4f255a737bfdcd40f2aba62fdf7ce9f2e967
Morten Sorvig <morten.sorvig@nokia.com>
2011-08-19 14:27:58 +0200

    Mac: Fix link error in printdialog.
    Recompiling QtWidgets on Mac fails with:
    "ld: duplicate symbol qInitResources_qprintdialog() in
    .obj/debug-shared/qrc_qprintdialog.o and
    .obj/debug-shared/qprintdialog_unix.o "
    
    Ifdef out the include from qprintdialog_unix,
    leaving us with one instance of the synbol.
    
    Change-Id: I7e1f45de65020253e0cad4155fca94944e66c177
    Reviewed-on: http://codereview.qt.nokia.com/3270
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@nokia.com>

--- src/printsupport/dialogs/qprintdialog_unix.cpp
+++ src/printsupport/dialogs/qprintdialog_unix.cpp
@@ -1280,7 +1280,9 @@ QT_END_NAMESPACE
 
 #include "moc_qprintdialog.cpp"
 #include "qprintdialog_unix.moc"
+#ifndef Q_OS_MAC
 #include "qrc_qprintdialog.cpp"
+#endif
 
 #endif // QT_NO_PRINTDIALOG
 
