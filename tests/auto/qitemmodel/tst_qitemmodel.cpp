5e10745dca1d10025404a9f268f03ae697fb10cc
Jason McDonald <jason.mcdonald@nokia.com>
2011-09-09 18:37:30 +1000

    Fix missing/broken license headers
    Change-Id: I54f3ca5979b78f9eec8115cc580c32321859acf6
    Reviewed-on: http://codereview.qt-project.org/4514
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>

--- tests/auto/qitemmodel/tst_qitemmodel.cpp
+++ tests/auto/qitemmodel/tst_qitemmodel.cpp
@@ -39,19 +39,6 @@
 **
 ****************************************************************************/
 
-
-/****************************************************************************
-**
-** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the test suite of the Qt Toolkit.
-**
-** $LICENSE$
-**
-****************************************************************************/
-
 #include <QtTest/QtTest>
 #include <QtCore>
 #include <qdebug.h>
