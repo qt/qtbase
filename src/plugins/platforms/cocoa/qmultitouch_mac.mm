6e1f1df625a85894822ea107b3c9423ad69441b7
Bjørn Erik Nilsen <bjorn.nilsen@nokia.com>
2011-09-19 15:12:20 +0200

    Cocoa platform plugin: update headers for multitouch.
    See commit: b888bf1b515b47bc12dfaeac06f33af762ce0372
    (Moved multitouch code from gui/widgets/ to plugins/platforms/cocoa)
    
    Change-Id: I7c71f4d858cba4918392d477c6597b48e271ea99
    Reviewed-on: http://codereview.qt-project.org/5154
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@nokia.com>

--- src/plugins/platforms/cocoa/qmultitouch_mac.mm
+++ src/plugins/platforms/cocoa/qmultitouch_mac.mm
@@ -4,7 +4,7 @@
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
-** This file is part of the QtGui module of the Qt Toolkit.
+** This file is part of the plugins of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
 ** GNU Lesser General Public License Usage
