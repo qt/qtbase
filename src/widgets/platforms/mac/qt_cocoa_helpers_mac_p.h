ac96f78398d96a3d2c9a1e67e1315e9d0f663ad6
Bradley T. Hughes <bradley.hughes@nokia.com>
2011-10-27 13:47:31 +0200

    Compilation fix (cannot assign const pointer to non-const pointer)
    After reinterpret_casting to const NSString *, we need to const_cast to
    drop the cast to match the return type.
    
    Change-Id: I3d202aec10e719a48f29d96b81621492936ac6ad
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@nokia.com>

--- src/widgets/platforms/mac/qt_cocoa_helpers_mac_p.h
+++ src/widgets/platforms/mac/qt_cocoa_helpers_mac_p.h
@@ -214,7 +214,7 @@ inline QString qt_mac_NSStringToQString(const NSString *nsstr)
 { return QCFString::toQString(reinterpret_cast<const CFStringRef>(nsstr)); }
 
 inline NSString *qt_mac_QStringToNSString(const QString &qstr)
-{ return [reinterpret_cast<const NSString *>(QCFString::toCFStringRef(qstr)) autorelease]; }
+{ return [const_cast<NSString *>(reinterpret_cast<const NSString *>(QCFString::toCFStringRef(qstr))) autorelease]; }
 
 #endif
 
