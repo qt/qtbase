9de6257657a4c23f35a5936b1df7d5657d33847b
Jason McDonald <jason.mcdonald@nokia.com>
2011-11-07 14:47:59 +1000

    Remove redundant copies of QTRY_COMPARE.
    QTRY_COMPARE is now part of qtestlib, so there's no need for copies of
    it.
    
    Change-Id: Ied4e7d3b30c1cf16ddcbf8655e4d976e74c2bd8a
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>

--- tests/auto/corelib/statemachine/qstate/tst_qstate.cpp
+++ tests/auto/corelib/statemachine/qstate/tst_qstate.cpp
@@ -45,20 +45,6 @@
 #include "qstatemachine.h"
 #include "qsignaltransition.h"
 
-// Will try to wait for the condition while allowing event processing
-#define QTRY_COMPARE(__expr, __expected) \
-    do { \
-        const int __step = 50; \
-        const int __timeout = 5000; \
-        if ((__expr) != (__expected)) { \
-            QTest::qWait(0); \
-        } \
-        for (int __i = 0; __i < __timeout && ((__expr) != (__expected)); __i+=__step) { \
-            QTest::qWait(__step); \
-        } \
-        QCOMPARE(__expr, __expected); \
-    } while(0)
-
 //TESTED_CLASS=
 //TESTED_FILES=
 
--- tests/auto/corelib/statemachine/qstatemachine/tst_qstatemachine.cpp
+++ tests/auto/corelib/statemachine/qstatemachine/tst_qstatemachine.cpp
@@ -54,20 +54,6 @@
 #include "private/qstate_p.h"
 #include "private/qstatemachine_p.h"
 
-// Will try to wait for the condition while allowing event processing
-#define QTRY_COMPARE(__expr, __expected) \
-    do { \
-        const int __step = 50; \
-        const int __timeout = 5000; \
-        if ((__expr) != (__expected)) { \
-            QTest::qWait(0); \
-        } \
-        for (int __i = 0; __i < __timeout && ((__expr) != (__expected)); __i+=__step) { \
-            QTest::qWait(__step); \
-        } \
-        QCOMPARE(__expr, __expected); \
-    } while(0)
-
 //TESTED_CLASS=
 //TESTED_FILES=
 
