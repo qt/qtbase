9a17df0090b6a58480646db3c4e4146c20797cbd
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2012-01-01 13:34:03 +0100

    raster: Fix typo in a function argument
    Change-Id: I903e1245667680f43414414bb0b4bfaa6240ea04
    Reviewed-by: Robin Burchell <robin+qt@viroteck.net>
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    Reviewed-by: Jørgen Lind <jorgen.lind@nokia.com>

--- src/gui/painting/qpaintengine_raster_p.h
+++ src/gui/painting/qpaintengine_raster_p.h
@@ -178,7 +178,7 @@ public:
     void drawPixmap(const QRectF &r, const QPixmap &pm, const QRectF &sr);
     void drawImage(const QPointF &p, const QImage &img);
     void drawImage(const QRectF &r, const QImage &pm, const QRectF &sr,
-                   Qt::ImageConversionFlags falgs = Qt::AutoColor);
+                   Qt::ImageConversionFlags flags = Qt::AutoColor);
     void drawTiledPixmap(const QRectF &r, const QPixmap &pm, const QPointF &sr);
     void drawTextItem(const QPointF &p, const QTextItem &textItem);
 
