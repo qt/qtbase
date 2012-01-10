da3e8e761b74ec42a31676a4640adcd05335f4e0
Xizhi Zhu <xizhi.zhu@gmail.com>
2012-01-10 04:58:58 +0200

    Remove the unused QTRY_COMPARE macro from bearer auto tests.
    The custom QTRY_VERIFY macro is needed since the one provided by testlib does
    not support custom timeout.
    
    Change-Id: I12bbc5efcb16c4f53514ad738d6115d217a55b05
    Reviewed-by: Alex <alex.blasche@nokia.com>

--- tests/auto/network/bearer/qbearertestcommon.h
+++ tests/auto/network/bearer/qbearertestcommon.h
@@ -69,19 +69,4 @@
         QVERIFY(__expr); \
     } while(0)
 
-// Will try to wait for the condition while allowing event processing
-#define QTRY_COMPARE(__expr, __expected) \
-    do { \
-        const int __step = 50; \
-        const int __timeout = 90000; \
-        if ((__expr) != (__expected)) { \
-            QTest::qWait(0); \
-        } \
-        for (int __i = 0; __i < __timeout && ((__expr) != (__expected)); __i+=__step) { \
-            QTest::qWait(__step); \
-        } \
-        QCOMPARE(__expr, __expected); \
-    } while(0)
-
 #endif
-
