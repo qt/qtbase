7549dc9e545e9e8a730af6a65c4954627979f53c
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2012-01-13 10:02:24 +0100

    Windows: Use a message-only window as clipboard viewer or for GL.
    Change-Id: Ib8d287404b157aae2f4493ef8eba220afaba6c47
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>

--- src/plugins/platforms/windows/qwindowscontext.cpp
+++ src/plugins/platforms/windows/qwindowscontext.cpp
@@ -542,8 +542,8 @@ QWindowsMimeConverter &QWindowsContext::mimeConverter() const
 }
 
 /*!
-    \brief Convenience to create a non-visible dummy window
-    for example used as clipboard watcher or for GL.
+    \brief Convenience to create a non-visible, message-only dummy
+    window for example used as clipboard watcher or for GL.
 */
 
 HWND QWindowsContext::createDummyWindow(const QString &classNameIn,
@@ -557,7 +557,7 @@ HWND QWindowsContext::createDummyWindow(const QString &classNameIn,
                           windowName, style,
                           CW_USEDEFAULT, CW_USEDEFAULT,
                           CW_USEDEFAULT, CW_USEDEFAULT,
-                          0, NULL, (HINSTANCE)GetModuleHandle(0), NULL);
+                          HWND_MESSAGE, NULL, (HINSTANCE)GetModuleHandle(0), NULL);
 }
 
 /*!
