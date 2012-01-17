8fbad679e921b2b394c46fbf2c602216cdb2f209
Jason McDonald <jason.mcdonald@nokia.com>
2012-01-17 15:06:23 +1000

    Change QSKIP to fail for outdated tests.
    Be more insistent that tests using the old two-argument version of QSKIP
    should be updated.  After a grace period the warning will be removed and
    incorrect usage of QSKIP will revert to a compilation failure.
    
    Change-Id: Ifa19b856d9f45738bd9d790bb65a8741f965d0f4
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>

--- src/testlib/qtestcase.h
+++ src/testlib/qtestcase.h
@@ -128,8 +128,8 @@ do {\
 #define QSKIP(statement, ...) \
 do {\
     if (strcmp(#__VA_ARGS__, "") != 0)\
-        QTest::qWarn("The two argument version of QSKIP is deprecated and will be removed soon. "\
-                     "Please update this test by removing the second parameter.", __FILE__, __LINE__);\
+        QTest::qFail("The two argument version of QSKIP is no longer available. "\
+                     "Please update this test by removing the second argument in each QSKIP.", __FILE__, __LINE__);\
     QTest::qSkip(statement, __FILE__, __LINE__);\
     return;\
 } while (0)
