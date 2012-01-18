855505ea357e46de1d014d0319c00ec548418a3a
Bradley T. Hughes <bradley.hughes@nokia.com>
2012-01-18 08:55:54 +0100

    Do not expect tst_QFileDialog::selectFiles() failure on Mac OS X
    This test no longer fails:
    
    XPASS  : tst_QFiledialog::selectFiles() '!listView->selectionModel()-
       Loc: [tst_qfiledialog.cpp(915)]
    
    Change-Id: Ib790c0f81b3d383117cceceaacbf0c3d6a673404
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>

--- tests/auto/widgets/dialogs/qfiledialog/tst_qfiledialog.cpp
+++ tests/auto/widgets/dialogs/qfiledialog/tst_qfiledialog.cpp
@@ -909,8 +909,8 @@ void tst_QFiledialog::selectFiles()
     QVERIFY(listView);
     for (int i = 0; i < list.count(); ++i) {
         fd.selectFile(fd.directory().path() + "/" + list.at(i));
-#if defined(Q_OS_MAC) || defined(Q_WS_WIN)
-    QEXPECT_FAIL("", "This test does not work on Mac or Windows", Abort);
+#if defined(Q_WS_WIN)
+    QEXPECT_FAIL("", "This test does not work on Windows", Abort);
 #endif
         QTRY_VERIFY(!listView->selectionModel()->selectedRows().isEmpty());
         toSelect.append(listView->selectionModel()->selectedRows().last());
