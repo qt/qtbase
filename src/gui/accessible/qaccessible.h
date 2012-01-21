fdf9ee12916651c708dcd63d557e11e36ec7eb4e
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2012-01-21 20:27:07 +0100

    Add accessible role Terminal.
    Forward ported from Qt 4.
    
    This is used to mark the role of VTs, terminal emulators.
    Needed in order to know about the special editing behavior.
    In order to make for example KDE's Konsole accessible this is required.
    
    reviewed-by: jan-arve sæther <jan-arve.saether@nokia.com>
    (cherry picked from commit 8ca33c6f1643ab5e89699fbf8b80bfaae108b1e5)
    
    Change-Id: Ic7beb2ae2ee70f2c526c7ec5bf982bec7ba9feda
    Reviewed-by: Jan-Arve Sæther <jan-arve.saether@nokia.com>

--- src/gui/accessible/qaccessible.h
+++ src/gui/accessible/qaccessible.h
@@ -291,6 +291,7 @@ public:
         Splitter       = 0x0000003E,
         // Additional Qt roles where enum value does not map directly to MSAA:
         LayeredPane    = 0x0000003F,
+        Terminal       = 0x00000040,
         UserRole       = 0x0000ffff
     };
 
