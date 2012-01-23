8820f836467e8ef96908b138db6a012b99c8f58c
Kurt Korbatits <kurt.korbatits@nokia.com>
2012-01-23 10:06:38 +1000

    Changed qxmlstream unittest to remove its created file after run.
    - Removes test.xml after test completed.
    
    Change-Id: I548e2d644cca8ae0d30c3002df45cf57433170af
    Reviewed-by: Kurt Korbatits  <kurt.korbatits@nokia.com>
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>

--- tests/auto/corelib/xml/qxmlstream/tst_qxmlstream.cpp
+++ tests/auto/corelib/xml/qxmlstream/tst_qxmlstream.cpp
@@ -532,6 +532,7 @@ public:
 
 private slots:
     void initTestCase();
+    void cleanupTestCase();
     void reportFailures() const;
     void reportFailures_data();
     void checkBaseline() const;
@@ -587,6 +588,11 @@ void tst_QXmlStream::initTestCase()
     reader.setContentHandler(&m_handler);
 
     QVERIFY(reader.parse(&source, false));
+}
+
+void tst_QXmlStream::cleanupTestCase()
+{
+    QFile::remove(QLatin1String("test.xml"));
 }
 
 void tst_QXmlStream::reportFailures() const
