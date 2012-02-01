3ddfa8d0eaa42864b055fafe046688a728c8d888
Kurt Korbatits <kurt.korbatits@nokia.com>
2012-02-01 12:03:59 +1000

    Fixed selftests unittest on windows
    - Needed to add toLower() for compare as c:\ and C:\ was causing
      a failure.
    
    Change-Id: Idd55774d118b7249e88362688166b4f9d9712c7c
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>

--- tests/auto/testlib/selftests/findtestdata/findtestdata.cpp
+++ tests/auto/testlib/selftests/findtestdata/findtestdata.cpp
@@ -134,11 +134,17 @@ void FindTestData::paths()
     // __FILE__ may be absolute or relative path; test both.
 
     // absolute:
+#if defined(Q_OS_WIN)
+    QCOMPARE(QTest::qFindTestData(TESTFILE, qPrintable(app_path + "/fakesrc/fakefile.cpp"), __LINE__).toLower(), testfile_path3.toLower());
+#else
     QCOMPARE(QTest::qFindTestData(TESTFILE, qPrintable(app_path + "/fakesrc/fakefile.cpp"), __LINE__), testfile_path3);
-
+#endif
     // relative: (pretend that we were compiled within fakebuild directory, pointing at files in ../fakesrc)
+#if defined(Q_OS_WIN)
+    QCOMPARE(QTest::qFindTestData(TESTFILE, "../fakesrc/fakefile.cpp", __LINE__, qPrintable(app_path + "/fakebuild")).toLower(), testfile_path3.toLower());
+#else
     QCOMPARE(QTest::qFindTestData(TESTFILE, "../fakesrc/fakefile.cpp", __LINE__, qPrintable(app_path + "/fakebuild")), testfile_path3);
-
+#endif
     QVERIFY(QFile(testfile_path3).remove());
 
     // Note, this is expected to generate a warning.
