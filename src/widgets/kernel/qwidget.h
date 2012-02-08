9c99f23ffb1666dae34f0a3aa0182f43de8e7d0c
Andreas Holzammer <andreas.holzammer@kdab.com>
2012-02-08 16:04:34 +0100

    Remove leftover show() prototype.
    In Qt4.8 this was declared in qwidget_wince.cpp. It
    was for auto maximizing. This would need to go
    into lighthouse if needed. This fixes the Windows CE
    compile.
    
    Change-Id: I6fe8361f745e16d9e8053ab58eb76ead0b22d261
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@digia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>

--- src/widgets/kernel/qwidget.h
+++ src/widgets/kernel/qwidget.h
@@ -481,11 +481,7 @@ public Q_SLOTS:
 
     virtual void setVisible(bool visible);
     inline void setHidden(bool hidden) { setVisible(!hidden); }
-#ifndef Q_OS_WINCE
     inline void show() { setVisible(true); }
-#else
-    void show();
-#endif
     inline void hide() { setVisible(false); }
 
     void showMinimized();
