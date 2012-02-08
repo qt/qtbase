0678a7db8e8eb6997b0acb106dd4347ef8c76fbe
Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
2012-02-08 10:21:28 +0100

    remove bizarre magic from precompiled header
    it doesn't serve any puropse (any more?) and it breaks the mingw build.
    
    Change-Id: I02a5e7502586e7e9f5956991498ff602eff66e81
    Reviewed-by: Joerg Bornemann <joerg.bornemann@nokia.com>

--- tools/configure/configure_pch.h
+++ tools/configure/configure_pch.h
@@ -39,34 +39,11 @@
 **
 ****************************************************************************/
 
-#if (defined(_WIN32) || defined(__NT__))
-#  define QT_UNDEF_MACROS_IN_PCH
-#  define _WINSCARD_H_
-#  define _POSIX_         /* Make sure PATH_MAX et al. are defined    */
-#  include <limits.h>
-#  undef _POSIX_          /* Don't polute                             */
-
-   /* Make sure IP v6 is defined first of all, before windows.h     */
-#     include <winsock2.h>
-#  include <stdlib.h>
-#endif
-
-#if defined __cplusplus
+#include <qplatformdefs.h>
 #include <qglobal.h>
 #include <qlist.h>
 #include <qvariant.h>  // All moc genereated code has this include
-#include <qplatformdefs.h>
 #include <qregexp.h>
 #include <qstring.h>
 #include <qstringlist.h>
 #include <qtextcodec.h>
-
-#include <limits.h>
-#include <stdlib.h>
-#endif
-
-#if defined(QT_UNDEF_MACROS_IN_PCH)
-#  undef max /*  These are defined in windef.h, but                   */
-#  undef min /*  we don't want them when building Qt                  */
-#  undef _WINSCARD_H_
-#endif
