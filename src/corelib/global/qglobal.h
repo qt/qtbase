d4441b014803e782abcd9ab598df5035f0ff58ed
Olivier Goffart <ogoffart@woboq.com>
2012-02-13 10:32:40 +0100

    Fix qmake compilation
    No need to error out in bootstrapped mode because we don't use -fPIC
    
    Change-Id: I0cc2889c75b41968edfd8e801b9161a1eb63f6ef
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -1784,7 +1784,7 @@ Q_CORE_EXPORT int qrand();
 #  endif
 #endif
 
-#if defined(QT_REDUCE_RELOCATIONS) && defined(__ELF__) && !defined(__PIC__)
+#if !defined(QT_BOOTSTRAPPED) && defined(QT_REDUCE_RELOCATIONS) && defined(__ELF__) && !defined(__PIC__)
 #  error "You must build your code with position independent code if Qt was built with -reduce-relocations. "\
          "Compile your code with -fPIC or -fPIE."
 #endif
