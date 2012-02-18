f83724a0f282862aa91a28baa20e555b1b64371c
Kent Hansen <kent.hansen@nokia.com>
2012-02-18 21:15:45 +0100

    Port QSignalEventGenerator meta-object to revision 6
    We want to drop support for old revisions in Qt 5.
    
    This commit brings the QSignalEventGenerator meta-object in sync with
    current moc output.
    
    Change-Id: Id5f4954b08cf49169850bd77543b2a7ab8274c18
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

--- src/corelib/statemachine/qstatemachine.cpp
+++ src/corelib/statemachine/qstatemachine.cpp
@@ -2210,16 +2210,19 @@ void QStateMachine::removeDefaultAnimation(QAbstractAnimation *animation)
 #endif // QT_NO_ANIMATION
 
 
+// Begin moc-generated code -- modify carefully (check "HAND EDIT" parts)!
 static const uint qt_meta_data_QSignalEventGenerator[] = {
 
  // content:
-       2,       // revision
+       6,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   12, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
       23,   22,   22,   22, 0x0a,
@@ -2231,9 +2234,26 @@ static const char qt_meta_stringdata_QSignalEventGenerator[] = {
     "QSignalEventGenerator\0\0execute()\0"
 };
 
+void QSignalEventGenerator::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        QSignalEventGenerator *_t = static_cast<QSignalEventGenerator *>(_o);
+        switch (_id) {
+        case 0: _t->execute(_a); break; // HAND EDIT: add the _a parameter
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData QSignalEventGenerator::staticMetaObjectExtraData = {
+    0,  qt_static_metacall
+};
+
 const QMetaObject QSignalEventGenerator::staticMetaObject = {
     { &QObject::staticMetaObject, qt_meta_stringdata_QSignalEventGenerator,
-      qt_meta_data_QSignalEventGenerator, 0 }
+      qt_meta_data_QSignalEventGenerator, &staticMetaObjectExtraData }
 };
 
 const QMetaObject *QSignalEventGenerator::metaObject() const
@@ -2255,25 +2275,26 @@ int QSignalEventGenerator::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        switch (_id) {
-        case 0: {
-// ### in Qt 4.6 we can use QObject::senderSignalIndex()
-            QObjectPrivate *d = static_cast<QObjectPrivate *>(d_ptr.data());
-            int signalIndex = -1;
-            QObject *sender = this->sender();
-            if (sender && d->currentSender)
-                signalIndex = d->currentSender->signal;
-
-            Q_ASSERT(signalIndex != -1);
-            QStateMachine *machine = qobject_cast<QStateMachine*>(parent());
-            QStateMachinePrivate::get(machine)->handleTransitionSignal(sender, signalIndex, _a);
-            break;
-        }
-        default: ;
-        }
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
         _id -= 1;
     }
     return _id;
+}
+// End moc-generated code
+
+void QSignalEventGenerator::execute(void **_a)
+{
+// ### in Qt 4.6 we can use QObject::senderSignalIndex()
+    QObjectPrivate *d = static_cast<QObjectPrivate *>(d_ptr.data());
+    int signalIndex = -1;
+    QObject *sender = this->sender();
+    if (sender && d->currentSender)
+        signalIndex = d->currentSender->signal;
+
+    Q_ASSERT(signalIndex != -1);
+    QStateMachine *machine = qobject_cast<QStateMachine*>(parent());
+    QStateMachinePrivate::get(machine)->handleTransitionSignal(sender, signalIndex, _a);
 }
 
 QSignalEventGenerator::QSignalEventGenerator(QStateMachine *parent)
