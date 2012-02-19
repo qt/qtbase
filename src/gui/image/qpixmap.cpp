ad1a0138789041f4cb7083ea68168a4b9316dd99
Richard Moore <rich@kde.org>
2012-02-19 16:31:39 +0000

    Remove dead method.
    Fix Qt 5 TODO
    
    Change-Id: I9a12cc07ebc7c039bbeb53922b0539f177b226f8
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>

--- src/gui/image/qpixmap.cpp
+++ src/gui/image/qpixmap.cpp
@@ -1003,14 +1003,6 @@ bool QPixmap::isDetached() const
     return data && data->ref.load() == 1;
 }
 
-/*! \internal
-  ### Qt5 - remove me.
-*/
-void QPixmap::deref()
-{
-    Q_ASSERT_X(false, "QPixmap::deref()", "Do not call this function anymore!");
-}
-
 /*!
     Replaces this pixmap's data with the given \a image using the
     specified \a flags to control the conversion.  The \a flags
