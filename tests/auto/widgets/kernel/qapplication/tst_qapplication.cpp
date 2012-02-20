8a182591be39e4c02f8acd907b31e5bebbd828a5
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-02-20 15:12:50 +1000

    tst_qapplication: mark expected failure on mac
    The tab key behavior in Qt5 no longer respects the "Text boxes and lists
    only" tab navigation option in OSX, which is the default.  This is a
    regression since Qt4.
    
    Task-number: QTBUG-24372
    Change-Id: I54c1663f8fb259dd847083432102a0bfad7dd69c
    Reviewed-by: Toby Tomkins <toby.tomkins@nokia.com>

--- tests/auto/widgets/kernel/qapplication/tst_qapplication.cpp
+++ tests/auto/widgets/kernel/qapplication/tst_qapplication.cpp
@@ -69,6 +69,10 @@
 #include <windows.h>
 #endif
 
+QT_BEGIN_NAMESPACE
+extern bool Q_GUI_EXPORT qt_tab_all_widgets; // from qapplication.cpp
+QT_END_NAMESPACE
+
 class tst_QApplication : public QObject
 {
 Q_OBJECT
@@ -1525,7 +1529,14 @@ void tst_QApplication::focusChanged()
     QSettings appleSettings(QLatin1String("apple.com"));
     QVariant appleValue = appleSettings.value(QLatin1String("AppleKeyboardUIMode"), 0);
     tabAllControls = (appleValue.toInt() & 0x2);
+    if (!tabAllControls) {
+        QEXPECT_FAIL("", "QTBUG-24372 Mac tab key \"Text boxes and lists only\" vs "
+                         "\"All controls\" setting is not respected in Qt5", Abort);
+    }
 #endif
+
+    // make sure Qt's idea of tabbing between widgets matches what we think it should
+    QCOMPARE(qt_tab_all_widgets, tabAllControls);
 
     tab.simulate(now);
     if (!tabAllControls) {
