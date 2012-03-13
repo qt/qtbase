7794928ebb6717bca920ba1d73219d43bfe2e9d9
Harald Fernengel <harald.fernengel@nokia.com>
2012-03-13 21:05:59 +0100

    Compile in the LSB env
    madvise is not part of the LSB, use posix_madvise instead
    
    Change-Id: I661e8c5aece5a764a2a9032fcfab49241ab96dd8
    Reviewed-by: Giuseppe D'Angelo <dangelog@gmail.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/3rdparty/pcre/sljit/sljitUtils.c
+++ src/3rdparty/pcre/sljit/sljitUtils.c
@@ -233,7 +233,7 @@ SLJIT_API_FUNC_ATTRIBUTE sljit_w SLJIT_CALL sljit_stack_resize(struct sljit_stac
 	aligned_new_limit = (new_limit + sljit_page_align) & ~sljit_page_align;
 	aligned_old_limit = (stack->limit + sljit_page_align) & ~sljit_page_align;
 	if (aligned_new_limit < aligned_old_limit)
-#ifdef __QNXNTO__
+#if defined(__QNXNTO__) || defined(__LSB_VERSION__)
 		posix_madvise((void*)aligned_new_limit, aligned_old_limit - aligned_new_limit, POSIX_MADV_DONTNEED);
 #else
 		madvise((void*)aligned_new_limit, aligned_old_limit - aligned_new_limit, MADV_DONTNEED);
