qt/qtbase

commit 50ca45f0595242937908854cd37428c397b9cf60

Worked around Metacity crashes in xcb plugin.
Setting the user time before mapping the window seems to prevent the
crasher from happening. We used to set the user time before mapping in
Qt 4.8 too, so it's probably the right thing to do.

Task-number: QTBUG-24462
Change-Id: Ia670b799bd1ed7a7e6399631d5242e57324918b3
Reviewed-by: Jan Arne Petersen <jpetersen@openismus.com>
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>

src/plugins/platforms/xcb/qxcbwindow.cpp
@@ -548,6 +548,8 @@ void QXcbWindow::show()
         updateNetWmStateBeforeMap();
     }
 
+    updateNetWmUserTime(connection()->time());
+
     Q_XCB_CALL(xcb_map_window(xcb_connection(), m_window));
     xcb_flush(xcb_connection());
 

commit 9a68dd015674a6c23764640977d83c272336d9c7

Made sure first created xcb window still receives focus.
Change 50ca45f059524293790 caused mapped windows to not receive focus by
default. The _NET_WM_USER_TIME spec says that if the user time is set to
0 (or CurrentTime), the window will not be initially focused. Thus, if
the connection time has not yet been set, we skip this part to restore
the old behavior.

Change-Id: I19de3602c78629ad2bc65f5e1976313949c82c4c
Reviewed-by: Jan Arne Petersen <jpetersen@openismus.com>
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>

src/plugins/platforms/xcb/qxcbwindow.cpp
@@ -549,7 +549,8 @@ void QXcbWindow::show()
         updateNetWmStateBeforeMap();
     }
 
-    updateNetWmUserTime(connection()->time());
+    if (connection()->time() != CurrentTime)
+        updateNetWmUserTime(connection()->time());
 
     Q_XCB_CALL(xcb_map_window(xcb_connection(), m_window));
     xcb_flush(xcb_connection());