3ed12e4b26bab091821ee3827eb44f61d22f47d7
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2012-03-15 15:16:43 +0100

    Remove Q_WS_X11
    The define is removed and only adds includes.
    
    Change-Id: Id6cc32aa22536f78bb5de10cb779a3168e79441a
    Reviewed-by: Gabriel de Dietrich <gabriel.dietrich-de@nokia.com>

--- src/widgets/styles/qwindowsstyle.cpp
+++ src/widgets/styles/qwindowsstyle.cpp
@@ -69,12 +69,6 @@
 #include <private/qmath_p.h>
 #include <qmath.h>
 
-#ifdef Q_WS_X11
-#include "qfileinfo.h"
-#include "qdir.h"
-#include <private/qt_x11_p.h>
-#endif
-
 #include <private/qstylehelper_p.h>
 
 QT_BEGIN_NAMESPACE
