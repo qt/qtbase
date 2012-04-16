1afad60e6030064fee772c37e2647ae8c891ac2f
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2012-04-16 16:53:52 -0300

    Do not redefine QT_SOCKLEN_T in freebsd-g++'s mkspec.
    QT_SOCKLEN_T was being redefined depending on whether FreeBSD was < or
    >= 4, branched in early 1999.
    
    FreeBSD < 7 is not supported by the upstream project anymore, so we
    can simplify the code and rely on the QT_SOCKLEN_T definition in
    common/posix/qplatformdefs.h instead.
    
    Change-Id: I0e06fc37e29158932b95a2619a7442c6068a7d32
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>

--- mkspecs/freebsd-g++/qplatformdefs.h
+++ mkspecs/freebsd-g++/qplatformdefs.h
@@ -79,17 +79,8 @@
 #include "../common/posix/qplatformdefs.h"
 
 #undef QT_OPEN_LARGEFILE
-#undef QT_SOCKLEN_T
 
 #define QT_OPEN_LARGEFILE       0
-
-#if !defined(__DragonFly__) && (__FreeBSD_version < 400000)
-// FreeBSD 1.0 - 3.5.1
-#define QT_SOCKLEN_T            int
-#else
-// FreeBSD 4.0 and better
-#define QT_SOCKLEN_T            socklen_t
-#endif
 
 #define QT_SNPRINTF		::snprintf
 #define QT_VSNPRINTF		::vsnprintf
