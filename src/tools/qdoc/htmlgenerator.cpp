99e3c08064b50457e71fd624d69800a647d67c7e
Casper van Donderen <casper.vandonderen@nokia.com>
2012-04-26 07:58:25 +0200

    QDoc: Disambiguate prev and next links in html source.
    Change-Id: Iaa82b9b6de71b577a44eae86eb6847c07d068670
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@nokia.com>

--- src/tools/qdoc/htmlgenerator.cpp
+++ src/tools/qdoc/htmlgenerator.cpp
@@ -1876,7 +1876,7 @@ void HtmlGenerator::generateHeader(const QString& title,
             out() << "  <link rel=\"prev\" href=\""
                   << anchorPair.first << "\" />\n";
 
-            navigationLinks += "<a href=\"" + anchorPair.first + "\">";
+            navigationLinks += "<a class=\"prevPage\" href=\"" + anchorPair.first + "\">";
             if (linkPair.first == linkPair.second && !anchorPair.second.isEmpty())
                 navigationLinks += protect(anchorPair.second);
             else
@@ -1894,7 +1894,7 @@ void HtmlGenerator::generateHeader(const QString& title,
             out() << "  <link rel=\"next\" href=\""
                   << anchorPair.first << "\" />\n";
 
-            navigationLinks += "<a href=\"" + anchorPair.first + "\">";
+            navigationLinks += "<a class=\"nextPage\" href=\"" + anchorPair.first + "\">";
             if (linkPair.first == linkPair.second && !anchorPair.second.isEmpty())
                 navigationLinks += protect(anchorPair.second);
             else
