7bd1f36d376cf191e50c5af7e75ed80064be1130
Jonathan Liu <net147@gmail.com>
2012-04-27 00:57:56 +1000

    Disable force_align_arg_pointer function attribute with MinGW-w64
    The force_align_arg_pointer function attribute is not necessary for
    MinGW-w64 to align stack for SSE and generates a compiler warning as
    it is ignored. Do not use force_align_arg_pointer with MinGW-w64 to
    silence the warning.
    
    Change-Id: I58a754dc3ec01b36a1d3c9490f45ccf0824468ba
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -386,7 +386,7 @@ QT_END_INCLUDE_NAMESPACE
 //defines the type for the WNDPROC on windows
 //the alignment needs to be forced for sse2 to not crash with mingw
 #if defined(Q_OS_WIN)
-#  if defined(Q_CC_MINGW)
+#  if defined(Q_CC_MINGW) && !defined(Q_OS_WIN64)
 #    define QT_ENSURE_STACK_ALIGNED_FOR_SSE __attribute__ ((force_align_arg_pointer))
 #  else
 #    define QT_ENSURE_STACK_ALIGNED_FOR_SSE
