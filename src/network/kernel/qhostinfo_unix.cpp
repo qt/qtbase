dac828dd810526a880ffdd6ef455b14af78f3e8e
Robin Burchell <robin.burchell@collabora.co.uk>
2012-05-02 14:35:54 +0200

    Android's resolv.h is busted, so stub out QHostInfo::localDomainName().
    Change-Id: Idcf5ca7298eed55121b690906885af0eb3fe0b9e
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Shane Kearns <shane.kearns@accenture.com>

--- src/network/kernel/qhostinfo_unix.cpp
+++ src/network/kernel/qhostinfo_unix.cpp
@@ -322,7 +322,7 @@ QString QHostInfo::localHostName()
 
 QString QHostInfo::localDomainName()
 {
-#if !defined(Q_OS_VXWORKS)
+#if !defined(Q_OS_VXWORKS) && !defined(Q_OS_LINUX_ANDROID)
     resolveLibrary();
     if (local_res_ninit) {
         // using thread-safe version
