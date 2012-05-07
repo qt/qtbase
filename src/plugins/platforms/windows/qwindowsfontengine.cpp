51998eb4f65b92640176973ba0e0ed14adee561d
Konstantin Ritt <ritt.ks@gmail.com>
2012-05-08 02:57:34 +0300

    fix glyph's CMAP index might be corrupted
    when SMP sybmol is present in the font.
    this is a simple typo fix, actually
    
    Change-Id: I54a4df43ece1a36f5c7997d121b7655afb2069e3
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@nokia.com>

--- src/plugins/platforms/windows/qwindowsfontengine.cpp
+++ src/plugins/platforms/windows/qwindowsfontengine.cpp
@@ -227,7 +227,7 @@ int QWindowsFontEngine::getGlyphIndexes(const QChar *str, int numChars, QGlyphLa
         if (symbol) {
             for (; i < numChars; ++i, ++glyph_pos) {
                 unsigned int uc = getChar(str, i, numChars);
-                glyphs->glyphs[i] = getTrueTypeGlyphIndex(cmap, uc);
+                glyphs->glyphs[glyph_pos] = getTrueTypeGlyphIndex(cmap, uc);
                 if(!glyphs->glyphs[glyph_pos] && uc < 0x100)
                     glyphs->glyphs[glyph_pos] = getTrueTypeGlyphIndex(cmap, uc + 0xf000);
             }
