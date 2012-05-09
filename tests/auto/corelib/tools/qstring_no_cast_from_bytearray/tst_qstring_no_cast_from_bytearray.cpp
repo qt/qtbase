qt/qtbase

commit 19d160b72ba8e1f52eab63e73c85f0e60f630cc6

Fix the QByteArray overloads to QString::fromXXXX
c951908bc201afa59402967d50fa926212845fae added these overloads, but
did not properly use qstrnlen to get the size. This means we get
subtle errors because other methods do have them.

For example:
  QByteArray ba("abc\0def", 7); // ba embedding a NUL
  QString s1(ba);
  QString s2 = QString::fromAscii(ba);
  s1 == s2; // FAILS

  QString s3;
  s3.append(ba);
  s3 == s2; // FAILS

Tested in an upcoming commit.

Change-Id: I22864521a42da789d522d7b75790696928d9ec32
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>

src/corelib/tools/qstring.h
@@ -456,10 +456,14 @@ public:
     {
         return fromLocal8Bit_helper(str, (str && size == -1) ? int(strlen(str)) : size);
     }
-    static inline QString fromAscii(const QByteArray &str) { return fromAscii(str.data(), str.size()); }
-    static inline QString fromLatin1(const QByteArray &str) { return fromLatin1(str.data(), str.size()); }
-    static inline QString fromUtf8(const QByteArray &str) { return fromUtf8(str.data(), str.size()); }
-    static inline QString fromLocal8Bit(const QByteArray &str) { return fromLocal8Bit(str.data(), str.size()); }
+    static inline QString fromAscii(const QByteArray &str)
+    { return fromAscii(str.data(), qstrnlen(str.constData(), str.size())); }
+    static inline QString fromLatin1(const QByteArray &str)
+    { return fromLatin1(str.data(), qstrnlen(str.constData(), str.size())); }
+    static inline QString fromUtf8(const QByteArray &str)
+    { return fromUtf8(str.data(), qstrnlen(str.constData(), str.size())); }
+    static inline QString fromLocal8Bit(const QByteArray &str)
+    { return fromLocal8Bit(str.data(), qstrnlen(str.constData(), str.size())); }
     static QString fromUtf16(const ushort *, int size = -1);
     static QString fromUcs4(const uint *, int size = -1);
     static QString fromRawData(const QChar *, int size);
@@ -580,13 +584,13 @@ public:
     inline QT_ASCII_CAST_WARN bool operator==(const QByteArray &s) const;
     inline QT_ASCII_CAST_WARN bool operator!=(const QByteArray &s) const;
     inline QT_ASCII_CAST_WARN bool operator<(const QByteArray &s) const
-    { return *this < QString::fromAscii(s.constData(), s.size()); }
+    { return *this < QString::fromAscii(s); }
     inline QT_ASCII_CAST_WARN bool operator>(const QByteArray &s) const
-    { return *this > QString::fromAscii(s.constData(), s.size()); }
+    { return *this > QString::fromAscii(s); }
     inline QT_ASCII_CAST_WARN bool operator<=(const QByteArray &s) const
-    { return *this <= QString::fromAscii(s.constData(), s.size()); }
+    { return *this <= QString::fromAscii(s); }
     inline QT_ASCII_CAST_WARN bool operator>=(const QByteArray &s) const
-    { return *this >= QString::fromAscii(s.constData(), s.size()); }
+    { return *this >= QString::fromAscii(s); }
 #endif
 
     typedef QChar *iterator;
@@ -700,17 +704,30 @@ public:
     { return s >= *this; }
 
     inline QT_ASCII_CAST_WARN bool operator==(const char *s) const
-        { return QString::fromAscii(s, s ? int(strlen(s)) : -1) == *this; }
+    { return QString::fromAscii(s) == *this; }
     inline QT_ASCII_CAST_WARN bool operator!=(const char *s) const
-        { return QString::fromAscii(s, s ? int(strlen(s)) : -1) != *this; }
+    { return QString::fromAscii(s) != *this; }
     inline QT_ASCII_CAST_WARN bool operator<(const char *s) const
-        { return QString::fromAscii(s, s ? int(strlen(s)) : -1) > *this; }
+    { return QString::fromAscii(s) > *this; }
     inline QT_ASCII_CAST_WARN bool operator>(const char *s) const
-        { return QString::fromAscii(s, s ? int(strlen(s)) : -1) < *this; }
+    { return QString::fromAscii(s) < *this; }
     inline QT_ASCII_CAST_WARN bool operator<=(const char *s) const
-        { return QString::fromAscii(s, s ? int(strlen(s)) : -1) >= *this; }
+    { return QString::fromAscii(s) >= *this; }
     inline QT_ASCII_CAST_WARN bool operator>=(const char *s) const
-        { return QString::fromAscii(s, s ? int(strlen(s)) : -1) <= *this; }
+    { return QString::fromAscii(s) <= *this; }
+
+    inline QT_ASCII_CAST_WARN bool operator==(const QByteArray &s) const
+    { return QString::fromAscii(s) == *this; }
+    inline QT_ASCII_CAST_WARN bool operator!=(const QByteArray &s) const
+    { return QString::fromAscii(s) != *this; }
+    inline QT_ASCII_CAST_WARN bool operator<(const QByteArray &s) const
+    { return QString::fromAscii(s) > *this; }
+    inline QT_ASCII_CAST_WARN bool operator>(const QByteArray &s) const
+    { return QString::fromAscii(s) < *this; }
+    inline QT_ASCII_CAST_WARN bool operator<=(const QByteArray &s) const
+    { return QString::fromAscii(s) >= *this; }
+    inline QT_ASCII_CAST_WARN bool operator>=(const QByteArray &s) const
+    { return QString::fromAscii(s) <= *this; }
 private:
     int m_size;
     const char *m_data;
@@ -1027,14 +1044,14 @@ inline bool operator>=(const QLatin1String &s1, const QLatin1String &s2)
 
 
 inline bool QString::operator==(const QByteArray &s) const
-{ return qStringComparisonHelper(*this, s.constData()); }
+{ return qStringComparisonHelper(*this, s); }
 inline bool QString::operator!=(const QByteArray &s) const
-{ return !qStringComparisonHelper(*this, s.constData()); }
+{ return !qStringComparisonHelper(*this, s); }
 
 inline bool QByteArray::operator==(const QString &s) const
-{ return qStringComparisonHelper(s, constData()); }
+{ return qStringComparisonHelper(s, *this); }
 inline bool QByteArray::operator!=(const QString &s) const
-{ return !qStringComparisonHelper(s, constData()); }
+{ return !qStringComparisonHelper(s, *this); }
 inline bool QByteArray::operator<(const QString &s) const
 { return QString::fromAscii(constData(), size()) < s; }
 inline bool QByteArray::operator>(const QString &s) const

commit 5ea4c3f148a5c293c5c5bb19866f8bfeabef7b38

Ensure qstring.h compiles with QT_NO_CAST_FROM_BYTEARRAY
19d160b72ba broke it temporarily, and this wasn't detected by
tst_headersclean, because it sets QT_NO_CAST_FROM_ASCII too,
which disabled the faulty code.
So this adds a new unittest for QT_NO_CAST_FROM_BYTEARRAY alone.

Change-Id: Iaf7a36a1378e77188bcc636e5dc9a1f9b84f70a7
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
Reviewed-by: Stephen Kelly <stephen.kelly@kdab.com>

tests/auto/corelib/tools/qstring_no_cast_from_bytearray/qstring_no_cast_from_bytearray.pro
@@ -1,0 +1,6 @@
+CONFIG += testcase parallel_test
+TARGET = tst_qstring_no_cast_from_bytearray
+QT = core testlib
+SOURCES = tst_qstring_no_cast_from_bytearray.cpp
+DEFINES += QT_NO_CAST_FROM_BYTEARRAY
+

tests/auto/corelib/tools/qstring_no_cast_from_bytearray/tst_qstring_no_cast_from_bytearray.cpp
@@ -1,0 +1,59 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtTest/QtTest>
+#include <QtCore/QtCore>
+
+class tst_QString_NoCastFromByteArray: public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void initTestCase();
+};
+
+void tst_QString_NoCastFromByteArray::initTestCase()
+{
+    QWARN("This is a compile test only");
+}
+
+QTEST_APPLESS_MAIN(tst_QString_NoCastFromByteArray)
+
+#include "tst_qstring_no_cast_from_bytearray.moc"

tests/auto/corelib/tools/tools.pro
@@ -36,6 +36,7 @@ SUBDIRS=\
     qsizef \
     qstl \
     qstring \
+    qstring_no_cast_from_bytearray \
     qstringbuilder \
     qstringlist \
     qstringmatcher \