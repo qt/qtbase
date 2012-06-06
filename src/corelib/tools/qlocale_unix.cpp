qt/qtbase

commit b9790a04eeba664ecdf9ace24911094a71b5f0bd

Fix locale issue on unix systems
QSystemLocale::query() was missing the LanguageId QueryType. Therefore
QSystemLocale::fallbackLocale() was always used as default language,
which reads environment variables in the order: LC_ALL -> LC_NUMERIC ->
LANG. The correct behaviour is to read LC_ALL -> LC_MESSAGES -> LANG.
This leads to problems for users that want to use english language, but
non-english localization features (date, number formats etc.)

Change-Id: I4310537dac8622a3dd79231fbad58e22f20ca262
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
Reviewed-by: John Layt <jlayt@kde.org>

src/corelib/tools/qlocale_unix.cpp
@@ -244,6 +244,8 @@ QVariant QSystemLocale::query(QueryType type, QVariant in) const
         return lc_messages.createSeparatedList(in.value<QStringList>());
     case LocaleChanged:
         Q_ASSERT(false);
+    case LanguageId:
+        return lc_messages.language();
     default:
         break;
     }

commit 7dfee3ffc5129307217eb949698c5088a4561642

Use LC_MESSAGES instead of LC_NUMERIC in QSystemLocale::fallbackLocale()
commit b9790a04eeba664ecdf9ace24911094a71b5f0bd
(https://codereview.qt-project.org/24304) introduced the problem that
the country gets initialized from LC_NUMERIC and the language from
LC_MESSAGES. For example, if LC_NUMERIC=ru_RU and LC_MESSAGE=fr_FR,
then QLocale::system().name() returns "fr_RU". It is not nice to mix
the values of two LC_ variables there. Therefore, revert this change
and use LC_MESSAGES instead of LC_NUMERIC in
QSystemLocale::fallbackLocale(). This was also suggested in the
changelog of b9790a04 and it looks like a better way to fix the
problem.

Change-Id: I8fa6fec2b33e9f1f5a31c4b288503a658dad6d30
Reviewed-by: Denis Dzyubenko
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>

src/corelib/tools/qlocale_unix.cpp
@@ -114,7 +114,7 @@ QLocale QSystemLocale::fallbackLocale() const
 {
     QByteArray lang = qgetenv("LC_ALL");
     if (lang.isEmpty())
-        lang = qgetenv("LC_NUMERIC");
+        lang = qgetenv("LC_MESSAGES");
     if (lang.isEmpty())
         lang = qgetenv("LANG");
     return QLocale(QString::fromLatin1(lang));
@@ -242,8 +242,6 @@ QVariant QSystemLocale::query(QueryType type, QVariant in) const
         return lc_messages.createSeparatedList(in.value<QStringList>());
     case LocaleChanged:
         Q_ASSERT(false);
-    case LanguageId:
-        return lc_messages.language();
     default:
         break;
     }