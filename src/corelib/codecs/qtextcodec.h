587b80f67dee42bd6231a2cd836dded05e5dfd13
Lars Knoll <lars.knoll@nokia.com>
2012-06-24 23:31:45 +0200

    The codecForTr() should be utf8
    tr() assumes utf8 as input encoding, not
    latin1.
    
    Change-Id: If834f8c169bdb431d78713d14b03542d3a7ca8e4
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/codecs/qtextcodec.h
+++ src/corelib/codecs/qtextcodec.h
@@ -73,7 +73,7 @@ public:
     static void setCodecForLocale(QTextCodec *c);
 
 #if QT_DEPRECATED_SINCE(5, 0)
-    QT_DEPRECATED static QTextCodec *codecForTr() { return codecForMib(4); /* Latin1 */ }
+    QT_DEPRECATED static QTextCodec *codecForTr() { return codecForMib(106); /* Utf8 */ }
 #endif
 
     static QTextCodec *codecForHtml(const QByteArray &ba);
