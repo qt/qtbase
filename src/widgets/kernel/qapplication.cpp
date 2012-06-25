6b5bbc531b30d8ece25425e39843c6ae1af1d045
Miikka Heikkinen <miikka.heikkinen@digia.com>
2012-06-25 16:56:16 +0300

    Implement automatic mouse grabbing on mouse button press.
    QWidget::grabMouse() documentation states that Qt automatically grabs
    the mouse when a button is pressed and ungrabs it when it is released.
    Implemented this functionality to make Qt5 behavior similar to Qt4.
    
    Task-number: QTBUG-25977
    Change-Id: I17aa7e8190f4f5224ebc382d62970c86ae492bdb
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>

--- src/widgets/kernel/qapplication.cpp
+++ src/widgets/kernel/qapplication.cpp
@@ -3153,6 +3153,8 @@ bool QApplication::notify(QObject *receiver, QEvent *e)
                     d->toolTipGlobalPos = mouse->globalPos();
                     d->toolTipWakeUp.start(d->toolTipFallAsleep.isActive()?20:700, this);
                 }
+
+                d->handleAutomaticMouseGrab(w, mouse);
             }
 
             bool eventAccepted = mouse->isAccepted();
