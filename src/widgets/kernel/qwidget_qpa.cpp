qt/qtbase

commit 8e280d1e187b09651c7036bd76272342d52ce401

QPlatformWindow: Add Window masks.
Add functionality for window masks to QPlatformWindow, which is
required to implement QWidget::setMask() in order to fix
the regression in functionality from Qt 4.8.

Change-Id: I2c2d5629f0b4c6d90e52595ad70b13559aab1f41
Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>

src/widgets/kernel/qwidget_qpa.cpp
@@ -50,6 +50,7 @@
 #include <qpa/qplatformwindow.h>
 #include "QtGui/qsurfaceformat.h"
 #include <qpa/qplatformopenglcontext.h>
+#include <qpa/qplatformintegration.h>
 #include "QtGui/private/qwindow_p.h"
 
 #include <qpa/qplatformcursor.h>
@@ -929,10 +930,24 @@ void QWidgetPrivate::registerDropSite(bool on)
     Q_UNUSED(on);
 }
 
-void QWidgetPrivate::setMask_sys(const QRegion &region)
+void QWidgetPrivate::setMask_sys(const QRegion &regionIn)
 {
-    Q_UNUSED(region);
-    // XXX
+    if (!QGuiApplicationPrivate::platformIntegration()->hasCapability(QPlatformIntegration::WindowMasks)) {
+        qWarning("%s: Not supported on %s.", Q_FUNC_INFO, qPrintable(QGuiApplication::platformName()));
+        return;
+    }
+    Q_Q(QWidget);
+    QRegion region = regionIn;
+    QWindow *window = q->windowHandle();
+    if (!window) {
+        if (QWidget *nativeParent = q->nativeParentWidget()) {
+            window = nativeParent->windowHandle();
+            region.translate(q->mapTo(nativeParent, QPoint(0, 0)));
+        }
+    }
+    if (window)
+        if (QPlatformWindow *platformWindow = window->handle())
+            platformWindow->setMask(region);
 }
 
 void QWidgetPrivate::updateFrameStrut()

src/gui/kernel/qplatformintegration.h
@@ -85,8 +85,8 @@ public:
         OpenGL = 2,
         ThreadedOpenGL = 3,
         SharedGraphicsCache = 4,
-        BufferQueueingOpenGL = 5
-
+        BufferQueueingOpenGL = 5,
+        WindowMasks = 6
     };
 
     virtual ~QPlatformIntegration() { }

src/gui/kernel/qplatformwindow.h
@@ -68,6 +68,7 @@ class QPlatformScreen;
 class QPlatformWindowPrivate;
 class QWindow;
 class QIcon;
+class QRegion;
 
 class Q_GUI_EXPORT QPlatformWindow : public QPlatformSurface
 {
@@ -105,6 +106,7 @@ public:
     virtual void propagateSizeHints();
 
     virtual void setOpacity(qreal level);
+    virtual void setMask(const QRegion &region);
     virtual void requestActivateWindow();
 
     virtual void handleContentOrientationChange(Qt::ScreenOrientation orientation);

src/gui/kernel/qplatformwindow_qpa.cpp
@@ -242,6 +242,16 @@ void QPlatformWindow::setOpacity(qreal level)
 }
 
 /*!
+  Reimplement to  be able to let Qt set the mask of a window
+*/
+
+void QPlatformWindow::setMask(const QRegion &region)
+{
+    Q_UNUSED(region);
+    qWarning("This plugin does not support setting window masks");
+}
+
+/*!
   Reimplement to let Qt be able to request activation/focus for a window
 
   Some window systems will probably not have callbacks for this functionality,

src/plugins/platforms/windows/qwindowsintegration.cpp
@@ -265,6 +265,8 @@ bool QWindowsIntegration::hasCapability(QPlatformIntegration::Capability cap) co
         return true;
 #  endif // QT_OPENGL_ES_2
 #endif // !QT_NO_OPENGL
+    case WindowMasks:
+        return true;
     default:
         return QPlatformIntegration::hasCapability(cap);
     }

src/plugins/platforms/windows/qwindowswindow.cpp
@@ -53,6 +53,7 @@
 #include <QtGui/QGuiApplication>
 #include <QtGui/QScreen>
 #include <QtGui/QWindow>
+#include <QtGui/QRegion>
 #include <private/qwindow_p.h>
 #include <QtGui/QWindowSystemInterface>
 
@@ -1342,6 +1343,49 @@ void QWindowsWindow::setOpacity(qreal level)
         if (m_data.hwnd)
             setWindowOpacity(m_data.hwnd, m_data.flags, level);
     }
+}
+
+static inline HRGN createRectRegion(const QRect &r)
+{
+    return CreateRectRgn(r.left(), r.top(), r.x() + r.width(), r.y() + r.height());
+}
+
+static inline void addRectToWinRegion(const QRect &rect, HRGN *winRegion)
+{
+    if (const HRGN rectRegion = createRectRegion(rect)) {
+        HRGN result = CreateRectRgn(0, 0, 0, 0);
+        if (CombineRgn(result, *winRegion, rectRegion, RGN_OR)) {
+            DeleteObject(winRegion);
+            *winRegion = result;
+        }
+        DeleteObject(rectRegion);
+    }
+}
+
+static HRGN qRegionToWinRegion(const QRegion &region)
+{
+    const QVector<QRect> rects = region.rects();
+    if (rects.isEmpty())
+        return NULL;
+    const int rectCount = rects.size();
+    if (rectCount == 1)
+        return createRectRegion(region.boundingRect());
+    HRGN hRegion = createRectRegion(rects.front());
+    for (int i = 1; i < rectCount; ++i)
+        addRectToWinRegion(rects.at(i), &hRegion);
+    return hRegion;
+}
+
+void QWindowsWindow::setMask(const QRegion &region)
+{
+    if (region.isEmpty()) {
+         SetWindowRgn(m_data.hwnd, 0, true);
+         return;
+    }
+    const HRGN winRegion = qRegionToWinRegion(region);
+    // SetWindowRgn takes ownership.
+    if (!SetWindowRgn(m_data.hwnd, winRegion, true))
+        DeleteObject(winRegion);
 }
 
 void QWindowsWindow::requestActivateWindow()

src/plugins/platforms/windows/qwindowswindow.h
@@ -168,6 +168,7 @@ public:
     virtual QMargins frameMargins() const;
 
     virtual void setOpacity(qreal level);
+    virtual void setMask(const QRegion &region);
     qreal opacity() const { return m_opacity; }
     virtual void requestActivateWindow();
 

src/plugins/platforms/xcb/qxcbintegration.cpp
@@ -203,6 +203,7 @@ bool QXcbIntegration::hasCapability(QPlatformIntegration::Capability cap) const
     case ThreadedPixmaps: return true;
     case OpenGL: return true;
     case ThreadedOpenGL: return false;
+    case WindowMasks: return true;
     default: return QPlatformIntegration::hasCapability(cap);
     }
 }

commit 2858d85281514a70a14ca3b930be6b1d29cf4c0a

Widgets: Apply masks to native windows only.
Do not go searching for a parent window handle in
setMask_sys() as this causes for example MDI areas
to set masks that show the child only.
Fix breakage introduced by 8e280d1e187b09651c7036bd76272342d52ce401 .

Change-Id: I57de41d16985ebc8d3fc81153a671f2e2807b1eb
Reviewed-by: Miikka Heikkinen <miikka.heikkinen@digia.com>

src/widgets/kernel/qwidget_qpa.cpp
@@ -930,22 +930,14 @@ void QWidgetPrivate::registerDropSite(bool on)
     Q_UNUSED(on);
 }
 
-void QWidgetPrivate::setMask_sys(const QRegion &regionIn)
+void QWidgetPrivate::setMask_sys(const QRegion &region)
 {
     if (!QGuiApplicationPrivate::platformIntegration()->hasCapability(QPlatformIntegration::WindowMasks)) {
         qWarning("%s: Not supported on %s.", Q_FUNC_INFO, qPrintable(QGuiApplication::platformName()));
         return;
     }
     Q_Q(QWidget);
-    QRegion region = regionIn;
-    QWindow *window = q->windowHandle();
-    if (!window) {
-        if (QWidget *nativeParent = q->nativeParentWidget()) {
-            window = nativeParent->windowHandle();
-            region.translate(q->mapTo(nativeParent, QPoint(0, 0)));
-        }
-    }
-    if (window)
+    if (const QWindow *window = q->windowHandle())
         if (QPlatformWindow *platformWindow = window->handle())
             platformWindow->setMask(region);
 }