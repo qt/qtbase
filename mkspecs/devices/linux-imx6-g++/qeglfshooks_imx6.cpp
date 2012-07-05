84792b5ad5d49013fe31d9cf258e978250d4ed7f
Thomas Senyk <thomas.senyk@nokia.com>
2012-07-05 18:55:29 +0200

    Adding iMX6-device-files
    Change-Id: Ifdfacf7201c9ee47783ae782d1f9042fe529a039
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>

---
+++ mkspecs/devices/linux-imx6-g++/qeglfshooks_imx6.cpp
@@ -0,0 +1,92 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/
+**
+** This file is part of the qmake spec of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qeglfshooks.h"
+#include <EGL/eglvivante.h>
+
+QT_BEGIN_NAMESPACE
+
+class QEglFSImx6Hooks : public QEglFSHooks
+{
+public:
+    QEglFSImx6Hooks();
+    virtual QSize screenSize() const;
+    virtual EGLNativeWindowType createNativeWindow(const QSize &size, const QSurfaceFormat &format);
+    virtual void destroyNativeWindow(EGLNativeWindowType window);
+
+private:
+    QSize mScreenSize;
+    EGLNativeDisplayType mNativeDisplay;
+};
+
+
+QEglFSImx6Hooks::QEglFSImx6Hooks()
+{
+    int width, height;
+    mNativeDisplay = fbGetDisplay();
+    fbGetDisplayGeometry(mNativeDisplay, &width, &height);
+    mScreenSize.setHeight(height);
+    mScreenSize.setWidth(width);
+}
+
+QSize QEglFSImx6Hooks::screenSize() const
+{
+    return mScreenSize;
+}
+
+EGLNativeWindowType QEglFSImx6Hooks::createNativeWindow(const QSize &size, const QSurfaceFormat &format)
+{
+    Q_UNUSED(format);
+
+    EGLNativeWindowType eglWindow = fbCreateWindow(mNativeDisplay, 0, 1, size.width(), size.height());
+    return eglWindow;
+}
+
+
+void QEglFSImx6Hooks::destroyNativeWindow(EGLNativeWindowType window)
+{
+    fbDestroyWindow(window);
+}
+
+QEglFSImx6Hooks eglFSImx6Hooks;
+QEglFSHooks *platformHooks = &eglFSImx6Hooks;
+
+QT_END_NAMESPACE
