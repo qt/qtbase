b5ad1fcd084d1fa8fe3d0869e119b12d7556857b
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2012-07-30 15:25:25 +0200

    Fix compilation: Move qHash(const T &t, uint seed) up.
    It needs to be visible from
    qHash(const QPair<T1, T2> &key, uint seed).
    
    Change-Id: Ibb63ce6da1e655bfb841c5e580e184ef66c5b766
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>

--- src/corelib/tools/qhash.h
+++ src/corelib/tools/qhash.h
@@ -103,14 +103,14 @@ template <class T> inline uint qHash(const T *key, uint seed = 0)
 #pragma warning( pop )
 #endif
 
+template<typename T> inline uint qHash(const T &t, uint seed) { return (qHash(t) ^ seed); }
+
 template <typename T1, typename T2> inline uint qHash(const QPair<T1, T2> &key, uint seed = 0)
 {
     uint h1 = qHash(key.first, seed);
     uint h2 = qHash(key.second, seed);
     return ((h1 << 16) | (h1 >> 16)) ^ h2 ^ seed;
 }
-
-template<typename T> inline uint qHash(const T &t, uint seed) { return (qHash(t) ^ seed); }
 
 struct Q_CORE_EXPORT QHashData
 {
