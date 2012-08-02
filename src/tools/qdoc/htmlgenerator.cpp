2264e606acd3beb6ce802d81a3cd35e1a602ed61
Casper van Donderen <casper.vandonderen@nokia.com>
2012-08-02 08:19:44 +0200

    QDoc: Set HTML Doctype to XHTML 1.0 Transitional.
    Previously QDoc HTML output was marked as XHTML 1.0 Strict, but did not pass validation.
    As XHTML 1.0 Transitional the W3C validation passes.
    
    Change-Id: Iced8c02b2602a82b94ee394e580bfb92023a517b
    Reviewed-by: Martin Smith <martin.smith@nokia.com>

--- src/tools/qdoc/htmlgenerator.cpp
+++ src/tools/qdoc/htmlgenerator.cpp
@@ -1825,7 +1825,7 @@ void HtmlGenerator::generateHeader(const QString& title,
                                    CodeMarker *marker)
 {
     out() << QString("<?xml version=\"1.0\" encoding=\"%1\"?>\n").arg(outputEncoding);
-    out() << "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n";
+    out() << "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n";
     out() << QString("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"%1\" lang=\"%1\">\n").arg(naturalLanguage);
     out() << "<head>\n";
     out() << "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
