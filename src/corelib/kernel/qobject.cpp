8e7b86de2c69c9c48c8431bf6ea9e6b0b29f00f8
Michael Brasser <michael.brasser@nokia.com>
2012-08-06 11:52:29 +1000

    blockSignals should block QML signal emission.
    Change-Id: Ief35f3ca326173a87240e3cb558e47f721bd04b7
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

--- src/corelib/kernel/qobject.cpp
+++ src/corelib/kernel/qobject.cpp
@@ -3325,12 +3325,12 @@ void QMetaObject::activate(QObject *sender, int signalOffset, int local_signal_i
     if (!sender->d_func()->isSignalConnected(signal_index))
         return; // nothing connected to these signals, and no spy
 
+    if (sender->d_func()->blockSig)
+        return;
+
     if (sender->d_func()->declarativeData && QAbstractDeclarativeData::signalEmitted)
         QAbstractDeclarativeData::signalEmitted(sender->d_func()->declarativeData, sender, 
                                                 signal_index, argv);
-
-    if (sender->d_func()->blockSig)
-        return;
 
     void *empty_argv[] = { 0 };
     if (qt_signal_spy_callback_set.signal_begin_callback != 0) {
