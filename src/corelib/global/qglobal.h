7ef395224ea42a9a8d3469194d960077d7ff21eb
Thiago Macieira <thiago.macieira@intel.com>
2012-08-08 15:55:14 +0200

    Remove unnecessary extra indentation in the EXPORT macros
    Change-Id: I54585fa7e38ea1984018c5cbff9bc4626016bace
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -481,30 +481,29 @@ class QDataStream;
 #if defined(QT_SHARED) || !defined(QT_STATIC)
 #  ifdef QT_STATIC
 #    error "Both QT_SHARED and QT_STATIC defined, please make up your mind"
+#  endif
+#  ifndef QT_SHARED
+#    define QT_SHARED
+#  endif
+#  if defined(QT_BUILD_CORE_LIB)
+#    define Q_CORE_EXPORT Q_DECL_EXPORT
 #  else
-#    ifndef QT_SHARED
-#      define QT_SHARED
-#    endif
-#    if defined(QT_BUILD_CORE_LIB)
-#      define Q_CORE_EXPORT Q_DECL_EXPORT
-#    else
-#      define Q_CORE_EXPORT Q_DECL_IMPORT
-#    endif
-#    if defined(QT_BUILD_GUI_LIB)
-#      define Q_GUI_EXPORT Q_DECL_EXPORT
-#    else
-#      define Q_GUI_EXPORT Q_DECL_IMPORT
-#    endif
-#    if defined(QT_BUILD_WIDGETS_LIB)
-#      define Q_WIDGETS_EXPORT Q_DECL_EXPORT
-#    else
-#      define Q_WIDGETS_EXPORT Q_DECL_IMPORT
-#    endif
-#    if defined(QT_BUILD_NETWORK_LIB)
-#      define Q_NETWORK_EXPORT Q_DECL_EXPORT
-#    else
-#      define Q_NETWORK_EXPORT Q_DECL_IMPORT
-#    endif
+#    define Q_CORE_EXPORT Q_DECL_IMPORT
+#  endif
+#  if defined(QT_BUILD_GUI_LIB)
+#    define Q_GUI_EXPORT Q_DECL_EXPORT
+#  else
+#    define Q_GUI_EXPORT Q_DECL_IMPORT
+#  endif
+#  if defined(QT_BUILD_WIDGETS_LIB)
+#    define Q_WIDGETS_EXPORT Q_DECL_EXPORT
+#  else
+#    define Q_WIDGETS_EXPORT Q_DECL_IMPORT
+#  endif
+#  if defined(QT_BUILD_NETWORK_LIB)
+#    define Q_NETWORK_EXPORT Q_DECL_EXPORT
+#  else
+#    define Q_NETWORK_EXPORT Q_DECL_IMPORT
 #  endif
 #else
 #  define Q_CORE_EXPORT
