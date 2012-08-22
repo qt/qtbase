adc5626f4fdeed30634a950bdaa2404d5e98a4de
Jeremy Katz <jeremy.katz@nokia.com>
2012-08-22 15:37:54 +0200

    change http example default URL to http[s]://qt-project.org
    This example used to refer by default to qt.nokia.com.
    
    Change-Id: Iecb682d0deb6270da91baf26897d44b16b39d09e
    Reviewed-by: Shane Kearns <shane.kearns@accenture.com>

--- examples/network/http/httpwindow.cpp
+++ examples/network/http/httpwindow.cpp
@@ -48,9 +48,9 @@ HttpWindow::HttpWindow(QWidget *parent)
     : QDialog(parent)
 {
 #ifndef QT_NO_SSL
-    urlLineEdit = new QLineEdit("https://qt.nokia.com/");
+    urlLineEdit = new QLineEdit("https://qt-project.org/");
 #else
-    urlLineEdit = new QLineEdit("http://qt.nokia.com/");
+    urlLineEdit = new QLineEdit("http://qt-project.org/");
 #endif
 
     urlLabel = new QLabel(tr("&URL:"));
