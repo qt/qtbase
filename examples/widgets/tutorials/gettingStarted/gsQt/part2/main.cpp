7ec469e900979b364c845eb67f8bd7f0ab920f01
Geir Vattekar <geir.vattekar@nokia.com>
2012-09-12 12:07:45 +0200

    Doc: Fix snippet bug in Getting Started Qt
    Change-Id: I9c8caabee4c6f6131a69851586ef992138beb211
    Reviewed-by: Qt Doc Bot <qt_docbot@qt-project.org>
    Reviewed-by: Jerome Pasion <jerome.pasion@nokia.com>

--- examples/widgets/tutorials/gettingStarted/gsQt/part2/main.cpp
+++ examples/widgets/tutorials/gettingStarted/gsQt/part2/main.cpp
@@ -44,17 +44,17 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QTextEdit textEdit;
-    QPushButton quitButton("&Quit");
+    QTextEdit *textEdit = new QTextEdit;
+    QPushButton *quitButton = new QPushButton("&Quit");
 
-    QObject::connect(&quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+    QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
 
-    QVBoxLayout layout;
-    layout.addWidget(&textEdit);
-    layout.addWidget(&quitButton);
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(textEdit);
+    layout->addWidget(quitButton);
 
     QWidget window;
-    window.setLayout(&layout);
+    window.setLayout(layout);
 
     window.show();
 
