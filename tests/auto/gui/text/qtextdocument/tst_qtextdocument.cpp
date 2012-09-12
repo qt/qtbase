qt/qtbase

commit 2e0003eda4783f69a40fb4b31e7084c761d9640d

Include pixel size of font in exported HTML from QTextDocument
When you copy-pasted rich text in which the font size had been set
using setPixelSize() the font size would be mysteriously forgotten.
The pixel size property in QTextCharFormat was added ad hoc, and not
integrated in the HTML exporter.

Task-number: QT-4792
Reviewed-by: Gunnar
(cherry picked from commit 5aa5c2e2935c1829cc6965198968699f17c24ec0)

src/gui/text/qtextdocument.cpp
@@ -2076,6 +2076,10 @@ QString QTextHtmlExporter::toHtml(const QByteArray &encoding, ExportMode mode)
             html += QLatin1String(" font-size:");
             html += QString::number(defaultCharFormat.fontPointSize());
             html += QLatin1String("pt;");
+        } else if (defaultCharFormat.hasProperty(QTextFormat::FontPixelSize)) {
+            html += QLatin1String(" font-size:");
+            html += QString::number(defaultCharFormat.intProperty(QTextFormat::FontPixelSize));
+            html += QLatin1String("px;");
         }
 
         html += QLatin1String(" font-weight:");
@@ -2156,6 +2160,10 @@ bool QTextHtmlExporter::emitCharFormatStyle(const QTextCharFormat &format)
             html += QLatin1Char(';');
             attributesEmitted = true;
         }
+    } else if (format.hasProperty(QTextFormat::FontPixelSize)) {
+        html += QLatin1String(" font-size:");
+        html += QString::number(format.intProperty(QTextFormat::FontPixelSize));
+        html += QLatin1String("px;");
     }
 
     if (format.hasProperty(QTextFormat::FontWeight)

tests/auto/qtextdocument/tst_qtextdocument.cpp
@@ -180,6 +180,8 @@ private slots:
     void escape_data();
     void escape();
 
+    void copiedFontSize();
+
 private:
     void backgroundImage_checkExpectedHtml(const QTextDocument &doc);
 
@@ -2732,6 +2734,30 @@ void tst_QTextDocument::escape()
     QFETCH(QString, expected);
 
     QCOMPARE(Qt::escape(original), expected);
+}
+
+void tst_QTextDocument::copiedFontSize()
+{
+    QTextDocument documentInput;
+    QTextDocument documentOutput;
+
+    QFont fontInput;
+    fontInput.setPixelSize(24);  // With pixels font size is not transfered in html
+
+    QTextCursor cursorInput(&documentInput);
+    QTextCharFormat formatInput = cursorInput.charFormat();
+    formatInput.setFont(fontInput);
+    cursorInput.insertText("Should be the same font", formatInput);
+    cursorInput.select(QTextCursor::Document);
+
+    QTextDocumentFragment fragmentInput(cursorInput);
+    QString html =  fragmentInput.toHtml();
+
+    QTextCursor cursorOutput(&documentOutput);
+    QTextDocumentFragment fragmentOutput = QTextDocumentFragment::fromHtml(html);
+    cursorOutput.insertFragment(fragmentOutput);
+
+    QCOMPARE(cursorOutput.charFormat().font().pixelSize(), 24);
 }
 
 QTEST_MAIN(tst_QTextDocument)

commit e640ebacb97f6eea9614925b1da3b97a0ff51409

Fix regression in QTextDocument::toHtml() with font pixel size
Introduced by 2e0003eda4783f69a40fb4b31e7084c761d9640d. We were
missing a attributesEmitted=true which caused the output from
toHtml() to sometimes be invalid when using pixel sizes to
specify the font size.

Task-number: QTBUG-25778
Change-Id: Ied61fcaef425a590d71c0b52292ac676cb88ba52
Reviewed-by: Qt Doc Bot <qt_docbot@qt-project.org>
Reviewed-by: Jiang Jiang <gzjjgod@gmail.com>

src/gui/text/qtextdocument.cpp
@@ -2142,6 +2142,7 @@ bool QTextHtmlExporter::emitCharFormatStyle(const QTextCharFormat &format)
         html += QLatin1String(" font-size:");
         html += QString::number(format.intProperty(QTextFormat::FontPixelSize));
         html += QLatin1String("px;");
+        attributesEmitted = true;
     }
 
     if (format.hasProperty(QTextFormat::FontWeight)

tests/auto/gui/text/qtextdocument/tst_qtextdocument.cpp
@@ -180,6 +180,8 @@ private slots:
 
     void copiedFontSize();
 
+    void QTBUG25778_pixelSizeFromHtml();
+
     void htmlExportImportBlockCount();
 
 private:
@@ -2838,6 +2840,18 @@ void tst_QTextDocument::receiveCursorPositionChangedAfterContentsChange()
             &rec, SLOT(contentsChange()));
     cursor.insertText("Hello World");
     QCOMPARE(rec.first, QString("contentsChanged"));
+}
+
+void tst_QTextDocument::QTBUG25778_pixelSizeFromHtml()
+{
+    QTextDocument document1;
+    QTextDocument document2;
+
+    document1.setHtml("<span style=\"font-size: 24px\">Foobar</span>");
+    document2.setHtml(document1.toHtml());
+
+    QTextCursor cursor(&document2);
+    QCOMPARE(cursor.charFormat().font().pixelSize(), 24);
 }
 
 void tst_QTextDocument::copiedFontSize()