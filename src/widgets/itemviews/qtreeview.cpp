1f2a2898348c19e78281f639aeed32b0abb3dd1f
Stephen Kelly <stephen.kelly@kdab.com>
2012-09-27 11:14:52 +0200

    Store an unsigned temporary for the internalId.
    This is compared later with the internalId() of other indexes, which
    produces a warning.
    
    Change-Id: I57ea248c792db3b49947cc6a8fe989c68b67ff0f
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>

--- src/widgets/itemviews/qtreeview.cpp
+++ src/widgets/itemviews/qtreeview.cpp
@@ -3375,7 +3375,7 @@ int QTreeViewPrivate::viewIndex(const QModelIndex &_index) const
     const int totalCount = viewItems.count();
     const QModelIndex index = _index.sibling(_index.row(), 0);
     const int row = index.row();
-    const qint64 internalId = index.internalId();
+    const quintptr internalId = index.internalId();
 
     // We start nearest to the lastViewedItem
     int localCount = qMin(lastViewedItem - 1, totalCount - lastViewedItem);
