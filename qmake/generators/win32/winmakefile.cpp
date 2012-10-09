3c05a0f3e7cad3ef4738eb11df4d427c8dbc13e8
J-P Nurmi <jpnurmi@digia.com>
2012-10-09 14:47:46 +0200

    qmake: fix .pc install target dir separators on windows
    Task-number: QTBUG-26455
    Change-Id: I578345b1676d0d2e812e0ab2e48468b4a8013ee9
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@digia.com>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

--- qmake/generators/win32/winmakefile.cpp
+++ qmake/generators/win32/winmakefile.cpp
@@ -807,7 +807,7 @@ QString Win32MakefileGenerator::defaultInstall(const QString &t)
             QString dst_pc = pkgConfigFileName(false);
             if (!dst_pc.isEmpty()) {
                 dst_pc = filePrefixRoot(root, targetdir + dst_pc);
-                const QString dst_pc_dir = fileInfo(dst_pc).path();
+                const QString dst_pc_dir = Option::fixPathToTargetOS(fileInfo(dst_pc).path(), false);
                 if (!dst_pc_dir.isEmpty()) {
                     if (!ret.isEmpty())
                         ret += "\n\t";
