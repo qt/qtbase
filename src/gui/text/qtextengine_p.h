dbb2581af5cd1e891cb4202ff8a8caa68cd74b6a
Konstantin Ritt <ritt.ks@gmail.com>
2012-10-16 07:20:23 +0300

    Remove Qt3 compat leftovers
    Change-Id: I91c3d26125168998d6279bddb0671cfb68eac2f4
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>

--- src/gui/text/qtextengine_p.h
+++ src/gui/text/qtextengine_p.h
@@ -540,24 +540,14 @@ public:
 
     int findItem(int strPos) const;
     inline QTextFormatCollection *formats() const {
-#ifdef QT_BUILD_COMPAT_LIB
-        return 0; // Compat should never reference this symbol
-#else
         if (block.docHandle())
             return block.docHandle()->formatCollection();
-        else if (specialData)
-            return specialData->formats.data();
-
-        return 0;
-#endif
+        return specialData ? specialData->formats.data() : 0;
     }
     QTextCharFormat format(const QScriptItem *si) const;
     inline QAbstractTextDocumentLayout *docLayout() const {
-#ifdef QT_BUILD_COMPAT_LIB
-        return 0; // Compat should never reference this symbol
-#else
+        Q_ASSERT(block.docHandle());
         return block.docHandle()->document()->documentLayout();
-#endif
     }
     int formatIndex(const QScriptItem *si) const;
 
