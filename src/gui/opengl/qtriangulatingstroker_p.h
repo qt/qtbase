d1ad991bee52e17452cae9dca55b3b340c6f0955
Friedemann Kleint <Friedemann.Kleint@digia.com>
2012-10-23 17:25:48 +0200

    Fix valgrind warning in QTriangulatingStroker.
    Initialize all member variables including m_invScale, which
    was uninitialized.
    
    Change-Id: Id581b1db9411b2f945281e17b7a0407c746761f8
    Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>

--- src/gui/opengl/qtriangulatingstroker_p.h
+++ src/gui/opengl/qtriangulatingstroker_p.h
@@ -54,7 +54,10 @@ QT_BEGIN_NAMESPACE
 class Q_GUI_EXPORT QTriangulatingStroker
 {
 public:
-    QTriangulatingStroker() : m_vertices(0) {}
+    QTriangulatingStroker() : m_vertices(0), m_cx(0), m_cy(0), m_nvx(0), m_nvy(0), m_width(1), m_miter_limit(2),
+        m_roundness(0), m_sin_theta(0), m_cos_theta(0), m_inv_scale(1), m_curvyness_mul(1), m_curvyness_add(0),
+        m_join_style(Qt::BevelJoin), m_cap_style(Qt::SquareCap) {}
+
     void process(const QVectorPath &path, const QPen &pen, const QRectF &clip, QPainter::RenderHints hints);
 
     inline int vertexCount() const { return m_vertices.size(); }
