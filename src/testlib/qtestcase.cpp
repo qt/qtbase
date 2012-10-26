8f1687c572ea661a7b91b0aa675796145513c275
Leena Miettinen <riitta-leena.miettinen@digia.com>
2012-10-26 10:11:36 +0200

    Doc: add landing page for Qt Test
    Replace "QTestLib" with "Qt Test". Some style and language edits.
    Remove some empty headings.
    
    Change-Id: I568b4bbde981c3ef1899c830931892c16b66b4e2
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    Reviewed-by: Jason McDonald <macadder1@gmail.com>

--- src/testlib/qtestcase.cpp
+++ src/testlib/qtestcase.cpp
@@ -95,10 +95,9 @@ QT_BEGIN_NAMESPACE
    \inmodule QtTest
 
    \brief The QTest namespace contains all the functions and
-   declarations that are related to the QTestLib tool.
+   declarations that are related to Qt Test.
 
-   Please refer to the \l{QTestLib Manual} documentation for information on
-   how to write unit tests.
+   See the \l{Qt Test Overview} for information about how to write unit tests.
 */
 
 /*! \macro QVERIFY(condition)
@@ -488,7 +487,7 @@ QT_BEGIN_NAMESPACE
 
     \snippet qtestlib/tutorial5/benchmarking.cpp 0
 
-    \sa {QTestLib Manual#Creating a Benchmark}{Creating a Benchmark},
+    \sa {Qt Test Overview#Creating a Benchmark}{Creating a Benchmark},
         {Chapter 5: Writing a Benchmark}{Writing a Benchmark}
 */
 
@@ -509,7 +508,7 @@ QT_BEGIN_NAMESPACE
     once. The elapsed time will be reported as "0" if it's to short to
     be measured by the selected backend. (Use)
 
-    \sa {QTestLib Manual#Creating a Benchmark}{Creating a Benchmark},
+    \sa {Qt Test Overview#Creating a Benchmark}{Creating a Benchmark},
     {Chapter 5: Writing a Benchmark}{Writing a Benchmark}
 */
 
