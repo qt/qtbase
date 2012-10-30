3c09f6bc9aee0c97427fe8da6efdc73b4ac473aa
Liang Qi <liang.qi@digia.com>
2012-10-30 14:01:12 +0100

    Mac: fix bugs for font selection in QFontDialog
    Use localized family name and style name when selecting font with
    non-English locale
    
    Task-number: QTBUG-27415
    Change-Id: Ie81507ed011fc096e0f5edad146e97c392e86494
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@digia.com>

--- src/gui/text/qfontdatabase.cpp
+++ src/gui/text/qfontdatabase.cpp
@@ -595,7 +595,7 @@ QtFontFamily *QFontDatabasePrivate::family(const QString &f, bool create)
     if (res < 0)
         pos++;
 
-    // qDebug("adding family %s at %d total=%d", f.latin1(), pos, count);
+    // qDebug() << "adding family " << f.toLatin1() << " at " << pos << " total=" << count;
     if (!(count % 8)) {
         QtFontFamily **newFamilies = (QtFontFamily **)
                    realloc(families,
