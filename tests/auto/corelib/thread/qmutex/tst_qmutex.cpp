qt/qtbase

commit f587e8f4fd670f11fa1e878c1015d7241033559a

Avoid calling QElapsedTimer::nsecsElapsed before the first futex sleep
The first time we're going to sleep, the timeout should be exactly the
value that was passed by the user. We don't need to calculate the time
elapsed between start() and a few lines below.

Change-Id: I99c363b6f0ecfd07ad787b79b75e61771733c2b3
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

src/corelib/thread/qmutex_linux.cpp
@@ -184,15 +184,18 @@ bool lockInternal_helper(QBasicAtomicPointer<QMutexData> &d_ptr, int timeout = -
     if (timeout == 0)
         return false;
 
+    struct timespec ts, *pts = 0;
     QElapsedTimer elapsedTimer;
     checkElapsedTimerIsTrivial();
-    if (IsTimed)
+    if (IsTimed) {
+        ts.tv_sec = timeout / Q_INT64_C(1000) / 1000 / 1000;
+        ts.tv_nsec = timeout % Q_INT64_C(1000) * 1000 * 1000;
         elapsedTimer.start();
+    }
 
     // the mutex is locked already, set a bit indicating we're waiting
     while (d_ptr.fetchAndStoreAcquire(dummyFutexValue()) != 0) {
-        struct timespec ts, *pts = 0;
-        if (IsTimed) {
+        if (IsTimed && pts == &ts) {
             // recalculate the timeout
             qint64 xtimeout = qint64(timeout) * 1000 * 1000;
             xtimeout -= elapsedTimer.nsecsElapsed();
@@ -202,8 +205,9 @@ bool lockInternal_helper(QBasicAtomicPointer<QMutexData> &d_ptr, int timeout = -
             }
             ts.tv_sec = xtimeout / Q_INT64_C(1000) / 1000 / 1000;
             ts.tv_nsec = xtimeout % (Q_INT64_C(1000) * 1000 * 1000);
-            pts = &ts;
         }
+        if (IsTimed)
+            pts = &ts;
 
         // successfully set the waiting bit, now sleep
         int r = _q_futex(&d_ptr, FUTEX_WAIT, quintptr(dummyFutexValue()), pts);

commit 2b308d4cb5cdf2f3374404c83dc0b193fcb78612

Fix QMutex::tryLock timeout computation on Linux
The timeout is in millisecond. So we just need to divide by 1000 to get
the number of seconds

Regression introduced in f587e8f4fd670f11fa1e878c1015d7241033559a
Reported in the comments of QTBUG-24795

Change-Id: Id16e05e7d04d33605860926f7516d14cdefd6a36
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/thread/qmutex_linux.cpp
@@ -188,8 +188,8 @@ bool lockInternal_helper(QBasicAtomicPointer<QMutexData> &d_ptr, int timeout = -
     QElapsedTimer elapsedTimer;
     checkElapsedTimerIsTrivial();
     if (IsTimed) {
-        ts.tv_sec = timeout / Q_INT64_C(1000) / 1000 / 1000;
-        ts.tv_nsec = timeout % Q_INT64_C(1000) * 1000 * 1000;
+        ts.tv_sec = timeout / 1000;
+        ts.tv_nsec = (timeout % 1000) * 1000 * 1000;
         elapsedTimer.start();
     }
 

tests/auto/corelib/thread/qmutex/tst_qmutex.cpp
@@ -132,6 +132,14 @@ void tst_QMutex::tryLock()
                 normalMutex.unlock();
                 testsTurn.release();
 
+                // TEST 7 overflow: thread can acquire lock, timeout = 3000 (QTBUG-24795)
+                threadsTurn.acquire();
+                timer.start();
+                QVERIFY(normalMutex.tryLock(3000));
+                QVERIFY(timer.elapsed() < 3000);
+                normalMutex.unlock();
+                testsTurn.release();
+
                 threadsTurn.acquire();
             }
         };
@@ -174,6 +182,13 @@ void tst_QMutex::tryLock()
         QVERIFY(lockCount.testAndSetRelaxed(1, 0));
         normalMutex.unlock();
         threadsTurn.release();
+
+        // TEST 7: thread can acquire lock, timeout = 3000   (QTBUG-24795)
+        testsTurn.acquire();
+        normalMutex.lock();
+        threadsTurn.release();
+        QThread::msleep(100);
+        normalMutex.unlock();
 
         // wait for thread to finish
         testsTurn.acquire();