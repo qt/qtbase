e1a7a12debe1bcdef9412cae6e0f925d6f8b9984
hjk <qthjk@ovi.com>
2012-11-23 08:12:22 +0100

    Fix outdated comment in kernel/qtranslator_p.h.
    Change-Id: I0e7cc1686c5bcce611e353987e41b4dae7af5c31
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

--- src/corelib/kernel/qtranslator_p.h
+++ src/corelib/kernel/qtranslator_p.h
@@ -47,8 +47,8 @@
 //  -------------
 //
 // This file is not part of the Qt API.  It exists for the convenience
-// of qfontencodings_x11.cpp and qfont_x11.cpp.  This header file may
-// change from version to version without notice, or even be removed.
+// of the Qt translation tools.  This header file may change from version
+// to version without notice, or even be removed.
 //
 // We mean it.
 //
