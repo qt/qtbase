303ff1bb1267322a730a5e7ac5a71a12d69d906a
Friedemann Kleint <Friedemann.Kleint@digia.com>
2012-12-12 10:43:29 +0100

    test: Skip tst_QMdiArea::nativeSubWindows() on Windows/ANGLE.
    Task-number: QTBUG-28545
    
    Change-Id: Ifa698b9efe38654c8f8fbcc184db5d176df7ba47
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>
    Reviewed-by: Mitch Curtis <mitch.curtis@digia.com>

--- tests/auto/widgets/widgets/qmdiarea/tst_qmdiarea.cpp
+++ tests/auto/widgets/widgets/qmdiarea/tst_qmdiarea.cpp
@@ -2594,7 +2594,10 @@ void tst_QMdiArea::nativeSubWindows()
     const QString platformName = QGuiApplication::platformName();
     if (platformName != QLatin1String("xcb") && platformName != QLatin1String("windows"))
         QSKIP(qPrintable(QString::fromLatin1("nativeSubWindows() does not work on this platform (%1).").arg(platformName)));
-
+#ifdef QT_OPENGL_ES_2_ANGLE
+    if (platformName == QLatin1String("windows"))
+        QSKIP("nativeSubWindows() does not work with ANGLE on Windows, QTBUG-28545.");
+#endif
     { // Add native widgets after show.
     QMdiArea mdiArea;
     mdiArea.addSubWindow(new QWidget);
