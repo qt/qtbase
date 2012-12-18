05659223bf02c8bac0463fe1e7a9364ef5677b75
Thiago Macieira <thiago.macieira@intel.com>
2012-12-17 22:24:55 -0800

    Rename qt_mips_asm_dsp.h to a "_p.h" header
    It mustn't be installed and it mustn't be available in the master
    include. It's an assembler header anyway.
    
    Also, take the opportunity to add the "We mean it".
    
    Change-Id: Id4233e5bda458714a0d329549afe499d861acc56
    Reviewed-by: Adrian Perez de Castro <aperez@igalia.com>
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>

--- src/gui/painting/qdrawhelper_mips_dsp_p.h
+++ src/gui/painting/qdrawhelper_mips_dsp_p.h
@@ -42,6 +42,17 @@
 #ifndef QDRAWHELPER_MIPS_DSP_P_H
 #define QDRAWHELPER_MIPS_DSP_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <private/qdrawhelper_p.h>
 
 QT_BEGIN_NAMESPACE
--- src/gui/painting/qt_mips_asm_dsp_p.h
+++ src/gui/painting/qt_mips_asm_dsp_p.h
@@ -42,6 +42,17 @@
 #ifndef QT_MIPS_ASM_DSP_H
 #define QT_MIPS_ASM_DSP_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #if 0
 #pragma qt_sync_stop_processing
 #endif
