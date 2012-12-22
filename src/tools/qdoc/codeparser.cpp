96b48ee0cbd7261c11215226f8dcdfb07fa6ab77
Thiago Macieira <thiago.macieira@intel.com>
2012-12-21 19:11:58 -0800

    Fix warning about unused variable in qdoc
    codeparser.cpp:245:18: error: unused variable ‘dn’ [-Werror=unused-variable]
    
    Change-Id: Id271b3829ba3278e80d4e79746db7aabfabd751a
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

--- src/tools/qdoc/codeparser.cpp
+++ src/tools/qdoc/codeparser.cpp
@@ -242,7 +242,7 @@ void CodeParser::processCommonMetaCommand(const Location& location,
     else if ((command == COMMAND_INGROUP) || (command == COMMAND_INPUBLICGROUP)) {
         // Note: \ingroup and \inpublicgroup are now the same.
         // Not that they were ever different.
-        DocNode* dn = qdb_->addToGroup(arg.first, node);
+        qdb_->addToGroup(arg.first, node);
     }
     else if (command == COMMAND_INMODULE) {
         qdb_->addToModule(arg.first,node);
