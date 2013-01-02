faaaa68db4d36efa239025fc6f9bdbe5d1112eff
Rafael Roquetto <rafael.roquetto.qnx@kdab.com>
2013-01-02 17:28:43 -0200

    Remove redundant Q_OS_BLACKBERRY define check.
    Q_OS_QNX is always implied by Q_OS_BLACKBERRY
    
    Change-Id: Ic07a967ab9a45b3c6c088aa8399bab823b9a413e
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/global/qcompilerdetection.h
+++ src/corelib/global/qcompilerdetection.h
@@ -693,7 +693,7 @@
 #endif /* Q_CC_MSVC */
 
 #ifdef __cplusplus
-# if defined(Q_OS_BLACKBERRY) || defined(Q_OS_QNX)
+# if defined(Q_OS_QNX)
 #  include <utility>
 #  if defined(_YVALS) || defined(_LIBCPP_VER)
 // QNX: libcpp (Dinkumware-based) doesn't have the <initializer_list>
@@ -704,7 +704,7 @@
 #    endif
 #  endif
 # endif
-#endif // Q_OS_BLACKBERRY || Q_OS_QNX
+#endif // Q_OS_QNX
 
 /*
  * C++11 keywords and expressions
