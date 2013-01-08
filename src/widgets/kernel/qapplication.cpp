1327ded7478de7781bcfe57a2eb84dc0c749c91a
Jędrzej Nowacki <jedrzej.nowacki@digia.com>
2013-01-08 13:07:16 +0100

    Fix QVariant in a static build.
    The patch cases registration of QVariant handler for widgets.
    
    Task-number: QTBUG-28528
    Change-Id: I645e8054bb96db0b92edf5df36f206ec1965ad40
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

--- src/widgets/kernel/qapplication.cpp
+++ src/widgets/kernel/qapplication.cpp
@@ -579,6 +579,7 @@ void QApplicationPrivate::construct()
 void qRegisterGuiStateMachine();
 void qUnregisterGuiStateMachine();
 #endif
+extern void qRegisterWidgetsVariant();
 
 /*!
   \fn void QApplicationPrivate::initialize()
@@ -589,6 +590,9 @@ void QApplicationPrivate::initialize()
 {
     QWidgetPrivate::mapper = new QWidgetMapper;
     QWidgetPrivate::allWidgets = new QWidgetSet;
+
+    // needed for a static build.
+    qRegisterWidgetsVariant();
 
     if (application_type != QApplicationPrivate::Tty)
         (void) QApplication::style();  // trigger creation of application style
