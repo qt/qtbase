qt/qtbase

commit 003860b5a74f833f13ae2572924e5b2d34b919b4

fixqt4headers: Remove QtQuick1 and add QtMultimedia
QtQuick1 is QtDeclarative again, so there's no header
file renaming needed. But Multimedia got renamed from
multimediakit and this needs to be included.

Change-Id: Ie787d6841f0525e1dc290f7ca8d9cae148308e32
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

bin/fixqt4headers.pl
@@ -145,13 +145,13 @@ findQtHeaders('QtConcurrent', $qtdir);
 findQtHeaders('QtWidgets', $qtdir);
 findQtHeaders('QtPrintSupport', $qtdir);
 
-if (-d $qtdir . '/include/QtQuick1') {
-    findQtHeaders('QtQuick1', $qtdir);
-} elsif (-d $qtdir . '/../qtdeclarative' ) {
+if (-d $qtdir . '/include/QtMultiMedia') {
+    findQtHeaders('QtMultiMedia', $qtdir);
+    findQtHeaders('QtMultiMediaWidgets', $qtdir);
+} elsif (-d $qtdir . '/../qtmultimedia' ) {
     # This is the case if QTDIR points to a source tree instead of an installed Qt
-    findQtHeaders('QtQuick1', $qtdir . '/../qtdeclarative');
-} else {
-    print "Warning - cannot find QtQuick1 headers\n";
+    findQtHeaders('QtMultiMedia', $qtdir . '/../qtmultimedia');
+    findQtHeaders('QtMultiMediaWidgets', $qtdir . '/../qtmultimedia');
 }
 
 # Support porting from "Qt 4.99" QtDeclarative to QtQuick (QQuickItem et al)

commit f9f83e1ef7b7a2c9b5c9e6b2c74ea6b79eb5d922

fixqt4headers: Fix typo in QtMultimedia headers module name
Commit 003860b5 introduced QtMultimedia, but with a typo. It broke the
script.

Change-Id: Ie3fd536d90f52419818f36ba1b28d437b965dcdc
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

bin/fixqt4headers.pl
@@ -145,13 +145,13 @@ findQtHeaders('QtConcurrent', $qtdir);
 findQtHeaders('QtWidgets', $qtdir);
 findQtHeaders('QtPrintSupport', $qtdir);
 
-if (-d $qtdir . '/include/QtMultiMedia') {
-    findQtHeaders('QtMultiMedia', $qtdir);
-    findQtHeaders('QtMultiMediaWidgets', $qtdir);
+if (-d $qtdir . '/include/QtMultimedia') {
+    findQtHeaders('QtMultimedia', $qtdir);
+    findQtHeaders('QtMultimediaWidgets', $qtdir);
 } elsif (-d $qtdir . '/../qtmultimedia' ) {
     # This is the case if QTDIR points to a source tree instead of an installed Qt
-    findQtHeaders('QtMultiMedia', $qtdir . '/../qtmultimedia');
-    findQtHeaders('QtMultiMediaWidgets', $qtdir . '/../qtmultimedia');
+    findQtHeaders('QtMultimedia', $qtdir . '/../qtmultimedia');
+    findQtHeaders('QtMultimediaWidgets', $qtdir . '/../qtmultimedia');
 }
 
 # Support porting from "Qt 4.99" QtDeclarative to QtQuick (QQuickItem et al)