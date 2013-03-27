95f4751ff313d5e0e410162b7569e3625dfd81d8
Ray Donnelly <mingw.android@gmail.com>
2013-03-27 20:04:15 +0000

    Add some more qconfigs to configureapp.
    REDUCE_EXPORTS -> QT_VISIBILITY_AVAILABLE
    REDUCE_RELOCATIONS -> QT_REDUCE_RELOCATIONS
    !QT_GETIFADDRS -> QT_NO_GETIFADDRS
    
    These will be used for Android build on Windows.
    
    Change-Id: I2cde989e41dc5b7f9bef1345e935cc7e19aba983
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

--- tools/configure/configureapp.cpp
+++ tools/configure/configureapp.cpp
@@ -2673,6 +2673,9 @@ void Configure::generateOutputVars()
     if (dictionary["STACK_PROTECTOR_STRONG"] == "yes")
         qtConfig += "stack-protector-strong";
 
+    if (dictionary["REDUCE_EXPORTS"] == "yes")
+        qtConfig += "reduce_exports";
+
     // We currently have no switch for QtConcurrent, so add it unconditionally.
     qtConfig += "concurrent";
 
@@ -3308,6 +3311,10 @@ void Configure::generateConfigfiles()
         if (dictionary["QT_ICONV"] == "no")          qconfigList += "QT_NO_ICONV";
         if (dictionary["QT_GLIB"] == "no")           qconfigList += "QT_NO_GLIB";
         if (dictionary["QT_INOTIFY"] == "no")        qconfigList += "QT_NO_INOTIFY";
+
+        if (dictionary["REDUCE_EXPORTS"] == "yes")     qconfigList += "QT_VISIBILITY_AVAILABLE";
+        if (dictionary["REDUCE_RELOCATIONS"] == "yes") qconfigList += "QT_REDUCE_RELOCATIONS";
+        if (dictionary["QT_GETIFADDRS"] == "no")       qconfigList += "QT_NO_GETIFADDRS";
 
         qconfigList.sort();
         for (int i = 0; i < qconfigList.count(); ++i)
