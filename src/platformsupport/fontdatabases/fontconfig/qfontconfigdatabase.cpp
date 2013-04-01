qt/qtbase

commit 9b0fab6b62df98519ebfab117f14b9d3465d8c68

Update Qt internals to use QChar::Script
...and remove the outdated QUnicodeTables::Script enum.
QFontEngineData now has one extra slot that never used
(engines[QChar::Script_Inherited]). engines[QChar::Script_Unknown],
if accessed, would be set with a Box engine instance, and could be used
as a minor optimization some time later.

In order to preserve the existing behavior, we map all scripts up to Latin to Common.

Change-Id: Ide4182a0f8447b4bf25713ecc3fe8097b8fed040
Reviewed-by: Pierre Rossi <pierre.rossi@gmail.com>
Reviewed-by: Konstantin Ritt <ritt.ks@gmail.com>

src/platformsupport/fontdatabases/fontconfig/qfontconfigdatabase.cpp
@@ -75,10 +75,11 @@ static inline bool requiresOpenType(int writingSystem)
     return ((writingSystem >= QFontDatabase::Syriac && writingSystem <= QFontDatabase::Sinhala)
             || writingSystem == QFontDatabase::Khmer || writingSystem == QFontDatabase::Nko);
 }
+
 static inline bool scriptRequiresOpenType(int script)
 {
-    return ((script >= QUnicodeTables::Syriac && script <= QUnicodeTables::Sinhala)
-            || script == QUnicodeTables::Khmer || script == QUnicodeTables::Nko);
+    return ((script >= QChar::Script_Syriac && script <= QChar::Script_Sinhala)
+            || script == QChar::Script_Khmer || script == QChar::Script_Nko);
 }
 
 static int getFCWeight(int fc_weight)
@@ -97,14 +98,17 @@ static int getFCWeight(int fc_weight)
 }
 
 static const char *specialLanguages[] = {
-    "en", // Common
+    "", // Unknown
+    "", // Inherited
+    "", // Common
+    "en", // Latin
     "el", // Greek
     "ru", // Cyrillic
     "hy", // Armenian
     "he", // Hebrew
     "ar", // Arabic
     "syr", // Syriac
-    "div", // Thaana
+    "dv", // Thaana
     "hi", // Devanagari
     "bn", // Bengali
     "pa", // Gurmukhi
@@ -121,44 +125,84 @@ static const char *specialLanguages[] = {
     "my", // Myanmar
     "ka", // Georgian
     "ko", // Hangul
-    "", // Ogham
-    "", // Runic
+    "am", // Ethiopic
+    "chr", // Cherokee
+    "cr", // CanadianAboriginal
+    "sga", // Ogham
+    "non", // Runic
     "km", // Khmer
-    "" // N'Ko
+    "mn", // Mongolian
+    "ja", // Hiragana
+    "ja", // Katakana
+    "zh", // Bopomofo
+    "zh", // Han
+    "ii", // Yi
+    "ett", // OldItalic
+    "got", // Gothic
+    "en", // Deseret
+    "fil", // Tagalog
+    "hnn", // Hanunoo
+    "bku", // Buhid
+    "tbw", // Tagbanwa
+    "cop", // Coptic
+    "lif", // Limbu
+    "tdd", // TaiLe
+    "grc", // LinearB
+    "uga", // Ugaritic
+    "en", // Shavian
+    "so", // Osmanya
+    "grc", // Cypriot
+    "", // Braille
+    "bug", // Buginese
+    "khb", // NewTaiLue
+    "cu", // Glagolitic
+    "shi", // Tifinagh
+    "syl", // SylotiNagri
+    "peo", // OldPersian
+    "pra", // Kharoshthi
+    "ban", // Balinese
+    "akk", // Cuneiform
+    "phn", // Phoenician
+    "lzh", // PhagsPa
+    "man", // Nko
+    "su", // Sundanese
+    "lep", // Lepcha
+    "sat", // OlChiki
+    "vai", // Vai
+    "saz", // Saurashtra
+    "eky", // KayahLi
+    "rej", // Rejang
+    "xlc", // Lycian
+    "xcr", // Carian
+    "xld", // Lydian
+    "cjm", // Cham
+    "nod", // TaiTham
+    "blt", // TaiViet
+    "ae", // Avestan
+    "egy", // EgyptianHieroglyphs
+    "smp", // Samaritan
+    "lis", // Lisu
+    "bax", // Bamum
+    "jv", // Javanese
+    "mni", // MeeteiMayek
+    "arc", // ImperialAramaic
+    "xsa", // OldSouthArabian
+    "xpr", // InscriptionalParthian
+    "pal", // InscriptionalPahlavi
+    "otk", // OldTurkic
+    "bh", // Kaithi
+    "bbc", // Batak
+    "pra", // Brahmi
+    "myz", // Mandaic
+    "ccp", // Chakma
+    "xmr", // MeroiticCursive
+    "xmr", // MeroiticHieroglyphs
+    "hmd", // Miao
+    "sa", // Sharada
+    "srb", // SoraSompeng
+    "doi"  // Takri
 };
 enum { SpecialLanguageCount = sizeof(specialLanguages) / sizeof(const char *) };
-
-static const ushort specialChars[] = {
-    0, // English
-    0, // Greek
-    0, // Cyrillic
-    0, // Armenian
-    0, // Hebrew
-    0, // Arabic
-    0, // Syriac
-    0, // Thaana
-    0, // Devanagari
-    0, // Bengali
-    0, // Gurmukhi
-    0, // Gujarati
-    0, // Oriya
-    0, // Tamil
-    0xc15, // Telugu
-    0xc95, // Kannada
-    0xd15, // Malayalam
-    0xd9a, // Sinhala
-    0, // Thai
-    0, // Lao
-    0, // Tibetan
-    0x1000, // Myanmar
-    0, // Georgian
-    0, // Hangul
-    0x1681, // Ogham
-    0x16a0, // Runic
-    0,  // Khmer
-    0x7ca // N'Ko
-};
-enum { SpecialCharCount = sizeof(specialChars) / sizeof(ushort) };
 
 // this could become a list of all languages used for each writing
 // system, instead of using the single most common language.
@@ -194,9 +238,9 @@ static const char *languageForWritingSystem[] = {
     "ko",  // Korean
     "vi",  // Vietnamese
     0, // Symbol
-    0, // Ogham
-    0, // Runic
-    0 // N'Ko
+    "sga", // Ogham
+    "non", // Runic
+    "man" // N'Ko
 };
 enum { LanguageCount = sizeof(languageForWritingSystem) / sizeof(const char *) };
 
@@ -477,13 +521,12 @@ void QFontconfigDatabase::populateFontDatabase()
 //    QApplication::setFont(font);
 }
 
-QFontEngineMulti *QFontconfigDatabase::fontEngineMulti(QFontEngine *fontEngine,
-                                                       QUnicodeTables::Script script)
+QFontEngineMulti *QFontconfigDatabase::fontEngineMulti(QFontEngine *fontEngine, QChar::Script script)
 {
     return new QFontEngineMultiFontConfig(fontEngine, script);
 }
 
-QFontEngine *QFontconfigDatabase::fontEngine(const QFontDef &f, QUnicodeTables::Script script, void *usrPtr)
+QFontEngine *QFontconfigDatabase::fontEngine(const QFontDef &f, QChar::Script script, void *usrPtr)
 {
     if (!usrPtr)
         return 0;
@@ -601,7 +644,7 @@ QFontEngine *QFontconfigDatabase::fontEngine(const QFontDef &f, QUnicodeTables::
     return engine;
 }
 
-QStringList QFontconfigDatabase::fallbacksForFamily(const QString family, const QFont::Style &style, const QFont::StyleHint &styleHint, const QUnicodeTables::Script &script) const
+QStringList QFontconfigDatabase::fallbacksForFamily(const QString &family, QFont::Style style, QFont::StyleHint styleHint, QChar::Script script) const
 {
     QStringList fallbackFamilies;
     FcPattern *pattern = FcPatternCreate();
@@ -621,8 +664,8 @@ QStringList QFontconfigDatabase::fallbacksForFamily(const QString family, const 
         slant_value = FC_SLANT_OBLIQUE;
     FcPatternAddInteger(pattern, FC_SLANT, slant_value);
 
-    if (script != QUnicodeTables::Common && *specialLanguages[script] != '\0') {
-        Q_ASSERT(script < QUnicodeTables::ScriptCount);
+    Q_ASSERT(uint(script) < SpecialLanguageCount);
+    if (*specialLanguages[script] != '\0') {
         FcLangSet *ls = FcLangSetCreate();
         FcLangSetAdd(ls, (const FcChar8*)specialLanguages[script]);
         FcPatternAddLangSet(pattern, FC_LANG, ls);

src/plugins/platforms/windows/qwindowsfontdatabase.cpp
@@ -1132,9 +1132,7 @@ QWindowsFontDatabase::~QWindowsFontDatabase()
     removeApplicationFonts();
 }
 
-QFontEngine * QWindowsFontDatabase::fontEngine(const QFontDef &fontDef,
-                                              QUnicodeTables::Script script,
-                                              void *handle)
+QFontEngine * QWindowsFontDatabase::fontEngine(const QFontDef &fontDef, QChar::Script script, void *handle)
 {
     QFontEngine *fe = QWindowsFontDatabase::createEngine(script, fontDef,
                                               0, QWindowsContext::instance()->defaultDPI(), false,
@@ -1187,7 +1185,7 @@ QFontEngine *QWindowsFontDatabase::fontEngine(const QByteArray &fontData, qreal 
             request.styleStrategy = QFont::NoFontMerging | QFont::PreferMatch;
             request.hintingPreference = hintingPreference;
 
-            fontEngine = QWindowsFontDatabase::createEngine(QUnicodeTables::Common, request, 0,
+            fontEngine = QWindowsFontDatabase::createEngine(QChar::Script_Common, request, 0,
                     QWindowsContext::instance()->defaultDPI(), false, QStringList(),
                     m_fontEngineData);
 
@@ -1300,7 +1298,7 @@ QFontEngine *QWindowsFontDatabase::fontEngine(const QByteArray &fontData, qreal 
     return fontEngine;
 }
 
-QStringList QWindowsFontDatabase::fallbacksForFamily(const QString family, const QFont::Style &style, const QFont::StyleHint &styleHint, const QUnicodeTables::Script &script) const
+QStringList QWindowsFontDatabase::fallbacksForFamily(const QString &family, QFont::Style style, QFont::StyleHint styleHint, QChar::Script script) const
 {
     QStringList result = QPlatformFontDatabase::fallbacksForFamily(family, style, styleHint, script);
     if (!result.isEmpty())
@@ -1538,8 +1536,8 @@ HFONT QWindowsFontDatabase::systemFont()
 
 static inline bool scriptRequiresOpenType(int script)
 {
-    return ((script >= QUnicodeTables::Syriac && script <= QUnicodeTables::Sinhala)
-            || script == QUnicodeTables::Khmer || script == QUnicodeTables::Nko);
+    return ((script >= QChar::Script_Syriac && script <= QChar::Script_Sinhala)
+            || script == QChar::Script_Khmer || script == QChar::Script_Nko);
 }
 
 static const char *other_tryFonts[] = {
@@ -1850,7 +1848,7 @@ QFontEngine *QWindowsFontDatabase::createEngine(int script, const QFontDef &requ
         directWriteFont->Release();
 #endif
 
-    if(script == QUnicodeTables::Common
+    if (script == QChar::Script_Common
        && !(request.styleStrategy & QFont::NoFontMerging)) {
        QFontDatabase db;
        if (!db.writingSystems(request.family).contains(QFontDatabase::Symbol)) {

src/plugins/platforms/windows/qwindowsfontdatabase_ft.cpp
@@ -362,7 +362,7 @@ void QWindowsFontDatabaseFT::populate(const QString &family)
     ReleaseDC(0, dummy);
 }
 
-QFontEngine * QWindowsFontDatabaseFT::fontEngine(const QFontDef &fontDef, QUnicodeTables::Script script, void *handle)
+QFontEngine * QWindowsFontDatabaseFT::fontEngine(const QFontDef &fontDef, QChar::Script script, void *handle)
 {
     QFontEngine *fe = QBasicFontDatabase::fontEngine(fontDef, script, handle);
     if (QWindowsContext::verboseFonts)
@@ -430,9 +430,9 @@ static const char *kr_tryFonts[] = {
 
 static const char **tryFonts = 0;
 
-QStringList QWindowsFontDatabaseFT::fallbacksForFamily(const QString family, const QFont::Style &style, const QFont::StyleHint &styleHint, const QUnicodeTables::Script &script) const
+QStringList QWindowsFontDatabaseFT::fallbacksForFamily(const QString &family, QFont::Style style, QFont::StyleHint styleHint, QChar::Script script) const
 {
-    if(script == QUnicodeTables::Common) {
+    if (script == QChar::Script_Common) {
 //            && !(request.styleStrategy & QFont::NoFontMerging)) {
         QFontDatabase db;
         if (!db.writingSystems(family).contains(QFontDatabase::Symbol)) {
@@ -518,8 +518,8 @@ HFONT QWindowsFontDatabaseFT::systemFont()
 
 static inline bool scriptRequiresOpenType(int script)
 {
-    return ((script >= QUnicodeTables::Syriac && script <= QUnicodeTables::Sinhala)
-            || script == QUnicodeTables::Khmer || script == QUnicodeTables::Nko);
+    return ((script >= QChar::Script_Syriac && script <= QChar::Script_Sinhala)
+            || script == QChar::Script_Khmer || script == QChar::Script_Nko);
 }
 
 static inline int verticalDPI()

src/corelib/tools/qharfbuzz_p.h
@@ -53,10 +53,85 @@
 #ifndef QHARFBUZZ_P_H
 #define QHARFBUZZ_P_H
 
-#include <QtCore/qglobal.h>
+#include <QtCore/qchar.h>
+
 #include <harfbuzz-shaper.h>
 
 QT_BEGIN_NAMESPACE
+
+static inline HB_Script script_to_hbscript(uchar script)
+{
+    switch (script) {
+    case QChar::Script_Inherited: return HB_Script_Inherited;
+    case QChar::Script_Common: return HB_Script_Common;
+    case QChar::Script_Arabic: return HB_Script_Arabic;
+    case QChar::Script_Armenian: return HB_Script_Armenian;
+    case QChar::Script_Bengali: return HB_Script_Bengali;
+    case QChar::Script_Cyrillic: return HB_Script_Cyrillic;
+    case QChar::Script_Devanagari: return HB_Script_Devanagari;
+    case QChar::Script_Georgian: return HB_Script_Georgian;
+    case QChar::Script_Greek: return HB_Script_Greek;
+    case QChar::Script_Gujarati: return HB_Script_Gujarati;
+    case QChar::Script_Gurmukhi: return HB_Script_Gurmukhi;
+    case QChar::Script_Hangul: return HB_Script_Hangul;
+    case QChar::Script_Hebrew: return HB_Script_Hebrew;
+    case QChar::Script_Kannada: return HB_Script_Kannada;
+    case QChar::Script_Khmer: return HB_Script_Khmer;
+    case QChar::Script_Lao: return HB_Script_Lao;
+    case QChar::Script_Malayalam: return HB_Script_Malayalam;
+    case QChar::Script_Myanmar: return HB_Script_Myanmar;
+    case QChar::Script_Ogham: return HB_Script_Ogham;
+    case QChar::Script_Oriya: return HB_Script_Oriya;
+    case QChar::Script_Runic: return HB_Script_Runic;
+    case QChar::Script_Sinhala: return HB_Script_Sinhala;
+    case QChar::Script_Syriac: return HB_Script_Syriac;
+    case QChar::Script_Tamil: return HB_Script_Tamil;
+    case QChar::Script_Telugu: return HB_Script_Telugu;
+    case QChar::Script_Thaana: return HB_Script_Thaana;
+    case QChar::Script_Thai: return HB_Script_Thai;
+    case QChar::Script_Tibetan: return HB_Script_Tibetan;
+    case QChar::Script_Nko: return HB_Script_Nko;
+    default: break;
+    };
+    return HB_Script_Common;
+}
+
+static inline uchar hbscript_to_script(uchar script)
+{
+    switch (script) {
+    case HB_Script_Inherited: return QChar::Script_Inherited;
+    case HB_Script_Common: return QChar::Script_Common;
+    case HB_Script_Arabic: return QChar::Script_Arabic;
+    case HB_Script_Armenian: return QChar::Script_Armenian;
+    case HB_Script_Bengali: return QChar::Script_Bengali;
+    case HB_Script_Cyrillic: return QChar::Script_Cyrillic;
+    case HB_Script_Devanagari: return QChar::Script_Devanagari;
+    case HB_Script_Georgian: return QChar::Script_Georgian;
+    case HB_Script_Greek: return QChar::Script_Greek;
+    case HB_Script_Gujarati: return QChar::Script_Gujarati;
+    case HB_Script_Gurmukhi: return QChar::Script_Gurmukhi;
+    case HB_Script_Hangul: return QChar::Script_Hangul;
+    case HB_Script_Hebrew: return QChar::Script_Hebrew;
+    case HB_Script_Kannada: return QChar::Script_Kannada;
+    case HB_Script_Khmer: return QChar::Script_Khmer;
+    case HB_Script_Lao: return QChar::Script_Lao;
+    case HB_Script_Malayalam: return QChar::Script_Malayalam;
+    case HB_Script_Myanmar: return QChar::Script_Myanmar;
+    case HB_Script_Ogham: return QChar::Script_Ogham;
+    case HB_Script_Oriya: return QChar::Script_Oriya;
+    case HB_Script_Runic: return QChar::Script_Runic;
+    case HB_Script_Sinhala: return QChar::Script_Sinhala;
+    case HB_Script_Syriac: return QChar::Script_Syriac;
+    case HB_Script_Tamil: return QChar::Script_Tamil;
+    case HB_Script_Telugu: return QChar::Script_Telugu;
+    case HB_Script_Thaana: return QChar::Script_Thaana;
+    case HB_Script_Thai: return QChar::Script_Thai;
+    case HB_Script_Tibetan: return QChar::Script_Tibetan;
+    case HB_Script_Nko: return QChar::Script_Nko;
+    default: break;
+    };
+    return QChar::Script_Common;
+}
 
 Q_CORE_EXPORT HB_Bool qShapeItem(HB_ShaperItem *item);
 

src/corelib/tools/qtextboundaryfinder.cpp
@@ -41,7 +41,6 @@
 #include <QtCore/qtextboundaryfinder.h>
 #include <QtCore/qvarlengtharray.h>
 
-#include <private/qunicodetables_p.h>
 #include <private/qunicodetools_p.h>
 
 QT_BEGIN_NAMESPACE
@@ -61,15 +60,15 @@ static void init(QTextBoundaryFinder::BoundaryType type, const QChar *chars, int
     // correctly assign script, isTab and isObject to the script analysis
     const ushort *uc = unicode;
     const ushort *e = uc + length;
-    int script = QUnicodeTables::Common;
-    int lastScript = QUnicodeTables::Common;
+    uchar script = QChar::Script_Common;
+    uchar lastScript = QChar::Script_Common;
     const ushort *start = uc;
     while (uc < e) {
-        int s = QUnicodeTables::script(*uc);
-        if (s != QUnicodeTables::Inherited)
+        int s = QChar::script(*uc);
+        if (s != QChar::Script_Inherited)
             script = s;
         if (*uc == QChar::ObjectReplacementCharacter || *uc == QChar::LineSeparator || *uc == 9) 
-            script = QUnicodeTables::Common;
+            script = QChar::Script_Common;
         if (script != lastScript) {
             if (uc != start) {
                 QUnicodeTools::ScriptItem item;

src/corelib/tools/qunicodetables.cpp
@@ -6724,43 +6724,6 @@ Q_CORE_EXPORT LineBreakClass QT_FASTCALL lineBreakClass(uint ucs4)
     return (LineBreakClass)qGetProp(ucs4)->lineBreakClass;
 }
 
-Q_CORE_EXPORT Script QT_FASTCALL script(uint ucs4)
-{
-    switch (qGetProp(ucs4)->script) {
-    case QChar::Script_Inherited: return Inherited;
-    case QChar::Script_Common: return Common;
-    case QChar::Script_Arabic: return Arabic;
-    case QChar::Script_Armenian: return Armenian;
-    case QChar::Script_Bengali: return Bengali;
-    case QChar::Script_Cyrillic: return Cyrillic;
-    case QChar::Script_Devanagari: return Devanagari;
-    case QChar::Script_Georgian: return Georgian;
-    case QChar::Script_Greek: return Greek;
-    case QChar::Script_Gujarati: return Gujarati;
-    case QChar::Script_Gurmukhi: return Gurmukhi;
-    case QChar::Script_Hangul: return Hangul;
-    case QChar::Script_Hebrew: return Hebrew;
-    case QChar::Script_Kannada: return Kannada;
-    case QChar::Script_Khmer: return Khmer;
-    case QChar::Script_Lao: return Lao;
-    case QChar::Script_Malayalam: return Malayalam;
-    case QChar::Script_Myanmar: return Myanmar;
-    case QChar::Script_Ogham: return Ogham;
-    case QChar::Script_Oriya: return Oriya;
-    case QChar::Script_Runic: return Runic;
-    case QChar::Script_Sinhala: return Sinhala;
-    case QChar::Script_Syriac: return Syriac;
-    case QChar::Script_Tamil: return Tamil;
-    case QChar::Script_Telugu: return Telugu;
-    case QChar::Script_Thaana: return Thaana;
-    case QChar::Script_Thai: return Thai;
-    case QChar::Script_Tibetan: return Tibetan;
-    case QChar::Script_Nko: return Nko;
-    default: break;
-    };
-    return Common;
-}
-
 
 static const ushort specialCaseMap[] = {
     0x0, // placeholder

src/corelib/tools/qunicodetables_p.h
@@ -89,113 +89,6 @@ struct Properties {
 Q_CORE_EXPORT const Properties * QT_FASTCALL properties(uint ucs4);
 Q_CORE_EXPORT const Properties * QT_FASTCALL properties(ushort ucs2);
 
-// See http://www.unicode.org/reports/tr24/tr24-5.html
-enum Script {
-    Common,
-    Greek,
-    Cyrillic,
-    Armenian,
-    Hebrew,
-    Arabic,
-    Syriac,
-    Thaana,
-    Devanagari,
-    Bengali,
-    Gurmukhi,
-    Gujarati,
-    Oriya,
-    Tamil,
-    Telugu,
-    Kannada,
-    Malayalam,
-    Sinhala,
-    Thai,
-    Lao,
-    Tibetan,
-    Myanmar,
-    Georgian,
-    Hangul,
-    Ogham,
-    Runic,
-    Khmer,
-    Nko,
-    Inherited,
-    ScriptCount = Inherited,
-    Latin = Common,
-    Ethiopic = Common,
-    Cherokee = Common,
-    CanadianAboriginal = Common,
-    Mongolian = Common,
-    Hiragana = Common,
-    Katakana = Common,
-    Bopomofo = Common,
-    Han = Common,
-    Yi = Common,
-    OldItalic = Common,
-    Gothic = Common,
-    Deseret = Common,
-    Tagalog = Common,
-    Hanunoo = Common,
-    Buhid = Common,
-    Tagbanwa = Common,
-    Limbu = Common,
-    TaiLe = Common,
-    LinearB = Common,
-    Ugaritic = Common,
-    Shavian = Common,
-    Osmanya = Common,
-    Cypriot = Common,
-    Braille = Common,
-    Buginese = Common,
-    Coptic = Common,
-    NewTaiLue = Common,
-    Glagolitic = Common,
-    Tifinagh = Common,
-    SylotiNagri = Common,
-    OldPersian = Common,
-    Kharoshthi = Common,
-    Balinese = Common,
-    Cuneiform = Common,
-    Phoenician = Common,
-    PhagsPa = Common,
-    Sundanese = Common,
-    Lepcha = Common,
-    OlChiki = Common,
-    Vai = Common,
-    Saurashtra = Common,
-    KayahLi = Common,
-    Rejang = Common,
-    Lycian = Common,
-    Carian = Common,
-    Lydian = Common,
-    Cham = Common,
-    TaiTham = Common,
-    TaiViet = Common,
-    Avestan = Common,
-    EgyptianHieroglyphs = Common,
-    Samaritan = Common,
-    Lisu = Common,
-    Bamum = Common,
-    Javanese = Common,
-    MeeteiMayek = Common,
-    ImperialAramaic = Common,
-    OldSouthArabian = Common,
-    InscriptionalParthian = Common,
-    InscriptionalPahlavi = Common,
-    OldTurkic = Common,
-    Kaithi = Common,
-    Batak = Common,
-    Brahmi = Common,
-    Mandaic = Common,
-    Chakma = Common,
-    MeroiticCursive = Common,
-    MeroiticHieroglyphs = Common,
-    Miao = Common,
-    Sharada = Common,
-    SoraSompeng = Common,
-    Takri = Common
-};
-
 enum GraphemeBreakClass {
     GraphemeBreak_Other,
     GraphemeBreak_CR,
@@ -273,10 +166,6 @@ inline SentenceBreakClass sentenceBreakClass(QChar ch)
 Q_CORE_EXPORT LineBreakClass QT_FASTCALL lineBreakClass(uint ucs4);
 inline LineBreakClass lineBreakClass(QChar ch)
 { return lineBreakClass(ch.unicode()); }
-
-Q_CORE_EXPORT Script QT_FASTCALL script(uint ucs4);
-inline Script script(QChar ch)
-{ return script(ch.unicode()); }
 
 } // namespace QUnicodeTables
 

src/corelib/tools/qunicodetools.cpp
@@ -44,7 +44,7 @@
 #include "qunicodetables_p.h"
 #include "qvarlengtharray.h"
 
-#include <harfbuzz-shaper.h>
+#include "qharfbuzz_p.h"
 
 #define FLAG(x) (1 << (x))
 
@@ -615,7 +615,7 @@ Q_CORE_EXPORT void initCharAttributes(const ushort *string, int length,
             HB_ScriptItem item;
             item.pos = items[start].position;
             item.length = items[i].position - items[start].position;
-            item.script = (HB_Script)items[start].script;
+            item.script = script_to_hbscript(items[start].script);
             item.bidiLevel = 0; // unused
             scriptItems.append(item);
             start = i;
@@ -624,7 +624,7 @@ Q_CORE_EXPORT void initCharAttributes(const ushort *string, int length,
             HB_ScriptItem item;
             item.pos = items[start].position;
             item.length = length - items[start].position;
-            item.script = (HB_Script)items[start].script;
+            item.script = script_to_hbscript(items[start].script);
             item.bidiLevel = 0; // unused
             scriptItems.append(item);
         }

commit 0fcadcca3d0842354de07ffaa8c622e607aab22c

QFontConfigDatabase: fix fallbacksForFamily() for CJK languages
For Han Unicode script, the language couldn't be determined algorithmically,
the only way is guessing based on the user's locale.

The is a regression introduced in 9b0fab6b62df98519ebfab117f14b9d3465d8c68

Change-Id: I84645885a825fdfb6c268edaf10185bf5e447eb5
Reviewed-by: Lars Knoll <lars.knoll@digia.com>

src/platformsupport/fontdatabases/fontconfig/qfontconfigdatabase.cpp
@@ -134,8 +134,8 @@ static const char *specialLanguages[] = {
     "mn", // Mongolian
     "ja", // Hiragana
     "ja", // Katakana
-    "zh", // Bopomofo
-    "zh", // Han
+    "zh-TW", // Bopomofo
+    "", // Han
     "ii", // Yi
     "ett", // OldItalic
     "got", // Gothic
@@ -697,11 +697,11 @@ QStringList QFontconfigDatabase::fallbacksForFamily(const QString &family, QFont
         FcPatternAddLangSet(pattern, FC_LANG, ls);
         FcLangSetDestroy(ls);
     } else if (!family.isEmpty()) {
-        // If script is common then it may include languages like CJK,
+        // If script is Common or Han, then it may include languages like CJK,
         // we should attach system default language set to the pattern
         // to obtain correct font fallback list (i.e. if LANG=zh_CN
         // then we normally want to use a Chinese font for CJK text;
-        // while a Japanese font should be use for that if LANG=ja)
+        // while a Japanese font should be used for that if LANG=ja)
         FcPattern *dummy = FcPatternCreate();
         FcDefaultSubstitute(dummy);
         FcChar8 *lang = 0;

src/plugins/platforms/windows/qwindowsfontdatabase.cpp
@@ -1790,7 +1790,7 @@ QFontEngine *QWindowsFontDatabase::createEngine(int script, const QFontDef &requ
         directWriteFont->Release();
 #endif
 
-    if (script == QChar::Script_Common
+    if ((script == QChar::Script_Common || script == QChar::Script_Han)
             && !(request.styleStrategy & QFont::NoFontMerging)) {
         QStringList extraFonts = extraTryFontsForFamily(request.family);
         if (extraFonts.size()) {

src/plugins/platforms/windows/qwindowsfontdatabase_ft.cpp
@@ -432,7 +432,7 @@ static const char **tryFonts = 0;
 
 QStringList QWindowsFontDatabaseFT::fallbacksForFamily(const QString &family, QFont::Style style, QFont::StyleHint styleHint, QChar::Script script) const
 {
-    if (script == QChar::Script_Common) {
+    if (script == QChar::Script_Common || script == QChar::Script_Han) {
 //            && !(request.styleStrategy & QFont::NoFontMerging)) {
         QFontDatabase db;
         if (!db.writingSystems(family).contains(QFontDatabase::Symbol)) {