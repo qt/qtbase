722798a359761a1eb635d18547b076615f192508
Stephen Kelly <stephen.kelly@kdab.com>
2013-04-02 17:52:54 +0200

    Don't call virtual methods after the source model is destroyed.
    Calling clear_mapping causes the persistent indexes to be queried, and
    mapped using map_to_source, so that they can be restored later. That
    is not the appropriate response to the source model being deleted
    because there won't be anything to restore.
    
    Simply clear the stored mapping information instead so that the source model
    actually exists when mapToSource is called by the framework.
    
    Change-Id: I99692ee7aa9c6714aec45c68fe4a2d62be189d60
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

--- src/corelib/itemmodels/qabstractproxymodel.cpp
+++ src/corelib/itemmodels/qabstractproxymodel.cpp
@@ -91,6 +91,7 @@ QT_BEGIN_NAMESPACE
 //detects the deletion of the source model
 void QAbstractProxyModelPrivate::_q_sourceModelDestroyed()
 {
+    invalidatePersistentIndexes();
     model = QAbstractItemModelPrivate::staticEmptyModel();
 }
 
