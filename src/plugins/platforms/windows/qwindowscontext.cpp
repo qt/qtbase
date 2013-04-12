qt/qtbase

commit 11e9f1368be986d0e128e8f7b2423d9cc7dd5436

Windows: Fix reported screen metrics.
A call to SetProcessDPIAware() is required in some cases.

Task-number: QTBUG-30063
Change-Id: Iba0203d76c8e7068bf9fd4581770c1aca76a4708
Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
Reviewed-by: Oliver Wolff <oliver.wolff@digia.com>

src/plugins/platforms/windows/qwindowscontext.cpp
@@ -172,7 +172,7 @@ QWindowsUser32DLL::QWindowsUser32DLL() :
     updateLayeredWindowIndirect(0),
     isHungAppWindow(0),
     registerTouchWindow(0), unregisterTouchWindow(0),
-    getTouchInputInfo(0), closeTouchInputHandle(0)
+    getTouchInputInfo(0), closeTouchInputHandle(0), setProcessDPIAware(0)
 {
 }
 
@@ -187,6 +187,7 @@ void QWindowsUser32DLL::init()
 
     updateLayeredWindowIndirect = (UpdateLayeredWindowIndirect)(library.resolve("UpdateLayeredWindowIndirect"));
     isHungAppWindow = (IsHungAppWindow)library.resolve("IsHungAppWindow");
+    setProcessDPIAware = (SetProcessDPIAware)library.resolve("SetProcessDPIAware");
 }
 
 bool QWindowsUser32DLL::initTouch()
@@ -252,7 +253,7 @@ struct QWindowsContextPrivate {
     QSet<QString> m_registeredWindowClassNames;
     HandleBaseWindowHash m_windows;
     HDC m_displayContext;
-    const int m_defaultDPI;
+    int m_defaultDPI;
     QWindowsKeyMapper m_keyMapper;
     QWindowsMouseHandler m_mouseHandler;
     QWindowsMimeConverter m_mimeConverter;
@@ -266,8 +267,6 @@ struct QWindowsContextPrivate {
 
 QWindowsContextPrivate::QWindowsContextPrivate() :
     m_systemInfo(0),
-    m_displayContext(GetDC(0)),
-    m_defaultDPI(GetDeviceCaps(m_displayContext,LOGPIXELSY)),
     m_oleInitializeResult(OleInitialize(NULL)),
     m_eventType(QByteArrayLiteral("windows_generic_MSG")),
     m_lastActiveWindow(0), m_asyncExpose(0)
@@ -276,6 +275,11 @@ QWindowsContextPrivate::QWindowsContextPrivate() :
     QWindowsContext::user32dll.init();
     QWindowsContext::shell32dll.init();
 #endif
+    // Ensure metrics functions report correct data, QTBUG-30063.
+    if (QWindowsContext::user32dll.setProcessDPIAware)
+        QWindowsContext::user32dll.setProcessDPIAware();
+    m_displayContext = GetDC(0);
+    m_defaultDPI = GetDeviceCaps(m_displayContext, LOGPIXELSY);
 
     const QSysInfo::WinVersion ver = QSysInfo::windowsVersion();
 #ifndef Q_OS_WINCE

src/plugins/platforms/windows/qwindowscontext.h
@@ -80,6 +80,7 @@ struct QWindowsUser32DLL
                  const BLENDFUNCTION *, DWORD);
     typedef BOOL (WINAPI *UpdateLayeredWindowIndirect)(HWND, const UPDATELAYEREDWINDOWINFO *);
     typedef BOOL (WINAPI *IsHungAppWindow)(HWND);
+    typedef BOOL (WINAPI *SetProcessDPIAware)();
 
     // Functions missing in Q_CC_GNU stub libraries.
     SetLayeredWindowAttributes setLayeredWindowAttributes;
@@ -94,6 +95,9 @@ struct QWindowsUser32DLL
     UnregisterTouchWindow unregisterTouchWindow;
     GetTouchInputInfo getTouchInputInfo;
     CloseTouchInputHandle closeTouchInputHandle;
+
+    // Windows Vista onwards
+    SetProcessDPIAware setProcessDPIAware;
 };
 
 struct QWindowsShell32DLL

commit f8a047d8435581d076e6bd4c9727209104a7fc84

Fix Windows CE compilation.
Breakage introduced by 11e9f1368be986d0e128e8f7b2423d9cc7dd5436 .

Simplify #ifdef-blocks.

Task-number: QTBUG-30063
Change-Id: I0710915ff7b0ad5947935904c43ff9b621f638f2
Reviewed-by: Andreas Holzammer <andreas.holzammer@kdab.com>
Reviewed-by: Björn Breitmeyer <bjoern.breitmeyer@kdab.com>
Reviewed-by: Janne Anttila <janne.anttila@digia.com>

src/plugins/platforms/windows/qwindowscontext.cpp
@@ -271,22 +271,19 @@ QWindowsContextPrivate::QWindowsContextPrivate() :
     m_eventType(QByteArrayLiteral("windows_generic_MSG")),
     m_lastActiveWindow(0), m_asyncExpose(0)
 {
+    const QSysInfo::WinVersion ver = QSysInfo::windowsVersion();
 #ifndef Q_OS_WINCE
     QWindowsContext::user32dll.init();
     QWindowsContext::shell32dll.init();
-#endif
     // Ensure metrics functions report correct data, QTBUG-30063.
     if (QWindowsContext::user32dll.setProcessDPIAware)
         QWindowsContext::user32dll.setProcessDPIAware();
-    m_displayContext = GetDC(0);
-    m_defaultDPI = GetDeviceCaps(m_displayContext, LOGPIXELSY);
 
-    const QSysInfo::WinVersion ver = QSysInfo::windowsVersion();
-#ifndef Q_OS_WINCE
     if (hasTouchSupport(ver) && QWindowsContext::user32dll.initTouch())
         m_systemInfo |= QWindowsContext::SI_SupportsTouch;
-#endif
-
+#endif // !Q_OS_WINCE
+    m_displayContext = GetDC(0);
+    m_defaultDPI = GetDeviceCaps(m_displayContext, LOGPIXELSY);
     if (useRTL_Extensions(ver)) {
         m_systemInfo |= QWindowsContext::SI_RTL_Extensions;
         m_keyMapper.setUseRTLExtensions(true);