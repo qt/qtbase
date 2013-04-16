qt/qtbase

commit 244cc5da55b92bf08f32da01d7557decbd9ba66c

FontConfig: Correctly report Symbol writing system support
When all the Q_WS_X11 were removed, we also removed the special
case for symbol fonts in QFontDatabase for FontConfig. This
was checkSymbolFont() in Qt 4, and this patch copies the
code and adapts it to Qt 5.

Task-number: QTBUG-30044
Change-Id: I346f208052013eae9de3c33c210bf5db1d2aef87
Reviewed-by: Konstantin Ritt <ritt.ks@gmail.com>

src/platformsupport/fontdatabases/fontconfig/qfontconfigdatabase.cpp
@@ -350,6 +350,26 @@ static const char *getFcFamilyForStyleHint(const QFont::StyleHint style)
     return stylehint;
 }
 
+static bool isSymbolFont(FontFile *fontFile)
+{
+    if (fontFile == 0 || fontFile->fileName.isEmpty())
+        return false;
+
+    QFontEngine::FaceId id;
+    id.filename = fontFile->fileName.toLocal8Bit();
+    id.index = fontFile->indexValue;
+
+    QFreetypeFace *f = QFreetypeFace::getFace(id);
+    if (f == 0) {
+        qWarning("isSymbolFont: Couldn't open face %s/%d", id.filename.data(), id.index);
+        return false;
+    }
+
+    bool hasSymbolMap = f->symbol_map;
+    f->release(id);
+    return hasSymbolMap;
+}
+
 void QFontconfigDatabase::populateFontDatabase()
 {
     FcFontSet  *fonts;
@@ -468,6 +488,9 @@ void QFontconfigDatabase::populateFontDatabase()
         FontFile *fontFile = new FontFile;
         fontFile->fileName = QLatin1String((const char *)file_value);
         fontFile->indexValue = indexValue;
+
+        if (isSymbolFont(fontFile))
+            writingSystems.setSupported(QFontDatabase::Other);
 
         QFont::Style style = (slant_value == FC_SLANT_ITALIC)
                          ? QFont::StyleItalic

commit 201bd2e59ac2ed19deb75809dbf41460dffad758

QFontConfigDatabase: Fix performance regression
..introduced by 244cc5da55b92bf08f32da01d7557decbd9ba66c
Symbol fonts usually don't have code ranges support except Latin.
Don't load FT face for font that doesn't look like a symbol font.

Change-Id: Iec46aa84e27227e0bda5d50f96a2b2f75f58e950
Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@digia.com>

src/platformsupport/fontdatabases/fontconfig/qfontconfigdatabase.cpp
@@ -491,8 +491,22 @@ void QFontconfigDatabase::populateFontDatabase()
         fontFile->fileName = QLatin1String((const char *)file_value);
         fontFile->indexValue = indexValue;
 
-        if (isSymbolFont(fontFile))
-            writingSystems.setSupported(QFontDatabase::Other);
+        if (!writingSystems.supported(QFontDatabase::Symbol)) {
+            // Symbol encoding used to encode various crap in the 32..255 character
+            // code range, which belongs to Latin character code range.
+            // Symbol fonts usually don't have any other code ranges support.
+            bool mightBeSymbolFont = true;
+            for (int j = 2; j < QFontDatabase::WritingSystemsCount; ++j) {
+                if (writingSystems.supported(QFontDatabase::WritingSystem(j))) {
+                    mightBeSymbolFont = false;
+                    break;
+                }
+            }
+            if (mightBeSymbolFont && isSymbolFont(fontFile)) {
+                writingSystems.setSupported(QFontDatabase::Latin, false);
+                writingSystems.setSupported(QFontDatabase::Symbol);
+            }
+        }
 
         QFont::Style style = (slant_value == FC_SLANT_ITALIC)
                          ? QFont::StyleItalic