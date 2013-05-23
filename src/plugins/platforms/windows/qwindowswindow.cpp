qt/qtbase

commit ded4613ae43842d4b960137843eb83a7472adc65

Windows: Position full screen windows on the correct screen.
Find effective screen by searching the virtual sibling that
contains the center as does QDesktopWidget::screenNumber().

Task-number: QTBUG-30724
Change-Id: I8441ab4f3e5ee8169613a82f150d1a4f1777b662
Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>

src/plugins/platforms/windows/qwindowswindow.cpp
@@ -1366,9 +1366,23 @@ void QWindowsWindow::setWindowState(Qt::WindowState state)
     }
 }
 
+// Return the effective screen for full screen mode in a virtual desktop.
+static const QScreen *effectiveScreen(const QWindow *w)
+{
+    QPoint center = w->geometry().center();
+    if (!w->isTopLevel())
+        center = w->mapToGlobal(center);
+    const QScreen *screen = w->screen();
+    if (!screen->geometry().contains(center))
+        foreach (const QScreen *sibling, screen->virtualSiblings())
+            if (sibling->geometry().contains(center))
+                return sibling;
+    return screen;
+}
+
 bool QWindowsWindow::isFullScreen_sys() const
 {
-    return geometry_sys() == window()->screen()->geometry();
+    return geometry_sys() == effectiveScreen(window())->geometry();
 }
 
 /*!
@@ -1444,7 +1458,7 @@ void QWindowsWindow::setWindowState_sys(Qt::WindowState newState)
                 newStyle |= WS_VISIBLE;
             setStyle(newStyle);
 
-            const QRect r = window()->screen()->geometry();
+            const QRect r = effectiveScreen(window())->geometry();
             const UINT swpf = SWP_FRAMECHANGED | SWP_NOACTIVATE;
             const bool wasSync = testFlag(SynchronousGeometryChangeEvent);
             setFlag(SynchronousGeometryChangeEvent);

commit 8078a04e1950d1e9957c3b3c3ea8e6e0c3d34b80

Observe initial screen for fullscreen windows.
Introduce flag to determine "create" state and do
not try to determine the screen from the geometry.

Fixes breakage introduced by
ded4613ae43842d4b960137843eb83a7472adc65.

Task-number: QTBUG-31166
Task-number: QTBUG-30724

Change-Id: Ibe8df95e0ea84be070fdd06b158a6a10c76d42be
Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>
Reviewed-by: Gunnar Sletta <gunnar.sletta@digia.com>

src/plugins/platforms/windows/qwindowswindow.cpp
@@ -775,7 +775,7 @@ QWindowCreationContext::QWindowCreationContext(const QWindow *w,
 QWindowsWindow::QWindowsWindow(QWindow *aWindow, const WindowData &data) :
     QPlatformWindow(aWindow),
     m_data(data),
-    m_flags(0),
+    m_flags(WithinCreate),
     m_hdc(0),
     m_windowState(Qt::WindowNoState),
     m_opacity(1.0),
@@ -826,6 +826,7 @@ QWindowsWindow::QWindowsWindow(QWindow *aWindow, const WindowData &data) :
     const qreal opacity = qt_window_private(aWindow)->opacity;
     if (!qFuzzyCompare(opacity, qreal(1.0)))
         setOpacity(opacity);
+    clearFlag(WithinCreate);
 }
 
 QWindowsWindow::~QWindowsWindow()
@@ -1481,8 +1482,10 @@ void QWindowsWindow::setWindowState_sys(Qt::WindowState newState)
             if (visible)
                 newStyle |= WS_VISIBLE;
             setStyle(newStyle);
-
-            const QRect r = effectiveScreen(window())->geometry();
+            // Use geometry of QWindow::screen() within creation or the virtual screen the
+            // window is in (QTBUG-31166, QTBUG-30724).
+            const QScreen *screen = testFlag(WithinCreate) ? window()->screen() : effectiveScreen(window());
+            const QRect r = screen->geometry();
             const UINT swpf = SWP_FRAMECHANGED | SWP_NOACTIVATE;
             const bool wasSync = testFlag(SynchronousGeometryChangeEvent);
             setFlag(SynchronousGeometryChangeEvent);

src/plugins/platforms/windows/qwindowswindow.h
@@ -134,7 +134,8 @@ public:
         WithinDestroy = 0x1000,
         TouchRegistered = 0x2000,
         AlertState = 0x4000,
-        Exposed = 0x08000
+        Exposed = 0x08000,
+        WithinCreate = 0x10000
     };
 
     struct WindowData