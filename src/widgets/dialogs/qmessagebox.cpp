qt/qtbase

commit e34dccc9e563966caafb113bd8cacb70cf5d57fd

Allow using Ctrl+C to copy selected text from DetailedText in QMessageBox
Task-number: QTBUG-21895

Change-Id: Ib28f064295493595263945bc72907bf95d2cb909
Reviewed-by: Stephen Kelly <stephen.kelly@kdab.com>

src/widgets/dialogs/qmessagebox.cpp
@@ -90,6 +90,7 @@ enum DetailButtonLabel { ShowLabel = 0, HideLabel = 1 };
 #ifndef QT_NO_TEXTEDIT
 class QMessageBoxDetailsText : public QWidget
 {
+    Q_OBJECT
 public:
     class TextEdit : public QTextEdit
     {
@@ -109,6 +110,7 @@ public:
 
     QMessageBoxDetailsText(QWidget *parent=0)
         : QWidget(parent)
+        , copyAvailable(false)
     {
         QVBoxLayout *layout = new QVBoxLayout;
         layout->setMargin(0);
@@ -122,10 +124,29 @@ public:
         textEdit->setReadOnly(true);
         layout->addWidget(textEdit);
         setLayout(layout);
+
+        connect(textEdit, SIGNAL(copyAvailable(bool)),
+                this, SLOT(textCopyAvailable(bool)));
     }
     void setText(const QString &text) { textEdit->setPlainText(text); }
     QString text() const { return textEdit->toPlainText(); }
+
+    bool copy()
+    {
+        if (!copyAvailable)
+            return false;
+        textEdit->copy();
+        return true;
+    }
+
+private slots:
+    void textCopyAvailable(bool available)
+    {
+        copyAvailable = available;
+    }
+
 private:
+    bool copyAvailable;
     TextEdit *textEdit;
 };
 #endif // QT_NO_TEXTEDIT
@@ -1362,7 +1383,19 @@ void QMessageBox::keyPressEvent(QKeyEvent *e)
             return;
         }
 
-#if defined (Q_OS_WIN) && !defined(QT_NO_CLIPBOARD) && !defined(QT_NO_SHORTCUT)
+
+#if !defined(QT_NO_CLIPBOARD) && !defined(QT_NO_SHORTCUT)
+
+#if !defined(QT_NO_TEXTEDIT)
+        if (e == QKeySequence::Copy) {
+            if (d->detailsText->isVisible() && d->detailsText->copy()) {
+                e->setAccepted(true);
+                return;
+            }
+        }
+#endif // !QT_NO_TEXTEDIT
+
+#if defined(Q_OS_WIN)
         if (e == QKeySequence::Copy) {
             QString separator = QString::fromLatin1("---------------------------\n");
             QString textToCopy = separator;
@@ -1383,7 +1416,9 @@ void QMessageBox::keyPressEvent(QKeyEvent *e)
             QApplication::clipboard()->setText(textToCopy);
             return;
         }
-#endif //QT_NO_SHORTCUT QT_NO_CLIPBOARD Q_OS_WIN
+#endif // Q_OS_WIN
+
+#endif // !QT_NO_CLIPBOARD && !QT_NO_SHORTCUT
 
 #ifndef QT_NO_SHORTCUT
     if (!(e->modifiers() & Qt::AltModifier)) {
@@ -2634,5 +2669,6 @@ QPixmap QMessageBox::standardIcon(Icon icon)
 QT_END_NAMESPACE
 
 #include "moc_qmessagebox.cpp"
+#include "qmessagebox.moc"
 
 #endif // QT_NO_MESSAGEBOX

commit eab6700ec0f637fd79a5dd1cb541ee6426a699d6

Fix crash for pressing Ctrl+C in a message box
This fixes a regression introduced with e34dccc9e

Task-number: QTBUG-31635
Change-Id: I7c991f3b98f5c51e5fb3c12db04d3ace7be87010
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
Reviewed-by: Mitch Curtis <mitch.curtis@digia.com>

src/widgets/dialogs/qmessagebox.cpp
@@ -1392,7 +1392,7 @@ void QMessageBox::keyPressEvent(QKeyEvent *e)
 
 #if !defined(QT_NO_TEXTEDIT)
         if (e == QKeySequence::Copy) {
-            if (d->detailsText->isVisible() && d->detailsText->copy()) {
+            if (d->detailsText && d->detailsText->isVisible() && d->detailsText->copy()) {
                 e->setAccepted(true);
                 return;
             }