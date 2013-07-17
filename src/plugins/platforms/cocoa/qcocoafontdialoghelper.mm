c9182bc11b77675405941a0a163603cac064d8bc
Liang Qi <liang.qi@digia.com>
2013-07-17 11:13:08 +0200

    OSX: Disable window restoration for the Mac font panel
    because if it is automatically restored it's out of the application's
    control, so the user's interaction will be ignored.  Change
    I8ce3cd94f5ae81d7877a346743ca4e0e188baa02 did this for normal windows
    by default, but the dialog helpers generate windows which aren't
    affected by that.
    
    Change-Id: I819d7ab4e51e90783d55cee0676dbc33b38c5b00
    Reviewed-by: Shawn Rutledge <shawn.rutledge@digia.com>

--- src/plugins/platforms/cocoa/qcocoafontdialoghelper.mm
+++ src/plugins/platforms/cocoa/qcocoafontdialoghelper.mm
@@ -140,6 +140,11 @@ static QFont qfontForCocoaFont(NSFont *cocoaFont, const QFont &resolveFont)
     mDialogIsExecuting = false;
     mResultSet = false;
 
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_7
+    if (QSysInfo::MacintoshVersion >= QSysInfo::MV_10_7)
+        [mFontPanel setRestorable:NO];
+#endif
+
     [mFontPanel setTitle:QCFString::toNSString(helper->options()->windowTitle())];
 
     if (mHelper->options()->testOption(QFontDialogOptions::NoButtons)) {
