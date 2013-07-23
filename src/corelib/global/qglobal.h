366aba402f7ed53f83447f23d044cbc950f87252
Sergio Ahumada <sergio.ahumada@digia.com>
2013-07-23 11:51:08 +0200

    Bump Qt version to 5.1.2
    Change-Id: Ibe3e6a37a874b75ea9a20e0a9ed8aa5f21bf6be2
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -45,11 +45,11 @@
 
 #include <stddef.h>
 
-#define QT_VERSION_STR   "5.1.1"
+#define QT_VERSION_STR   "5.1.2"
 /*
    QT_VERSION is (major << 16) + (minor << 8) + patch.
 */
-#define QT_VERSION 0x050101
+#define QT_VERSION 0x050102
 /*
    can be used like #if (QT_VERSION >= QT_VERSION_CHECK(4, 4, 0))
 */
