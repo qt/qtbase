0352c74755a0e593b8fe32fd4bedf8f73e2cc155
Sergio Ahumada <sergio.ahumada@digia.com>
2013-08-09 12:18:26 +0200

    test: Mark tst_QGlyphRun::drawMultiScriptText2() as XFAIL
    This is a flaky test on OS X 10.8, so marking it as XFAIL if it is
    expected to fail.
    
    Task-number: QTBUG-32690
    Change-Id: I0665c7474bb62c4c0a70e4b93cc977e3dbf1e150
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@digia.com>

--- tests/auto/gui/text/qglyphrun/tst_qglyphrun.cpp
+++ tests/auto/gui/text/qglyphrun/tst_qglyphrun.cpp
@@ -502,6 +502,10 @@ void tst_QGlyphRun::drawMultiScriptText2()
     drawGlyphs.save("drawMultiScriptText2_drawGlyphIndexes.png");
 #endif
 
+#ifdef Q_OS_MACX
+    if (drawGlyphs.toImage() != textLayoutDraw.toImage())
+        QEXPECT_FAIL("", "See QTBUG-32690", Continue);
+#endif // Q_OS_MACX
     QCOMPARE(drawGlyphs, textLayoutDraw);
 }
 
