79e77a58a6b71f79af2e864ffa91eb2cc16c5216
Konstantin Ritt <ritt.ks@gmail.com>
2013-08-26 14:27:33 +0300

    Make it possible to build HarfBuzz-NG in Qt
    Change-Id: Ib956adb0ab7bcfba3ce73acf7c19bda815d6eb73
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>

---
+++ src/3rdparty/harfbuzz-ng/src/config.h
@@ -0,0 +1,24 @@
+#ifndef HB_CONFIG_H
+#define HB_CONFIG_H
+
+#define HAVE_OT
+
+#define HB_NO_MT
+#define HB_NO_UNICODE_FUNCS
+
+#include <QtCore/qglobal.h>
+
+#ifndef HB_INTERNAL
+#  define HB_INTERNAL Q_DECL_HIDDEN
+#endif
+
+// because strdup() is not part of strict Posix, declare it here
+extern "C" char *strdup(const char *src);
+
+#ifndef HAVE_ATEXIT
+#  define HAVE_ATEXIT 1
+#  include <QtCore/qcoreapplication.h>
+#  define atexit qAddPostRoutine
+#endif
+
+#endif /* HB_CONFIG_H */
