134a020e2a3314ad2ddcfe22741e60ba251138dc
Oliver Wolff <oliver.wolff@digia.com>
2013-09-04 13:10:15 +0200

    Implement windowsErrorString for WinRT
    FORMAT_MESSAGE_ALLOCATE_BUFFER isn't available for WinRT
    
    Change-Id: Ib9cf2a11a4e84f3ff010665bd53b4ffb0d0bfcce
    Reviewed-by: Andrew Knight <andrew.knight@digia.com>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

--- src/corelib/kernel/qsystemerror.cpp
+++ src/corelib/kernel/qsystemerror.cpp
@@ -84,6 +84,7 @@ namespace {
 static QString windowsErrorString(int errorCode)
 {
     QString ret;
+#ifndef Q_OS_WINRT
     wchar_t *string = 0;
     FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
                   NULL,
@@ -94,6 +95,17 @@ static QString windowsErrorString(int errorCode)
                   NULL);
     ret = QString::fromWCharArray(string);
     LocalFree((HLOCAL)string);
+#else
+    wchar_t errorString[1024];
+    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM,
+                  NULL,
+                  errorCode,
+                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                  (LPWSTR)&errorString,
+                  sizeof(errorString)/sizeof(wchar_t),
+                  NULL);
+    ret = QString::fromWCharArray(errorString);
+#endif  // Q_OS_WINRT
 
     if (ret.isEmpty() && errorCode == ERROR_MOD_NOT_FOUND)
         ret = QString::fromLatin1("The specified module could not be found.");
