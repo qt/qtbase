qt/qtbase

commit 0aad3fc742a7aa15c21fe75e2b1b001f1f73468c

purge QT_{SOURCE,BUILD}_TREE from dictionary
both variables are available class-wide anyway.

Change-Id: I97c13de9ead44638e9310b62f02d8cd1c910df94
Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

tools/configure/configureapp.cpp
@@ -157,8 +157,6 @@ Configure::Configure(int& argc, char** argv)
     defaultBuildParts << QStringLiteral("libs") << QStringLiteral("tools") << QStringLiteral("examples");
     allBuildParts = defaultBuildParts;
     allBuildParts << QStringLiteral("tests");
-    dictionary[ "QT_SOURCE_TREE" ]    = sourcePath;
-    dictionary[ "QT_BUILD_TREE" ]     = buildPath;
     dictionary[ "QT_INSTALL_PREFIX" ] = installPath;
 
     dictionary[ "QMAKESPEC" ] = getenv("QMAKESPEC");
@@ -1141,7 +1139,7 @@ void Configure::parseCmdLine()
         else if (configCmdLine.at(i) == "-hostprefix") {
             ++i;
             if (i == argCount || configCmdLine.at(i).startsWith('-'))
-                dictionary[ "QT_HOST_PREFIX" ] = dictionary[ "QT_BUILD_TREE" ];
+                dictionary[ "QT_HOST_PREFIX" ] = buildPath;
             else
                 dictionary[ "QT_HOST_PREFIX" ] = configCmdLine.at(i);
         }
@@ -2814,7 +2812,7 @@ void Configure::generateCachefile()
 {
     // Generate qmodule.pri
     {
-        FileWriter moduleStream(dictionary[ "QT_BUILD_TREE" ] + "/mkspecs/qmodule.pri");
+        FileWriter moduleStream(buildPath + "/mkspecs/qmodule.pri");
 
         moduleStream << "QT_BUILD_PARTS += " << buildParts.join(' ') << endl;
         if (!skipModules.isEmpty())
@@ -3074,7 +3072,7 @@ void Configure::generateQConfigPri()
 {
     // Generate qconfig.pri
     {
-        FileWriter configStream(dictionary[ "QT_BUILD_TREE" ] + "/mkspecs/qconfig.pri");
+        FileWriter configStream(buildPath + "/mkspecs/qconfig.pri");
 
         configStream << "CONFIG+= ";
         configStream << dictionary[ "BUILD" ];
@@ -3357,7 +3355,7 @@ void Configure::generateConfigfiles()
     }
 
     {
-        FileWriter tmpStream(dictionary["QT_BUILD_TREE"] + "/mkspecs/qdevice.pri");
+        FileWriter tmpStream(buildPath + "/mkspecs/qdevice.pri");
 
         QString android_platform(dictionary.contains("ANDROID_PLATFORM")
                   ? dictionary["ANDROID_PLATFORM"]
@@ -3501,8 +3499,8 @@ void Configure::displayConfig()
     sout << "    SQLite2................." << dictionary[ "SQL_SQLITE2" ] << endl;
     sout << "    InterBase..............." << dictionary[ "SQL_IBASE" ] << endl << endl;
 
-    sout << "Sources are in.............." << QDir::toNativeSeparators(dictionary["QT_SOURCE_TREE"]) << endl;
-    sout << "Build is done in............" << QDir::toNativeSeparators(dictionary["QT_BUILD_TREE"]) << endl;
+    sout << "Sources are in.............." << QDir::toNativeSeparators(sourcePath) << endl;
+    sout << "Build is done in............" << QDir::toNativeSeparators(buildPath) << endl;
     sout << "Install prefix.............." << QDir::toNativeSeparators(dictionary["QT_INSTALL_PREFIX"]) << endl;
     sout << "Headers installed to........" << QDir::toNativeSeparators(dictionary["QT_INSTALL_HEADERS"]) << endl;
     sout << "Libraries installed to......" << QDir::toNativeSeparators(dictionary["QT_INSTALL_LIBS"]) << endl;

commit fa7f446f8f9eb06546de6b8e80b0e1613c75b8cf

fix configure -commercial
Commit 0aad3fc742a7aa15c21fe75e2b1b001f1f73468c broke
"configure -commercial". The license check needs to know where Qt's
source directory is.

Change-Id: I5e53135b6b628eb5e413b2c2b3b8721dae02b503
Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>
Reviewed-by: Kalle Viironen <kalle.viironen@digia.com>
Reviewed-by: Iikka Eklund <iikka.eklund@digia.com>

tools/configure/configureapp.cpp
@@ -4187,7 +4187,7 @@ void Configure::readLicense()
     }
 #ifdef COMMERCIAL_VERSION
     else {
-        Tools::checkLicense(dictionary, licenseInfo, firstLicensePath());
+        Tools::checkLicense(dictionary, licenseInfo, firstLicensePath(), sourcePath);
         if (dictionary["DONE"] != "error") {
             // give the user some feedback, and prompt for license acceptance
             cout << endl << "This is the " << dictionary["PLATFORM NAME"] << " " << dictionary["EDITION"] << " Edition."<< endl << endl;

tools/configure/tools.cpp
@@ -56,16 +56,16 @@ std::ostream &operator<<(std::ostream &s, const QString &val); // defined in con
 using namespace std;
 
 void Tools::checkLicense(QMap<QString,QString> &dictionary, QMap<QString,QString> &licenseInfo,
-                         const QString &path)
+                         const QString &path, const QString &sourcePath)
 {
-    QString tpLicense = dictionary["QT_SOURCE_TREE"] + "/LICENSE.PREVIEW.OPENSOURCE";
+    QString tpLicense = sourcePath + "/LICENSE.PREVIEW.OPENSOURCE";
     if (QFile::exists(tpLicense)) {
         dictionary["EDITION"] = "Preview";
         dictionary["LICENSE FILE"] = tpLicense;
         dictionary["QT_EDITION"] = "QT_EDITION_OPENSOURCE";
         return; // No license key checking in Tech Preview
     }
-    tpLicense = dictionary["QT_SOURCE_TREE"] + "/LICENSE.PREVIEW.COMMERCIAL";
+    tpLicense = sourcePath + "/LICENSE.PREVIEW.COMMERCIAL";
     if (QFile::exists(tpLicense)) {
         dictionary["EDITION"] = "Preview";
         dictionary["LICENSE FILE"] = tpLicense;
@@ -204,7 +204,7 @@ void Tools::checkLicense(QMap<QString,QString> &dictionary, QMap<QString,QString
     if (dictionary["EDITION"] == "Evaluation")
         dictionary["LICENSE_EXTENSION"] = "-EVALUATION";
 
-    if (QFile::exists(dictionary["QT_SOURCE_TREE"] + "/.LICENSE")) {
+    if (QFile::exists(sourcePath + "/.LICENSE")) {
         // Generic, no-suffix license
         dictionary["LICENSE_EXTENSION"].clear();
     } else if (dictionary["LICENSE_EXTENSION"].isEmpty()) {
@@ -220,8 +220,8 @@ void Tools::checkLicense(QMap<QString,QString> &dictionary, QMap<QString,QString
     }
 
     // copy one of .LICENSE-*(-US) to LICENSE
-    QString toLicenseFile   = dictionary["QT_SOURCE_TREE"] + "/LICENSE";
-    QString fromLicenseFile = dictionary["QT_SOURCE_TREE"] + "/.LICENSE" + dictionary["LICENSE_EXTENSION"];
+    QString toLicenseFile   = sourcePath + "/LICENSE";
+    QString fromLicenseFile = sourcePath + "/.LICENSE" + dictionary["LICENSE_EXTENSION"];
     if (licenseFeatures == 'B' || licenseFeatures == 'G'
         || licenseFeatures == 'L' || licenseFeatures == 'Y')
         fromLicenseFile += "-US";

tools/configure/tools.h
@@ -51,7 +51,7 @@ class Tools
 {
 public:
     static void checkLicense(QMap<QString,QString> &dictionary, QMap<QString,QString> &licenseInfo,
-                             const QString &path);
+                             const QString &path, const QString &sourcePath);
 };
 
 #endif // _TOOLS_H_