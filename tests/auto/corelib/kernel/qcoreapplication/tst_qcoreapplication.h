2c925906f516e7e47db9e82ed8bda1f675ab414a
Tor Arne Vestbø <tor.arne.vestbo@digia.com>
2013-09-25 16:28:24 +0200

    Base tst_QGuiApplication on tst_QCoreApplication to increase GUI ED coverage
    The QCoreApplication test has quite a few test cases that we would like
    to exercise using the GUI event-dispatcher. Instead of duplicating the
    tests for the GUI dispatcher, we inherit tst_QCoreApplication, which
    also lets us add extra tests that are specific to tst_QGuiApplication.
    
    Change-Id: Ib411457131b8d3fed871f682c1c0568577f6127d
    Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

---
+++ tests/auto/corelib/kernel/qcoreapplication/tst_qcoreapplication.h
@@ -0,0 +1,73 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef TST_QCOREAPPLICATION_H
+#define TST_QCOREAPPLICATION_H
+
+#include <QtCore/QtCore>
+
+class tst_QCoreApplication: public QObject
+{
+    Q_OBJECT
+private slots:
+    void sendEventsOnProcessEvents(); // this must be the first test
+    void getSetCheck();
+    void qAppName();
+#ifndef Q_OS_WIN
+    void argc();
+#endif
+    void postEvent();
+    void removePostedEvents();
+#ifndef QT_NO_THREAD
+    void deliverInDefinedOrder();
+#endif
+    void applicationPid();
+    void globalPostedEventsCount();
+    void processEventsAlwaysSendsPostedEvents();
+    void reexec();
+    void execAfterExit();
+    void eventLoopExecAfterExit();
+    void customEventDispatcher();
+    void testQuitLock();
+    void QTBUG31606_QEventDestructorDeadLock();
+};
+
+#endif // TST_QCOREAPPLICATION_H
