8e7dc25380dceebca094e092d9feb21ad167ba91
Tor Arne Vestbø <tor.arne.vestbo@digia.com>
2013-10-09 16:26:44 +0200

    qmake: Load extra variables only if also loading pre-files
    The extra variables only need to be applied once, when we
    are loading the pro file (and hence are loding pre files),
    not for every single pri/prf that's loaded as a result of that
    (which do not load pre files themselves).
    
    Change-Id: I3118694a8eeccf2dc32c4f62df754033fad13528
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

--- qmake/library/qmakeevaluator.cpp
+++ qmake/library/qmakeevaluator.cpp
@@ -1365,10 +1365,6 @@ QMakeEvaluator::VisitReturn QMakeEvaluator::visitProFile(
             loadDefaults();
     }
 
-    for (ProValueMap::ConstIterator it = m_extraVars.constBegin();
-         it != m_extraVars.constEnd(); ++it)
-        m_valuemapStack.first().insert(it.key(), it.value());
-
     VisitReturn vr;
 
     m_handler->aboutToEval(currentProFile(), pro, type);
@@ -1376,6 +1372,10 @@ QMakeEvaluator::VisitReturn QMakeEvaluator::visitProFile(
     valuesRef(ProKey("PWD")) = ProStringList(ProString(currentDirectory()));
     if (flags & LoadPreFiles) {
         setupProject();
+
+        for (ProValueMap::ConstIterator it = m_extraVars.constBegin();
+             it != m_extraVars.constEnd(); ++it)
+            m_valuemapStack.first().insert(it.key(), it.value());
 
         if ((vr = evaluateFeatureFile(QLatin1String("default_pre.prf"))) == ReturnError)
             goto failed;
