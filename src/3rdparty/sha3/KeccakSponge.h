191f96cb8817a6d1632331c67f4f8d45535205e1
Jan Arve Saether <jan-arve.saether@digia.com>
2013-11-13 08:49:44 +0100

    Avoid compiler warning about alignment for MSVC2013 (x64)
    Only align if we actually *need to* (due to SSE/XOP usage)
    
    The code will currently not compile as it is now with UseSSE or
    UseXOP defined, (so the ALIGN macro could just
    be totally removed). In order to keep the diff to upstream as small
    as possible, we keep the ALIGN macro definition.
    
    Task-number: QTBUG-34362
    Change-Id: I6cf420205896e3a476cdcfbf3308a07e0ae0c84e
    Reviewed-by: Richard J. Moore <rich@kde.org>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/3rdparty/sha3/KeccakSponge.h
+++ src/3rdparty/sha3/KeccakSponge.h
@@ -19,12 +19,16 @@ http://creativecommons.org/publicdomain/zero/1.0/
 #define KeccakMaximumRate 1536
 #define KeccakMaximumRateInBytes (KeccakMaximumRate/8)
 
+#if defined(UseSSE) || defined(UseXOP)
 #if defined(__GNUC__)
 #define ALIGN __attribute__ ((aligned(32)))
 #elif defined(_MSC_VER)
 #define ALIGN __declspec(align(32))
-#else
-#define ALIGN
+#endif
+#endif
+
+#ifndef ALIGN
+# define ALIGN
 #endif
 
 ALIGN typedef struct spongeStateStruct {
