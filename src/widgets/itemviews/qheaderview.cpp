qt/qtbase

commit 524c3e05e811d81fbcd71eedb377b7eaa0a21ef3

QHeaderView - prevent negative default+minimum section sizes
In 96f1fe8855082017fbbecccdab1eb11fd8c1f870 we agreed that sections
with negative sizes did not make sense. Of the same reason default
section sizes and minimum section sizes should be not negative.

Change-Id: I6a770e7f510d8e2bb90bfd8f38b4fa0566fc137b
Reviewed-by: Stephen Kelly <stephen.kelly@kdab.com>

src/widgets/itemviews/qheaderview.cpp
@@ -1469,6 +1469,8 @@ int QHeaderView::defaultSectionSize() const
 void QHeaderView::setDefaultSectionSize(int size)
 {
     Q_D(QHeaderView);
+    if (size < 0)
+        return;
     d->setDefaultSectionSize(size);
 }
 
@@ -1502,6 +1504,8 @@ int QHeaderView::minimumSectionSize() const
 void QHeaderView::setMinimumSectionSize(int size)
 {
     Q_D(QHeaderView);
+    if (size < 0)
+        return;
     d->minimumSectionSize = size;
 }
 

tests/auto/widgets/itemviews/qheaderview/tst_qheaderview.cpp
@@ -338,23 +338,21 @@ void tst_QHeaderView::getSetCheck()
 
     // int QHeaderView::defaultSectionSize()
     // void QHeaderView::setDefaultSectionSize(int)
+    obj1.setDefaultSectionSize(-1);
+    QVERIFY(obj1.defaultSectionSize() >= 0);
     obj1.setDefaultSectionSize(0);
     QCOMPARE(0, obj1.defaultSectionSize());
-    obj1.setDefaultSectionSize(INT_MIN);
-    QCOMPARE(INT_MIN, obj1.defaultSectionSize());
     obj1.setDefaultSectionSize(INT_MAX);
     QCOMPARE(INT_MAX, obj1.defaultSectionSize());
-    // ### the test above does not make sense for values below 0
 
     // int QHeaderView::minimumSectionSize()
     // void QHeaderView::setMinimumSectionSize(int)
+    obj1.setMinimumSectionSize(-1);
+    QVERIFY(obj1.minimumSectionSize() >= 0);
     obj1.setMinimumSectionSize(0);
     QCOMPARE(0, obj1.minimumSectionSize());
-    obj1.setMinimumSectionSize(INT_MIN);
-    QCOMPARE(INT_MIN, obj1.minimumSectionSize());
     obj1.setMinimumSectionSize(INT_MAX);
     QCOMPARE(INT_MAX, obj1.minimumSectionSize());
-    // ### the test above does not make sense for values below 0
 
     // int QHeaderView::offset()
     // void QHeaderView::setOffset(int)

commit aafbd7392ead765c2038a1ac6b569d083effb81d

QHeaderView - do not ignore -1 as minimum section size
We claim that -1 is a special automatic value, but calling
setMinimumSectionSize with -1 is unfortunately ignored due a
regression in 524c3e05e811d81fbcd71eedb377b7eaa0a21ef3

Change-Id: I7d7e5dbbf78e561849d2f2352c9edb2df36aa181
Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>

src/widgets/itemviews/qheaderview.cpp
@@ -1599,7 +1599,7 @@ int QHeaderView::minimumSectionSize() const
 void QHeaderView::setMinimumSectionSize(int size)
 {
     Q_D(QHeaderView);
-    if (size < 0 || size > maxSizeSection)
+    if (size < -1 || size > maxSizeSection)
         return;
     d->minimumSectionSize = size;
     if (d->minimumSectionSize > maximumSectionSize())

tests/auto/widgets/itemviews/qheaderview/tst_qheaderview.cpp
@@ -368,6 +368,8 @@ void tst_QHeaderView::getSetCheck()
     QCOMPARE(0, obj1.minimumSectionSize());
     obj1.setMinimumSectionSize(99999);
     QCOMPARE(99999, obj1.minimumSectionSize());
+    obj1.setMinimumSectionSize(-1);
+    QVERIFY(obj1.minimumSectionSize() < 100);
 
     // int QHeaderView::offset()
     // void QHeaderView::setOffset(int)