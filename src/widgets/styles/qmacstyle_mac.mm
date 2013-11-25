qt/qtbase

commit 65b39b3cdcdfe175d80ccf9428168ed51be77fd8

Fixed text rendering in selected tab button
We resort to custom Qt rendering when selected since it looks better
than using Carbon's HITheme engine.

Change-Id: Iefcc2bebb1ba58d0d99f078c4960c5126542f637
Reviewed-by: Jens Bache-Wiig <jens.bache-wiig@digia.com>

src/widgets/styles/qmacstyle_mac.mm
@@ -4002,13 +4002,17 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
             QStyleOptionTabV3 myTab = *tab;
             ThemeTabDirection ttd = getTabDirection(myTab.shape);
             bool verticalTabs = ttd == kThemeTabWest || ttd == kThemeTabEast;
+            bool selected = (myTab.state & QStyle::State_Selected);
+
+            if (selected && !myTab.documentMode)
+                myTab.palette.setColor(QPalette::WindowText, QColor(Qt::white));
 
             // Check to see if we use have the same as the system font
             // (QComboMenuItem is internal and should never be seen by the
             // outside world, unless they read the source, in which case, it's
             // their own fault).
             bool nonDefaultFont = p->font() != qt_app_fonts_hash()->value("QComboMenuItem");
-            if (verticalTabs || nonDefaultFont || !tab->icon.isNull()
+            if (selected || verticalTabs || nonDefaultFont || !tab->icon.isNull()
                 || !myTab.leftButtonSize.isNull() || !myTab.rightButtonSize.isNull()) {
                 int heightOffset = 0;
                 if (verticalTabs) {
@@ -4019,12 +4023,15 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
                 }
                 myTab.rect.setHeight(myTab.rect.height() + heightOffset);
 
-                if (myTab.documentMode) {
+                if (myTab.documentMode || selected) {
                     p->save();
                     rotateTabPainter(p, myTab.shape, myTab.rect);
 
+                    QColor shadowColor = QColor(myTab.documentMode ? Qt::white : Qt::black);
+                    shadowColor.setAlpha(75);
                     QPalette np = tab->palette;
-                    np.setColor(QPalette::WindowText, QColor(255, 255, 255, 75));
+                    np.setColor(QPalette::WindowText, shadowColor);
+
                     QRect nr = subElementRect(SE_TabBarTabText, opt, w);
                     nr.moveTop(-1);
                     int alignment = Qt::AlignCenter | Qt::TextShowMnemonic | Qt::TextHideMnemonic;

commit f6cd378e2451823a30601f8942227a0ec0854dc4

Mac style: Fix selected tab text color on 10.6
This is a regression introduced by 65b39b3cdcdfe175d80ccf9428168ed51be77fd8.

Task-number: QTBUG-34540
Change-Id: I1f78337b13c806f6b6794b769cfe99206445ce7a
Reviewed-by: Jens Bache-Wiig <jens.bache-wiig@digia.com>

src/widgets/styles/qmacstyle_mac.mm
@@ -3865,8 +3865,9 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
             ThemeTabDirection ttd = getTabDirection(myTab.shape);
             bool verticalTabs = ttd == kThemeTabWest || ttd == kThemeTabEast;
             bool selected = (myTab.state & QStyle::State_Selected);
+            bool usingModernOSX = QSysInfo::MacintoshVersion > QSysInfo::MV_10_6;
 
-            if (selected && !myTab.documentMode)
+            if (usingModernOSX && selected && !myTab.documentMode)
                 myTab.palette.setColor(QPalette::WindowText, QColor(Qt::white));
 
             // Check to see if we use have the same as the system font
@@ -3874,7 +3875,7 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
             // outside world, unless they read the source, in which case, it's
             // their own fault).
             bool nonDefaultFont = p->font() != qt_app_fonts_hash()->value("QComboMenuItem");
-            if (selected || verticalTabs || nonDefaultFont || !tab->icon.isNull()
+            if ((usingModernOSX && selected) || verticalTabs || nonDefaultFont || !tab->icon.isNull()
                 || !myTab.leftButtonSize.isNull() || !myTab.rightButtonSize.isNull()) {
                 int heightOffset = 0;
                 if (verticalTabs) {
@@ -3885,7 +3886,7 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
                 }
                 myTab.rect.setHeight(myTab.rect.height() + heightOffset);
 
-                if (myTab.documentMode || selected) {
+                if (myTab.documentMode || (usingModernOSX && selected)) {
                     p->save();
                     rotateTabPainter(p, myTab.shape, myTab.rect);
 