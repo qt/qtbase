f568e43e107ba909eaad300729ee93354b0a1846
Tasuku Suzuki <stasuku@gmail.com>
2014-01-03 14:38:12 +0900

    Make qtbase compile with QT_NO_GESTURES
    Change-Id: I90f173265e177ff37ce80da3983080651856259d
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

--- src/gui/kernel/qwindowsysteminterface_p.h
+++ src/gui/kernel/qwindowsysteminterface_p.h
@@ -91,7 +91,9 @@ public:
         PlatformPanel = UserInputEvent | 0x17,
         ContextMenu = UserInputEvent | 0x18,
         EnterWhatsThisMode = UserInputEvent | 0x19,
+#ifndef QT_NO_GESTURES
         Gesture = UserInputEvent | 0x1a,
+#endif
         ApplicationStateChanged = 0x19,
         FlushEvents = 0x20,
         WindowScreenChanged = 0x21
@@ -400,6 +402,7 @@ public:
     };
 #endif
 
+#ifndef QT_NO_GESTURES
     class GestureEvent : public InputEvent {
     public:
         GestureEvent(QWindow *window, ulong time, Qt::NativeGestureType type, QPointF pos, QPointF globalPos)
@@ -414,6 +417,7 @@ public:
         ulong sequenceId;
         quint64 intValue;
     };
+#endif
 
     class WindowSystemEventList {
         QList<WindowSystemEvent *> impl;
