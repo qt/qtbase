qt/qtbase

commit bdebec4e2ef79f1771d2dcc22f9a919eb4487567

Menu item shortcuts reaching Qt's event loop are disabled
See comment in code and Cocoa event dispatching overview at
https://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/EventOverview/EventArchitecture/EventArchitecture.html

Task-number: QTBUG-30657
Change-Id: I88907aeeefa4962e1121495cd51af17a8e71b7de
Reviewed-by: Frederik Gladhorn <frederik.gladhorn@digia.com>

src/widgets/kernel/qshortcut.cpp
@@ -246,9 +246,19 @@ static bool correctActionContext(Qt::ShortcutContext context, QAction *a, QWidge
         QWidget *w = widgets.at(i);
 #ifndef QT_NO_MENU
         if (QMenu *menu = qobject_cast<QMenu *>(w)) {
+#ifdef Q_OS_MAC
+            // On Mac, menu item shortcuts are processed before reaching any window.
+            // That means that if a menu action shortcut has not been already processed
+            // (and reaches this point), then the menu item itself has been disabled.
+            // This occurs at the QPA level on Mac, were we disable all the Cocoa menus
+            // when showing a modal window.
+            Q_UNUSED(menu);
+            continue;
+#else
             QAction *a = menu->menuAction();
             if (correctActionContext(context, a, active_window))
                 return true;
+#endif
         } else
 #endif
             if (correctWidgetContext(context, w, active_window))

commit 183a3ca2ec5b81771cddec104baf37540661ac96

Cocoa: fix gnu emacs style shortcut for action in menu
The regression was introduced in bdebec4e2ef79f1771d2dcc22f9a919eb4487567.

Cocoa QPA plugin only could handle the shortcut with one key sequence, not the
multiple key sequences. We just let Qt to handle those cases.

Task-number: QTBUG-33627

Change-Id: I3898308d9f4ecda8bd716d8b286f05f9f311774a
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

src/widgets/kernel/qshortcut.cpp
@@ -279,13 +279,12 @@ static bool correctActionContext(Qt::ShortcutContext context, QAction *a, QWidge
             // (and reaches this point), then the menu item itself has been disabled.
             // This occurs at the QPA level on Mac, were we disable all the Cocoa menus
             // when showing a modal window.
-            Q_UNUSED(menu);
-            continue;
-#else
+            if (a->shortcut().count() <= 1)
+                continue;
+#endif
             QAction *a = menu->menuAction();
             if (correctActionContext(context, a, active_window))
                 return true;
-#endif
         } else
 #endif
             if (correctWidgetContext(context, w, active_window))