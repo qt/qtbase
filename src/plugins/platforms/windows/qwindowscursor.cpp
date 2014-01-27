97bc28778227d00affb8cbd6a1ac9a2258cdf95d
Friedemann Kleint <Friedemann.Kleint@digia.com>
2014-01-27 13:19:22 +0100

    QWindowsCursor: Add API for determining the cursor state.
    Detect the 'suppressed' state new in Windows 8.
    
    Change-Id: I0faa994aa7b91869cedba36b777b1784818efcce
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

--- src/plugins/platforms/windows/qwindowscursor.cpp
+++ src/plugins/platforms/windows/qwindowscursor.cpp
@@ -465,6 +465,22 @@ QPoint QWindowsCursor::mousePosition()
     return QPoint(p.x, p.y);
 }
 
+QWindowsCursor::CursorState QWindowsCursor::cursorState()
+{
+#ifndef Q_OS_WINCE
+    enum { cursorShowing = 0x1, cursorSuppressed = 0x2 }; // Windows 8: CURSOR_SUPPRESSED
+    CURSORINFO cursorInfo;
+    cursorInfo.cbSize = sizeof(CURSORINFO);
+    if (GetCursorInfo(&cursorInfo)) {
+        if (cursorInfo.flags & CursorShowing)
+            return CursorShowing;
+        if (cursorInfo.flags & cursorSuppressed)
+            return CursorSuppressed;
+    }
+#endif // !Q_OS_WINCE
+    return CursorHidden;
+}
+
 void QWindowsCursor::setPos(const QPoint &pos)
 {
     SetCursorPos(pos.x(), pos.y());
