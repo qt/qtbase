qt/qtbase

commit 7d72516b52b20b0782d972224a55a43e74b8ae5a

Change platform selectors to match qmake selectors
Previously matched Qt.platform.os, however that can only provide one
string. Multiple selectors can be present at once, so we can provide
both unix and linux instead of having to pick the most specialized one.

Task-number: QTBUG-34796
Change-Id: I219517d740fa7385e923a9e09cb7e241378fbaee
Reviewed-by: David Faure <david.faure@kdab.com>

src/corelib/io/qfileselector.cpp
@@ -348,22 +348,28 @@ void QFileSelectorPrivate::updateSelectors()
 QStringList QFileSelectorPrivate::platformSelectors()
 {
     QStringList ret;
-#if defined(Q_OS_LINUX_ANDROID)
-    ret << QStringLiteral("android");
-#elif defined(Q_OS_BLACKBERRY)
-    ret << QStringLiteral("blackberry");
-#elif defined(Q_OS_IOS)
-    ret << QStringLiteral("ios");
-#elif defined(Q_OS_WINCE)
-    ret << QStringLiteral("wince");
-#elif defined(Q_OS_WIN)
+#if defined(Q_OS_WIN)
     ret << QStringLiteral("windows");
-#elif defined(Q_OS_LINUX)
-    ret << QStringLiteral("linux");
-#elif defined(Q_OS_OSX)
-    ret << QStringLiteral("osx");
+#  if defined(Q_OS_WINCE)
+    ret << QStringLiteral("wince");
+#  endif
 #elif defined(Q_OS_UNIX)
-    ret << QStringLiteral("generic_unix");
+    ret << QStringLiteral("unix");
+#  if defined(Q_OS_LINUX_ANDROID)
+    ret << QStringLiteral("android");
+#  elif defined(Q_OS_BLACKBERRY)
+    ret << QStringLiteral("blackberry");
+#  elif defined(Q_OS_IOS)
+    ret << QStringLiteral("ios");
+#  elif defined(Q_OS_LINUX)
+    ret << QStringLiteral("linux");
+#  elif defined(Q_OS_MAC)
+    ret << QStringLiteral("mac");
+#  else
+    struct utsname u;
+    if (uname(&u) != -1)
+        ret << QString::fromLatin1(u.sysname).toLower();
+#  endif
 #endif
     return ret;
 }

tests/auto/corelib/io/qfileselector/qfileselector.qrc
@@ -11,13 +11,27 @@
    <file>extras/+custom3/+custom5/test</file>
    <file>extras/+custom5/+custom3/test</file>
    <file>platforms/test</file>
+   <file>platforms/+unix/+android/test</file>
+   <file>platforms/+unix/+blackberry/test</file>
+   <file>platforms/+unix/+ios/test</file>
+   <file>platforms/+unix/+mac/test</file>
+   <file>platforms/+windows/+wince/test</file>
+   <file>platforms/+windows/test</file>
+   <file>platforms/+windows/test2</file>
+   <file>platforms/+unix/+linux/test</file>
+   <file>platforms/+unix/test</file>
+   <file>platforms/test2</file>
+   <file>platforms/+android/test2</file>
+   <file>platforms/+blackberry/test2</file>
+   <file>platforms/+ios/test2</file>
+   <file>platforms/+mac/test2</file>
+   <file>platforms/+linux/test2</file>
+   <file>platforms/+wince/test2</file>
    <file>platforms/+android/test</file>
    <file>platforms/+blackberry/test</file>
    <file>platforms/+ios/test</file>
-   <file>platforms/+osx/test</file>
-   <file>platforms/+wince/test</file>
-   <file>platforms/+windows/test</file>
+   <file>platforms/+mac/test</file>
    <file>platforms/+linux/test</file>
-   <file>platforms/+generic_unix/test</file>
+   <file>platforms/+wince/test</file>
 </qresource>
 </RCC>

tests/auto/corelib/io/qfileselector/tst_qfileselector.cpp
@@ -91,9 +91,33 @@ void tst_QFileSelector::basicTest_data()
     QTest::addColumn<QString>("expectedPath");
 
     QString test("/test");// '/' is here so dir string can also be selector string
-    QTest::newRow("platform") <<  QString(":/platforms/test") << QStringList()
-        << QString(":/platforms/")  + QLatin1Char(selectorIndicator)
-           + QFileSelectorPrivate::platformSelectors().first() + test;
+    QString test2("/test2");
+    QString expectedPlatform1File(":/platforms");
+    QString expectedPlatform2File(""); //Only the last selector
+#if defined(Q_OS_UNIX) && !defined(Q_OS_ANDROID) && !defined(Q_OS_BLACKBERRY) && !defined(Q_OS_IOS) && !defined(Q_OS_LINUX) && !defined(Q_OS_MAC)
+    /* We are only aware of specific unixes, and do not have test files for any of the others.
+       However those unixes can get a selector added from the result of a uname call, so this will
+       lead to a case where we don't have that file so we can't expect the concatenation of platform
+       selectors to work. It should just find the +unix/test file.*/
+    expectedPlatform1File = QString(":/platforms/") + QLatin1Char(selectorIndicator)
+        + QString("unix/test");
+    expectedPlatform2File = QString(":/platforms/test2");
+#else
+    foreach (const QString &selector, QFileSelectorPrivate::platformSelectors()) {
+        expectedPlatform1File = expectedPlatform1File + QLatin1Char('/') + QLatin1Char(selectorIndicator)
+            + selector;
+        expectedPlatform2File = selector;
+    }
+    expectedPlatform1File += test;
+    expectedPlatform2File = QLatin1String(":/platforms/") + QLatin1Char(selectorIndicator)
+        + expectedPlatform2File + test2;
+#endif
+
+    QTest::newRow("platform1") <<  QString(":/platforms/test") << QStringList()
+        << expectedPlatform1File;
+
+    QTest::newRow("platform2") <<  QString(":/platforms/test2") << QStringList()
+        << expectedPlatform2File;
 
     QString resourceTestPath(":/extras/test");
     QString custom1("custom1");

commit fdef360bad0f2f1059ff81f1bdd608332541abac

QFileSelector: correct the macros for Apple platforms.
This fixes a serious regression from
7d72516b52b20b0782d972224a55a43e74b8ae5a.

[ChangeLog] QFileSelector: the identifier for OS X has been changed back
to 'osx' from 'mac', and 'mac' and 'darwin' have now been added as
selectors for Darwin OS (which is the base of both OS X and iOS).

Task-number: QTBUG-35073
Change-Id: I83183e34c5a697338cc1ddcac33a41bd379ded12
Reviewed-by: Alan Alpert <aalpert@blackberry.com>

src/corelib/io/qfileselector.cpp
@@ -157,7 +157,7 @@ QFileSelectorPrivate::QFileSelectorPrivate()
     Selectors normally available are
     \list
     \li platform, any of the following strings which match the platform the application is running
-        on: android, blackberry, ios, mac, linux, wince, unix, windows.
+        on: android, blackberry, ios, osx, darwin, mac, linux, wince, unix, windows.
     \li locale, same as QLocale::system().name().
     \endlist
 
@@ -374,12 +374,16 @@ QStringList QFileSelectorPrivate::platformSelectors()
     ret << QStringLiteral("blackberry");
 #  elif defined(Q_OS_QNX)
     ret << QStringLiteral("qnx");
-#  elif defined(Q_OS_IOS)
-    ret << QStringLiteral("ios");
 #  elif defined(Q_OS_LINUX)
     ret << QStringLiteral("linux");
-#  elif defined(Q_OS_MAC)
-    ret << QStringLiteral("mac");
+#  elif defined(Q_OS_DARWIN)
+    ret << QStringLiteral("darwin");
+    ret << QStringLiteral("mac"); // compatibility synonym
+#    if defined(Q_OS_IOS)
+    ret << QStringLiteral("ios");
+#    elif defined(Q_OS_OSX)
+    ret << QStringLiteral("osx");
+#    endif
 #  else
     struct utsname u;
     if (uname(&u) != -1)

tests/auto/corelib/io/qfileselector/qfileselector.qrc
@@ -13,8 +13,10 @@
    <file>platforms/test</file>
    <file>platforms/+unix/+android/test</file>
    <file>platforms/+unix/+blackberry/test</file>
-   <file>platforms/+unix/+ios/test</file>
-   <file>platforms/+unix/+mac/test</file>
+   <file>platforms/+unix/+darwin/+mac/+ios/test</file>
+   <file>platforms/+unix/+darwin/+mac/+osx/test</file>
+   <file>platforms/+unix/+darwin/+mac/test</file>
+   <file>platforms/+unix/+darwin/test</file>
    <file>platforms/+windows/+wince/test</file>
    <file>platforms/+windows/test</file>
    <file>platforms/+windows/test2</file>
@@ -24,12 +26,14 @@
    <file>platforms/+android/test2</file>
    <file>platforms/+blackberry/test2</file>
    <file>platforms/+ios/test2</file>
-   <file>platforms/+mac/test2</file>
+   <file>platforms/+osx/test2</file>
    <file>platforms/+linux/test2</file>
    <file>platforms/+wince/test2</file>
    <file>platforms/+android/test</file>
    <file>platforms/+blackberry/test</file>
    <file>platforms/+ios/test</file>
+   <file>platforms/+osx/test</file>
+   <file>platforms/+darwin/test</file>
    <file>platforms/+mac/test</file>
    <file>platforms/+linux/test</file>
    <file>platforms/+wince/test</file>

tests/auto/corelib/io/qfileselector/tst_qfileselector.cpp
@@ -94,7 +94,8 @@ void tst_QFileSelector::basicTest_data()
     QString test2("/test2");
     QString expectedPlatform1File(":/platforms");
     QString expectedPlatform2File(""); //Only the last selector
-#if defined(Q_OS_UNIX) && !defined(Q_OS_ANDROID) && !defined(Q_OS_BLACKBERRY) && !defined(Q_OS_IOS) && !defined(Q_OS_LINUX) && !defined(Q_OS_MAC)
+#if defined(Q_OS_UNIX) && !defined(Q_OS_ANDROID) && !defined(Q_OS_BLACKBERRY) && \
+    !defined(Q_OS_DARWIN) && !defined(Q_OS_LINUX)
     /* We are only aware of specific unixes, and do not have test files for any of the others.
        However those unixes can get a selector added from the result of a uname call, so this will
        lead to a case where we don't have that file so we can't expect the concatenation of platform