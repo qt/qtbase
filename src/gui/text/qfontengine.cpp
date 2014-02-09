qt/qtbase

commit d8225fab8fb8b21fb5d35968d9edb5340b76bcc2

[QFontEngineBox] Fix incorrect advances for SMP code points
In UTF-16, SMP code point is encoded with a pair of surrogates,
thus making SMP code point twice wider than BMP code point.

Change-Id: Ifb0e78cff50cca7231d07097a730188bd98c54cb
Reviewed-by: Lars Knoll <lars.knoll@digia.com>

src/gui/text/qfontengine.cpp
@@ -1299,17 +1299,24 @@ QFontEngineBox::~QFontEngineBox()
 {
 }
 
-bool QFontEngineBox::stringToCMap(const QChar *, int len, QGlyphLayout *glyphs, int *nglyphs, QFontEngine::ShaperFlags flags) const
+bool QFontEngineBox::stringToCMap(const QChar *str, int len, QGlyphLayout *glyphs, int *nglyphs, QFontEngine::ShaperFlags flags) const
 {
     if (*nglyphs < len) {
         *nglyphs = len;
         return false;
     }
 
-    memset(glyphs->glyphs, 0, len * sizeof(glyph_t));
+    int ucs4Length = 0;
+    for (int i = 0; i < len; ++i) {
+        if (str[i].isHighSurrogate() && i + 1 < len && str[i + 1].isLowSurrogate())
+            ++ucs4Length;
+        ++ucs4Length;
+    }
 
-    *nglyphs = len;
-    glyphs->numGlyphs = len;
+    memset(glyphs->glyphs, 0, ucs4Length * sizeof(glyph_t));
+
+    *nglyphs = ucs4Length;
+    glyphs->numGlyphs = ucs4Length;
 
     if (!(flags & GlyphIndicesOnly))
         recalcAdvances(glyphs, flags);

commit 535086e9e59bb21dca9ee33ba2c6c55e1aded6a7

QFontEngineBox: Fix assertion for code points beyond BMP
...due to a typo introduced in d8225fab8fb8b21fb5d and stricter checks
introduced in 4e319ca4c47a99295c1.
This uses recently added QStringIterator to decrease the code duplication.

Change-Id: Ib1c2519db1c4d65f3ac14e6f9ae3fa272860b1dd
Reviewed-by: Simon Hausmann <simon.hausmann@digia.com>

src/gui/text/qfontengine.cpp
@@ -48,6 +48,7 @@
 #include "qvarlengtharray.h"
 #include <qmath.h>
 #include <qendian.h>
+#include <private/qstringiterator_p.h>
 
 #ifdef QT_ENABLE_HARFBUZZ_NG
 #  include "qharfbuzzng_p.h"
@@ -1364,13 +1365,11 @@ bool QFontEngineBox::stringToCMap(const QChar *str, int len, QGlyphLayout *glyph
     }
 
     int ucs4Length = 0;
-    for (int i = 0; i < len; ++i) {
-        if (str[i].isHighSurrogate() && i + 1 < len && str[i + 1].isLowSurrogate())
-            ++ucs4Length;
-        ++ucs4Length;
+    QStringIterator it(str, str + len);
+    while (it.hasNext()) {
+        it.advance();
+        glyphs->glyphs[ucs4Length++] = 0;
     }
-
-    memset(glyphs->glyphs, 0, ucs4Length * sizeof(glyph_t));
 
     *nglyphs = ucs4Length;
     glyphs->numGlyphs = ucs4Length;