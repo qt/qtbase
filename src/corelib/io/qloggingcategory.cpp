qt/qtbase

commit 85e576535c9c39756ecaddefa245f71fb35b8589

Let Q_DECLARE_LOGGING_CATEGORY and Q_LOGGING_CATEGORY return a const reference
In general QLoggingCategory should be treated as a const object that's
configured by the backend. The only legitimate place where user code
should call setEnabled is in a CategoryFilter ...

[ChangeLog][QtCore][Logging] Make Q_LOGGING_CATEGORY
and Q_DECLARE_LOGGING_CATEGORY return a const object.

Change-Id: I6140442ab48286e05cd3b55064a502bbe6dfb16a
Reviewed-by: Konstantin Ritt <ritt.ks@gmail.com>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/io/qloggingcategory.h
@@ -84,13 +84,13 @@ private:
 };
 
 #define Q_DECLARE_LOGGING_CATEGORY(name) \
-    extern QLoggingCategory &name();
+    extern const QLoggingCategory &name();
 
 // relies on QLoggingCategory(QString) being thread safe!
 #define Q_LOGGING_CATEGORY(name, string) \
-    QLoggingCategory &name() \
+    const QLoggingCategory &name() \
     { \
-        static QLoggingCategory category(string); \
+        static const QLoggingCategory category(string); \
         return category; \
     }
 

src/corelib/doc/snippets/code/qlogging/qlogging.cpp
@@ -54,5 +54,5 @@
 //! [1]
 
 //! [2]
-    QLoggingCategory &category();
+    const QLoggingCategory &category();
 //! [2]

src/corelib/global/qlogging.h
@@ -100,7 +100,7 @@ public:
     void warning(const char *msg, ...) const Q_ATTRIBUTE_FORMAT_PRINTF(2, 3);
     void critical(const char *msg, ...) const Q_ATTRIBUTE_FORMAT_PRINTF(2, 3);
 
-    typedef QLoggingCategory &(*CategoryFunction)();
+    typedef const QLoggingCategory &(*CategoryFunction)();
 
     void debug(const QLoggingCategory &cat, const char *msg, ...) const Q_ATTRIBUTE_FORMAT_PRINTF(3, 4);
     void debug(CategoryFunction catFunc, const char *msg, ...) const Q_ATTRIBUTE_FORMAT_PRINTF(3, 4);

commit c9cdbcb12f80cd72905e49ce1a673eae9f559ca3

Add const overload for QLoggingCategory::operator()()
Change 85e57653 caused a compile error for code that does

  Q_DECLARE_LOGGING_CATEGORY(cat);
  //..
  qCDebug(cat()) << // ...

error: C3848: expression having type 'const QLoggingCategory' would lose
some const-volatile qualifiers in order to call 'QLoggingCategory
&QLoggingCategory::operator ()(void)'

This is a regression from Qt 5.2. Fix the error by adding a const version
of operator()().

Change-Id: I2fb04f2e155962adee0f98089fc5a159000bef56
Reviewed-by: Alex Blasche <alexander.blasche@digia.com>
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/io/qloggingcategory.cpp
@@ -274,6 +274,14 @@ void QLoggingCategory::setEnabled(QtMsgType type, bool enable)
  */
 
 /*!
+    \fn const QLoggingCategory &QLoggingCategory::operator()() const
+
+    Returns the object itself. This allows both a QLoggingCategory variable, and
+    a factory method returning a QLoggingCategory, to be used in \l qCDebug(),
+    \l qCWarning(), \l qCCritical() macros.
+ */
+
+/*!
     Returns a pointer to the global category \c "default" that
     is used e.g. by qDebug(), qWarning(), qCritical(), qFatal().
 

src/corelib/io/qloggingcategory.h
@@ -65,6 +65,7 @@ public:
 
     // allows usage of both factory method and variable in qCX macros
     QLoggingCategory &operator()() { return *this; }
+    const QLoggingCategory &operator()() const { return *this; }
 
     static QLoggingCategory *defaultCategory();
 

tests/auto/corelib/io/qloggingcategory/tst_qloggingcategory.cpp
@@ -428,6 +428,8 @@ private slots:
         QCOMPARE(logMessage, QString());
         qCDebug(TST_LOG, "Check debug with no filter active");
         QCOMPARE(logMessage, QString());
+        qCDebug(TST_LOG(), "Check debug with no filter active");
+        QCOMPARE(logMessage, QString());
         buf = QStringLiteral("tst.log.warning: Check warning with no filter active");
         qCWarning(TST_LOG) << "Check warning with no filter active";
         QCOMPARE(logMessage, buf);