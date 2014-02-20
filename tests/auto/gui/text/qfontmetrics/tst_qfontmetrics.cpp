qt/qtbase

commit f4dd5344fbbce257a40e014acc4e87f4773f40ac

Fix rounding issue
The correct rounding that respects ForceIntegerMetrics flag
is done just a few lines below.

Change-Id: I8e79ebaded90753092ecd3e99eece8df4c824362
Reviewed-by: Lars Knoll <lars.knoll@digia.com>

src/gui/text/qfontengine_ft.cpp
@@ -1604,7 +1604,7 @@ void QFontEngineFT::recalcAdvances(QGlyphLayout *glyphs, QFontEngine::ShaperFlag
                 face = lockFace();
             g = loadGlyph(cacheEnabled ? &defaultGlyphSet : 0, glyphs->glyphs[i], 0, Format_None, true);
             glyphs->advances_x[i] = design ? QFixed::fromFixed(face->glyph->linearHoriAdvance >> 10)
-                                           : QFixed::fromFixed(face->glyph->metrics.horiAdvance).round();
+                                           : QFixed::fromFixed(face->glyph->metrics.horiAdvance);
             if (!cacheEnabled)
                 delete g;
         }

commit d641792ff29cae3ef92bbc76f593e9262b4789ef

Fix QFontMetrics width
Commit f4dd534 introduced a regression, so that QFontMetrics reported
a wrong size (to be more specific width) for FreeType fonts.
The calculation of glyph advances has to to reflect (rounded)
integral number of pixels. This was only done when the glyph was cached.
So in some cases the first call to QFontMetrics::size gave a different
result than the second.
This patch reverts f4dd5344fbbce257a40e014acc4e87f4773f40.

The tst_QFontMetrics::same auto test only happened to work on some
platforms, on BlackBerry for instance it did not. Extended the test
case to make sure it works for different font sizes.

Change-Id: Ia5bb9abd3ff98193c9bba048b85207672ed8d9c3
Reviewed-by: Konstantin Ritt <ritt.ks@gmail.com>

src/gui/text/qfontengine_ft.cpp
@@ -1617,7 +1617,7 @@ void QFontEngineFT::recalcAdvances(QGlyphLayout *glyphs, QFontEngine::ShaperFlag
                 face = lockFace();
             g = loadGlyph(cacheEnabled ? &defaultGlyphSet : 0, glyphs->glyphs[i], 0, Format_None, true);
             glyphs->advances[i] = design ? QFixed::fromFixed(face->glyph->linearHoriAdvance >> 10)
-                                         : QFixed::fromFixed(face->glyph->metrics.horiAdvance);
+                                         : QFixed::fromFixed(face->glyph->metrics.horiAdvance).round();
             if (!cacheEnabled)
                 delete g;
         }

tests/auto/gui/text/qfontmetrics/tst_qfontmetrics.cpp
@@ -100,6 +100,12 @@ void tst_QFontMetrics::same()
     const QString text = QLatin1String("Some stupid STRING");
     QCOMPARE(fm.size(0, text), fm.size(0, text)) ;
 
+    for (int i = 10; i <= 32; ++i) {
+        font.setPixelSize(i);
+        QFontMetrics fm1(font);
+        QCOMPARE(fm1.size(0, text), fm1.size(0, text));
+    }
+
     {
         QImage image;
         QFontMetrics fm2(font, &image);