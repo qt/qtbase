584088f2007d36658e3667df38c4e7f66fe66c9a
Michael Brüning <michael.bruning@digia.com>
2014-03-21 14:36:02 +0100

    Initialize the picpainter State in the QAlphaPaintEngine.
    Patch by John Layt.
    
    Analog to how the print preview problems on windows were fixed, this
    is needed to correctly print e.g. web view contents.
    
    Task-number: QTBUG-36308
    Task-number: QTBUG-37240
    Change-Id: If4ecffde969ed221bbbeea80232f29f095fd71cd
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    Reviewed-by: John Layt <jlayt@kde.org>

--- src/printsupport/kernel/qpaintengine_alpha.cpp
+++ src/printsupport/kernel/qpaintengine_alpha.cpp
@@ -345,6 +345,7 @@ void QAlphaPaintEngine::flushAndInit(bool init)
         d->m_picpainter->setFont(painter()->font());
         d->m_picpainter->setOpacity(painter()->opacity());
         d->m_picpainter->setTransform(painter()->combinedTransform());
+        *d->m_picpainter->d_func()->state = *painter()->d_func()->state;
         d->m_picengine->syncState();
     }
 }
