9ce697f2d54be6d94381c72af28dda79cbc027d4
Wolfgang Bremer <wbremer@blackberry.com>
2014-04-03 12:31:54 +0200

    Rename QNX arm mkspecs to be aligned with SDP structure
    The current SDP uses CPUVARDIR variable to define whether x86 or
    armle-v7 is used. Therefore, the whole structure uses these two
    definitions to separate simulator and device builds. Renaming
    qnx-armv7le-qcc to qnx-armle-v7-qcc allows to directly use CPUVARDIR
    during Qt5 builds. For compatibility reasons the old folder is kept
    and includes the new qmake.conf.
    
    This change is similar to fe61f2d6b29cca87b46dc37c7968b2f765f670ef
    where we already aligned the BB10 mkspecs to the NDK structure.
    
    Change-Id: I7e1c0c2d137dc4049549233940e7f9d1c9f671f0
    Reviewed-by: Andreas Holzammer <andreas.holzammer@kdab.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    Reviewed-by: Fabian Bumberger <fbumberger@rim.com>

--- mkspecs/qnx-armv7le-qcc/qplatformdefs.h
+++ mkspecs/qnx-armv7le-qcc/qplatformdefs.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2012 Research In Motion Limited. <blackberry-qt@qnx.com>
+** Copyright (C) 2012 - 2014 BlackBerry Limited. All rights reserved.
 ** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of the QtCore module of the Qt Toolkit.
@@ -39,71 +39,4 @@
 **
 ****************************************************************************/
 
-#ifndef QPLATFORMDEFS_H
-#define QPLATFORMDEFS_H
-
-// Get Qt defines/settings
-
-#include "qglobal.h"
-
-// Set any POSIX/XOPEN defines at the top of this file to turn on specific APIs
-
-#include <unistd.h>
-
-#define __STDC_CONSTANT_MACROS
-
-// We are hot - unistd.h should have turned on the specific APIs we requested
-
-
-#include <pthread.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <grp.h>
-#include <pwd.h>
-#include <signal.h>
-
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <sys/ipc.h>
-#include <sys/time.h>
-// QNX doesn't have the System V <sys/shm.h> header. This is not a standard
-// POSIX header, it's only documented in the Single UNIX Specification.
-// The preferred POSIX compliant way to share memory is to use the functions
-// in <sys/mman.h> that comply with the POSIX Real Time Interface (1003.1b).
-#include <sys/mman.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <netinet/in.h>
-#ifndef QT_NO_IPV6IFNAME
-#include <net/if.h>
-#endif
-
-// for htonl
-#include <arpa/inet.h>
-
-#define QT_USE_XOPEN_LFS_EXTENSIONS
-#if !defined(__EXT_QNX__READDIR64_R)
-#define QT_NO_READDIR64
-#endif
-#include "../common/posix/qplatformdefs.h"
-#if defined(__EXT_QNX__READDIR64_R)
-#define QT_EXT_QNX_READDIR_R    ::_readdir64_r
-#elif defined(__EXT_QNX__READDIR_R)
-#define QT_EXT_QNX_READDIR_R    ::_readdir_r
-#endif
-
-#define QT_SNPRINTF ::snprintf
-#define QT_VSNPRINTF ::vsnprintf
-
-// QNX6 doesn't have getpagesize()
-inline int getpagesize()
-{
-    return ::sysconf(_SC_PAGESIZE);
-}
-
-#include <stdlib.h>
-
-#define QT_QWS_TEMP_DIR QString::fromLatin1(qgetenv("TMP"))
-
-#endif // QPLATFORMDEFS_H
+#include "../qnx-armle-v7-qcc/qplatformdefs.h"
