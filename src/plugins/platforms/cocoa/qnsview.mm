qt/qtbase

commit 0be1c4899c15221bf265713e714f5301bfa909d5

Cocoa: Don't consume events when closing tool tips
With change acebf677 we treat Qt::ToolTip similar
to Qt::Popup and close them on a mouse click on the
parent window. This mouse click is not forwarded
to the standard mouse event handler.

Add an exception for Qt::ToolTip.

Task-number: QTBUG-38267
Change-Id: Ie3121f651a6ccc2427040e61db4f63967467604d
Reviewed-by: Eike Ziller <eike.ziller@digia.com>
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

src/plugins/platforms/cocoa/qnsview.mm
@@ -639,8 +639,12 @@ static QTouchDevice *touchDevice = 0;
     if (m_platformWindow->m_activePopupWindow) {
         QWindowSystemInterface::handleCloseEvent(m_platformWindow->m_activePopupWindow);
         QWindowSystemInterface::flushWindowSystemEvents();
+        Qt::WindowType type = m_platformWindow->m_activePopupWindow->type();
         m_platformWindow->m_activePopupWindow = 0;
-        return;
+        // Consume the mouse event when closing the popup, except for tool tips
+        // were it's expected that the event is processed normally.
+        if (type != Qt::ToolTip)
+            return;
     }
     if ([self hasMarkedText]) {
         NSInputManager* inputManager = [NSInputManager currentInputManager];

commit 079f2bf5d8a4221b821ec8062affe2efaa9668a0

Cocoa: Fix crash on Combobox popup close.
Regression caused by 0be1c4899c.

The calls to handleCloseEvent/flushWindowSystemEvents
may result in popup window deletion and a stale/null
pointer access.

Get the window type before closing it.

Task-number: QTBUG-38418
Change-Id: I212a56979e0248076e1eb5bf9ede1ff0d424e041
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

src/plugins/platforms/cocoa/qnsview.mm
@@ -637,9 +637,9 @@ static QTouchDevice *touchDevice = 0;
         return [super mouseDown:theEvent];
     m_sendUpAsRightButton = false;
     if (m_platformWindow->m_activePopupWindow) {
+        Qt::WindowType type = m_platformWindow->m_activePopupWindow->type();
         QWindowSystemInterface::handleCloseEvent(m_platformWindow->m_activePopupWindow);
         QWindowSystemInterface::flushWindowSystemEvents();
-        Qt::WindowType type = m_platformWindow->m_activePopupWindow->type();
         m_platformWindow->m_activePopupWindow = 0;
         // Consume the mouse event when closing the popup, except for tool tips
         // were it's expected that the event is processed normally.