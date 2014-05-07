9cd7403801f9dc842a4546ba793f88828cb01552
Oliver Wolff <oliver.wolff@digia.com>
2014-05-07 13:35:42 +0200

    winrt: do not try to close non existent sockets
    Change-Id: I41103db89985f8fbde1faaccaf33c7a76c275f7d
    Reviewed-by: Andrew Knight <andrew.knight@digia.com>

--- src/network/socket/qnativesocketengine_winrt.cpp
+++ src/network/socket/qnativesocketengine_winrt.cpp
@@ -475,9 +475,9 @@ void QNativeSocketEngine::close()
     Q_D(QNativeSocketEngine);
     if (d->socketDescriptor != -1) {
         IClosable *socket = 0;
-        if (d->socketType == QAbstractSocket::TcpSocket)
+        if (d->socketType == QAbstractSocket::TcpSocket && d->tcp)
             d->tcp->QueryInterface(IID_PPV_ARGS(&socket));
-        else if (d->socketType == QAbstractSocket::UdpSocket)
+        else if (d->socketType == QAbstractSocket::UdpSocket && d->udp)
             d->udp->QueryInterface(IID_PPV_ARGS(&socket));
 
         if (socket) {
