c045cb950b3610278b4257712eca88d4e3ecf9fd
Peter Hartmann <phartmann@blackberry.com>
2014-05-20 14:43:27 +0200

    Socks5 socket engine test: Disable UDP over Socks test
    ... because it fails on the new network test server. The Socks5 tests
    in QUdpSocket have already been disabled by commit
    aa3eaf9d2ec4927df51e7d773a66f68ec5e4fce9 .
    
    Task-number: QTBUG-35490
    Change-Id: Ib062adb422ff6e5538f14d15a266d79c3bb53956
    Reviewed-by: Richard J. Moore <rich@kde.org>
    Reviewed-by: Tony Sarajärvi <tony.sarajarvi@digia.com>

--- tests/auto/network/socket/qsocks5socketengine/tst_qsocks5socketengine.cpp
+++ tests/auto/network/socket/qsocks5socketengine/tst_qsocks5socketengine.cpp
@@ -570,16 +570,10 @@ void tst_QSocks5SocketEngine::udpTest()
     QVERIFY(udpSocket.state() == QAbstractSocket::UnconnectedState);
 
     // Bind #1
-#if defined(UBUNTU_ONEIRIC) && defined(__x86_64__)
-    {
-        bool bindSuccessful = udpSocket.bind(QHostAddress("0.0.0.0"), 0);
-        if (!bindSuccessful)
-            QEXPECT_FAIL("", "QTBUG-23380: Fails on some Ubuntu 11.10 x64 configurations", Abort);
-        QVERIFY(bindSuccessful);
-    }
-#else
-    QVERIFY(udpSocket.bind(QHostAddress("0.0.0.0"), 0));
-#endif
+    bool bindSuccessful = udpSocket.bind(QHostAddress("0.0.0.0"), 0);
+    if (!bindSuccessful)
+        QEXPECT_FAIL("", "QTBUG-23380 / QTBUG-35490: Fails on some Ubuntu 11.10 x64 configurations and on new network test server", Abort);
+    QVERIFY(bindSuccessful);
     QVERIFY(udpSocket.state() == QAbstractSocket::BoundState);
     QVERIFY(udpSocket.localPort() != 0);
 
