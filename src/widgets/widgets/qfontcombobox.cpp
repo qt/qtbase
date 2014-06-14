qt/qtbase

commit bfb25c03523757d66974189e54dda97962206ef9

Restrict QFontComboBox's popup width to screen width
Change-Id: I5a92308beb2f245d9f031918bb8154757316ef56
Reviewed-by: Marc Mutz <marc.mutz@kdab.com>

src/widgets/widgets/qfontcombobox.cpp
@@ -50,6 +50,7 @@
 #include <qevent.h>
 #include <qapplication.h>
 #include <private/qcombobox_p.h>
+#include <QDesktopWidget>
 #include <qdebug.h>
 
 QT_BEGIN_NAMESPACE
@@ -546,8 +547,10 @@ bool QFontComboBox::event(QEvent *e)
 {
     if (e->type() == QEvent::Resize) {
         QListView *lview = qobject_cast<QListView*>(view());
-        if (lview)
-            lview->window()->setFixedWidth(width() * 5 / 3);
+        if (lview) {
+            setFixedWidth(qMin(width() * 5 / 3,
+                               QApplication::desktop()->availableGeometry(lview).width()));
+        }
     }
     return QComboBox::event(e);
 }

commit 16b90bb68343dc45a34d59083a2b62200fcc9551

QFontComboBox sometimes became too wide because of wrong fixed size
The fixed width should not be set on QFontComboBox itself, but on the
popup.

This regressed with commit bfb25c03523757d66974189e54dda97962206ef9,
and caused assistants preferences dialog to become wider than the
desktop.

Change-Id: I2059794fc12d34837cdb7dfded80df57d102a6f0
Reviewed-by: Fabian Bumberger <fbumberger@rim.com>

src/widgets/widgets/qfontcombobox.cpp
@@ -548,7 +548,7 @@ bool QFontComboBox::event(QEvent *e)
     if (e->type() == QEvent::Resize) {
         QListView *lview = qobject_cast<QListView*>(view());
         if (lview) {
-            setFixedWidth(qMin(width() * 5 / 3,
+            lview->window()->setFixedWidth(qMin(width() * 5 / 3,
                                QApplication::desktop()->availableGeometry(lview).width()));
         }
     }