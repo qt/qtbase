qt/qtbase

commit 39e06078258393f28c3def96326c26c55e450844

Load winsys and gl dynamically in the windows plugin
The dynamic builds (-opengl dynamic) are now functional on Windows.

In such a build no components in Qt link to any OpenGL libraries directly
and qmake will not automatically add any such libraries to the
applications' makefiles. Instead, the libraries are chosen and loaded
during runtime and applications are expected to use QOpenGLFunctions
instead of direct OpenGLfunction calls.

Set the environment variable QT_OPENGL to desktop or angle to skip testing
and force the given implementation. The application attributes (AA_UseOpenGLES
and such) are also taken into account.

The testing logic is same as before: We try to load opengl32 and
resolve a shader related function. If this fails, ANGLE is chosen. This
allows utilizing full desktop OpenGL on systems that have proper drivers,
while a transparent fallback to ANGLE will be done automatically for
systems that don't. The latter includes also remote desktop connections.

Software rendering via Mesa llvmpipe is supported too. The fallback is
automatic on systems where the desktop test fails and ANGLE fails to load
or initialize (e.g. due to missing libs like d3dcompiler), as long as a
suitable patched build of Mesa is available.

[ChangeLog][QtGui] Dynamic OpenGL implementation loading is now supported
on Windows. This requires Qt to be configured with -opengl dynamic.

Task-number: QTBUG-36483
Change-Id: Ie8bb25a6d55b3a1609b00150aeccd909aec27313
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

src/plugins/platforms/windows/qwindowswindow.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
 ** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of the plugins of the Qt Toolkit.
@@ -48,15 +48,11 @@
 #  include "qwindowscursor.h"
 #endif
 
-#if defined(QT_OPENGL_ES_2) || defined(QT_OPENGL_DYNAMIC)
-#  include "qwindowseglcontext.h"
-#  include <QtGui/QOpenGLFunctions>
-#endif
-
 #include <QtGui/QGuiApplication>
 #include <QtGui/QScreen>
 #include <QtGui/QWindow>
 #include <QtGui/QRegion>
+#include <QtGui/QOpenGLContext>
 #include <private/qsystemlibrary_p.h>
 #include <private/qwindow_p.h>
 #include <private/qguiapplication_p.h>
@@ -867,15 +863,13 @@ QWindowsWindow::QWindowsWindow(QWindow *aWindow, const QWindowsWindowData &data)
     m_opacity(1.0),
     m_dropTarget(0),
     m_savedStyle(0),
-    m_format(aWindow->format()),
-#if defined(QT_OPENGL_ES_2) || defined(QT_OPENGL_DYNAMIC)
-    m_eglSurface(0),
-#endif
+    m_format(aWindow->requestedFormat()),
 #ifdef Q_OS_WINCE
     m_previouslyHidden(false),
 #endif
     m_iconSmall(0),
-    m_iconBig(0)
+    m_iconBig(0),
+    m_surface(0)
 {
     // Clear the creation context as the window can be found in QWindowsContext's map.
     QWindowsContext::instance()->setWindowCreationContext(QSharedPointer<QWindowCreationContext>());
@@ -883,13 +877,14 @@ QWindowsWindow::QWindowsWindow(QWindow *aWindow, const QWindowsWindowData &data)
     const Qt::WindowType type = aWindow->type();
     if (type == Qt::Desktop)
         return; // No further handling for Qt::Desktop
+#ifndef QT_NO_OPENGL
     if (aWindow->surfaceType() == QWindow::OpenGLSurface) {
-        setFlag(OpenGLSurface);
-#if defined(QT_OPENGL_ES_2) || defined(QT_OPENGL_DYNAMIC)
-        if (QOpenGLContext::openGLModuleType() != QOpenGLContext::LibGL)
+        if (QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL)
+            setFlag(OpenGLSurface);
+        else
             setFlag(OpenGL_ES2);
-#endif
     }
+#endif // QT_NO_OPENGL
     updateDropSite();
 
 #ifndef Q_OS_WINCE
@@ -953,13 +948,10 @@ void QWindowsWindow::destroyWindow()
         if (hasMouseCapture())
             setMouseGrabEnabled(false);
         setDropSiteEnabled(false);
-#if defined(QT_OPENGL_ES_2) || defined(QT_OPENGL_DYNAMIC)
-        if (m_eglSurface) {
-            qCDebug(lcQpaGl) << __FUNCTION__ << "Freeing EGL surface " << m_eglSurface << window();
-            eglDestroySurface(m_staticEglContext->display(), m_eglSurface);
-            m_eglSurface = 0;
+        if (m_surface) {
+            m_data.staticOpenGLContext->destroyWindowSurface(m_surface);
+            m_surface = 0;
         }
-#endif
 #ifdef Q_OS_WINCE
         if ((m_windowState & Qt::WindowFullScreen) && !m_previouslyHidden) {
             HWND handle = FindWindow(L"HHTaskBar", L"");
@@ -2144,23 +2136,6 @@ void QWindowsWindow::setEnabled(bool enabled)
         setStyle(newStyle);
 }
 
-#if defined(QT_OPENGL_ES_2) || defined(QT_OPENGL_DYNAMIC)
-EGLSurface QWindowsWindow::ensureEglSurfaceHandle(const QWindowsWindow::QWindowsEGLStaticContextPtr &staticContext, EGLConfig config)
-{
-    if (!m_eglSurface) {
-        m_staticEglContext = staticContext;
-        m_eglSurface = eglCreateWindowSurface(staticContext->display(), config, (EGLNativeWindowType)m_data.hwnd, NULL);
-        if (m_eglSurface == EGL_NO_SURFACE)
-            qWarning("%s: Could not create the egl surface for %s/'%s' (eglCreateWindowSurface failed): error = 0x%x\n",
-                     Q_FUNC_INFO, window()->metaObject()->className(),
-                     qPrintable(window()->objectName()), eglGetError());
-
-            qCDebug(lcQpaGl) << __FUNCTION__<<"Created EGL surface "<< m_eglSurface <<window();
-    }
-    return m_eglSurface;
-}
-#endif // QT_OPENGL_ES_2
-
 QByteArray QWindowsWindow::debugWindowFlags(Qt::WindowFlags wf)
 {
     const int iwf = int(wf);
@@ -2270,6 +2245,14 @@ void QWindowsWindow::setCustomMargins(const QMargins &newCustomMargins)
             << currentFrameGeometry << "->" << newFrame;
         SetWindowPos(m_data.hwnd, 0, newFrame.x(), newFrame.y(), newFrame.width(), newFrame.height(), SWP_NOZORDER | SWP_FRAMECHANGED);
     }
+}
+
+void *QWindowsWindow::surface(void *nativeConfig)
+{
+    if (!m_surface)
+        m_surface = m_data.staticOpenGLContext->createWindowSurface(m_data.hwnd, nativeConfig);
+
+    return m_surface;
 }
 
 QT_END_NAMESPACE

src/corelib/global/qnamespace.h
@@ -521,6 +521,7 @@ public:
         AA_ForceRasterWidgets = 14,
         AA_UseDesktopOpenGL = 15,
         AA_UseOpenGLES = 16,
+        AA_UseSoftwareOpenGL = 17,
 
         // Add new attributes before this line
         AA_AttributeCount

src/corelib/global/qnamespace.qdoc
@@ -168,13 +168,25 @@
     \value AA_ForceRasterWidgets Make top-level widgets use pure raster surfaces,
            and do not support non-native GL-based child widgets.
 
-    \value AA_UseDesktopOpenGL Forces the usage of the desktop OpenGL on
+    \value AA_UseDesktopOpenGL Forces the usage of desktop OpenGL (for example,
+           \e opengl32.dll or \e libGL.so) on platforms that use dynamic loading
+           of the OpenGL implementation.
+           This value has been added in Qt 5.3.
+
+    \value AA_UseOpenGLES Forces the usage of OpenGL ES 2.0 or higher on
            platforms that use dynamic loading of the OpenGL implementation.
            This value has been added in Qt 5.3.
 
-    \value AA_UseOpenGLES Forces the usage of OpenGL ES 2.0 on platforms that
-           use dynamic loading of the OpenGL implementation.
-           This value has been added in Qt 5.3.
+    \value AA_UseSoftwareOpenGL Forces the usage of a software based OpenGL
+           implementation on platforms that use dynamic loading of the OpenGL
+           implementation. This will typically be a patched build of
+           \l{http://www.mesa3d.org/llvmpipe.html}{Mesa llvmpipe}, providing
+           OpenGL 2.1. The value may have no effect if no such OpenGL
+           implementation is available. The default name of this library is
+           QtSoftwareOpenGL.dll and can be overridden by setting the environment
+           variable \e QT_OPENGL_DLL. See the platform-specific pages, for
+           instance \l{Qt for Windows}, for more information.  This value has
+           been added in Qt 5.4.
 
     The following values are obsolete:
 

src/gui/opengl/qopenglversionfunctions.cpp
@@ -227,7 +227,9 @@ QOpenGLFunctions_1_0_CoreBackend::QOpenGLFunctions_1_0_CoreBackend(QOpenGLContex
 {
     // OpenGL 1.0 core functions
 #if defined(Q_OS_WIN)
-    HMODULE handle = GetModuleHandleA("opengl32.dll");
+    HMODULE handle = static_cast<HMODULE>(QOpenGLContext::openGLModuleHandle());
+    if (!handle)
+        handle = GetModuleHandleA("opengl32.dll");
     Viewport = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLint , GLint , GLsizei , GLsizei )>(GetProcAddress(handle, "glViewport"));
     DepthRange = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLdouble , GLdouble )>(GetProcAddress(handle, "glDepthRange"));
     IsEnabled = reinterpret_cast<GLboolean (QOPENGLF_APIENTRYP)(GLenum )>(GetProcAddress(handle, "glIsEnabled"));
@@ -339,7 +341,9 @@ QOpenGLFunctions_1_1_CoreBackend::QOpenGLFunctions_1_1_CoreBackend(QOpenGLContex
 {
     // OpenGL 1.1 core functions
 #if defined(Q_OS_WIN)
-    HMODULE handle = GetModuleHandleA("opengl32.dll");
+    HMODULE handle = static_cast<HMODULE>(QOpenGLContext::openGLModuleHandle());
+    if (!handle)
+        handle = GetModuleHandleA("opengl32.dll");
     Indexubv = reinterpret_cast<void (QOPENGLF_APIENTRYP)(const GLubyte *)>(GetProcAddress(handle, "glIndexubv"));
     Indexub = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLubyte )>(GetProcAddress(handle, "glIndexub"));
     IsTexture = reinterpret_cast<GLboolean (QOPENGLF_APIENTRYP)(GLuint )>(GetProcAddress(handle, "glIsTexture"));
@@ -991,7 +995,9 @@ QOpenGLFunctions_1_0_DeprecatedBackend::QOpenGLFunctions_1_0_DeprecatedBackend(Q
 {
     // OpenGL 1.0 deprecated functions
 #if defined(Q_OS_WIN)
-    HMODULE handle = GetModuleHandleA("opengl32.dll");
+    HMODULE handle = static_cast<HMODULE>(QOpenGLContext::openGLModuleHandle());
+    if (!handle)
+        handle = GetModuleHandleA("opengl32.dll");
     Translatef = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLfloat , GLfloat , GLfloat )>(GetProcAddress(handle, "glTranslatef"));
     Translated = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLdouble , GLdouble , GLdouble )>(GetProcAddress(handle, "glTranslated"));
     Scalef = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLfloat , GLfloat , GLfloat )>(GetProcAddress(handle, "glScalef"));
@@ -1523,7 +1529,9 @@ QOpenGLFunctions_1_1_DeprecatedBackend::QOpenGLFunctions_1_1_DeprecatedBackend(Q
 {
     // OpenGL 1.1 deprecated functions
 #if defined(Q_OS_WIN)
-    HMODULE handle = GetModuleHandleA("opengl32.dll");
+    HMODULE handle = static_cast<HMODULE>(QOpenGLContext::openGLModuleHandle());
+    if (!handle)
+        handle = GetModuleHandleA("opengl32.dll");
     PushClientAttrib = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLbitfield )>(GetProcAddress(handle, "glPushClientAttrib"));
     PopClientAttrib = reinterpret_cast<void (QOPENGLF_APIENTRYP)()>(GetProcAddress(handle, "glPopClientAttrib"));
     PrioritizeTextures = reinterpret_cast<void (QOPENGLF_APIENTRYP)(GLsizei , const GLuint *, const GLfloat *)>(GetProcAddress(handle, "glPrioritizeTextures"));

src/platformsupport/eglconvenience/eglconvenience.pri
@@ -40,15 +40,4 @@ contains(QT_CONFIG,egl) {
             $$PWD/qxlibeglintegration.cpp
     }
     CONFIG += egl
-
-} else: contains(QT_CONFIG,dynamicgl) {
-    HEADERS += \
-        $$PWD/qeglconvenience_p.h \
-        $$PWD/qeglplatformcontext_p.h \
-        $$PWD/qeglpbuffer_p.h
-
-    SOURCES += \
-        $$PWD/qeglconvenience.cpp \
-        $$PWD/qeglplatformcontext.cpp \
-        $$PWD/qeglpbuffer.cpp
 }

commit 21c8385105dde242384b5ce05bc953a9995403f4

Windows: Fix build with QT_NO_OPENGL
Fixes compiler breakage introduced in 39e06078258393f.

Change-Id: I39813018915a70b848ca9377089e262952715545
Reviewed-by: Laszlo Agocs <laszlo.agocs@digia.com>

src/plugins/platforms/windows/qwindowswindow.cpp
@@ -956,10 +956,12 @@ void QWindowsWindow::destroyWindow()
         if (hasMouseCapture())
             setMouseGrabEnabled(false);
         setDropSiteEnabled(false);
+#ifndef QT_NO_OPENGL
         if (m_surface) {
             m_data.staticOpenGLContext->destroyWindowSurface(m_surface);
             m_surface = 0;
         }
+#endif
 #ifdef Q_OS_WINCE
         if ((m_windowState & Qt::WindowFullScreen) && !m_previouslyHidden) {
             HWND handle = FindWindow(L"HHTaskBar", L"");
@@ -2287,10 +2289,14 @@ void QWindowsWindow::setCustomMargins(const QMargins &newCustomMargins)
 
 void *QWindowsWindow::surface(void *nativeConfig)
 {
+#ifdef QT_NO_OPENGL
+    return 0;
+#else
     if (!m_surface)
         m_surface = m_data.staticOpenGLContext->createWindowSurface(m_data.hwnd, nativeConfig);
 
     return m_surface;
+#endif
 }
 
 QT_END_NAMESPACE