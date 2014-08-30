qt/qtbase

commit 67c83f329e7fb6fbf5d8e402f42ea8916c34f01c

Tighten Q_OS_WINRT ifdefs in qfunctions_winrt.h
This allows the convenience macros to be used on desktop Windows 8 when
interacting with Windows Runtime types.

Change-Id: I09c6b18a6ee9711371ef7dc23fb1d3354198db1c
Reviewed-by: Maurice Kalinowski <maurice.kalinowski@digia.com>

src/corelib/kernel/qfunctions_winrt.h
@@ -44,8 +44,6 @@
 
 #include <QtCore/qglobal.h>
 
-#ifdef Q_OS_WINRT
-
 #include <QtCore/QThread>
 #include <QtCore/QAbstractEventDispatcher>
 #include <QtCore/qt_windows.h>
@@ -56,6 +54,8 @@ QT_BEGIN_NAMESPACE
 #endif
 
 QT_END_NAMESPACE
+
+#ifdef Q_OS_WINRT
 
 // Environment ------------------------------------------------------
 errno_t qt_winrt_getenv_s(size_t*, char*, size_t, const char*);
@@ -121,6 +121,8 @@ generate_inline_return_func4(getenv_s, errno_t, size_t *, char *, size_t, const 
 generate_inline_return_func2(_putenv_s, errno_t, const char *, const char *)
 generate_inline_return_func0(tzset, void)
 generate_inline_return_func0(_tzset, void)
+
+#endif // Q_OS_WINRT
 
 // Convenience macros for handling HRESULT values
 #define RETURN_IF_FAILED(msg, ret) \
@@ -211,5 +213,4 @@ static inline HRESULT await(const Microsoft::WRL::ComPtr<T> &asyncOp, U *results
 
 } // QWinRTFunctions
 
-#endif // Q_OS_WINRT
 #endif // QFUNCTIONS_WINRT_H

commit bdcd636dcbf8a69579aacc2a468d4452ba41d077

Fix headercheck failure introduced by 67c83f329e7fb6fbf5d8e402f42
That commit exposed part of qfunctions_winrt.h for non-WinRT use
(Windows 8) without wrapping with Q_OS_WIN. That meant the headercheck
pass failed to compile when outside of Windows.

Change-Id: Ie731cce21e5102f5e5879b147b7738a7d0a91ecd
Reviewed-by: Andrew Knight <andrew.knight@digia.com>

src/corelib/kernel/qfunctions_winrt.h
@@ -44,6 +44,8 @@
 
 #include <QtCore/qglobal.h>
 
+#ifdef Q_OS_WIN
+
 #include <QtCore/QThread>
 #include <QtCore/QAbstractEventDispatcher>
 #include <QtCore/qt_windows.h>
@@ -212,5 +214,7 @@ static inline HRESULT await(const Microsoft::WRL::ComPtr<T> &asyncOp, U *results
 }
 
 } // QWinRTFunctions
+
+#endif // Q_OS_WIN
 
 #endif // QFUNCTIONS_WINRT_H