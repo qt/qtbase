qt/qtbase

commit 37ca2224eca671200a2710f57f970d2993e62aa5

QFileDialog: don't create widgets if the platform dialog will be used
This is a performance and memory optimization which also fixes bugs
that are related to creating widgets, file system models etc.
despite using platform native dialogs.  Similar to
785bc64f8e743ac269f15cbe7fecba93d3d507ac for QColorDialog.

Task-number: QTBUG-33039
Change-Id: Ia1aa7ec1f43b47006b9ebd377aed15c958538a17
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

src/widgets/dialogs/qfiledialog_p.h
@@ -135,8 +135,11 @@ public:
     QList<QUrl> addDefaultSuffixToUrls(const QList<QUrl> &urlsToFix) const;
     bool removeDirectory(const QString &path);
     void setLabelTextControl(QFileDialog::DialogLabel label, const QString &text);
+    inline void updateLookInLabel();
     inline void updateFileNameLabel();
+    inline void updateFileTypeLabel();
     void updateOkButtonText(bool saveAsOnFolder = false);
+    void updateCancelButtonText();
 
     inline QModelIndex mapToSource(const QModelIndex &index) const;
     inline QModelIndex mapFromSource(const QModelIndex &index) const;
@@ -249,6 +252,7 @@ public:
     // dialog. Returning false means that a non-native dialog must be
     // used instead.
     bool canBeNativeDialog();
+    inline bool usingWidgets() const;
 
     void setDirectory_sys(const QUrl &directory);
     QUrl directory_sys() const;
@@ -347,7 +351,7 @@ inline QModelIndex QFileDialogPrivate::mapFromSource(const QModelIndex &index) c
 }
 
 inline QString QFileDialogPrivate::rootPath() const {
-    return model->rootPath();
+    return (model ? model->rootPath() : QStringLiteral("/"));
 }
 
 inline void QFileDialogPrivate::setDirectory_sys(const QUrl &directory)

src/plugins/platforms/windows/qwindowsdialoghelpers.cpp
@@ -578,8 +578,8 @@ bool QWindowsDialogHelperBase<BaseClass>::show(Qt::WindowFlags,
         m_ownerWindow = 0;
     }
     if (QWindowsContext::verboseDialogs)
-        qDebug("%s modal=%d native=%p parent=%p" ,
-               __FUNCTION__, modal, m_nativeDialog.data(), m_ownerWindow);
+        qDebug("%s modal=%d modal supported? %d native=%p parent=%p" ,
+               __FUNCTION__, modal, supportsNonModalDialog(parent), m_nativeDialog.data(), m_ownerWindow);
     if (!modal && !supportsNonModalDialog(parent))
         return false; // Was it changed in-between?
     if (!ensureNativeDialog())

tests/auto/widgets/dialogs/qfiledialog/tst_qfiledialog.cpp
@@ -66,6 +66,8 @@
 #include <private/qfilesystemmodel_p.h>
 #include <private/qfiledialog_p.h>
 #endif
+#include <private/qguiapplication_p.h>
+#include <qpa/qplatformtheme.h>
 #include <QFileDialog>
 #include <QFileSystemModel>
 
@@ -145,6 +147,7 @@ private slots:
     void clearLineEdit();
     void enableChooseButton();
     void hooks();
+    void widgetlessNativeDialog();
 #ifdef Q_OS_UNIX
 #ifdef QT_BUILD_INTERNAL
     void tildeExpansion_data();
@@ -1394,6 +1397,20 @@ void tst_QFiledialog::hooks()
     QCOMPARE(QFileDialog::getOpenFileUrl(), QUrl("http://openUrl"));
     QCOMPARE(QFileDialog::getOpenFileUrls(), QList<QUrl>() << QUrl("http://openUrls"));
     QCOMPARE(QFileDialog::getSaveFileUrl(), QUrl("http://saveUrl"));
+}
+
+void tst_QFiledialog::widgetlessNativeDialog()
+{
+    if (!QGuiApplicationPrivate::platformTheme()->usePlatformNativeDialog(QPlatformTheme::FileDialog))
+        QSKIP("This platform always uses widgets to realize its QFileDialog, instead of the native file dialog.");
+    QFileDialog fd;
+    fd.setWindowModality(Qt::ApplicationModal);
+    fd.show();
+    QTRY_VERIFY(fd.isVisible());
+    QFileSystemModel *model = fd.findChild<QFileSystemModel*>("qt_filesystem_model");
+    QVERIFY(!model);
+    QPushButton *button = fd.findChild<QPushButton*>();
+    QVERIFY(!button);
 }
 
 #ifdef Q_OS_UNIX

commit 2113bdee5dc78d6b615768afabc3ecd7d14f4b83

QFileDialog: save and restore all settings even if widgets not used
Some applications may use native file dialogs and some may use the
widget implementation, but QFileDialog always saves all settings to
the same place each time it destroyed.  So it is important to remember
all settings read in restoreState and put them back in saveState.
This bug was introduced by 37ca2224eca671200a2710f57f970d2993e62aa5

Task-number: QTBUG-36888
Change-Id: Ied1db83817480312e963a042784d7bdd16f0a2b1
Reviewed-by: Jan Arve Sæther <jan-arve.saether@digia.com>

src/widgets/dialogs/qfiledialog.cpp
@@ -454,15 +454,15 @@ QByteArray QFileDialog::saveState() const
         stream << d->qFileDialogUi->splitter->saveState();
         stream << d->qFileDialogUi->sidebar->urls();
     } else {
-        stream << QByteArray();
-        stream << QList<QUrl>();
+        stream << d->splitterState;
+        stream << d->sidebarUrls;
     }
     stream << history();
     stream << *lastVisitedDir();
     if (d->usingWidgets())
         stream << d->qFileDialogUi->treeView->header()->saveState();
     else
-        stream << QByteArray();
+        stream << d->headerData;
     stream << qint32(viewMode());
     return data;
 }
@@ -484,9 +484,6 @@ bool QFileDialog::restoreState(const QByteArray &state)
     QDataStream stream(&sd, QIODevice::ReadOnly);
     if (stream.atEnd())
         return false;
-    QByteArray splitterState;
-    QByteArray headerData;
-    QList<QUrl> bookmarks;
     QStringList history;
     QString currentDirectory;
     qint32 marker;
@@ -497,11 +494,11 @@ bool QFileDialog::restoreState(const QByteArray &state)
     if (marker != QFileDialogMagic || v != version)
         return false;
 
-    stream >> splitterState
-           >> bookmarks
+    stream >> d->splitterState
+           >> d->sidebarUrls
            >> history
            >> currentDirectory
-           >> headerData
+           >> d->headerData
            >> viewMode;
 
     setDirectory(lastVisitedDir()->isEmpty() ? currentDirectory : *lastVisitedDir());
@@ -510,7 +507,7 @@ bool QFileDialog::restoreState(const QByteArray &state)
     if (!d->usingWidgets())
         return true;
 
-    if (!d->qFileDialogUi->splitter->restoreState(splitterState))
+    if (!d->qFileDialogUi->splitter->restoreState(d->splitterState))
         return false;
     QList<int> list = d->qFileDialogUi->splitter->sizes();
     if (list.count() >= 2 && list.at(0) == 0 && list.at(1) == 0) {
@@ -519,12 +516,12 @@ bool QFileDialog::restoreState(const QByteArray &state)
         d->qFileDialogUi->splitter->setSizes(list);
     }
 
-    d->qFileDialogUi->sidebar->setUrls(bookmarks);
+    d->qFileDialogUi->sidebar->setUrls(d->sidebarUrls);
     while (history.count() > 5)
         history.pop_front();
     setHistory(history);
     QHeaderView *headerView = d->qFileDialogUi->treeView->header();
-    if (!headerView->restoreState(headerData))
+    if (!headerView->restoreState(d->headerData))
         return false;
 
     QList<QAction*> actions = headerView->actions();

src/widgets/dialogs/qfiledialog_p.h
@@ -273,6 +273,11 @@ public:
 
     QSharedPointer<QFileDialogOptions> options;
 
+    // Memory of what was read from QSettings in restoreState() in case widgets are not used
+    QByteArray splitterState;
+    QByteArray headerData;
+    QList<QUrl> sidebarUrls;
+
     ~QFileDialogPrivate();
 
 private: