bb3d2ca9f18071537888622310ca0cb7f587e61d
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2014-09-16 16:33:57 +0200

    QToolButton: properly reset the size hint when a menu is set on it
    QToolButton::sizeHint() takes into account the presence of a menu.
    However, setMenu() doesn't retrigger a size hint recalculation. Hence,
    (un)setting a menu on an already sized tool button won't properly
    reset the size hint. Since the calculated size hint is cached,
    delete the cached value and call updateGeometry to cause a
    recalculation.
    
    Task-number: QTBUG-38949
    Change-Id: I6e79e5e70e31afdfd129282b3668875eca86f51d
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>

--- src/widgets/widgets/qtoolbutton.cpp
+++ src/widgets/widgets/qtoolbutton.cpp
@@ -652,6 +652,10 @@ void QToolButton::setMenu(QMenu* menu)
     } else {
         d->menuAction = 0;
     }
+
+    // changing the menu set may change the size hint, so reset it
+    d->sizeHint = QSize();
+    updateGeometry();
     update();
 }
 
