qt/qtbase

commit bdebec4e2ef79f1771d2dcc22f9a919eb4487567

Menu item shortcuts reaching Qt's event loop are disabled
See comment in code and Cocoa event dispatching overview at
https://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/EventOverview/EventArchitecture/EventArchitecture.html

Task-number: QTBUG-30657
Change-Id: I88907aeeefa4962e1121495cd51af17a8e71b7de
Reviewed-by: Frederik Gladhorn <frederik.gladhorn@digia.com>

src/widgets/kernel/qshortcut.cpp
@@ -246,9 +246,19 @@ static bool correctActionContext(Qt::ShortcutContext context, QAction *a, QWidge
         QWidget *w = widgets.at(i);
 #ifndef QT_NO_MENU
         if (QMenu *menu = qobject_cast<QMenu *>(w)) {
+#ifdef Q_OS_MAC
+            // On Mac, menu item shortcuts are processed before reaching any window.
+            // That means that if a menu action shortcut has not been already processed
+            // (and reaches this point), then the menu item itself has been disabled.
+            // This occurs at the QPA level on Mac, were we disable all the Cocoa menus
+            // when showing a modal window.
+            Q_UNUSED(menu);
+            continue;
+#else
             QAction *a = menu->menuAction();
             if (correctActionContext(context, a, active_window))
                 return true;
+#endif
         } else
 #endif
             if (correctWidgetContext(context, w, active_window))

commit 412ec70f14aa0c966d13481a35c498e6c905dec3

OS X: Fix menu item shortcuts without modifiers
A regression was introduced by bdebec4e2ef79f1771d2dcc22f9a919eb4487567.
The intention of the change was to avoid using Qt's shortcut mechanism
to trigger menu items which were already triggered through the regular
menu API in Cocoa. However, Cocoa has trouble with key equivalents that
do not have any keyboard modifiers. Thus, we have to allow these
particular key sequences to go through the regular system. I've verified
that the original bug is still fixed with this change.

[ChangeLog][OS X] Fixed menu item shortcuts without keyboard
modifiers.

Task-number: QTBUG-41192
Change-Id: I8f5a9cbc7a448b3cb0519baed95be5cbb630205c
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

src/widgets/kernel/qshortcut.cpp
@@ -279,7 +279,7 @@ static bool correctActionContext(Qt::ShortcutContext context, QAction *a, QWidge
             // (and reaches this point), then the menu item itself has been disabled.
             // This occurs at the QPA level on Mac, were we disable all the Cocoa menus
             // when showing a modal window.
-            if (a->shortcut().count() <= 1)
+            if (a->shortcut().count() < 1 || (a->shortcut().count() == 1 && (a->shortcut()[0] & Qt::MODIFIER_MASK) != 0))
                 continue;
 #endif
             QAction *a = menu->menuAction();