qt/qtbase

commit 8eb3d724a92b8eeb1e96362bd6997fcee178af89

Improve documentation regarding moving item view columns.
The only way I was able to find information on this was through bug
reports.

Change-Id: Iac59a82fab9c216495860ad3b1efad1125130002
Reviewed-by: Andy Shaw <andy.shaw@digia.com>
Reviewed-by: Marc Mutz <marc.mutz@kdab.com>

src/widgets/itemviews/qheaderview.cpp
@@ -1087,6 +1087,9 @@ int QHeaderView::logicalIndex(int visualIndex) const
     If \a movable is true, the header may be moved by the user; otherwise it
     is fixed in place.
 
+    By default, all sections are movable except the first, which always stays
+    fixed.
+
     \sa sectionsMovable(), sectionMoved()
 */
 

src/widgets/itemviews/qtreeview.cpp
@@ -109,6 +109,10 @@ QT_BEGIN_NAMESPACE
     its header. If this value is set to true, this property will override the
     resize mode set on the last section in the header.
 
+    By default, all columns in a tree view are movable except the first. To
+    disable movement of these columns, use QHeaderView's
+    \l {QHeaderView::}{setSectionsMovable()} function. For more information
+    about rearranging sections, see \l {Moving Header Sections}.
 
     \section1 Key Bindings
 

commit 3312ac91692c3a4a033d123eecbf982d3ff10471

Correct information about all headers being movable.
This only applies to QTreeView.

8eb3d724a92b8eeb1e96362bd6997fcee178af89 introduced this error.

Change-Id: Ia8a2c387afbd19e4d98ea6f81e5ce6409d79bcd9
Reviewed-by: Thorbjørn Lund Martsum <tmartsum@gmail.com>

src/widgets/itemviews/qheaderview.cpp
@@ -1087,9 +1087,6 @@ int QHeaderView::logicalIndex(int visualIndex) const
     If \a movable is true, the header may be moved by the user; otherwise it
     is fixed in place.
 
-    By default, all sections are movable except the first, which always stays
-    fixed.
-
     \sa sectionsMovable(), sectionMoved()
 */
 