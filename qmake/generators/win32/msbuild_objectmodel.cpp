qt/qtbase

commit 4f21eb0387be35b52bc76e2f7dd10fe8376f8c05

uncopy & -pastify code
Make use of the mythical C++ feature "function" to soothe the brain ache
of anyone who looks into this part of the code.

Change-Id: I740e29f1777d91d3b34a61fa62a5c23c222334b9
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

qmake/generators/win32/msbuild_objectmodel.cpp
@@ -1805,31 +1805,7 @@ void VCXProjectWriter::addFilters(VCProject &project, XmlOutput &xmlFilter, cons
     bool added = false;
 
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
-        VCFilter filter;
-        const VCProjectSingleConfig &singleCfg = project.SingleProjects.at(i);
-        if (filtername == "Root Files") {
-            filter = singleCfg.RootFiles;
-        } else if (filtername == "Source Files") {
-            filter = singleCfg.SourceFiles;
-        } else if (filtername == "Header Files") {
-            filter = singleCfg.HeaderFiles;
-        } else if (filtername == "Generated Files") {
-            filter = singleCfg.GeneratedFiles;
-        } else if (filtername == "LexYacc Files") {
-            filter = singleCfg.LexYaccFiles;
-        } else if (filtername == "Translation Files") {
-            filter = singleCfg.TranslationFiles;
-        } else if (filtername == "Form Files") {
-            filter = singleCfg.FormFiles;
-        } else if (filtername == "Resource Files") {
-            filter = singleCfg.ResourceFiles;
-        } else if (filtername == "Deployment Files") {
-            filter = singleCfg.DeploymentFiles;
-        } else {
-            // ExtraCompilers
-            filter = project.SingleProjects[i].filterForExtraCompiler(filtername);
-        }
-
+        const VCFilter filter = project.SingleProjects.at(i).filterByName(filtername);
         if(!filter.Files.isEmpty() && !added) {
             xmlFilter << tag("Filter")
                       << attrTag("Include", filtername)
@@ -1851,31 +1827,7 @@ void VCXProjectWriter::outputFilter(VCProject &project, XmlOutput &xml, XmlOutpu
         root.reset(new XTreeNode);
 
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
-        VCFilter filter;
-        const VCProjectSingleConfig &singleCfg = project.SingleProjects.at(i);
-        if (filtername == "Root Files") {
-            filter = singleCfg.RootFiles;
-        } else if (filtername == "Source Files") {
-            filter = singleCfg.SourceFiles;
-        } else if (filtername == "Header Files") {
-            filter = singleCfg.HeaderFiles;
-        } else if (filtername == "Generated Files") {
-            filter = singleCfg.GeneratedFiles;
-        } else if (filtername == "LexYacc Files") {
-            filter = singleCfg.LexYaccFiles;
-        } else if (filtername == "Translation Files") {
-            filter = singleCfg.TranslationFiles;
-        } else if (filtername == "Form Files") {
-            filter = singleCfg.FormFiles;
-        } else if (filtername == "Resource Files") {
-            filter = singleCfg.ResourceFiles;
-        } else if (filtername == "Deployment Files") {
-            filter = singleCfg.DeploymentFiles;
-        } else {
-            // ExtraCompilers
-            filter = project.SingleProjects[i].filterForExtraCompiler(filtername);
-        }
-
+        const VCFilter filter = project.SingleProjects.at(i).filterByName(filtername);
         // Merge all files in this filter to root tree
         for (int x = 0; x < filter.Files.count(); ++x)
             root->addElement(filter.Files.at(x));
@@ -1896,31 +1848,7 @@ void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, Xml
     bool fileAdded = false;
 
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
-        VCFilter filter;
-        const VCProjectSingleConfig &singleCfg = project.SingleProjects.at(i);
-        if (filtername.startsWith("Root Files")) {
-            filter = singleCfg.RootFiles;
-        } else if (filtername.startsWith("Source Files")) {
-            filter = singleCfg.SourceFiles;
-        } else if (filtername.startsWith("Header Files")) {
-            filter = singleCfg.HeaderFiles;
-        } else if (filtername.startsWith("Generated Files")) {
-            filter = singleCfg.GeneratedFiles;
-        } else if (filtername.startsWith("LexYacc Files")) {
-            filter = singleCfg.LexYaccFiles;
-        } else if (filtername.startsWith("Translation Files")) {
-            filter = singleCfg.TranslationFiles;
-        } else if (filtername.startsWith("Form Files")) {
-            filter = singleCfg.FormFiles;
-        } else if (filtername.startsWith("Resource Files")) {
-            filter = singleCfg.ResourceFiles;
-        } else if (filtername.startsWith("Deployment Files")) {
-            filter = singleCfg.DeploymentFiles;
-        } else {
-            // ExtraCompilers
-            filter = project.SingleProjects[i].filterForExtraCompiler(filtername);
-        }
-
+        VCFilter filter = project.SingleProjects.at(i).filterByName(filtername);
         if (filter.Config) // only if the filter is not empty
             if (outputFileConfig(filter, xml, xmlFilter, info.file, filtername, fileAdded)) // only add it once.
                 fileAdded = true;

qmake/generators/win32/msvc_objectmodel.cpp
@@ -2414,11 +2414,34 @@ bool VCFilter::addExtraCompiler(const VCFilterFile &info)
 }
 
 // VCProjectSingleConfig --------------------------------------------
-VCFilter& VCProjectSingleConfig::filterForExtraCompiler(const QString &compilerName)
+const VCFilter &VCProjectSingleConfig::filterByName(const QString &name) const
+{
+    if (name == "Root Files")
+        return RootFiles;
+    if (name == "Source Files")
+        return SourceFiles;
+    if (name == "Header Files")
+        return HeaderFiles;
+    if (name == "Generated Files")
+        return GeneratedFiles;
+    if (name == "LexYacc Files")
+        return LexYaccFiles;
+    if (name == "Translation Files")
+        return TranslationFiles;
+    if (name == "Form Files")
+        return FormFiles;
+    if (name == "Resource Files")
+        return ResourceFiles;
+    if (name == "Deployment Files")
+        return DeploymentFiles;
+    return filterForExtraCompiler(name);
+}
+
+const VCFilter &VCProjectSingleConfig::filterForExtraCompiler(const QString &compilerName) const
 {
     for (int i = 0; i < ExtraCompilersFiles.count(); ++i)
         if (ExtraCompilersFiles.at(i).Name == compilerName)
-            return ExtraCompilersFiles[i];
+            return ExtraCompilersFiles.at(i);
 
     static VCFilter nullFilter;
     return nullFilter;
@@ -2885,30 +2908,7 @@ void VCProjectWriter::outputFilter(VCProject &project, XmlOutput &xml, const QSt
     triState parse = unset;
 
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
-        VCFilter filter;
-        const VCProjectSingleConfig &projectSingleConfig = project.SingleProjects.at(i);
-        if (filtername == "RootFiles") {
-            filter = projectSingleConfig.RootFiles;
-        } else if (filtername == "Sources") {
-            filter = projectSingleConfig.SourceFiles;
-        } else if (filtername == "Headers") {
-            filter = projectSingleConfig.HeaderFiles;
-        } else if (filtername == "GeneratedFiles") {
-            filter = projectSingleConfig.GeneratedFiles;
-        } else if (filtername == "LexYaccFiles") {
-            filter = projectSingleConfig.LexYaccFiles;
-        } else if (filtername == "TranslationFiles") {
-            filter = projectSingleConfig.TranslationFiles;
-        } else if (filtername == "FormFiles") {
-            filter = projectSingleConfig.FormFiles;
-        } else if (filtername == "ResourceFiles") {
-            filter = projectSingleConfig.ResourceFiles;
-        } else if (filtername == "DeploymentFiles") {
-            filter = projectSingleConfig.DeploymentFiles;
-        } else {
-            // ExtraCompilers
-            filter = project.SingleProjects[i].filterForExtraCompiler(filtername);
-        }
+        const VCFilter filter = project.SingleProjects.at(i).filterByName(filtername);
 
         // Merge all files in this filter to root tree
         for (int x = 0; x < filter.Files.count(); ++x)
@@ -2947,31 +2947,7 @@ void VCProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, cons
     xml << tag(q_File)
             << attrS(_RelativePath, Option::fixPathToLocalOS(info.file));
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
-        VCFilter filter;
-        const VCProjectSingleConfig &projectSingleConfig = project.SingleProjects.at(i);
-        if (filtername == "RootFiles") {
-            filter = projectSingleConfig.RootFiles;
-        } else if (filtername == "Sources") {
-            filter = projectSingleConfig.SourceFiles;
-        } else if (filtername == "Headers") {
-            filter = projectSingleConfig.HeaderFiles;
-        } else if (filtername == "GeneratedFiles") {
-            filter = projectSingleConfig.GeneratedFiles;
-        } else if (filtername == "LexYaccFiles") {
-            filter = projectSingleConfig.LexYaccFiles;
-        } else if (filtername == "TranslationFiles") {
-            filter = projectSingleConfig.TranslationFiles;
-        } else if (filtername == "FormFiles") {
-            filter = projectSingleConfig.FormFiles;
-        } else if (filtername == "ResourceFiles") {
-            filter = projectSingleConfig.ResourceFiles;
-        } else if (filtername == "DeploymentFiles") {
-            filter = projectSingleConfig.DeploymentFiles;
-        } else {
-            // ExtraCompilers
-            filter = project.SingleProjects[i].filterForExtraCompiler(filtername);
-        }
-
+        VCFilter filter = project.SingleProjects.at(i).filterByName(filtername);
         if (filter.Config) // only if the filter is not empty
             outputFileConfig(filter, xml, info.file);
     }

qmake/generators/win32/msvc_objectmodel.h
@@ -1020,8 +1020,8 @@ public:
 
     bool                    flat_files;
 
-    // Accessor for extracompilers
-    VCFilter               &filterForExtraCompiler(const QString &compilerName);
+    const VCFilter &filterByName(const QString &name) const;
+    const VCFilter &filterForExtraCompiler(const QString &compilerName) const;
 };
 
 // Tree & Flat view of files --------------------------------------------------

commit e5a8134765cdc69ba916416166f4d416137e2ffc

fix vcxproj generation for CONFIG-=flat
Commit 4f21eb03 broke the generation of non-flat vcxprojs.
XTreeNode passes filter names to outputFileConfigs that have
the source subdirectory suffixed (e.g. "Generated Files\subdir").
That's why the original code tested the filter names with
QString::startsWith.
I've changed the signature of outputFileConfigs to take a filterId
parameter which contains the unaltered filter name (e.g.
"Generated Files") that will determine the correct filter.

Task-number: QTBUG-41746
Change-Id: If33428526a098f433cd6ceb8ab6608bd9f94ef17
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

qmake/generators/win32/msbuild_objectmodel.cpp
@@ -341,7 +341,8 @@ static QStringList unquote(const QStringList &values)
 }
 
 // Tree file generation ---------------------------------------------
-void XTreeNode::generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName, VCProject &tool, const QString &filter) {
+void XTreeNode::generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName,
+                            VCProject &tool, const QString &filter, const QString &filterId) {
 
     if (children.size()) {
         // Filter
@@ -362,38 +363,39 @@ void XTreeNode::generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString 
             if ((*it)->children.size())
             {
                 if ( !tempFilterName.isEmpty() )
-                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, tempFilterName);
+                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, tempFilterName, filterId);
                 else
-                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, filter);
+                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, filter, filterId);
             }
         // Second round, do leafs
         for (it = children.constBegin(); it != end; ++it)
             if (!(*it)->children.size())
             {
                 if ( !tempFilterName.isEmpty() )
-                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, tempFilterName);
+                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, tempFilterName, filterId);
                 else
-                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, filter);
+                    (*it)->generateXML(xml, xmlFilter, it.key(), tool, filter, filterId);
             }
     } else {
         // Leaf
         xml << tag(_ItemGroup);
         xmlFilter << tag(_ItemGroup);
-        VCXProjectWriter::outputFileConfigs(tool, xml, xmlFilter, info, filter);
+        VCXProjectWriter::outputFileConfigs(tool, xml, xmlFilter, info, filter, filterId);
         xmlFilter << closetag();
         xml << closetag();
     }
 }
 
 // Flat file generation ---------------------------------------------
-void XFlatNode::generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &/*tagName*/, VCProject &tool, const QString &filter) {
+void XFlatNode::generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &/*tagName*/,
+                            VCProject &tool, const QString &filter, const QString &filterId) {
     if (children.size()) {
         ChildrenMapFlat::ConstIterator it = children.constBegin();
         ChildrenMapFlat::ConstIterator end = children.constEnd();
         xml << tag(_ItemGroup);
         xmlFilter << tag(_ItemGroup);
         for (; it != end; ++it) {
-            VCXProjectWriter::outputFileConfigs(tool, xml, xmlFilter, (*it), filter);
+            VCXProjectWriter::outputFileConfigs(tool, xml, xmlFilter, (*it), filter, filterId);
         }
         xml << closetag();
         xmlFilter << closetag();
@@ -1836,19 +1838,21 @@ void VCXProjectWriter::outputFilter(VCProject &project, XmlOutput &xml, XmlOutpu
     if (!root->hasElements())
         return;
 
-    root->generateXML(xml, xmlFilter, "", project, filtername); // output root tree
+    root->generateXML(xml, xmlFilter, "", project, filtername, filtername); // output root tree
 }
 
 // Output all configurations (by filtername) for a file (by info)
 // A filters config output is in VCFilter.outputFileConfig()
-void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter, const VCFilterFile &info, const QString &filtername)
+void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter,
+                                         const VCFilterFile &info, const QString &filtername,
+                                         const QString &filterId)
 {
     // We need to check if the file has any custom build step.
     // If there is one then it has to be included with "CustomBuild Include"
     bool hasCustomBuildStep = false;
     QVarLengthArray<OutputFilterData> data(project.SingleProjects.count());
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
-        data[i].filter = project.SingleProjects.at(i).filterByName(filtername);
+        data[i].filter = project.SingleProjects.at(i).filterByName(filterId);
         if (!data[i].filter.Config) // only if the filter is not empty
             continue;
         VCFilter &filter = data[i].filter;
@@ -1869,7 +1873,7 @@ void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, Xml
 
     bool fileAdded = false;
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
-        const VCFilter &filter = project.SingleProjects.at(i).filterByName(filtername);
+        const VCFilter &filter = project.SingleProjects.at(i).filterByName(filterId);
         if (!filter.Config) // only if the filter is not empty
             continue;
         if (outputFileConfig(&data[i], xml, xmlFilter, info.file, fileAdded,

qmake/generators/win32/msbuild_objectmodel.h
@@ -56,7 +56,8 @@ public:
     }
     virtual void addElement(const QString &filepath, const VCFilterFile &allInfo) = 0;
     virtual void removeElements()= 0;
-    virtual void generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName, VCProject &tool, const QString &filter) = 0;
+    virtual void generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName,
+                             VCProject &tool, const QString &filter, const QString &filterId) = 0;
     virtual bool hasElements() = 0;
 };
 
@@ -106,7 +107,8 @@ public:
         children.clear();
     }
 
-    void generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName, VCProject &tool, const QString &filter);
+    void generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName, VCProject &tool,
+                     const QString &filter, const QString &filterId);
     bool hasElements() {
         return children.size() != 0;
     }
@@ -146,7 +148,8 @@ public:
         children.clear();
     }
 
-    void generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName, VCProject &proj, const QString &filter);
+    void generateXML(XmlOutput &xml, XmlOutput &xmlFilter, const QString &tagName, VCProject &proj,
+                     const QString &filter, const QString &filterId);
     bool hasElements() {
         return children.size() != 0;
     }
@@ -180,7 +183,7 @@ private:
 
     static void addFilters(VCProject &project, XmlOutput &xmlFilter, const QString &filterName);
     static void outputFilter(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filtername);
-    static void outputFileConfigs(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter, const VCFilterFile &info, const QString &filtername);
+    static void outputFileConfigs(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter, const VCFilterFile &info, const QString &filtername, const QString &filterId);
     static bool outputFileConfig(OutputFilterData *d, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filename, bool fileAdded, bool hasCustomBuildStep);
     static void outputFileConfig(XmlOutput &xml, XmlOutput &xmlFilter, const QString &fileName, const QString &filterName);
     static QString generateCondition(const VCConfiguration &config);