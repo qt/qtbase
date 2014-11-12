4c1ea8f72ce806a5059ddec1ec1364f9a92294f0
David Faure <david.faure@kdab.com>
2014-11-12 09:56:43 +0100

    QIdentityProxyModel: add missing default value, compared to base class.
    Change-Id: I8e3ff4923834c87a0a8d289e5445346a87542a80
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>

--- src/corelib/itemmodels/qidentityproxymodel.h
+++ src/corelib/itemmodels/qidentityproxymodel.h
@@ -57,7 +57,7 @@ public:
     QModelIndex mapToSource(const QModelIndex& proxyIndex) const;
     QModelIndex parent(const QModelIndex& child) const;
     int rowCount(const QModelIndex& parent = QModelIndex()) const;
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
     bool dropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column, const QModelIndex& parent);
     QModelIndex sibling(int row, int column, const QModelIndex &idx) const;
 
