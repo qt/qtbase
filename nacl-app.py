c3c8aaa785a483d0fd6fdc3fdd6e0c46fb5eb723
Morten Johan Sørvig <morten.sorvig@digia.com>
2014-12-02 13:50:43 +0100

    Add "--dryrun" option.

--- nacl-app.py
+++ nacl-app.py
@@ -10,19 +10,24 @@ import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input', nargs='*')
+parser.add_argument('-d', '--dryrun',   action='store_true',   help='dry run. print commands only. (might create directories)')
 args = parser.parse_args()
+if len(args.input) <= 0:
+    print ("Usage: nacl-app <app-source-dir> <qt-build-dir>")
+    sys.exit(0)
+
 
 appSourceDir = path.abspath(args.input[0])
 qtBuildsDir = path.abspath(args.input[1])
 appProFile = glob.glob(appSourceDir + '/*.pro')[0]
 qtBuildNames = os.walk(qtBuildsDir).next()[1]
+dryrun = args.dryrun
 
 print ''
 print 'app source dir: ' + appSourceDir
 print 'app pro file  : '   + appProFile
 print 'qt builds dir : ' + qtBuildsDir
 print 'qt build names: ' + str(qtBuildNames)
-
 
 #sys.exit(0)
 
@@ -39,8 +44,8 @@ for qtBuildName in qtBuildNames:
     qmake = path.join(qtBuildDir, 'qtbase', 'bin', 'qmake')
     cmd = qmake + ' ' + appProFile + ' -r && make -k'
     print cmd
-    Popen(cmd, shell=True, cwd=qtBuildName)
-    
+    if not dryrun:
+        Popen(cmd, shell=True, cwd=qtBuildName)
 print ''    
     
 
