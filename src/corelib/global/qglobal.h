dc469691a95581553196c77c4b81cb4fb9b29f78
Thiago Macieira <thiago.macieira@intel.com>
2014-12-07 11:01:05 -0800

    Bump version number to 5.4.1
    Change-Id: I54231de422ffc63162e49c587f371c56c3312210
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -37,11 +37,11 @@
 
 #include <stddef.h>
 
-#define QT_VERSION_STR   "5.4.0"
+#define QT_VERSION_STR   "5.4.1"
 /*
    QT_VERSION is (major << 16) + (minor << 8) + patch.
 */
-#define QT_VERSION 0x050400
+#define QT_VERSION 0x050401
 /*
    can be used like #if (QT_VERSION >= QT_VERSION_CHECK(4, 4, 0))
 */
