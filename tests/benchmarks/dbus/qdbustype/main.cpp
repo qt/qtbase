1fc4053337b55c98b66de63b4fae223f1eb72563
Thiago Macieira <thiago.macieira@intel.com>
2014-12-11 17:37:42 -0800

    Allow the QtDBus type benchmark to run without libdbus-1 found
    Change-Id: I1086e6cd6534dc96311e981cc9c73f501c1128ac
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@theqtcompany.com>

--- tests/benchmarks/dbus/qdbustype/main.cpp
+++ tests/benchmarks/dbus/qdbustype/main.cpp
@@ -48,7 +48,8 @@ private Q_SLOTS:
 
 static inline void benchmarkAddRow(const char *name, const char *data)
 {
-    QTest::newRow(QByteArray(QByteArray("native-") + name)) << data << true;
+    if (qdbus_loadLibDBus())
+        QTest::newRow(QByteArray(QByteArray("native-") + name)) << data << true;
     QTest::newRow(name) << data << false;
 }
 
