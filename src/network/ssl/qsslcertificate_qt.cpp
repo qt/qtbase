qt/qtbase

commit f17d7a124f0fa817a7e1a2dda6f48098432c0dc0

Use categorized logging for ssl code
Place all debug output into the 'qt.network.ssl' category.
This allows people to disable certain warnings at runtime.

Task-number: QTBUG-43173
Change-Id: Ide731fae3109f9cd7730cc096ee069a5b99d35f1
Reviewed-by: Richard J. Moore <rich@kde.org>

src/network/ssl/qsslcertificate_qt.cpp
@@ -40,7 +40,7 @@
 ****************************************************************************/
 
 
-
+#include "qssl_p.h"
 #include "qsslcertificate.h"
 #include "qsslcertificate_p.h"
 #include "qsslkey.h"
@@ -83,8 +83,9 @@ bool QSslCertificate::isSelfSigned() const
     if (d->null)
         return false;
 
-    qWarning("QSslCertificate::isSelfSigned: This function does not check, whether the certificate "
-             "is actually signed. It just checks whether issuer and subject are identical");
+    qCWarning(lcSsl,
+              "QSslCertificate::isSelfSigned: This function does not check, whether the certificate "
+              "is actually signed. It just checks whether issuer and subject are identical");
     return d->subjectMatchesIssuer;
 }
 

src/network/ssl/qssl.cpp
@@ -33,8 +33,11 @@
 
 
 #include "qsslkey.h"
+#include "qssl_p.h"
 
 QT_BEGIN_NAMESPACE
+
+Q_LOGGING_CATEGORY(lcSsl, "qt.network.ssl");
 
 /*! \namespace QSsl
 

src/network/ssl/qssl_p.h
@@ -1,0 +1,58 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the QtNetwork module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL21$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia. For licensing terms and
+** conditions see http://qt.digia.com/licensing. For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 or version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE.LGPLv21 and
+** LICENSE.LGPLv3 included in the packaging of this file. Please review the
+** following information to ensure the GNU Lesser General Public License
+** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights. These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+
+#ifndef QSSL_P_H
+#define QSSL_P_H
+
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of qsslcertificate.cpp.  This header file may change from version to version
+// without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtCore/QLoggingCategory>
+
+QT_BEGIN_NAMESPACE
+
+Q_DECLARE_LOGGING_CATEGORY(lcSsl)
+
+QT_END_NAMESPACE
+
+#endif // QSSL_P_H

src/network/ssl/qsslcertificate.cpp
@@ -112,6 +112,7 @@
 #include "qsslsocket_winrt_p.h"
 #endif
 
+#include "qssl_p.h"
 #include "qsslcertificate.h"
 #include "qsslcertificate_p.h"
 #include "qsslkey_p.h"
@@ -524,7 +525,7 @@ QList<QSslCertificate> QSslCertificate::fromPath(const QString &path,
 QList<QSslCertificate> QSslCertificate::fromDevice(QIODevice *device, QSsl::EncodingFormat format)
 {
     if (!device) {
-        qWarning("QSslCertificate::fromDevice: cannot read from a null device");
+        qCWarning(lcSsl, "QSslCertificate::fromDevice: cannot read from a null device");
         return QList<QSslCertificate>();
     }
     return fromData(device->readAll(), format);

src/network/ssl/qsslcertificate_openssl.cpp
@@ -39,6 +39,7 @@
 **
 ****************************************************************************/
 
+#include "qssl_p.h"
 #include "qsslsocket_openssl_symbols_p.h"
 #include "qsslcertificate_p.h"
 #include "qsslkey_p.h"
@@ -303,7 +304,7 @@ static QVariant x509UnknownExtensionToValue(X509_EXTENSION *ext)
         else
             return list;
     } else if (meth->i2s && ext_internal) {
-        //qDebug() << meth->i2s(meth, ext_internal);
+        //qCDebug(lcSsl) << meth->i2s(meth, ext_internal);
         QVariant result(QString::fromUtf8(meth->i2s(meth, ext_internal)));
         return result;
     } else if (meth->i2r && ext_internal) {
@@ -371,7 +372,7 @@ static QVariant x509ExtensionToValue(X509_EXTENSION *ext)
 
                     result[QString::fromUtf8(QSslCertificatePrivate::asn1ObjectName(ad->method))] = uri;
                 } else {
-                    qWarning() << "Strange location type" << name->type;
+                    qCWarning(lcSsl) << "Strange location type" << name->type;
                 }
             }
 
@@ -516,7 +517,7 @@ void QSslCertificatePrivate::init(const QByteArray &data, QSsl::EncodingFormat f
 QByteArray QSslCertificatePrivate::QByteArray_from_X509(X509 *x509, QSsl::EncodingFormat format)
 {
     if (!x509) {
-        qWarning("QSslSocketBackendPrivate::X509_to_QByteArray: null X509");
+        qCWarning(lcSsl, "QSslSocketBackendPrivate::X509_to_QByteArray: null X509");
         return QByteArray();
     }
 
@@ -551,7 +552,7 @@ QByteArray QSslCertificatePrivate::QByteArray_from_X509(X509 *x509, QSsl::Encodi
 QString QSslCertificatePrivate::text_from_X509(X509 *x509)
 {
     if (!x509) {
-        qWarning("QSslSocketBackendPrivate::text_from_X509: null X509");
+        qCWarning(lcSsl, "QSslSocketBackendPrivate::text_from_X509: null X509");
         return QString();
     }
 

src/network/ssl/qsslconfiguration.cpp
@@ -32,6 +32,7 @@
 **
 ****************************************************************************/
 
+#include "qssl_p.h"
 #include "qsslconfiguration.h"
 #include "qsslconfiguration_p.h"
 #include "qsslsocket.h"
@@ -341,7 +342,8 @@ int QSslConfiguration::peerVerifyDepth() const
 void QSslConfiguration::setPeerVerifyDepth(int depth)
 {
     if (depth < 0) {
-        qWarning("QSslConfiguration::setPeerVerifyDepth: cannot set negative depth of %d", depth);
+        qCWarning(lcSsl,
+                 "QSslConfiguration::setPeerVerifyDepth: cannot set negative depth of %d", depth);
         return;
     }
     d->peerVerifyDepth = depth;

src/network/ssl/qsslcontext_openssl.cpp
@@ -36,6 +36,7 @@
 #include <QtNetwork/qsslsocket.h>
 #include <QtCore/qmutex.h>
 
+#include "private/qssl_p.h"
 #include "private/qsslcontext_openssl_p.h"
 #include "private/qsslsocket_p.h"
 #include "private/qsslsocket_openssl_p.h"
@@ -357,7 +358,7 @@ static int next_proto_cb(SSL *, unsigned char **out, unsigned char *outlen,
         ctx->status = QSslConfiguration::NextProtocolNegotiationUnsupported;
         break;
     default:
-        qWarning("OpenSSL sent unknown NPN status");
+        qCWarning(lcSsl, "OpenSSL sent unknown NPN status");
     }
 
     return SSL_TLSEXT_ERR_OK;
@@ -384,7 +385,7 @@ SSL* QSslContext::createSsl()
     if (session) {
         // Try to resume the last session we cached
         if (!q_SSL_set_session(ssl, session)) {
-            qWarning("could not set SSL session");
+            qCWarning(lcSsl, "could not set SSL session");
             q_SSL_SESSION_free(session);
             session = 0;
         }
@@ -396,8 +397,8 @@ SSL* QSslContext::createSsl()
         m_supportedNPNVersions.clear();
         for (int a = 0; a < protocols.count(); ++a) {
             if (protocols.at(a).size() > 255) {
-                qWarning() << "TLS NPN extension" << protocols.at(a)
-                           << "is too long and will be truncated to 255 characters.";
+                qCWarning(lcSsl) << "TLS NPN extension" << protocols.at(a)
+                                 << "is too long and will be truncated to 255 characters.";
                 protocols[a] = protocols.at(a).left(255);
             }
             m_supportedNPNVersions.append(protocols.at(a).size()).append(protocols.at(a));
@@ -433,7 +434,7 @@ bool QSslContext::cacheSession(SSL* ssl)
             m_sessionASN1.resize(sessionSize);
             unsigned char *data = reinterpret_cast<unsigned char *>(m_sessionASN1.data());
             if (!q_i2d_SSL_SESSION(session, &data))
-                qWarning("could not store persistent version of SSL session");
+                qCWarning(lcSsl, "could not store persistent version of SSL session");
             m_sessionTicketLifeTimeHint = session->tlsext_tick_lifetime_hint;
         }
     }

src/network/ssl/qsslkey_winrt.cpp
@@ -39,6 +39,7 @@
 **
 ****************************************************************************/
 
+#include "qssl_p.h"
 #include "qsslkey.h"
 #include "qsslkey_p.h"
 #include "qsslcertificate_p.h"
@@ -144,7 +145,7 @@ static QByteArray doCrypt(QSslKeyPrivate::Cipher cipher, QByteArray data, const 
         if (padding > 0 && padding <= blockLength)
             resultLength -= padding;
         else
-            qWarning("Invalid padding length of %u; decryption likely failed.", padding);
+            qCWarning(lcSsl, "Invalid padding length of %u; decryption likely failed.", padding);
     }
 
     return QByteArray(reinterpret_cast<const char *>(resultData), resultLength);

commit 7f34bee8493bce5b690f864a1c5da4493ce8e409

Fix include headers for qsslcertificate_qt.cpp
The order is:
 1) own header
 2) own private header, if any
 3) other headers

Commit f17d7a124f0fa817a7e1a2dda6f48098432c0dc0 broke the order.

Change-Id: I7225024691db91fd936a057accdad65bacb3f979
Reviewed-by: Richard J. Moore <rich@kde.org>

src/network/ssl/qsslcertificate_qt.cpp
@@ -39,10 +39,10 @@
 **
 ****************************************************************************/
 
-
-#include "qssl_p.h"
 #include "qsslcertificate.h"
 #include "qsslcertificate_p.h"
+
+#include "qssl_p.h"
 #include "qsslkey.h"
 #include "qsslkey_p.h"
 #include "qsslcertificateextension.h"