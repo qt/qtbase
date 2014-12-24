51ba25e0fc89882e5021bed718de91d58f7f3907
Fatih Aşıcı <fatih.asici@gmail.com>
2014-12-24 17:29:11 +0200

    logging: Check if uClibc has backtrace support
    execinfo.h is optional in uClibc. We need to check
    __UCLIBC_HAS_BACKTRACE__ if uClibc is used.
    
    Change-Id: Ie28be85b0b70472df1fc4a208581bb66ad34229e
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/global/qlogging.cpp
+++ src/corelib/global/qlogging.cpp
@@ -77,12 +77,19 @@
 #endif
 
 #if !defined QT_NO_REGULAREXPRESSION && !defined(QT_BOOTSTRAPPED)
-#  if (defined(__GLIBC__) && defined(__GLIBCXX__)) || (__has_include(<cxxabi.h>) && __has_include(<execinfo.h>))
+#  ifdef __UCLIBC__
+#    if __UCLIBC_HAS_BACKTRACE__
+#      define QLOGGING_HAVE_BACKTRACE
+#    endif
+#  elif (defined(__GLIBC__) && defined(__GLIBCXX__)) || (__has_include(<cxxabi.h>) && __has_include(<execinfo.h>))
 #    define QLOGGING_HAVE_BACKTRACE
-#    include <qregularexpression.h>
-#    include <cxxabi.h>
-#    include <execinfo.h>
 #  endif
+#endif
+
+#ifdef QLOGGING_HAVE_BACKTRACE
+#  include <qregularexpression.h>
+#  include <cxxabi.h>
+#  include <execinfo.h>
 #endif
 
 #include <stdio.h>
