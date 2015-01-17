qt/qtbase

commit bec1854cc023fb705319c582a636d5f484adafcc

QSortFilterProxyModel: keep the sorting on unrelated changes
Whenever the source model of a QSortFilterProxyModel changes, and
the changes involve the sorted column, the implementation removes
the changed rows from the mapping, sorts them, and inserts them back;
in case of identical items, the rows are inserted at the end of the
block of equal rows.

The problem is that if the change doesn't actually happen on the roles
that are used for sorting, then we shuffle the rows, terribly confusing
the user. The typical case is a model with identical checkable rows:
(un)checking one row will move it at the end.

So, instead of trying to be smart with the removal/sort/insert sorted,
simply resort everything under the changed parent index. Since the
sorting used is stable, this keeps the items in the same positions.

Task-number: QTBUG-1548
Change-Id: Id0e61bd49da53b0a3e8aefa6b6893ac41179dc6f
Reviewed-by: David Faure <david.faure@kdab.com>

src/corelib/itemmodels/qsortfilterproxymodel.cpp
@@ -1196,11 +1196,7 @@ void QSortFilterProxyModelPrivate::_q_sourceDataChanged(const QModelIndex &sourc
         parents << q->mapFromSource(source_parent);
         emit q->layoutAboutToBeChanged(parents, QAbstractItemModel::VerticalSortHint);
         QModelIndexPairList source_indexes = store_persistent_indexes();
-        remove_source_items(m->proxy_rows, m->source_rows, source_rows_resort,
-                            source_parent, Qt::Vertical, false);
-        sort_source_rows(source_rows_resort, source_parent);
-        insert_source_items(m->proxy_rows, m->source_rows, source_rows_resort,
-                            source_parent, Qt::Vertical, false);
+        sort_source_rows(m->source_rows, source_parent);
         update_persistent_indexes(source_indexes);
         emit q->layoutChanged(parents, QAbstractItemModel::VerticalSortHint);
         // Make sure we also emit dataChanged for the rows

tests/auto/corelib/itemmodels/qsortfilterproxymodel/tst_qsortfilterproxymodel.cpp
@@ -95,6 +95,7 @@ private slots:
     void changeFilter();
     void changeSourceData_data();
     void changeSourceData();
+    void changeSourceDataKeepsStableSorting_qtbug1548();
     void sortFilterRole();
     void selectionFilteredOut();
     void match_data();
@@ -2007,6 +2008,79 @@ void tst_QSortFilterProxyModel::changeSourceData()
         QModelIndex index = proxy.index(i, 0, QModelIndex());
         QCOMPARE(proxy.data(index, Qt::DisplayRole).toString(), proxyItems.at(i));
     }
+}
+
+// Checks that the model is a table, and that each and every row is like this:
+// i-th row:   ( rows.at(i), i )
+static void checkSortedTableModel(const QAbstractItemModel *model, const QStringList &rows)
+{
+    QCOMPARE(model->rowCount(), rows.length());
+    QCOMPARE(model->columnCount(), 2);
+
+    for (int row = 0; row < model->rowCount(); ++row) {
+        const QString column0 = model->index(row, 0).data().toString();
+        const int column1 = model->index(row, 1).data().toString().toInt();
+
+        QCOMPARE(column0, rows.at(row));
+        QCOMPARE(column1, row);
+    }
+}
+
+void tst_QSortFilterProxyModel::changeSourceDataKeepsStableSorting_qtbug1548()
+{
+    // Check that emitting dataChanged from the source model
+    // for a change of a role which is not the sorting role
+    // doesn't alter the sorting. In this case, we sort on the DisplayRole,
+    // and play with other roles.
+
+    static const QStringList rows
+            = QStringList() << "a" << "b" << "b" << "b" << "c" << "c" << "x";
+
+    // Build a table of pairs (string, #row) in each row
+    QStandardItemModel model(0, 2);
+
+    for (int rowNumber = 0; rowNumber < rows.length(); ++rowNumber) {
+        QStandardItem *column0 = new QStandardItem(rows.at(rowNumber));
+        column0->setCheckable(true);
+        column0->setCheckState(Qt::Unchecked);
+
+        QStandardItem *column1 = new QStandardItem(QString::number(rowNumber));
+
+        const QList<QStandardItem *> row
+                = QList<QStandardItem *>() << column0 << column1;
+
+        model.appendRow(row);
+    }
+
+    checkSortedTableModel(&model, rows);
+
+    // Build the proxy model
+    QSortFilterProxyModel proxy;
+    proxy.setSourceModel(&model);
+    proxy.setDynamicSortFilter(true);
+    proxy.sort(0);
+
+    // The proxy is now sorted by the first column, check that the sorting
+    // * is correct (the input is already sorted, so it must not have changed)
+    // * was stable (by looking at the second column)
+    checkSortedTableModel(&model, rows);
+
+    // Change the check status of an item. That must not break the stable sorting
+    // changes the middle "b"
+    model.item(2)->setCheckState(Qt::Checked);
+    checkSortedTableModel(&model, rows);
+
+    // changes the starting "a"
+    model.item(0)->setCheckState(Qt::Checked);
+    checkSortedTableModel(&model, rows);
+
+    // change the background color of the first "c"
+    model.item(4)->setBackground(Qt::red);
+    checkSortedTableModel(&model, rows);
+
+    // change the background color of the second "c"
+    model.item(5)->setBackground(Qt::red);
+    checkSortedTableModel(&model, rows);
 }
 
 void tst_QSortFilterProxyModel::sortFilterRole()

commit e9ad46ed412987e3e46c5a641e5f30408b97ac90

QSortFilterProxyModel: fix a regression
bec1854cc023fb705319c582a636d5f484adafcc introduced a regression:
when sorting a tree model, children items would not follow the sorted
parents any more (they wouldn't be remapped correctly), resulting
in crashes.

So, the fix needs more reasoning; let's revert the fix,
but leave  the original test around for any subsequent attempt, and
introduce a new test which looks for the right behavior when
sorting trees.

This commit partially reverts bec1854cc023fb705319c582a636d5f484adafcc.

Task-number: QTBUG-43827
Change-Id: Ic83ac53aef639f870f6c36a8b4b2992f5b485b13
Reviewed-by: David Faure <david.faure@kdab.com>
Reviewed-by: Volker Krause <volker.krause@kdab.com>

src/corelib/itemmodels/qsortfilterproxymodel.cpp
@@ -1198,7 +1198,11 @@ void QSortFilterProxyModelPrivate::_q_sourceDataChanged(const QModelIndex &sourc
         parents << q->mapFromSource(source_parent);
         emit q->layoutAboutToBeChanged(parents, QAbstractItemModel::VerticalSortHint);
         QModelIndexPairList source_indexes = store_persistent_indexes();
-        sort_source_rows(m->source_rows, source_parent);
+        remove_source_items(m->proxy_rows, m->source_rows, source_rows_resort,
+                            source_parent, Qt::Vertical, false);
+        sort_source_rows(source_rows_resort, source_parent);
+        insert_source_items(m->proxy_rows, m->source_rows, source_rows_resort,
+                            source_parent, Qt::Vertical, false);
         update_persistent_indexes(source_indexes);
         emit q->layoutChanged(parents, QAbstractItemModel::VerticalSortHint);
         // Make sure we also emit dataChanged for the rows

tests/auto/corelib/itemmodels/qsortfilterproxymodel/tst_qsortfilterproxymodel.cpp
@@ -97,6 +97,7 @@ private slots:
     void changeSourceData();
     void changeSourceDataKeepsStableSorting_qtbug1548();
     void changeSourceDataForwardsRoles_qtbug35440();
+    void resortingDoesNotBreakTreeModels();
     void sortFilterRole();
     void selectionFilteredOut();
     void match_data();
@@ -2030,6 +2031,8 @@ static void checkSortedTableModel(const QAbstractItemModel *model, const QString
 
 void tst_QSortFilterProxyModel::changeSourceDataKeepsStableSorting_qtbug1548()
 {
+    QSKIP("This test will fail, see QTBUG-1548");
+
     // Check that emitting dataChanged from the source model
     // for a change of a role which is not the sorting role
     // doesn't alter the sorting. In this case, we sort on the DisplayRole,
@@ -4058,6 +4061,40 @@ void tst_QSortFilterProxyModel::canDropMimeData()
     // i.e. accept drops on the first 5 rows and refuse drops on the next 5.
     for (int row = 0; row < proxy.rowCount(); ++row)
         QCOMPARE(proxy.canDropMimeData(0, Qt::CopyAction, -1, -1, proxy.index(row, 0)), row < 5);
+}
+
+void tst_QSortFilterProxyModel::resortingDoesNotBreakTreeModels()
+{
+    QStandardItemModel *treeModel = new QStandardItemModel(this);
+    QStandardItem *e1 = new QStandardItem("Loading...");
+    e1->appendRow(new QStandardItem("entry10"));
+    treeModel->appendRow(e1);
+    QStandardItem *e0 = new QStandardItem("Loading...");
+    e0->appendRow(new QStandardItem("entry00"));
+    e0->appendRow(new QStandardItem("entry01"));
+    treeModel->appendRow(e0);
+
+    QSortFilterProxyModel proxy;
+    proxy.setDynamicSortFilter(true);
+    proxy.sort(0);
+    proxy.setSourceModel(treeModel);
+
+    ModelTest modelTest(&proxy);
+
+    QCOMPARE(proxy.rowCount(), 2);
+    e1->setText("entry1");
+    e0->setText("entry0");
+
+    QModelIndex pi0 = proxy.index(0, 0);
+    QCOMPARE(pi0.data().toString(), QString("entry0"));
+    QCOMPARE(proxy.rowCount(pi0), 2);
+
+    QModelIndex pi01 = proxy.index(1, 0, pi0);
+    QCOMPARE(pi01.data().toString(), QString("entry01"));
+
+    QModelIndex pi1 = proxy.index(1, 0);
+    QCOMPARE(pi1.data().toString(), QString("entry1"));
+    QCOMPARE(proxy.rowCount(pi1), 1);
 }
 
 QTEST_MAIN(tst_QSortFilterProxyModel)