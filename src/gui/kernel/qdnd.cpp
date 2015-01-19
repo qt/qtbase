qt/qtbase

commit e4becdc3d310a0dd1a6d34d0796a52b21dedeb2d

Move deletion of the QDrag object into QDragManager::drag().
The QDrag objects were apparently leaking on all platforms
other than XCB.

Task-number: QTBUG-39651
Change-Id: I09efcd250c1f42eb385e9d5779be7af6b9b59376
Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
Reviewed-by: Tim Jenssen <tim.jenssen@digia.com>

src/gui/kernel/qdnd.cpp
@@ -142,6 +142,7 @@ Qt::DropAction QDragManager::drag(QDrag *o)
     QGuiApplicationPrivate::instance()->notifyDragStarted(o);
     const Qt::DropAction result = m_platformDrag->drag(m_object);
     m_object = 0;
+    o->deleteLater();
     return result;
 }
 

src/plugins/platforms/xcb/qxcbdrag.cpp
@@ -1010,9 +1010,6 @@ void QXcbDrag::handleFinished(const xcb_client_message_event_t *event)
 //            current_target = 0;
 //            current_proxy_target = 0;
 
-            if (t.drag)
-                t.drag->deleteLater();
-
 //            current_target = target;
 //            current_proxy_target = proxy_target;
 //            current_embedding_widget = embedding_widget;

commit 3c21c4581dbd957d9f660dd52d0298ecef1001cb

Fix drag and drop regression
Fix regression introduced by e4becdc3d310a0dd1a6d34d0796a52b21dedeb2d

Add QPlatformDrag::ownsDragObject() function, QDragManager can use the
return value of this function to decide if it should take care of deleting
QDrag object or platform plugin will take care of deleting QDrag.

XCB platform plugins uses async dnd data delivery mechanism. It allows
user to drop something and then continue working with the assurance that
the target will get the data regardless of how slow the network connections
are, which means that a source window should preserve QDrag data until
dnd has finished.

Change-Id: I1fbad7380cddec98b756698993dd397409833150
Task-number: QTBUG-43436
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>

src/gui/kernel/qdnd.cpp
@@ -134,7 +134,8 @@ Qt::DropAction QDragManager::drag(QDrag *o)
     QGuiApplicationPrivate::instance()->notifyDragStarted(o);
     const Qt::DropAction result = m_platformDrag->drag(m_object);
     m_object = 0;
-    o->deleteLater();
+    if (!m_platformDrag->ownsDragObject())
+        o->deleteLater();
     return result;
 }
 

src/gui/kernel/qplatformdrag.cpp
@@ -241,6 +241,18 @@ QPixmap QPlatformDrag::defaultPixmap()
     return *qt_drag_default_pixmap();
 }
 
+/*!
+    \since 5.4
+    \brief Returns bool indicating whether QPlatformDrag takes ownership
+    and therefore responsibility of deleting the QDrag object passed in
+    from QPlatformDrag::drag. This can be useful on platforms where QDrag
+    object has to be kept around.
+ */
+bool QPlatformDrag::ownsDragObject() const
+{
+    return false;
+}
+
 #endif // QT_NO_DRAGANDDROP
 
 QT_END_NAMESPACE

src/gui/kernel/qplatformdrag.h
@@ -98,6 +98,8 @@ public:
 
     static QPixmap defaultPixmap();
 
+    virtual bool ownsDragObject() const;
+
 private:
     QPlatformDragPrivate *d_ptr;
 

src/plugins/platforms/xcb/qxcbdrag.cpp
@@ -999,6 +999,8 @@ void QXcbDrag::handleFinished(const xcb_client_message_event_t *event)
         if (at != -1) {
 
             Transaction t = transactions.takeAt(at);
+            if (t.drag)
+                t.drag->deleteLater();
 //            QDragManager *manager = QDragManager::self();
 
 //            Window target = current_target;
@@ -1184,6 +1186,11 @@ bool QXcbDrag::dndEnable(QXcbWindow *w, bool on)
         }
         return true;
     }
+}
+
+bool QXcbDrag::ownsDragObject() const
+{
+    return true;
 }
 
 QXcbDropData::QXcbDropData(QXcbDrag *d)

src/plugins/platforms/xcb/qxcbdrag.h
@@ -70,12 +70,11 @@ public:
 
     virtual QMimeData *platformDropData();
 
-
-    void startDrag();
-    void cancel();
-    void move(const QMouseEvent *me);
-    void drop(const QMouseEvent *me);
-    void endDrag();
+    void startDrag() Q_DECL_OVERRIDE;
+    void cancel() Q_DECL_OVERRIDE;
+    void move(const QMouseEvent *me) Q_DECL_OVERRIDE;
+    void drop(const QMouseEvent *me) Q_DECL_OVERRIDE;
+    void endDrag() Q_DECL_OVERRIDE;
 
     void handleEnter(QWindow *window, const xcb_client_message_event_t *event);
     void handlePosition(QWindow *w, const xcb_client_message_event_t *event);
@@ -87,6 +86,7 @@ public:
     void handleFinished(const xcb_client_message_event_t *event);
 
     bool dndEnable(QXcbWindow *win, bool on);
+    bool ownsDragObject() const Q_DECL_OVERRIDE;
 
     void updatePixmap();
     xcb_timestamp_t targetTime() { return target_time; }