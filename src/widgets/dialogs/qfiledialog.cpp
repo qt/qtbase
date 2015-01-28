4ad83407ddc4d5838b77e645f4b421a356e844e1
Richard Moe Gustavsen <richard.gustavsen@theqtcompany.com>
2015-01-28 14:25:27 +0100

    QFileDialog: don't warn about directory url prematurly
    The app can set a directory url before showing the dialog.
    Since we don't really know if the dialog is going to
    be native or not at that point, it is too early to warn.
    
    Change-Id: Ie9875ca90a500258841bad702ee864a46775641c
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>

--- src/widgets/dialogs/qfiledialog.cpp
+++ src/widgets/dialogs/qfiledialog.cpp
@@ -991,7 +991,7 @@ void QFileDialog::setDirectoryUrl(const QUrl &directory)
         d->setDirectory_sys(directory);
     else if (directory.isLocalFile())
         setDirectory(directory.toLocalFile());
-    else
+    else if (d->usingWidgets())
         qWarning() << "Non-native QFileDialog supports only local files";
 }
 
