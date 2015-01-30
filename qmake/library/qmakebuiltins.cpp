ab74cf09a8f71aa939ac3de6165f9d6b45465ef8
Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
2015-01-30 19:18:54 +0100

    move some functions into ProStringList for saner OO design
    Change-Id: I60f00f38f459fc9f8eebee9187158e4198fc9546
    Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

--- qmake/library/qmakebuiltins.cpp
+++ qmake/library/qmakebuiltins.cpp
@@ -1687,7 +1687,7 @@ QMakeEvaluator::VisitReturn QMakeEvaluator::evaluateBuiltinConditional(
                         if (mode == CacheAdd)
                             newval += diffval;
                         else
-                            removeEach(&newval, diffval);
+                            newval.removeEach(diffval);
                     }
                     if (oldval != newval) {
                         if (target != TargetStash || !m_stashfile.isEmpty()) {
