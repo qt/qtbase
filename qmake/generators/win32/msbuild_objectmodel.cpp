qt/qtbase

commit 8ee2e497ae9722167a33711843e1aa6a894fab22

simplify VCXProjectWriter::outputFileConfig a bit
We don't need the filtername parameter.

Change-Id: I653db4a200c83d095520b47e1451dfe59b956d92
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

qmake/generators/win32/msbuild_objectmodel.cpp
@@ -1850,7 +1850,7 @@ void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, Xml
     for (int i = 0; i < project.SingleProjects.count(); ++i) {
         VCFilter filter = project.SingleProjects.at(i).filterByName(filtername);
         if (filter.Config) // only if the filter is not empty
-            if (outputFileConfig(filter, xml, xmlFilter, info.file, filtername, fileAdded)) // only add it once.
+            if (outputFileConfig(filter, xml, xmlFilter, info.file, fileAdded)) // only add it once.
                 fileAdded = true;
     }
 
@@ -1861,7 +1861,7 @@ void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, Xml
     xmlFilter << closetag();
 }
 
-bool VCXProjectWriter::outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filename, const QString &filtername, bool fileAllreadyAdded)
+bool VCXProjectWriter::outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filename, bool fileAllreadyAdded)
 {
     bool fileAdded = false;
 
@@ -1902,7 +1902,7 @@ bool VCXProjectWriter::outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOut
 
     // Actual XML output ----------------------------------
     if (filter.useCustomBuildTool || filter.useCompilerTool
-            || !inBuild || filtername.startsWith("Deployment Files")) {
+            || !inBuild || filter.Name.startsWith("Deployment Files")) {
 
         if (filter.useCustomBuildTool)
         {
@@ -1912,13 +1912,15 @@ bool VCXProjectWriter::outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOut
 
                 xmlFilter << tag("CustomBuild")
                     << attrTag("Include",Option::fixPathToLocalOS(filename))
-                    << attrTagS("Filter", filtername);
+                    << attrTagS("Filter", filter.Name);
 
                 xml << tag("CustomBuild")
                     << attrTag("Include",Option::fixPathToLocalOS(filename));
 
-                if (filtername.startsWith("Form Files") || filtername.startsWith("Generated Files")
-                        || filtername.startsWith("Resource Files") || filtername.startsWith("Deployment Files"))
+                if (filter.Name.startsWith("Form Files")
+                        || filter.Name.startsWith("Generated Files")
+                        || filter.Name.startsWith("Resource Files")
+                        || filter.Name.startsWith("Deployment Files"))
                     xml << attrTagS("FileType", "Document");
             }
 
@@ -1928,7 +1930,7 @@ bool VCXProjectWriter::outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOut
         if ( !fileAdded && !fileAllreadyAdded )
         {
             fileAdded = true;
-            outputFileConfig(xml, xmlFilter, filename, filtername);
+            outputFileConfig(xml, xmlFilter, filename, filter.Name);
         }
 
         const QString condition = generateCondition(*filter.Config);
@@ -1938,7 +1940,7 @@ bool VCXProjectWriter::outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOut
                 << valueTag("true");
         }
 
-        if (filtername.startsWith("Deployment Files") && inBuild) {
+        if (filter.Name.startsWith("Deployment Files") && inBuild) {
             xml << tag("DeploymentContent")
                 << attrTag("Condition", condition)
                 << valueTag("true");

qmake/generators/win32/msbuild_objectmodel.h
@@ -174,7 +174,7 @@ private:
     static void addFilters(VCProject &project, XmlOutput &xmlFilter, const QString &filterName);
     static void outputFilter(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filtername);
     static void outputFileConfigs(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter, const VCFilterFile &info, const QString &filtername);
-    static bool outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filename, const QString &filtername, bool fileAllreadyAdded);
+    static bool outputFileConfig(VCFilter &filter, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filename, bool fileAllreadyAdded);
     static void outputFileConfig(XmlOutput &xml, XmlOutput &xmlFilter, const QString &fileName, const QString &filterName);
     static QString generateCondition(const VCConfiguration &config);
 

commit f90c1860580bcbe6b898ceb86cdd746532470c64

fix vcxproj generation for CONFIG-=flat, part II
Commit 8ee2e497 introduced a regression for CONFIG-=flat vcxproj files.
Files with custom build steps (e.g. foo.h with Q_OBJECT) were written
into top-level filters ("Header Files" instead of
"Header Files\my\sub\dir").
The assumption that the parameter filtername always equals
VCFilter::name was wrong.

Change-Id: Id5178550310d06b73e42f18597a27012ddd89bb7
Task-number: QTBUG-44413
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

qmake/generators/win32/msbuild_objectmodel.cpp
@@ -1898,8 +1898,10 @@ void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, Xml
         OutputFilterData *d = &data[i];
         if (!d->filter.Config) // only if the filter is not empty
             continue;
-        if (outputFileConfig(d, xml, xmlFilter, info.file, fileAdded, hasCustomBuildStep))
+        if (outputFileConfig(d, xml, xmlFilter, info.file, filtername, fileAdded,
+                             hasCustomBuildStep)) {
             fileAdded = true;
+        }
     }
 
     if ( !fileAdded )
@@ -1910,8 +1912,8 @@ void VCXProjectWriter::outputFileConfigs(VCProject &project, XmlOutput &xml, Xml
 }
 
 bool VCXProjectWriter::outputFileConfig(OutputFilterData *d, XmlOutput &xml, XmlOutput &xmlFilter,
-                                        const QString &filename, bool fileAdded,
-                                        bool hasCustomBuildStep)
+                                        const QString &filename, const QString &fullFilterName,
+                                        bool fileAdded, bool hasCustomBuildStep)
 {
     VCFilter &filter = d->filter;
     if (d->inBuild) {
@@ -1934,7 +1936,7 @@ bool VCXProjectWriter::outputFileConfig(OutputFilterData *d, XmlOutput &xml, Xml
 
                 xmlFilter << tag("CustomBuild")
                     << attrTag("Include",Option::fixPathToLocalOS(filename))
-                    << attrTagS("Filter", filter.Name);
+                    << attrTagS("Filter", fullFilterName);
 
                 xml << tag("CustomBuild")
                     << attrTag("Include",Option::fixPathToLocalOS(filename));
@@ -1952,7 +1954,7 @@ bool VCXProjectWriter::outputFileConfig(OutputFilterData *d, XmlOutput &xml, Xml
         if (!fileAdded)
         {
             fileAdded = true;
-            outputFileConfig(xml, xmlFilter, filename, filter.Name);
+            outputFileConfig(xml, xmlFilter, filename, fullFilterName);
         }
 
         const QString condition = generateCondition(*filter.Config);

qmake/generators/win32/msbuild_objectmodel.h
@@ -185,7 +185,9 @@ private:
     static void outputFilter(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filtername);
     static void outputFileConfigs(VCProject &project, XmlOutput &xml, XmlOutput &xmlFilter,
                                   const VCFilterFile &info, const QString &filtername);
-    static bool outputFileConfig(OutputFilterData *d, XmlOutput &xml, XmlOutput &xmlFilter, const QString &filename, bool fileAdded, bool hasCustomBuildStep);
+    static bool outputFileConfig(OutputFilterData *d, XmlOutput &xml, XmlOutput &xmlFilter,
+                                 const QString &filename, const QString &fullFilterName,
+                                 bool fileAdded, bool hasCustomBuildStep);
     static void outputFileConfig(XmlOutput &xml, XmlOutput &xmlFilter, const QString &fileName, const QString &filterName);
     static QString generateCondition(const VCConfiguration &config);
 