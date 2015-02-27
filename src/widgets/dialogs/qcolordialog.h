381fbbf748fd32d1bc210acefff07116f140f5e4
Marc Mutz <marc.mutz@kdab.com>
2015-02-27 21:10:00 +0100

    QColorDialog: remove unused friendship on QColorShower
    This is preparation of moving all the helper classes into the unnamed
    namespace.
    
    Change-Id: I255eded0a8c9ff864889d0d23c022012d5fc1eab
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>

--- src/widgets/dialogs/qcolordialog.h
+++ src/widgets/dialogs/qcolordialog.h
@@ -113,7 +113,6 @@ private:
     Q_PRIVATE_SLOT(d_func(), void _q_newStandard(int, int))
     Q_PRIVATE_SLOT(d_func(), void _q_pickScreenColor())
     Q_PRIVATE_SLOT(d_func(), void _q_updateColorPicking())
-    friend class QColorShower;
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(QColorDialog::ColorDialogOptions)
