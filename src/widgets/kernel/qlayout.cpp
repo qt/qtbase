d59638f21d7ea7c2a7f5c57b3eca27da984a1917
Jan Arve Saether <jan-arve.saether@theqtcompany.com>
2015-03-13 14:49:51 +0100

    Fixed bug in QLayout::replaceWidget()
    If the \a to widget was already part of a layout it would be removed from
    the layout. This happened because the function that was supposed to
    perform this removal was called after we had done the replacement.
    QLayout::addChildWidget() should therefore be called first.  This is also
    documented in QLayout::addChildWidget()
    
    Change-Id: Ie718935a14ebad81827fad962920e930263c05b8
    Task-number: QTBUG-37724
    Reviewed-by: Thorbjørn Lund Martsum <tmartsum@gmail.com>

--- src/widgets/kernel/qlayout.cpp
+++ src/widgets/kernel/qlayout.cpp
@@ -1175,13 +1175,12 @@ QLayoutItem *QLayout::replaceWidget(QWidget *from, QWidget *to, Qt::FindChildOpt
     if (index == -1)
         return 0;
 
+    addChildWidget(to);
     QLayoutItem *newitem = new QWidgetItem(to);
     newitem->setAlignment(item->alignment());
     QLayoutItem *r = d->replaceAt(index, newitem);
     if (!r)
         delete newitem;
-    else
-        addChildWidget(to);
     return r;
 }
 
