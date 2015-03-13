qt/qtbase

commit de5553aad52f8a8343d53bac5e9d29bcd67fbf2c

qmake: Resolve QMAKE_INFO_PLIST against current working dir
We're not in the output directory yet, so resolving using vanilla
fileFixify will end up failing when we check if the file exists, since
QFileInfo resolves relative paths against the current directory.

Change-Id: I414c6a2e83b49e3fb30e6153a49f7a90a8e528a0
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

qmake/generators/unix/unixmake2.cpp
@@ -1218,7 +1218,7 @@ void UnixMakefileGenerator::init2()
     }
 
     if(!project->isEmpty("QMAKE_BUNDLE")) {
-        QString plist = fileFixify(project->first("QMAKE_INFO_PLIST").toQString());
+        QString plist = fileFixify(project->first("QMAKE_INFO_PLIST").toQString(), qmake_getpwd());
         if(plist.isEmpty())
             plist = specdir() + QDir::separator() + "Info.plist." + project->first("TEMPLATE");
         if(exists(Option::fixPathToLocalOS(plist))) {

commit 3f8e3373e1b7fd34d4223b8853587273ae6beb34

Fix QMAKE_INFO_PLIST regression with relative paths
In de5553aa, qmake was fixed to resolve QMAKE_INFO_PLIST based on the
current pwd to fix QTBUG-21267. This fix was lost as part of 8c138054
in 5.4.

This fixes the error:

"WARNING: Could not resolve Info.plist ..."

when using qmake for shadow builds on OS X.

[ChangeLog][qmake][OS X/iOS] Fixed QMAKE_INFO_PLIST path resolution
for shadow builds

Change-Id: Icb42b2b3a44856f9b9a86a008081a3353951640d
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

qmake/generators/unix/unixmake2.cpp
@@ -779,14 +779,17 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
         }
         //copy the plist
         while (!project->isActiveConfig("no_plist")) {  // 'while' just to be able to 'break'
-            QString info_plist = escapeFilePath(fileFixify(project->first("QMAKE_INFO_PLIST").toQString()));
-            if (info_plist.isEmpty())
-                info_plist = specdir() + QDir::separator() + "Info.plist." + project->first("TEMPLATE");
-            if (!exists(Option::fixPathToLocalOS(info_plist))) {
+            QString info_plist = project->first("QMAKE_INFO_PLIST").toQString();
+            if (info_plist.isEmpty()) {
+                info_plist = escapeFilePath(specdir() + QDir::separator() + "Info.plist." + project->first("TEMPLATE"));
+            } else if (!exists(Option::fixPathToLocalOS(info_plist))) {
                 warn_msg(WarnLogic, "Could not resolve Info.plist: '%s'. Check if QMAKE_INFO_PLIST points to a valid file.",
                          info_plist.toLatin1().constData());
                 break;
+            } else {
+                info_plist = escapeFilePath(fileFixify(info_plist));
             }
+
             bool isApp = (project->first("TEMPLATE") == "app");
             QString info_plist_out = escapeFilePath(
                     bundle_dir + (isApp ? "Contents/Info.plist"