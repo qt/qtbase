ddf6ac03cb40c4a299f14b7f0b0c9257587ee828
Pasi Petäjäjärvi <pasi.petajajarvi@theqtcompany.com>
2015-03-20 17:09:53 +0200

    Do not search test binary if QProcess is not supported.
    Without QProcess support we do not run test which initTestCase is
    trying to find.
    
    Change-Id: Icecaad90f61a019489e372517a6d62372155bd46
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- tests/auto/corelib/thread/qthreadstorage/tst_qthreadstorage.cpp
+++ tests/auto/corelib/thread/qthreadstorage/tst_qthreadstorage.cpp
@@ -83,6 +83,7 @@ int Pointer::count = 0;
 
 void tst_QThreadStorage::initTestCase()
 {
+#ifndef QT_NO_PROCESS
     const QString crashOnExitDir = QFINDTESTDATA("crashonexit");
     QVERIFY2(!crashOnExitDir.isEmpty(),
              qPrintable(QString::fromLatin1("Could not find 'crashonexit' starting from '%1'")
@@ -93,6 +94,7 @@ void tst_QThreadStorage::initTestCase()
 #endif
     QVERIFY2(QFileInfo(m_crashOnExit).isExecutable(),
              qPrintable(QDir::toNativeSeparators(m_crashOnExit) + QStringLiteral(" does not exist or is not executable.")));
+#endif
 }
 
 void tst_QThreadStorage::hasLocalData()
