1bd07b029396ccfebebefe92ec5e1b6841d5760c
Saytgalin Alexander <a.saytgalin@2gis.ru>
2015-04-01 13:50:16 +0600

    Always release surfaces when application is suspended or hidden.
    It is needed to recreate surfaces and to redraw when application is
    resumed because on some devices when the application is suspended
    Android destroys the surfaces.
    
    Change-Id: I8934e94af038b4ecf116d93aea223ad040b0bff1
    Task-number: QTBUG-45019
    Reviewed-by: BogDan Vatra <bogdan@kde.org>
    Reviewed-by: Sergey Galin <s.galin@2gis.ru>

--- src/plugins/platforms/android/qandroidplatformscreen.cpp
+++ src/plugins/platforms/android/qandroidplatformscreen.cpp
@@ -256,7 +256,7 @@ void QAndroidPlatformScreen::applicationStateChanged(Qt::ApplicationState state)
     foreach (QAndroidPlatformWindow *w, m_windowStack)
         w->applicationStateChanged(state);
 
-    if (state <=  Qt::ApplicationHidden && QtAndroid::blockEventLoopsWhenSuspended()) {
+    if (state <=  Qt::ApplicationHidden) {
         lockSurface();
         QtAndroid::destroySurface(m_id);
         m_id = -1;
