51028d147289c2e388684ad195f5ec2daa2e7b3e
Marc Mutz <marc.mutz@kdab.com>
2015-04-05 19:49:05 +0200

    QQueue: unhide QList::swap(int, int)
    Task-number: QTBUG-34197
    Change-Id: I0b405ab0fabff8852b2705d651848fbff74fc4c2
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

--- src/corelib/tools/qqueue.h
+++ src/corelib/tools/qqueue.h
@@ -46,6 +46,11 @@ public:
     inline QQueue() {}
     inline ~QQueue() {}
     inline void swap(QQueue<T> &other) { QList<T>::swap(other); } // prevent QList<->QQueue swaps
+#ifndef Q_QDOC
+    // bring in QList::swap(int, int). We cannot say using QList<T>::swap,
+    // because we don't want to make swap(QList&) available.
+    inline void swap(int i, int j) { QList<T>::swap(i, j); }
+#endif
     inline void enqueue(const T &t) { QList<T>::append(t); }
     inline T dequeue() { return QList<T>::takeFirst(); }
     inline T &head() { return QList<T>::first(); }
