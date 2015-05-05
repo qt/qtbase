f1687148af820121a4d5e0c99e9e34c92acc3405
Sérgio Martins <sergio.martins@kdab.com>
2015-05-05 21:30:44 +0100

    docs: QFile doesn't support QUrl style qrc paths
    Change-Id: I2c5f90e7d1e64d652e920bdbf9da98ad8ac1e4f9
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>

--- src/corelib/doc/snippets/code/doc_src_resources.cpp
+++ src/corelib/doc/snippets/code/doc_src_resources.cpp
@@ -48,7 +48,7 @@ MyClass::MyClass() : BaseClass()
 {
     Q_INIT_RESOURCE(resources);
 
-    QFile file("qrc:/myfile.dat");
+    QFile file(":/myfile.dat");
     ...
 }
 //! [5]
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     Q_INIT_RESOURCE(graphlib);
 
-    QFile file("qrc:/graph.png");
+    QFile file(":/graph.png");
     ...
     return app.exec();
 }
