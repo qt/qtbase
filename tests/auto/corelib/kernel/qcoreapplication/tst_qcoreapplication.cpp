qt/qtbase

commit 6c973dee2cb1686ea32657fff7dced3e611b98ce

Make QCoreApplication::applicationName available after app destruction.
Calling applicationName() in the destructor of a global static (e.g.
via QLockFile) was working when calling setApplicationName explicitly
but otherwise it would suddenly return an empty string.
This led to inconsistencies, the application name switching from
non-empty to empty at saving-on-destruction time.

There was already a global static, used when setting the app name
explicitly before construction. Use it now to store the app name
in all cases (explicitly set, or fallback).

Change-Id: I71d3a0c40158f8bfd022c385b198346a2594b1cb
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/kernel/qcoreapplication.cpp
@@ -368,7 +368,9 @@ struct QCoreApplicationData {
     }
 #endif
 
-    QString orgName, orgDomain, application;
+    QString orgName, orgDomain;
+    QString application; // application name, initially from argv[0], can then be modified.
+    QString applicationNameCompat; // for QDesktopServices. Only set explicitly.
     QString applicationVersion;
 
 #ifndef QT_NO_LIBRARY
@@ -749,6 +751,9 @@ void QCoreApplication::init()
 
     Q_ASSERT_X(!self, "QCoreApplication", "there should be only one application object");
     QCoreApplication::self = this;
+
+    // Store app name (so it's still available after QCoreApplication is destroyed)
+    coreappdata()->application = d_func()->appName();
 
     QLoggingRegistry::instance()->init();
 
@@ -2345,9 +2350,13 @@ QString QCoreApplication::organizationDomain()
 */
 void QCoreApplication::setApplicationName(const QString &application)
 {
-    if (coreappdata()->application == application)
+    QString newAppName = application;
+    if (newAppName.isEmpty() && QCoreApplication::self)
+        newAppName = QCoreApplication::self->d_func()->appName();
+    if (coreappdata()->application == newAppName)
         return;
-    coreappdata()->application = application;
+    coreappdata()->application = newAppName;
+    coreappdata()->applicationNameCompat = newAppName;
 #ifndef QT_NO_QOBJECT
     if (QCoreApplication::self)
         emit QCoreApplication::self->applicationNameChanged();
@@ -2359,16 +2368,13 @@ QString QCoreApplication::applicationName()
 #ifdef Q_OS_BLACKBERRY
     coreappdata()->loadManifest();
 #endif
-    QString appname = coreappdata() ? coreappdata()->application : QString();
-    if (appname.isEmpty() && QCoreApplication::self)
-        appname = QCoreApplication::self->d_func()->appName();
-    return appname;
+    return coreappdata() ? coreappdata()->application : QString();
 }
 
 // Exported for QDesktopServices (Qt4 behavior compatibility)
 Q_CORE_EXPORT QString qt_applicationName_noFallback()
 {
-    return coreappdata()->application;
+    return coreappdata()->applicationNameCompat;
 }
 
 /*!

tests/auto/corelib/kernel/qcoreapplication/tst_qcoreapplication.cpp
@@ -102,10 +102,15 @@ void tst_QCoreApplication::qAppName()
     const char* appName = "tst_qcoreapplication";
 #endif
 
-    int argc = 1;
-    char *argv[] = { const_cast<char*>(appName) };
-    TestApplication app(argc, argv);
-    QCOMPARE(::qAppName(), QString::fromLatin1(appName));
+    {
+        int argc = 1;
+        char *argv[] = { const_cast<char*>(appName) };
+        TestApplication app(argc, argv);
+        QCOMPARE(::qAppName(), QString::fromLatin1(appName));
+        QCOMPARE(QCoreApplication::applicationName(), QString::fromLatin1(appName));
+    }
+    // The application name should still be available after destruction;
+    // global statics often rely on this.
     QCOMPARE(QCoreApplication::applicationName(), QString::fromLatin1(appName));
 }
 

commit f1bfc4266bb0b4a3269d79f6ed34eaa8a1cadbca

Don't overwrite applicationName if already set.
My commit 6c973dee2cb1686ea32657 broke the case where setApplicationName
is called before the QCoreApplication constructor.
Fixed and added autotest.

Task-number: QTBUG-45283
Change-Id: If7bdb0d82be50b50a95a04027f5f9d7143c1a7ac
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
Reviewed-by: Shawn Rutledge <shawn.rutledge@digia.com>
Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>

src/corelib/kernel/qcoreapplication.cpp
@@ -326,6 +326,7 @@ struct QCoreApplicationData {
 #ifndef QT_NO_LIBRARY
         app_libpaths = 0;
 #endif
+        applicationNameSet = false;
     }
     ~QCoreApplicationData() {
 #ifndef QT_NO_LIBRARY
@@ -370,8 +371,8 @@ struct QCoreApplicationData {
 
     QString orgName, orgDomain;
     QString application; // application name, initially from argv[0], can then be modified.
-    QString applicationNameCompat; // for QDesktopServices. Only set explicitly.
     QString applicationVersion;
+    bool applicationNameSet; // true if setApplicationName was called
 
 #ifndef QT_NO_LIBRARY
     QStringList *app_libpaths;
@@ -753,7 +754,8 @@ void QCoreApplication::init()
     QCoreApplication::self = this;
 
     // Store app name (so it's still available after QCoreApplication is destroyed)
-    coreappdata()->application = d_func()->appName();
+    if (!coreappdata()->applicationNameSet)
+        coreappdata()->application = d_func()->appName();
 
     QLoggingRegistry::instance()->init();
 
@@ -2350,13 +2352,13 @@ QString QCoreApplication::organizationDomain()
 */
 void QCoreApplication::setApplicationName(const QString &application)
 {
+    coreappdata()->applicationNameSet = !application.isEmpty();
     QString newAppName = application;
     if (newAppName.isEmpty() && QCoreApplication::self)
         newAppName = QCoreApplication::self->d_func()->appName();
     if (coreappdata()->application == newAppName)
         return;
     coreappdata()->application = newAppName;
-    coreappdata()->applicationNameCompat = newAppName;
 #ifndef QT_NO_QOBJECT
     if (QCoreApplication::self)
         emit QCoreApplication::self->applicationNameChanged();
@@ -2374,7 +2376,7 @@ QString QCoreApplication::applicationName()
 // Exported for QDesktopServices (Qt4 behavior compatibility)
 Q_CORE_EXPORT QString qt_applicationName_noFallback()
 {
-    return coreappdata()->applicationNameCompat;
+    return coreappdata()->applicationNameSet ? coreappdata()->application : QString();
 }
 
 /*!

tests/auto/corelib/kernel/qcoreapplication/tst_qcoreapplication.cpp
@@ -112,6 +112,22 @@ void tst_QCoreApplication::qAppName()
     // The application name should still be available after destruction;
     // global statics often rely on this.
     QCOMPARE(QCoreApplication::applicationName(), QString::fromLatin1(appName));
+
+    // Setting the appname before creating the application should work (QTBUG-45283)
+    const QString wantedAppName("my app name");
+    {
+        int argc = 1;
+        char *argv[] = { const_cast<char*>(appName) };
+        QCoreApplication::setApplicationName(wantedAppName);
+        TestApplication app(argc, argv);
+        QCOMPARE(::qAppName(), QString::fromLatin1(appName));
+        QCOMPARE(QCoreApplication::applicationName(), wantedAppName);
+    }
+    QCOMPARE(QCoreApplication::applicationName(), wantedAppName);
+
+    // Restore to initial value
+    QCoreApplication::setApplicationName(QString());
+    QCOMPARE(QCoreApplication::applicationName(), QString());
 }
 
 void tst_QCoreApplication::argc()