8f5dceee3c0639de937474a2237e5ca82a52def5
Sérgio Martins <sergio.martins@kdab.com>
2015-06-16 19:44:52 +0100

    qprinter: Remove superfluous iteration and memory allocation
    Instead of iterating through a QList that was constructed
    after iterating through all of QSet's elements just iterate
    through QSet's elements.
    
    Change-Id: Ie502017976536b14ea9170af550a2d7156443391
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>

--- src/printsupport/kernel/qprinter.cpp
+++ src/printsupport/kernel/qprinter.cpp
@@ -160,7 +160,7 @@ void QPrinterPrivate::changeEngines(QPrinter::OutputFormat format, const QPrinte
     initEngines(format, printer);
 
     if (oldPrintEngine) {
-        foreach (QPrintEngine::PrintEnginePropertyKey key, m_properties.values()) {
+        foreach (QPrintEngine::PrintEnginePropertyKey key, m_properties) {
             QVariant prop;
             // PPK_NumberOfCopies need special treatmeant since it in most cases
             // will return 1, disregarding the actual value that was set
