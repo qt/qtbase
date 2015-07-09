qt/qtbase

commit aa5f70c00a88edcddd26e8fea767a40e8c5c31b8

Make sure QIconLoader is always initialized
It is necessary to properly initialize the icon loader in all case.
Otherwise some calls might give wrong results if a platform theme plugin
is involved.
For instance, we might miss the actual theme name reported by the
platform theme, eg it's what happen with QStyle::standardPixmap if no
one created a QIcon before its first call.

Also clean up the accesses to the global static and have only
QIconLoader::instance() use it. All other call go through the static
method. This way only instance() needs to call ensureInitialized(),
definitely safer and looks cleaner to me.

Change-Id: Id3ea6816edd5a65586004f69464960abc3602cf8
Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

src/gui/image/qicon.cpp
@@ -1063,7 +1063,6 @@ void QIcon::setThemeName(const QString &name)
 */
 QString QIcon::themeName()
 {
-    QIconLoader::instance()->ensureInitialized();
     return QIconLoader::instance()->themeName();
 }
 

src/gui/image/qiconloader.cpp
@@ -127,6 +127,7 @@ void QIconLoader::ensureInitialized()
 
 QIconLoader *QIconLoader::instance()
 {
+   iconLoaderInstance()->ensureInitialized();
    return iconLoaderInstance();
 }
 
@@ -367,17 +368,14 @@ bool QIconLoaderEngine::hasIcon() const
 // Lazily load the icon
 void QIconLoaderEngine::ensureLoaded()
 {
-
-    iconLoaderInstance()->ensureInitialized();
-
-    if (!(iconLoaderInstance()->themeKey() == m_key)) {
+    if (!(QIconLoader::instance()->themeKey() == m_key)) {
 
         while (!m_entries.isEmpty())
             delete m_entries.takeLast();
 
         Q_ASSERT(m_entries.size() == 0);
-        m_entries = iconLoaderInstance()->loadIcon(m_iconName);
-        m_key = iconLoaderInstance()->themeKey();
+        m_entries = QIconLoader::instance()->loadIcon(m_iconName);
+        m_key = QIconLoader::instance()->themeKey();
     }
 }
 
@@ -564,7 +562,7 @@ void QIconLoaderEngine::virtual_hook(int id, void *data)
         {
             QIconEngine::AvailableSizesArgument &arg
                     = *reinterpret_cast<QIconEngine::AvailableSizesArgument*>(data);
-            const QList<QIconDirInfo> directoryKey = iconLoaderInstance()->theme().keyList();
+            const QList<QIconDirInfo> directoryKey = QIconLoader::instance()->theme().keyList();
             arg.sizes.clear();
 
             // Gets all sizes from the DirectoryInfo entries

commit 8c7a9bfbbc8f65256c0ae9dcca2c6f765340d792

Do not support static QIcon instances
There was an attempt to allow static instances of QIcon in
7727a4355876607a1a022ff54e2570dae883f79c (Qt 4). This patch does only
solve some of the corner cases and broke with
aa5f70c00a88edcddd26e8fea767a40e8c5c31b8. Since the "breakage" has been
there for two years, let's officially declare it unsupported instead of
trying to work around the issue.

Change-Id: I61e12fd03953763ee2e70eae58bcaecabdcb85b8
Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>

src/gui/image/qicon.cpp
@@ -595,6 +595,8 @@ QFactoryLoader *qt_iconEngineFactoryLoader()
 
   \image icon.png QIcon
 
+  \note QIcon needs a QGuiApplication instance before the icon is created.
+
   \sa {fowler}{GUI Design Handbook: Iconic Label}, {Icons Example}
 */
 
@@ -1174,8 +1176,6 @@ QIcon QIcon::fromTheme(const QString &name, const QIcon &fallback)
         qtIconCache()->insert(name, cachedIcon);
     }
 
-    // Note the qapp check is to allow lazy loading of static icons
-    // Supporting fallbacks will not work for this case.
     if (qApp && icon.availableSizes().isEmpty())
         return fallback;
 

src/gui/image/qiconloader.cpp
@@ -68,9 +68,6 @@ QIconLoader::QIconLoader() :
 {
 }
 
-// We lazily initialize the loader to make static icons
-// work. Though we do not officially support this.
-
 static inline QString systemThemeName()
 {
     if (const QPlatformTheme *theme = QGuiApplicationPrivate::platformTheme()) {

tests/auto/gui/image/qicon/tst_qicon.cpp
@@ -75,13 +75,7 @@ private:
     const QString m_pngImageFileName;
     const QString m_pngRectFileName;
     const QString m_sourceFileName;
-
-    const static QIcon staticIcon;
 };
-
-// Creating an icon statically should not cause a crash.
-// But we do not officially support this. See QTBUG-8666
-const QIcon tst_QIcon::staticIcon = QIcon::fromTheme("edit-find");
 
 bool tst_QIcon::haveImageFormat(QByteArray const& desiredFormat)
 {