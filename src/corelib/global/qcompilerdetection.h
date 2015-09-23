ddee17e7705fd3f64c564dcd926531d181387fd1
Thiago Macieira <thiago.macieira@intel.com>
2015-09-23 10:41:21 -0700

    Remove QT_WARNING_DISABLE_GCC for Clang
    There's QT_WARNING_DISABLE_CLANG for when a warning applies to a Clang
    build.
    
    Change-Id: I42e7ef1a481840699a8dffff1406ac36b6a6eac3
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>

--- src/corelib/global/qcompilerdetection.h
+++ src/corelib/global/qcompilerdetection.h
@@ -1140,7 +1140,7 @@
 #  define QT_WARNING_PUSH                       QT_DO_PRAGMA(clang diagnostic push)
 #  define QT_WARNING_POP                        QT_DO_PRAGMA(clang diagnostic pop)
 #  define QT_WARNING_DISABLE_CLANG(text)        QT_DO_PRAGMA(clang diagnostic ignored text)
-#  define QT_WARNING_DISABLE_GCC(text)          QT_DO_PRAGMA(GCC diagnostic ignored text)   // GCC directives work in Clang too
+#  define QT_WARNING_DISABLE_GCC(text)
 #  define QT_WARNING_DISABLE_INTEL(number)
 #  define QT_WARNING_DISABLE_MSVC(number)
 #elif defined(Q_CC_GNU) && (__GNUC__ * 100 + __GNUC_MINOR__ >= 406)
