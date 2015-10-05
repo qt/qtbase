da5b8bfe4647f4a0494e7dd2e08d6652f72803f0
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-10-05 10:53:00 +0200

    Add missing "We mean it" comments to private headers.
    Change-Id: If81a5e1db0fe93377e7cc54a78b01c50b44abe57
    Reviewed-by: Liang Qi <liang.qi@theqtcompany.com>

--- src/platformsupport/dbusmenu/qdbusmenuadaptor_p.h
+++ src/platformsupport/dbusmenu/qdbusmenuadaptor_p.h
@@ -45,6 +45,17 @@
 #ifndef DBUSMENUADAPTOR_H
 #define DBUSMENUADAPTOR_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/QObject>
 #include <QtDBus/QtDBus>
 #include "qdbusmenutypes_p.h"
