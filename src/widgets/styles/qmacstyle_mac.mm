363498ee5a64dcd14f7c43174921492af497d569
Tor Arne Vestbø <tor.arne.vestbo@theqtcompany.com>
2015-10-06 14:17:05 +0200

    QMacStyle: Set NSButton title to empty string, not nil
    Fixes the following warning [-Wnonnull]:
    
      warning: null passed to a callee that requires a non-null argument
    
    Change-Id: I52f7dc338afdcfe0cd605281d1bf59025abb5ac6
    Reviewed-by: Jake Petroules <jake.petroules@theqtcompany.com>

--- src/widgets/styles/qmacstyle_mac.mm
+++ src/widgets/styles/qmacstyle_mac.mm
@@ -1867,7 +1867,7 @@ NSView *QMacStylePrivate::cocoaControl(QCocoaWidget widget) const
 
         if ([bv isKindOfClass:[NSButton class]]) {
             NSButton *bc = (NSButton *)bv;
-            bc.title = nil;
+            bc.title = @"";
         }
 
         if ([bv isKindOfClass:[NSControl class]]) {
