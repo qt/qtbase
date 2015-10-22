qt/qtbase

commit 4dccb2ca674e9eafca65da0775254932102c7f4b

Handle action events in QLineEditIconButton.
Ensure QAction::setVisible() is
handled and reposition if visibility changes.

Task-number: QTBUG-39660
Change-Id: I14f0659aedc8dc89ddef3159d3a500b40b1563ff
Reviewed-by: David Faure <david.faure@kdab.com>

src/widgets/widgets/qlineedit_p.cpp
@@ -324,6 +324,24 @@ void QLineEditIconButton::paintEvent(QPaintEvent *)
     painter.drawPixmap(pixmapRect, iconPixmap);
 }
 
+void QLineEditIconButton::actionEvent(QActionEvent *e)
+{
+    switch (e->type()) {
+    case QEvent::ActionChanged: {
+        const QAction *action = e->action();
+        if (isVisible() != action->isVisible()) {
+            setVisible(action->isVisible());
+            if (QLineEdit *le = qobject_cast<QLineEdit *>(parentWidget()))
+                static_cast<QLineEditPrivate *>(qt_widget_private(le))->positionSideWidgets();
+        }
+    }
+        break;
+    default:
+        break;
+    }
+    QToolButton::actionEvent(e);
+}
+
 void QLineEditIconButton::setOpacity(qreal value)
 {
     if (!qFuzzyCompare(m_opacity, value)) {
@@ -415,12 +433,14 @@ void QLineEditPrivate::positionSideWidgets()
         QRect widgetGeometry(QPoint(QLineEditIconButton::IconMargin, (contentRect.height() - iconSize.height()) / 2), iconSize);
         foreach (const SideWidgetEntry &e, leftSideWidgetList()) {
             e.widget->setGeometry(widgetGeometry);
-            widgetGeometry.moveLeft(widgetGeometry.left() + delta);
+            if (e.widget->isVisible())
+                widgetGeometry.moveLeft(widgetGeometry.left() + delta);
         }
         widgetGeometry.moveLeft(contentRect.width() - iconSize.width() - QLineEditIconButton::IconMargin);
         foreach (const SideWidgetEntry &e, rightSideWidgetList()) {
             e.widget->setGeometry(widgetGeometry);
-            widgetGeometry.moveLeft(widgetGeometry.left() - delta);
+            if (e.widget->isVisible())
+                widgetGeometry.moveLeft(widgetGeometry.left() - delta);
         }
     }
 }

tests/auto/widgets/widgets/qlineedit/tst_qlineedit.cpp
@@ -306,6 +306,7 @@ private slots:
     void clearButton();
     void clearButtonVisibleAfterSettingText_QTBUG_45518();
     void sideWidgets();
+    void sideWidgetsActionEvents();
 
     void shouldShowPlaceholderText_data();
     void shouldShowPlaceholderText();
@@ -4318,6 +4319,13 @@ void tst_QLineEdit::clearButtonVisibleAfterSettingText_QTBUG_45518()
 #endif // QT_BUILD_INTERNAL
 }
 
+static inline QIcon sideWidgetTestIcon()
+{
+    QImage image(QSize(20, 20), QImage::Format_ARGB32);
+    image.fill(Qt::yellow);
+    return QIcon(QPixmap::fromImage(image));
+}
+
 void tst_QLineEdit::sideWidgets()
 {
     QWidget testWidget;
@@ -4325,9 +4333,7 @@ void tst_QLineEdit::sideWidgets()
     QLineEdit *lineEdit = new QLineEdit(&testWidget);
     l->addWidget(lineEdit);
     l->addSpacerItem(new QSpacerItem(0, 50, QSizePolicy::Ignored, QSizePolicy::Fixed));
-    QImage image(QSize(20, 20), QImage::Format_ARGB32);
-    image.fill(Qt::yellow);
-    QAction *iconAction = new QAction(QIcon(QPixmap::fromImage(image)), QString(), lineEdit);
+    QAction *iconAction = new QAction(sideWidgetTestIcon(), QString(), lineEdit);
     QWidgetAction *label1Action = new QWidgetAction(lineEdit);
     label1Action->setDefaultWidget(new QLabel(QStringLiteral("l1")));
     QWidgetAction *label2Action = new QWidgetAction(lineEdit);
@@ -4352,6 +4358,38 @@ void tst_QLineEdit::sideWidgets()
     lineEdit->removeAction(label1Action);
     lineEdit->addAction(iconAction);
     lineEdit->addAction(iconAction);
+}
+
+void tst_QLineEdit::sideWidgetsActionEvents()
+{
+    // QTBUG-39660, verify whether action events are handled by the widget.
+    QWidget testWidget;
+    QVBoxLayout *l = new QVBoxLayout(&testWidget);
+    QLineEdit *lineEdit = new QLineEdit(&testWidget);
+    l->addWidget(lineEdit);
+    l->addSpacerItem(new QSpacerItem(0, 50, QSizePolicy::Ignored, QSizePolicy::Fixed));
+    QAction *iconAction = lineEdit->addAction(sideWidgetTestIcon(), QLineEdit::LeadingPosition);
+    testWidget.move(300, 300);
+    testWidget.show();
+    QVERIFY(QTest::qWaitForWindowExposed(&testWidget));
+
+    QWidget *toolButton = Q_NULLPTR;
+    foreach (QWidget *w, iconAction->associatedWidgets()) {
+        if (qobject_cast<QToolButton *>(w)) {
+            toolButton = w;
+            break;
+        }
+    }
+    QVERIFY(toolButton);
+
+    QVERIFY(toolButton->isVisible());
+    QVERIFY(toolButton->isEnabled());
+
+    iconAction->setEnabled(false);
+    QVERIFY(!toolButton->isEnabled());
+
+    iconAction->setVisible(false);
+    QVERIFY(!toolButton->isVisible());
 }
 
 Q_DECLARE_METATYPE(Qt::AlignmentFlag)

src/widgets/widgets/qlineedit_p.h
@@ -61,6 +61,8 @@
 
 #include "private/qwidgetlinecontrol_p.h"
 
+#include <algorithm>
+
 QT_BEGIN_NAMESPACE
 
 // QLineEditIconButton: This is a simple helper class that represents clickable icons that fade in with text
@@ -81,6 +83,7 @@ public:
 #endif
 
 protected:
+    void actionEvent(QActionEvent *e) Q_DECL_OVERRIDE;
     void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
 
 private slots:
@@ -226,14 +229,23 @@ private:
     mutable QSize m_iconSize;
 };
 
+static bool isSideWidgetVisible(const QLineEditPrivate::SideWidgetEntry &e)
+{
+   return e.widget->isVisible();
+}
+
 inline int QLineEditPrivate::effectiveLeftTextMargin() const
 {
-    return leftTextMargin + leftSideWidgetList().size() * (QLineEditIconButton::IconMargin + iconSize().width());
+    return leftTextMargin + (QLineEditIconButton::IconMargin + iconSize().width())
+        * int(std::count_if(leftSideWidgetList().constBegin(), leftSideWidgetList().constEnd(),
+                            isSideWidgetVisible));
 }
 
 inline int QLineEditPrivate::effectiveRightTextMargin() const
 {
-    return rightTextMargin + rightSideWidgetList().size() * (QLineEditIconButton::IconMargin + iconSize().width());
+    return rightTextMargin + (QLineEditIconButton::IconMargin + iconSize().width())
+        * int(std::count_if(rightSideWidgetList().constBegin(), rightSideWidgetList().constEnd(),
+                            isSideWidgetVisible));
 }
 
 #endif // QT_NO_LINEEDIT

commit 13b1c23f8b2cdf283703a75f475ddcf06653bf7e

QLineEdit: Fix visibility handling of side widgets.
Compare against isVisibleTo() in QLineEditIconButton::actionEvent()
so that action events received before show() are handled correctly.

Fix a regression introduced by change
4dccb2ca674e9eafca65da0775254932102c7f4b for handling action
events causing side widgets to overlap when added before the widget was
shown. Use QAction::isVisible() to determine visibility.

Task-number: QTBUG-48806
Task-number: QTBUG-48899
Task-number: QTBUG-39660
Change-Id: I7a39a3b9a094f2c74cde09544f1158deb2b81cf2
Reviewed-by: David Faure <david.faure@kdab.com>

src/widgets/widgets/qlineedit_p.cpp
@@ -329,7 +329,7 @@ void QLineEditIconButton::actionEvent(QActionEvent *e)
     switch (e->type()) {
     case QEvent::ActionChanged: {
         const QAction *action = e->action();
-        if (isVisible() != action->isVisible()) {
+        if (isVisibleTo(parentWidget()) != action->isVisible()) {
             setVisible(action->isVisible());
             if (QLineEdit *le = qobject_cast<QLineEdit *>(parentWidget()))
                 static_cast<QLineEditPrivate *>(qt_widget_private(le))->positionSideWidgets();
@@ -433,13 +433,13 @@ void QLineEditPrivate::positionSideWidgets()
         QRect widgetGeometry(QPoint(QLineEditIconButton::IconMargin, (contentRect.height() - iconSize.height()) / 2), iconSize);
         foreach (const SideWidgetEntry &e, leftSideWidgetList()) {
             e.widget->setGeometry(widgetGeometry);
-            if (e.widget->isVisible())
+            if (e.action->isVisible())
                 widgetGeometry.moveLeft(widgetGeometry.left() + delta);
         }
         widgetGeometry.moveLeft(contentRect.width() - iconSize.width() - QLineEditIconButton::IconMargin);
         foreach (const SideWidgetEntry &e, rightSideWidgetList()) {
             e.widget->setGeometry(widgetGeometry);
-            if (e.widget->isVisible())
+            if (e.action->isVisible())
                 widgetGeometry.moveLeft(widgetGeometry.left() - delta);
         }
     }

tests/auto/widgets/widgets/qlineedit/tst_qlineedit.cpp
@@ -4319,10 +4319,10 @@ void tst_QLineEdit::clearButtonVisibleAfterSettingText_QTBUG_45518()
 #endif // QT_BUILD_INTERNAL
 }
 
-static inline QIcon sideWidgetTestIcon()
+static inline QIcon sideWidgetTestIcon(Qt::GlobalColor color = Qt::yellow)
 {
     QImage image(QSize(20, 20), QImage::Format_ARGB32);
-    image.fill(Qt::yellow);
+    image.fill(color);
     return QIcon(QPixmap::fromImage(image));
 }
 
@@ -4360,6 +4360,15 @@ void tst_QLineEdit::sideWidgets()
     lineEdit->addAction(iconAction);
 }
 
+template <class T> T *findAssociatedWidget(const QAction *a)
+{
+    foreach (QWidget *w, a->associatedWidgets()) {
+        if (T *result = qobject_cast<T *>(w))
+            return result;
+    }
+    return Q_NULLPTR;
+}
+
 void tst_QLineEdit::sideWidgetsActionEvents()
 {
     // QTBUG-39660, verify whether action events are handled by the widget.
@@ -4368,28 +4377,43 @@ void tst_QLineEdit::sideWidgetsActionEvents()
     QLineEdit *lineEdit = new QLineEdit(&testWidget);
     l->addWidget(lineEdit);
     l->addSpacerItem(new QSpacerItem(0, 50, QSizePolicy::Ignored, QSizePolicy::Fixed));
-    QAction *iconAction = lineEdit->addAction(sideWidgetTestIcon(), QLineEdit::LeadingPosition);
+    QAction *iconAction1 = lineEdit->addAction(sideWidgetTestIcon(Qt::red), QLineEdit::LeadingPosition);
+    QAction *iconAction2 = lineEdit->addAction(sideWidgetTestIcon(Qt::blue), QLineEdit::LeadingPosition);
+    QAction *iconAction3 = lineEdit->addAction(sideWidgetTestIcon(Qt::yellow), QLineEdit::LeadingPosition);
+    iconAction3->setVisible(false);
+
     testWidget.move(300, 300);
     testWidget.show();
     QVERIFY(QTest::qWaitForWindowExposed(&testWidget));
 
-    QWidget *toolButton = Q_NULLPTR;
-    foreach (QWidget *w, iconAction->associatedWidgets()) {
-        if (qobject_cast<QToolButton *>(w)) {
-            toolButton = w;
-            break;
-        }
-    }
-    QVERIFY(toolButton);
+    QWidget *toolButton1 = findAssociatedWidget<QToolButton>(iconAction1);
+    QWidget *toolButton2 = findAssociatedWidget<QToolButton>(iconAction2);
+    QWidget *toolButton3 = findAssociatedWidget<QToolButton>(iconAction3);
 
-    QVERIFY(toolButton->isVisible());
-    QVERIFY(toolButton->isEnabled());
+    QVERIFY(toolButton1);
+    QVERIFY(toolButton2);
+    QVERIFY(toolButton3);
 
-    iconAction->setEnabled(false);
-    QVERIFY(!toolButton->isEnabled());
+    QVERIFY(!toolButton3->isVisible()); // QTBUG-48899 , action hidden before show().
 
-    iconAction->setVisible(false);
-    QVERIFY(!toolButton->isVisible());
+    QVERIFY(toolButton1->isVisible());
+    QVERIFY(toolButton1->isEnabled());
+
+    QVERIFY(toolButton2->isVisible());
+    QVERIFY(toolButton2->isEnabled());
+
+    const int toolButton1X = toolButton1->x();
+    const int toolButton2X = toolButton2->x();
+    QVERIFY(toolButton1X < toolButton2X); // QTBUG-48806, positioned beside each other.
+
+    iconAction1->setEnabled(false);
+    QVERIFY(!toolButton1->isEnabled());
+
+    iconAction1->setVisible(false);
+    QVERIFY(!toolButton1->isVisible());
+
+    // QTBUG-39660, button 2 takes position of invisible button 1.
+    QCOMPARE(toolButton2->x(), toolButton1X);
 }
 
 Q_DECLARE_METATYPE(Qt::AlignmentFlag)