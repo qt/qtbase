9daef8a54c32162a50b0f558dac829333165618f
Adam Strzelecki <ono@java.pl>
2015-11-05 22:35:42 +0100

    QComboBox: Disable wheel events on OS X & iOS
    This is follow-up for QTabBar fix ea47d152b35158ba07a55d009f57df0e4c2a048f.
    
    In native OS X applications using mouse wheel on combo boxes have absolutely no
    effect. We should bring the same behavior to Qt based OS X apps too, as users
    are complaining of unexpected behavior, eg. randomly switching Qt Creator
    sidebar mode when scrolling file list and moving mouse pointer little bit
    above. Moreover inertial mouse behavior on OS X makes combo box usually move
    several indexes, rather than single one on slight finger slide.
    
    This also applies to iOS apps so the change affects all Apple platforms.
    
    Task-number: QTBUG-10707
    Change-Id: I6582265039198707ad8c2f54de96ee2a0b0e0b47
    Reviewed-by: Adam Strzelecki <ono@java.pl>
    Reviewed-by: Jake Petroules <jake.petroules@theqtcompany.com>

--- src/widgets/widgets/qcombobox.cpp
+++ src/widgets/widgets/qcombobox.cpp
@@ -3238,6 +3238,9 @@ void QComboBox::keyReleaseEvent(QKeyEvent *e)
 #ifndef QT_NO_WHEELEVENT
 void QComboBox::wheelEvent(QWheelEvent *e)
 {
+#ifdef Q_OS_DARWIN
+    Q_UNUSED(e);
+#else
     Q_D(QComboBox);
     if (!d->viewContainer()->isVisible()) {
         int newIndex = currentIndex();
@@ -3258,6 +3261,7 @@ void QComboBox::wheelEvent(QWheelEvent *e)
         }
         e->accept();
     }
+#endif
 }
 #endif
 
