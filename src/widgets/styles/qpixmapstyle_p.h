64684c23fd2847aecc0f1bcee3b24defdd091649
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-11-25 11:11:20 +0100

    QPixmapStyle: Add "We mean it." warning.
    Fix syncqt-warning:
    QtWidgets: qtbase/src/widgets/styles/qpixmapstyle_p.h does not have the "We mean it." warning
    
    Change-Id: I66ab3a12e53fe91254d539e236a58ada16bb2cd2
    Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>

--- src/widgets/styles/qpixmapstyle_p.h
+++ src/widgets/styles/qpixmapstyle_p.h
@@ -39,6 +39,17 @@
 #include <QtWidgets/QCommonStyle>
 #include <QtWidgets/QTileRules>
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 QT_BEGIN_NAMESPACE
 
 class QPixmapStylePrivate;
