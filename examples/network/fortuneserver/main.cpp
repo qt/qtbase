de7d2eb2adcdee82e48b04945aca87d455e65f9f
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-11-26 15:04:33 +0100

    Polish the fortune server/client examples.
    - Remove Qt::WindowContextHelpButtonHint.
    - Make the server label interactive (enable copy).
    - Introduce new connection syntax.
    - Remove unneeded member variables.
    - Use constructor initialization where appropriate.
    - Adapt the layout to fullscreen platforms by wrapping it
      into a QGroupBox.
    
    Change-Id: I6e397ad082f22ba1e99fc5a17440b2be1f9584f6
    Reviewed-by: Topi Reiniö <topi.reinio@theqtcompany.com>

--- examples/network/fortuneserver/main.cpp
+++ examples/network/fortuneserver/main.cpp
@@ -48,6 +48,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QGuiApplication::setApplicationDisplayName(Server::tr("Fortune Server"));
     Server server;
     server.show();
     qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
