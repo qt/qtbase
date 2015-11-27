63a5b3b09da867f4c005dac1359b87e76f8904ee
Lars Knoll <lars.knoll@theqtcompany.com>
2015-11-27 10:32:01 +0100

    Remove a reference to Mac OS X 10.4
    It's been a while since we stopped supporting that version :)
    
    Change-Id: I2fa4900945551024ac920c36c47d20b47aee50c0
    Reviewed-by: Jake Petroules <jake.petroules@theqtcompany.com>

--- src/corelib/io/qfilesystemwatcher.cpp
+++ src/corelib/io/qfilesystemwatcher.cpp
@@ -177,7 +177,7 @@ void QFileSystemWatcherPrivate::_q_directoryChanged(const QString &path, bool re
     \note The act of monitoring files and directories for
     modifications consumes system resources. This implies there is a
     limit to the number of files and directories your process can
-    monitor simultaneously. On Mac OS X 10.4 and all BSD variants, for
+    monitor simultaneously. On all BSD variants, for
     example, an open file descriptor is required for each monitored
     file. Some system limits the number of open file descriptors to 256
     by default. This means that addPath() and addPaths() will fail if
@@ -185,7 +185,7 @@ void QFileSystemWatcherPrivate::_q_directoryChanged(const QString &path, bool re
     the file system monitor. Also note that your process may have
     other file descriptors open in addition to the ones for files
     being monitored, and these other open descriptors also count in
-    the total. Mac OS X 10.5 and up use a different backend and do not
+    the total. OS X uses a different backend and does not
     suffer from this issue.
 
 
