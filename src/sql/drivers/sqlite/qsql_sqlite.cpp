qt/qtbase

commit d79ae904660af7a83dc42a389c2457a8c94020f7

qsql: apply Qt's PIMPL idiom to Q*ResultPrivate
QResult and QResultPrivate are not derived from QObject and
QObjectPrivate respectively, but can still benefit from Qt's
PIMPL idiom. There are several interrelated aspects to this:

  - Base all driver ResultPrivate classes on QResultPrivate.
    Previously, each level in the Result hierarchy tended to keep
    its own private data class.

  - The ResultPrivate class initializes its own Result (q_ptr)
    and Driver members. This ensures that these pointers are
    correctly set in time for the ResultPrivate constructors
    and Result constructors. This is more efficient and makes
    it a lot easier to follow what's being allocated, initialized,
    and cleaned-up.

  - Use macros Q_DECLARE_PRIVATE, Q_DECLARE_PUBLIC, Q_D, and
    Q_Q for access to and from ResultPrivate objects.

  - ResultPrivate classes refer frequently to their counterpart
    DriverPrivate. Various patterns were used to do this. Now
    Q_DECLARE_SQLDRIVER_PRIVATE arranges this uniformly while
    hiding ugly casting. It creates a public method in the
    ResultPrivate returning the correctly typed pointer to the
    corresponding DriverPrivate object. Since the method is public,
    the Result class and helper classes and functions can also use it.

  - The explicit const is removed from QResultPrivate::sqldriver,
    even though it is treated (mostly) like a const within the
    context of Result and ResultPrivate. This is the same pattern
    seen in Qt's PIMPL idiom. The macro created getter methods
    take care of const.

  - qsql_mysql was using a signal/slot connection to zero its own
    copy of the driver pointer when the driver was destroyed. This is
    no longer necessary.

Change-Id: Ida4933bc92fb3e9a05ea4b53b48085894734e36e
Reviewed-by: Israel Lins Albuquerque <israelins85@yahoo.com.br>
Reviewed-by: Mark Brand <mabrand@mabrand.nl>

src/sql/drivers/sqlite/qsql_sqlite.cpp
@@ -105,8 +105,9 @@ class QSQLiteResultPrivate;
 
 class QSQLiteResult : public QSqlCachedResult
 {
+    Q_DECLARE_PRIVATE(QSQLiteResult)
     friend class QSQLiteDriver;
-    friend class QSQLiteResultPrivate;
+
 public:
     explicit QSQLiteResult(const QSQLiteDriver* db);
     ~QSQLiteResult();
@@ -123,13 +124,12 @@ protected:
     QSqlRecord record() const Q_DECL_OVERRIDE;
     void detachFromResultSet() Q_DECL_OVERRIDE;
     void virtual_hook(int id, void *data) Q_DECL_OVERRIDE;
-
-private:
-    QSQLiteResultPrivate* d;
 };
 
 class QSQLiteDriverPrivate : public QSqlDriverPrivate
 {
+    Q_DECLARE_PUBLIC(QSQLiteDriver)
+
 public:
     inline QSQLiteDriverPrivate() : QSqlDriverPrivate(), access(0) { dbmsType = QSqlDriver::SQLite; }
     sqlite3 *access;
@@ -137,17 +137,19 @@ public:
 };
 
 
-class QSQLiteResultPrivate
+class QSQLiteResultPrivate: public QSqlCachedResultPrivate
 {
+    Q_DECLARE_PUBLIC(QSQLiteResult)
+
 public:
-    QSQLiteResultPrivate(QSQLiteResult *res);
+    Q_DECLARE_SQLDRIVER_PRIVATE(QSQLiteDriver)
+    QSQLiteResultPrivate(QSQLiteResult *q, const QSQLiteDriver *drv);
     void cleanup();
     bool fetchNext(QSqlCachedResult::ValueCache &values, int idx, bool initialFetch);
     // initializes the recordInfo and the cache
     void initColumns(bool emptyResultset);
     void finalize();
 
-    QSQLiteResult* q;
     sqlite3 *access;
 
     sqlite3_stmt *stmt;
@@ -158,13 +160,17 @@ public:
     QVector<QVariant> firstRow;
 };
 
-QSQLiteResultPrivate::QSQLiteResultPrivate(QSQLiteResult* res) : q(res), access(0),
-    stmt(0), skippedStatus(false), skipRow(false)
+QSQLiteResultPrivate::QSQLiteResultPrivate(QSQLiteResult *q, const QSQLiteDriver *drv)
+    : QSqlCachedResultPrivate(q, drv),
+      stmt(0),
+      skippedStatus(false),
+      skipRow(false)
 {
 }
 
 void QSQLiteResultPrivate::cleanup()
 {
+    Q_Q(QSQLiteResult);
     finalize();
     rInf.clear();
     skippedStatus = false;
@@ -185,6 +191,7 @@ void QSQLiteResultPrivate::finalize()
 
 void QSQLiteResultPrivate::initColumns(bool emptyResultset)
 {
+    Q_Q(QSQLiteResult);
     int nCols = sqlite3_column_count(stmt);
     if (nCols <= 0)
         return;
@@ -236,6 +243,7 @@ void QSQLiteResultPrivate::initColumns(bool emptyResultset)
 
 bool QSQLiteResultPrivate::fetchNext(QSqlCachedResult::ValueCache &values, int idx, bool initialFetch)
 {
+    Q_Q(QSQLiteResult);
     int res;
     int i;
 
@@ -336,20 +344,18 @@ bool QSQLiteResultPrivate::fetchNext(QSqlCachedResult::ValueCache &values, int i
 }
 
 QSQLiteResult::QSQLiteResult(const QSQLiteDriver* db)
-    : QSqlCachedResult(db)
+    : QSqlCachedResult(*new QSQLiteResultPrivate(this, db))
 {
-    d = new QSQLiteResultPrivate(this);
-    d->access = db->d_func()->access;
-    const_cast<QSQLiteDriverPrivate*>(db->d_func())->results.append(this);
+    Q_D(QSQLiteResult);
+    d->access = d->drv_d_func()->access;
+    const_cast<QSQLiteDriverPrivate*>(d->drv_d_func())->results.append(this);
 }
 
 QSQLiteResult::~QSQLiteResult()
 {
-    const QSqlDriver *sqlDriver = driver();
-    if (sqlDriver)
-        const_cast<QSQLiteDriverPrivate*>(qobject_cast<const QSQLiteDriver *>(sqlDriver)->d_func())->results.removeOne(this);
+    Q_D(QSQLiteResult);
+    const_cast<QSQLiteDriverPrivate*>(d->drv_d_func())->results.removeOne(this);
     d->cleanup();
-    delete d;
 }
 
 void QSQLiteResult::virtual_hook(int id, void *data)
@@ -366,6 +372,7 @@ bool QSQLiteResult::reset(const QString &query)
 
 bool QSQLiteResult::prepare(const QString &query)
 {
+    Q_D(QSQLiteResult);
     if (!driver() || !driver()->isOpen() || driver()->isOpenError())
         return false;
 
@@ -399,6 +406,7 @@ bool QSQLiteResult::prepare(const QString &query)
 
 bool QSQLiteResult::exec()
 {
+    Q_D(QSQLiteResult);
     const QVector<QVariant> values = boundValues();
 
     d->skippedStatus = false;
@@ -493,6 +501,7 @@ bool QSQLiteResult::exec()
 
 bool QSQLiteResult::gotoNext(QSqlCachedResult::ValueCache& row, int idx)
 {
+    Q_D(QSQLiteResult);
     return d->fetchNext(row, idx, false);
 }
 
@@ -503,11 +512,13 @@ int QSQLiteResult::size()
 
 int QSQLiteResult::numRowsAffected()
 {
+    Q_D(const QSQLiteResult);
     return sqlite3_changes(d->access);
 }
 
 QVariant QSQLiteResult::lastInsertId() const
 {
+    Q_D(const QSQLiteResult);
     if (isActive()) {
         qint64 id = sqlite3_last_insert_rowid(d->access);
         if (id)
@@ -518,6 +529,7 @@ QVariant QSQLiteResult::lastInsertId() const
 
 QSqlRecord QSQLiteResult::record() const
 {
+    Q_D(const QSQLiteResult);
     if (!isActive() || !isSelect())
         return QSqlRecord();
     return d->rInf;
@@ -525,12 +537,14 @@ QSqlRecord QSQLiteResult::record() const
 
 void QSQLiteResult::detachFromResultSet()
 {
+    Q_D(QSQLiteResult);
     if (d->stmt)
         sqlite3_reset(d->stmt);
 }
 
 QVariant QSQLiteResult::handle() const
 {
+    Q_D(const QSQLiteResult);
     return QVariant::fromValue(d->stmt);
 }
 
@@ -640,7 +654,7 @@ void QSQLiteDriver::close()
     Q_D(QSQLiteDriver);
     if (isOpen()) {
         foreach (QSQLiteResult *result, d->results) {
-            result->d->finalize();
+            result->d_func()->finalize();
         }
 
         if (sqlite3_close(d->access) != SQLITE_OK)

src/sql/drivers/db2/qsql_db2.cpp
@@ -66,6 +66,8 @@ static const SQLSMALLINT qParamType[4] = { SQL_PARAM_INPUT, SQL_PARAM_INPUT, SQL
 
 class QDB2DriverPrivate : public QSqlDriverPrivate
 {
+    Q_DECLARE_PUBLIC(QDB2Driver)
+
 public:
     QDB2DriverPrivate() : QSqlDriverPrivate(), hEnv(0), hDbc(0) { dbmsType = QSqlDriver::DB2; }
     SQLHANDLE hEnv;
@@ -77,8 +79,10 @@ class QDB2ResultPrivate;
 
 class QDB2Result: public QSqlResult
 {
+    Q_DECLARE_PRIVATE(QDB2Result)
+
 public:
-    QDB2Result(const QDB2Driver *dr, const QDB2DriverPrivate *dp);
+    QDB2Result(const QDB2Driver *drv);
     ~QDB2Result();
     bool prepare(const QString &query) Q_DECL_OVERRIDE;
     bool exec() Q_DECL_OVERRIDE;
@@ -98,15 +102,17 @@ protected:
     void virtual_hook(int id, void *data) Q_DECL_OVERRIDE;
     void detachFromResultSet() Q_DECL_OVERRIDE;
     bool nextResult() Q_DECL_OVERRIDE;
-
-private:
-    QDB2ResultPrivate *d;
 };
 
-class QDB2ResultPrivate
+class QDB2ResultPrivate: public QSqlResultPrivate
 {
+    Q_DECLARE_PUBLIC(QDB2Result)
+
 public:
-    QDB2ResultPrivate(const QDB2DriverPrivate* d): dp(d), hStmt(0)
+    Q_DECLARE_SQLDRIVER_PRIVATE(QDB2Driver)
+    QDB2ResultPrivate(QDB2Result *q, const QDB2Driver *drv)
+        : QSqlResultPrivate(q, drv),
+          hStmt(0)
     {}
     ~QDB2ResultPrivate()
     {
@@ -125,7 +131,6 @@ public:
         valueCache.clear();
     }
 
-    const QDB2DriverPrivate* dp;
     SQLHANDLE hStmt;
     QSqlRecord recInf;
     QVector<QVariant*> valueCache;
@@ -171,8 +176,8 @@ static QString qDB2Warn(const QDB2DriverPrivate* d)
 
 static QString qDB2Warn(const QDB2ResultPrivate* d)
 {
-    return (qWarnDB2Handle(SQL_HANDLE_ENV, d->dp->hEnv) + QLatin1Char(' ')
-             + qWarnDB2Handle(SQL_HANDLE_DBC, d->dp->hDbc)
+    return (qWarnDB2Handle(SQL_HANDLE_ENV, d->drv_d_func()->hEnv) + QLatin1Char(' ')
+             + qWarnDB2Handle(SQL_HANDLE_DBC, d->drv_d_func()->hDbc)
              + qWarnDB2Handle(SQL_HANDLE_STMT, d->hStmt));
 }
 
@@ -497,7 +502,7 @@ static bool qMakeStatement(QDB2ResultPrivate* d, bool forwardOnly, bool setForwa
     SQLRETURN r;
     if (!d->hStmt) {
         r = SQLAllocHandle(SQL_HANDLE_STMT,
-                            d->dp->hDbc,
+                            d->drv_d_func()->hDbc,
                             &d->hStmt);
         if (r != SQL_SUCCESS) {
             qSqlWarning(QLatin1String("QDB2Result::reset: Unable to allocate statement handle"), d);
@@ -536,30 +541,31 @@ static bool qMakeStatement(QDB2ResultPrivate* d, bool forwardOnly, bool setForwa
 
 QVariant QDB2Result::handle() const
 {
+    Q_D(const QDB2Result);
     return QVariant(qRegisterMetaType<SQLHANDLE>("SQLHANDLE"), &d->hStmt);
 }
 
 /************************************/
 
-QDB2Result::QDB2Result(const QDB2Driver* dr, const QDB2DriverPrivate* dp)
-    : QSqlResult(dr)
+QDB2Result::QDB2Result(const QDB2Driver *drv)
+    : QSqlResult(*new QDB2ResultPrivate(this, drv))
 {
-    d = new QDB2ResultPrivate(dp);
 }
 
 QDB2Result::~QDB2Result()
 {
+    Q_D(const QDB2Result);
     if (d->hStmt) {
         SQLRETURN r = SQLFreeHandle(SQL_HANDLE_STMT, d->hStmt);
         if (r != SQL_SUCCESS)
             qSqlWarning(QLatin1String("QDB2Driver: Unable to free statement handle ")
                         + QString::number(r), d);
     }
-    delete d;
 }
 
 bool QDB2Result::reset (const QString& query)
 {
+    Q_D(QDB2Result);
     setActive(false);
     setAt(QSql::BeforeFirstRow);
     SQLRETURN r;
@@ -596,6 +602,7 @@ bool QDB2Result::reset (const QString& query)
 
 bool QDB2Result::prepare(const QString& query)
 {
+    Q_D(QDB2Result);
     setActive(false);
     setAt(QSql::BeforeFirstRow);
     SQLRETURN r;
@@ -620,6 +627,7 @@ bool QDB2Result::prepare(const QString& query)
 
 bool QDB2Result::exec()
 {
+    Q_D(QDB2Result);
     QList<QByteArray> tmpStorage; // holds temporary ptrs
     QVarLengthArray<SQLINTEGER, 32> indicators(boundValues().count());
 
@@ -871,6 +879,7 @@ bool QDB2Result::exec()
 
 bool QDB2Result::fetch(int i)
 {
+    Q_D(QDB2Result);
     if (isForwardOnly() && i < at())
         return false;
     if (i == at())
@@ -905,6 +914,7 @@ bool QDB2Result::fetch(int i)
 
 bool QDB2Result::fetchNext()
 {
+    Q_D(QDB2Result);
     SQLRETURN r;
     d->clearValueCache();
     r = SQLFetchScroll(d->hStmt,
@@ -922,6 +932,7 @@ bool QDB2Result::fetchNext()
 
 bool QDB2Result::fetchFirst()
 {
+    Q_D(QDB2Result);
     if (isForwardOnly() && at() != QSql::BeforeFirstRow)
         return false;
     if (isForwardOnly())
@@ -943,6 +954,7 @@ bool QDB2Result::fetchFirst()
 
 bool QDB2Result::fetchLast()
 {
+    Q_D(QDB2Result);
     d->clearValueCache();
 
     int i = at();
@@ -974,6 +986,7 @@ bool QDB2Result::fetchLast()
 
 QVariant QDB2Result::data(int field)
 {
+    Q_D(QDB2Result);
     if (field >= d->recInf.count()) {
         qWarning("QDB2Result::data: column %d out of range", field);
         return QVariant();
@@ -1080,6 +1093,7 @@ QVariant QDB2Result::data(int field)
 
 bool QDB2Result::isNull(int i)
 {
+    Q_D(const QDB2Result);
     if (i >= d->valueCache.size())
         return true;
 
@@ -1090,6 +1104,7 @@ bool QDB2Result::isNull(int i)
 
 int QDB2Result::numRowsAffected()
 {
+    Q_D(const QDB2Result);
     SQLINTEGER affectedRowCount = 0;
     SQLRETURN r = SQLRowCount(d->hStmt, &affectedRowCount);
     if (r == SQL_SUCCESS || r == SQL_SUCCESS_WITH_INFO)
@@ -1106,6 +1121,7 @@ int QDB2Result::size()
 
 QSqlRecord QDB2Result::record() const
 {
+    Q_D(const QDB2Result);
     if (isActive())
         return d->recInf;
     return QSqlRecord();
@@ -1113,6 +1129,7 @@ QSqlRecord QDB2Result::record() const
 
 bool QDB2Result::nextResult()
 {
+    Q_D(QDB2Result);
     setActive(false);
     setAt(QSql::BeforeFirstRow);
     d->recInf.clear();
@@ -1148,6 +1165,7 @@ void QDB2Result::virtual_hook(int id, void *data)
 
 void QDB2Result::detachFromResultSet()
 {
+    Q_D(QDB2Result);
     if (d->hStmt)
         SQLCloseCursor(d->hStmt);
 }
@@ -1309,8 +1327,7 @@ void QDB2Driver::close()
 
 QSqlResult *QDB2Driver::createResult() const
 {
-    Q_D(const QDB2Driver);
-    return new QDB2Result(this, d);
+    return new QDB2Result(this);
 }
 
 QSqlRecord QDB2Driver::record(const QString& tableName) const

src/sql/drivers/db2/qsql_db2_p.h
@@ -63,6 +63,8 @@ class Q_EXPORT_SQLDRIVER_DB2 QDB2Driver : public QSqlDriver
 {
     Q_DECLARE_PRIVATE(QDB2Driver)
     Q_OBJECT
+    friend class QDB2ResultPrivate;
+
 public:
     explicit QDB2Driver(QObject* parent = 0);
     QDB2Driver(Qt::HANDLE env, Qt::HANDLE con, QObject* parent = 0);

src/sql/drivers/ibase/qsql_ibase.cpp
@@ -345,7 +345,7 @@ class QIBaseResultPrivate;
 
 class QIBaseResult : public QSqlCachedResult
 {
-    friend class QIBaseResultPrivate;
+    Q_DECLARE_PRIVATE(QIBaseResult)
 
 public:
     explicit QIBaseResult(const QIBaseDriver* db);
@@ -361,20 +361,22 @@ protected:
     int size() Q_DECL_OVERRIDE;
     int numRowsAffected() Q_DECL_OVERRIDE;
     QSqlRecord record() const Q_DECL_OVERRIDE;
-
-private:
-    QIBaseResultPrivate* d;
 };
 
-class QIBaseResultPrivate
+class QIBaseResultPrivate: public QSqlCachedResultPrivate
 {
+    Q_DECLARE_PUBLIC(QIBaseResult)
+
 public:
-    QIBaseResultPrivate(QIBaseResult *d, const QIBaseDriver *ddb);
+    Q_DECLARE_SQLDRIVER_PRIVATE(QIBaseDriver)
+
+    QIBaseResultPrivate(QIBaseResult *q, const QIBaseDriver *drv);
     ~QIBaseResultPrivate() { cleanup(); }
 
     void cleanup();
     bool isError(const char *msg, QSqlError::ErrorType typ = QSqlError::UnknownError)
     {
+        Q_Q(QIBaseResult);
         QString imsg;
         ISC_LONG sqlcode;
         if (!getIBaseError(imsg, status, sqlcode, tc))
@@ -395,8 +397,6 @@ public:
     bool writeArray(int i, const QList<QVariant> &list);
 
 public:
-    QIBaseResult *q;
-    const QIBaseDriver *db;
     ISC_STATUS status[20];
     isc_tr_handle trans;
     //indicator whether we have a local transaction or a transaction on driver level
@@ -410,14 +410,22 @@ public:
 };
 
 
-QIBaseResultPrivate::QIBaseResultPrivate(QIBaseResult *d, const QIBaseDriver *ddb):
-    q(d), db(ddb), trans(0), stmt(0), ibase(ddb->d_func()->ibase), sqlda(0), inda(0), queryType(-1), tc(ddb->d_func()->tc)
+QIBaseResultPrivate::QIBaseResultPrivate(QIBaseResult *q, const QIBaseDriver *drv)
+    : QSqlCachedResultPrivate(q, drv),
+      trans(0),
+      localTransaction(!drv_d_func()->ibase),
+      stmt(0),
+      ibase(drv_d_func()->ibase),
+      sqlda(0),
+      inda(0),
+      queryType(-1),
+      tc(drv_d_func()->tc)
 {
-    localTransaction = (ddb->d_func()->ibase == 0);
 }
 
 void QIBaseResultPrivate::cleanup()
 {
+    Q_Q(QIBaseResult);
     commit();
     if (!localTransaction)
         trans = 0;
@@ -779,6 +787,7 @@ static char* createArrayBuffer(char *buffer, const QList<QVariant> &list,
 
 bool QIBaseResultPrivate::writeArray(int column, const QList<QVariant> &list)
 {
+    Q_Q(QIBaseResult);
     QString error;
     ISC_QUAD *arrayId = (ISC_QUAD*) inda->sqlvar[column].sqldata;
     ISC_ARRAY_DESC desc;
@@ -854,9 +863,9 @@ bool QIBaseResultPrivate::transaction()
 {
     if (trans)
         return true;
-    if (db->d_func()->trans) {
+    if (drv_d_func()->trans) {
         localTransaction = false;
-        trans = db->d_func()->trans;
+        trans = drv_d_func()->trans;
         return true;
     }
     localTransaction = true;
@@ -887,19 +896,18 @@ bool QIBaseResultPrivate::commit()
 
 //////////
 
-QIBaseResult::QIBaseResult(const QIBaseDriver* db):
-    QSqlCachedResult(db)
+QIBaseResult::QIBaseResult(const QIBaseDriver *db)
+    : QSqlCachedResult(*new QIBaseResultPrivate(this, db))
 {
-    d = new QIBaseResultPrivate(this, db);
 }
 
 QIBaseResult::~QIBaseResult()
 {
-    delete d;
 }
 
 bool QIBaseResult::prepare(const QString& query)
 {
+    Q_D(QIBaseResult);
 //     qDebug("prepare: %s", qPrintable(query));
     if (!driver() || !driver()->isOpen() || driver()->isOpenError())
         return false;
@@ -976,6 +984,7 @@ bool QIBaseResult::prepare(const QString& query)
 
 bool QIBaseResult::exec()
 {
+    Q_D(QIBaseResult);
     bool ok = true;
 
     if (!d->trans)
@@ -1113,6 +1122,7 @@ bool QIBaseResult::reset (const QString& query)
 
 bool QIBaseResult::gotoNext(QSqlCachedResult::ValueCache& row, int rowIdx)
 {
+    Q_D(QIBaseResult);
     ISC_STATUS stat = 0;
 
     // Stored Procedures are special - they populate our d->sqlda when executing,
@@ -1307,6 +1317,7 @@ int QIBaseResult::size()
 
 int QIBaseResult::numRowsAffected()
 {
+    Q_D(QIBaseResult);
     static char acCountInfo[] = {isc_info_sql_records};
     char cCountType;
     bool bIsProcedure = false;
@@ -1361,6 +1372,7 @@ int QIBaseResult::numRowsAffected()
 
 QSqlRecord QIBaseResult::record() const
 {
+    Q_D(const QIBaseResult);
     QSqlRecord rec;
     if (!isActive() || !d->sqlda)
         return rec;
@@ -1400,6 +1412,7 @@ QSqlRecord QIBaseResult::record() const
 
 QVariant QIBaseResult::handle() const
 {
+    Q_D(const QIBaseResult);
     return QVariant(qRegisterMetaType<isc_stmt_handle>("isc_stmt_handle"), &d->stmt);
 }
 

commit 02a730331e123d2db778c8af62d7c0e3098da813

qsql_sqlite: fix segfault in dtor of orphan result
d79ae904660af7a83dc42a389c2457a8c94020f7 accidentally removed the check
on the driver pointer before dereferencing it. This causes a segfault
when deleting a result object after its driver has already been deleted.
This situation can arise when a database is removed, explicitly or
probably also upon unloading the driver plugin, while related
results still exist. The problem affected
qttools/tests/auto/qhelpcontentmode.

Task-number: QTBUG-49836
Change-Id: I8ce8f8a5e27d787309ac2ff644b388e333d76435
Reviewed-by: Mark Brand <mabrand@mabrand.nl>

src/sql/drivers/sqlite/qsql_sqlite.cpp
@@ -351,7 +351,8 @@ QSQLiteResult::QSQLiteResult(const QSQLiteDriver* db)
 QSQLiteResult::~QSQLiteResult()
 {
     Q_D(QSQLiteResult);
-    const_cast<QSQLiteDriverPrivate*>(d->drv_d_func())->results.removeOne(this);
+    if (d->drv_d_func())
+        const_cast<QSQLiteDriverPrivate*>(d->drv_d_func())->results.removeOne(this);
     d->cleanup();
 }
 