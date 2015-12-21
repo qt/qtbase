qt/qtbase

commit ed0c0070f9b05c647019270dfc42073d071c830a

Introduce qt_subtract_from_timeout to reduce code duplication.
The same qt_timeout_value function was copied 5 times in qtbase's code,
so provide a common implementation in QIoDevice that can be used by
everyone.

This commit also corrects the remaining time calculation in
QProcess::waitForBytesWritten and QProcess::waitForFinished by using
this new function.

For QProcess::waitForFinished, if the process started within almost exactly
the timeout time passed to waitForFinished, msecs - stopWatch.elapsed() would
be -1, which is a special value.

Change-Id: I7b76ee6bae695eafdd02e3db03e2ff1e23a7f40c
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

src/corelib/io/qwinoverlappedionotifier.cpp
@@ -41,6 +41,7 @@
 #include <qthread.h>
 #include <qt_windows.h>
 #include <private/qobject_p.h>
+#include <private/qiodevice_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -332,11 +333,9 @@ bool QWinOverlappedIoNotifier::waitForNotified(int msecs, OVERLAPPED *overlapped
             return false;
         if (triggeredOverlapped == overlapped)
             return true;
-        if (msecs != -1) {
-            t = msecs - stopWatch.elapsed();
-            if (t < 0)
-                return false;
-        }
+        msecs = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
+        if (t == 0)
+            return false;
     }
 }
 

tests/auto/other/networkselftest/tst_networkselftest.cpp
@@ -34,6 +34,7 @@
 #include <QtTest/QtTest>
 #include <QtNetwork/QtNetwork>
 #include <QtCore/QDateTime>
+#include <QtCore/private/qiodevice_p.h>
 
 #ifndef QT_NO_BEARERMANAGEMENT
 #include <QtNetwork/qnetworkconfigmanager.h>
@@ -171,10 +172,11 @@ static bool doSocketRead(QTcpSocket *socket, int minBytesAvailable, int timeout 
     forever {
         if (socket->bytesAvailable() >= minBytesAvailable)
             return true;
+        timeout = qt_subtract_from_timeout(timeout, timer.elapsed());
         if (socket->state() == QAbstractSocket::UnconnectedState
-            || timer.elapsed() >= timeout)
+            || timeout == 0)
             return false;
-        if (!socket->waitForReadyRead(timeout - timer.elapsed()))
+        if (!socket->waitForReadyRead(timeout))
             return false;
     }
 }
@@ -202,10 +204,11 @@ static bool doSocketFlush(QTcpSocket *socket, int timeout = 4000)
 #endif
             )
             return true;
+        timeout = qt_subtract_from_timeout(timeout, timer.elapsed());
         if (socket->state() == QAbstractSocket::UnconnectedState
-            || timer.elapsed() >= timeout)
+            || timeout == 0)
             return false;
-        if (!socket->waitForBytesWritten(timeout - timer.elapsed()))
+        if (!socket->waitForBytesWritten(timeout))
             return false;
     }
 }

src/corelib/io/qiodevice.cpp
@@ -1667,6 +1667,23 @@ QString QIODevice::errorString() const
     \sa read(), write()
 */
 
+/*!
+  \internal
+  \fn int qt_subtract_from_timeout(int timeout, int elapsed)
+
+  Reduces the \a timeout by \a elapsed, taking into account that -1 is a
+  special value for timeouts.
+*/
+
+int qt_subtract_from_timeout(int timeout, int elapsed)
+{
+    if (timeout == -1)
+        return -1;
+
+    timeout = timeout - elapsed;
+    return timeout < 0 ? 0 : timeout;
+}
+
 
 #if !defined(QT_NO_DEBUG_STREAM)
 QDebug operator<<(QDebug debug, QIODevice::OpenMode modes)

src/corelib/io/qiodevice_p.h
@@ -60,6 +60,8 @@ QT_BEGIN_NAMESPACE
 #define QIODEVICE_BUFFERSIZE Q_INT64_C(16384)
 #endif
 
+Q_CORE_EXPORT int qt_subtract_from_timeout(int timeout, int elapsed);
+
 // This is QIODevice's read buffer, optimized for read(), isEmpty() and getChar()
 class QIODevicePrivateLinearBuffer
 {

src/corelib/io/qprocess.cpp
@@ -1793,8 +1793,7 @@ bool QProcess::waitForBytesWritten(int msecs)
         bool started = waitForStarted(msecs);
         if (!started)
             return false;
-        if (msecs != -1)
-            msecs -= stopWatch.elapsed();
+        msecs = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
     }
 
     return d->waitForBytesWritten(msecs);
@@ -1830,8 +1829,7 @@ bool QProcess::waitForFinished(int msecs)
         bool started = waitForStarted(msecs);
         if (!started)
             return false;
-        if (msecs != -1)
-            msecs -= stopWatch.elapsed();
+        msecs = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
     }
 
     return d->waitForFinished(msecs);

src/corelib/io/qprocess_unix.cpp
@@ -1019,19 +1019,6 @@ void QProcessPrivate::killProcess()
         ::kill(pid_t(pid), SIGKILL);
 }
 
-/*
-   Returns the difference between msecs and elapsed. If msecs is -1,
-   however, -1 is returned.
-*/
-static int qt_timeout_value(int msecs, int elapsed)
-{
-    if (msecs == -1)
-        return -1;
-
-    int timeout = msecs - elapsed;
-    return timeout < 0 ? 0 : timeout;
-}
-
 bool QProcessPrivate::waitForStarted(int msecs)
 {
     Q_Q(QProcess);
@@ -1106,7 +1093,7 @@ bool QProcessPrivate::waitForReadyRead(int msecs)
         if (!stdinChannel.buffer.isEmpty() && stdinChannel.pipe[1] != -1)
             add_fd(nfds, stdinChannel.pipe[1], &fdwrite);
 
-        int timeout = qt_timeout_value(msecs, stopWatch.elapsed());
+        int timeout = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
 #ifdef Q_OS_BLACKBERRY
         int ret = bb_select(notifiers, nfds + 1, &fdread, &fdwrite, timeout);
 #else
@@ -1187,7 +1174,7 @@ bool QProcessPrivate::waitForBytesWritten(int msecs)
         if (!stdinChannel.buffer.isEmpty() && stdinChannel.pipe[1] != -1)
             add_fd(nfds, stdinChannel.pipe[1], &fdwrite);
 
-        int timeout = qt_timeout_value(msecs, stopWatch.elapsed());
+        int timeout = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
 #ifdef Q_OS_BLACKBERRY
         int ret = bb_select(notifiers, nfds + 1, &fdread, &fdwrite, timeout);
 #else
@@ -1262,7 +1249,7 @@ bool QProcessPrivate::waitForFinished(int msecs)
         if (!stdinChannel.buffer.isEmpty() && stdinChannel.pipe[1] != -1)
             add_fd(nfds, stdinChannel.pipe[1], &fdwrite);
 
-        int timeout = qt_timeout_value(msecs, stopWatch.elapsed());
+        int timeout = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
 #ifdef Q_OS_BLACKBERRY
         int ret = bb_select(notifiers, nfds + 1, &fdread, &fdwrite, timeout);
 #else

src/network/socket/qabstractsocket.cpp
@@ -1976,20 +1976,6 @@ QVariant QAbstractSocket::socketOption(QAbstractSocket::SocketOption option)
         return QVariant(ret);
 }
 
-
-/*
-   Returns the difference between msecs and elapsed. If msecs is -1,
-   however, -1 is returned.
-*/
-static int qt_timeout_value(int msecs, int elapsed)
-{
-    if (msecs == -1)
-        return -1;
-
-    int timeout = msecs - elapsed;
-    return timeout < 0 ? 0 : timeout;
-}
-
 /*!
     Waits until the socket is connected, up to \a msecs
     milliseconds. If the connection has been established, this
@@ -2067,7 +2053,7 @@ bool QAbstractSocket::waitForConnected(int msecs)
     int attempt = 1;
 #endif
     while (state() == ConnectingState && (msecs == -1 || stopWatch.elapsed() < msecs)) {
-        int timeout = qt_timeout_value(msecs, stopWatch.elapsed());
+        int timeout = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
         if (msecs != -1 && timeout > QT_CONNECT_TIMEOUT)
             timeout = QT_CONNECT_TIMEOUT;
 #if defined (QABSTRACTSOCKET_DEBUG)
@@ -2146,7 +2132,7 @@ bool QAbstractSocket::waitForReadyRead(int msecs)
         bool readyToRead = false;
         bool readyToWrite = false;
         if (!d->socketEngine->waitForReadOrWrite(&readyToRead, &readyToWrite, true, !d->writeBuffer.isEmpty(),
-                                               qt_timeout_value(msecs, stopWatch.elapsed()))) {
+                                               qt_subtract_from_timeout(msecs, stopWatch.elapsed()))) {
             d->socketError = d->socketEngine->error();
             setErrorString(d->socketEngine->errorString());
 #if defined (QABSTRACTSOCKET_DEBUG)
@@ -2169,7 +2155,7 @@ bool QAbstractSocket::waitForReadyRead(int msecs)
 
         if (state() != ConnectedState)
             return false;
-    } while (msecs == -1 || qt_timeout_value(msecs, stopWatch.elapsed()) > 0);
+    } while (msecs == -1 || qt_subtract_from_timeout(msecs, stopWatch.elapsed()) > 0);
     return false;
 }
 
@@ -2218,7 +2204,7 @@ bool QAbstractSocket::waitForBytesWritten(int msecs)
         bool readyToRead = false;
         bool readyToWrite = false;
         if (!d->socketEngine->waitForReadOrWrite(&readyToRead, &readyToWrite, true, !d->writeBuffer.isEmpty(),
-                                               qt_timeout_value(msecs, stopWatch.elapsed()))) {
+                                               qt_subtract_from_timeout(msecs, stopWatch.elapsed()))) {
             d->socketError = d->socketEngine->error();
             setErrorString(d->socketEngine->errorString());
 #if defined (QABSTRACTSOCKET_DEBUG)
@@ -2300,7 +2286,7 @@ bool QAbstractSocket::waitForDisconnected(int msecs)
         bool readyToWrite = false;
         if (!d->socketEngine->waitForReadOrWrite(&readyToRead, &readyToWrite, state() == ConnectedState,
                                                !d->writeBuffer.isEmpty(),
-                                               qt_timeout_value(msecs, stopWatch.elapsed()))) {
+                                               qt_subtract_from_timeout(msecs, stopWatch.elapsed()))) {
             d->socketError = d->socketEngine->error();
             setErrorString(d->socketEngine->errorString());
 #if defined (QABSTRACTSOCKET_DEBUG)

src/network/socket/qhttpsocketengine.cpp
@@ -36,6 +36,7 @@
 #include "qhostaddress.h"
 #include "qurl.h"
 #include "private/qhttpnetworkreply_p.h"
+#include "private/qiodevice_p.h"
 #include "qelapsedtimer.h"
 #include "qnetworkinterface.h"
 
@@ -330,19 +331,6 @@ bool QHttpSocketEngine::setOption(SocketOption option, int value)
     return false;
 }
 
-/*
-   Returns the difference between msecs and elapsed. If msecs is -1,
-   however, -1 is returned.
-*/
-static int qt_timeout_value(int msecs, int elapsed)
-{
-    if (msecs == -1)
-        return -1;
-
-    int timeout = msecs - elapsed;
-    return timeout < 0 ? 0 : timeout;
-}
-
 bool QHttpSocketEngine::waitForRead(int msecs, bool *timedOut)
 {
     Q_D(const QHttpSocketEngine);
@@ -355,7 +343,7 @@ bool QHttpSocketEngine::waitForRead(int msecs, bool *timedOut)
 
     // Wait for more data if nothing is available.
     if (!d->socket->bytesAvailable()) {
-        if (!d->socket->waitForReadyRead(qt_timeout_value(msecs, stopWatch.elapsed()))) {
+        if (!d->socket->waitForReadyRead(qt_subtract_from_timeout(msecs, stopWatch.elapsed()))) {
             if (d->socket->state() == QAbstractSocket::UnconnectedState)
                 return true;
             setError(d->socket->error(), d->socket->errorString());
@@ -367,7 +355,7 @@ bool QHttpSocketEngine::waitForRead(int msecs, bool *timedOut)
 
     // If we're not connected yet, wait until we are, or until an error
     // occurs.
-    while (d->state != Connected && d->socket->waitForReadyRead(qt_timeout_value(msecs, stopWatch.elapsed()))) {
+    while (d->state != Connected && d->socket->waitForReadyRead(qt_subtract_from_timeout(msecs, stopWatch.elapsed()))) {
         // Loop while the protocol handshake is taking place.
     }
 
@@ -403,7 +391,7 @@ bool QHttpSocketEngine::waitForWrite(int msecs, bool *timedOut)
     // If we're not connected yet, wait until we are, and until bytes have
     // been received (i.e., the socket has connected, we have sent the
     // greeting, and then received the response).
-    while (d->state != Connected && d->socket->waitForReadyRead(qt_timeout_value(msecs, stopWatch.elapsed()))) {
+    while (d->state != Connected && d->socket->waitForReadyRead(qt_subtract_from_timeout(msecs, stopWatch.elapsed()))) {
         // Loop while the protocol handshake is taking place.
     }
 

commit e96fa5a780665e24fe4710868e399b3216a5d3b3

Fix timeout calculations using qt_subtract_from_timeout
Commit ed0c0070 introduced qt_subtract_from_timeout but used it
incorrectly in several places.

Change-Id: I80ea16088707929a45d5a61ec6f3370f8e63d1cd
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

src/corelib/io/qwinoverlappedionotifier.cpp
@@ -364,7 +364,7 @@ bool QWinOverlappedIoNotifier::waitForNotified(int msecs, OVERLAPPED *overlapped
             return false;
         if (triggeredOverlapped == overlapped)
             return true;
-        msecs = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
+        t = qt_subtract_from_timeout(msecs, stopWatch.elapsed());
         if (t == 0)
             return false;
     }

tests/auto/other/networkselftest/tst_networkselftest.cpp
@@ -169,14 +169,16 @@ static bool doSocketRead(QTcpSocket *socket, int minBytesAvailable, int timeout 
 {
     QElapsedTimer timer;
     timer.start();
+    int t = timeout;
     forever {
         if (socket->bytesAvailable() >= minBytesAvailable)
             return true;
-        timeout = qt_subtract_from_timeout(timeout, timer.elapsed());
-        if (socket->state() == QAbstractSocket::UnconnectedState
-            || timeout == 0)
+        if (socket->state() == QAbstractSocket::UnconnectedState)
             return false;
-        if (!socket->waitForReadyRead(timeout))
+        if (!socket->waitForReadyRead(t))
+            return false;
+        t = qt_subtract_from_timeout(timeout, timer.elapsed());
+        if (t == 0)
             return false;
     }
 }
@@ -197,6 +199,7 @@ static bool doSocketFlush(QTcpSocket *socket, int timeout = 4000)
 #endif
     QTime timer;
     timer.start();
+    int t = timeout;
     forever {
         if (socket->bytesToWrite() == 0
 #ifndef QT_NO_SSL
@@ -204,11 +207,12 @@ static bool doSocketFlush(QTcpSocket *socket, int timeout = 4000)
 #endif
             )
             return true;
-        timeout = qt_subtract_from_timeout(timeout, timer.elapsed());
-        if (socket->state() == QAbstractSocket::UnconnectedState
-            || timeout == 0)
+        if (socket->state() == QAbstractSocket::UnconnectedState)
             return false;
-        if (!socket->waitForBytesWritten(timeout))
+        if (!socket->waitForBytesWritten(t))
+            return false;
+        t = qt_subtract_from_timeout(timeout, timer.elapsed());
+        if (t == 0)
             return false;
     }
 }