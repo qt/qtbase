a62132b22c77c869d8f8e1e48d197bcce53e50a6
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2016-01-06 09:11:23 +0100

    Diaglib: Output more information about QWindow.
    Output surface class/type, screen name and scaling factors.
    
    Task-number: QTBUG-50206
    Change-Id: I88d3494962b29985cd9e492c1761361b73e0172e
    Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>
    Reviewed-by: Oliver Wolff <oliver.wolff@theqtcompany.com>

--- tests/manual/diaglib/qwindowdump.cpp
+++ tests/manual/diaglib/qwindowdump.cpp
@@ -38,6 +38,9 @@
 #  include <QtGui/QScreen>
 #  include <QtGui/QWindow>
 #  include <qpa/qplatformwindow.h>
+#  if QT_VERSION >= 0x050600
+#    include <private/qhighdpiscaling_p.h>
+#  endif
 #endif
 #include <QtCore/QMetaObject>
 #include <QtCore/QRect>
@@ -131,9 +134,19 @@ void formatWindow(QTextStream &str, const QWindow *w, FormatWindowOptions option
         str << "[top] ";
     if (w->isExposed())
         str << "[exposed] ";
+    if (w->surfaceClass() == QWindow::Offscreen)
+        str << "[offscreen] ";
+    str << "surface=" << w->surfaceType() << ' ';
     if (const Qt::WindowState state = w->windowState())
         str << "windowState=" << state << ' ';
     formatRect(str, w->geometry());
+    if (w->isTopLevel()) {
+        str << " \"" << w->screen()->name() << "\" ";
+#if QT_VERSION >= 0x050600
+        if (QHighDpiScaling::isActive())
+            str << "factor=" << QHighDpiScaling::factor(w) << ' ';
+#endif
+    }
     if (!(options & DontPrintWindowFlags)) {
         str << ' ';
         formatWindowFlags(str, w->flags());
