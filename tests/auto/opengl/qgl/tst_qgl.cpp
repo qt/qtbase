a6991376cc4c7084a151272d7e27a5140597cc9e
Morten Johan Sørvig <morten.sorvig@qt.io>
2016-01-21 10:49:22 +0100

    Wait for window expose on the viewport widget
    The child viewport window covers the entire client
    area of the parent window, which prevents the parent
    window from being exposed.
    
    This worked by accident until now, and was uncovered
    by changes to the cocoa platform plugin implementation.
    
    Change-Id: I0d62a1a33f125e5a9030c8def452a49886e54956
    Task-number: QTBUG-50414
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- tests/auto/opengl/qgl/tst_qgl.cpp
+++ tests/auto/opengl/qgl/tst_qgl.cpp
@@ -868,7 +868,7 @@ void tst_QGL::graphicsViewClipping()
 
     scene.setSceneRect(view.viewport()->rect());
 
-    QVERIFY(QTest::qWaitForWindowExposed(&view));
+    QVERIFY(QTest::qWaitForWindowExposed(view.viewport()));
     #ifdef Q_OS_MAC
         // The black rectangle jumps from the center to the upper left for some reason.
         QTest::qWait(100);
