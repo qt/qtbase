qt/qtbase

commit e3689949ba5b23decb0ea85741a3f4829696788e

Android: Remove support for API < 16
Starting with Qt 5.7 we removed support for API < 16 (Android v4.1)

[ChangeLog][Android] Support for Android API < 16 was removed.

Change-Id: I8bf396a9d70ab0996965e0c9f629800aa1fa6a45
Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@theqtcompany.com>

src/plugins/platforms/android/androidjnimenu.cpp
@@ -87,14 +87,7 @@ namespace QtAndroidMenu
     void showContextMenu(QAndroidPlatformMenu *menu, const QRect &anchorRect, JNIEnv *env)
     {
         QMutexLocker lock(&visibleMenuMutex);
-        if (QtAndroidPrivate::androidSdkVersion() > 10 &&
-                QtAndroidPrivate::androidSdkVersion() < 14 &&
-                anchorRect.isValid()) {
-            pendingContextMenus.clear();
-        } else if (visibleMenu) {
-            pendingContextMenus.append(visibleMenu);
-        }
-
+        pendingContextMenus.append(visibleMenu);
         visibleMenu = menu;
         menu->aboutToShow();
         env->CallStaticVoidMethod(applicationClass(), openContextMenuMethodID, anchorRect.x(), anchorRect.y(), anchorRect.width(), anchorRect.height());

src/android/accessibility/accessibility.pro
@@ -1,2 +1,0 @@
-TEMPLATE      = subdirs
-SUBDIRS       = jar

src/android/accessibility/jar/AndroidManifest.xml
@@ -1,7 +1,0 @@
-<?xml version='1.0' encoding='utf-8'?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    android:versionCode="1"
-    android:versionName="1.0"
-    package="org.qtproject.qt5.android.accessibility">
-    <supports-screens android:largeScreens="true" android:normalScreens="true" android:anyDensity="true" android:smallScreens="true"/>
-</manifest>

src/android/accessibility/jar/bundledjar.pro
@@ -1,3 +1,0 @@
-TARGET = QtAndroidAccessibility-bundled
-CONFIG += bundled_jar_file
-include(jar.pri)

src/android/accessibility/jar/distributedjar.pro
@@ -1,2 +1,0 @@
-TARGET = QtAndroidAccessibility
-include(jar.pri)

src/android/accessibility/jar/jar.pri
@@ -1,15 +1,0 @@
-CONFIG += java
-DESTDIR = $$[QT_INSTALL_PREFIX/get]/jar
-API_VERSION = android-16
-
-PATHPREFIX = $$PWD/src/org/qtproject/qt5/android/accessibility
-
-JAVACLASSPATH += $$PWD/src/ \
-                 $$DESTDIR/QtAndroid-bundled.jar
-JAVASOURCES += \
-    $$PATHPREFIX/QtAccessibilityDelegate.java \
-    $$PATHPREFIX/QtNativeAccessibility.java
-
-# install
-target.path = $$[QT_INSTALL_PREFIX]/jar
-INSTALLS += target

src/android/accessibility/jar/jar.pro
@@ -1,2 +1,0 @@
-TEMPLATE = subdirs
-SUBDIRS += bundledjar.pro distributedjar.pro

src/android/android.pro
@@ -1,3 +1,3 @@
 TEMPLATE      = subdirs
 CONFIG        += ordered
-SUBDIRS       = jar java templates accessibility
+SUBDIRS       = jar java templates

commit 7696ff89c9a9cc8f738e3134bfcab831ff314968

Android: Fix crash, don't add null visible menus to pendingContextMenus
Regression introduced in e3689949ba5b23decb0ea85741a3f4829696788e

Change-Id: I9d3e37733d933e037f04d3a6efaf05607e16d9c5
Reviewed-by: Christian Stromme <christian.stromme@theqtcompany.com>

src/plugins/platforms/android/androidjnimenu.cpp
@@ -93,7 +93,8 @@ namespace QtAndroidMenu
     void showContextMenu(QAndroidPlatformMenu *menu, const QRect &anchorRect, JNIEnv *env)
     {
         QMutexLocker lock(&visibleMenuMutex);
-        pendingContextMenus.append(visibleMenu);
+        if (visibleMenu)
+            pendingContextMenus.append(visibleMenu);
         visibleMenu = menu;
         menu->aboutToShow();
         env->CallStaticVoidMethod(applicationClass(), openContextMenuMethodID, anchorRect.x(), anchorRect.y(), anchorRect.width(), anchorRect.height());