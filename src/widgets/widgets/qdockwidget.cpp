qt/qtbase

commit 75b705fec8e9517047d7dfa98203edff69f2bf8a

End the drag if the dockwidget is being hidden
When the window flags are changed for the widget then it is possible to
minimize the dockwidget via the titlebar. This will cause it to be ready
to start a drag internally but since the dockwidget never gets a mouse
release event it doesn't end it. Therefore it ends up being in an invalid
state later on when restored, so the endDrag() needs to happen in the hide
event to ensure this is not an issue later on.

Change-Id: Ia84bee96b9eed49896869e6a15d4de6d01964264
Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>

src/widgets/widgets/qdockwidget.cpp
@@ -1445,6 +1445,8 @@ bool QDockWidget::event(QEvent *event)
     switch (event->type()) {
 #ifndef QT_NO_ACTION
     case QEvent::Hide:
+        if (d->state)
+            d->endDrag(true);
         if (layout != 0)
             layout->keepSize(this);
         d->toggleViewAction->setChecked(false);

commit 798e0064e9be78f8320ff25a9af50d1b5e5badb1

Fix crash introduced by 75b705fec8e9517047d7dfa98203edff69f2bf8a
This fixes the crash introduced by
75b705fec8e9517047d7dfa98203edff69f2bf8a. The drag only needs to be ended
on the hide if it is actually in the middle of dragging. This enables the
case of dragging the dock widget out with the mouse to continue to work.

Task-number: QTBUG-50890
Change-Id: I72309dd40ee670319f2ff607ae201c46f8de4652
Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>

src/widgets/widgets/qdockwidget.cpp
@@ -1445,7 +1445,7 @@ bool QDockWidget::event(QEvent *event)
     switch (event->type()) {
 #ifndef QT_NO_ACTION
     case QEvent::Hide:
-        if (d->state)
+        if (d->state && d->state->dragging)
             d->endDrag(true);
         if (layout != 0)
             layout->keepSize(this);