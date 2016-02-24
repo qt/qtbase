b6f84ea2aa13b22ceb2a2f2c12d7a9d9c86742c0
Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
2016-02-24 12:50:05 +0100

    Bump version
    Change-Id: Ie10f5c3a922b810b2f5b09f6ebb65aae918d30f1

--- src/corelib/io/qdatastream.h
+++ src/corelib/io/qdatastream.h
@@ -91,10 +91,11 @@ public:
         Qt_5_5 = Qt_5_4,
         Qt_5_6 = 17,
         Qt_5_7 = Qt_5_6,
-#if QT_VERSION >= 0x050800
+        Qt_5_8 = Qt_5_7,
+#if QT_VERSION >= 0x050900
 #error Add the datastream version for this Qt version and update Qt_DefaultCompiledVersion
 #endif
-        Qt_DefaultCompiledVersion = Qt_5_7
+        Qt_DefaultCompiledVersion = Qt_5_8
     };
 
     enum ByteOrder {
