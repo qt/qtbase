28fab275033a6b4f8d6e78da0f729837144b0420
Shawn Rutledge <shawn.rutledge@theqtcompany.com>
2016-04-22 15:30:26 +0200

    add AA_CompressHighFrequencyEvents to control compression on xcb
    7edd10e6c added this compression feature, but some applications may
    need to disable it.
    
    [ChangeLog][X11] It's now possible to unset AA_CompressHighFrequencyEvents
    to disable the new X event compression feature that was added in 5.6.0.
    This is a replacement for the WA_NoX11EventCompression flag in Qt 4.
    
    Task-number: QTBUG-44964
    Change-Id: I37a9c8a4831f1c02eda0f03b54125f3255d25500
    Reviewed-by: Gatis Paeglis <gatis.paeglis@qt.io>

--- src/corelib/global/qnamespace.h
+++ src/corelib/global/qnamespace.h
@@ -508,6 +508,7 @@ public:
         AA_UseStyleSheetPropagationInWidgetStyles = 22, // ### Qt 6: remove me
         AA_DontUseNativeDialogs = 23,
         AA_SynthesizeMouseForUnhandledTabletEvents = 24,
+        AA_CompressHighFrequencyEvents = 25,
 
         // Add new attributes before this line
         AA_AttributeCount
