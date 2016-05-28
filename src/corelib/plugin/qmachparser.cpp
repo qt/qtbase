e8224f814989358941c390efb0c058b94ff65b54
Jake Petroules <jake.petroules@qt.io>
2016-05-28 02:06:00 -0700

    Fix unimplemented Mach-O parsing for ARM64 platforms in QPluginLoader.
    Change-Id: I4853b5ce1691bd84578ebe46af9f73270598387a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/plugin/qmachparser.cpp
+++ src/corelib/plugin/qmachparser.cpp
@@ -59,6 +59,9 @@ static const cpu_type_t my_cputype = CPU_TYPE_X86;
 static const cpu_type_t my_cputype = CPU_TYPE_POWERPC64;
 #elif defined(Q_PROCESSOR_POWER_32)
 static const cpu_type_t my_cputype = CPU_TYPE_POWERPC;
+#elif defined(Q_PROCESSOR_ARM_64)
+#  define MACHO64
+static const cpu_type_t my_cputype = CPU_TYPE_ARM64;
 #elif defined(Q_PROCESSOR_ARM)
 static const cpu_type_t my_cputype = CPU_TYPE_ARM;
 #else
