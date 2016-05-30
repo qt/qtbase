3cc2884c031e0ab1098f59fae9aa6e05f54d36f8
Olivier Blin <olivier.blin@softathome.com>
2016-05-30 11:16:19 +0200

    Fix defining QGuiApplication::sync() when QT_NO_SESSIONMANAGER is set
    sync() is unconditionally declared in the QGuiApplication header, but
    the definition was under QT_NO_SESSIONMANAGER guards.
    
    This commit moves the definition of sync() outside of
    the QT_NO_SESSIONMANAGER guards, since the sync code has nothing to do
    with session management.
    
    [ChangeLog][QtGui][QGuiApplication] Fixed a bug that would cause
    QGuiApplication::sync() to be left undefined for Qt builds without
    session management support.
    
    Change-Id: Ieb46f7c90c9193e89469126170117d9df672f4cb
    Task-number: QTBUG-51703
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/gui/kernel/qguiapplication.cpp
+++ src/gui/kernel/qguiapplication.cpp
@@ -3275,28 +3275,6 @@ bool QGuiApplication::isSavingSession() const
     return d->is_saving_session;
 }
 
-/*!
-    \since 5.2
-
-    Function that can be used to sync Qt state with the Window Systems state.
-
-    This function will first empty Qts events by calling QCoreApplication::processEvents(),
-    then the platform plugin will sync up with the windowsystem, and finally Qts events
-    will be delived by another call to QCoreApplication::processEvents();
-
-    This function is timeconsuming and its use is discouraged.
-*/
-void QGuiApplication::sync()
-{
-    QCoreApplication::processEvents();
-    if (QGuiApplicationPrivate::platform_integration
-            && QGuiApplicationPrivate::platform_integration->hasCapability(QPlatformIntegration::SyncState)) {
-        QGuiApplicationPrivate::platform_integration->sync();
-        QCoreApplication::processEvents();
-        QWindowSystemInterface::flushWindowSystemEvents();
-    }
-}
-
 void QGuiApplicationPrivate::commitData()
 {
     Q_Q(QGuiApplication);
@@ -3320,6 +3298,28 @@ void QGuiApplicationPrivate::saveState()
     is_saving_session = false;
 }
 #endif //QT_NO_SESSIONMANAGER
+
+/*!
+    \since 5.2
+
+    Function that can be used to sync Qt state with the Window Systems state.
+
+    This function will first empty Qts events by calling QCoreApplication::processEvents(),
+    then the platform plugin will sync up with the windowsystem, and finally Qts events
+    will be delived by another call to QCoreApplication::processEvents();
+
+    This function is timeconsuming and its use is discouraged.
+*/
+void QGuiApplication::sync()
+{
+    QCoreApplication::processEvents();
+    if (QGuiApplicationPrivate::platform_integration
+            && QGuiApplicationPrivate::platform_integration->hasCapability(QPlatformIntegration::SyncState)) {
+        QGuiApplicationPrivate::platform_integration->sync();
+        QCoreApplication::processEvents();
+        QWindowSystemInterface::flushWindowSystemEvents();
+    }
+}
 
 /*!
     \property QGuiApplication::layoutDirection
