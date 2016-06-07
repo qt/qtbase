df3f685826d0baf8b68e9a01ee44749abbac8ecf
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-06-07 11:18:00 +0200

    tst_QFileSystemModel: Remove Win32LongFileName().
    Modern SSD drives no longer have short file names enabled, causing
    the test to fail.
    
    Task-number: QTBUG-29403
    Change-Id: I2e9866d8f8a6ed3df9d2dc6630b8cfa47ade0728
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- tests/auto/widgets/dialogs/qfilesystemmodel/tst_qfilesystemmodel.cpp
+++ tests/auto/widgets/dialogs/qfilesystemmodel/tst_qfilesystemmodel.cpp
@@ -109,10 +109,6 @@ private slots:
 
     void caseSensitivity();
 
-#if defined(Q_OS_WIN) && !defined(Q_OS_WINCE)
-    void Win32LongFileName();
-#endif
-
     void drives_data();
     void drives();
     void dirsBeforeFiles();
@@ -931,20 +927,6 @@ void tst_QFileSystemModel::caseSensitivity()
         QVERIFY(model->index(tmp + '/' + files.at(i)).isValid());
     }
 }
-
-#if defined(Q_OS_WIN) && !defined(Q_OS_WINCE)
-void tst_QFileSystemModel::Win32LongFileName()
-{
-    QString tmp = flatDirTestPath;
-    QStringList files;
-    files << "aaaaaaaaaa" << "bbbbbbbbbb" << "cccccccccc";
-    QVERIFY(createFiles(tmp, files));
-    QModelIndex root = model->setRootPath(tmp);
-    QTRY_VERIFY(model->index(tmp + QLatin1String("/aaaaaa~1")).isValid());
-    QTRY_VERIFY(model->index(tmp + QLatin1String("/bbbbbb~1")).isValid());
-    QTRY_VERIFY(model->index(tmp + QLatin1String("/cccccc~1")).isValid());
-}
-#endif
 
 void tst_QFileSystemModel::drives_data()
 {
