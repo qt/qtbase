qt/qtbase

commit 88ebee5cd7fe2416775b12b25498ac86ac4d9af8

uikit: Remove use of ifdef when resolving available screen geometry
More explicit that we're falling back to m_geometry on non-iOS platforms.

Change-Id: I09bd5270aee5edb977983893a8407218eea0f74a
Reviewed-by: Jake Petroules <jake.petroules@qt.io>

src/plugins/platforms/ios/qiosscreen.mm
@@ -232,11 +232,8 @@ void QIOSScreen::updateProperties()
     QRect previousAvailableGeometry = m_availableGeometry;
 
     m_geometry = QRectF::fromCGRect(m_uiScreen.bounds).toRect();
-#ifndef Q_OS_TVOS
-    m_availableGeometry = QRectF::fromCGRect(m_uiScreen.applicationFrame).toRect();
-#else
-    m_availableGeometry = QRectF::fromCGRect(m_uiScreen.bounds).toRect();
-#endif
+    m_availableGeometry = QSysInfo::MacintoshVersion & QSysInfo::MV_IOS ?
+        QRectF::fromCGRect(m_uiScreen.applicationFrame).toRect() : m_geometry;
 
 #ifndef Q_OS_TVOS
     if (m_uiScreen == [UIScreen mainScreen]) {

commit d23c2e005ff525e2274f871ac9433c04cda9f849

Restore use of ifdef for TVOS
UIScreen.applicationFrame is not defined at all on tvOS.
Was broken by 88ebee5cd7fe2416775b12b25498ac86ac4d9af8

Change-Id: I692f464d538561961b40a6fbdcab51a3320e5779
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@theqtcompany.com>

src/plugins/platforms/ios/qiosscreen.mm
@@ -232,8 +232,11 @@ void QIOSScreen::updateProperties()
     QRect previousAvailableGeometry = m_availableGeometry;
 
     m_geometry = QRectF::fromCGRect(m_uiScreen.bounds).toRect();
-    m_availableGeometry = QSysInfo::MacintoshVersion & QSysInfo::MV_IOS ?
-        QRectF::fromCGRect(m_uiScreen.applicationFrame).toRect() : m_geometry;
+#ifdef Q_OS_TVOS
+    m_availableGeometry = m_geometry;
+#else
+    m_availableGeometry = QRectF::fromCGRect(m_uiScreen.applicationFrame).toRect();
+#endif
 
 #ifndef Q_OS_TVOS
     if (m_uiScreen == [UIScreen mainScreen]) {