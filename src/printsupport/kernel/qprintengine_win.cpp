c3f645a24ac743e66f4345058983ad877f723a14
Andy Shaw <andy.shaw@theqtcompany.com>
2016-06-23 09:44:58 +0200

    Windows: Add support for the PdmDevicePixelRatioScaled metric
    Change-Id: If65018a86e386a54f40ee12e81b5137e3412d655
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Andy Shaw <andy.shaw@qt.io>

--- src/printsupport/kernel/qprintengine_win.cpp
+++ src/printsupport/kernel/qprintengine_win.cpp
@@ -388,6 +388,9 @@ int QWin32PrintEngine::metric(QPaintDevice::PaintDeviceMetric m) const
     case QPaintDevice::PdmDevicePixelRatio:
         val = 1;
         break;
+    case QPaintDevice::PdmDevicePixelRatioScaled:
+        val = 1 * QPaintDevice::devicePixelRatioFScale();
+        break;
     default:
         qWarning("QPrinter::metric: Invalid metric command");
         return 0;
