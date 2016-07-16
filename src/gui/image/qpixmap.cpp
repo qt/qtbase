65bad0d0f4e2ac14e7f439659c500c1d2c6817bc
Sérgio Martins <sergio.martins@kdab.com>
2016-07-16 13:16:49 +0100

    Update documentation regarding QPixmap HICON conversions
    The QPixmap methods are gone in Qt5. QtWinExtras should be used instead.
    
    Task-Id: QTBUG-54838
    Change-Id: Ia52ab9786f57d92caf4c44142a3131dbf973a193
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/gui/image/qpixmap.cpp
+++ src/gui/image/qpixmap.cpp
@@ -1412,10 +1412,8 @@ QPixmap QPixmap::transformed(const QMatrix &matrix, Qt::TransformationMode mode)
     QPixmap using the fromImage(). If this is too expensive an
     operation, you can use QBitmap::fromImage() instead.
 
-    The QPixmap class also supports conversion to and from HICON:
-    the toWinHICON() function creates a HICON equivalent to the
-    QPixmap, and returns the HICON handle. The fromWinHICON()
-    function returns a QPixmap that is equivalent to the given icon.
+    To convert a QPixmap to and from HICON you can use the QtWinExtras
+    functions QtWin::toHICON() and QtWin::fromHICON() respectively.
 
     \section1 Pixmap Transformations
 
