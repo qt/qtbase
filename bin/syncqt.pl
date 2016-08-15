a5bb91c2b81388e7c84548a7aed44257f5662776
Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
2016-08-15 11:31:22 +0200

    make it possible to exclude private headers from syncqt include check
    the limitation to public headers seemed pretty arbitrary.
    
    Change-Id: I0f2290fe09c8a6a70d056ec250b902be7049790e
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- bin/syncqt.pl
+++ bin/syncqt.pl
@@ -333,8 +333,8 @@ sub check_header {
     my ($lib, $header, $iheader, $public_header, $private_header) = @_;
     my $header_skip_qt_begin_namespace_test = 0;
 
+    return if ($ignore_for_include_check{$header});
     if ($public_header) {
-        return if ($ignore_for_include_check{$header});
         $header_skip_qt_begin_namespace_test = 1 if ($ignore_for_qt_begin_namespace_check{$header});
     }
 
