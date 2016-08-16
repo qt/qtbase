ff00b2efbd359b1241dfdc4caf325d8f4b7e6118
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-08-16 09:03:31 +0200

    Add " We mean it" comment to qdeadlinetimer_p.h
    Fix warning:
    QtCore: WARNING: qtbase/src/corelib/kernel/qdeadlinetimer_p.h does not have the "We mean it." warning
    
    Amends change 12eacc3bab00f23d187a295b35e4a0d283ba85f4
    
    Change-Id: Ibb8fd25cee0249380996ae271200055e131d359b
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/corelib/kernel/qdeadlinetimer_p.h
+++ src/corelib/kernel/qdeadlinetimer_p.h
@@ -40,6 +40,17 @@
 #ifndef QDEADLINETIMER_P_H
 #define QDEADLINETIMER_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qglobal.h>
 
 QT_BEGIN_NAMESPACE
