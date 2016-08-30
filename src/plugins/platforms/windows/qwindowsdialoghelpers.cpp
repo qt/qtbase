531a2b1b1c26127f75bfe1230051db615e5874c5
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-08-30 12:25:00 +0200

    Windows QPA: Fix leaks in native file dialogs
    Release the IShellItem instances according to the
    documentation of IFile[Open]Dialog.
    
    Task-number: QTBUG-55509
    Task-number: QTBUG-55459
    Change-Id: Ib79622cde21982b1bda0be7d0483c6e652a1d5fe
    Reviewed-by: Andreas Holzammer <andreas.holzammer@kdab.com>
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@qt.io>

--- src/plugins/platforms/windows/qwindowsdialoghelpers.cpp
+++ src/plugins/platforms/windows/qwindowsdialoghelpers.cpp
@@ -1044,12 +1044,15 @@ void QWindowsNativeFileDialogBase::setDirectory(const QUrl &directory)
 
 QString QWindowsNativeFileDialogBase::directory() const
 {
+    QString result;
 #ifndef Q_OS_WINCE
     IShellItem *item = 0;
-    if (m_fileDialog && SUCCEEDED(m_fileDialog->GetFolder(&item)) && item)
-        return QWindowsNativeFileDialogBase::itemPath(item);
+    if (m_fileDialog && SUCCEEDED(m_fileDialog->GetFolder(&item)) && item) {
+        result = QWindowsNativeFileDialogBase::itemPath(item);
+        item->Release();
+    }
 #endif
-    return QString();
+    return result;
 }
 
 void QWindowsNativeFileDialogBase::doExec(HWND owner)
@@ -1514,8 +1517,10 @@ QList<QUrl> QWindowsNativeSaveFileDialog::selectedFiles() const
     QList<QUrl> result;
     IShellItem *item = 0;
     const HRESULT hr = fileDialog()->GetCurrentSelection(&item);
-    if (SUCCEEDED(hr) && item)
+    if (SUCCEEDED(hr) && item) {
         result.push_back(QUrl::fromLocalFile(QWindowsNativeSaveFileDialog::itemPath(item)));
+        item->Release();
+    }
     return result;
 }
 
