qt/qtbase

commit c32ef0a725c7ac9d8a9ab053407389ef2fddc64e

Don't error out on preprocessor concatenation of two strings
"foo" ## "bar" doesn't make a lot of sense, but MSVC allows them
(although gcc errors out on them). Simply ignore the ## in this case
instead of aborting with an error.

Fixes parsing of the Windows winsock2.h header.

Task-number: QTBUG-54560
Change-Id: I84cd5fbb56a006cf379430708c955cf0da475cff
Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

src/tools/moc/preprocessor.cpp
@@ -695,12 +695,9 @@ Symbols Preprocessor::macroExpandIdentifier(Preprocessor *that, SymbolStack &sym
                     next = arg.at(0);
                 }
 
-                if (!expansion.isEmpty() && expansion.constLast().token == s.token) {
-                    Symbol last = expansion.constLast();
+                Symbol last = expansion.constLast();
+                if (!expansion.isEmpty() && last.token == s.token && last.token != STRING_LITERAL) {
                     expansion.pop_back();
-
-                    if (last.token == STRING_LITERAL || s.token == STRING_LITERAL)
-                        that->error("Can't concatenate non identifier tokens");
 
                     QByteArray lexem = last.lexem() + next.lexem();
                     expansion += Symbol(lineNum, last.token, lexem);

tests/auto/tools/moc/tst_moc.cpp
@@ -70,6 +70,12 @@
 #include "non-gadget-parent-class.h"
 #include "grand-parent-gadget-class.h"
 
+#ifdef Q_MOC_RUN
+// check that moc can parse these constructs, they are being used in Windows winsock2.h header
+#define STRING_HASH_HASH(x) ("foo" ## x ## "bar")
+const char *string_hash_hash = STRING_HASH_HASH("baz");
+#endif
+
 Q_DECLARE_METATYPE(const QMetaObject*);
 
 

commit 97710f429fb06914b5cb86fdd3aa333eb8a90dff

moc: Fix asserts on moc on empty token preceding ## operator
Regression introduced in c32ef0a725c7ac9d8a9ab053407389ef2fddc64e
The expansion vector can be empty, in that case it is not allowed to
call constLast()

Task-number: QTBUG-55853
Change-Id: I47aa8eb7507ee91662215df42b4a66eebaa32bb5
Reviewed-by: Jędrzej Nowacki <jedrzej.nowacki@theqtcompany.com>

src/tools/moc/preprocessor.cpp
@@ -696,9 +696,9 @@ Symbols Preprocessor::macroExpandIdentifier(Preprocessor *that, SymbolStack &sym
                     next = arg.at(0);
                 }
 
-                Symbol last = expansion.constLast();
-                if (!expansion.isEmpty() && last.token == s.token && last.token != STRING_LITERAL) {
-                    expansion.pop_back();
+                if (!expansion.isEmpty() && expansion.constLast().token == s.token
+                    && expansion.constLast().token != STRING_LITERAL) {
+                    Symbol last = expansion.takeLast();
 
                     QByteArray lexem = last.lexem() + next.lexem();
                     expansion += Symbol(lineNum, last.token, lexem);

tests/auto/tools/moc/parse-defines.h
@@ -139,6 +139,9 @@ public slots:
 signals:
     DEFINE_CMDLINE_SIGNAL;
 
+#define QTBUG55853(X) PD_DEFINE1(X, signalQTBUG55853)
+#define PD_EMPTY /* empty */
+    void QTBUG55853(PD_EMPTY)();
 };
 
 #undef QString

tests/auto/tools/moc/tst_moc.cpp
@@ -3397,6 +3397,9 @@ void tst_Moc::parseDefines()
 
     index = mo->indexOfSignal("cmdlineSignal(QMap<int,int>)");
     QVERIFY(index != -1);
+
+    index = mo->indexOfSignal("signalQTBUG55853()");
+    QVERIFY(index != -1);
 }
 
 void tst_Moc::preprocessorOnly()