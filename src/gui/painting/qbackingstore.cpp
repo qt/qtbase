qt/qtbase

commit 2d2d90781abdcdbd2a98201877563c83d926dd1a

macOS: Implement QCocoaBackingStore in terms of QRasterBackingStore
The previous detection of device pixel ratio changes in paintDevice()
is not needed, as QBackingStore::beginPaint() already does this check
and calls resize().

Change-Id: I9ee8410fa3a5404c5ec19d2cba4543a9e3359fe9
Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

src/platformsupport/graphics/qrasterbackingstore.cpp
@@ -62,11 +62,18 @@ void QRasterBackingStore::resize(const QSize &size, const QRegion &staticContent
     if (m_image.size() == effectiveBufferSize)
         return;
 
-    QImage::Format format = window()->format().hasAlpha() ? QImage::Format_ARGB32_Premultiplied : QImage::Format_RGB32;
-    m_image = QImage(effectiveBufferSize, format);
+    m_image = QImage(effectiveBufferSize, format());
     m_image.setDevicePixelRatio(windowDevicePixelRatio);
-    if (format == QImage::Format_ARGB32_Premultiplied)
+    if (m_image.format() == QImage::Format_ARGB32_Premultiplied)
         m_image.fill(Qt::transparent);
+}
+
+QImage::Format QRasterBackingStore::format() const
+{
+    if (window()->format().hasAlpha())
+        return QImage::Format_ARGB32_Premultiplied;
+    else
+        return QImage::Format_RGB32;
 }
 
 QPaintDevice *QRasterBackingStore::paintDevice()

src/platformsupport/graphics/qrasterbackingstore_p.h
@@ -62,13 +62,16 @@ public:
     QRasterBackingStore(QWindow *window);
     ~QRasterBackingStore();
 
-    QPaintDevice *paintDevice() Q_DECL_OVERRIDE;
-    QImage toImage() const Q_DECL_OVERRIDE;
-    void resize (const QSize &size, const QRegion &) Q_DECL_OVERRIDE;
+    void resize(const QSize &size, const QRegion &staticContents) Q_DECL_OVERRIDE;
     bool scroll(const QRegion &area, int dx, int dy) Q_DECL_OVERRIDE;
     void beginPaint(const QRegion &region) Q_DECL_OVERRIDE;
 
+    QPaintDevice *paintDevice() Q_DECL_OVERRIDE;
+    QImage toImage() const Q_DECL_OVERRIDE;
+
 protected:
+    virtual QImage::Format format() const;
+
     QImage m_image;
 };
 

src/platformsupport/platformsupport.pro
@@ -29,6 +29,6 @@ unix:!darwin:qtConfig(dbus) {
     include(dbusmenu/dbusmenu.pri)
     include(dbustray/dbustray.pri)
 }
-uikit: include(graphics/graphics.pri)
+darwin: include(graphics/graphics.pri)
 
 load(qt_module)

src/plugins/platforms/cocoa/qcocoabackingstore.h
@@ -40,33 +40,20 @@
 #ifndef QBACKINGSTORE_COCOA_H
 #define QBACKINGSTORE_COCOA_H
 
-#include <AppKit/AppKit.h>
-
-#include "qcocoawindow.h"
-#include "qnsview.h"
-
-#include <qpa/qplatformbackingstore.h>
+#include <QtPlatformSupport/private/qrasterbackingstore_p.h>
 
 QT_BEGIN_NAMESPACE
 
-class QCocoaBackingStore : public QPlatformBackingStore
+class QCocoaBackingStore : public QRasterBackingStore
 {
 public:
     QCocoaBackingStore(QWindow *window);
     ~QCocoaBackingStore();
 
-    QPaintDevice *paintDevice() Q_DECL_OVERRIDE;
-    void flush(QWindow *widget, const QRegion &region, const QPoint &offset) Q_DECL_OVERRIDE;
-    QImage toImage() const Q_DECL_OVERRIDE;
-
-    void resize (const QSize &size, const QRegion &) Q_DECL_OVERRIDE;
-    bool scroll(const QRegion &area, int dx, int dy) Q_DECL_OVERRIDE;
-    void beginPaint(const QRegion &region) Q_DECL_OVERRIDE;
-    qreal getBackingStoreDevicePixelRatio();
+    void flush(QWindow *, const QRegion &, const QPoint &) Q_DECL_OVERRIDE;
 
 private:
-    QImage m_qImage;
-    QSize m_requestedSize;
+    QImage::Format format() const Q_DECL_OVERRIDE;
 };
 
 QT_END_NAMESPACE

src/plugins/platforms/cocoa/qcocoabackingstore.mm
@@ -38,13 +38,13 @@
 ****************************************************************************/
 
 #include "qcocoabackingstore.h"
-#include <QtGui/QPainter>
-#include "qcocoahelpers.h"
+
+#include "qcocoawindow.h"
 
 QT_BEGIN_NAMESPACE
 
 QCocoaBackingStore::QCocoaBackingStore(QWindow *window)
-    : QPlatformBackingStore(window)
+    : QRasterBackingStore(window)
 {
 }
 
@@ -54,69 +54,21 @@ QCocoaBackingStore::~QCocoaBackingStore()
         [cocoaWindow->m_qtView clearBackingStore:this];
 }
 
-QPaintDevice *QCocoaBackingStore::paintDevice()
+QImage::Format QCocoaBackingStore::format() const
 {
-    QCocoaWindow *cocoaWindow = static_cast<QCocoaWindow *>(window()->handle());
-    int windowDevicePixelRatio = int(cocoaWindow->devicePixelRatio());
+    if (static_cast<QCocoaWindow *>(window()->handle())->m_drawContentBorderGradient)
+        return QImage::Format_ARGB32_Premultiplied;
 
-    // Receate the backing store buffer if the effective buffer size has changed,
-    // either due to a window resize or devicePixelRatio change.
-    QSize effectiveBufferSize = m_requestedSize * windowDevicePixelRatio;
-    if (m_qImage.size() != effectiveBufferSize) {
-        QImage::Format format = (window()->format().hasAlpha() || cocoaWindow->m_drawContentBorderGradient)
-                ? QImage::Format_ARGB32_Premultiplied : QImage::Format_RGB32;
-        m_qImage = QImage(effectiveBufferSize, format);
-        m_qImage.setDevicePixelRatio(windowDevicePixelRatio);
-        if (format == QImage::Format_ARGB32_Premultiplied)
-            m_qImage.fill(Qt::transparent);
-    }
-    return &m_qImage;
+    return QRasterBackingStore::format();
 }
 
-void QCocoaBackingStore::flush(QWindow *win, const QRegion &region, const QPoint &offset)
+void QCocoaBackingStore::flush(QWindow *window, const QRegion &region, const QPoint &offset)
 {
-    if (!m_qImage.isNull()) {
-        if (QCocoaWindow *cocoaWindow = static_cast<QCocoaWindow *>(win->handle()))
-            [cocoaWindow->m_qtView flushBackingStore:this region:region offset:offset];
-    }
-}
+    if (m_image.isNull())
+        return;
 
-QImage QCocoaBackingStore::toImage() const
-{
-    return m_qImage;
-}
-
-void QCocoaBackingStore::resize(const QSize &size, const QRegion &)
-{
-    m_requestedSize = size;
-}
-
-bool QCocoaBackingStore::scroll(const QRegion &area, int dx, int dy)
-{
-    extern void qt_scrollRectInImage(QImage &img, const QRect &rect, const QPoint &offset);
-    const qreal devicePixelRatio = m_qImage.devicePixelRatio();
-    QPoint qpoint(dx * devicePixelRatio, dy * devicePixelRatio);
-    for (const QRect &rect : area) {
-        const QRect qrect(rect.topLeft() * devicePixelRatio, rect.size() * devicePixelRatio);
-        qt_scrollRectInImage(m_qImage, qrect, qpoint);
-    }
-    return true;
-}
-
-void QCocoaBackingStore::beginPaint(const QRegion &region)
-{
-    if (m_qImage.hasAlphaChannel()) {
-        QPainter p(&m_qImage);
-        p.setCompositionMode(QPainter::CompositionMode_Source);
-        const QColor blank = Qt::transparent;
-        for (const QRect &rect : region)
-            p.fillRect(rect, blank);
-    }
-}
-
-qreal QCocoaBackingStore::getBackingStoreDevicePixelRatio()
-{
-    return m_qImage.devicePixelRatio();
+    if (QCocoaWindow *cocoaWindow = static_cast<QCocoaWindow *>(window->handle()))
+        [cocoaWindow->m_qtView flushBackingStore:this region:region offset:offset];
 }
 
 QT_END_NAMESPACE

src/plugins/platforms/cocoa/qnsview.mm
@@ -495,7 +495,7 @@ static bool _q_dontOverrideCtrlLMB = false;
     qCDebug(lcQpaCocoaWindow) << "[QNSView flushBackingStore:]" << m_window << region.rectCount() << region.boundingRect() << offset;
 
     m_backingStore = backingStore;
-    m_backingStoreOffset = offset * m_backingStore->getBackingStoreDevicePixelRatio();
+    m_backingStoreOffset = offset * m_backingStore->paintDevice()->devicePixelRatio();
     for (const QRect &rect : region)
         [self setNeedsDisplayInRect:NSMakeRect(rect.x(), rect.y(), rect.width(), rect.height())];
 }
@@ -577,7 +577,7 @@ static bool _q_dontOverrideCtrlLMB = false;
 
     // The backing store source rect will be larger on retina displays.
     // Scale dirtyRect by the device pixel ratio:
-    const qreal devicePixelRatio = m_backingStore->getBackingStoreDevicePixelRatio();
+    const qreal devicePixelRatio = m_backingStore->paintDevice()->devicePixelRatio();
     CGRect dirtyBackingRect = CGRectMake(dirtyRect.origin.x * devicePixelRatio,
                                          dirtyRect.origin.y * devicePixelRatio,
                                          dirtyRect.size.width * devicePixelRatio,

commit 8a2557fbb7a0202a3de3846d10ed57d2d93e6de2

Pick up changes to device-pixel-ratio in QRasterBackingStore
Regression from 2d2d9078, which assumed the code in QBackingStore::beginPaint
would apply to macOS as well, but QHighDpiScaling::isActive() is only active
when Qt does the high DPI scaling, so we never hit the resize() code-path.

Adds backingStore() accessor to go from QPlatformBackingStore to QBackingStore.

Change-Id: I5de2ade74e731df445ea22e0f49b2a121e678efe
Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

src/gui/painting/qbackingstore.cpp
@@ -138,6 +138,7 @@ QBackingStore::QBackingStore(QWindow *window)
     : d_ptr(new QBackingStorePrivate(window))
 {
     d_ptr->platformBackingStore = QGuiApplicationPrivate::platformIntegration()->createPlatformBackingStore(window);
+    d_ptr->platformBackingStore->setBackingStore(this);
 }
 
 /*!

src/gui/painting/qplatformbackingstore.cpp
@@ -77,6 +77,7 @@ class QPlatformBackingStorePrivate
 public:
     QPlatformBackingStorePrivate(QWindow *w)
         : window(w)
+        , backingStore(0)
 #ifndef QT_NO_OPENGL
         , textureId(0)
         , blitter(0)
@@ -100,6 +101,7 @@ public:
 #endif
     }
     QWindow *window;
+    QBackingStore *backingStore;
 #ifndef QT_NO_OPENGL
     mutable GLuint textureId;
     mutable QSize textureSize;
@@ -620,6 +622,23 @@ QPlatformBackingStore::~QPlatformBackingStore()
 QWindow* QPlatformBackingStore::window() const
 {
     return d_ptr->window;
+}
+
+/*!
+    Sets the backing store associated with this surface.
+*/
+void QPlatformBackingStore::setBackingStore(QBackingStore *backingStore)
+{
+    d_ptr->backingStore = backingStore;
+}
+
+/*!
+    Returns a pointer to the backing store associated with this
+    surface.
+*/
+QBackingStore *QPlatformBackingStore::backingStore() const
+{
+    return d_ptr->backingStore;
 }
 
 /*!

src/gui/painting/qplatformbackingstore.h
@@ -112,6 +112,7 @@ public:
     virtual ~QPlatformBackingStore();
 
     QWindow *window() const;
+    QBackingStore *backingStore() const;
 
     virtual QPaintDevice *paintDevice() = 0;
 
@@ -145,6 +146,9 @@ public:
 
 private:
     QPlatformBackingStorePrivate *d_ptr;
+
+    void setBackingStore(QBackingStore *);
+    friend class QBackingStore;
 };
 
 #ifndef QT_NO_OPENGL

src/platformsupport/graphics/qrasterbackingstore.cpp
@@ -39,6 +39,7 @@
 
 #include "qrasterbackingstore_p.h"
 
+#include <QtGui/qbackingstore.h>
 #include <QtGui/qpainter.h>
 
 QT_BEGIN_NAMESPACE
@@ -104,6 +105,10 @@ bool QRasterBackingStore::scroll(const QRegion &region, int dx, int dy)
 
 void QRasterBackingStore::beginPaint(const QRegion &region)
 {
+    // Keep backing store device pixel ratio in sync with window
+    if (m_image.devicePixelRatio() != window()->devicePixelRatio())
+        resize(backingStore()->size(), backingStore()->staticContents());
+
     if (!m_image.hasAlphaChannel())
         return;
 