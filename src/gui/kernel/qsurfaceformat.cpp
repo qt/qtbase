qt/qtbase

commit 51767affb380eea5961637f07a2e881b93b6fea5

Show warning when setting new default QSurfaceFormat
If a global shared context is created, and afterwards a new default
QSurfaceFormat with a different version or profile is set, it can
lead to issues with context sharing.
The documentation for QSurfaceFormat::setDefaultFormat mentions
this. It would be helpful to actually show a warning in case it
happens.

Change-Id: I71f7ce95496e1ecbfc6a0c7d7bed146ef8dc351e
Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

src/gui/kernel/qsurfaceformat.cpp
@@ -35,6 +35,7 @@
 
 #include <QtCore/qatomic.h>
 #include <QtCore/QDebug>
+#include <QOpenGLContext>
 
 #ifdef major
 #undef major
@@ -758,6 +759,11 @@ Q_GLOBAL_STATIC(QSurfaceFormat, qt_default_surface_format)
  */
 void QSurfaceFormat::setDefaultFormat(const QSurfaceFormat &format)
 {
+    QOpenGLContext *globalContext = QOpenGLContext::globalShareContext();
+    if (globalContext && globalContext->isValid()) {
+        qWarning("Warning: Setting a new default format with a different version or profile after "
+                 "the global shared context is created may cause issues with context sharing.");
+    }
     *qt_default_surface_format() = format;
 }
 

commit f773ddd026a6227cdb754dea4a5fe0a25137033f

Fix when calling QSurfaceFormat::setDefaultFormat before Q*Application
Fixes a regression introduced in commit 51767affb.
QOpenGLContext::globalShareContext requires a QGuiApplication to exist.
This wasn't required so far.

Task-number: QTBUG-56614
Change-Id: I07bcf434fca536c4dc50feee7ea17eb541fd589a
Reviewed-by: Liang Qi <liang.qi@qt.io>

src/gui/kernel/qsurfaceformat.cpp
@@ -36,6 +36,7 @@
 #include <QtCore/qatomic.h>
 #include <QtCore/QDebug>
 #include <QOpenGLContext>
+#include <QtGui/qguiapplication.h>
 
 #ifdef major
 #undef major
@@ -760,10 +761,13 @@ Q_GLOBAL_STATIC(QSurfaceFormat, qt_default_surface_format)
 void QSurfaceFormat::setDefaultFormat(const QSurfaceFormat &format)
 {
 #ifndef QT_NO_OPENGL
-    QOpenGLContext *globalContext = QOpenGLContext::globalShareContext();
-    if (globalContext && globalContext->isValid()) {
-        qWarning("Warning: Setting a new default format with a different version or profile after "
-                 "the global shared context is created may cause issues with context sharing.");
+    if (qApp) {
+        QOpenGLContext *globalContext = QOpenGLContext::globalShareContext();
+        if (globalContext && globalContext->isValid()) {
+            qWarning("Warning: Setting a new default format with a different version or profile "
+                     "after the global shared context is created may cause issues with context "
+                     "sharing.");
+        }
     }
 #endif
     *qt_default_surface_format() = format;