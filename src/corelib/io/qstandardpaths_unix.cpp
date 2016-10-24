99331f661d6ef88f9bb964133ee4eaf8b6d74e80
Lars Knoll <lars.knoll@qt.io>
2016-10-24 10:17:35 +0200

    Fix QStandardPaths::FontsLocation to be compliant with the latest XDG spec
    According to the latest XDG spec, FontsLocation should point to
    XDG_DATA_DIR/fonts, not ~/.fonts.
    
    Task-number: QTBUG-55507
    Change-Id: Ia62ffe172abbb93d5ce7fd58bdf038ff13954f1b
    Reviewed-by: David Faure <david.faure@kdab.com>

--- src/corelib/io/qstandardpaths_unix.cpp
+++ src/corelib/io/qstandardpaths_unix.cpp
@@ -245,7 +245,7 @@ QString QStandardPaths::writableLocation(StandardLocation type)
         break;
 
     case FontsLocation:
-        path = QDir::homePath() + QLatin1String("/.fonts");
+        path = writableLocation(GenericDataLocation) + QLatin1String("/fonts");
         break;
 
     case MusicLocation:
@@ -339,6 +339,9 @@ QStringList QStandardPaths::standardLocations(StandardLocation type)
         dirs = xdgDataDirs();
         for (int i = 0; i < dirs.count(); ++i)
             appendOrganizationAndApp(dirs[i]);
+        break;
+    case FontsLocation:
+        dirs += QDir::homePath() + QLatin1String("/.fonts");
         break;
     default:
         break;
