qt/qtbase

commit 18ed6f20ad4327fa2ffbb687ac2f3ca7429c4348

Change some #elif to #endif / #if in qcompilerdetection.h
The conditions aren't exclusive, but complementary.

This change removes one check for defined(Q_CC_INTEL) because it was
disabled before and requires testing before we confirm it to be correct.

Change-Id: I33dc971f005a4848bb8ffffd14748cde4dd23333
Reviewed-by: Marc Mutz <marc.mutz@kdab.com>

src/corelib/global/qcompilerdetection.h
@@ -1031,7 +1031,8 @@
 // critical definitions. (Reported as Intel Issue ID 6000117277)
 #  define __USE_CONSTEXPR 1
 #  define __USE_NOEXCEPT 1
-# elif defined(Q_CC_MSVC) && (defined(Q_CC_CLANG) || defined(Q_CC_INTEL))
+# endif
+# if defined(Q_CC_MSVC) && defined(Q_CC_CLANG)
 // Clang and the Intel compiler support more C++ features than the Microsoft compiler
 // so make sure we don't enable them if the MS headers aren't properly adapted.
 #  ifndef _HAS_CONSTEXPR
@@ -1058,7 +1059,8 @@
 #  ifndef _HAS_VARIADIC_TEMPLATES
 #   undef Q_COMPILER_VARIADIC_TEMPLATES
 #  endif
-# elif defined(_LIBCPP_VERSION)
+# endif
+# if defined(_LIBCPP_VERSION)
 // libc++ uses __has_feature(cxx_atomic), so disable the feature if the compiler
 // doesn't support it. That's required for the Intel compiler 14.x or earlier on OS X, for example.
 #  if !__has_feature(cxx_atomic)

commit ea9a8672a19588e89c0bda8e0c31424687c1e05f

Fix build with ICC on macOS: <atomic> is properly supported
I'm guess I introduced the regression in the commit
18ed6f20ad4327fa2ffbb687ac2f3ca7429c4348, which I wasn't sure about.

Change-Id: Ic46ff326a6ba46bc877cfffd14839f84fdf796e7
Reviewed-by: Jake Petroules <jake.petroules@qt.io>

src/corelib/global/qcompilerdetection.h
@@ -1060,13 +1060,6 @@
 #   undef Q_COMPILER_VARIADIC_TEMPLATES
 #  endif
 # endif
-# if defined(_LIBCPP_VERSION)
-// libc++ uses __has_feature(cxx_atomic), so disable the feature if the compiler
-// doesn't support it. That's required for the Intel compiler 14.x or earlier on OS X, for example.
-#  if !__has_feature(cxx_atomic)
-#   undef Q_COMPILER_ATOMICS
-#  endif
-# endif
 # if defined(Q_COMPILER_THREADSAFE_STATICS) && defined(Q_OS_MAC)
 // Apple's low-level implementation of the C++ support library
 // (libc++abi.dylib, shared between libstdc++ and libc++) has deadlocks. The