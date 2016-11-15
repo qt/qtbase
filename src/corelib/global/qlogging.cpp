4b1c343e5e8f75b567e88541c1b8ea89a3a5b666
Kai Koehne <kai.koehne@qt.io>
2016-11-15 15:11:00 +0100

    Doc: Add missing reference to qInfo()
    Change-Id: I7438aa8ff9fddf2e0155ffe0d442f96d4d9265d4
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

--- src/corelib/global/qlogging.cpp
+++ src/corelib/global/qlogging.cpp
@@ -1748,7 +1748,7 @@ void qErrnoWarning(int code, const char *msg, ...)
 
     \snippet code/src_corelib_global_qglobal.cpp 23
 
-    \sa QtMessageHandler, QtMsgType, qDebug(), qWarning(), qCritical(), qFatal(),
+    \sa QtMessageHandler, QtMsgType, qDebug(), qInfo(), qWarning(), qCritical(), qFatal(),
     {Debugging Techniques}
 */
 
