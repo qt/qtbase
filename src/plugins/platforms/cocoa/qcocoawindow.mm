qt/qtbase

commit 593ab638609eb33542cf47b1693e70d6dbd7093b

Cocoa integration - do not make a window key during modal session
It can happen that we create a new window and make it visible during a modal
session. In this case making a window into the new key window looks quite
strange and misleading - since we're in a modal session, this window is completely
inactive/useless. Use orderFront instead.

Change-Id: Ic091e263508b452be4b5adf799a06017b044e441
Task-number: QTBUG-46304
Reviewed-by: Morten Johan Sørvig <morten.sorvig@theqtcompany.com>

src/plugins/platforms/cocoa/qcocoawindow.mm
@@ -686,7 +686,16 @@ void QCocoaWindow::setVisible(bool visible)
                     cocoaEventDispatcherPrivate->beginModalSession(window());
                     m_hasModalSession = true;
                 } else if ([m_nsWindow canBecomeKeyWindow]) {
-                    [m_nsWindow makeKeyAndOrderFront:nil];
+                    QCocoaEventDispatcher *cocoaEventDispatcher = qobject_cast<QCocoaEventDispatcher *>(QGuiApplication::instance()->eventDispatcher());
+                    QCocoaEventDispatcherPrivate *cocoaEventDispatcherPrivate = 0;
+                    if (cocoaEventDispatcher)
+                        cocoaEventDispatcherPrivate = static_cast<QCocoaEventDispatcherPrivate *>(QObjectPrivate::get(cocoaEventDispatcher));
+
+                    if (!(cocoaEventDispatcherPrivate && cocoaEventDispatcherPrivate->currentModalSession()))
+                        [m_nsWindow makeKeyAndOrderFront:nil];
+                    else
+                        [m_nsWindow orderFront:nil];
+
                     foreach (QCocoaWindow *childWindow, m_childWindows)
                         childWindow->show(true);
                 } else {

commit 3edeb5c909a4ec62440e2075b9d49ec256a1ba68

Cocoa: fix regression preventing windows from showing up
The regression was introduced by 593ab638609 which fixed another bug
related to window modality. To determine whether the window needs to be
(made key and ordered front) or just (ordered front), instead of calling
currentModalSession() which might change internal state of event
dispatcher we just check if cocoaModalSessionStack is empty or not.

Task-number: QTBUG-57991
Task-number: QTBUG-56166
Change-Id: I6c4f92860d8c93decd44e572af1690ed7be6f1f0
Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

src/plugins/platforms/cocoa/qcocoawindow.mm
@@ -695,7 +695,7 @@ void QCocoaWindow::setVisible(bool visible)
                     if (cocoaEventDispatcher)
                         cocoaEventDispatcherPrivate = static_cast<QCocoaEventDispatcherPrivate *>(QObjectPrivate::get(cocoaEventDispatcher));
 
-                    if (!(cocoaEventDispatcherPrivate && cocoaEventDispatcherPrivate->currentModalSession()))
+                    if (cocoaEventDispatcherPrivate && cocoaEventDispatcherPrivate->cocoaModalSessionStack.isEmpty())
                         [m_nsWindow makeKeyAndOrderFront:nil];
                     else
                         [m_nsWindow orderFront:nil];