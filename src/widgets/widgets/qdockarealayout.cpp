qt/qtbase

commit 0feeb6f6d2cfaa964763ca1fcab65672812b4eef

Fix dock widget having the wrong parent after a drag
When dragging a dock widget from a floating group to the main window
reparentWidgets() is supposed to be called. It's usually triggered
by some unrelated event, like a LayoutRequest.

Instead of relying on luck for reparentWidgets() to get called be
explicit, otherwise the dock widget that was dropped into main window
will still have as parent the floating group window.

The item.skip() condition seems overly restrictive.

Task-number: QTBUG-58036
Change-Id: I65b5699e1acb6ca9bedb10620daa055fa9d91943
Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>

src/widgets/widgets/qdockarealayout.cpp
@@ -2092,7 +2092,7 @@ void QDockAreaLayoutInfo::reparentWidgets(QWidget *parent)
         const QDockAreaLayoutItem &item = item_list.at(i);
         if (item.flags & QDockAreaLayoutItem::GapItem)
             continue;
-        if (item.skip())
+        if (!item.widgetItem && item.skip())
             continue;
         if (item.subinfo)
             item.subinfo->reparentWidgets(parent);
@@ -2608,7 +2608,9 @@ QLayoutItem *QDockAreaLayout::plug(const QList<int> &path)
     Q_ASSERT(!path.isEmpty());
     const int index = path.first();
     Q_ASSERT(index >= 0 && index < QInternal::DockCount);
-    return docks[index].plug(path.mid(1));
+    QLayoutItem *item = docks[index].plug(path.mid(1));
+    docks[index].reparentWidgets(mainWindow);
+    return item;
 }
 
 QLayoutItem *QDockAreaLayout::unplug(const QList<int> &path)

commit d57bb19902f863fc6db07674f6bd8881b0886b39

Fix crash while dropping a tabbed group into a single floating QDockWidget
The problem was caused by commit 0feeb6f6d2cfaa964763ca1fcab65672812b4eef
which fixed QTBUG-58036. It reparented widget that used to be skiped.
In particular, floating widgets are skiped. But seting the parent of a
floating widget docks it. And so as a result it would not be skiped anymore.
This has two side effect: This breaks the animation (as the widget is
docked too early; and cause crash when QDockWidgetGroupWindow get
reparented as this breaks invariant that these are always floating.

So restore the skip from before commit 0feeb6f6d2cfaa964763ca1fcab65672812b4eef,
and explicitly set the parent in all cases when the animation finishes.

Change-Id: I0e3e29ad22d6ffe4d62242d48a18dadb916fc14f
Reviewed-by: Sérgio Martins <sergio.martins@kdab.com>

src/widgets/widgets/qdockarealayout.cpp
@@ -2092,7 +2092,7 @@ void QDockAreaLayoutInfo::reparentWidgets(QWidget *parent)
         const QDockAreaLayoutItem &item = item_list.at(i);
         if (item.flags & QDockAreaLayoutItem::GapItem)
             continue;
-        if (!item.widgetItem && item.skip())
+        if (item.skip())
             continue;
         if (item.subinfo)
             item.subinfo->reparentWidgets(parent);

src/widgets/widgets/qmainwindowlayout.cpp
@@ -2195,10 +2195,8 @@ void QMainWindowLayout::animationFinished(QWidget *widget)
         }
 
         if (QDockWidget *dw = qobject_cast<QDockWidget*>(widget)) {
-            if (currentHoveredFloat) {
-                dw->setParent(currentHoveredFloat);
-                dw->show();
-            }
+            dw->setParent(currentHoveredFloat ? currentHoveredFloat.data() : parentWidget());
+            dw->show();
             dw->d_func()->plug(currentGapRect);
         }
 #endif