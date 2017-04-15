2814744a739dfab123448f92e2009c34a82976a3
Thiago Macieira <thiago.macieira@intel.com>
2017-04-15 15:56:47 -0700

    Update the Clang usual versions for Apple's build
    Change-Id: Id69a70a52573241e1b6a05bd62a3fd01a8e78550
    Reviewed-by: Jake Petroules <jake.petroules@qt.io>

--- src/corelib/global/qcompilerdetection.h
+++ src/corelib/global/qcompilerdetection.h
@@ -172,7 +172,11 @@
 /* Clang also masquerades as GCC */
 #    if defined(__apple_build_version__)
 #      /* http://en.wikipedia.org/wiki/Xcode#Toolchain_Versions */
-#      if __apple_build_version__ >= 7000053
+#      if __apple_build_version__ >= 8020041
+#        define Q_CC_CLANG 309
+#      elif __apple_build_version__ >= 8000038
+#        define Q_CC_CLANG 308
+#      elif __apple_build_version__ >= 7000053
 #        define Q_CC_CLANG 306
 #      elif __apple_build_version__ >= 6000051
 #        define Q_CC_CLANG 305
