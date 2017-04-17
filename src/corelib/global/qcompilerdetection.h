285789781cd2893302edfc004d8fc07fc06b1148
Thiago Macieira <thiago.macieira@intel.com>
2017-04-16 20:29:39 -0700

    icc: thread_local in macOS is working since 10.7
    Change-Id: I0e1a09998253489388abfffd14b611b1403a0c9f
    Reviewed-by: Jake Petroules <jake.petroules@qt.io>

--- src/corelib/global/qcompilerdetection.h
+++ src/corelib/global/qcompilerdetection.h
@@ -632,10 +632,7 @@
 #      define Q_COMPILER_ALIGNAS
 #      define Q_COMPILER_ALIGNOF
 #      define Q_COMPILER_INHERITING_CONSTRUCTORS
-#      ifndef Q_OS_OSX
-//       C++11 thread_local is broken on OS X (Clang doesn't support it either)
-#        define Q_COMPILER_THREAD_LOCAL
-#      endif
+#      define Q_COMPILER_THREAD_LOCAL
 #      define Q_COMPILER_UDL
 #    endif
 #    ifdef _MSC_VER
