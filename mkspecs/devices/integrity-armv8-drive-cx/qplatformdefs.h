aea4e3823dc52618767ab7ebf0bce53696a4b80b
Tero Alamäki <tero.alamaki@qt.io>
2017-04-26 10:36:53 +0300

    Add INTEGRITY mkspec for ARMv8 Drive CX
    Change-Id: I038de7a0cc9e6046aec3fc930876d43263702e90
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@qt.io>
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- mkspecs/devices/integrity-armv8-drive-cx/qplatformdefs.h
+++ mkspecs/devices/integrity-armv8-drive-cx/qplatformdefs.h
@@ -1,9 +1,9 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2017 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
-** This file is part of the QtGui module of the Qt Toolkit.
+** This file is part of the qmake spec of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
 ** Commercial License Usage
@@ -37,83 +37,9 @@
 **
 ****************************************************************************/
 
-#ifndef QT_EGL_P_H
-#define QT_EGL_P_H
+#ifndef QPLATFORMDEFS_H
+#define QPLATFORMDEFS_H
 
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
+#include "../../common/integrity/qplatformdefs.h"
 
-#ifdef QT_EGL_NO_X11
-# define MESA_EGL_NO_X11_HEADERS // MESA
-# if !defined(Q_OS_INTEGRITY)
-#  define WIN_INTERFACE_CUSTOM   // NV
-# endif // Q_OS_INTEGRITY
-#endif  // QT_EGL_NO_X11
-
-#ifdef QT_EGL_WAYLAND
-# define WAYLAND // NV
-#endif // QT_EGL_WAYLAND
-
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-
-#include <stdint.h>
-
-QT_BEGIN_NAMESPACE
-
-namespace QtInternal {
-
-template <class FromType, class ToType>
-struct QtEglConverter
-{
-    static inline ToType convert(FromType v)
-    { return v; }
-};
-
-template <>
-struct QtEglConverter<uint32_t, uintptr_t>
-{
-    static inline uintptr_t convert(uint32_t v)
-    { return v; }
-};
-
-#if QT_POINTER_SIZE > 4
-template <>
-struct QtEglConverter<uintptr_t, uint32_t>
-{
-    static inline uint32_t convert(uintptr_t v)
-    { return uint32_t(v); }
-};
-#endif
-
-template <>
-struct QtEglConverter<uint32_t, void *>
-{
-    static inline void *convert(uint32_t v)
-    { return reinterpret_cast<void *>(uintptr_t(v)); }
-};
-
-template <>
-struct QtEglConverter<void *, uint32_t>
-{
-    static inline uint32_t convert(void *v)
-    { return uintptr_t(v); }
-};
-
-} // QtInternal
-
-template <class ToType, class FromType>
-static inline ToType qt_egl_cast(FromType from)
-{ return QtInternal::QtEglConverter<FromType, ToType>::convert(from); }
-
-QT_END_NAMESPACE
-
-#endif // QT_EGL_P_H
+#endif // QPLATFORMDEFS_H
