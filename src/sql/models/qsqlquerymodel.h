395ef2535315f826c3358f454fc2286585ec0177
Jesus Fernandez <jesus.fernandez@qt.io>
2017-05-18 15:46:38 +0200

    Use modern C++ keywords
    Unifies the usage of modern C++ keywords with the previous commit.
    
    Change-Id: I2dbcf3774634cbd3a24eb1cac58f34bae241cfea
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/sql/models/qsqlquerymodel.h
+++ src/sql/models/qsqlquerymodel.h
@@ -58,22 +58,22 @@ class Q_SQL_EXPORT QSqlQueryModel: public QAbstractTableModel
     Q_DECLARE_PRIVATE(QSqlQueryModel)
 
 public:
-    explicit QSqlQueryModel(QObject *parent = Q_NULLPTR);
+    explicit QSqlQueryModel(QObject *parent = nullptr);
     virtual ~QSqlQueryModel();
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QSqlRecord record(int row) const;
     QSqlRecord record() const;
 
-    QVariant data(const QModelIndex &item, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    QVariant data(const QModelIndex &item, int role = Qt::DisplayRole) const override;
     QVariant headerData(int section, Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+                        int role = Qt::DisplayRole) const override;
     bool setHeaderData(int section, Qt::Orientation orientation, const QVariant &value,
-                       int role = Qt::EditRole) Q_DECL_OVERRIDE;
+                       int role = Qt::EditRole) override;
 
-    bool insertColumns(int column, int count, const QModelIndex &parent = QModelIndex()) Q_DECL_OVERRIDE;
-    bool removeColumns(int column, int count, const QModelIndex &parent = QModelIndex()) Q_DECL_OVERRIDE;
+    bool insertColumns(int column, int count, const QModelIndex &parent = QModelIndex()) override;
+    bool removeColumns(int column, int count, const QModelIndex &parent = QModelIndex()) override;
 
     void setQuery(const QSqlQuery &query);
     void setQuery(const QString &query, const QSqlDatabase &db = QSqlDatabase());
@@ -83,8 +83,8 @@ public:
 
     QSqlError lastError() const;
 
-    void fetchMore(const QModelIndex &parent = QModelIndex()) Q_DECL_OVERRIDE;
-    bool canFetchMore(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    void fetchMore(const QModelIndex &parent = QModelIndex()) override;
+    bool canFetchMore(const QModelIndex &parent = QModelIndex()) const override;
 
     QHash<int, QByteArray> roleNames() const override;
 
@@ -107,7 +107,7 @@ protected:
 
     virtual QModelIndex indexInQuery(const QModelIndex &item) const;
     void setLastError(const QSqlError &error);
-    QSqlQueryModel(QSqlQueryModelPrivate &dd, QObject *parent = Q_NULLPTR);
+    QSqlQueryModel(QSqlQueryModelPrivate &dd, QObject *parent = nullptr);
 };
 
 QT_END_NAMESPACE
