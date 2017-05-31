qt/qtbase

commit 6255cb893d411b055758f2e64e94fde0bce91ea8

Remove qtypetraits.h's contents altogether
So that QFlags can use an (un)signed int matching the
underlying type as identified by the compiler and not by us.

Requires fixing a few warnings about sign conversion due to
QFlags misusages in qtbase that were either plain wrong, or
were relying on the enum being backed by an (un)signed int
when it wasn't.

Keep qtypetraits.h in the source tree in order to prevent
source breaks if some downstream #includes it (note however
that it did not contain any public API).

Change-Id: Ib3a92b98db7031e793a088fb2a3b306eff4d7a3c
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/itemmodels/qabstractitemmodel.cpp
@@ -2044,7 +2044,7 @@ Qt::DropActions QAbstractItemModel::supportedDropActions() const
 Qt::DropActions QAbstractItemModel::supportedDragActions() const
 {
     Q_D(const QAbstractItemModel);
-    if (d->supportedDragActions != -1)
+    if (d->supportedDragActions != Qt::IgnoreAction)
         return d->supportedDragActions;
     return supportedDropActions();
 }

src/corelib/global/qflags.h
@@ -42,12 +42,11 @@
 #ifndef QFLAGS_H
 #define QFLAGS_H
 
-#include <QtCore/qtypeinfo.h>
-#include <QtCore/qtypetraits.h>
-
 #ifdef Q_COMPILER_INITIALIZER_LISTS
 #include <initializer_list>
 #endif
+
+#include <type_traits>
 
 QT_BEGIN_NAMESPACE
 
@@ -94,6 +93,8 @@ class QFlags
     Q_STATIC_ASSERT_X((sizeof(Enum) <= sizeof(int)),
                       "QFlags uses an int as storage, so an enum with underlying "
                       "long long will overflow.");
+    Q_STATIC_ASSERT_X((std::is_enum<Enum>::value), "QFlags is only usable on enumeration types.");
+
     struct Private;
     typedef int (Private::*Zero);
 public:
@@ -103,7 +104,7 @@ public:
     typedef int Int;
 #else
     typedef typename std::conditional<
-            QtPrivate::QIsUnsignedEnum<Enum>::value,
+            std::is_unsigned<typename std::underlying_type<Enum>::type>::value,
             unsigned int,
             signed int
         >::type Int;

src/corelib/global/qtypetraits.h
@@ -37,6 +37,12 @@
 **
 ****************************************************************************/
 
+// ### Qt 6: remove this header
+//
+// This header is deliberately empty. Although it did not contain any public API,
+// it was accidentally made public in Qt 5. So: do not remove it for the moment
+// being, to prevent #include breaks in downstreams.
+
 #include "QtCore/qglobal.h"
 
 #ifndef QTYPETRAITS_H
@@ -44,53 +50,6 @@
 
 QT_BEGIN_NAMESPACE
 
-namespace QtPrivate {
-
-//
-// Define QIsUnsignedEnum, QIsSignedEnum -
-// std::is_signed, std::is_unsigned does not work for enum's
-//
-
-// a metafunction to invert an integral_constant:
-template <typename T>
-struct not_
-    : std::integral_constant<bool, !T::value> {};
-
-// Checks whether a type is unsigned (T must be convertible to unsigned int):
-template <typename T>
-struct QIsUnsignedEnum
-    : std::integral_constant<bool, (T(0) < T(-1))> {};
-
-// Checks whether a type is signed (T must be convertible to int):
-template <typename T>
-struct QIsSignedEnum
-    : not_< QIsUnsignedEnum<T> > {};
-
-Q_STATIC_ASSERT(( QIsUnsignedEnum<quint8>::value));
-Q_STATIC_ASSERT((!QIsUnsignedEnum<qint8>::value));
-
-Q_STATIC_ASSERT((!QIsSignedEnum<quint8>::value));
-Q_STATIC_ASSERT(( QIsSignedEnum<qint8>::value));
-
-Q_STATIC_ASSERT(( QIsUnsignedEnum<quint16>::value));
-Q_STATIC_ASSERT((!QIsUnsignedEnum<qint16>::value));
-
-Q_STATIC_ASSERT((!QIsSignedEnum<quint16>::value));
-Q_STATIC_ASSERT(( QIsSignedEnum<qint16>::value));
-
-Q_STATIC_ASSERT(( QIsUnsignedEnum<quint32>::value));
-Q_STATIC_ASSERT((!QIsUnsignedEnum<qint32>::value));
-
-Q_STATIC_ASSERT((!QIsSignedEnum<quint32>::value));
-Q_STATIC_ASSERT(( QIsSignedEnum<qint32>::value));
-
-Q_STATIC_ASSERT(( QIsUnsignedEnum<quint64>::value));
-Q_STATIC_ASSERT((!QIsUnsignedEnum<qint64>::value));
-
-Q_STATIC_ASSERT((!QIsSignedEnum<quint64>::value));
-Q_STATIC_ASSERT(( QIsSignedEnum<qint64>::value));
-
-} // namespace QtPrivate
-
 QT_END_NAMESPACE
+
 #endif  // QTYPETRAITS_H

src/corelib/io/qurl.h
@@ -69,8 +69,8 @@ public:
     Q_DECL_CONSTEXPR inline QUrlTwoFlags(E1 f) : i(f) {}
     Q_DECL_CONSTEXPR inline QUrlTwoFlags(E2 f) : i(f) {}
     Q_DECL_CONSTEXPR inline QUrlTwoFlags(QFlag f) : i(f) {}
-    Q_DECL_CONSTEXPR inline QUrlTwoFlags(QFlags<E1> f) : i(f.operator int()) {}
-    Q_DECL_CONSTEXPR inline QUrlTwoFlags(QFlags<E2> f) : i(f.operator int()) {}
+    Q_DECL_CONSTEXPR inline QUrlTwoFlags(QFlags<E1> f) : i(f.operator typename QFlags<E1>::Int()) {}
+    Q_DECL_CONSTEXPR inline QUrlTwoFlags(QFlags<E2> f) : i(f.operator typename QFlags<E2>::Int()) {}
     Q_DECL_CONSTEXPR inline QUrlTwoFlags(Zero = 0) : i(0) {}
 
     inline QUrlTwoFlags &operator&=(int mask) { i &= mask; return *this; }

src/corelib/kernel/qmetatype.cpp
@@ -1027,7 +1027,7 @@ int QMetaType::registerNormalizedType(const NS(QByteArray) &normalizedTypeName,
                                   normalizedTypeName.size());
 
     int previousSize = 0;
-    int previousFlags = 0;
+    QMetaType::TypeFlags::Int previousFlags = 0;
     if (idx == UnknownType) {
         QWriteLocker locker(customTypesLock());
         int posInVector = -1;

src/corelib/kernel/qmetatype_p.h
@@ -129,7 +129,7 @@ public:
     QMetaType::Constructor constructor;
     QMetaType::Destructor destructor;
     int size;
-    quint32 flags; // same as QMetaType::TypeFlags
+    QMetaType::TypeFlags::Int flags;
     const QMetaObject *metaObject;
 };
 

src/gui/opengl/qopengltexturecache.cpp
@@ -371,7 +371,7 @@ static void freeTexture(QOpenGLFunctions *funcs, GLuint id)
     funcs->glDeleteTextures(1, &id);
 }
 
-QOpenGLCachedTexture::QOpenGLCachedTexture(GLuint id, int options, QOpenGLContext *context) : m_options(options)
+QOpenGLCachedTexture::QOpenGLCachedTexture(GLuint id, QOpenGLTextureCache::BindOptions options, QOpenGLContext *context) : m_options(options)
 {
     m_resource = new QOpenGLSharedResourceGuard(context, id, freeTexture);
 }

src/gui/opengl/qopengltexturecache_p.h
@@ -60,19 +60,7 @@
 
 QT_BEGIN_NAMESPACE
 
-class QOpenGLCachedTexture
-{
-public:
-    QOpenGLCachedTexture(GLuint id, int options, QOpenGLContext *context);
-    ~QOpenGLCachedTexture() { m_resource->free(); }
-
-    GLuint id() const { return m_resource->id(); }
-    int options() const { return m_options; }
-
-private:
-    QOpenGLSharedResourceGuard *m_resource;
-    int m_options;
-};
+class QOpenGLCachedTexture;
 
 class Q_GUI_EXPORT QOpenGLTextureCache : public QOpenGLSharedResource
 {
@@ -105,6 +93,20 @@ private:
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(QOpenGLTextureCache::BindOptions)
+
+class QOpenGLCachedTexture
+{
+public:
+    QOpenGLCachedTexture(GLuint id, QOpenGLTextureCache::BindOptions options, QOpenGLContext *context);
+    ~QOpenGLCachedTexture() { m_resource->free(); }
+
+    GLuint id() const { return m_resource->id(); }
+    QOpenGLTextureCache::BindOptions options() const { return m_options; }
+
+private:
+    QOpenGLSharedResourceGuard *m_resource;
+    QOpenGLTextureCache::BindOptions m_options;
+};
 
 QT_END_NAMESPACE
 

commit f33cf18d882ada727da0f378525e55d9421e3b16

QAbstractItemModel::supportedDragActions: fix regression
This method now returns -1 by default, due to commit 6255cb893d
which mistakenly replaced -1 with Qt::IgnoreAction (0x0).

As a result, dropping is forbidden in a number of applications
(I detected this in zanshin).

Change-Id: I4922451216e08d5d3fe36f8ba87364a361b691bf
Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>

src/corelib/itemmodels/qabstractitemmodel.cpp
@@ -2044,7 +2044,7 @@ Qt::DropActions QAbstractItemModel::supportedDropActions() const
 Qt::DropActions QAbstractItemModel::supportedDragActions() const
 {
     Q_D(const QAbstractItemModel);
-    if (d->supportedDragActions != Qt::IgnoreAction)
+    if (int(d->supportedDragActions) != -1)
         return d->supportedDragActions;
     return supportedDropActions();
 }

tests/auto/corelib/itemmodels/qabstractitemmodel/tst_qabstractitemmodel.cpp
@@ -101,6 +101,7 @@ private slots:
 
     void testRoleNames();
     void testDragActions();
+    void dragActionsFallsBackToDropActions();
 
     void testFunctionPointerSignalConnection();
 
@@ -2155,6 +2156,27 @@ void tst_QAbstractItemModel::testDragActions()
     const Qt::DropActions actions = model->supportedDragActions();
     QVERIFY(actions & Qt::CopyAction); // Present by default
     QVERIFY(actions & Qt::MoveAction);
+}
+
+class OverrideDropActions : public QStringListModel
+{
+    Q_OBJECT
+public:
+    OverrideDropActions(QObject *parent = 0)
+      : QStringListModel(parent)
+    {
+    }
+    Qt::DropActions supportedDropActions() const override
+    {
+        return Qt::MoveAction;
+    }
+};
+
+void tst_QAbstractItemModel::dragActionsFallsBackToDropActions()
+{
+    QAbstractItemModel *model = new OverrideDropActions(this);
+    QCOMPARE(model->supportedDragActions(), Qt::MoveAction);
+    QCOMPARE(model->supportedDropActions(), Qt::MoveAction);
 }
 
 class SignalConnectionTester : public QObject

tests/auto/corelib/itemmodels/qstringlistmodel/tst_qstringlistmodel.cpp
@@ -80,6 +80,8 @@ private slots:
 
     void setData_emits_both_roles_data();
     void setData_emits_both_roles();
+
+    void supportedDragDropActions();
 };
 
 void tst_QStringListModel::rowsAboutToBeRemoved_rowsRemoved_data()
@@ -248,6 +250,13 @@ void tst_QStringListModel::setData_emits_both_roles()
     QCOMPARE(spy.size(), 1);
     QCOMPARE(sorted(spy.at(0).at(2).value<QVector<int> >()),
              expected);
+}
+
+void tst_QStringListModel::supportedDragDropActions()
+{
+    QStringListModel model;
+    QCOMPARE(model.supportedDragActions(), Qt::CopyAction | Qt::MoveAction);
+    QCOMPARE(model.supportedDropActions(), Qt::CopyAction | Qt::MoveAction);
 }
 
 QTEST_MAIN(tst_QStringListModel)

tests/auto/gui/itemmodels/qstandarditemmodel/tst_qstandarditemmodel.cpp
@@ -125,6 +125,7 @@ private slots:
 
     void itemRoleNames();
     void getMimeDataWithInvalidModelIndex();
+    void supportedDragDropActions();
 
 private:
     QAbstractItemModel *m_model;
@@ -1664,6 +1665,13 @@ void tst_QStandardItemModel::getMimeDataWithInvalidModelIndex()
     QTest::ignoreMessage(QtWarningMsg, "QStandardItemModel::mimeData: No item associated with invalid index");
     QMimeData *data = model.mimeData(QModelIndexList() << QModelIndex());
     QVERIFY(!data);
+}
+
+void tst_QStandardItemModel::supportedDragDropActions()
+{
+    QStandardItemModel model;
+    QCOMPARE(model.supportedDragActions(), Qt::CopyAction | Qt::MoveAction);
+    QCOMPARE(model.supportedDropActions(), Qt::CopyAction | Qt::MoveAction);
 }
 
 QTEST_MAIN(tst_QStandardItemModel)