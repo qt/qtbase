fdbf400b27ff25f98a64879adeeb342931a84d9a
Oliver Wolff <oliver.wolff@qt.io>
2017-06-19 14:30:19 +0200

    winrt: Fix build for Windows Kit 10.0.10586.0
    The old SDK does not consider the macro value a constant expression, so
    the build fails.
    
    Change-Id: I95bdcd9ad34ee4da81016ff07e70439ed229f853
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@qt.io>

--- src/network/socket/qnativesocketengine_winrt.cpp
+++ src/network/socket/qnativesocketengine_winrt.cpp
@@ -251,17 +251,16 @@ public:
     {
         HRESULT hr = action->GetResults();
         if (FAILED(hr)) {
-            switch (hr) {
-            case HRESULT_FROM_WIN32(WSAETIMEDOUT):
+            if (hr == HRESULT_FROM_WIN32(WSAETIMEDOUT)) {
                 emit connectOpFinished(false, QAbstractSocket::NetworkError, WinRTSocketEngine::ConnectionTimeOutErrorString);
                 return S_OK;
-            case HRESULT_FROM_WIN32(WSAEHOSTUNREACH):
+            } else if (hr == HRESULT_FROM_WIN32(WSAEHOSTUNREACH)) {
                 emit connectOpFinished(false, QAbstractSocket::HostNotFoundError, WinRTSocketEngine::HostUnreachableErrorString);
                 return S_OK;
-            case HRESULT_FROM_WIN32(WSAECONNREFUSED):
+            } else if (hr == HRESULT_FROM_WIN32(WSAECONNREFUSED)) {
                 emit connectOpFinished(false, QAbstractSocket::ConnectionRefusedError, WinRTSocketEngine::ConnectionRefusedErrorString);
                 return S_OK;
-            default:
+            } else {
                 emit connectOpFinished(false, QAbstractSocket::UnknownSocketError, WinRTSocketEngine::UnknownSocketErrorString);
                 return S_OK;
             }
