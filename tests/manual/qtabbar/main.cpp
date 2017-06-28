qt/qtbase

commit 175f33ed855b0a8a30daafacd4f48fa3f8e76a9b

QMacStyle: update QTabBar style
Task-number: QTBUG-58266
Change-Id: I135e4dae44e2e97d73b7c7c97d8e682bcf459d75
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@qt.io>

src/widgets/widgets/qtabbar.cpp
@@ -519,12 +519,14 @@ void QTabBarPrivate::layoutTabs()
         maxExtent = maxWidth;
     }
 
+    if (!expanding) {
+        // Mirror our front item.
+        tabChain[tabChainIndex].init();
+        tabChain[tabChainIndex].expansive = (tabAlignment != Qt::AlignRight)
+                                            && (tabAlignment != Qt::AlignJustify);
+        tabChain[tabChainIndex].empty = true;
+    }
     Q_ASSERT(tabChainIndex == tabChain.count() - 1); // add an assert just to make sure.
-    // Mirror our front item.
-    tabChain[tabChainIndex].init();
-    tabChain[tabChainIndex].expansive = (tabAlignment != Qt::AlignRight)
-                                        && (tabAlignment != Qt::AlignJustify);
-    tabChain[tabChainIndex].empty = true;
 
     // Do the calculation
     qGeomCalc(tabChain, 0, tabChain.count(), 0, qMax(available, last), 0);
@@ -662,6 +664,15 @@ QRect QTabBarPrivate::normalizedScrollRect(int index)
 
         return QRect(leftEdge, 0, rightEdge - leftEdge, q->height());
     }
+}
+
+int QTabBarPrivate::hoveredTabIndex() const
+{
+    if (dragInProgress)
+        return currentIndex;
+    if (hoverIndex >= 0)
+        return hoverIndex;
+    return -1;
 }
 
 void QTabBarPrivate::makeVisible(int index)
@@ -1053,6 +1064,17 @@ void QTabBar::removeTab(int index)
         }
         d->refresh();
         d->autoHideTabs();
+        if (!d->hoverRect.isEmpty()) {
+            for (int i = 0; i < d->tabList.count(); ++i) {
+                const QRect area = tabRect(i);
+                if (area.contains(mapFromGlobal(QCursor::pos()))) {
+                    d->hoverIndex = i;
+                    d->hoverRect = area;
+                    break;
+                }
+            }
+            update(d->hoverRect);
+        }
         tabRemoved(index);
     }
 }
@@ -1577,20 +1599,28 @@ bool QTabBar::event(QEvent *event)
         QHoverEvent *he = static_cast<QHoverEvent *>(event);
         if (!d->hoverRect.contains(he->pos())) {
             QRect oldHoverRect = d->hoverRect;
+            bool cursorOverTabs = false;
             for (int i = 0; i < d->tabList.count(); ++i) {
                 QRect area = tabRect(i);
                 if (area.contains(he->pos())) {
+                    d->hoverIndex = i;
                     d->hoverRect = area;
+                    cursorOverTabs = true;
                     break;
                 }
+            }
+            if (!cursorOverTabs) {
+                d->hoverIndex = -1;
+                d->hoverRect = QRect();
             }
             if (he->oldPos() != QPoint(-1, -1))
                 update(oldHoverRect);
             update(d->hoverRect);
         }
         return true;
-    } else if (event->type() == QEvent::HoverLeave ) {
+    } else if (event->type() == QEvent::HoverLeave) {
         QRect oldHoverRect = d->hoverRect;
+        d->hoverIndex = -1;
         d->hoverRect = QRect();
         update(oldHoverRect);
         return true;
@@ -2435,7 +2465,7 @@ void QTabBar::setMovable(bool movable)
 
     This property is used as a hint for styles to draw the tabs in a different
     way then they would normally look in a tab widget.  On \macos this will
-    look similar to the tabs in Safari or Leopard's Terminal.app.
+    look similar to the tabs in Safari or Sierra's Terminal.app.
 
     \sa QTabWidget::documentMode
 */

tests/manual/qtabbar/main.cpp
@@ -1,0 +1,153 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QApplication>
+#include <QWidget>
+#include <QStackedWidget>
+#include <QTabBar>
+#include <QLabel>
+#include <QLayout>
+#include <QDesktopWidget>
+#include <QTabWidget>
+
+const int TabCount = 5;
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QWidget widget;
+    QStackedWidget stackedWidget;
+    QTabBar tabBar;
+    tabBar.setDocumentMode(true);
+    tabBar.setTabsClosable(true);
+    tabBar.setMovable(true);
+    tabBar.setExpanding(true);
+
+    // top
+    tabBar.setShape(QTabBar::RoundedNorth);
+    // bottom
+//    tabBar.setShape(QTabBar::RoundedSouth);
+    // left
+//    tabBar.setShape(QTabBar::RoundedWest);
+    // right
+//    tabBar.setShape(QTabBar::RoundedEast);
+
+    QMap<int, QWidget*> tabs;
+    for (int i = 0; i < TabCount; i++) {
+        QString tabNumberString = QString::number(i);
+        QLabel *label = new QLabel(QStringLiteral("Tab %1 content").arg(tabNumberString));
+        tabs[i] = label;
+        label->setAlignment(Qt::AlignCenter);
+        stackedWidget.addWidget(label);
+        tabBar.addTab(QStringLiteral("Tab %1").arg(tabNumberString));
+    }
+
+    QObject::connect(&tabBar, &QTabBar::tabMoved, [&tabs](int from, int to) {
+        QWidget *thisWidget = tabs[from];
+        QWidget *thatWidget = tabs[to];
+        tabs[from] = thatWidget;
+        tabs[to] = thisWidget;
+    });
+
+    QObject::connect(&tabBar, &QTabBar::currentChanged, [&stackedWidget, &tabs](int index) {
+        if (index >= 0)
+            stackedWidget.setCurrentWidget(tabs[index]);
+    });
+
+    QObject::connect(&tabBar, &QTabBar::tabCloseRequested, [&stackedWidget, &tabBar, &tabs](int index) {
+        QWidget *widget = tabs[index];
+        tabBar.removeTab(index);
+        for (int i = index + 1; i < TabCount; i++)
+            tabs[i-1] = tabs[i];
+        int currentIndex = tabBar.currentIndex();
+        if (currentIndex >= 0)
+            stackedWidget.setCurrentWidget(tabs[currentIndex]);
+        delete widget;
+    });
+
+    QLayout *layout;
+    switch (tabBar.shape()) {
+    case QTabBar::RoundedEast:
+    case QTabBar::TriangularEast:
+        tabBar.setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
+        layout = new QHBoxLayout(&widget);
+        layout->addWidget(&stackedWidget);
+        layout->addWidget(&tabBar);
+        break;
+    case QTabBar::RoundedWest:
+    case QTabBar::TriangularWest:
+        tabBar.setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
+        layout = new QHBoxLayout(&widget);
+        layout->addWidget(&tabBar);
+        layout->addWidget(&stackedWidget);
+        break;
+    case QTabBar::RoundedNorth:
+    case QTabBar::TriangularNorth:
+        tabBar.setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+        layout = new QVBoxLayout(&widget);
+        layout->addWidget(&tabBar);
+        layout->addWidget(&stackedWidget);
+        break;
+    case QTabBar::RoundedSouth:
+    case QTabBar::TriangularSouth:
+        tabBar.setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+        layout = new QVBoxLayout(&widget);
+        layout->addWidget(&stackedWidget);
+        layout->addWidget(&tabBar);
+        break;
+    }
+
+    layout->setMargin(0);
+    widget.resize(QApplication::desktop()->screenGeometry(&widget).size() * 0.5);
+    widget.show();
+
+    return app.exec();
+}

src/widgets/styles/qmacstyle_mac_p_p.h
@@ -229,6 +229,10 @@ public:
 
     void drawFocusRing(QPainter *p, const QRect &targetRect, int hMargin, int vMargin, qreal radius = 0) const;
 
+#ifndef QT_NO_TABBAR
+    void tabLayout(const QStyleOptionTab *opt, const QWidget *widget, QRect *textRect) const;
+#endif
+
 public:
     mutable QPointer<QObject> pressedButton;
     mutable QPointer<QObject> defaultButton;

src/widgets/widgets/qtabbar_p.h
@@ -87,7 +87,7 @@ class QTabBarPrivate  : public QWidgetPrivate
 public:
     QTabBarPrivate()
         :currentIndex(-1), pressedIndex(-1), shape(QTabBar::RoundedNorth), layoutDirty(false),
-        drawBase(true), scrollOffset(0), elideModeSetByUser(false), useScrollButtonsSetByUser(false), expanding(true), closeButtonOnTabs(false),
+        drawBase(true), scrollOffset(0), hoverIndex(-1), elideModeSetByUser(false), useScrollButtonsSetByUser(false), expanding(true), closeButtonOnTabs(false),
         selectionBehaviorOnRemove(QTabBar::SelectRightTab), paintWithOffsets(true), movable(false),
         dragInProgress(false), documentMode(false), autoHide(false), changeCurrentOnDrag(false),
         switchTabCurrentIndex(-1), switchTabTimerId(0), movingTab(0)
@@ -192,6 +192,7 @@ public:
     void moveTab(int index, int offset);
     void moveTabFinished(int index);
     QRect hoverRect;
+    int hoverIndex;
 
     void refresh();
     void layoutTabs();
@@ -202,6 +203,7 @@ public:
     void setupMovableTab();
     void autoHideTabs();
     QRect normalizedScrollRect(int index = -1);
+    int hoveredTabIndex() const;
 
     void initBasicStyleOption(QStyleOptionTab *option, int tabIndex) const;
 

src/widgets/widgets/qtabwidget.cpp
@@ -209,7 +209,6 @@ public:
     bool dirty;
     QTabWidget::TabPosition pos;
     QTabWidget::TabShape shape;
-    int alignment;
     QWidget *leftCornerWidget;
     QWidget *rightCornerWidget;
 };

tests/manual/manual.pro
@@ -52,7 +52,8 @@ xembed-widgets \
 shortcuts \
 dialogs \
 windowtransparency \
-unc
+unc \
+qtabbar
 
 !qtConfig(openssl): SUBDIRS -= qssloptions
 

tests/manual/qtabbar/qtabbar.pro
@@ -1,0 +1,4 @@
+TARGET   = qtabbar
+TEMPLATE = app
+QT       = core gui widgets
+SOURCES  = main.cpp

commit 6e9d24212db60743f13497f942713961d6601844

QTabBar: fix expanded tabs appearance
Fix regression introduced by 175f33ed8. 'expanding' property set to true
was ignored when QStyle::styleHint() returned Qt::AlignRight for
SH_TabBar_Alignment.

When we calculate tabs geometry, we put an empty tab at the front and
back and set its expansive attribute depending on tab alignment AND
'expanding' property.

Task-number: QTBUG-61480
Change-Id: I6a1827ae8a3f2c6bee5124c18c7f2b1c0a7862f3
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@qt.io>

src/widgets/widgets/qtabbar.cpp
@@ -447,9 +447,10 @@ void QTabBarPrivate::layoutTabs()
     QVector<QLayoutStruct> tabChain(tabList.count() + 2);
 
     // We put an empty item at the front and back and set its expansive attribute
-    // depending on tabAlignment.
+    // depending on tabAlignment and expanding.
     tabChain[tabChainIndex].init();
-    tabChain[tabChainIndex].expansive = (tabAlignment != Qt::AlignLeft)
+    tabChain[tabChainIndex].expansive = (!expanding)
+                                        && (tabAlignment != Qt::AlignLeft)
                                         && (tabAlignment != Qt::AlignJustify);
     tabChain[tabChainIndex].empty = true;
     ++tabChainIndex;
@@ -514,13 +515,12 @@ void QTabBarPrivate::layoutTabs()
         maxExtent = maxWidth;
     }
 
-    if (!expanding) {
-        // Mirror our front item.
-        tabChain[tabChainIndex].init();
-        tabChain[tabChainIndex].expansive = (tabAlignment != Qt::AlignRight)
-                                            && (tabAlignment != Qt::AlignJustify);
-        tabChain[tabChainIndex].empty = true;
-    }
+    // Mirror our front item.
+    tabChain[tabChainIndex].init();
+    tabChain[tabChainIndex].expansive = (!expanding)
+                                        && (tabAlignment != Qt::AlignRight)
+                                        && (tabAlignment != Qt::AlignJustify);
+    tabChain[tabChainIndex].empty = true;
     Q_ASSERT(tabChainIndex == tabChain.count() - 1); // add an assert just to make sure.
 
     // Do the calculation

tests/manual/qtabbar/main.cpp
@@ -56,12 +56,28 @@
 #include <QLayout>
 #include <QDesktopWidget>
 #include <QTabWidget>
+#include <QProxyStyle>
+
+class MyProxyStyle : public QProxyStyle
+{
+public:
+    int styleHint(StyleHint hint, const QStyleOption *option = 0,
+                  const QWidget *widget = 0, QStyleHintReturn *returnData = 0) const
+    {
+        if (hint == QStyle::SH_TabBar_Alignment)
+            return Qt::AlignLeft;
+//            return Qt::AlignRight;
+//            return Qt::AlignCenter;
+        return QProxyStyle::styleHint(hint, option, widget, returnData);
+    }
+};
 
 const int TabCount = 5;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setStyle(new MyProxyStyle);
 
     QWidget widget;
     QStackedWidget stackedWidget;
@@ -69,7 +85,7 @@ int main(int argc, char *argv[])
     tabBar.setDocumentMode(true);
     tabBar.setTabsClosable(true);
     tabBar.setMovable(true);
-    tabBar.setExpanding(true);
+    tabBar.setExpanding(false);
 
     // top
     tabBar.setShape(QTabBar::RoundedNorth);