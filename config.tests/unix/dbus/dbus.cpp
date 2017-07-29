1229558a40d269477a31b39e84cc5522489cf8c9
Oswald Buddenhagen <oswald.buddenhagen@qt.io>
2017-07-29 10:36:28 +0200

    configure: modernize dbus test
    remove useless define and ifdef/error, and check for a more appropriate
    function.
    
    Change-Id: I1a1622cc157c49ebb6787068ade7b33e45e228ca
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- config.tests/unix/dbus/dbus.cpp
+++ config.tests/unix/dbus/dbus.cpp
@@ -37,15 +37,10 @@
 **
 ****************************************************************************/
 
-#define DBUS_API_SUBJECT_TO_CHANGE
 #include <dbus/dbus.h>
-
-#if DBUS_MAJOR_PROTOCOL_VERSION < 1
-#error Needs at least dbus version 1
-#endif
 
 int main(int, char **)
 {
-    dbus_shutdown();
+    (void) dbus_bus_get_private(DBUS_BUS_SYSTEM, (DBusError *)NULL);
     return 0;
 }
