275c748ada9d869d803d5f446b48b915d686ab33
David Faure <david.faure@kdab.com>
2017-08-28 16:28:03 +0200

    Fix naming of new property QSortFilterProxyModel::recursiveFiltering
    Add "Enabled" to match Qt API naming rules.
    + fix \since tag.
    
    Change-Id: Iaf312648f7385cd7e8d3d101b561fbd4e955df25
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/corelib/itemmodels/qsortfilterproxymodel.h
+++ src/corelib/itemmodels/qsortfilterproxymodel.h
@@ -67,7 +67,7 @@ class Q_CORE_EXPORT QSortFilterProxyModel : public QAbstractProxyModel
     Q_PROPERTY(bool isSortLocaleAware READ isSortLocaleAware WRITE setSortLocaleAware)
     Q_PROPERTY(int sortRole READ sortRole WRITE setSortRole)
     Q_PROPERTY(int filterRole READ filterRole WRITE setFilterRole)
-    Q_PROPERTY(bool recursiveFiltering READ recursiveFiltering WRITE setRecursiveFiltering)
+    Q_PROPERTY(bool recursiveFilteringEnabled READ isRecursiveFilteringEnabled WRITE setRecursiveFilteringEnabled)
 
 public:
     explicit QSortFilterProxyModel(QObject *parent = Q_NULLPTR);
@@ -108,8 +108,8 @@ public:
     int filterRole() const;
     void setFilterRole(int role);
 
-    bool recursiveFiltering() const;
-    void setRecursiveFiltering(bool recursive);
+    bool isRecursiveFilteringEnabled() const;
+    void setRecursiveFilteringEnabled(bool recursive);
 
 public Q_SLOTS:
     void setFilterRegExp(const QString &pattern);
