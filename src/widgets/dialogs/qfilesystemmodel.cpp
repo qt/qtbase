77687bc64e9ae7172586271de91b266be7ab19ce
Friedemann Kleint <Friedemann.Kleint@qt.io>
2017-08-29 09:02:31 +0200

    QFileSystemModel::index(): Add a list size check
    This prevents an out of range assert when monitoring directory
    with activity (for example, temp).
    
    Task-number: QTBUG-62841
    Change-Id: Id3aa4098e9bbd665c7b17a667516885fa7c7f473
    Reviewed-by: Jesus Fernandez <Jesus.Fernandez@qt.io>
    Reviewed-by: Martin Rotter <rotter.martinos@gmail.com>
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

--- src/widgets/dialogs/qfilesystemmodel.cpp
+++ src/widgets/dialogs/qfilesystemmodel.cpp
@@ -260,7 +260,10 @@ QModelIndex QFileSystemModel::index(int row, int column, const QModelIndex &pare
     Q_ASSERT(parentNode);
 
     // now get the internal pointer for the index
-    const QString &childName = parentNode->visibleChildren.at(d->translateVisibleLocation(parentNode, row));
+    const int i = d->translateVisibleLocation(parentNode, row);
+    if (i >= parentNode->visibleChildren.size())
+        return QModelIndex();
+    const QString &childName = parentNode->visibleChildren.at(i);
     const QFileSystemModelPrivate::QFileSystemNode *indexNode = parentNode->children.value(childName);
     Q_ASSERT(indexNode);
 
