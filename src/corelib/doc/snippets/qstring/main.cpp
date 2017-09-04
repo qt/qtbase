c23c4a921d0e9e2d18a62af82d38ca27eac7bcb3
Friedemann Kleint <Friedemann.Kleint@qt.io>
2017-09-04 11:42:09 +0200

    Fix documentation code snippet of QString::arg(QStringView)
    Fix qdoc warning:
    src/corelib/tools/qstring.cpp:7809: warning: Command '\snippet (//! [11-qstringview])' failed at end of file 'qstring/main.cpp'
    
    Change-Id: Ie5a57bd94ba64ceab5179593f1df35be29c9985a
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- src/corelib/doc/snippets/qstring/main.cpp
+++ src/corelib/doc/snippets/qstring/main.cpp
@@ -263,14 +263,14 @@ void Widget::argFunction()
     //! [11]
 
     {
-    //! [11]
+    //! [11-qstringview]
     int i;                // current file's number
     int total;            // number of files to process
     QStringView fileName; // current file's name
 
     QString status = QString("Processing file %1 of %2: %3")
                     .arg(i).arg(total).arg(fileName);
-    //! [11]
+    //! [11-qstringview]
     }
 
     //! [12] //! [13]
