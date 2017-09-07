f43de3ed3175dfcd871e0e0ed57f09df2d1e09c2
Laszlo Agocs <laszlo.agocs@qt.io>
2017-09-07 13:46:15 +0200

    Add We mean it warning to the EDID table
    Change-Id: I3183a2eae372e37bbed9e96b953e3ead4246a543
    Reviewed-by: Pier Luigi Fiorini <pierluigi.fiorini@liri.io>

--- src/platformsupport/edid/qedidvendortable_p.h
+++ src/platformsupport/edid/qedidvendortable_p.h
@@ -47,6 +47,15 @@
 #ifndef QEDIDVENDORTABLE_P_H
 #define QEDIDVENDORTABLE_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+
 QT_BEGIN_NAMESPACE
 
 typedef struct VendorTable {
--- util/edid/qedidvendortable.py
+++ util/edid/qedidvendortable.py
@@ -82,6 +82,15 @@ header = """
 #ifndef QEDIDVENDORTABLE_P_H
 #define QEDIDVENDORTABLE_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+
 QT_BEGIN_NAMESPACE
 
 typedef struct VendorTable {
