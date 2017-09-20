e0c2d328b11ab893538393fad66ad61d22d823c6
Edward Welbourne <edward.welbourne@qt.io>
2017-09-20 10:28:48 +0200

    Fix #if-ery and accompanying comment
    The comment was back-to-front on the meaning it needed to address; and
    the #if-ery used a deprecated define, now changed to match what
    sanity-bot asked for.
    
    Change-Id: I0a971ab2e405e5908066da86964d67c8b852f114
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- tests/auto/network/kernel/qnetworkproxyfactory/tst_qnetworkproxyfactory.cpp
+++ tests/auto/network/kernel/qnetworkproxyfactory/tst_qnetworkproxyfactory.cpp
@@ -368,8 +368,8 @@ void tst_QNetworkProxyFactory::genericSystemProxy()
     QFETCH(QString, hostName);
     QFETCH(int, port);
 
-// The generic system proxy is only available on the following platforms
-#if (!defined Q_OS_WIN) && (!defined Q_OS_OSX) && !QT_CONFIG(libproxy)
+// We can only use the generic system proxy where available:
+#if !defined(Q_OS_WIN) && !defined(Q_OS_MACOS) && !QT_CONFIG(libproxy)
     qputenv(envVar, url);
     const QList<QNetworkProxy> systemProxy = QNetworkProxyFactory::systemProxyForQuery();
     QCOMPARE(systemProxy.size(), 1);
