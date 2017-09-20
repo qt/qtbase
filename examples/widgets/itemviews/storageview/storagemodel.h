29b38bea45712c85a0c9f07bfa26524f3a35668f
Friedemann Kleint <Friedemann.Kleint@qt.io>
2017-09-20 11:36:42 +0200

    Polish the StorageView example
    Introduce nullptr and override. Change the ready/valid columns to
    use check marks. Right-align the numerical columns. Add a shortcut
    to refresh. Sort by path, making sure the root volume is first.
    
    Task-number: QTBUG-60635
    Change-Id: I74cda7647f544902aaf4d2a0ab76986f1523aa6f
    Reviewed-by: Gatis Paeglis <gatis.paeglis@qt.io>

--- examples/widgets/itemviews/storageview/storagemodel.h
+++ examples/widgets/itemviews/storageview/storagemodel.h
@@ -74,13 +74,17 @@ public:
         ColumnCount
     };
 
-    explicit StorageModel(QObject *parent = 0);
+    explicit StorageModel(QObject *parent = nullptr);
 
-    int columnCount(const QModelIndex &parent) const;
-    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const override;
+    int rowCount(const QModelIndex &parent) const override;
 
-    QVariant data(const QModelIndex &index, int role) const;
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    QVariant data(const QModelIndex &index, int role) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
+public slots:
+    void refresh();
 
 private:
     QList<QStorageInfo> m_volumes;
