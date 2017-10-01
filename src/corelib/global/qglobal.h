291233a8d05b79a19e3ae2951824d1c0444cbd27
Jake Petroules <jake.petroules@qt.io>
2017-10-01 16:34:33 -0700

    Fix strict-prototypes warning with qVersion function
    This is enabled by default with Xcode 9 and would therefore be seen by
    anyone calling this function from C or Objective-C.
    
    Task-number: QTBUG-63450
    Change-Id: Iecd67017b6774c9f2fce2433002ff852058dd3ed
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -355,7 +355,7 @@ typedef double qreal;
 #if !defined(QT_NAMESPACE) && defined(__cplusplus) && !defined(Q_QDOC)
 extern "C"
 #endif
-Q_CORE_EXPORT const char *qVersion() Q_DECL_NOTHROW;
+Q_CORE_EXPORT const char *qVersion(void) Q_DECL_NOTHROW;
 
 #if defined(__cplusplus)
 
