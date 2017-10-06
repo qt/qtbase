qt/qtbase

commit 8e70241dccaf5a9e5c79c8d6da5665b881c5914d

Let QPlatformBackingStore handle its own QOpenGLContext
The resources allocated by QPlatformBackingStore are owned by the class,
and should be allocated in a context the class also owns. This removes
the asymmetry of having to pass in a context to composeAndFlush, while
having to make the same context current before destroying the platform
backingstore.

The context owned by QPlatformBackingStore is shared with the associated
window though a new QWindowPrivate::shareContext() API.

The result is that on e.g. iOS, the backingstore does not need to tie
the resource allocation of QPlatformBackingStore to the global share
context, but can instead tie them to the per-window context, and hence
clean them up after each window is closed.

Task-number: QTBUG-56653
Change-Id: Ic1bcae50dafeeafaa8d16a7febd83b840ec6367a
Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

src/gui/kernel/qwindow.cpp
@@ -45,6 +45,7 @@
 #ifndef QT_NO_OPENGL
 #include <qpa/qplatformopenglcontext.h>
 #include "qopenglcontext.h"
+#include "qopenglcontext_p.h"
 #endif
 #include "qscreen.h"
 
@@ -2632,6 +2633,11 @@ QWindow *QWindowPrivate::topLevelWindow() const
 
     return window;
 }
+
+QOpenGLContext *QWindowPrivate::shareContext() const
+{
+    return qt_gl_global_share_context();
+};
 
 /*!
     Creates a local representation of a window created by another process or by

src/gui/kernel/qwindow_p.h
@@ -130,6 +130,8 @@ public:
 
     QWindow *topLevelWindow() const;
 
+    virtual QOpenGLContext *shareContext() const;
+
     virtual QWindow *eventReceiver() { Q_Q(QWindow); return q; }
 
     virtual void setVisible(bool visible);

src/gui/painting/qplatformbackingstore.cpp
@@ -50,6 +50,7 @@
 #include <QtGui/QOpenGLFunctions>
 #ifndef QT_NO_OPENGL
 #include <QtGui/qopengltextureblitter.h>
+#include <QtGui/qoffscreensurface.h>
 #endif
 #include <qpa/qplatformgraphicsbuffer.h>
 #include <qpa/qplatformgraphicsbufferhelper.h>
@@ -95,14 +96,15 @@ public:
     ~QPlatformBackingStorePrivate()
     {
 #ifndef QT_NO_OPENGL
-        QOpenGLContext *ctx = QOpenGLContext::currentContext();
-        if (ctx) {
+        if (context) {
+            QOffscreenSurface offscreenSurface;
+            offscreenSurface.setFormat(context->format());
+            offscreenSurface.create();
+            context->makeCurrent(&offscreenSurface);
             if (textureId)
-                ctx->functions()->glDeleteTextures(1, &textureId);
+                context->functions()->glDeleteTextures(1, &textureId);
             if (blitter)
                 blitter->destroy();
-        } else if (textureId || blitter) {
-            qWarning("No context current during QPlatformBackingStore destruction, OpenGL resources not released");
         }
         delete blitter;
 #endif
@@ -110,6 +112,7 @@ public:
     QWindow *window;
     QBackingStore *backingStore;
 #ifndef QT_NO_OPENGL
+    QScopedPointer<QOpenGLContext> context;
     mutable GLuint textureId;
     mutable QSize textureSize;
     mutable bool needsSwizzle;
@@ -316,20 +319,31 @@ static void blitTextureForWidget(const QPlatformTextureList *textures, int idx, 
 
 void QPlatformBackingStore::composeAndFlush(QWindow *window, const QRegion &region,
                                             const QPoint &offset,
-                                            QPlatformTextureList *textures, QOpenGLContext *context,
+                                            QPlatformTextureList *textures,
                                             bool translucentBackground)
 {
     if (!qt_window_private(window)->receivedExpose)
         return;
 
-    if (!context->makeCurrent(window)) {
+    if (!d_ptr->context) {
+        d_ptr->context.reset(new QOpenGLContext);
+        d_ptr->context->setFormat(d_ptr->window->requestedFormat());
+        d_ptr->context->setScreen(d_ptr->window->screen());
+        d_ptr->context->setShareContext(qt_window_private(d_ptr->window)->shareContext());
+        if (!d_ptr->context->create()) {
+            qWarning("composeAndFlush: QOpenGLContext creation failed");
+            return;
+        }
+    }
+
+    if (!d_ptr->context->makeCurrent(window)) {
         qWarning("composeAndFlush: makeCurrent() failed");
         return;
     }
 
     QWindowPrivate::get(window)->lastComposeTime.start();
 
-    QOpenGLFunctions *funcs = context->functions();
+    QOpenGLFunctions *funcs = d_ptr->context->functions();
     funcs->glViewport(0, 0, window->width() * window->devicePixelRatio(), window->height() * window->devicePixelRatio());
     funcs->glClearColor(0, 0, 0, translucentBackground ? 0 : 1);
     funcs->glClear(GL_COLOR_BUFFER_BIT);
@@ -435,7 +449,7 @@ void QPlatformBackingStore::composeAndFlush(QWindow *window, const QRegion &regi
     funcs->glDisable(GL_BLEND);
     d_ptr->blitter->release();
 
-    context->swapBuffers(window);
+    d_ptr->context->swapBuffers(window);
 }
 #endif
 /*!

src/gui/painting/qplatformbackingstore.h
@@ -120,7 +120,7 @@ public:
     virtual void flush(QWindow *window, const QRegion &region, const QPoint &offset) = 0;
 #ifndef QT_NO_OPENGL
     virtual void composeAndFlush(QWindow *window, const QRegion &region, const QPoint &offset,
-                                 QPlatformTextureList *textures, QOpenGLContext *context,
+                                 QPlatformTextureList *textures,
                                  bool translucentBackground);
 #endif
     virtual QImage toImage() const;

src/platformsupport/platformcompositor/qopenglcompositorbackingstore.cpp
@@ -202,14 +202,13 @@ void QOpenGLCompositorBackingStore::flush(QWindow *window, const QRegion &region
 }
 
 void QOpenGLCompositorBackingStore::composeAndFlush(QWindow *window, const QRegion &region, const QPoint &offset,
-                                               QPlatformTextureList *textures, QOpenGLContext *context,
+                                               QPlatformTextureList *textures,
                                                bool translucentBackground)
 {
     // QOpenGLWidget/QQuickWidget content provided as textures. The raster content goes on top.
 
     Q_UNUSED(region);
     Q_UNUSED(offset);
-    Q_UNUSED(context);
     Q_UNUSED(translucentBackground);
 
     QOpenGLCompositor *compositor = QOpenGLCompositor::instance();
@@ -218,7 +217,7 @@ void QOpenGLCompositorBackingStore::composeAndFlush(QWindow *window, const QRegi
 
     // The compositor's context and the context to which QOpenGLWidget/QQuickWidget
     // textures belong are not the same. They share resources, though.
-    Q_ASSERT(context->shareGroup() == dstCtx->shareGroup());
+    Q_ASSERT(qt_window_private(window)->shareContext()->shareGroup() == dstCtx->shareGroup());
 
     QWindow *dstWin = compositor->targetWindow();
     if (!dstWin)

src/platformsupport/platformcompositor/qopenglcompositorbackingstore_p.h
@@ -75,7 +75,7 @@ public:
 
     QImage toImage() const Q_DECL_OVERRIDE;
     void composeAndFlush(QWindow *window, const QRegion &region, const QPoint &offset,
-                         QPlatformTextureList *textures, QOpenGLContext *context,
+                         QPlatformTextureList *textures,
                          bool translucentBackground) Q_DECL_OVERRIDE;
 
     const QPlatformTextureList *textures() const { return m_textures; }

src/plugins/platforms/ios/qiosbackingstore.h
@@ -55,9 +55,6 @@ public:
     ~QIOSBackingStore();
 
     void flush(QWindow *window, const QRegion &region, const QPoint &offset) Q_DECL_OVERRIDE;
-
-private:
-    QOpenGLContext *m_context;
 };
 
 QT_END_NAMESPACE

src/plugins/platforms/ios/qiosbackingstore.mm
@@ -55,7 +55,6 @@ QT_BEGIN_NAMESPACE
 */
 QIOSBackingStore::QIOSBackingStore(QWindow *window)
     : QRasterBackingStore(window)
-    , m_context(new QOpenGLContext)
 {
     // We use the surface both for raster operations and for GL drawing (when
     // we blit the raster image), so the type needs to cover both use cases.
@@ -64,22 +63,10 @@ QIOSBackingStore::QIOSBackingStore(QWindow *window)
 
     Q_ASSERT_X(window->surfaceType() != QSurface::OpenGLSurface, "QIOSBackingStore",
         "QBackingStore on iOS can only be used with raster-enabled surfaces.");
-
-    m_context->setFormat(window->requestedFormat());
-    m_context->setScreen(window->screen());
-    Q_ASSERT(QOpenGLContext::globalShareContext());
-    m_context->setShareContext(QOpenGLContext::globalShareContext());
-    m_context->create();
 }
 
 QIOSBackingStore::~QIOSBackingStore()
 {
-    // We're using composeAndFlush from QPlatformBackingStore, which
-    // need to clean up any textures in its destructor, so make the
-    // context current and keep it alive until QPlatformBackingStore
-    // has cleaned up everything.
-    m_context->makeCurrent(window());
-    m_context->deleteLater();
 }
 
 void QIOSBackingStore::flush(QWindow *window, const QRegion &region, const QPoint &offset)
@@ -98,7 +85,7 @@ void QIOSBackingStore::flush(QWindow *window, const QRegion &region, const QPoin
     }
 
     static QPlatformTextureList emptyTextureList;
-    composeAndFlush(window, region, offset, &emptyTextureList, m_context, false);
+    composeAndFlush(window, region, offset, &emptyTextureList, false);
 }
 
 QT_END_NAMESPACE

commit 467cede61e1bd8754d1e4df0fc2f281ee9082f18

Fix build with -no-opengl and on watchOS in general
This fixes a regression introduced in 8e70241dcc.

Task-number: QTBUG-63631
Change-Id: I5a2c23e06b790a482e1542ac2db3dcf25927caf2
Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/gui/kernel/qwindow.cpp
@@ -2634,10 +2634,12 @@ QWindow *QWindowPrivate::topLevelWindow() const
     return window;
 }
 
+#if QT_CONFIG(opengl)
 QOpenGLContext *QWindowPrivate::shareContext() const
 {
     return qt_gl_global_share_context();
 };
+#endif
 
 /*!
     Creates a local representation of a window created by another process or by

src/gui/kernel/qwindow_p.h
@@ -130,7 +130,9 @@ public:
 
     QWindow *topLevelWindow() const;
 
+#if QT_CONFIG(opengl)
     virtual QOpenGLContext *shareContext() const;
+#endif
 
     virtual QWindow *eventReceiver() { Q_Q(QWindow); return q; }
 