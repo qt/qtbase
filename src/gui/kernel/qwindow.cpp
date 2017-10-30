qt/qtbase

commit a3d59c7c7f675b0a4e128efeb781aa1c2f7db4c0

Block input to a window shown while an application modal dialog is visible
Although the window is refused input for the most part from the system,
it does not act like that it is blocked by the application modal dialog.
This ensures that it is the case and prevents things like being able to
double click on the title bar to maximize the window on Windows.

Task-number: QTBUG-49102
Change-Id: If1582819b90cb2ec9d891f664da24f13bfec7103
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

src/gui/kernel/qwindow.cpp
@@ -576,6 +576,8 @@ void QWindow::setVisible(bool visible)
             QGuiApplicationPrivate::showModalWindow(this);
         else
             QGuiApplicationPrivate::hideModalWindow(this);
+    } else if (visible && QGuiApplication::modalWindow()) {
+        QGuiApplicationPrivate::updateBlockedStatus(this);
     }
 
 #ifndef QT_NO_CURSOR

tests/auto/gui/kernel/qwindow/tst_qwindow.cpp
@@ -105,6 +105,7 @@ private slots:
     void stateChange();
     void flags();
     void cleanup();
+    void testBlockingWindowShownAfterModalDialog();
 
 private:
     QPoint m_availableTopLeft;
@@ -2239,6 +2240,47 @@ void tst_QWindow::flags()
     QCOMPARE(window.flags(), baseFlags | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
     window.setFlag(Qt::FramelessWindowHint, false);
     QCOMPARE(window.flags(), baseFlags | Qt::WindowStaysOnTopHint);
+}
+
+class EventWindow : public QWindow
+{
+public:
+    EventWindow() : QWindow(), gotBlocked(false) {}
+    bool gotBlocked;
+protected:
+    bool event(QEvent *e)
+    {
+        if (e->type() == QEvent::WindowBlocked)
+            gotBlocked = true;
+        return QWindow::event(e);
+    }
+};
+
+void tst_QWindow::testBlockingWindowShownAfterModalDialog()
+{
+    EventWindow normalWindow;
+    normalWindow.setFramePosition(m_availableTopLeft + QPoint(80, 80));
+    normalWindow.resize(m_testWindowSize);
+    normalWindow.show();
+    QVERIFY(QTest::qWaitForWindowExposed(&normalWindow));
+    QVERIFY(!normalWindow.gotBlocked);
+
+    QWindow dialog;
+    dialog.setFramePosition(m_availableTopLeft + QPoint(200, 200));
+    dialog.resize(m_testWindowSize);
+    dialog.setModality(Qt::ApplicationModal);
+    dialog.setFlags(Qt::Dialog);
+    dialog.show();
+    QVERIFY(QTest::qWaitForWindowExposed(&dialog));
+    QVERIFY(normalWindow.gotBlocked);
+
+    EventWindow normalWindowAfter;
+    normalWindowAfter.setFramePosition(m_availableTopLeft + QPoint(80, 80));
+    normalWindowAfter.resize(m_testWindowSize);
+    QVERIFY(!normalWindowAfter.gotBlocked);
+    normalWindowAfter.show();
+    QVERIFY(QTest::qWaitForWindowExposed(&normalWindowAfter));
+    QVERIFY(normalWindowAfter.gotBlocked);
 }
 
 #include <tst_qwindow.moc>

commit acdb3340321d1b8823b54f2ea492f975c6f942d8

Fix dragging inside a modal window when a QShapedPixmapWindow is used
A regression was introduced with a3d59c7c7f675b0a4e128efeb781aa1c2f7db4c0
which caused dragging to fail within a modal dialog on the XCB platform.
By adding an exception for the QShapedPixmapWindow, which is the window
used for the drag, we can allow that to continue to work whilst blocking
to the other newly created windows.

Task-number: QTBUG-63846
Change-Id: I7c7f365f30fcf5f04f50dc1a7fff7a09e6e5ed6c
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/gui/kernel/qwindow.cpp
@@ -54,6 +54,7 @@
 #  include "qaccessible.h"
 #endif
 #include "qhighdpiscaling_p.h"
+#include "qshapedpixmapdndwindow_p.h"
 
 #include <private/qevent_p.h>
 
@@ -576,7 +577,9 @@ void QWindow::setVisible(bool visible)
             QGuiApplicationPrivate::showModalWindow(this);
         else
             QGuiApplicationPrivate::hideModalWindow(this);
-    } else if (visible && QGuiApplication::modalWindow()) {
+    // QShapedPixmapWindow is used on some platforms for showing a drag pixmap, so don't block
+    // input to this window as it is performing a drag - QTBUG-63846
+    } else if (visible && QGuiApplication::modalWindow() && !qobject_cast<QShapedPixmapWindow *>(this)) {
         QGuiApplicationPrivate::updateBlockedStatus(this);
     }
 