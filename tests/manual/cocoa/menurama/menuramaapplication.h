f55a40ac039ee1734ddf06c0ddb33cf760b6ebb9
Gabriel de Dietrich <gabriel.dedietrich@qt.io>
2017-11-14 09:23:49 -0700

    Menurama: Fix custom application class constructor signature
    Change-Id: I989b9205dde9280f97dedbaad47ef4a8d75004ac
    Reviewed-by: Jake Petroules <jake.petroules@qt.io>

--- tests/manual/cocoa/menurama/menuramaapplication.h
+++ tests/manual/cocoa/menurama/menuramaapplication.h
@@ -36,7 +36,7 @@
 class MenuramaApplication : public QApplication
 {
 public:
-    MenuramaApplication(int argc, char **argv);
+    MenuramaApplication(int &argc, char **argv);
     void addDynMenu(QLatin1String title, QMenu *parentMenu);
     QAction *findAction(QLatin1String title, QMenu *parentMenu);
 
