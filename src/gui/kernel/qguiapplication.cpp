58541d1c68fcf121318fe153e1469b9d0ebfec63
Jesus Fernandez <jesus.fernandez@qt.io>
2018-01-23 12:39:48 +0100

    QGuiApplicationPrivate::processDrag(): Add assert
    CID 186303 (#1 of 3): Dereference after null check (FORWARD_NULL)
    var_deref_model: Passing null pointer w to sendEvent, which dereferences it.
    
    Coverity-Id: 186303
    Change-Id: If398165858b91508ccdf4be0c239d6bb104d78fa
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/gui/kernel/qguiapplication.cpp
+++ src/gui/kernel/qguiapplication.cpp
@@ -2882,7 +2882,8 @@ QPlatformDragQtResponse QGuiApplicationPrivate::processDrag(QWindow *w, const QM
     static QPointer<QWindow> currentDragWindow;
     static Qt::DropAction lastAcceptedDropAction = Qt::IgnoreAction;
     QPlatformDrag *platformDrag = platformIntegration()->drag();
-    if (!platformDrag || (w && w->d_func()->blockedByModalWindow)) {
+    Q_ASSERT(w);
+    if (!platformDrag || w->d_func()->blockedByModalWindow) {
         lastAcceptedDropAction = Qt::IgnoreAction;
         return QPlatformDragQtResponse(false, lastAcceptedDropAction, QRect());
     }
