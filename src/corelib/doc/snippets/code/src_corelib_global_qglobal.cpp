abc891ee7cf12ae3b359811883d674c61793cc1d
Thiago Macieira <thiago.macieira@intel.com>
2018-02-08 08:32:45 -0800

    Documentation: don't suggest abort() in qInstallMessageHandler()
    The backend will do the aborting. The handler doesn't need to do it.
    
    Task-number: QTBUG-66295
    Change-Id: I3debfc11127e4516b505fffd151166d4a8e9ec53
    Reviewed-by: Martin Smith <martin.smith@qt.io>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/corelib/doc/snippets/code/src_corelib_global_qglobal.cpp
+++ src/corelib/doc/snippets/code/src_corelib_global_qglobal.cpp
@@ -294,7 +294,7 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
         break;
     case QtFatalMsg:
         fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-        abort();
+        break;
     }
 }
 
