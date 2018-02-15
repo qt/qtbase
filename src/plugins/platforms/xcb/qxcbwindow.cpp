qt/qtbase

commit cb142954c54b7a6e391950d9209b5cea9252092b

XCB: Implement native window dump for diaglib
Extract a helper function to determine the window title from QXcbConnection
and add an invokable function to the native interface that dumps the window
tree similar to existing functionality on Windows.

Change-Id: I5544d69ea2b801eb16d3b5b8d64021b3e567b0d8
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/plugins/platforms/xcb/qxcbwindow.cpp
@@ -2835,5 +2835,18 @@ QXcbScreen *QXcbWindow::xcbScreen() const
     return static_cast<QXcbScreen *>(screen());
 }
 
+QString QXcbWindow::windowTitle(const QXcbConnection *conn, xcb_window_t window)
+{
+    const xcb_atom_t utf8Atom = conn->atom(QXcbAtom::UTF8_STRING);
+    auto reply = Q_XCB_REPLY_UNCHECKED(xcb_get_property, conn->xcb_connection(),
+                                       false, window, conn->atom(QXcbAtom::_NET_WM_NAME),
+                                       utf8Atom, 0, 1024);
+    if (reply && reply->format == 8 && reply->type == utf8Atom) {
+        const char *name = reinterpret_cast<const char *>(xcb_get_property_value(reply.get()));
+        return QString::fromUtf8(name);
+    }
+    return QString();
+}
+
 QT_END_NAMESPACE
 

src/plugins/platforms/xcb/qxcbnativeinterface.cpp
@@ -673,4 +673,60 @@ void *QXcbNativeInterface::handlerNativeResourceForBackingStore(const QByteArray
     return nullptr;
 }
 
+static void dumpNativeWindowsRecursion(const QXcbConnection *connection, xcb_window_t window,
+                                       int level, QTextStream &str)
+{
+    if (level)
+        str << QByteArray(2 * level, ' ');
+
+    xcb_connection_t *conn = connection->xcb_connection();
+    auto geomReply = Q_XCB_REPLY(xcb_get_geometry, conn, window);
+    if (!geomReply)
+        return;
+    const QRect geom(geomReply->x, geomReply->y, geomReply->width, geomReply->height);
+    if (!geom.isValid() || (geom.width() <= 3 && geom.height() <= 3))
+        return; // Skip helper/dummy windows.
+    str << "0x";
+    const int oldFieldWidth = str.fieldWidth();
+    const QChar oldPadChar =str.padChar();
+    str.setFieldWidth(8);
+    str.setPadChar(QLatin1Char('0'));
+    str << hex << window;
+    str.setFieldWidth(oldFieldWidth);
+    str.setPadChar(oldPadChar);
+    str << dec << " \""
+        << QXcbWindow::windowTitle(connection, window) << "\" "
+        << geom.width() << 'x' << geom.height() << forcesign << geom.x() << geom.y()
+        << noforcesign << '\n';
+
+    auto reply = Q_XCB_REPLY(xcb_query_tree, conn, window);
+    if (reply) {
+        const int count = xcb_query_tree_children_length(reply.get());
+        const xcb_window_t *children = xcb_query_tree_children(reply.get());
+        for (int i = 0; i < count; ++i)
+            dumpNativeWindowsRecursion(connection, children[i], level + 1, str);
+    }
+}
+
+QString QXcbNativeInterface::dumpConnectionNativeWindows(const QXcbConnection *connection, WId root) const
+{
+    QString result;
+    QTextStream str(&result);
+    if (root) {
+        dumpNativeWindowsRecursion(connection, xcb_window_t(root), 0, str);
+    } else {
+        for (const QXcbScreen *screen : connection->screens()) {
+            str << "Screen: \"" << screen->name() << "\"\n";
+            dumpNativeWindowsRecursion(connection, screen->root(), 0, str);
+            str << '\n';
+        }
+    }
+    return result;
+}
+
+QString QXcbNativeInterface::dumpNativeWindows(WId root) const
+{
+    return dumpConnectionNativeWindows(QXcbIntegration::instance()->defaultConnection(), root);
+}
+
 QT_END_NAMESPACE

src/plugins/platforms/xcb/qxcbnativeinterface.h
@@ -127,6 +127,8 @@ public:
     Q_INVOKABLE bool systrayVisualHasAlphaChannel();
     Q_INVOKABLE bool requestSystemTrayWindowDock(const QWindow *window);
     Q_INVOKABLE QRect systemTrayWindowGlobalGeometry(const QWindow *window);
+    Q_INVOKABLE QString dumpConnectionNativeWindows(const QXcbConnection *connection, WId root) const;
+    Q_INVOKABLE QString dumpNativeWindows(WId root = 0) const;
 
     void addHandler(QXcbNativeInterfaceHandler *handler);
     void removeHandler(QXcbNativeInterfaceHandler *handler);

src/plugins/platforms/xcb/qxcbscreen.cpp
@@ -91,16 +91,8 @@ QXcbVirtualDesktop::QXcbVirtualDesktop(QXcbConnection *connection, xcb_screen_t 
     if (reply && reply->format == 32 && reply->type == XCB_ATOM_WINDOW) {
         xcb_window_t windowManager = *((xcb_window_t *)xcb_get_property_value(reply.get()));
 
-        if (windowManager != XCB_WINDOW_NONE) {
-            auto windowManagerReply = Q_XCB_REPLY_UNCHECKED(xcb_get_property, xcb_connection(),
-                                                            false, windowManager,
-                                                            atom(QXcbAtom::_NET_WM_NAME),
-                                                            atom(QXcbAtom::UTF8_STRING), 0, 1024);
-            if (windowManagerReply && windowManagerReply->format == 8 && windowManagerReply->type == atom(QXcbAtom::UTF8_STRING)) {
-                m_windowManagerName = QString::fromUtf8((const char *)xcb_get_property_value(windowManagerReply.get()),
-                                                        xcb_get_property_value_length(windowManagerReply.get()));
-            }
-        }
+        if (windowManager != XCB_WINDOW_NONE)
+            m_windowManagerName = QXcbWindow::windowTitle(connection, windowManager);
     }
 
     const xcb_query_extension_reply_t *sync_reply = xcb_get_extension_data(xcb_connection(), &xcb_sync_id);

src/plugins/platforms/xcb/qxcbwindow.h
@@ -182,6 +182,8 @@ public:
     virtual void create();
     virtual void destroy();
 
+    static QString windowTitle(const QXcbConnection *conn, xcb_window_t window);
+
 public Q_SLOTS:
     void updateSyncRequestCounter();
 

tests/manual/diaglib/nativewindowdump.cpp
@@ -28,10 +28,25 @@
 
 #include "nativewindowdump.h"
 
+#if QT_VERSION >= 0x050000
+#  include <QtGui/QGuiApplication>
+#  include <qpa/qplatformnativeinterface.h>
+#endif
+
+#include <QtCore/QDebug>
+
 namespace QtDiag {
 
-void dumpNativeWindows(WId)
+void dumpNativeWindows(WId wid)
 {
+#if QT_VERSION >= 0x050000
+    QPlatformNativeInterface *ni = QGuiApplication::platformNativeInterface();
+    QString result;
+    QMetaObject::invokeMethod(ni, "dumpNativeWindows", Qt::DirectConnection,
+                              Q_RETURN_ARG(QString, result),
+                              Q_ARG(WId, wid));
+    qDebug().noquote() << result;
+#endif // Qt 5
 }
 
 void dumpNativeQtTopLevels()

commit 2a80c04d3bbc45b5d1293a5141839c0b4719f772

Fix crash when reading window titles with XCB
This is a regression introduced with commit
cb142954c54b7a6e391950d9209b5cea9252092b that changed the code from
using QString:fromUtf8(name, propertyLength) to QString::fromUtf8(name),
assuming that the property name is a zero-terminated string. That
however is not correct.

ASAN trace:

==4039==ERROR: AddressSanitizer: heap-buffer-overflow on address 0x60400001e0b4 at pc 0x7f3383c7d66e bp 0x7ffdc8e3d9b0 sp 0x7ffdc8e3d158
READ of size 5 at 0x60400001e0b4 thread T0
    #0 0x7f3383c7d66d  (/usr/lib/x86_64-linux-gnu/libasan.so.4+0x5166d)
    #1 0x7f337602f32a in QString::fromUtf8(char const*, int) ../../../../include/QtCore/../../src/corelib/tools/qstring.h:569
    #2 0x7f337602f32a in QXcbWindow::windowTitle(QXcbConnection const*, unsigned int) /home/simon/dev/qt-5.11/qtbase/src/plugins/platforms/xcb/qxcbwindow.cpp:2861
[...]

0x60400001e0b4 is located 0 bytes to the right of 36-byte region [0x60400001e090,0x60400001e0b4)
allocated by thread T1 (QXcbEventReader) here:
    #0 0x7f3383d0ab50 in __interceptor_malloc (/usr/lib/x86_64-linux-gnu/libasan.so.4+0xdeb50)
    #1 0x7f337b397e2b  (/usr/lib/x86_64-linux-gnu/libxcb.so.1+0xde2b)

Change-Id: Ia5024602d3aacb924b5dcd3956672da2a8f10feb
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

src/plugins/platforms/xcb/qxcbwindow.cpp
@@ -2857,7 +2857,7 @@ QString QXcbWindow::windowTitle(const QXcbConnection *conn, xcb_window_t window)
                                        utf8Atom, 0, 1024);
     if (reply && reply->format == 8 && reply->type == utf8Atom) {
         const char *name = reinterpret_cast<const char *>(xcb_get_property_value(reply.get()));
-        return QString::fromUtf8(name);
+        return QString::fromUtf8(name, xcb_get_property_value_length(reply.get()));
     }
     return QString();
 }