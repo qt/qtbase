3697366642f05dd9239f730b48feea165f411ce6
Anton Kudryavtsev <anton.kudryavtsev@corp.mail.ru>
2018-04-10 12:39:29 +0300

    QSplitter: fix doc for widget() and handle()
    Change-Id: I7b8a0ba9f6b99eae22c2230c761d7a14045256db
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- src/widgets/widgets/qsplitter.cpp
+++ src/widgets/widgets/qsplitter.cpp
@@ -1234,9 +1234,9 @@ QSplitterHandle *QSplitter::createHandle()
 }
 
 /*!
-    Returns the handle to the left (or above) for the item in the
-    splitter's layout at the given \a index. The handle at index 0 is
-    always hidden.
+    Returns the handle to the left of (or above) the item in the
+    splitter's layout at the given \a index, or \c nullptr if there is no such item.
+    The handle at index 0 is always hidden.
 
     For right-to-left languages such as Arabic and Hebrew, the layout
     of horizontal splitters is reversed. The handle will be to the
@@ -1253,7 +1253,8 @@ QSplitterHandle *QSplitter::handle(int index) const
 }
 
 /*!
-    Returns the widget at the given \a index in the splitter's layout.
+    Returns the widget at the given \a index in the splitter's layout,
+    or \c nullptr if there is no such widget.
 
     \sa count(), handle(), indexOf(), insertWidget()
 */
