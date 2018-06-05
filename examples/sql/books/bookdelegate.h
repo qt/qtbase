fcbfa534fab7dc0e989382a664031a42ba35ee92
Paul Wicking <paul.wicking@qt.io>
2018-06-05 13:55:43 +0200

    Doc: Clean up SQL example: Books (part 2)
    Improve readability, enforce consistent code formatting.
    
    Task-number: QTBUG-68652
    Change-Id: I7bbe0b2baa6273e0133df84d7c3cc2aa6a0b6ff2
    Reviewed-by: Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>

--- examples/sql/books/bookdelegate.h
+++ examples/sql/books/bookdelegate.h
@@ -66,7 +66,8 @@ public:
     void paint(QPainter *painter, const QStyleOptionViewItem &option,
                const QModelIndex &index) const override;
 
-    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+    QSize sizeHint(const QStyleOptionViewItem &option,
+                   const QModelIndex &index) const override;
 
     bool editorEvent(QEvent *event, QAbstractItemModel *model,
                      const QStyleOptionViewItem &option,
