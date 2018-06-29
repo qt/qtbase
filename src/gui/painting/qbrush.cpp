13337096f9c2090b97e839102cefaacfaafdd870
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2018-06-29 14:56:41 +0200

    Re-enable QGradient::Preset documentation by explicitly ignoring warnings
    Change-Id: Ibde6d905c74a571dc2e9d2942e99682e0a4f9b8c
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- src/gui/painting/qbrush.cpp
+++ src/gui/painting/qbrush.cpp
@@ -1341,8 +1341,6 @@ QGradient::QGradient()
     \enum QGradient::Preset
     \since 5.12
 
-    \internal
-
     This enum specifies a set of predefined presets for QGradient,
     based on the gradients from https://webgradients.com/.
 */
