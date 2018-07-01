32591b660e498e9e1178fad1fd0070c6aaea8177
Thiago Macieira <thiago.macieira@intel.com>
2018-07-01 14:52:15 -0700

    Update the Qt version tagging to adapt to expected Qt 5.16
    Change-Id: I61ecce6b1324410bbab4fffd153d5d362d4429bb
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/global/qversiontagging.cpp
+++ src/corelib/global/qversiontagging.cpp
@@ -93,7 +93,16 @@ make_versioned_symbol(SYM, QT_VERSION_MAJOR, 11, "@");
 make_versioned_symbol(SYM, QT_VERSION_MAJOR, 12, "@");
 #endif
 #if QT_VERSION_MINOR > 13
-// We don't expect there will be a Qt 5.13
+make_versioned_symbol(SYM, QT_VERSION_MAJOR, 13, "@");
+#endif
+#if QT_VERSION_MINOR > 14
+make_versioned_symbol(SYM, QT_VERSION_MAJOR, 14, "@");
+#endif
+#if QT_VERSION_MINOR > 15
+make_versioned_symbol(SYM, QT_VERSION_MAJOR, 15, "@");
+#endif
+#if QT_VERSION_MINOR > 16
+// We don't expect there will be a Qt 5.17
 #  error "Please update this file with more Qt versions."
 #endif
 
