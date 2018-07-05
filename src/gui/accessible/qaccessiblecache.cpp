23e20e4030aee001ec24301cf5c7ff0a6cd2bc16
Frederik Gladhorn <frederik.gladhorn@qt.io>
2018-07-05 10:10:08 +0200

    Add logging for QAccessibleCache
    Change-Id: I2cbaf6c6dfb80b2b5c2732f1633da411fd816710
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

--- src/gui/accessible/qaccessiblecache.cpp
+++ src/gui/accessible/qaccessiblecache.cpp
@@ -38,10 +38,14 @@
 ****************************************************************************/
 
 #include "qaccessiblecache_p.h"
+#include <QtCore/qdebug.h>
+#include <QtCore/qloggingcategory.h>
 
 #ifndef QT_NO_ACCESSIBILITY
 
 QT_BEGIN_NAMESPACE
+
+Q_LOGGING_CATEGORY(lcAccessibilityCache, "qt.accessibility.cache");
 
 /*!
     \class QAccessibleCache
@@ -122,6 +126,7 @@ QAccessible::Id QAccessibleCache::insert(QObject *object, QAccessibleInterface *
     }
     idToInterface.insert(id, iface);
     interfaceToId.insert(iface, id);
+    qCDebug(lcAccessibilityCache) << "insert - id:" << id << " iface:" << iface;
     return id;
 }
 
@@ -137,6 +142,7 @@ void QAccessibleCache::objectDestroyed(QObject* obj)
 void QAccessibleCache::deleteInterface(QAccessible::Id id, QObject *obj)
 {
     QAccessibleInterface *iface = idToInterface.take(id);
+    qCDebug(lcAccessibilityCache) << "delete - id:" << id << " iface:" << iface;
     if (!iface) // the interface may be deleted already
         return;
     interfaceToId.take(iface);
