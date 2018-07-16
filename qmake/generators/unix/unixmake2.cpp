qt/qtbase

commit 7c34e0a7b48572be1f9e3fb45911a13b01798e37

qmake: escape colons and hashmarks in dependency paths
these characters can appear in file names, but are meta characters in
dependency context. they have different semantics in make commands, so
this required some reshuffling in the windows generator (which just
treated dependencies and commands the same way).

we don't actually escape colons for nmake, because it has magic
treatment of drive letters anyway (and colons cannot appear elsewhere).
also, if a target's filename gets quoted, batch rules will blow up.
therefore, "funny" file names are really only supported as inputs -
which is just enough to make resource embedding work.

Task-number: QTBUG-22863
Task-number: QTBUG-68635
Change-Id: I473b0bf47d045298fd2ae481a29de603a3c1be30
Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

qmake/generators/win32/mingw_make.cpp
@@ -46,8 +46,7 @@ QString MingwMakefileGenerator::escapeDependencyPath(const QString &path) const
 {
     QString ret = path;
     ret.replace('\\', "/");  // ### this shouldn't be here
-    ret.replace(' ', QLatin1String("\\ "));
-    return ret;
+    return MakefileGenerator::escapeDependencyPath(ret);
 }
 
 QString MingwMakefileGenerator::getManifestFileForRcFile() const

qmake/generators/win32/winmakefile.cpp
@@ -773,6 +773,18 @@ QString Win32MakefileGenerator::escapeFilePath(const QString &path) const
     return ret;
 }
 
+QString Win32MakefileGenerator::escapeDependencyPath(const QString &path) const
+{
+    QString ret = path;
+    if (!ret.isEmpty()) {
+        static const QRegExp criticalChars(QStringLiteral("([\t #])"));
+        if (ret.contains(criticalChars))
+            ret = "\"" + ret + "\"";
+        debug_msg(2, "EscapeDependencyPath: %s -> %s", path.toLatin1().constData(), ret.toLatin1().constData());
+    }
+    return ret;
+}
+
 QString Win32MakefileGenerator::cQuoted(const QString &str)
 {
     QString ret = str;

qmake/generators/makefile.cpp
@@ -2823,6 +2823,19 @@ MakefileGenerator::escapeFilePaths(const ProStringList &paths) const
     return ret;
 }
 
+QString
+MakefileGenerator::escapeDependencyPath(const QString &path) const
+{
+    QString ret = path;
+    if (!ret.isEmpty()) {
+        // Unix make semantics, to be inherited by unix and mingw generators.
+        static const QRegExp criticalChars(QStringLiteral("([\t :#])"));
+        ret.replace(criticalChars, QStringLiteral("\\\\1"));
+        debug_msg(2, "escapeDependencyPath: %s -> %s", path.toLatin1().constData(), ret.toLatin1().constData());
+    }
+    return ret;
+}
+
 ProString
 MakefileGenerator::escapeDependencyPath(const ProString &path) const
 {

qmake/generators/makefile.h
@@ -130,11 +130,11 @@ protected:
     QMakeProject *project;
 
     //escape
-    virtual QString escapeFilePath(const QString &path) const { return path; }
+    virtual QString escapeFilePath(const QString &path) const = 0;
     ProString escapeFilePath(const ProString &path) const;
     QStringList escapeFilePaths(const QStringList &paths) const;
     ProStringList escapeFilePaths(const ProStringList &paths) const;
-    virtual QString escapeDependencyPath(const QString &path) const { return escapeFilePath(path); }
+    virtual QString escapeDependencyPath(const QString &path) const;
     ProString escapeDependencyPath(const ProString &path) const;
     QStringList escapeDependencyPaths(const QStringList &paths) const;
     ProStringList escapeDependencyPaths(const ProStringList &paths) const;

qmake/generators/projectgenerator.h
@@ -42,6 +42,9 @@ class ProjectGenerator : public MakefileGenerator
 protected:
     virtual void init();
     virtual bool writeMakefile(QTextStream &);
+
+    virtual QString escapeFilePath(const QString &path) const { Q_ASSERT(false); return QString(); }
+
 public:
     ProjectGenerator();
     ~ProjectGenerator();

qmake/generators/win32/mingw_make.h
@@ -39,8 +39,8 @@ public:
     MingwMakefileGenerator();
     ~MingwMakefileGenerator();
 protected:
-    QString escapeDependencyPath(const QString &path) const;
-    ProString escapeDependencyPath(const ProString &path) const { return MakefileGenerator::escapeDependencyPath(path); }
+    using MakefileGenerator::escapeDependencyPath;
+    virtual QString escapeDependencyPath(const QString &path) const;
     virtual ProString fixLibFlag(const ProString &lib);
     virtual QString getManifestFileForRcFile() const;
     bool writeMakefile(QTextStream &);

qmake/generators/win32/winmakefile.h
@@ -47,8 +47,10 @@ protected:
     virtual void writeObjectsPart(QTextStream &t);
     virtual void writeImplicitRulesPart(QTextStream &t);
     virtual void writeBuildRulesPart(QTextStream &);
+    using MakefileGenerator::escapeFilePath;
     virtual QString escapeFilePath(const QString &path) const;
-    ProString escapeFilePath(const ProString &path) const { return MakefileGenerator::escapeFilePath(path); }
+    using MakefileGenerator::escapeDependencyPath;
+    virtual QString escapeDependencyPath(const QString &path) const;
 
     virtual void writeRcFilePart(QTextStream &t);
 

commit 6b4f2f755671843e794b15ef795524816b9ffc4a

qmake: get rid of (some) makefile expansions in dependency paths
the variables are quoted correctly for commands, which is incompatible
with quoting for dependencies under mingw. so insert the paths as
literals, where we can control quoting.

this fixes building in directories with spaces, which i broke in
7c34e0a7b by using different quoting styles for deps and commands in the
first place.

this breaks the hypothetical use case where somebody wants to override
TARGET or DESTDIR (or DESTDIR_TARGET under windows) on the *make*
command line. not sure why anyone would do that - just do it at the
*qmake* level.

we did not get rid of OBJECTS, because that would cause significant
duplication in the makefile (not that it would matter too much, given
the dependency lists ...). this isn't a problem, because these are
short relative paths which are not expected to contain "funny"
characters.

an alternative would have been to change the variables' quoting and
eliminate them from the commands instead, but that would be
backwards-incompatible, because commands are "user-servicable".
for the same reason, we cannot get rid of the variables entirely.

Change-Id: Ic7592c7fc67d8b7d2b64de80808365cd1c3f79d0
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

qmake/generators/unix/unixmake2.cpp
@@ -280,11 +280,11 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
     if (destd.endsWith('\\'))
         destd += '\\';
     t << "DESTDIR       = " << destd << endl;
-    t << "TARGET        = " << fileVar("TARGET") << endl; // ### mixed use!
+    t << "TARGET        = " << fileVar("TARGET") << endl;
     if(project->isActiveConfig("plugin")) {
         t << "TARGETD       = " << fileVar("TARGET") << endl;
     } else if(!project->isActiveConfig("staticlib") && project->values("QMAKE_APP_FLAG").isEmpty()) {
-        t << "TARGETA       = " << fileVar("TARGETA") << endl; // ### mixed use!
+        t << "TARGETA       = " << fileVar("TARGETA") << endl;
         if(!project->isEmpty("QMAKE_BUNDLE")) {
             t << "TARGETD       = " << fileVar("TARGET_x.y") << endl;
             t << "TARGET0       = " << fileVar("TARGET_") << endl;
@@ -502,7 +502,7 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
                 t << "\n\t" << var("QMAKE_POST_LINK");
             t << endl << endl;
         } else {
-            t << "$(TARGET): " << depVar("PRE_TARGETDEPS") << " $(OBJECTS) "
+            t << depVar("TARGET") << ": " << depVar("PRE_TARGETDEPS") << " $(OBJECTS) "
               << target_deps << ' ' << depVar("POST_TARGETDEPS") << "\n\t";
             if (project->first("TEMPLATE") != "aux") {
                 if (!destdir.isEmpty())
@@ -515,7 +515,7 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
             }
             t << endl << endl;
         }
-        allDeps = " $(TARGET)";
+        allDeps = ' ' + depVar("TARGET");
     } else if(!project->isActiveConfig("staticlib")) {
         QString destdir_r = project->first("DESTDIR").toQString(), incr_deps;
         if(!project->isEmpty("QMAKE_BUNDLE")) {
@@ -580,14 +580,14 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
             }
 
             //real target
-            t << destdir_d << "$(TARGET): " << depVar("PRE_TARGETDEPS") << ' '
+            t << destdir_d << depVar("TARGET") << ": " << depVar("PRE_TARGETDEPS") << ' '
               << incr_deps << " $(SUBLIBS) " << target_deps << ' ' << depVar("POST_TARGETDEPS");
         } else {
-            t << destdir_d << "$(TARGET): " << depVar("PRE_TARGETDEPS")
+            t << destdir_d << depVar("TARGET") << ": " << depVar("PRE_TARGETDEPS")
               << " $(OBJECTS) $(SUBLIBS) $(OBJCOMP) " << target_deps
               << ' ' << depVar("POST_TARGETDEPS");
         }
-        allDeps = ' ' + destdir_d + "$(TARGET)";
+        allDeps = ' ' + destdir_d + depVar("TARGET");
         if(!destdir.isEmpty())
             t << "\n\t" << mkdir_p_asstring(destdir, false);
         if(!project->isEmpty("QMAKE_PRE_LINK"))
@@ -604,7 +604,7 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
                 t << "\n\t" << var("QMAKE_POST_LINK");
             t << endl << endl;
         } else if(!project->isEmpty("QMAKE_BUNDLE")) {
-            bundledFiles << destdir_r + "$(TARGET)";
+            bundledFiles << destdir_r + var("TARGET");
             t << "\n\t"
               << "-$(DEL_FILE) $(TARGET) $(TARGET0) $(DESTDIR)$(TARGET0)\n\t"
               << var("QMAKE_LINK_SHLIB_CMD") << "\n\t"
@@ -669,8 +669,8 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
         t << endl << endl;
 
         if (! project->isActiveConfig("plugin")) {
-            t << "staticlib: $(TARGETA)\n\n";
-            t << "$(TARGETA): " << depVar("PRE_TARGETDEPS") << " $(OBJECTS) $(OBJCOMP)";
+            t << "staticlib: " << depVar("TARGETA") << "\n\n";
+            t << depVar("TARGETA") << ": " << depVar("PRE_TARGETDEPS") << " $(OBJECTS) $(OBJCOMP)";
             if(do_incremental)
                 t << " $(INCREMENTAL_OBJECTS)";
             t << ' ' << depVar("POST_TARGETDEPS") << "\n\t";
@@ -690,11 +690,11 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
         QString destdir_r = project->first("DESTDIR").toQString();
         QString destdir_d = escapeDependencyPath(destdir_r);
         QString destdir = escapeFilePath(destdir_r);
-        allDeps = ' ' + destdir_d + "$(TARGET)"
+        allDeps = ' ' + destdir_d + depVar("TARGET")
                   + varGlue("QMAKE_AR_SUBLIBS", ' ' + destdir_d, ' ' + destdir_d, "");
         t << "staticlib: " << destdir_d << "$(TARGET)\n\n";
         if(project->isEmpty("QMAKE_AR_SUBLIBS")) {
-            t << destdir_d << "$(TARGET): " << depVar("PRE_TARGETDEPS")
+            t << destdir_d << depVar("TARGET") << ": " << depVar("PRE_TARGETDEPS")
               << " $(OBJECTS) $(OBJCOMP) " << depVar("POST_TARGETDEPS") << "\n\t";
             if(!destdir.isEmpty())
                 t << mkdir_p_asstring(destdir, false) << "\n\t";
@@ -719,7 +719,7 @@ UnixMakefileGenerator::writeMakeParts(QTextStream &t)
                 QString ar;
                 ProString lib = destdir + escapeFilePath(*libit);
                 if((*libit) == "$(TARGET)") {
-                    t << destdir_d << "$(TARGET): " << depVar("PRE_TARGETDEPS")
+                    t << destdir_d << depVar("TARGET") << ": " << depVar("PRE_TARGETDEPS")
                       << ' ' << depVar("POST_TARGETDEPS") << valList(escapeDependencyPaths(build)) << "\n\t";
                     ar = project->first("QMAKE_AR_CMD").toQString();
                     ar.replace(QLatin1String("$(OBJECTS)"), escapeFilePaths(build).join(' '));

qmake/generators/win32/mingw_make.cpp
@@ -325,8 +325,9 @@ void MingwMakefileGenerator::writeBuildRulesPart(QTextStream &t)
 {
     t << "first: all\n";
     t << "all: " << escapeDependencyPath(fileFixify(Option::output.fileName()))
-      << ' ' << depVar("ALL_DEPS") << " $(DESTDIR_TARGET)\n\n";
-    t << "$(DESTDIR_TARGET): " << depVar("PRE_TARGETDEPS") << " $(OBJECTS) " << depVar("POST_TARGETDEPS");
+      << ' ' << depVar("ALL_DEPS") << ' ' << depVar("DEST_TARGET") << "\n\n";
+    t << depVar("DEST_TARGET") << ": "
+      << depVar("PRE_TARGETDEPS") << " $(OBJECTS) " << depVar("POST_TARGETDEPS");
     if (project->first("TEMPLATE") == "aux") {
         t << "\n\n";
         return;

qmake/generators/win32/msvc_nmake.cpp
@@ -584,8 +584,9 @@ void NmakeMakefileGenerator::writeBuildRulesPart(QTextStream &t)
 
     t << "first: all\n";
     t << "all: " << escapeDependencyPath(fileFixify(Option::output.fileName()))
-      << ' ' << depVar("ALL_DEPS") << " $(DESTDIR_TARGET)\n\n";
-    t << "$(DESTDIR_TARGET): " << depVar("PRE_TARGETDEPS") << " $(OBJECTS) " << depVar("POST_TARGETDEPS");
+      << ' ' << depVar("ALL_DEPS") << ' ' << depVar("DEST_TARGET") << "\n\n";
+    t << depVar("DEST_TARGET") << ": "
+      << depVar("PRE_TARGETDEPS") << " $(OBJECTS) " << depVar("POST_TARGETDEPS");
     if (templateName == "aux") {
         t << "\n\n";
         return;

qmake/generators/win32/winmakefile.cpp
@@ -580,7 +580,7 @@ void Win32MakefileGenerator::writeStandardParts(QTextStream &t)
 
     t << "DIST          = " << fileVarList("DISTFILES") << ' '
       << fileVarList("HEADERS") << ' ' << fileVarList("SOURCES") << endl;
-    t << "QMAKE_TARGET  = " << fileVar("QMAKE_ORIG_TARGET") << endl;
+    t << "QMAKE_TARGET  = " << fileVar("QMAKE_ORIG_TARGET") << endl;  // unused
     // The comment is important to maintain variable compatibility with Unix
     // Makefiles, while not interpreting a trailing-slash as a linebreak
     t << "DESTDIR        = " << escapeFilePath(destDir) << " #avoid trailing-slash linebreak\n";