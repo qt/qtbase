435d1d53bd61491010dbb2130a27e35cd9798965
Jesus Fernandez <jesus.fernandez@qt.io>
2018-07-16 13:34:49 +0200

    Use nullptr instead of 0
    Change-Id: I37e776b84193d705b48d5d75ad620fd41b271d9e
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- src/network/access/qnetworkaccessmanager.cpp
+++ src/network/access/qnetworkaccessmanager.cpp
@@ -548,7 +548,7 @@ void QNetworkAccessManager::setProxy(const QNetworkProxy &proxy)
     Q_D(QNetworkAccessManager);
     delete d->proxyFactory;
     d->proxy = proxy;
-    d->proxyFactory = 0;
+    d->proxyFactory = nullptr;
 }
 
 /*!
@@ -1804,7 +1804,7 @@ void QNetworkAccessManagerPrivate::destroyThread()
             delete thread;
         else
             QObject::connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
-        thread = 0;
+        thread = nullptr;
     }
 }
 
