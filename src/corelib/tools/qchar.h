qt/qtbase

commit 208c71768cca1dda9ea42f3a395889a85810005e

QChar: add (char16_t) and (wchar_t) ctors
... for better std C++ integration.

[ChangeLog][QtCore][QChar] Added constructors from char16_t and, on
Windows, wchar_t.

Change-Id: I2d18ea3a37e869b8ea9f4036d7200d9d13c7d929
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/tools/qchar.h
@@ -86,6 +86,13 @@ public:
     Q_DECL_CONSTEXPR QChar(int rc) Q_DECL_NOTHROW : ucs(ushort(rc & 0xffff)) {}
     Q_DECL_CONSTEXPR QChar(SpecialCharacter s) Q_DECL_NOTHROW : ucs(ushort(s)) {} // implicit
     Q_DECL_CONSTEXPR QChar(QLatin1Char ch) Q_DECL_NOTHROW : ucs(ch.unicode()) {} // implicit
+#if !defined(Q_OS_WIN) || defined(Q_COMPILER_UNICODE_STRINGS)
+    Q_DECL_CONSTEXPR QChar(char16_t ch) Q_DECL_NOTHROW : ucs(ushort(ch)) {} // implicit
+#endif
+#if defined(Q_OS_WIN)
+    Q_STATIC_ASSERT(sizeof(wchar_t) == sizeof(ushort));
+    Q_DECL_CONSTEXPR QChar(wchar_t ch) Q_DECL_NOTHROW : ucs(ushort(ch)) {} // implicit
+#endif
 
 #ifndef QT_NO_CAST_FROM_ASCII
     QT_ASCII_CAST_WARN Q_DECL_CONSTEXPR explicit QChar(char c) Q_DECL_NOTHROW : ucs(uchar(c)) { }

src/corelib/tools/qchar.cpp
@@ -608,6 +608,24 @@ QT_BEGIN_NAMESPACE
 */
 
 /*!
+    \fn QChar::QChar(char16_t ch)
+    \since 5.10
+
+    Constructs a QChar corresponding to the UTF-16 character \a ch.
+
+    \note This constructor is not available on MSVC 2013.
+*/
+
+/*!
+    \fn QChar::QChar(wchar_t ch)
+    \since 5.10
+
+    Constructs a QChar corresponding to the wide character \a ch.
+
+    \note This constructor is only available on Windows.
+*/
+
+/*!
     \fn QChar::QChar(char ch)
 
     Constructs a QChar corresponding to ASCII/Latin-1 character \a ch.

tests/auto/corelib/tools/qchar/tst_qchar.cpp
@@ -36,6 +36,8 @@ class tst_QChar : public QObject
 {
     Q_OBJECT
 private slots:
+    void fromChar16_t();
+    void fromWchar_t();
     void operator_eqeq_int();
     void operators_data();
     void operators();
@@ -74,6 +76,30 @@ private slots:
 
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
+
+void tst_QChar::fromChar16_t()
+{
+#if !defined(Q_OS_WIN) || defined(Q_COMPILER_UNICODE_STRINGS)
+    QChar aUmlaut = u'\u00E4'; // German small letter a-umlaut
+    QCOMPARE(aUmlaut, QChar(0xE4));
+    QChar replacementCharacter = u'\uFFFD';
+    QCOMPARE(replacementCharacter, QChar(QChar::ReplacementCharacter));
+#else
+    QSKIP("This test requires C++11 char16_t support enabled in the compiler.");
+#endif
+}
+
+void tst_QChar::fromWchar_t()
+{
+#if defined(Q_OS_WIN)
+    QChar aUmlaut = L'\u00E4'; // German small letter a-umlaut
+    QCOMPARE(aUmlaut, QChar(0xE4));
+    QChar replacementCharacter = L'\uFFFD';
+    QCOMPARE(replacementCharacter, QChar(QChar::ReplacementCharacter));
+#else
+    QSKIP("This is a Windows-only test.");
+#endif
+}
 
 void tst_QChar::operator_eqeq_int()
 {

commit c575977645ece4b8d1e6aaec21daa043da5affa8

Fix MSVC builds with /Zc:wchar_t-
208c71768 introduced a problem for our users, who build on Windows with
/Zc:wchar_t-, which makes wchar_t a typedef for the type 'unsigned short',
preventing them from switching to more recent versions of Qt. While MSDN
recommends against this option, we can add more #if-ery to avoid compiler's
bailing out on a constructor's redefinition.

Task-number: QTBUG-65101
Change-Id: I62a1d9b2572f3d4b1f70bcbc3e52e795b1944558
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

src/corelib/tools/qchar.h
@@ -93,7 +93,9 @@ public:
     Q_STATIC_ASSERT(sizeof(wchar_t) == sizeof(ushort));
 #endif
 #if defined(Q_OS_WIN) || defined(Q_CLANG_QDOC)
+#   if !defined(_WCHAR_T_DEFINED) || defined(_NATIVE_WCHAR_T_DEFINED)
     Q_DECL_CONSTEXPR QChar(wchar_t ch) Q_DECL_NOTHROW : ucs(ushort(ch)) {} // implicit
+#   endif
 #endif
 
 #ifndef QT_NO_CAST_FROM_ASCII