b7c5c2e65bd00d2b6729a54ae7ba5ddd4e891a03
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-09-06 11:20:36 +0200

    Add NEON optimized ARGB32 unpremultiply routines
    Mirroring similar routines recently added for SSE4.1
    
    Change-Id: Ibb9d10cc34655ce1dc0e97fdff4e4f6a81d47d05
    Reviewed-by: Erik Verbruggen <erik.verbruggen@qt.io>
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>

--- src/gui/painting/qdrawhelper.cpp
+++ src/gui/painting/qdrawhelper.cpp
@@ -6505,10 +6505,19 @@ static void qInitDrawhelperFunctions()
                                                               const QVector<QRgb> *, QDitherInfo *);
     extern const uint *QT_FASTCALL fetchRGBA8888ToARGB32PM_neon(uint *buffer, const uchar *src, int index, int count,
                                                                 const QVector<QRgb> *, QDitherInfo *);
+    extern void QT_FASTCALL storeARGB32FromARGB32PM_neon(uchar *dest, const uint *src, int index, int count,
+                                                         const QVector<QRgb> *, QDitherInfo *);
+    extern void QT_FASTCALL storeRGBA8888FromARGB32PM_neon(uchar *dest, const uint *src, int index, int count,
+                                                           const QVector<QRgb> *, QDitherInfo *);
+    extern void QT_FASTCALL storeRGBXFromARGB32PM_neon(uchar *dest, const uint *src, int index, int count,
+                                                       const QVector<QRgb> *, QDitherInfo *);
     qPixelLayouts[QImage::Format_ARGB32].fetchToARGB32PM = fetchARGB32ToARGB32PM_neon;
     qPixelLayouts[QImage::Format_ARGB32].convertToARGB32PM = convertARGB32ToARGB32PM_neon;
+    qPixelLayouts[QImage::Format_ARGB32].storeFromARGB32PM = storeARGB32FromARGB32PM_neon;
     qPixelLayouts[QImage::Format_RGBA8888].fetchToARGB32PM = fetchRGBA8888ToARGB32PM_neon;
     qPixelLayouts[QImage::Format_RGBA8888].convertToARGB32PM = convertRGBA8888ToARGB32PM_neon;
+    qPixelLayouts[QImage::Format_RGBA8888].storeFromARGB32PM = storeRGBA8888FromARGB32PM_neon;
+    qPixelLayouts[QImage::Format_RGBX8888].storeFromARGB32PM = storeRGBXFromARGB32PM_neon;
 #endif
 
 #if defined(ENABLE_PIXMAN_DRAWHELPERS)
