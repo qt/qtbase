qt/qtbase

commit ff2a71e310b18a43a41daf2d197f5715f7c26d29

Doc: Move the literal code blocks to a separate file
It's easier to override the code blocks this way.

Change-Id: I98d40626a94fdb70a95c50332c2da141e9277070
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

src/corelib/doc/snippets/code/src_corelib_tools_qeasingcurve.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2018 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the documentation of the Qt Toolkit.
@@ -49,6 +49,17 @@
 ****************************************************************************/
 
 //! [0]
-qreal myEasingFunction(qreal progress);
+    QEasingCurve easing(QEasingCurve::InOutQuad);
+
+    for (qreal t = 0.0; t < 1.0; t+=0.1)
+        qWarning() << "Effective progress" << t << " is
+                   << easing.valueForProgress(t);
 //! [0]
 
+//! [1]
+    QPropertyAnimation animation;
+    animation.setStartValue(0);
+    animation.setEndValue(1000);
+    animation.setDuration(1000);
+    animation.setEasingCurve(QEasingCurve::InOutQuad);
+//! [1]

src/corelib/tools/qeasingcurve.cpp
@@ -72,24 +72,15 @@
     curve is a linear curve. This is the default behaviour.
 
     For example,
-    \code
-    QEasingCurve easing(QEasingCurve::InOutQuad);
 
-    for(qreal t = 0.0; t < 1.0; t+=0.1)
-        qWarning() << "Effective progress" << t << " is
-                   << easing.valueForProgress(t);
-    \endcode
+    \snippet code/src_corelib_tools_qeasingcurve.cpp 0
+
     will print the effective progress of the interpolation between 0 and 1.
 
     When using a QPropertyAnimation, the associated easing curve will be used to control the
     progress of the interpolation between startValue and endValue:
-    \code
-    QPropertyAnimation animation;
-    animation.setStartValue(0);
-    animation.setEndValue(1000);
-    animation.setDuration(1000);
-    animation.setEasingCurve(QEasingCurve::InOutQuad);
-    \endcode
+
+    \snippet code/src_corelib_tools_qeasingcurve.cpp 1
 
     The ability to set an amplitude, overshoot, or period depends on
     the QEasingCurve type. Amplitude access is available to curves

commit 1a9b8715ced2c72d5f6b474fee5878272874def9

Doc: Fix QEasingCurve code snippets
Commit ff2a71e3 accidentally removed a code snippet used in documenting
EasingCurve::EasingFunction. This commit restores it and fixes other
minor issues in QEasingCurve code snippets.

Change-Id: Ib19f602a4abbca3511d3d26c5f6da4910f7104a3
Reviewed-by: Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>

src/corelib/doc/snippets/code/src_corelib_tools_qeasingcurve.cpp
@@ -48,11 +48,15 @@
 **
 ****************************************************************************/
 
+//! [typedef]
+qreal myEasingFunction(qreal progress);
+//! [typedef]
+
 //! [0]
     QEasingCurve easing(QEasingCurve::InOutQuad);
 
-    for (qreal t = 0.0; t < 1.0; t+=0.1)
-        qWarning() << "Effective progress" << t << " is
+    for (qreal t = 0.0; t < 1.0; t += 0.1)
+        qWarning() << "Effective progress" << t << "is"
                    << easing.valueForProgress(t);
 //! [0]
 

src/corelib/tools/qeasingcurve.cpp
@@ -292,7 +292,7 @@
     This is a typedef for a pointer to a function with the following
     signature:
 
-    \snippet code/src_corelib_tools_qeasingcurve.cpp 0
+    \snippet code/src_corelib_tools_qeasingcurve.cpp typedef
 */
 
 #include "qeasingcurve.h"