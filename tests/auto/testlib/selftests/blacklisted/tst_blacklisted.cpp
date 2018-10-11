33d7f76f0e847d7e0fb00dd6056e7bba45b8b1e7
Edward Welbourne <edward.welbourne@qt.io>
2018-10-11 15:11:38 +0200

    Tweak a selftest blacklist to exercise more of the blacklisting code
    In the process, corrected an inaccurate XFAIL message (an XPASS is
    normally an error, unless blacklisting ignores it so turns it into a
    BPASS). Added the missing .tap file to its expected output.
    Documented the similarity to the silent/ selftest.
    
    Task-number: QTPM-1385
    Change-Id: Id74a1353d54af2f3bfe2c764e33c1f051958ab21
    Reviewed-by: Jędrzej Nowacki <jedrzej.nowacki@qt.io>

--- tests/auto/testlib/selftests/blacklisted/tst_blacklisted.cpp
+++ tests/auto/testlib/selftests/blacklisted/tst_blacklisted.cpp
@@ -45,7 +45,8 @@ private slots:
     void messages();
 };
 
-// All the tests below have been blacklisted in blacklisted/BLACKLIST
+// All the tests below except pass() have been blacklisted in blacklisted/BLACKLIST
+// Contrast with ../silent/, for the same tests without blacklisting but with -silent
 
 void tst_Blacklisted::pass()
 {
@@ -64,7 +65,7 @@ void tst_Blacklisted::fail()
 
 void tst_Blacklisted::xfail()
 {
-    QEXPECT_FAIL("", "This test should XFAIL then BFAIL", Abort);
+    QEXPECT_FAIL("", "This test should XFAIL then BPASS", Abort);
     QVERIFY(false);
 }
 
