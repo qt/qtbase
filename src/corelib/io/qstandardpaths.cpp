qt/qtbase

commit 1afe110b8fe6da51ec23736fa3a105013255f904

Doc: corrected link/autolink issues in corelib
Task-number: QTBUG-43115
Change-Id: I31da92e3a7c4dd7b75ee283dbfecd77e284978ca
Reviewed-by: Martin Smith <martin.smith@digia.com>

src/corelib/io/qstandardpaths.cpp
@@ -77,15 +77,10 @@ QT_BEGIN_NAMESPACE
     this user, but should still be assumed to be unreachable by applications by
     other users.
 
-    The only exception is QStandardPaths::TempLocation (which is the same as
-    QDir::tempPath()): the path returned may be application-specific, but files
-    stored there may be accessed by other applications run by the same user.
-
     Data interchange with other users is out of the scope of QStandardPaths.
 
     \value DesktopLocation Returns the user's desktop directory. This is a generic value.
-           On systems with no concept of a desktop, this is the same as
-           QStandardPaths::HomeLocation.
+           On systems with no concept of a desktop.
     \value DocumentsLocation Returns the directory containing user document files.
            This is a generic value. The returned path is never empty.
     \value FontsLocation Returns the directory containing user's fonts. This is a generic value.
@@ -118,9 +113,6 @@ QT_BEGIN_NAMESPACE
     \value GenericCacheLocation Returns a directory location where user-specific non-essential
            (cached) data, shared across applications, should be written. This is a generic value.
            Note that the returned path may be empty if the system has no concept of shared cache.
-    \value GenericDataLocation Returns a directory location where persistent
-           data shared across applications can be stored. This is a generic value. The returned
-           path is never empty.
     \value RuntimeLocation Returns a directory location where runtime communication
            files should be written, like Unix local sockets. This is a generic value.
            The returned path may be empty on some systems.

src/corelib/animation/qparallelanimationgroup.cpp
@@ -44,7 +44,7 @@
     animations in parallel. The animation group finishes when the
     longest lasting animation has finished.
 
-    You can treat QParallelAnimation as any other QAbstractAnimation,
+    You can treat QParallelAnimationGroup as any other QAbstractAnimation,
     e.g., pause, resume, or add it to other animation groups.
 
     \code

src/corelib/doc/qtcore.qdocconf
@@ -25,7 +25,7 @@ qhp.QtCore.subprojects.classes.sortPages = true
 
 tagfile                 = ../../../doc/qtcore/qtcore.tags
 
-depends                 += activeqt qtdbus qtgui qtwidgets qtnetwork qtdoc qtmacextras qtquick qtlinguist qtdesigner qtconcurrent qtxml qmake
+depends                 += activeqt qtdbus qtgui qtwidgets qtnetwork qtdoc qtmacextras qtquick qtlinguist qtdesigner qtconcurrent qtxml qmake qtwinextras
 # depends               += qtqml # Qt namespace collides with QtQml::Qt, see QTBUG-38630
 
 headerdirs  += ..

src/corelib/global/qnamespace.qdoc
@@ -498,7 +498,7 @@
     \value TextExpandTabs Makes the U+0009 (ASCII tab) character move to
            the next tab stop.
     \value TextShowMnemonic Displays the string "\&P" as \underline{P}
-           (see QButton for an example). For an ampersand, use "\&\&".
+           For an ampersand, use "\&\&".
     \value TextWordWrap Breaks lines at appropriate points, e.g. at word
            boundaries.
     \value TextWrapAnywhere Breaks lines anywhere, even within words.
@@ -902,8 +902,7 @@
     on QWidget::contentsRect(). This is set by the widget's author.
 
     \value WA_LayoutUsesWidgetRect Ignore the layout item rect from the style
-    when laying out this widget with QLayout. This makes a difference in
-    QMacStyle and QPlastiqueStyle for some widgets.
+    when laying out this widget with QLayout.
 
     \value WA_MacNoClickThrough When a widget that has this attribute set
     is clicked, and its window is inactive, the click will make the window
@@ -1162,7 +1161,7 @@
     _NET_WM_WINDOW_TYPE X11 window property. See
     http://standards.freedesktop.org/wm-spec/ for more details. This
     attribute has no effect on non-X11 platforms. \b Note: Qt
-    automatically sets this attribute for QMenus added to a QMenuBar.
+    automatically sets this attribute for QMenu objects added to a QMenuBar.
 
     \value WA_X11NetWmWindowTypePopupMenu Adds _NET_WM_WINDOW_TYPE_POPUP_MENU
     to the window's _NET_WM_WINDOW_TYPE X11 window property. See

src/corelib/io/qfileselector.cpp
@@ -229,7 +229,7 @@ static QString qrcScheme()
 }
 
 /*!
-   This is a convenience version of select operating on QUrls. If the scheme is not file or qrc,
+   This is a convenience version of select operating on QUrl objects. If the scheme is not file or qrc,
    \a filePath is returned immediately. Otherwise selection is applied to the path of \a filePath
    and a QUrl is returned with the selected path and other QUrl parts the same as \a filePath.
 

src/corelib/io/qiodevice.cpp
@@ -708,7 +708,7 @@ bool QIODevice::reset()
     number of bytes to allocate in a buffer before reading.
 
     Subclasses that reimplement this function must call the base
-    implementation in order to include the size of QIODevices' buffer. Example:
+    implementation in order to include the size of the buffer of QIODevice. Example:
 
     \snippet code/src_corelib_io_qiodevice.cpp 1
 

src/corelib/io/qprocess.cpp
@@ -1446,7 +1446,7 @@ void QProcess::setStandardErrorFile(const QString &fileName, OpenMode mode)
     The following shell command:
     \snippet code/src_corelib_io_qprocess.cpp 2
 
-    Can be accomplished with QProcesses with the following code:
+    Can be accomplished with QProcess with the following code:
     \snippet code/src_corelib_io_qprocess.cpp 3
 */
 void QProcess::setStandardOutputProcess(QProcess *destination)

src/corelib/io/qresource.cpp
@@ -197,7 +197,7 @@ Q_GLOBAL_STATIC(QStringList, resourceSearchPaths)
     path requested in setFileName().
 
     The unregisterResource() function removes a reference to a particular
-    file. If there are QResources that currently reference resources related
+    file. If there are QResource objects that currently reference resources related
     to the unregistered file, they will continue to be valid but the resource
     file itself will be removed from the resource roots, and thus no further
     QResource can be created pointing into this resource data. The resource

commit 0479379d2463ff44c54e774d41746fb1500cf481

Fix docs for QStandardPaths::DesktopLocation
Was broken by 1afe110b8fe6da51ec23736fa3a105013255f904

Change-Id: I63e000c663d227f8527ad5162c7662c5c011cf72
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/io/qstandardpaths.cpp
@@ -94,7 +94,8 @@ QT_BEGIN_NAMESPACE
     Data interchange with other users is out of the scope of QStandardPaths.
 
     \value DesktopLocation Returns the user's desktop directory. This is a generic value.
-           On systems with no concept of a desktop.
+           On systems with no concept of a desktop, this is the same as
+           QStandardPaths::HomeLocation.
     \value DocumentsLocation Returns the directory containing user document files.
            This is a generic value. The returned path is never empty.
     \value FontsLocation Returns the directory containing user's fonts. This is a generic value.