c7275f6723089b0ba5f305fe8f3b51cb6f1eb845
Eirik Aavitsland <eirik.aavitsland@qt.io>
2018-10-25 12:21:35 +0200

    Doc: fix formatting error in QLineF
    This is a whitespace-only change; removing a couple of newlines that
    broke qdoc formatting of the enum values documentation.
    
    Change-Id: Id371a4519922c71d79a11f3cda131e6683812696
    Reviewed-by: Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>

--- src/corelib/tools/qline.cpp
+++ src/corelib/tools/qline.cpp
@@ -387,10 +387,9 @@ QDataStream &operator>>(QDataStream &stream, QLine &line)
 
     \value UnboundedIntersection The two lines intersect, but not
     within the range defined by their lengths. This will be the case
-    if the lines are not parallel.
-
-    intersect() will also return this value if the intersect point is
-    within the start and end point of only one of the lines.
+    if the lines are not parallel. intersect() will also return this
+    value if the intersect point is within the start and end point of
+    only one of the lines.
 
     \value BoundedIntersection The two lines intersect with each other
     within the start and end points of each line.
