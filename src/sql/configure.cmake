806595c5b6c9fc3bfc3bc843ddd1a4b126d6a426
Tobias Hunger <tobias.hunger@gmail.com>
2018-11-02 14:27:57 +0100

    Add Sql build and tests
    Change-Id: I9bf7d61a65950eafcfe6b3ea9c437e353ff7b2ed
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@qt.io>
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

---
+++ src/sql/configure.cmake
@@ -0,0 +1,21 @@
+
+
+#### Inputs
+
+
+
+#### Libraries
+
+
+
+#### Tests
+
+
+
+#### Features
+
+qt_feature("sqlmodel" PUBLIC
+    LABEL "SQL item models"
+    PURPOSE "Provides item model classes backed by SQL databases."
+    CONDITION QT_FEATURE_itemmodel
+)
