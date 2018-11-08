f1812aad895ed2b8337a6d98d05623714680aba9
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2018-11-08 14:11:11 +0100

    QFocusFrame: protect it from being moved around by accident
    If a focus frame is set around a widget that exist inside a
    QAbstractItemView, both the focus frame and the widget will
    be scrolled when the table is scrolled (since the focus frame
    is a child of the view). The result is that after the widget
    has been scrolled (which will move the focus frame to the
    correct position as well), the focus frame will be scrolled
    next, and therefore away from the widget.
    
    This patch will catch this case by always adjusting the
    focus frame position when someone tries to move it. Trying
    to move the focus frame away from the widget it tracks
    will anyway be flaky.
    
    Fixes: QTBUG-63877
    Change-Id: Ic2aacc4fafc219280e32092c258a7539d0db9cd0
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- tests/auto/widgets/widgets/qfocusframe/tst_qfocusframe.cpp
+++ tests/auto/widgets/widgets/qfocusframe/tst_qfocusframe.cpp
@@ -32,6 +32,8 @@
 #include <qcoreapplication.h>
 #include <qdebug.h>
 #include <qfocusframe.h>
+#include <qtableview.h>
+#include <qstandarditemmodel.h>
 
 class tst_QFocusFrame : public QObject
 {
@@ -43,6 +45,7 @@ public:
 
 private slots:
     void getSetCheck();
+    void focusFrameInsideScrollview();
 };
 
 tst_QFocusFrame::tst_QFocusFrame()
@@ -66,6 +69,42 @@ void tst_QFocusFrame::getSetCheck()
     obj1->setWidget((QWidget *)0);
     QCOMPARE((QWidget *)0, obj1->widget());
     delete obj1;
+}
+
+void tst_QFocusFrame::focusFrameInsideScrollview()
+{
+    // Make sure that the focus frame follows the widget, even
+    // if the widget is inside a QAbstractItemView. A QAbstractItemView will scroll
+    // all the children, including the focus frame, when it scrolls, which
+    // is why special considerations are taken inside the focus frame to
+    // prevent the frame to scroll away from the widget it tracks.
+
+    if (qApp->style()->objectName() != QLatin1String("macintosh"))
+        QSKIP("This test is only valid when using a style that has a focus frame");
+
+    QWidget window;
+    window.setGeometry(100, 100, 500, 500);
+
+    QTableView tableView(&window);
+    tableView.resize(window.size());
+    QStandardItemModel *itemModel = new QStandardItemModel();
+    for (int i = 0; i < 50; ++i)
+        itemModel->appendRow(new QStandardItem("Value"));
+    tableView.setModel(itemModel);
+    tableView.edit(itemModel->index(8, 0));
+
+    window.show();
+    QFocusFrame *focusFrame = nullptr;
+    QTRY_VERIFY(focusFrame = window.findChild<QFocusFrame *>());
+    const QPoint initialOffset = focusFrame->widget()->mapToGlobal(QPoint()) - focusFrame->mapToGlobal(QPoint());
+
+    tableView.scrollTo(itemModel->index(40, 0));
+    QPoint offsetAfterScroll = focusFrame->widget()->mapToGlobal(QPoint()) - focusFrame->mapToGlobal(QPoint());
+    QCOMPARE(offsetAfterScroll, initialOffset);
+
+    tableView.scrollTo(itemModel->index(0, 0));
+    offsetAfterScroll = focusFrame->widget()->mapToGlobal(QPoint()) - focusFrame->mapToGlobal(QPoint());
+    QCOMPARE(offsetAfterScroll, initialOffset);
 }
 
 QTEST_MAIN(tst_QFocusFrame)
