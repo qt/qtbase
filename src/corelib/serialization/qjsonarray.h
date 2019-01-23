c2b4e7a2153842fa8eb47ced96018e2477f4347f
Lars Knoll <lars.knoll@qt.io>
2019-01-23 11:36:25 +0100

    Add QJsonArray::cbegin()/cend() methods
    Add those to be consistent with the rest of Qt and the STL.
    
    Fixes: QTBUG-32793
    Change-Id: Ib712b7b16b8be6627aeac79b90c6e9cdf92b60e0
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/serialization/qjsonarray.h
+++ src/corelib/serialization/qjsonarray.h
@@ -214,9 +214,11 @@ public:
     inline iterator begin() { detach2(); return iterator(this, 0); }
     inline const_iterator begin() const { return const_iterator(this, 0); }
     inline const_iterator constBegin() const { return const_iterator(this, 0); }
+    inline const_iterator cbegin() const { return const_iterator(this, 0); }
     inline iterator end() { detach2(); return iterator(this, size()); }
     inline const_iterator end() const { return const_iterator(this, size()); }
     inline const_iterator constEnd() const { return const_iterator(this, size()); }
+    inline const_iterator cend() const { return const_iterator(this, size()); }
     iterator insert(iterator before, const QJsonValue &value) { insert(before.i, value); return before; }
     iterator erase(iterator it) { removeAt(it.i); return it; }
 
