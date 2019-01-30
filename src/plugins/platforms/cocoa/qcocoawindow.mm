9f22ac0aa0254f20f9b26aec7b124d74141fdfcd
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2019-01-30 16:29:59 +0100

    macOS: Don't send redundant geometry change events for top level windows
    Top level windows already get their geometry changes via windowDidMove
    and windowDidResize.
    
    Change-Id: Ie6370aa290ef48c8b3ac770e77adb57ce43cbb47
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- src/plugins/platforms/cocoa/qcocoawindow.mm
+++ src/plugins/platforms/cocoa/qcocoawindow.mm
@@ -1101,6 +1101,9 @@ void QCocoaWindow::setEmbeddedInForeignView()
 
 void QCocoaWindow::viewDidChangeFrame()
 {
+    if (isContentView())
+        return; // Handled below
+
     handleGeometryChange();
 }
 
