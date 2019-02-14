441520141ead6dec74f3bdbb4c1d1e48d3356435
Dmitry Sokolov <sokolov_d_s@mail.ru>
2019-02-14 10:49:52 +0100

    Inline expression to bypass compiler bug
    MSVC managed to trigger the this != &other assertion in
    QString(const QString &other); so just skip creation of the
    intermediate string in the function whose body tripped over this.
    
    Change-Id: I687003cfc588531018c6069863ce2a76078c8e3f
    Fixes: QTBUG-73802
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- qmake/generators/makefile.cpp
+++ qmake/generators/makefile.cpp
@@ -81,8 +81,8 @@ bool MakefileGenerator::canExecute(const QStringList &cmdline, int *a) const
 
 QString MakefileGenerator::mkdir_p_asstring(const QString &dir, bool escape) const
 {
-    QString edir = escape ? escapeFilePath(Option::fixPathToTargetOS(dir, false, false)) : dir;
-    return "@" + makedir.arg(edir);
+    return "@" + makedir.arg(
+        escape ? escapeFilePath(Option::fixPathToTargetOS(dir, false, false)) : dir);
 }
 
 bool MakefileGenerator::mkdir(const QString &in_path) const
