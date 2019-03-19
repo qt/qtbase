ad1d6160332842bf07879cf76197f4cf8274854e
Dongmei Wang <dongmei.wang@qt.io>
2019-03-18 20:09:18 -0700

    wasm: add Qt Virtual Keyboard support
    Change-Id: I454ab677cad25b50ba7b03ef2c4b6046e1227ce8
    Reviewed-by: Lorn Potter <lorn.potter@gmail.com>

--- src/plugins/platforms/wasm/qwasmintegration.h
+++ src/plugins/platforms/wasm/qwasmintegration.h
@@ -34,6 +34,7 @@
 
 #include <qpa/qplatformintegration.h>
 #include <qpa/qplatformscreen.h>
+#include <qpa/qplatforminputcontext.h>
 
 #include <QtCore/qhash.h>
 
@@ -73,6 +74,9 @@ public:
     QPlatformTheme *createPlatformTheme(const QString &name) const override;
     QPlatformServices *services() const override;
     QPlatformClipboard *clipboard() const override;
+    void initialize() override;
+    QPlatformInputContext *inputContext() const override;
+
     QWasmClipboard *getWasmClipboard() { return m_clipboard; }
 
     static QWasmIntegration *get() { return s_instance; }
@@ -91,6 +95,7 @@ private:
     QHash<QString, QWasmScreen *> m_screens;
     mutable QWasmClipboard *m_clipboard;
     qreal m_fontDpi = -1;
+    mutable QScopedPointer<QPlatformInputContext> m_inputContext;
     static QWasmIntegration *s_instance;
 };
 
