86fc0b0e88c3a798be292cf3979f0e05b105feeb
Friedemann Kleint <Friedemann.Kleint@qt.io>
2019-03-21 09:48:03 +0100

    QDockWidgetPrivate: Use member initialization
    Task-number: QTBUG-74242
    Change-Id: I9f243bf37c7685e38a4a961c1173d45925b1b0e3
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

--- src/widgets/widgets/qdockwidget_p.h
+++ src/widgets/widgets/qdockwidget_p.h
@@ -81,28 +81,21 @@ class QDockWidgetPrivate : public QWidgetPrivate
     };
 
 public:
-    inline QDockWidgetPrivate()
-        : QWidgetPrivate(), state(0),
-          features(QDockWidget::DockWidgetClosable
-                   | QDockWidget::DockWidgetMovable
-                   | QDockWidget::DockWidgetFloatable),
-          allowedAreas(Qt::AllDockWidgetAreas), resizer(0)
-    { }
-
     void init();
     void _q_toggleView(bool); // private slot
     void _q_toggleTopLevel(); // private slot
 
     void updateButtons();
-    DragState *state;
+    DragState *state = nullptr;
 
-    QDockWidget::DockWidgetFeatures features;
-    Qt::DockWidgetAreas allowedAreas;
+    QDockWidget::DockWidgetFeatures features = QDockWidget::DockWidgetClosable
+        | QDockWidget::DockWidgetMovable | QDockWidget::DockWidgetFloatable;
+    Qt::DockWidgetAreas allowedAreas = Qt::AllDockWidgetAreas;
 
     QFont font;
 
 #ifndef QT_NO_ACTION
-    QAction *toggleViewAction;
+    QAction *toggleViewAction = nullptr;
 #endif
 
 //    QMainWindow *findMainWindow(QWidget *widget) const;
@@ -129,7 +122,7 @@ public:
     bool isAnimating() const;
 
 private:
-    QWidgetResizeHandler *resizer;
+    QWidgetResizeHandler *resizer = nullptr;
 };
 
 class Q_WIDGETS_EXPORT QDockWidgetLayout : public QLayout
