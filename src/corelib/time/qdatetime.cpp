192d5ad8c2104d90fa8dbce9c449002c298740ed
Edward Welbourne <edward.welbourne@qt.io>
2019-03-21 15:06:18 +0100

    Save DST status when computed for a mutable QDateTimeData
    Most calls to localMSecsToEpochMSecs() happen from functions that
    can't save the DST status it repots (due to the data being const); but
    refreshDateTime() can and (given its name) clearly should.
    
    Change-Id: Ib53c88d2233925da275f0ac52f768cada92c5d2d
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit a04411119ead3d4473e4f0ac4bceedc585977b2f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/time/qdatetime.cpp
+++ src/corelib/time/qdatetime.cpp
@@ -3184,6 +3184,7 @@ static void refreshDateTime(QDateTimeData &d)
     if (spec == Qt::LocalTime) {
         auto dstStatus = extractDaylightStatus(status);
         epochMSecs = localMSecsToEpochMSecs(msecs, &dstStatus, &testDate, &testTime);
+        status = mergeDaylightStatus(status, dstStatus);
     }
     if (timeToMSecs(testDate, testTime) == msecs) {
         status |= QDateTimePrivate::ValidDateTime;
