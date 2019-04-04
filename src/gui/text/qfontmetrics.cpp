d603ee689f0e3fdcfa3230b3d75cdce6c5af05c1
Friedemann Kleint <Friedemann.Kleint@qt.io>
2019-04-04 13:53:19 +0200

    Widget style: Use per-screen DPI in QStyleHelper::dpiScaled()
    Pass the style option to dpiScaled() in order to get
    the correct screen DPI. The style option contains the
    font, which again contains the current DPI value.
    
    Add QFontMetrics::fontDpi() accessors to get the DPI
    from the QFont. This DPI will/should be updated on screen
    change.
    
    Replace hardcoded Q_OS_MAC DPI with hardcoded base
    DPI. This makes per-screen DPI testable on macOS, too.
    
    Task-number: QTBUG-45055
    Change-Id: I75f8b37d45eb50c3334b46b8469a546d29712f1b
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

--- src/gui/text/qfontmetrics.cpp
+++ src/gui/text/qfontmetrics.cpp
@@ -1036,8 +1036,15 @@ int QFontMetrics::lineWidth() const
     return qRound(engine->lineThickness());
 }
 
+/*!
+    \since 5.14
 
-
+    Returns the font DPI.
+*/
+qreal QFontMetrics::fontDpi() const
+{
+    return d->dpi;
+}
 
 /*****************************************************************************
   QFontMetricsF member functions
@@ -1911,6 +1918,16 @@ qreal QFontMetricsF::lineWidth() const
     QFontEngine *engine = d->engineForScript(QChar::Script_Common);
     Q_ASSERT(engine != 0);
     return engine->lineThickness().toReal();
+}
+
+/*!
+    \since 5.14
+
+    Returns the font DPI.
+*/
+qreal QFontMetricsF::fontDpi() const
+{
+    return d->dpi;
 }
 
 QT_END_NAMESPACE
