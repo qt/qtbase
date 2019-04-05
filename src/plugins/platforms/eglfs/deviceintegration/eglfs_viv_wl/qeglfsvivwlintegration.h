0bdded64accc3f654b2d12d9bfaf64f842cedf26
Johan Klokkhammer Helsing <johan.helsing@qt.io>
2019-04-05 09:50:16 +0200

    eglfs_viv_wl: Clean up the wl_display
    Task-number: QTBUG-74879
    Change-Id: Idb39a39a10bccb1822bdb80343fbe1b5c92560e9
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- src/plugins/platforms/eglfs/deviceintegration/eglfs_viv_wl/qeglfsvivwlintegration.h
+++ src/plugins/platforms/eglfs/deviceintegration/eglfs_viv_wl/qeglfsvivwlintegration.h
@@ -49,6 +49,7 @@ class QEglFSVivWaylandIntegration : public QEglFSDeviceIntegration
 {
 public:
     void platformInit() override;
+    void platformDestroy() override;
     QSize screenSize() const override;
     EGLNativeWindowType createNativeWindow(QPlatformWindow *window, const QSize &size, const QSurfaceFormat &format) override;
     void destroyNativeWindow(EGLNativeWindowType window) override;
