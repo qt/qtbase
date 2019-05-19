1065777a2ac1e6a9a0c4fc64d6ee0de02124c682
Marc Mutz <marc.mutz@kdab.com>
2019-05-19 19:37:00 +0200

    QtGui: get rid of the last Java-style iterator
    Trivial.
    
    Java-style iterators are going to be deprecated.
    
    Change-Id: Ie94658be988cc095fb3b05d0d4ef6e7e3bf9a2af
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>

--- src/gui/text/qtextodfwriter.cpp
+++ src/gui/text/qtextodfwriter.cpp
@@ -473,9 +473,7 @@ void QTextOdfWriter::writeFormats(QXmlStreamWriter &writer, const QSet<int> &for
 {
     writer.writeStartElement(officeNS, QString::fromLatin1("automatic-styles"));
     QVector<QTextFormat> allStyles = m_document->allFormats();
-    QSetIterator<int> formatId(formats);
-    while(formatId.hasNext()) {
-        int formatIndex = formatId.next();
+    for (int formatIndex : formats) {
         QTextFormat textFormat = allStyles.at(formatIndex);
         switch (textFormat.type()) {
         case QTextFormat::CharFormat:
