qt/qtbase

commit b91e6f6f40864d54903d707d7f19a9732188b670

Issue a warning about unused result of qScopeGuard and QScopeGuard
If the result is unassigned then resulting QScopeGuard is destroyed
immediately, we can warn about it, as it is definitely a bug.

Change-Id: I627b05cecb3d0e62dbc24373e621f2be36d9b324
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

src/corelib/tools/qscopeguard.h
@@ -50,7 +50,12 @@ template <typename F> class QScopeGuard;
 template <typename F> QScopeGuard<F> qScopeGuard(F f);
 
 template <typename F>
-class QScopeGuard
+class
+#ifndef __INTEL_COMPILER
+// error #2621: attribute "__warn_unused_result__" does not apply here
+Q_REQUIRED_RESULT
+#endif
+QScopeGuard
 {
 public:
     QScopeGuard(QScopeGuard &&other) Q_DECL_NOEXCEPT
@@ -86,6 +91,10 @@ private:
 
 
 template <typename F>
+#ifndef __INTEL_COMPILER
+// Causes "error #3058: GNU attributes on a template redeclaration have no effect"
+Q_REQUIRED_RESULT
+#endif
 QScopeGuard<F> qScopeGuard(F f)
 {
     return QScopeGuard<F>(std::move(f));

commit f946c9fb78e9a267647e9fd1b397d9b7ca2b6664

Fix compilation error on compilers not supporting [[nodiscard]]
__warn_unused_result__ and [[nodiscard]] both are masked by
Q_REQUIRED_RESULT but there are some minor differences between them.
In general [[nodiscard]] is more flexible while
__warn_unused_result__ can cause warnings in some contexts, for
example:

  error #2621: attribute "__warn_unused_result__" does not apply here
  error #3058: GNU attributes on a template redeclaration have no effect

That is a fix for regression caused by
b91e6f6f40864d54903d707d7f19a9732188b670.

Change-Id: Icf11b832f31e714a88536828051f4b7f348cdb36
Reviewed-by: Marc Mutz <marc.mutz@kdab.com>

src/corelib/tools/qscopeguard.h
@@ -51,8 +51,9 @@ template <typename F> QScopeGuard<F> qScopeGuard(F f);
 
 template <typename F>
 class
-#ifndef __INTEL_COMPILER
-// error #2621: attribute "__warn_unused_result__" does not apply here
+#if QT_HAS_CPP_ATTRIBUTE(nodiscard)
+// Q_REQUIRED_RESULT can be defined as __warn_unused_result__ or as [[nodiscard]]
+// but the 1st one has some limitations for example can be placed only on functions.
 Q_REQUIRED_RESULT
 #endif
 QScopeGuard
@@ -90,8 +91,7 @@ private:
 
 
 template <typename F>
-#ifndef __INTEL_COMPILER
-// Causes "error #3058: GNU attributes on a template redeclaration have no effect"
+#if QT_HAS_CPP_ATTRIBUTE(nodiscard)
 Q_REQUIRED_RESULT
 #endif
 QScopeGuard<F> qScopeGuard(F f)