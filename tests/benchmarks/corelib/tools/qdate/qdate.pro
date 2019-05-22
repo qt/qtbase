7efce5d5100dc0fddba36fba2ecb5a6c5e9cae37
Edward Welbourne <edward.welbourne@qt.io>
2019-05-22 15:00:58 +0200

    Add a bench-mark for QDate's isValid() and daysInMonth()
    The two perform related calculations, so test them together.
    RESULT : tst_QDate::monthLengths():
         0.33 msecs per iteration (total: 87, iterations: 256)
    
    Change-Id: I86b36a9182b00c0a1f40e858ed3e7812434974c4
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

---
+++ tests/benchmarks/corelib/tools/qdate/qdate.pro
@@ -0,0 +1,4 @@
+TARGET = tst_bench_qdate
+QT = core testlib
+
+SOURCES += tst_bench_qdate.cpp
---
+++ tests/benchmarks/corelib/tools/qdate/tst_bench_qdate.cpp
@@ -0,0 +1,53 @@
+/****************************************************************************
+**
+** Copyright (C) 2019 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the QtCore module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QDateTime>
+#include <QTest>
+
+class tst_QDate : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void monthLengths();
+};
+
+void tst_QDate::monthLengths()
+{
+    QBENCHMARK {
+        for (int year = 1900; year <= 2100; year++) {
+            bool check = true;
+            for (int month = 1; month <= 12; month++)
+                check &= QDate::isValid(year, month, QDate(year, month, 1).daysInMonth());
+            Q_UNUSED(check);
+        }
+    }
+}
+
+QTEST_MAIN(tst_QDate)
+#include "tst_bench_qdate.moc"
