4cc6e1419294a729e53d698bace2254903c1429b
Marc Mutz <marc.mutz@kdab.com>
2019-06-21 18:35:22 +0200

    QSimpleTextCodec: fix load memory order of atomic pointer
    The pointer value is not the only data we're interested in, but
    instead points to indirect data, so we need a release fence on store
    (present) and a corresponding acquire fence on load (was missing).
    
    Change-Id: I51f8251c0c7f4056192880430f2be5e0836dbed6
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 6f84829031f318bfda1deff5f409b5ea6c6a5c5f)

--- src/corelib/codecs/qsimplecodec.cpp
+++ src/corelib/codecs/qsimplecodec.cpp
@@ -612,7 +612,7 @@ QSimpleTextCodec::QSimpleTextCodec(int i) : forwardIndex(i), reverseMap(0)
 
 QSimpleTextCodec::~QSimpleTextCodec()
 {
-    delete reverseMap.load();
+    delete reverseMap.loadAcquire();
 }
 
 static QByteArray *buildReverseMap(int forwardIndex)
@@ -664,12 +664,12 @@ QByteArray QSimpleTextCodec::convertFromUnicode(const QChar *in, int length, Con
     const char replacement = (state && state->flags & ConvertInvalidToNull) ? 0 : '?';
     int invalid = 0;
 
-    QByteArray *rmap = reverseMap.load();
+    QByteArray *rmap = reverseMap.loadAcquire();
     if (!rmap){
         rmap = buildReverseMap(this->forwardIndex);
         if (!reverseMap.testAndSetRelease(0, rmap)) {
             delete rmap;
-            rmap = reverseMap.load();
+            rmap = reverseMap.loadAcquire();
         }
     }
 
