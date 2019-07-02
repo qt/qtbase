24b9424adcda1ab083fae4ae857007227ba5fee2
Dimitrios Apostolou <dimitrios.apostolou@qt.io>
2019-07-02 14:24:25 +0200

    Skip flaky test on MacOS_10_12
    This is the most flaky-pass test currently.
    It fails the first time it is run on MacOS_10_12, but
    succeeds all the following times.
    This happens extremely often, so disable it until the issue
    is resolved.
    
    Task-number: QTBUG-76566
    Change-Id: I94359eceb91c3b958930424e6c8b5957fb3f1252
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- tests/auto/widgets/graphicsview/qgraphicsitem/tst_qgraphicsitem.cpp
+++ tests/auto/widgets/graphicsview/qgraphicsitem/tst_qgraphicsitem.cpp
@@ -5049,6 +5049,12 @@ public:
 
 void tst_QGraphicsItem::paint()
 {
+#if defined(Q_OS_MACOS)
+    if (QSysInfo::productVersion() == QLatin1String("10.12")) {
+        QSKIP("Test is very flaky on MacOS_10_12, see QTBUG-76566");
+    }
+#endif
+
     QGraphicsScene scene;
 
     PaintTester paintTester;
