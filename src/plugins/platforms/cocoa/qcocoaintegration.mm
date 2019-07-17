af683471bde93d1859544e7fd4c0638995b1fd5f
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2019-07-17 18:09:47 +0200

    macOS: Add QCocoaWindowManager for dealing with window levels
    Moves and improves the logic for lowering splash screens to a dedicated
    window manager, which will learn more tricks in patches to come.
    
    Change-Id: I8b8fd1dd78fdaf6f106a59c84d2a59254f3539c3
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- src/plugins/platforms/cocoa/qcocoaintegration.mm
+++ src/plugins/platforms/cocoa/qcocoaintegration.mm
@@ -217,25 +217,6 @@ QCocoaIntegration::QCocoaIntegration(const QStringList &paramList)
 
     connect(qGuiApp, &QGuiApplication::focusWindowChanged,
         this, &QCocoaIntegration::focusWindowChanged);
-
-    static auto splashScreenHider = QMacKeyValueObserver(NSApp, @"modalWindow", []{
-        const QWindowList allWindows = QGuiApplication::topLevelWindows();
-        for (QWindow *window : allWindows) {
-            if ((window->flags() & Qt::SplashScreen) == Qt::SplashScreen) {
-                QCocoaWindow *platformWindow = static_cast<QCocoaWindow*>(window->handle());
-                NSWindow *splashWindow = platformWindow->view().window;
-                if (!splashWindow)
-                    continue;
-                if (NSApp.modalWindow) {
-                    NSInteger originalLevel = splashWindow.level;
-                    splashWindow.level = NSNormalWindowLevel;
-                    window->setProperty("_q_levelBeforeModalSession", (qlonglong)originalLevel);
-                } else if (NSInteger originalLevel = window->property("_q_levelBeforeModalSession").toLongLong()) {
-                    splashWindow.level = originalLevel;
-                }
-            }
-        }
-    });
 }
 
 QCocoaIntegration::~QCocoaIntegration()
