qt/qtbase

commit 8010e906d3612aface0daccde41d1a65fca04b0c

Optimize non-const overload of QJsonObject::operator[]
Refactored parts of insert() into a new private method insertAt(), which
can also be called by operator[]() to avoid a redundant key lookup.

This is in preparation for overloading QJsonObject's non-const methods
on QLatin1String.

As a bonus, this also avoids a redundant key lookup in
QJsonValueRef::operator=().

Change-Id: Ic481981d838e50bc55fb8e7844536749781899ce
Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
Reviewed-by: Anton Kudryavtsev <antkudr@mail.ru>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/serialization/qjsonobject.cpp
@@ -446,11 +446,10 @@ QJsonValue QJsonObject::operator [](const QString &key) const
  */
 QJsonValueRef QJsonObject::operator [](const QString &key)
 {
-    // ### somewhat inefficient, as we lookup the key twice if it doesn't yet exist
     bool keyExists = false;
-    int index = o ? o->indexOf(key, &keyExists) : -1;
+    int index = o ? o->indexOf(key, &keyExists) : 0;
     if (!keyExists) {
-        iterator i = insert(key, QJsonValue());
+        iterator i = insertAt(index, key, QJsonValue(), false);
         index = i.i;
     }
     return QJsonValueRef(this, index);
@@ -485,6 +484,16 @@ QJsonObject::iterator QJsonObject::insert(const QString &key, const QJsonValue &
         remove(key);
         return end();
     }
+    bool keyExists = false;
+    int pos = o ? o->indexOf(key, &keyExists) : 0;
+    return insertAt(pos, key, value, keyExists);
+}
+
+/*!
+    \internal
+ */
+QJsonObject::iterator QJsonObject::insertAt(int pos, const QString &key, const QJsonValue &value, bool keyExists)
+{
     QJsonValue val = value;
 
     bool latinOrIntValue;
@@ -500,8 +509,6 @@ QJsonObject::iterator QJsonObject::insert(const QString &key, const QJsonValue &
     if (!o->length)
         o->tableOffset = sizeof(QJsonPrivate::Object);
 
-    bool keyExists = false;
-    int pos = o->indexOf(key, &keyExists);
     if (keyExists)
         ++d->compactionCounter;
 
@@ -1289,7 +1296,7 @@ void QJsonObject::setValueAt(int i, const QJsonValue &val)
     Q_ASSERT(o && i >= 0 && i < (int)o->length);
 
     QJsonPrivate::Entry *e = o->entryAt(i);
-    insert(e->key(), val);
+    insertAt(i, e->key(), val, true);
 }
 
 uint qHash(const QJsonObject &object, uint seed)

src/corelib/serialization/qjsonobject.h
@@ -251,6 +251,7 @@ private:
     QString keyAt(int i) const;
     QJsonValue valueAt(int i) const;
     void setValueAt(int i, const QJsonValue &val);
+    iterator insertAt(int i, const QString &key, const QJsonValue &val, bool exists);
 
     QJsonPrivate::Data *d;
     QJsonPrivate::Object *o;

commit 31ffc7bf2a0efe40116764a8c84939d2df4cdeae

Fix removal of QJsonObject properties when assigning undefined
Commit 8010e906d3612aface0daccde41d1a65fca04b0c accidentally ended up
removing the removal-on-undefined-insertion check by calling insertAt
instead of insert, which had it. This patch moves the check back into
setValueAt.

Change-Id: Ic381e284d3da37e31c4eb29f79dfab9c55c2e3e9
Fixes: QTBUG-77204
Reviewed-by: Liang Qi <liang.qi@qt.io>
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

src/corelib/serialization/qjsonobject.cpp
@@ -1476,7 +1476,10 @@ void QJsonObject::setValueAt(int i, const QJsonValue &val)
     Q_ASSERT(o && i >= 0 && i < (int)o->length);
 
     QJsonPrivate::Entry *e = o->entryAt(i);
-    insertAt(i, e->key(), val, true);
+    if (val.t == QJsonValue::Undefined)
+        removeAt(i);
+    else
+        insertAt(i, e->key(), val, true);
 }
 
 /*!

tests/auto/corelib/serialization/json/tst_qtjson.cpp
@@ -1132,6 +1132,8 @@ void tst_QtJson::undefinedValues()
     QJsonObject object;
     object.insert("Key", QJsonValue(QJsonValue::Undefined));
     QCOMPARE(object.size(), 0);
+    object["Key"] = QJsonValue(QJsonValue::Undefined);
+    QCOMPARE(object.size(), 0);
 
     object.insert("Key", QLatin1String("Value"));
     QCOMPARE(object.size(), 1);