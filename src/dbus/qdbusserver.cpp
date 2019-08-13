6d3a4546934827955f0eb2b07a9928f82790ba37
Mårten Nordheim <marten.nordheim@qt.io>
2019-08-13 15:17:40 +0200

    DBus: fix deadlock when destroying QDBusServer
    Observed infrequently in the QDBus tests, it would deadlock when
    destroying QDBusServer at the same time as qDBusNewConnection was being
    executed as they were locking the same locks, but in opposite order.
    
    QDBusServer locks d->lock, then QDBusConnectionManager::instance()->mutex.
    While qDBusNewConnection locks QDBusConnectionManager::instance()->mutex,
    then serverConnection->lock (and serverConnection here
    is QDBusServer's d-pointer).
    
    QOrderedMutexLocker cannot be used in this situation because it
    operates on QMutex*, which d->lock (QReadWriteLock) is not.
    
    Change the code to lock QDBusConnectionManager's mutex before d->lock
    and then unlock the QMutexLocker where it would previously destruct.
    If QDBusConnectionManager has already been destroyed then we pass a
    nullptr to the QMutexLocker which is fine and will not do anything.
    
    Fixes: QTBUG-74635
    Change-Id: I7f02d7759da67377996ef042c81b0969ccb8aadb
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/dbus/qdbusserver.cpp
+++ src/dbus/qdbusserver.cpp
@@ -109,12 +109,16 @@ QDBusServer::QDBusServer(QObject *parent)
 */
 QDBusServer::~QDBusServer()
 {
-    QWriteLocker locker(&d->lock);
+    QMutex *managerMutex = nullptr;
+    if (QDBusConnectionManager::instance())
+        managerMutex = &QDBusConnectionManager::instance()->mutex;
+    QMutexLocker locker(managerMutex);
+    QWriteLocker writeLocker(&d->lock);
     if (QDBusConnectionManager::instance()) {
-        QMutexLocker locker(&QDBusConnectionManager::instance()->mutex);
         for (const QString &name : qAsConst(d->serverConnectionNames))
             QDBusConnectionManager::instance()->removeConnection(name);
         d->serverConnectionNames.clear();
+        locker.unlock();
     }
     d->serverObject = nullptr;
     d->ref.store(0);
