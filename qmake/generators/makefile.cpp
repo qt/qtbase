qt/qtbase

commit 80dea66424c4887f66f64ea1920ab460f27b78af

qmake: don't drop non-existing files from .depend_command output
this allows for dynamic generation of the dependencies.

Task-number: QTBUG-61267
Change-Id: If5b8aed6b9e4bde189cc3ba6a5f13dcf8def3a1e
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

qmake/generators/makefile.cpp
@@ -1997,7 +1997,9 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                             for(int i = 0; i < dep_cmd_deps.count(); ++i) {
                                 QString &file = dep_cmd_deps[i];
                                 QString absFile = outDir.absoluteFilePath(file);
-                                if (exists(absFile)) {
+                                if (absFile == file) {
+                                    // already absolute; don't do any checks.
+                                } else if (exists(absFile)) {
                                     file = absFile;
                                 } else {
                                     QString localFile;
@@ -2016,7 +2018,7 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                                                                      " prints paths relative to source directory",
                                                                      (*it).toLatin1().constData());
                                         else
-                                            file.clear();
+                                            file = absFile;  // fallback for generated resources
                                     } else {
                                         file = localFile;
                                     }
@@ -2090,7 +2092,9 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                         for(int i = 0; i < dep_cmd_deps.count(); ++i) {
                             QString &file = dep_cmd_deps[i];
                             QString absFile = outDir.absoluteFilePath(file);
-                            if (exists(absFile)) {
+                            if (absFile == file) {
+                                // already absolute; don't do any checks.
+                            } else if (exists(absFile)) {
                                 file = absFile;
                             } else {
                                 QString localFile;
@@ -2109,7 +2113,7 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                                                                  " prints paths relative to source directory",
                                                                  (*it).toLatin1().constData());
                                     else
-                                        file.clear();
+                                        file = absFile;  // fallback for generated resources
                                 } else {
                                     file = localFile;
                                 }

commit 90779d96adfa51a3fc00b6c0d93168b4bbfcf983

Ignore non-existent .ui file dependencies
Commit 80dea664 broke .ui files with global includes that are not part
of the project, because we blindly added every file path that falls
out of 'uic -d' as dependency.

Introduce the extra compiler CONFIG flag dep_existing_only to bring
back the old behavior that ignores non-existent dependencies and set
it for uic.

Change-Id: I6eaa82817c932a98ebac6d08115a9815d4b9dd21
Fixes: QTBUG-78144
Reviewed-by: Kai Koehne <kai.koehne@qt.io>

mkspecs/features/uic.prf
@@ -9,7 +9,7 @@ uic.depend_command = $$QMAKE_UIC_DEP -d ${QMAKE_FILE_IN}
 uic.output = $$UI_DIR/$${QMAKE_MOD_UIC}${QMAKE_FILE_BASE}$${first(QMAKE_EXT_H)}
 uic.input = FORMS
 uic.variable_out = GENERATED_FILES
-uic.CONFIG += no_link target_predeps dep_lines
+uic.CONFIG += no_link target_predeps dep_lines dep_existing_only
 uic.name = UIC ${QMAKE_FILE_IN}
 silent:uic.commands = @echo uic ${QMAKE_FILE_IN} && $$uic.commands
 QMAKE_EXTRA_COMPILERS += uic

qmake/doc/src/qmake-manual.qdoc
@@ -4816,6 +4816,11 @@
         \li The dependencies for the output only get generated from the depends
             member and from nowhere else.
     \row
+        \li dep_existing_only
+        \li Every dependency that is a result of .depend_command is checked for
+            existence. Non-existing dependencies are ignored.
+            This value was introduced in Qt 5.13.2.
+    \row
         \li dep_lines
         \li The output from the .depend_command is interpreted to be one file
             per line. The default is to split on whitespace and is maintained

qmake/generators/makefile.cpp
@@ -1992,6 +1992,7 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
             }
             t << endl;
         }
+        const bool existingDepsOnly = config.contains("dep_existing_only");
         QStringList tmp_dep = project->values(ProKey(*it + ".depends")).toQStringList();
         if (config.indexOf("combine") != -1) {
             if (tmp_out.contains(QRegExp("(^|[^$])\\$\\{QMAKE_(?!VAR_)"))) {
@@ -2033,6 +2034,8 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                                             warn_msg(WarnDeprecated, ".depend_command for extra compiler %s"
                                                                      " prints paths relative to source directory",
                                                                      (*it).toLatin1().constData());
+                                        else if (existingDepsOnly)
+                                            file.clear();
                                         else
                                             file = absFile;  // fallback for generated resources
                                     } else {
@@ -2118,6 +2121,8 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                                         warn_msg(WarnDeprecated, ".depend_command for extra compiler %s"
                                                                  " prints paths relative to source directory",
                                                                  (*it).toLatin1().constData());
+                                    else if (existingDepsOnly)
+                                        file.clear();
                                     else
                                         file = absFile;  // fallback for generated resources
                                 } else {