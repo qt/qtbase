52889ded67c55b1bdd847b3445f271c46a5268d2
Shawn Rutledge <shawn.rutledge@qt.io>
2019-09-20 13:13:17 +0200

    Add QQuickMultiPointTouchArea as a friend of QTouchEvent
    QQuickMultiPointTouchArea needs to understand whether a stationary
    touchpoint has relevant property changes, to decide whether to emit its
    updated() signal. Amends 217dd1b3b03cd40b4bb926631464c684f7f84a69
    
    Task-number: QTBUG-77142
    Change-Id: I85e031820bef9d687369b7d67a57c67fe2875b4b
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit bc35941dbbf6cbd09805a2217ecb062c565855ad)

--- src/gui/kernel/qevent.h
+++ src/gui/kernel/qevent.h
@@ -936,6 +936,7 @@ public:
         friend class QApplication;
         friend class QApplicationPrivate;
         friend class QQuickPointerTouchEvent;
+        friend class QQuickMultiPointTouchArea;
     };
 
 #if QT_DEPRECATED_SINCE(5, 0)
