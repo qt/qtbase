c4956dbb6749540af5aab4937b1f913a78dda2e6
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-09-23 11:21:17 +0200

    Do not pass nullptr as '%s'
    Fixes GCC 9 warning.
    
    Change-Id: I2a21d863267b444a29697aa026c21b47e3ac8382
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>

--- src/testlib/qtestcase.cpp
+++ src/testlib/qtestcase.cpp
@@ -2709,8 +2709,11 @@ template <> Q_TESTLIB_EXPORT char *QTest::toString<char>(const char &t)
  */
 char *QTest::toString(const char *str)
 {
-    if (!str)
-        return 0;
+    if (!str) {
+        char *msg = new char[1];
+        *msg = '\0';
+        return msg;
+    }
     char *msg = new char[strlen(str) + 1];
     return qstrcpy(msg, str);
 }
