1daaaf74a63a09702b65676ba18f559d3c6ebc83
Alexandru Croitor <alexandru.croitor@qt.io>
2019-09-27 15:12:46 +0200

    Fix libsystemd find module name
    At least on Ubuntu the name of the pkg config package
    is libsystemd and not systemd.
    
    This fixes libsystemd showing up as not found in the feature
    summary.
    
    Change-Id: I099a253026ca8096b9e2f348a5f35209d44acc6c
    Reviewed-by: Albert Astals Cid <albert.astals.cid@kdab.com>
    Reviewed-by: Qt CMake Build Bot

--- cmake/FindLibsystemd.cmake
+++ cmake/FindLibsystemd.cmake
@@ -1,6 +1,6 @@
 find_package(PkgConfig)
 
-pkg_check_modules(Libsystemd systemd IMPORTED_TARGET)
+pkg_check_modules(Libsystemd libsystemd IMPORTED_TARGET)
 
 if (NOT TARGET PkgConfig::Libsystemd)
     set(Libsystemd_FOUND 0)
