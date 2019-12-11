5a26bf9d65b75db3abefa967beb13b2510719bd5
Friedemann Kleint <Friedemann.Kleint@qt.io>
2019-12-11 09:30:26 +0100

    Avoid initializing QFlags with 0 or nullptr in macOS-specific code
    It is being deprecated.
    
    Change-Id: If1b0b058140e197d41efae93025c4eefc2ed9bbd
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- src/widgets/widgets/qmaccocoaviewcontainer_mac.mm
+++ src/widgets/widgets/qmaccocoaviewcontainer_mac.mm
@@ -138,7 +138,7 @@ QMacCocoaViewContainerPrivate::~QMacCocoaViewContainerPrivate()
 
 */
 QMacCocoaViewContainer::QMacCocoaViewContainer(NSView *view, QWidget *parent)
-   : QWidget(*new QMacCocoaViewContainerPrivate, parent, 0)
+   : QWidget(*new QMacCocoaViewContainerPrivate, parent, {})
 {
     // Ensures that we have a QWindow, even if we're not a top level widget
     setAttribute(Qt::WA_NativeWindow);
