10afe69c16e641909901159bfad5b233d641151a
Lars Knoll <lars.knoll@qt.io>
2020-01-28 12:48:53 +0100

    Use a QMap instead of a QHash for the ProValueMap
    qmake relies heavily on stable references to nodes stored within the
    container. QHash in Qt6 doesn't offer that guarantee, so use a QMap
    instead, that supports this.
    
    Change-Id: Ifcf3d67098585ea26f4e02f4570d407a56e33c9c
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- qmake/library/proitems.h
+++ qmake/library/proitems.h
@@ -35,6 +35,7 @@
 #include <qstring.h>
 #include <qvector.h>
 #include <qhash.h>
+#include <qmap.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -316,7 +317,7 @@ Q_DECLARE_TYPEINFO(ProStringList, Q_MOVABLE_TYPE);
 inline ProStringList operator+(const ProStringList &one, const ProStringList &two)
     { ProStringList ret = one; ret += two; return ret; }
 
-typedef QHash<ProKey, ProStringList> ProValueMap;
+typedef QMap<ProKey, ProStringList> ProValueMap;
 
 // These token definitions affect both ProFileEvaluator and ProWriter
 enum ProToken {
