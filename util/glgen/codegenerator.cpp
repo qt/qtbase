qt/qtbase

commit ff2b2032a089d74975da4a3fac7c7c90989e6dc5

Remove usages of deprecated APIs from QtAlgorithms
Task-number: QTBUG-76491
Change-Id: I9dab736a0cbd2e86588919640c26e8ce6b3674d0
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>
Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

util/glgen/codegenerator.cpp
@@ -184,7 +184,7 @@ void CodeGenerator::writeCoreFactoryImplementation(const QString &fileName) cons
 
     // Get the set of version functions classes we need to create
     QList<Version> versions = m_parser->versions();
-    qSort(versions.begin(), versions.end(), qGreater<Version>());
+    std::sort(m_versions.begin(), m_versions.end(), std::greater<Version>());
 
     // Outout the #include statements
     stream << QStringLiteral("#if !defined(QT_OPENGL_ES_2)") << endl;

src/corelib/doc/snippets/code/doc_src_qset.cpp
@@ -131,7 +131,7 @@ while (i != set.end()) {
 //! [10]
 QSet<QString> set;
 ...
-QSet<QString>::iterator it = qFind(set.begin(), set.end(), "Jeanette");
+QSet<QString>::iterator it = std::find(set.begin(), set.end(), "Jeanette");
 if (it != set.end())
     cout << "Found Jeanette" << Qt::endl;
 //! [10]
@@ -150,7 +150,7 @@ for (i = set.begin(); i != set.end(); ++i)
 //! [12]
 QSet<QString> set;
 ...
-QSet<QString>::iterator it = qFind(set.begin(), set.end(), "Jeanette");
+QSet<QString>::iterator it = std::find(set.begin(), set.end(), "Jeanette");
 if (it != set.constEnd())
     cout << "Found Jeanette" << Qt::endl;
 //! [12]
@@ -161,7 +161,7 @@ QSet<QString> set;
 set << "red" << "green" << "blue" << ... << "black";
 
 QList<QString> list = set.toList();
-qSort(list);
+std::sort(list.begin(), list.end());
 //! [13]
 
 

src/corelib/doc/snippets/code/src_corelib_tools_qlinkedlist.cpp
@@ -119,8 +119,8 @@ for (i = list.begin(); i != list.end(); ++i)
 //! [8]
 QLinkedList<QString> list;
 ...
-QLinkedList<QString>::iterator it = qFind(list.begin(),
-                                          list.end(), "Joel");
+QLinkedList<QString>::iterator it = std::find(list.begin(),
+                                              list.end(), "Joel");
 if (it != list.end())
     cout << "Found Joel" << Qt::endl;
 //! [8]
@@ -189,8 +189,8 @@ for (i = list.constBegin(); i != list.constEnd(); ++i)
 //! [15]
 QLinkedList<QString> list;
 ...
-QLinkedList<QString>::iterator it = qFind(list.constBegin(),
-                                          list.constEnd(), "Joel");
+QLinkedList<QString>::const_iterator it = std::find(list.constBegin(),
+                                                    list.constEnd(), "Joel");
 if (it != list.constEnd())
     cout << "Found Joel" << Qt::endl;
 //! [15]

src/corelib/doc/snippets/code/src_corelib_tools_qlistdata.cpp
@@ -247,7 +247,7 @@ QSet<int> set;
 set << 20 << 30 << 40 << ... << 70;
 
 QList<int> list = QList<int>::fromSet(set);
-qSort(list);
+std::sort(list.begin(), list.end());
 //! [23]
 
 

src/corelib/doc/src/containers.qdoc
@@ -666,7 +666,7 @@
     \li \b{Logarithmic time:} O(log \e n). A function that runs in
        logarithmic time is a function whose running time is
        proportional to the logarithm of the number of items in the
-       container. One example is qBinaryFind().
+       container. One example is the binary search algorithm.
 
     \li \b{Linear time:} O(\e n). A function that runs in linear time
        will execute in a time directly proportional to the number of

src/corelib/tools/qlinkedlist.cpp
@@ -742,8 +742,7 @@ const QLinkedListData QLinkedListData::shared_null = {
     \snippet code/src_corelib_tools_qlinkedlist.cpp 7
 
     STL-style iterators can be used as arguments to \l{generic
-    algorithms}. For example, here's how to find an item in the list
-    using the qFind() algorithm:
+    algorithms}. For example, here's how to find an item in the list:
 
     \snippet code/src_corelib_tools_qlinkedlist.cpp 8
 
@@ -987,8 +986,7 @@ const QLinkedListData QLinkedListData::shared_null = {
     \snippet code/src_corelib_tools_qlinkedlist.cpp 14
 
     STL-style iterators can be used as arguments to \l{generic
-    algorithms}. For example, here's how to find an item in the list
-    using the qFind() algorithm:
+    algorithms}. For example, here's how to find an item in the list:
 
     \snippet code/src_corelib_tools_qlinkedlist.cpp 15
 

src/widgets/doc/src/model-view-programming.qdoc
@@ -1912,7 +1912,7 @@
 
     \section3 Custom sorting models
 
-    QSortFilterProxyModel instances use Qt's built-in qStableSort() function to set up
+    QSortFilterProxyModel instances use std::stable_sort() function to set up
     mappings between items in the source model and those in the proxy model, allowing a
     sorted hierarchy of items to be exposed to views without modifying the structure of the
     source model. To provide custom sorting behavior, reimplement the

tests/auto/corelib/io/largefile/tst_largefile.cpp
@@ -28,7 +28,6 @@
 
 #include <QTest>
 
-#include <QtAlgorithms>
 #include <QFile>
 #include <QFileInfo>
 #include <QRandomGenerator>

commit d8df846631a54c624ccd6fd5a533c8ddc8f2ab35

glgen: Fix compile error
Fixes compile error introduced in ff2b2032a08.

Change-Id: Iee1016f7cf05638db575475c5c0d632f8edb3a8c
Reviewed-by: Sona Kurazyan <sona.kurazyan@qt.io>

util/glgen/codegenerator.cpp
@@ -184,7 +184,7 @@ void CodeGenerator::writeCoreFactoryImplementation(const QString &fileName) cons
 
     // Get the set of version functions classes we need to create
     QList<Version> versions = m_parser->versions();
-    std::sort(m_versions.begin(), m_versions.end(), std::greater<Version>());
+    std::sort(versions.begin(), versions.end(), std::greater<Version>());
 
     // Outout the #include statements
     stream << QStringLiteral("#if !defined(QT_OPENGL_ES_2)") << endl;