qt/qtbase

commit dd04fb639bf357e66d0586faed78a3043a62819e

Set also ndk.dir
It's needed when ANDROID_NDK_HOME is not set

Fixes: QTBUG-81978
Change-Id: Id6108083ae62757ca839d652793d98a89e6cb6b5
Reviewed-by: Andy Shaw <andy.shaw@qt.io>

src/tools/androiddeployqt/main.cpp
@@ -2287,7 +2287,8 @@ static bool mergeGradleProperties(const QString &path, GradleProperties properti
 bool buildAndroidProject(const Options &options)
 {
     GradleProperties localProperties;
-    localProperties["sdk.dir"] = options.sdkPath.toLocal8Bit();
+    localProperties["sdk.dir"] = options.sdkPath.toUtf8();
+    localProperties["ndk.dir"] = options.ndkPath.toUtf8();
 
     if (!mergeGradleProperties(options.outputDirectory + QLatin1String("local.properties"), localProperties))
         return false;

commit 1a18e138f4d17c219c6ffd6e4396c30b960818b4

Android: Fix deployment on Windows host
The paths in the build properties require forward slashes
apparently. On Windows, we would default to native backslashes
and they would be stripped from the path. Converting to forward
slashes fixes the problem.

Issue was introduced by dd04fb639bf357e66d0586faed78a3043a62819e,
since before that, the NDK path was retrieved from the environment.

Fixes: QTBUG-82944
Change-Id: I6c51113efcf671461a5871991b3225a52b95266c
Reviewed-by: BogDan Vatra <bogdan@kdab.com>

src/tools/androiddeployqt/main.cpp
@@ -2287,8 +2287,8 @@ static bool mergeGradleProperties(const QString &path, GradleProperties properti
 bool buildAndroidProject(const Options &options)
 {
     GradleProperties localProperties;
-    localProperties["sdk.dir"] = options.sdkPath.toUtf8();
-    localProperties["ndk.dir"] = options.ndkPath.toUtf8();
+    localProperties["sdk.dir"] = QDir::fromNativeSeparators(options.sdkPath).toUtf8();
+    localProperties["ndk.dir"] = QDir::fromNativeSeparators(options.ndkPath).toUtf8();
 
     if (!mergeGradleProperties(options.outputDirectory + QLatin1String("local.properties"), localProperties))
         return false;