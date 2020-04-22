2aa0e71507953ea87cc63cf27f9701bac1bbb094
Kai Koehne <kai.koehne@qt.io>
2020-04-22 16:49:34 +0200

    CMake: Document versionless commands
    Change-Id: I249c10bf3338b08477ce04f7c2d5098390a6f9a0
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 55b470e41120a5878a7cdbb792f12e63e603ea68)
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>

--- src/corelib/doc/snippets/cmake-macros/examples.cmake
+++ src/corelib/doc/snippets/cmake-macros/examples.cmake
@@ -4,11 +4,23 @@ qt5_wrap_cpp(SOURCES myapp.h)
 add_executable(myapp ${SOURCES})
 #! [qt5_wrap_cpp]
 
+#! [qt_wrap_cpp]
+set(SOURCES myapp.cpp main.cpp)
+qt_wrap_cpp(SOURCES myapp.h)
+add_executable(myapp ${SOURCES})
+#! [qt_wrap_cpp]
+
 #! [qt5_add_resources]
 set(SOURCES main.cpp)
 qt5_add_resources(SOURCES example.qrc)
 add_executable(myapp ${SOURCES})
 #! [qt5_add_resources]
+
+#! [qt_add_resources]
+set(SOURCES main.cpp)
+qt_add_resources(SOURCES example.qrc)
+add_executable(myapp ${SOURCES})
+#! [qt_add_resources]
 
 #! [qt5_add_big_resources]
 set(SOURCES main.cpp)
@@ -16,14 +28,29 @@ qt5_add_big_resources(SOURCES big_resource.qrc)
 add_executable(myapp ${SOURCES})
 #! [qt5_add_big_resources]
 
+#! [qt_add_big_resources]
+set(SOURCES main.cpp)
+qt_add_big_resources(SOURCES big_resource.qrc)
+add_executable(myapp ${SOURCES})
+#! [qt_add_big_resources]
+
 #! [qt5_add_binary_resources]
 qt5_add_binary_resources(resources project.qrc OPTIONS -no-compress)
 add_dependencies(myapp resources)
 #! [qt5_add_binary_resources]
 
+#! [qt_add_binary_resources]
+qt_add_binary_resources(resources project.qrc OPTIONS -no-compress)
+add_dependencies(myapp resources)
+#! [qt_add_binary_resources]
+
 #! [qt5_generate_moc]
 qt5_generate_moc(main.cpp main.moc TARGET myapp)
 #! [qt5_generate_moc]
+
+#! [qt_generate_moc]
+qt_generate_moc(main.cpp main.moc TARGET myapp)
+#! [qt_generate_moc]
 
 #! [qt5_import_plugins]
 add_executable(myapp main.cpp)
@@ -35,3 +62,14 @@ qt5_import_plugins(myapp
     EXCLUDE_BY_TYPE sqldrivers
 )
 #! [qt5_import_plugins]
+
+#! [qt_import_plugins]
+add_executable(myapp main.cpp)
+target_link_libraries(myapp Qt::Gui Qt::Sql)
+qt_import_plugins(myapp
+    INCLUDE Qt::QCocoaIntegrationPlugin
+    EXCLUDE Qt::QMinimalIntegrationPlugin
+    INCLUDE_BY_TYPE imageformats Qt5::QGifPlugin Qt5::QJpegPlugin
+    EXCLUDE_BY_TYPE sqldrivers
+)
+#! [qt_import_plugins]
