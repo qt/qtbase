55b470e41120a5878a7cdbb792f12e63e603ea68
Kai Koehne <kai.koehne@qt.io>
2020-04-22 16:49:34 +0200

    CMake: Document versionless commands
    Pick-to: 5.15
    Change-Id: I249c10bf3338b08477ce04f7c2d5098390a6f9a0
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/widgets/doc/snippets/cmake-macros/examples.cmake
+++ src/widgets/doc/snippets/cmake-macros/examples.cmake
@@ -3,3 +3,9 @@ set(SOURCES mainwindow.cpp main.cpp)
 qt5_wrap_ui(SOURCES mainwindow.ui)
 add_executable(myapp ${SOURCES})
 #! [qt5_wrap_ui]
+
+#! [qt_wrap_ui]
+set(SOURCES mainwindow.cpp main.cpp)
+qt_wrap_ui(SOURCES mainwindow.ui)
+add_executable(myapp ${SOURCES})
+#! [qt_wrap_ui]
