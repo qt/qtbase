66876da1fd18a037c613639a552001ee87e68a7a
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-05-14 08:56:02 +0200

    Initialize TextureOp and BufferOp structs
    Fixes Coverity warnings, and avoids hard-to-debug errors, at minimal
    overhead.
    
    Change-Id: I3ff530a9263693d1123932458b3e186e79a14b7e
    Coverity-Id: 263692
    Coverity-Id: 263693
    Coverity-Id: 263699
    Coverity-Id: 263700
    Coverity-Id: 263702
    Coverity-Id: 263705
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit 6f34c0c650d3309fdda4a3be5424c4082f1f531c)

--- src/gui/rhi/qrhi_p_p.h
+++ src/gui/rhi/qrhi_p_p.h
@@ -291,7 +291,7 @@ public:
 
         static BufferOp dynamicUpdate(QRhiBuffer *buf, int offset, int size, const void *data)
         {
-            BufferOp op;
+            BufferOp op = {};
             op.type = DynamicUpdate;
             op.buf = buf;
             op.offset = offset;
@@ -301,7 +301,7 @@ public:
 
         static BufferOp staticUpload(QRhiBuffer *buf, int offset, int size, const void *data)
         {
-            BufferOp op;
+            BufferOp op = {};
             op.type = StaticUpload;
             op.buf = buf;
             op.offset = offset;
@@ -311,7 +311,7 @@ public:
 
         static BufferOp read(QRhiBuffer *buf, int offset, int size, QRhiBufferReadbackResult *result)
         {
-            BufferOp op;
+            BufferOp op = {};
             op.type = Read;
             op.buf = buf;
             op.offset = offset;
@@ -343,7 +343,7 @@ public:
 
         static TextureOp upload(QRhiTexture *tex, const QRhiTextureUploadDescription &desc)
         {
-            TextureOp op;
+            TextureOp op = {};
             op.type = Upload;
             op.dst = tex;
             for (auto it = desc.cbeginEntries(), itEnd = desc.cendEntries(); it != itEnd; ++it)
@@ -353,7 +353,7 @@ public:
 
         static TextureOp copy(QRhiTexture *dst, QRhiTexture *src, const QRhiTextureCopyDescription &desc)
         {
-            TextureOp op;
+            TextureOp op = {};
             op.type = Copy;
             op.dst = dst;
             op.src = src;
@@ -363,7 +363,7 @@ public:
 
         static TextureOp read(const QRhiReadbackDescription &rb, QRhiReadbackResult *result)
         {
-            TextureOp op;
+            TextureOp op = {};
             op.type = Read;
             op.rb = rb;
             op.result = result;
@@ -372,7 +372,7 @@ public:
 
         static TextureOp genMips(QRhiTexture *tex, int layer)
         {
-            TextureOp op;
+            TextureOp op = {};
             op.type = GenMips;
             op.dst = tex;
             op.layer = layer;
