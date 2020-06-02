005f7cb29eccb436113d5d6c46c86c74420a8bbe
Matthias Kardel <matthias.kardel@seequent.com>
2020-06-03 11:37:13 +1200

    REAR-2054 Changed number of standard colour rows.

--- src/widgets/dialogs/qcolordialog.cpp
+++ src/widgets/dialogs/qcolordialog.cpp
@@ -1521,7 +1521,7 @@ void QColorDialogPrivate::setCurrentQColor(const QColor &color)
 // size of standard and custom color selector
 enum {
     colorColumns = 8,
-    standardColorRows = 6,
+    standardColorRows = 3,
     customColorRows = 2
 };
 
@@ -1597,7 +1597,7 @@ void QColorDialogPrivate::_q_newCustom(int r, int c)
 
 void QColorDialogPrivate::_q_newStandard(int r, int c)
 {
-    setCurrentRgbColor(QColorDialogOptions::standardColor(r + c * 6));
+    setCurrentRgbColor(QColorDialogOptions::standardColor(r + c * standardColorRows));
     if (custom)
         custom->setSelected(-1,-1);
 }
