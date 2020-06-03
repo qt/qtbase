f7ee1bfe6be8ac9d945ed73ded36e493ca25aea9
Kai Koehne <kai.koehne@qt.io>
2020-06-03 16:54:16 +0200

    Doc: Update CMake snippets to Qt 6
    Change-Id: I53021781a25c141db5d5fc6771192cd8d6ed732a
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/corelib/doc/snippets/overview/using-qt-core.cmake
+++ src/corelib/doc/snippets/overview/using-qt-core.cmake
@@ -1,2 +1,2 @@
-find_package(Qt5 COMPONENTS Core REQUIRED)
-target_link_libraries(mytarget Qt5::Core)
+find_package(Qt6 COMPONENTS Core REQUIRED)
+target_link_libraries(mytarget Qt::Core)
--- src/gui/doc/snippets/overview/using-qt-gui.cmake
+++ src/gui/doc/snippets/overview/using-qt-gui.cmake
@@ -1,2 +1,2 @@
-find_package(Qt5 COMPONENTS Gui REQUIRED)
-target_link_libraries(mytarget Qt5::Gui)
+find_package(Qt6 COMPONENTS Gui REQUIRED)
+target_link_libraries(mytarget Qt::Gui)
