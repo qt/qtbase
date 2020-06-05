ed4c3a2d21832139c590bb248257c55bae853981
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2020-06-05 12:50:43 +0200

    macOS: Respect window type when determining main window status
    Fixes: QTBUG-84405
    Change-Id: I3fc6b15b07a81e7e7e417a5767c2853083c13516
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    (cherry picked from commit 1cd7cbf6179692fc824dc3ba15580372493a4355)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/cocoa/qnswindow.mm
+++ src/plugins/platforms/cocoa/qnswindow.mm
@@ -298,14 +298,12 @@ OSStatus CGSClearWindowTags(const CGSConnectionID, const CGSWindowID, int *, int
 
 - (BOOL)canBecomeMainWindow
 {
-    BOOL canBecomeMain = YES; // By default, windows can become the main window
-
     // Windows with a transient parent (such as combobox popup windows)
     // cannot become the main window:
     if (!m_platformWindow || m_platformWindow->window()->transientParent())
-        canBecomeMain = NO;
+        return NO;
 
-    return canBecomeMain;
+    return [super canBecomeMainWindow];
 }
 
 - (BOOL)isOpaque
