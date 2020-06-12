b7724048f9afa8b3c6bbb35f2eb71b766bb68f8c
Timur Pocheptsov <timur.pocheptsov@qt.io>
2020-06-12 10:38:21 +0200

    long_name_onDemandCerts_member - stop testing/using the deprecated API
    Pick-to: 5.15
    Change-Id: I3d7ea78627afcd0225e717d243da49fd70b58461
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- tests/auto/network/ssl/qsslsocket_onDemandCertificates_member/tst_qsslsocket_onDemandCertificates_member.cpp
+++ tests/auto/network/ssl/qsslsocket_onDemandCertificates_member/tst_qsslsocket_onDemandCertificates_member.cpp
@@ -212,17 +212,6 @@ void tst_QSslSocket_onDemandCertificates_member::onDemandRootCertLoadingMemberMe
 {
     const QString host("www.qt.io");
 
-#if QT_DEPRECATED_SINCE(5, 5)
-    {
-        // not using any root certs -> should not work
-        QSslSocketPtr socket2 = newSocket();
-        this->socket = socket2.data();
-        socket2->setCaCertificates(QList<QSslCertificate>());
-        socket2->connectToHostEncrypted(host, 443);
-        QVERIFY(!waitForEncrypted(socket2.data()));
-    }
-#endif
-
     // not using any root certs -> should not work
     QSslSocketPtr socket2 = newSocket();
     this->socket = socket2.data();
@@ -237,17 +226,6 @@ void tst_QSslSocket_onDemandCertificates_member::onDemandRootCertLoadingMemberMe
     this->socket = socket.data();
     socket->connectToHostEncrypted(host, 443);
     QVERIFY2(waitForEncrypted(socket.data()), qPrintable(socket->errorString()));
-
-#if QT_DEPRECATED_SINCE(5, 5)
-    {
-        // not using any root certs again -> should not work
-        QSslSocketPtr socket3 = newSocket();
-        this->socket = socket3.data();
-        socket3->setCaCertificates(QList<QSslCertificate>());
-        socket3->connectToHostEncrypted(host, 443);
-        QVERIFY(!waitForEncrypted(socket3.data()));
-    }
-#endif
 
     // not using any root certs again -> should not work
     QSslSocketPtr socket3 = newSocket();
