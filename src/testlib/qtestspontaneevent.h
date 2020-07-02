cd67fe582142b308f98eab30d057eceaee8d6d88
Lars Schmertmann <Lars.Schmertmann@governikus.de>
2020-07-02 17:03:35 +0200

    Cleanup qtestspontaneevent.h for Qt 6
    This change is motivated by the comment from bc087db59:
    Qt 6: remove everything except QSpontaneKeyEvent::setSpontaneous()
    
    Task-number: QTBUG-82978
    Change-Id: I5f46ca366c193c06235f88022ec22c6848fbb7b0
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/testlib/qtestspontaneevent.h
+++ src/testlib/qtestspontaneevent.h
@@ -49,54 +49,13 @@
 
 QT_BEGIN_NAMESPACE
 
-
-#ifndef QTEST_NO_SIZEOF_CHECK
-template <int>
-class QEventSizeOfChecker
-{
-private:
-    QEventSizeOfChecker() {}
-};
-
-template <>
-class QEventSizeOfChecker<sizeof(QEvent)>
-{
-public:
-    QEventSizeOfChecker() {}
-};
-#endif
-
 class QSpontaneKeyEvent
 {
 public:
-    void setSpontaneous() { spont = 1; Q_UNUSED(posted) Q_UNUSED(m_accept) Q_UNUSED(reserved) }
-    bool spontaneous() { return spont; }
-    virtual void dummyFunc() {}
-    virtual ~QSpontaneKeyEvent() {}
-
-#ifndef QTEST_NO_SIZEOF_CHECK
-    inline void ifYouGetCompileErrorHereYouUseWrongQt()
-    {
-        // this is a static assert in case QEvent changed in Qt
-        QEventSizeOfChecker<sizeof(QSpontaneKeyEvent)> dummy;
-    }
-#endif
-
-    // ### Qt 6: remove everything except this function:
     static inline void setSpontaneous(QEvent *ev)
     {
         ev->setSpontaneous();
     }
-
-protected:
-    void *d;
-    ushort t;
-
-private:
-    ushort posted : 1;
-    ushort spont : 1;
-    ushort m_accept : 1;
-    ushort reserved : 13;
 };
 
 QT_END_NAMESPACE
