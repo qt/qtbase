8c9482cd3036b11a52f949f6117dd17bc52508ca
Kai Koehne <kai.koehne@qt.io>
2020-08-11 15:27:33 +0200

    Add missing 'We mean it' header to qwindowsmime_p.h
    Change-Id: Ia0085b75b9127d99b10518768cecbb79a4286c8e
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/gui/platform/windows/qwindowsmime_p.h
+++ src/gui/platform/windows/qwindowsmime_p.h
@@ -40,6 +40,17 @@
 #ifndef QWINDOWSMIME_P_H
 #define QWINDOWSMIME_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qt_windows.h>
 #include <QtCore/qvariant.h>
 
