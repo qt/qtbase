81159cf3f7c47dbab4c781f3b4ffa9e0a202b1dc
Laszlo Agocs <laszlo.agocs@qt.io>
2020-08-17 12:28:16 +0200

    rhi: Enable msaatexture manual test on OpenGL
    ...now that a recent path enables MultisampleTextures for ES >= 3.1 and
    GL 3.x and up.
    
    Just need to make sure the .qsb contains something for OpenGL as well.
    
    While we are at it, make updated .qsb files for all the commonly used
    shaders since what we had before was version 4. Bump them to version 5.
    
    Change-Id: If2040f4894e6360d1ebd5daf7e698508e5e6e42e
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- tests/manual/rhi/shared/buildshaders.bat
+++ tests/manual/rhi/shared/buildshaders.bat
@@ -2,4 +2,4 @@ qsb --glsl "100 es,120" --hlsl 50 --msl 12 -c color.vert -o color.vert.qsb
 qsb --glsl "100 es,120" --hlsl 50 --msl 12 -c color.frag -o color.frag.qsb
 qsb --glsl "100 es,120" --hlsl 50 --msl 12 -c texture.vert -o texture.vert.qsb
 qsb --glsl "100 es,120" --hlsl 50 --msl 12 -c texture.frag -o texture.frag.qsb
-qsb --hlsl 50 --msl 12 -c texture_ms4.frag -o texture_ms4.frag.qsb
+qsb --glsl "310 es,150" --hlsl 50 --msl 12 -c texture_ms4.frag -o texture_ms4.frag.qsb
