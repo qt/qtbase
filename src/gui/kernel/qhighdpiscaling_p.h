qt/qtbase

commit 37d5aaa4b42f9c837f0d27edb9da2185971d02be

Change QWindow/QWidget::map(To/From)(Global/Parent) to operate in float
Change the functions to operate in float and add the
QPoint versions as overload calling them. This is
more in-line with the event accessors using float
and allows for removing some workarounds using a delta when
converting touch points.

Leave QPlatformWindow::map(To/From)Global() as is
for now and add helpers for float.

Change-Id: I2d46b8dbda8adff26539e358074b55073dc80b6f
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

src/gui/kernel/qhighdpiscaling_p.h
@@ -112,8 +112,8 @@ public:
 
     static QPoint mapPositionFromNative(const QPoint &pos, const QPlatformScreen *platformScreen);
     static QPoint mapPositionToNative(const QPoint &pos, const QPlatformScreen *platformScreen);
-    static QPoint mapPositionToGlobal(const QPoint &pos, const QPoint &windowGlobalPosition, const QWindow *window);
-    static QPoint mapPositionFromGlobal(const QPoint &pos, const QPoint &windowGlobalPosition, const QWindow *window);
+    static QPointF mapPositionToGlobal(const QPointF &pos, const QPoint &windowGlobalPosition, const QWindow *window);
+    static QPointF mapPositionFromGlobal(const QPointF &pos, const QPoint &windowGlobalPosition, const QWindow *window);
     static QDpi logicalDpi(const QScreen *screen);
 
 private:

src/gui/kernel/qguiapplication.cpp
@@ -2191,8 +2191,7 @@ void QGuiApplicationPrivate::processMouseEvent(QWindowSystemInterfacePrivate::Mo
                 window = currentMousePressWindow;
                 currentMousePressWindow = nullptr;
             }
-            QPointF delta = globalPoint - globalPoint.toPoint();
-            localPoint = window->mapFromGlobal(globalPoint.toPoint()) + delta;
+            localPoint = window->mapFromGlobal(globalPoint);
         }
     }
 
@@ -2285,10 +2284,8 @@ void QGuiApplicationPrivate::processWheelEvent(QWindowSystemInterfacePrivate::Wh
 
     if (e->nullWindow()) {
         window = QGuiApplication::topLevelAt(globalPoint.toPoint());
-        if (window) {
-            QPointF delta = globalPoint - globalPoint.toPoint();
-            localPoint = window->mapFromGlobal(globalPoint.toPoint()) + delta;
-        }
+        if (window)
+            localPoint = window->mapFromGlobal(globalPoint);
     }
 
     if (!window)
@@ -2962,12 +2959,7 @@ void QGuiApplicationPrivate::processTouchEvent(QWindowSystemInterfacePrivate::To
 
         for (QEventPoint &pt : touchEvent.touchPoints()) {
             auto &touchPoint = QMutableEventPoint::from(pt);
-
-            // preserve the sub-pixel resolution
-            const QPointF screenPos = touchPoint.globalPosition();
-            const QPointF delta = screenPos - screenPos.toPoint();
-
-            touchPoint.setPosition(w->mapFromGlobal(screenPos.toPoint()) + delta);
+            touchPoint.setPosition(w->mapFromGlobal(touchPoint.globalPosition()));
         }
 
         QGuiApplication::sendSpontaneousEvent(w, &touchEvent);

src/gui/kernel/qhighdpiscaling.cpp
@@ -599,9 +599,9 @@ QPoint QHighDpiScaling::mapPositionFromNative(const QPoint &pos, const QPlatform
     return (pos - topLeft) / scaleFactor + topLeft;
 }
 
-QPoint QHighDpiScaling::mapPositionToGlobal(const QPoint &pos, const QPoint &windowGlobalPosition, const QWindow *window)
+QPointF QHighDpiScaling::mapPositionToGlobal(const QPointF &pos, const QPoint &windowGlobalPosition, const QWindow *window)
 {
-    QPoint globalPosCandidate = pos + windowGlobalPosition;
+    QPointF globalPosCandidate = pos + QPointF(windowGlobalPosition);
     if (QGuiApplicationPrivate::screen_list.size() <= 1)
         return globalPosCandidate;
 
@@ -609,18 +609,18 @@ QPoint QHighDpiScaling::mapPositionToGlobal(const QPoint &pos, const QPoint &win
     // in cases where a window spans screens. Detect this case and map via
     // native coordinates to the correct screen.
     auto currentScreen = window->screen();
-    if (currentScreen && !currentScreen->geometry().contains(globalPosCandidate)) {
-        auto nativeGlobalPos = QHighDpi::toNativePixels(globalPosCandidate, currentScreen);
-        if (auto actualPlatformScreen = currentScreen->handle()->screenForPosition(nativeGlobalPos))
+    if (currentScreen && !currentScreen->geometry().contains(globalPosCandidate.toPoint())) {
+        QPointF nativeGlobalPos = QHighDpi::toNativePixels(globalPosCandidate, currentScreen);
+        if (auto actualPlatformScreen = currentScreen->handle()->screenForPosition(nativeGlobalPos.toPoint()))
             return QHighDpi::fromNativePixels(nativeGlobalPos, actualPlatformScreen->screen());
     }
 
     return globalPosCandidate;
 }
 
-QPoint QHighDpiScaling::mapPositionFromGlobal(const QPoint &pos, const QPoint &windowGlobalPosition, const QWindow *window)
+QPointF QHighDpiScaling::mapPositionFromGlobal(const QPointF &pos, const QPoint &windowGlobalPosition, const QWindow *window)
 {
-    QPoint windowPosCandidate = pos - windowGlobalPosition;
+    QPointF windowPosCandidate = pos - QPointF(windowGlobalPosition);
     if (QGuiApplicationPrivate::screen_list.size() <= 1 || window->handle() == nullptr)
         return windowPosCandidate;
 
@@ -629,10 +629,10 @@ QPoint QHighDpiScaling::mapPositionFromGlobal(const QPoint &pos, const QPoint &w
     // position-to-be-mapped may not work in cases where a window spans multiple screens.
     // Map both positions to native global space (using the correct screens), subtract there,
     // and then map the difference back using the scale factor for the window.
-    QScreen *posScreen = QGuiApplication::screenAt(pos);
+    QScreen *posScreen = QGuiApplication::screenAt(pos.toPoint());
     if (posScreen && posScreen != window->screen()) {
-        QPoint nativePos = QHighDpi::toNativePixels(pos, posScreen);
-        QPoint windowNativePos = window->handle()->geometry().topLeft();
+        QPointF nativePos = QHighDpi::toNativePixels(pos, posScreen);
+        QPointF windowNativePos = window->handle()->geometry().topLeft();
         return QHighDpi::fromNativeLocalPosition(nativePos - windowNativePos, window);
     }
 

src/gui/kernel/qplatformwindow.cpp
@@ -266,6 +266,20 @@ QPoint QPlatformWindow::mapToGlobal(const QPoint &pos) const
     return result;
 }
 
+QPointF QPlatformWindow::mapToGlobalF(const QPointF &pos) const
+{
+    const QPoint posPt = pos.toPoint();
+    const QPointF delta = pos - posPt;
+    return mapToGlobal(posPt) + delta;
+}
+
+QPointF QPlatformWindow::mapFromGlobalF(const QPointF &pos) const
+{
+    const QPoint posPt = pos.toPoint();
+    const QPointF delta = pos - posPt;
+    return mapFromGlobal(posPt) + delta;
+}
+
 /*!
     Translates the global screen coordinate \a pos to window
     coordinates using native methods. This is required for embedded windows,

src/gui/kernel/qplatformwindow.h
@@ -113,7 +113,9 @@ public:
     virtual bool isEmbedded() const;
     virtual bool isForeignWindow() const { return false; }
     virtual QPoint mapToGlobal(const QPoint &pos) const;
+    QPointF mapToGlobalF(const QPointF &pos) const;
     virtual QPoint mapFromGlobal(const QPoint &pos) const;
+    QPointF mapFromGlobalF(const QPointF &pos) const;
 
     virtual void propagateSizeHints();
 

src/gui/kernel/qsimpledrag.cpp
@@ -180,7 +180,7 @@ bool QBasicDrag::eventFilter(QObject *o, QEvent *e)
             qCDebug(lcDnd) << "mouse released over" << releaseWindow << "after drag from" << m_sourceWindow << "globalPos" << release->globalPosition().toPoint();
             if (!releaseWindow)
                 releaseWindow = m_sourceWindow;
-            QPoint releaseWindowPos = (releaseWindow ? releaseWindow->mapFromGlobal(release->globalPosition().toPoint()) : release->globalPosition().toPoint());
+            QPointF releaseWindowPos = (releaseWindow ? releaseWindow->mapFromGlobal(release->globalPosition()) : release->globalPosition());
             QMouseEvent *newRelease = new QMouseEvent(release->type(),
                 releaseWindowPos, releaseWindowPos, release->globalPosition(),
                 release->button(), release->buttons(),

src/gui/kernel/qwindow.cpp
@@ -2631,51 +2631,68 @@ bool QWindow::nativeEvent(const QByteArray &eventType, void *message, long *resu
 }
 
 /*!
-    \fn QPoint QWindow::mapToGlobal(const QPoint &pos) const
+    \fn QPointF QWindow::mapToGlobal(const QPointF &pos) const
 
     Translates the window coordinate \a pos to global screen
-    coordinates. For example, \c{mapToGlobal(QPoint(0,0))} would give
+    coordinates. For example, \c{mapToGlobal(QPointF(0,0))} would give
     the global coordinates of the top-left pixel of the window.
 
     \sa mapFromGlobal()
+    \since 6.0
 */
-QPoint QWindow::mapToGlobal(const QPoint &pos) const
+QPointF QWindow::mapToGlobal(const QPointF &pos) const
 {
     Q_D(const QWindow);
     // QTBUG-43252, prefer platform implementation for foreign windows.
     if (d->platformWindow
         && (d->platformWindow->isForeignWindow() || d->platformWindow->isEmbedded())) {
-        return QHighDpi::fromNativeLocalPosition(d->platformWindow->mapToGlobal(QHighDpi::toNativeLocalPosition(pos, this)), this);
+        return QHighDpi::fromNativeLocalPosition(d->platformWindow->mapToGlobalF(QHighDpi::toNativeLocalPosition(pos, this)), this);
     }
 
     if (QHighDpiScaling::isActive())
         return QHighDpiScaling::mapPositionToGlobal(pos, d->globalPosition(), this);
 
-    return pos + d->globalPosition();
+    return pos + QPointF(d->globalPosition());
 }
 
+/*!
+    \overload
+*/
+QPoint QWindow::mapToGlobal(const QPoint &pos) const
+{
+    return mapToGlobal(QPointF(pos)).toPoint();
+}
 
 /*!
-    \fn QPoint QWindow::mapFromGlobal(const QPoint &pos) const
+    \fn QPointF QWindow::mapFromGlobal(const QPointF &pos) const
 
     Translates the global screen coordinate \a pos to window
     coordinates.
 
     \sa mapToGlobal()
+    \since 6.0
 */
-QPoint QWindow::mapFromGlobal(const QPoint &pos) const
+QPointF QWindow::mapFromGlobal(const QPointF &pos) const
 {
     Q_D(const QWindow);
     // QTBUG-43252, prefer platform implementation for foreign windows.
     if (d->platformWindow
         && (d->platformWindow->isForeignWindow() || d->platformWindow->isEmbedded())) {
-        return QHighDpi::fromNativeLocalPosition(d->platformWindow->mapFromGlobal(QHighDpi::toNativeLocalPosition(pos, this)), this);
+        return QHighDpi::fromNativeLocalPosition(d->platformWindow->mapFromGlobalF(QHighDpi::toNativeLocalPosition(pos, this)), this);
     }
 
     if (QHighDpiScaling::isActive())
         return QHighDpiScaling::mapPositionFromGlobal(pos, d->globalPosition(), this);
 
-    return pos - d->globalPosition();
+    return pos - QPointF(d->globalPosition());
+}
+
+/*!
+    \overload
+*/
+QPoint QWindow::mapFromGlobal(const QPoint &pos) const
+{
+    return QWindow::mapFromGlobal(QPointF(pos)).toPoint();
 }
 
 QPoint QWindowPrivate::globalPosition() const

src/gui/kernel/qwindow.h
@@ -260,6 +260,8 @@ public:
     virtual QAccessibleInterface *accessibleRoot() const;
     virtual QObject *focusObject() const;
 
+    QPointF mapToGlobal(const QPointF &pos) const;
+    QPointF mapFromGlobal(const QPointF &pos) const;
     QPoint mapToGlobal(const QPoint &pos) const;
     QPoint mapFromGlobal(const QPoint &pos) const;
 

commit 21de5aa1ac0255ca0aeaf89db09c2f2beaf33694

Fix build without features.highdpiscaling
Broken since 37d5aaa4b42f9c837f0d27edb9da2185971d02be

Change-Id: Id741f23ccae4f619e6a389ee71b3e7fe0c599989
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

src/gui/kernel/qhighdpiscaling_p.h
@@ -320,8 +320,8 @@ public:
     static inline QPoint origin(const QPlatformScreen *) { return QPoint(); }
     static inline QPoint mapPositionFromNative(const QPoint &pos, const QPlatformScreen *) { return pos; }
     static inline QPoint mapPositionToNative(const QPoint &pos, const QPlatformScreen *) { return pos; }
-    static inline QPoint mapPositionToGlobal(const QPoint &pos, const QPoint &windowGlobalPosition, const QWindow *window) { return pos; }
-    static inline QPoint mapPositionFromGlobal(const QPoint &pos, const QPoint &windowGlobalPosition, const QWindow *window) { return pos; }
+    static inline QPointF mapPositionToGlobal(const QPointF &pos, const QPoint &windowGlobalPosition, const QWindow *window) { return pos; }
+    static inline QPointF mapPositionFromGlobal(const QPointF &pos, const QPoint &windowGlobalPosition, const QWindow *window) { return pos; }
     static inline QDpi logicalDpi(const QScreen *screen) { return QDpi(-1,-1); }
 };
 