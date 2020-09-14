d649d5863c95f7512405965a18b54b2e445b1ece
André Klitzing <aklitzing@gmail.com>
2020-09-14 13:46:56 +0200

    Fix -Wunguarded-availability-new for userInterfaceStyle
    'userInterfaceStyle' is only available on iOS 12.0 or newer [-Werror,-Wunguarded-availability-new]
           if (previousTraitCollection.userInterfaceStyle != self.traitCollection.userInterfaceStyle) {
    
    Change-Id: Id6340f70019d55ab2a0707b1aebd6d333e9544cb
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit b79b75adf93afa983217f5c7fdd40044909a0ad2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/ios/qiosscreen.mm
+++ src/plugins/platforms/ios/qiosscreen.mm
@@ -212,10 +212,12 @@ static QIOSScreen* qtPlatformScreenFor(UIScreen *uiScreen)
 {
     [super traitCollectionDidChange:previousTraitCollection];
 
-    if (self.screen == UIScreen.mainScreen) {
-        if (previousTraitCollection.userInterfaceStyle != self.traitCollection.userInterfaceStyle) {
-            QIOSTheme::initializeSystemPalette();
-            QWindowSystemInterface::handleThemeChange<QWindowSystemInterface::SynchronousDelivery>(nullptr);
+    if (@available(iOS 12, *)) {
+        if (self.screen == UIScreen.mainScreen) {
+            if (previousTraitCollection.userInterfaceStyle != self.traitCollection.userInterfaceStyle) {
+                QIOSTheme::initializeSystemPalette();
+                QWindowSystemInterface::handleThemeChange<QWindowSystemInterface::SynchronousDelivery>(nullptr);
+            }
         }
     }
 }
