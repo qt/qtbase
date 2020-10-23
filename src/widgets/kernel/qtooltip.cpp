qt/qtbase

commit 7e2fded55e67727043c3dd0a1a5b3883655101c4

Remove usage of QDesktopWidget(Private) from most places in QtWidgets
Call QGuiApplication and QScreen APIs directly to get geometries, and
make use of QScreen::grabWindow grabbing the screen it's called on when
called with WId == 0.

This assumes that QGuiApplication::screen and QWidget::screen never
return nullptr, which is already assumed in other places.

In QSplashScreen, simplify the code to operate on the screen of the
QSplashScreen itself. Remove the case that handles a QDesktopWidget
parent - QSplashScreen doesn't have a constructor that takes a
QWidget* parent anymore.

In the QEffect implementation, we can rely on the widget pointer
not being nullptr (it's tested in the free functions client code
uses).

Includes a few drive-by changes to coding style and logic in
qtooltip.cpp, where the tip label placement now prefers the screen
of the widget the label is created for, and uses the position only
as a fallback.

What remains is the special handling of QDesktopWidget and the
Qt::Desktop type in QWidget and QApplication.

Change-Id: I30b67bab8ae82ddfcc7bbbec3c10f6e935b74f06
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

src/widgets/kernel/qtooltip.cpp
@@ -40,7 +40,6 @@
 #include <QtWidgets/private/qtwidgetsglobal_p.h>
 
 #include <qapplication.h>
-#include <private/qdesktopwidget_p.h>
 #include <qevent.h>
 #include <qpointer.h>
 #include <qstyle.h>
@@ -167,11 +166,11 @@ private:
 QTipLabel *QTipLabel::instance = nullptr;
 
 QTipLabel::QTipLabel(const QString &text, const QPoint &pos, QWidget *w, int msecDisplayTime)
+    : QLabel(w, Qt::ToolTip | Qt::BypassGraphicsProxyWidget)
 #ifndef QT_NO_STYLE_STYLESHEET
-    : QLabel(w, Qt::ToolTip | Qt::BypassGraphicsProxyWidget), styleSheetParent(nullptr), widget(nullptr)
-#else
-    : QLabel(w, Qt::ToolTip | Qt::BypassGraphicsProxyWidget), widget(0)
+    , styleSheetParent(nullptr)
 #endif
+    , widget(nullptr)
 {
     delete instance;
     instance = this;
@@ -225,15 +224,10 @@ void  QTipLabel::updateSize(const QPoint &pos)
         ++extra.rheight();
     setWordWrap(Qt::mightBeRichText(text()));
     QSize sh = sizeHint();
-    QScreen *screen = windowHandle() ? windowHandle()->screen() : QGuiApplication::screenAt(pos);
-    if (!screen)
-        screen = QGuiApplication::primaryScreen();
-    if (screen) {
-        const qreal screenWidth = screen->geometry().width();
-        if (!wordWrap() && sh.width() > screenWidth) {
-            setWordWrap(true);
-            sh = sizeHint();
-        }
+    const QScreen *screen = getTipScreen(pos, this);
+    if (!wordWrap() && sh.width() > screen->geometry().width()) {
+        setWordWrap(true);
+        sh = sizeHint();
     }
     resize(sh + extra);
 }
@@ -367,10 +361,7 @@ bool QTipLabel::eventFilter(QObject *o, QEvent *e)
 
 QScreen *QTipLabel::getTipScreen(const QPoint &pos, QWidget *w)
 {
-    int screenNo = QGuiApplication::primaryScreen()->virtualSiblings().size() > 1
-                 ? QDesktopWidgetPrivate::screenNumber(pos)
-                 : QDesktopWidgetPrivate::screenNumber(w);
-    return QDesktopWidgetPrivate::screen(screenNo);
+    return w ? w->screen() : QGuiApplication::primaryScreen()->virtualSiblingAt(pos);
 }
 
 void QTipLabel::placeTip(const QPoint &pos, QWidget *w)
@@ -463,12 +454,11 @@ bool QTipLabel::tipChanged(const QPoint &pos, const QString &text, QObject *o)
 
 void QToolTip::showText(const QPoint &pos, const QString &text, QWidget *w, const QRect &rect, int msecDisplayTime)
 {
-    if (QTipLabel::instance && QTipLabel::instance->isVisible()){ // a tip does already exist
+    if (QTipLabel::instance && QTipLabel::instance->isVisible()) { // a tip does already exist
         if (text.isEmpty()){ // empty text means hide current tip
             QTipLabel::instance->hideTip();
             return;
-        }
-        else if (!QTipLabel::instance->fadingOut){
+        } else if (!QTipLabel::instance->fadingOut) {
             // If the tip has changed, reuse the one
             // that is showing (removes flickering)
             QPoint localPos = pos;
@@ -483,23 +473,22 @@ void QToolTip::showText(const QPoint &pos, const QString &text, QWidget *w, cons
         }
     }
 
-    if (!text.isEmpty()){ // no tip can be reused, create new tip:
-        QWidget *tipLabelParent = [pos, w]() -> QWidget* {
+    if (!text.isEmpty()) { // no tip can be reused, create new tip:
+        QWidget *tipLabelParent = [w]() -> QWidget* {
 #ifdef Q_OS_WIN32
             // On windows, we can't use the widget as parent otherwise the window will be
             // raised when the tooltip will be shown
-            QScreen *screen = QTipLabel::getTipScreen(pos, w);
-            return QApplication::desktop(screen);
+            Q_UNUSED(w);
+            return nullptr;
 #else
-            Q_UNUSED(pos);
             return w;
 #endif
         }();
         new QTipLabel(text, pos, tipLabelParent, msecDisplayTime); // sets QTipLabel::instance to itself
+        QWidgetPrivate::get(QTipLabel::instance)->setScreen(QTipLabel::getTipScreen(pos, w));
         QTipLabel::instance->setTipRect(w, rect);
         QTipLabel::instance->placeTip(pos, w);
         QTipLabel::instance->setObjectName(QLatin1String("qtooltip_label"));
-
 
 #if QT_CONFIG(effects)
         if (QApplication::isEffectEnabled(Qt::UI_FadeTooltip))

src/widgets/dialogs/qcolordialog.cpp
@@ -40,7 +40,6 @@
 #include "qcolordialog.h"
 
 #include "qapplication.h"
-#include <private/qdesktopwidget_p.h>
 #include "qdrawutil.h"
 #include "qevent.h"
 #include "qimage.h"
@@ -1564,9 +1563,11 @@ bool QColorDialogPrivate::selectColor(const QColor &col)
 
 QColor QColorDialogPrivate::grabScreenColor(const QPoint &p)
 {
-    const QWidget *desktop = QApplication::desktop();
-    const QPixmap pixmap = QGuiApplication::primaryScreen()->grabWindow(desktop->winId(), p.x(), p.y(), 1, 1);
-    QImage i = pixmap.toImage();
+    QScreen *screen = QGuiApplication::screenAt(p);
+    if (!screen)
+        screen = QGuiApplication::primaryScreen();
+    const QPixmap pixmap = screen->grabWindow(0, p.x(), p.y(), 1, 1);
+    const QImage i = pixmap.toImage();
     return i.pixel(0, 0);
 }
 
@@ -1758,7 +1759,7 @@ void QColorDialogPrivate::initWidgets()
     } else {
         // better color picker size for small displays
 #if defined(QT_SMALL_COLORDIALOG)
-        QSize screenSize = QDesktopWidgetPrivate::availableGeometry(QCursor::pos()).size();
+        QSize screenSize = QGuiApplication::screenAt(QCursor::pos())->availableGeometry().size();
         pWidth = pHeight = qMin(screenSize.width(), screenSize.height());
         pHeight -= 20;
         if(screenSize.height() > screenSize.width())

src/widgets/dialogs/qdialog.cpp
@@ -49,7 +49,6 @@
 #endif
 
 #include "qevent.h"
-#include <private/qdesktopwidget_p.h>
 #include "qapplication.h"
 #include "qlayout.h"
 #if QT_CONFIG(sizegrip)
@@ -872,23 +871,23 @@ void QDialog::showEvent(QShowEvent *event)
 /*! \internal */
 void QDialog::adjustPosition(QWidget* w)
 {
-
     if (const QPlatformTheme *theme = QGuiApplicationPrivate::platformTheme())
         if (theme->themeHint(QPlatformTheme::WindowAutoPlacement).toBool())
             return;
     QPoint p(0, 0);
-    int extraw = 0, extrah = 0, scrn = 0;
+    int extraw = 0, extrah = 0;
     if (w)
         w = w->window();
     QRect desk;
-    if (w) {
-        scrn = QDesktopWidgetPrivate::screenNumber(w);
-    } else if (QGuiApplication::primaryScreen()->virtualSiblings().size() > 1) {
-        scrn = QDesktopWidgetPrivate::screenNumber(QCursor::pos());
-    } else {
-        scrn = QDesktopWidgetPrivate::screenNumber(this);
-    }
-    desk = QDesktopWidgetPrivate::availableGeometry(scrn);
+    QScreen *scrn = nullptr;
+    if (w)
+        scrn = w->screen();
+    else if (QGuiApplication::primaryScreen()->virtualSiblings().size() > 1)
+        scrn = QGuiApplication::screenAt(QCursor::pos());
+    else
+        scrn = screen();
+    if (scrn)
+        desk = scrn->availableGeometry();
 
     QWidgetList list = QApplication::topLevelWidgets();
     for (int i = 0; (extraw == 0 || extrah == 0) && i < list.size(); ++i) {
@@ -942,9 +941,9 @@ void QDialog::adjustPosition(QWidget* w)
     // QTBUG-52735: Manually set the correct target screen since scaling in a
     // subsequent call to QWindow::resize() may otherwise use the wrong factor
     // if the screen changed notification is still in an event queue.
-    if (scrn >= 0) {
+    if (scrn) {
         if (QWindow *window = windowHandle())
-            window->setScreen(QGuiApplication::screens().at(scrn));
+            window->setScreen(scrn);
     }
 
     move(p);

src/widgets/dialogs/qmessagebox.cpp
@@ -64,7 +64,6 @@
 #include <QtGui/qfontmetrics.h>
 #include <QtGui/qclipboard.h>
 #include "private/qabstractbutton_p.h"
-#include <private/qdesktopwidget_p.h>
 
 #ifdef Q_OS_WIN
 #    include <QtCore/qt_windows.h>
@@ -363,7 +362,7 @@ void QMessageBoxPrivate::updateSize()
     if (!q->isVisible())
         return;
 
-    QSize screenSize = QDesktopWidgetPrivate::availableGeometry(QCursor::pos()).size();
+    const QSize screenSize = QGuiApplication::screenAt(QCursor::pos())->availableGeometry().size();
     int hardLimit = qMin(screenSize.width() - 480, 1000); // can never get bigger than this
     // on small screens allows the messagebox be the same size as the screen
     if (screenSize.width() <= 1024)

src/widgets/dialogs/qwizard.cpp
@@ -47,7 +47,6 @@
 #include "qapplication.h"
 #include "qboxlayout.h"
 #include "qlayoutitem.h"
-#include <private/qdesktopwidget_p.h>
 #include "qevent.h"
 #include "qframe.h"
 #include "qlabel.h"

src/widgets/kernel/qwhatsthis.cpp
@@ -41,7 +41,7 @@
 #include "qpointer.h"
 #include "qapplication.h"
 #include <private/qguiapplication_p.h>
-#include <private/qdesktopwidget_p.h>
+#include "qwidget.h"
 #include "qevent.h"
 #include "qpixmap.h"
 #include "qscreen.h"
@@ -235,8 +235,7 @@ QWhatsThat::~QWhatsThat()
 
 void QWhatsThat::showEvent(QShowEvent *)
 {
-    background = QGuiApplication::primaryScreen()->grabWindow(QApplication::desktop()->internalWinId(),
-                                                              x(), y(), width(), height());
+    background = QGuiApplication::primaryScreen()->grabWindow(0, x(), y(), width(), height());
 }
 
 void QWhatsThat::mousePressEvent(QMouseEvent* e)
@@ -579,16 +578,16 @@ void QWhatsThisPrivate::say(QWidget * widget, const QString &text, int x, int y)
     QWhatsThat *whatsThat = new QWhatsThat(text, nullptr, widget);
 
     // okay, now to find a suitable location
-    int scr = (widget ?
-                QDesktopWidgetPrivate::screenNumber(widget) :
-                QDesktopWidgetPrivate::screenNumber(QPoint(x,y))
-               );
-    QRect screen = QDesktopWidgetPrivate::screenGeometry(scr);
+    QScreen *screen = widget ? widget->screen()
+                             : QGuiApplication::screenAt(QPoint(x, y));
+    if (!screen)
+        screen = QGuiApplication::primaryScreen();
+    QRect screenRect = screen->geometry();
 
     int w = whatsThat->width();
     int h = whatsThat->height();
-    int sx = screen.x();
-    int sy = screen.y();
+    int sx = screenRect.x();
+    int sy = screenRect.y();
 
     // first try locating the widget immediately above/below,
     // with nice alignment if possible.
@@ -601,13 +600,13 @@ void QWhatsThisPrivate::say(QWidget * widget, const QString &text, int x, int y)
     else
         x = x - w/2;
 
-        // squeeze it in if that would result in part of what's this
-        // being only partially visible
-    if (x + w  + shadowWidth > sx+screen.width())
-        x = (widget? (qMin(screen.width(),
-                           pos.x() + widget->width())
-                     ) : screen.width())
+    // squeeze it in if that would result in part of what's this
+    // being only partially visible
+    if (x + w  + shadowWidth > sx+screenRect.width()) {
+        x = (widget ? qMin(screenRect.width(), pos.x() + widget->width())
+                    : screenRect.width())
             - w;
+    }
 
     if (x < sx)
         x = sx;
@@ -615,18 +614,18 @@ void QWhatsThisPrivate::say(QWidget * widget, const QString &text, int x, int y)
     if (widget && h > widget->height() + 16) {
         y = pos.y() + widget->height() + 2; // below, two pixels spacing
         // what's this is above or below, wherever there's most space
-        if (y + h + 10 > sy+screen.height())
+        if (y + h + 10 > sy + screenRect.height())
             y = pos.y() + 2 - shadowWidth - h; // above, overlap
     }
     y = y + 2;
 
-        // squeeze it in if that would result in part of what's this
-        // being only partially visible
-    if (y + h + shadowWidth > sy+screen.height())
-        y = (widget ? (qMin(screen.height(),
-                             pos.y() + widget->height())
-                       ) : screen.height())
+    // squeeze it in if that would result in part of what's this
+    // being only partially visible
+    if (y + h + shadowWidth > sy + screenRect.height()) {
+        y = (widget ? qMin(screenRect.height(), pos.y() + widget->height())
+                    : screenRect.height())
             - h;
+    }
     if (y < sy)
         y = sy;
 

src/widgets/util/qcompleter.cpp
@@ -158,7 +158,7 @@
 #include "QtWidgets/qapplication.h"
 #include "QtGui/qevent.h"
 #include <private/qapplication_p.h>
-#include <private/qdesktopwidget_p.h>
+#include <private/qwidget_p.h>
 #if QT_CONFIG(lineedit)
 #include "QtWidgets/qlineedit.h"
 #endif

src/widgets/util/qscroller.cpp
@@ -57,7 +57,6 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #endif
-#include <private/qdesktopwidget_p.h>
 #include <QVector2D>
 #include <QtCore/qmath.h>
 #include <QtGui/qevent.h>

commit 074f71cfdabd49ae23ac956a9459c8e13f8e627e

Display tooltips on the correct screen
Display QTipLabel on the correct screen, also when the
parent widget spans multiple screens. In this case,
QWidget::screen() will return the main screen for the
widget, which will not necessarily be the screen at
the tool tip position.

Fixes regression introduced by 7e2fded5.

Change-Id: Iffb920b6658468c5032928eadbd749f58fe077a7
Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

src/widgets/kernel/qtooltip.cpp
@@ -361,7 +361,9 @@ bool QTipLabel::eventFilter(QObject *o, QEvent *e)
 
 QScreen *QTipLabel::getTipScreen(const QPoint &pos, QWidget *w)
 {
-    return w ? w->screen() : QGuiApplication::primaryScreen()->virtualSiblingAt(pos);
+    QScreen *guess = w ? w->screen() : QGuiApplication::primaryScreen();
+    QScreen *exact = guess->virtualSiblingAt(pos);
+    return exact ? exact : guess;
 }
 
 void QTipLabel::placeTip(const QPoint &pos, QWidget *w)