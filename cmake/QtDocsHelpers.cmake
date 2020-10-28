qt/qtbase

commit 748b3b9c8971e359ce785c92d52c1de98feaef21

CMake: Fix html_docs targets for top-level builds
Do not append "/qtbase" to QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX
when doing a top-level build. The "/qtbase" suffix is already part of
this variable's content.

Fixes: QTBUG-87682
Change-Id: If1c2075dc58d4b07a4c3a1eed12cc3336c5dc8e6
Reviewed-by: Paul Wicking <paul.wicking@qt.io>
Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

cmake/QtDocsHelpers.cmake
@@ -16,7 +16,7 @@ function(qt_internal_add_docs)
         return()
     endif()
 
-    if (NOT QT_SUPERBUILD OR QT_WILL_INSTALL)
+    if (QT_SUPERBUILD OR QT_WILL_INSTALL)
         set(doc_tools_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/${INSTALL_BINDIR}")
     else()
         set(doc_tools_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/qtbase/${INSTALL_BINDIR}")
@@ -55,9 +55,6 @@ function(qt_internal_add_docs)
     if (QT_WILL_INSTALL)
         set(qdoc_output_dir "${CMAKE_BINARY_DIR}/${INSTALL_DOCDIR}/${doc_target}")
         set(index_dir "${CMAKE_BINARY_DIR}/${INSTALL_DOCDIR}")
-    elseif (QT_SUPERBUILD)
-        set(qdoc_output_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/qtbase/${INSTALL_DOCDIR}/${doc_target}")
-        set(index_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/qtbase/${INSTALL_DOCDIR}")
     else()
         set(qdoc_output_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/${INSTALL_DOCDIR}/${doc_target}")
         set(index_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/${INSTALL_DOCDIR}")
@@ -85,8 +82,6 @@ function(qt_internal_add_docs)
 
     if (QT_WILL_INSTALL)
         set(qt_install_docs_env "${CMAKE_INSTALL_PREFIX}/${INSTALL_DOCDIR}")
-    elseif (QT_SUPERBUILD)
-        set(qt_install_docs_env "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/qtbase/${INSTALL_DOCDIR}")
     else()
         set(qt_install_docs_env "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/${INSTALL_DOCDIR}")
     endif()

commit 1f6543902d18a05dbb87dd361a169a7cdb3da731

Fix qdoc builds for non-top-level developer builds
Fixes a regression introduced in commit 748b3b9c8971e35 for builds
that are both not a QT_SUPERBUILD and not QT_WILL_INSTALL.

Change-Id: I52b920176a2696fa6206b89b2b69ae1a7f3fbc25
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

cmake/QtDocsHelpers.cmake
@@ -33,10 +33,8 @@ function(qt_internal_add_docs)
 
     if(QT_SUPERBUILD)
         set(doc_tools_dir "${QtBase_BINARY_DIR}/${INSTALL_BINDIR}")
-    elseif(QT_WILL_INSTALL)
-        set(doc_tools_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/${INSTALL_BINDIR}")
     else()
-        set(doc_tools_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/qtbase/${INSTALL_BINDIR}")
+        set(doc_tools_dir "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}/${INSTALL_BINDIR}")
     endif()
 
     set(qdoc_bin "${doc_tools_dir}/qdoc${CMAKE_EXECUTABLE_SUFFIX}")