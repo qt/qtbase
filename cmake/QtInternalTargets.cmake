f9e45287d820b8af2d40050288034c7ba8262d26
Yuhang Zhao <2546789017@qq.com>
2020-11-03 11:47:25 +0800

    CMake: don't add the same parameter twice
    Change-Id: Ic5001f296b56f1181f5e37718e090f931eca8b50
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- cmake/QtInternalTargets.cmake
+++ cmake/QtInternalTargets.cmake
@@ -122,13 +122,6 @@ endif()
 
 target_compile_definitions(PlatformCommonInternal INTERFACE $<$<NOT:$<CONFIG:Debug>>:QT_NO_DEBUG>)
 
-if(MSVC)
-    # To mimic mkspecs/common/msvc-desktop.conf add optimization flag for non-debug configs.
-    # In qmake, the flag is added to any user project built with qmake. In CMake land, to be on the
-    # safe side, only add when building Qt itself.
-    target_link_options(PlatformCommonInternal INTERFACE "$<$<NOT:$<CONFIG:Debug>>:/OPT:REF>")
-endif()
-
 function(qt_internal_apply_bitcode_flags target)
     # See mkspecs/features/uikit/bitcode.prf
     set(release_flags "-fembed-bitcode")
@@ -201,8 +194,7 @@ if (MSVC)
 
     target_link_options(PlatformCommonInternal INTERFACE
         -DYNAMICBASE -NXCOMPAT
-        $<$<CONFIG:Release>:-OPT:REF>
-        $<$<CONFIG:RelWithDebInfo>:-OPT:REF>
+        $<$<NOT:$<CONFIG:Debug>>:-OPT:REF>
     )
 endif()
 
