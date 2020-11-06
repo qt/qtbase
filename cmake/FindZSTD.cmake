2433e5804c82f69aeaf7d762dfad4499efadf9ef
Alexey Edelev <alexey.edelev@qt.io>
2020-11-06 12:06:23 +0100

    CMake: Locate zstd using CONFIG
    Trying to locate zstd package configuration prior to use pkg-config.
    
    Define explicit IMPORTED_LOCATION_DEBUG and IMPORTED_LOCATION_RELEASE
    in case of pkg-config usage.
    
    Fixes: QTBUG-88091
    Change-Id: I3cb86a66fad4e7b78b3d2fcff061508fc25e25a7
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- cmake/FindZSTD.cmake
+++ cmake/FindZSTD.cmake
@@ -18,32 +18,62 @@
 # ``ZSTD::ZSTD``
 #     The zstd library
 
-find_package(PkgConfig QUIET)
-pkg_check_modules(PC_ZSTD QUIET libzstd)
-
-find_path(ZSTD_INCLUDE_DIRS
-          NAMES zstd.h
-          HINTS ${PC_ZSTD_INCLUDEDIR}
-          PATH_SUFFIXES zstd)
-
-find_library(ZSTD_LIBRARIES
-             NAMES zstd zstd_static
-             HINTS ${PC_ZSTD_LIBDIR}
-)
+find_package(zstd CONFIG)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ZSTD REQUIRED_VARS ZSTD_LIBRARIES ZSTD_INCLUDE_DIRS
-                                       VERSION_VAR PC_ZSTD_VERSION)
 
-if(ZSTD_FOUND AND NOT TARGET ZSTD::ZSTD)
-  add_library(ZSTD::ZSTD UNKNOWN IMPORTED)
-  set_target_properties(ZSTD::ZSTD PROPERTIES
-                        IMPORTED_LOCATION "${ZSTD_LIBRARIES}"
-                        INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIRS}")
+if(TARGET zstd::libzstd_static OR TARGET zstd::libzstd_shared)
+    find_package_handle_standard_args(ZSTD REQUIRED_VARS zstd_DIR VERSION_VAR zstd_VERSION)
+    if(TARGET zstd::libzstd_static)
+        set(zstdtargetsuffix "_static")
+    else()
+        set(zstdtargetsuffix "_shared")
+    endif()
+    if(NOT TARGET ZSTD::ZSTD)
+        add_library(ZSTD::ZSTD INTERFACE IMPORTED)
+        set_target_properties(ZSTD::ZSTD PROPERTIES
+                              INTERFACE_LINK_LIBRARIES "zstd::libzstd${zstdtargetsuffix}")
+    endif()
+else()
+    find_package(PkgConfig QUIET)
+    pkg_check_modules(PC_ZSTD QUIET libzstd)
+
+    find_path(ZSTD_INCLUDE_DIRS
+              NAMES zstd.h
+              HINTS ${PC_ZSTD_INCLUDEDIR}
+              PATH_SUFFIXES zstd)
+
+    find_library(ZSTD_LIBRARY_RELEASE
+                 NAMES zstd zstd_static
+                 HINTS ${PC_ZSTD_LIBDIR}
+    )
+    find_library(ZSTD_LIBRARY_DEBUG
+                 NAMES zstdd zstd_staticd zstd zstd_static
+                 HINTS ${PC_ZSTD_LIBDIR}
+    )
+
+    include(SelectLibraryConfigurations)
+    select_library_configurations(ZSTD)
+
+    find_package_handle_standard_args(ZSTD REQUIRED_VARS ZSTD_LIBRARIES ZSTD_INCLUDE_DIRS
+                                           VERSION_VAR PC_ZSTD_VERSION)
+
+    if(ZSTD_FOUND AND NOT TARGET ZSTD::ZSTD)
+      add_library(ZSTD::ZSTD UNKNOWN IMPORTED)
+      set_target_properties(ZSTD::ZSTD PROPERTIES
+                            INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIRS}")
+      if(ZSTD_LIBRARY_RELEASE)
+          set_target_properties(ZSTD::ZSTD PROPERTIES
+                                IMPORTED_LOCATION_RELEASE "${ZSTD_LIBRARY_RELEASE}")
+      endif()
+      if(ZSTD_LIBRARY_DEBUG)
+          set_target_properties(ZSTD::ZSTD PROPERTIES
+                                IMPORTED_LOCATION_DEBUG "${ZSTD_LIBRARY_DEBUG}")
+      endif()
+    endif()
+
+    mark_as_advanced(ZSTD_INCLUDE_DIRS ZSTD_LIBRARIES ZSTD_LIBRARY_RELEASE ZSTD_LIBRARY_DEBUG)
 endif()
-
-mark_as_advanced(ZSTD_INCLUDE_DIRS ZSTD_LIBRARIES)
-
 include(FeatureSummary)
 set_package_properties(ZSTD PROPERTIES
   URL "https://github.com/facebook/zstd"
