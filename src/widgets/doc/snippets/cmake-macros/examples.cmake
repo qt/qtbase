b6dae38c278764339956abd1edde2b7f2a47ab1d
Kai Koehne <kai.koehne@qt.io>
2020-11-18 14:26:47 +0100

    Doc: Update CMake functions in Qt Widgets for Qt 6
    Change-Id: I4110e8695c75c40cec6f94bb94879b07282b29d3
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit c082213556991c6619ef2d2c00a7042406f867a7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/doc/snippets/cmake-macros/examples.cmake
+++ src/widgets/doc/snippets/cmake-macros/examples.cmake
@@ -1,9 +1,3 @@
-#! [qt5_wrap_ui]
-set(SOURCES mainwindow.cpp main.cpp)
-qt5_wrap_ui(SOURCES mainwindow.ui)
-add_executable(myapp ${SOURCES})
-#! [qt5_wrap_ui]
-
 #! [qt_wrap_ui]
 set(SOURCES mainwindow.cpp main.cpp)
 qt_wrap_ui(SOURCES mainwindow.ui)
