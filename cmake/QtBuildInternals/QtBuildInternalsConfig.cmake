qt/qtbase

commit 9e0fb9c4fbe7a11c09a70d7ea1be97405c6bf0f8

CMake: Fix Android top-level prefix build
During a top-level prefix build, while configuring qtsvg,
Qt6BuildInternalsConfig tries to include QtBuildInternalsAndroid.cmake
but such files are not copied by qt_copy_or_install() at configure
time.

We need to use the "module" form of include() to take CMAKE_MODULE_PATH
into account. Then QtBuildInternalsAndroid.cmake can be loaded from the
source tree.

The include's current form was introduced in commit
3cb9ee3a5b36e2c10dbaf1564852472e7e02a1ae without further
explanation. Assuming an accidental change.

Pick-to: 6.0 6.0.0
Task-number: QTBUG-88460
Change-Id: If06ac715fae3c2bebbeffca44b7e07265ffb0f71
Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

cmake/QtBuildInternals/QtBuildInternalsConfig.cmake
@@ -570,5 +570,5 @@ macro(qt_examples_build_end)
 endmacro()
 
 if (ANDROID)
-    include(${CMAKE_CURRENT_LIST_DIR}/QtBuildInternalsAndroid.cmake)
+    include(QtBuildInternals/QtBuildInternalsAndroid)
 endif()

commit 510186a218e63c88100efc38cb1611f122f94570

CMake: Fix Android non-top-level build against qtbase
Commit 9e0fb9c4fbe7a11c09a70d7ea1be97405c6bf0f8 introduced build
failures in non-top-level builds when building Qt modules against
qtbase.

This was observed on in the CI for CentOS.

Partly revert above commit by taking the old code path for non-top-level
builds.

Pick-to: dev 6.0
Fixes: QTBUG-88718
Change-Id: I8926e1e4628ec5bdcea99e10497c1f65c10d072d
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

cmake/QtBuildInternals/QtBuildInternalsConfig.cmake
@@ -570,5 +570,10 @@ macro(qt_examples_build_end)
 endmacro()
 
 if (ANDROID)
-    include(QtBuildInternals/QtBuildInternalsAndroid)
+    if(QT_SUPERBUILD)
+        include(QtBuildInternals/QtBuildInternalsAndroid)
+    else()
+        ### TODO: Find out why this is needed. See QTBUG-88718.
+        include(${CMAKE_CURRENT_LIST_DIR}/QtBuildInternalsAndroid.cmake)
+    endif()
 endif()