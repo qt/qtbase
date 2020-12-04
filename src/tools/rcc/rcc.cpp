fc9cda5f08ac848e88f63dd4a07c08b2fbc6bf17
Friedemann Kleint <Friedemann.Kleint@qt.io>
2020-12-04 11:14:27 +0100

    Fix uic/rcc generating outdated export for Qt for Python
    Bump version to 6.
    
    Pick-to: dev 6.0
    Fixes: QTBUG-89124
    Change-Id: Ifcf60552b5b6efb86f79da34da9c34b8efae9fa4
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- src/tools/rcc/rcc.cpp
+++ src/tools/rcc/rcc.cpp
@@ -1112,7 +1112,7 @@ bool RCCResourceLibrary::writeHeader()
         writeByteArray(QT_VERSION_STR);
         writeString("\n");
         writeString("# WARNING! All changes made in this file will be lost!\n\n");
-        writeString("from PySide2 import QtCore\n\n");
+        writeString("from PySide6 import QtCore\n\n");
         break;
     case Binary:
         writeString("qres");
