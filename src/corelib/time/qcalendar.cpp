dd95c814ef90dde447b1c586a7cb350e579d8e8c
Edward Welbourne <edward.welbourne@qt.io>
2020-12-10 16:19:17 +0100

    Check date validity in calendar methods taking a QDate
    Previously neglected, in dateFromParts() and dayOfWeek(), which only
    make sense for valid dates.
    
    Change-Id: I44879bb441dbf51b96c8fd4d45e8f07423e63047
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit ac1008c16f58d95ae4ff0a7864e3bcb0783cc439)
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/corelib/time/qcalendar.cpp
+++ src/corelib/time/qcalendar.cpp
@@ -1138,7 +1138,7 @@ QDate QCalendar::dateFromParts(const QCalendar::YearMonthDay &parts) const
 */
 QCalendar::YearMonthDay QCalendar::partsFromDate(QDate date) const
 {
-    return d ? d->julianDayToDate(date.toJulianDay()) : YearMonthDay();
+    return d && date.isValid() ? d->julianDayToDate(date.toJulianDay()) : YearMonthDay();
 }
 
 /*!
@@ -1152,7 +1152,7 @@ QCalendar::YearMonthDay QCalendar::partsFromDate(QDate date) const
 */
 int QCalendar::dayOfWeek(QDate date) const
 {
-    return d ? d->dayOfWeek(date.toJulianDay()) : 0;
+    return d && date.isValid() ? d->dayOfWeek(date.toJulianDay()) : 0;
 }
 
 // Locale data access
