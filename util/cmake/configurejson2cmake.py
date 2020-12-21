bbebfa0be2e51b638459d6d812e4db25a65a0d6d
Alexey Edelev <alexey.edelev@qt.io>
2020-12-21 19:29:30 +0100

    CMake: Add --enable-new-dtags flag support
    Remove enable-new-dtags from feature mapping, handle it as regular
    feature. Regenerate configure.cmake with 'enable-new-dtags' enabled.
    Check for enable_new_dtags feature, add -Wl,--enable-new-dtags if
    enabled.
    
    Fixes: QTBUG-89013
    Pick-to: 6.0
    Change-Id: Ice57c02dc85f7f9ecf14e3bc65f31a57b589ce24
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    Reviewed-by: Craig Scott <craig.scott@qt.io>

--- util/cmake/configurejson2cmake.py
+++ util/cmake/configurejson2cmake.py
@@ -900,7 +900,6 @@ def get_feature_mapping():
             "condition": "CMAKE_BUILD_TYPE STREQUAL Debug OR Debug IN_LIST CMAKE_CONFIGURATION_TYPES",
         },
         "dlopen": {"condition": "UNIX"},
-        "enable_new_dtags": None,
         "force_debug_info": {
             "autoDetect": "CMAKE_BUILD_TYPE STREQUAL RelWithDebInfo OR RelWithDebInfo IN_LIST CMAKE_CONFIGURATION_TYPES"
         },
