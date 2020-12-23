6d85e6e45ac9efe12f01ae38619487a23c585e9e
Mårten Nordheim <marten.nordheim@qt.io>
2020-12-23 17:56:41 +0100

    NetworkManager based backend for QNetworkInformation
    For Linux. Based on the code from the old bearer plugin for
    NetworkManager with changes to fit better the objective of
    QNetworkInformation.
    
    Pick-to: dev
    Task-number: QTBUG-86966
    Change-Id: I90c3488ff31ef6dfdcfb877c0e9c592c6c328a89
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

---
+++ src/plugins/networkinformationbackends/networkmanager/qnetworkmanagerservice.cpp
@@ -0,0 +1,129 @@
+/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the plugins of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qnetworkmanagerservice.h"
+
+#include <QObject>
+#include <QList>
+#include <QtDBus/QtDBus>
+#include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusError>
+#include <QtDBus/QDBusInterface>
+#include <QtDBus/QDBusMessage>
+#include <QtDBus/QDBusReply>
+#include <QtDBus/QDBusPendingCallWatcher>
+#include <QtDBus/QDBusObjectPath>
+#include <QtDBus/QDBusPendingCall>
+
+#define DBUS_PROPERTIES_INTERFACE "org.freedesktop.DBus.Properties"
+
+QT_BEGIN_NAMESPACE
+
+QNetworkManagerInterfaceBase::QNetworkManagerInterfaceBase(QObject *parent)
+    : QDBusAbstractInterface(QLatin1String(NM_DBUS_SERVICE), QLatin1String(NM_DBUS_PATH),
+                             NM_DBUS_INTERFACE, QDBusConnection::systemBus(), parent)
+{
+}
+
+bool QNetworkManagerInterfaceBase::networkManagerAvailable()
+{
+    return QNetworkManagerInterfaceBase().isValid();
+}
+
+QNetworkManagerInterface::QNetworkManagerInterface(QObject *parent)
+    : QNetworkManagerInterfaceBase(parent)
+{
+    if (!isValid())
+        return;
+
+    PropertiesDBusInterface managerPropertiesInterface(
+            QLatin1String(NM_DBUS_SERVICE), QLatin1String(NM_DBUS_PATH), DBUS_PROPERTIES_INTERFACE,
+            QDBusConnection::systemBus());
+    QList<QVariant> argumentList;
+    argumentList << QLatin1String(NM_DBUS_INTERFACE);
+    QDBusPendingReply<QVariantMap> propsReply = managerPropertiesInterface.callWithArgumentList(
+            QDBus::Block, QLatin1String("GetAll"), argumentList);
+    if (!propsReply.isError()) {
+        propertyMap = propsReply.value();
+    } else {
+        qWarning() << "propsReply" << propsReply.error().message();
+    }
+
+    QDBusConnection::systemBus().connect(
+            QLatin1String(NM_DBUS_SERVICE), QLatin1String(NM_DBUS_PATH),
+            QLatin1String(NM_DBUS_INTERFACE), QLatin1String("PropertiesChanged"), this,
+            SLOT(setProperties(QMap<QString, QVariant>)));
+}
+
+QNetworkManagerInterface::~QNetworkManagerInterface()
+{
+    QDBusConnection::systemBus().disconnect(
+            QLatin1String(NM_DBUS_SERVICE), QLatin1String(NM_DBUS_PATH),
+            QLatin1String(NM_DBUS_INTERFACE), QLatin1String("PropertiesChanged"), this,
+            SLOT(setProperties(QMap<QString, QVariant>)));
+}
+
+QNetworkManagerInterface::NMState QNetworkManagerInterface::state()
+{
+    if (propertyMap.contains("State"))
+        return static_cast<QNetworkManagerInterface::NMState>(propertyMap.value("State").toUInt());
+    return QNetworkManagerInterface::NM_STATE_UNKNOWN;
+}
+
+void QNetworkManagerInterface::setProperties(const QMap<QString, QVariant> &map)
+{
+    for (auto i = map.cbegin(), end = map.cend(); i != end; ++i) {
+        const bool isState = i.key() == QLatin1String("State");
+        bool stateUpdate = isState;
+        auto it = propertyMap.lowerBound(i.key());
+        if (it != propertyMap.end() && it.key() == i.key()) {
+            stateUpdate &= (it.value() != i.value());
+            *it = *i;
+        } else {
+            propertyMap.insert(it, i.key(), i.value());
+        }
+
+        if (stateUpdate) {
+            quint32 state = i.value().toUInt();
+            Q_EMIT stateChanged(state);
+        }
+    }
+}
+
+QT_END_NAMESPACE
