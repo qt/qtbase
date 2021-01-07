qt/qtbase

commit d25589e0529732996e405aaff8d6c46b012e1601

QTestlib: Enable comparing QList against initializer lists/arrays
It is unnecessary to create a QList container just for comparison.

Split out helpers for comparing sequence sizes and sequences from
qCompare(QList) and add a template for an array with a non-type template
parameter for the size.

One can then write something like:

const int expected[] = {10, 12,...};
QCOMPARE(QFontDatabase.pointSizes(...), expected)

Unfortunately, any commas in such an array will be misread by macro expansion
as macro argument separators, so any expected array with more than one entry
needs an extra macro expanding __VA_ARGS__.

Change-Id: Ie7c8dc20bf669bbb25f6d7f8562455f8d03968c8
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/testlib/qtest.h
@@ -64,6 +64,7 @@
 #include <QtCore/qsize.h>
 #include <QtCore/qrect.h>
 
+#include <initializer_list>
 #include <memory>
 
 QT_BEGIN_NAMESPACE
@@ -276,27 +277,36 @@ inline bool qCompare(QLatin1String const &t1, QString const &t2, const char *act
     return qCompare(QString(t1), t2, actual, expected, file, line);
 }
 
-template <typename T>
-inline bool qCompare(QList<T> const &t1, QList<T> const &t2, const char *actual, const char *expected,
-                    const char *file, int line)
+namespace Internal {
+
+// Compare sequences of equal size
+template <typename ActualIterator, typename ExpectedIterator>
+bool compareSequence(ActualIterator actualIt, ActualIterator actualEnd,
+                     ExpectedIterator expectedBegin, ExpectedIterator expectedEnd,
+                     const char *actual, const char *expected,
+                     const char *file, int line)
 {
     char msg[1024];
     msg[0] = '\0';
-    bool isOk = true;
-    const int actualSize = t1.count();
-    const int expectedSize = t2.count();
-    if (actualSize != expectedSize) {
-        qsnprintf(msg, sizeof(msg), "Compared lists have different sizes.\n"
-                  "   Actual   (%s) size: %d\n"
-                  "   Expected (%s) size: %d", actual, actualSize, expected, expectedSize);
-        isOk = false;
-    }
-    for (int i = 0; isOk && i < actualSize; ++i) {
-        if (!(t1.at(i) == t2.at(i))) {
-            char *val1 = toString(t1.at(i));
-            char *val2 = toString(t2.at(i));
 
-            qsnprintf(msg, sizeof(msg), "Compared lists differ at index %d.\n"
+    const qsizetype actualSize = actualEnd - actualIt;
+    const qsizetype expectedSize = expectedEnd - expectedBegin;
+    bool isOk = actualSize == expectedSize;
+
+    if (!isOk) {
+        qsnprintf(msg, sizeof(msg), "Compared lists have different sizes.\n"
+                  "   Actual   (%s) size: %zd\n"
+                  "   Expected (%s) size: %zd", actual, actualSize,
+                  expected, expectedSize);
+    }
+
+    for (auto expectedIt = expectedBegin; isOk && expectedIt < expectedEnd; ++actualIt, ++expectedIt) {
+        if (!(*actualIt == *expectedIt)) {
+            const qsizetype i = qsizetype(expectedIt - expectedBegin);
+            char *val1 = QTest::toString(*actualIt);
+            char *val2 = QTest::toString(*expectedIt);
+
+            qsnprintf(msg, sizeof(msg), "Compared lists differ at index %zd.\n"
                       "   Actual   (%s): %s\n"
                       "   Expected (%s): %s", i, actual, val1 ? val1 : "<null>",
                       expected, val2 ? val2 : "<null>");
@@ -307,6 +317,35 @@ inline bool qCompare(QList<T> const &t1, QList<T> const &t2, const char *actual,
         }
     }
     return compare_helper(isOk, msg, nullptr, nullptr, actual, expected, file, line);
+}
+
+} // namespace Internal
+
+template <typename T>
+inline bool qCompare(QList<T> const &t1, QList<T> const &t2, const char *actual, const char *expected,
+                     const char *file, int line)
+{
+    return Internal::compareSequence(t1.cbegin(), t1.cend(), t2.cbegin(), t2.cend(),
+                                     actual, expected, file, line);
+}
+
+template <typename T, int N>
+bool qCompare(QList<T> const &t1, std::initializer_list<T> t2,
+              const char *actual, const char *expected,
+              const char *file, int line)
+{
+    return Internal::compareSequence(t1.cbegin(), t1.cend(), t2.cbegin(), t2.cend(),
+                                     actual, expected, file, line);
+}
+
+// Compare QList against array
+template <typename T, int N>
+bool qCompare(QList<T> const &t1, const T (& t2)[N],
+              const char *actual, const char *expected,
+              const char *file, int line)
+{
+    return Internal::compareSequence(t1.cbegin(), t1.cend(), t2, t2 + N,
+                                     actual, expected, file, line);
 }
 
 template <>

src/testlib/doc/snippets/code/src_qtestlib_qtestcase.cpp
@@ -49,6 +49,9 @@
 ****************************************************************************/
 #include <QTest>
 #include <QSqlDatabase>
+#include <QtGui/qfontdatabase.h>
+
+#include <initializer_list>
 
 // dummy
 class TestBenchmark : public QObject
@@ -245,4 +248,21 @@ QVERIFY2(file.open(QIODevice::WriteOnly),
          qPrintable(QString("open %1: %2")
                     .arg(file.fileName()).arg(file.errorString())));
 //! [33]
+}
+
+void compareListToArray()
+{
+//! [34]
+    const int expected[] = {8, 10, 12, 16, 20, 24};
+    QCOMPARE(QFontDatabase::standardSizes(), expected);
+//! [34]
+}
+
+void compareListToInitializerList()
+{
+//! [35]
+ #define ARG(...) __VA_ARGS__
+     QCOMPARE(QFontDatabase::standardSizes(), ARG({8, 10, 12, 16, 20, 24}));
+ #undef ARG
+//! [35]
 }

src/testlib/qtestcase.qdoc
@@ -116,19 +116,28 @@
    Expected.  If the parameter order is swapped, debugging a failing test can be
    confusing and tests expecting zero may fail due to rounding errors.
 
-   When comparing floating-point types (\c float, \c double, and \c qfloat16),
-   \l qFuzzyCompare() is used for finite values. If qFuzzyIsNull() is true for
-   both values, they are also considered equal. Infinities match if they have
-   the same sign, and any NaN as actual value matches with any NaN as expected
-   value (even though NaN != NaN, even when they're identical).
-
    QCOMPARE() tries to output the contents of the values if the comparison fails,
    so it is visible from the test log why the comparison failed.
 
    Example:
    \snippet code/src_qtestlib_qtestcase.cpp 2
 
-   \note This macro can only be used in a test function that is invoked
+   When comparing floating-point types (\c float, \c double, and \c qfloat16),
+   \l qFuzzyCompare() is used for finite values. If qFuzzyIsNull() is true for
+   both values, they are also considered equal. Infinities match if they have
+   the same sign, and any NaN as actual value matches with any NaN as expected
+   value (even though NaN != NaN, even when they're identical).
+
+   When comparing QList, arrays and initializer lists of the value type
+   can be passed as expected value:
+   \snippet code/src_qtestlib_qtestcase.cpp 34
+
+   Note that using initializer lists requires a defining a helper macro
+   to prevent the preprocessor from interpreting the commas as macro argument
+   delimiters:
+   \snippet code/src_qtestlib_qtestcase.cpp 35
+
+   \note QCOMPARE() can only be used in a test function that is invoked
    by the test framework.
 
    For your own classes, you can use \l QTest::toString() to format values for

tests/auto/gui/text/qglyphrun/tst_qglyphrun.cpp
@@ -493,6 +493,9 @@ void tst_QGlyphRun::drawMultiScriptText2()
 
 void tst_QGlyphRun::detach()
 {
+
+#define ARG(...) __VA_ARGS__
+
     QGlyphRun glyphs;
 
     glyphs.setGlyphIndexes(QList<quint32>() << 1 << 2 << 3);
@@ -504,8 +507,10 @@ void tst_QGlyphRun::detach()
 
     otherGlyphs.setGlyphIndexes(QList<quint32>() << 4 << 5 << 6);
 
-    QCOMPARE(otherGlyphs.glyphIndexes(), QList<quint32>() << 4 << 5 << 6);
-    QCOMPARE(glyphs.glyphIndexes(), QList<quint32>() << 1 << 2 << 3);
+    QCOMPARE(otherGlyphs.glyphIndexes(), ARG({4, 5, 6}));
+    QCOMPARE(glyphs.glyphIndexes(), ARG({1, 2, 3}));
+
+#undef ARG
 }
 
 void tst_QGlyphRun::drawRightToLeft()

tests/auto/gui/util/qundogroup/tst_qundogroup.cpp
@@ -237,20 +237,21 @@ void tst_QUndoGroup::addRemoveStack()
     QUndoGroup group;
 
     QUndoStack stack1(&group);
-    QCOMPARE(group.stacks(), QList<QUndoStack*>() << &stack1);
+    QCOMPARE(group.stacks(), {&stack1});
 
     QUndoStack stack2;
+    QUndoStack *expected12[] = {&stack1, &stack2};
     group.addStack(&stack2);
-    QCOMPARE(group.stacks(), QList<QUndoStack*>() << &stack1 << &stack2);
+    QCOMPARE(group.stacks(), expected12);
 
     group.addStack(&stack1);
-    QCOMPARE(group.stacks(), QList<QUndoStack*>() << &stack1 << &stack2);
+    QCOMPARE(group.stacks(), expected12);
 
     group.removeStack(&stack1);
-    QCOMPARE(group.stacks(), QList<QUndoStack*>() << &stack2);
+    QCOMPARE(group.stacks(), {&stack2});
 
     group.removeStack(&stack1);
-    QCOMPARE(group.stacks(), QList<QUndoStack*>() << &stack2);
+    QCOMPARE(group.stacks(), {&stack2});
 
     group.removeStack(&stack2);
     QVERIFY(group.stacks().isEmpty());
@@ -280,7 +281,7 @@ void tst_QUndoGroup::deleteStack()
     QCOMPARE(group.activeStack(), stack1);
 
     delete stack1;
-    QCOMPARE(group.stacks(), QList<QUndoStack*>() << stack3);
+    QCOMPARE(group.stacks(), {stack3});
     QCOMPARE(group.activeStack(), nullptr);
 
     stack3->setActive(false);

tests/auto/testlib/selftests/cmptest/tst_cmptest.cpp
@@ -141,7 +141,12 @@ private slots:
     void compare_tostring_data();
     void compareQStringLists();
     void compareQStringLists_data();
+    void compareQListInt_data();
     void compareQListInt();
+    void compareQListIntToArray_data();
+    void compareQListIntToArray();
+    void compareQListIntToInitializerList_data();
+    void compareQListIntToInitializerList();
     void compareQListDouble();
 #ifdef QT_GUI_LIB
     void compareQColor_data();
@@ -425,11 +430,48 @@ void tst_Cmptest::compareQStringLists()
     QCOMPARE(opA, opB);
 }
 
+using IntList = QList<int>;
+
+void tst_Cmptest::compareQListInt_data()
+{
+      QTest::addColumn<IntList>("actual");
+
+      QTest::newRow("match") << IntList{1, 2, 3};
+      QTest::newRow("size mismatch") << IntList{1, 2};
+      QTest::newRow("value mismatch") << IntList{1, 2, 4};
+}
+
 void tst_Cmptest::compareQListInt()
 {
-    QList<int> int1; int1 << 1 << 2 << 3;
-    QList<int> int2; int2 << 1 << 2 << 4;
-    QCOMPARE(int1, int2);
+    QFETCH(IntList, actual);
+    const QList<int> expected{1, 2, 3};
+    QCOMPARE(actual, expected);
+}
+
+void tst_Cmptest::compareQListIntToArray_data()
+{
+    compareQListInt_data();
+}
+
+void tst_Cmptest::compareQListIntToArray()
+{
+    QFETCH(IntList, actual);
+    const int expected[] = {1, 2, 3};
+    QCOMPARE(actual, expected);
+}
+
+void tst_Cmptest::compareQListIntToInitializerList_data()
+{
+    compareQListInt_data();
+}
+
+void tst_Cmptest::compareQListIntToInitializerList()
+{
+    QFETCH(IntList, actual);
+    // Protect ',' in the list
+#define ARG(...) __VA_ARGS__
+    QCOMPARE(actual,  ARG({1, 2, 3}));
+#undef ARG
 }
 
 void tst_Cmptest::compareQListDouble()

tests/auto/testlib/selftests/expected_cmptest.junitxml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<testsuite errors="0" failures="40" tests="26" name="tst_Cmptest">
+<testsuite errors="0" failures="45" tests="28" name="tst_Cmptest">
   <properties>
     <property value="@INSERT_QT_VERSION_HERE@" name="QTestVersion"/>
     <property value="@INSERT_QT_VERSION_HERE@" name="QtVersion"/>
@@ -70,9 +70,28 @@
    Expected (opB) size: 12" result="fail"/>
   </testcase>
   <testcase result="fail" name="compareQListInt">
-    <failure message="Compared lists differ at index 2.
-   Actual   (int1): 3
-   Expected (int2): 4" result="fail"/>
+    <failure tag="size mismatch" message="Compared lists have different sizes.
+   Actual   (actual) size: 2
+   Expected (expected) size: 3" result="fail"/>
+    <failure tag="value mismatch" message="Compared lists differ at index 2.
+   Actual   (actual): 4
+   Expected (expected): 3" result="fail"/>
+  </testcase>
+  <testcase result="fail" name="compareQListIntToArray">
+    <failure tag="size mismatch" message="Compared lists have different sizes.
+   Actual   (actual) size: 2
+   Expected (expected) size: 3" result="fail"/>
+    <failure tag="value mismatch" message="Compared lists differ at index 2.
+   Actual   (actual): 4
+   Expected (expected): 3" result="fail"/>
+  </testcase>
+  <testcase result="fail" name="compareQListIntToInitializerList">
+    <failure tag="size mismatch" message="Compared lists have different sizes.
+   Actual   (actual) size: 2
+   Expected (ARG({1, 2, 3})) size: 3" result="fail"/>
+    <failure tag="value mismatch" message="Compared lists differ at index 2.
+   Actual   (actual): 4
+   Expected (ARG({1, 2, 3})): 3" result="fail"/>
   </testcase>
   <testcase result="fail" name="compareQListDouble">
     <failure message="Compared lists differ at index 0.

tests/auto/testlib/selftests/expected_cmptest.lightxml
@@ -147,10 +147,56 @@
     <Duration msecs="0"/>
 </TestFunction>
 <TestFunction name="compareQListInt">
+<Incident type="pass" file="" line="0">
+    <DataTag><![CDATA[match]]></DataTag>
+</Incident>
 <Incident type="fail" file="qtbase/tests/auto/testlib/selftests/cmptest/tst_cmptest.cpp" line="0">
+    <DataTag><![CDATA[size mismatch]]></DataTag>
+    <Description><![CDATA[Compared lists have different sizes.
+   Actual   (actual) size: 2
+   Expected (expected) size: 3]]></Description>
+</Incident>
+<Incident type="fail" file="qtbase/tests/auto/testlib/selftests/cmptest/tst_cmptest.cpp" line="0">
+    <DataTag><![CDATA[value mismatch]]></DataTag>
     <Description><![CDATA[Compared lists differ at index 2.
-   Actual   (int1): 3
-   Expected (int2): 4]]></Description>
+   Actual   (actual): 4
+   Expected (expected): 3]]></Description>
+</Incident>
+    <Duration msecs="0"/>
+</TestFunction>
+<TestFunction name="compareQListIntToArray">
+<Incident type="pass" file="" line="0">
+    <DataTag><![CDATA[match]]></DataTag>
+</Incident>
+<Incident type="fail" file="qtbase/tests/auto/testlib/selftests/cmptest/tst_cmptest.cpp" line="0">
+    <DataTag><![CDATA[size mismatch]]></DataTag>
+    <Description><![CDATA[Compared lists have different sizes.
+   Actual   (actual) size: 2
+   Expected (expected) size: 3]]></Description>
+</Incident>
+<Incident type="fail" file="qtbase/tests/auto/testlib/selftests/cmptest/tst_cmptest.cpp" line="0">
+    <DataTag><![CDATA[value mismatch]]></DataTag>
+    <Description><![CDATA[Compared lists differ at index 2.
+   Actual   (actual): 4
+   Expected (expected): 3]]></Description>
+</Incident>
+    <Duration msecs="0"/>
+</TestFunction>
+<TestFunction name="compareQListIntToInitializerList">
+<Incident type="pass" file="" line="0">
+    <DataTag><![CDATA[match]]></DataTag>
+</Incident>
+<Incident type="fail" file="qtbase/tests/auto/testlib/selftests/cmptest/tst_cmptest.cpp" line="0">
+    <DataTag><![CDATA[size mismatch]]></DataTag>
+    <Description><![CDATA[Compared lists have different sizes.
+   Actual   (actual) size: 2
+   Expected (ARG({1, 2, 3})) size: 3]]></Description>
+</Incident>
+<Incident type="fail" file="qtbase/tests/auto/testlib/selftests/cmptest/tst_cmptest.cpp" line="0">
+    <DataTag><![CDATA[value mismatch]]></DataTag>
+    <Description><![CDATA[Compared lists differ at index 2.
+   Actual   (actual): 4
+   Expected (ARG({1, 2, 3})): 3]]></Description>
 </Incident>
     <Duration msecs="0"/>
 </TestFunction>

commit fca3ea040c9d05f112c6fc018f0c8e8c68892642

QTestlib: Fix custom toString() implementations for QList comparison
d25589e0529732996e405aaff8d6c46b012e1601 mistakenly changed the code
to use toString() from the QTest namespace. Call toString()
unqualified and move the compareSequence() helper out of the
internal namespace.

Fixes: QTBUG-89737
Pick-to: 6.0
Change-Id: I8452a0aead7771ad13a5b95438b54e202ccce76b
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/testlib/qtest.h
@@ -420,14 +420,12 @@ inline bool qCompare(QLatin1String const &t1, QString const &t2, const char *act
     return qCompare(QString(t1), t2, actual, expected, file, line);
 }
 
-namespace Internal {
-
 // Compare sequences of equal size
 template <typename ActualIterator, typename ExpectedIterator>
-bool compareSequence(ActualIterator actualIt, ActualIterator actualEnd,
-                     ExpectedIterator expectedBegin, ExpectedIterator expectedEnd,
-                     const char *actual, const char *expected,
-                     const char *file, int line)
+bool _q_compareSequence(ActualIterator actualIt, ActualIterator actualEnd,
+                        ExpectedIterator expectedBegin, ExpectedIterator expectedEnd,
+                        const char *actual, const char *expected,
+                        const char *file, int line)
 {
     char msg[1024];
     msg[0] = '\0';
@@ -446,8 +444,8 @@ bool compareSequence(ActualIterator actualIt, ActualIterator actualEnd,
     for (auto expectedIt = expectedBegin; isOk && expectedIt < expectedEnd; ++actualIt, ++expectedIt) {
         if (!(*actualIt == *expectedIt)) {
             const qsizetype i = qsizetype(expectedIt - expectedBegin);
-            char *val1 = QTest::toString(*actualIt);
-            char *val2 = QTest::toString(*expectedIt);
+            char *val1 = toString(*actualIt);
+            char *val2 = toString(*expectedIt);
 
             qsnprintf(msg, sizeof(msg), "Compared lists differ at index %zd.\n"
                       "   Actual   (%s): %s\n"
@@ -461,6 +459,8 @@ bool compareSequence(ActualIterator actualIt, ActualIterator actualEnd,
     }
     return compare_helper(isOk, msg, nullptr, nullptr, actual, expected, file, line);
 }
+
+namespace Internal {
 
 #if defined(TESTCASE_LOWDPI)
 void disableHighDpi()
@@ -476,7 +476,7 @@ template <typename T>
 inline bool qCompare(QList<T> const &t1, QList<T> const &t2, const char *actual, const char *expected,
                      const char *file, int line)
 {
-    return Internal::compareSequence(t1.cbegin(), t1.cend(), t2.cbegin(), t2.cend(),
+    return _q_compareSequence(t1.cbegin(), t1.cend(), t2.cbegin(), t2.cend(),
                                      actual, expected, file, line);
 }
 
@@ -485,7 +485,7 @@ bool qCompare(QList<T> const &t1, std::initializer_list<T> t2,
               const char *actual, const char *expected,
               const char *file, int line)
 {
-    return Internal::compareSequence(t1.cbegin(), t1.cend(), t2.cbegin(), t2.cend(),
+    return _q_compareSequence(t1.cbegin(), t1.cend(), t2.cbegin(), t2.cend(),
                                      actual, expected, file, line);
 }
 
@@ -495,7 +495,7 @@ bool qCompare(QList<T> const &t1, const T (& t2)[N],
               const char *actual, const char *expected,
               const char *file, int line)
 {
-    return Internal::compareSequence(t1.cbegin(), t1.cend(), t2, t2 + N,
+    return _q_compareSequence(t1.cbegin(), t1.cend(), t2, t2 + N,
                                      actual, expected, file, line);
 }
 