dafd26acbe7b08f5ddfe60432fe0e49422ac085c
ChunLin Wang <wangchunlin@uniontech.com>
2021-01-11 13:14:43 +0800

    Fix QApplication::font returns the font unstable according to the object
    If a default font was not registered for the widget's class, it returns the default font of its nearest registered superclass.
    
    Fixes: QTBUG-89910
    Pick-to: 5.15 6.0
    Change-Id: I6e6b2c6a0044462f84db9f76a03be0c6cfaaae8e
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/widgets/kernel/qapplication.cpp
+++ src/widgets/kernel/qapplication.cpp
@@ -1270,9 +1270,11 @@ QFont QApplication::font()
 /*!
     \overload
 
-    Returns the default font for the \a widget.
+    Returns the default font for the \a widget. If a default font was not
+    registered for the \a{widget}'s class, it returns the default font of
+    its nearest registered superclass.
 
-    \sa fontMetrics(), QWidget::setFont()
+    \sa fontMetrics(), setFont(), QWidget::setFont()
 */
 
 QFont QApplication::font(const QWidget *widget)
@@ -1290,14 +1292,16 @@ QFont QApplication::font(const QWidget *widget)
             return hash->value(QByteArrayLiteral("QMiniFont"));
         }
 #endif
-        FontHashConstIt it = hash->constFind(widget->metaObject()->className());
+        // Return the font for the nearest registered superclass
+        const QMetaObject *metaObj = widget->metaObject();
+        FontHashConstIt it = hash->constFind(metaObj->className());
         const FontHashConstIt cend = hash->constEnd();
+        while (it == cend && metaObj != &QWidget::staticMetaObject) {
+            metaObj = metaObj->superClass();
+            it = hash->constFind(metaObj->className());
+        }
         if (it != cend)
             return it.value();
-        for (it = hash->constBegin(); it != cend; ++it) {
-            if (widget->inherits(it.key()))
-                return it.value();
-        }
     }
     return font();
 }
