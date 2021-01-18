0afad46bb71b106780dd641de768a81d9f6c6a51
Thiago Macieira <thiago.macieira@intel.com>
2021-01-18 07:40:54 -0800

    Fix build with GCC 11: include <limits>
    Fixes: QTBUG-90395
    Change-Id: Iecc74d2000eb40dfbe7bfffd165b5dd3708b7a40
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 9c56d4da2ff631a8c1c30475bd792f6c86bda53c)
    Reviewed-by: Andy Shaw <andy.shaw@qt.io>

--- src/corelib/global/qfloat16.h
+++ src/corelib/global/qfloat16.h
@@ -43,6 +43,7 @@
 
 #include <QtCore/qglobal.h>
 #include <QtCore/qmetatype.h>
+#include <limits>
 #include <string.h>
 
 #if defined(QT_COMPILER_SUPPORTS_F16C) && defined(__AVX2__) && !defined(__F16C__)
