b2af6332ea37e45ab230a7a5d2d278f86d961b83
Thiago Macieira <thiago.macieira@intel.com>
2021-01-18 07:40:54 -0800

    Fix build with GCC 11: include <limits>
    Fixes: QTBUG-90395
    Change-Id: Iecc74d2000eb40dfbe7bfffd165b5dd3708b7a40
    (cherry picked from commit 9c56d4da2ff631a8c1c30475bd792f6c86bda53c)
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/corelib/global/qendian.h
+++ src/corelib/global/qendian.h
@@ -1,7 +1,7 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
-** Copyright (C) 2016 Intel Corporation.
+** Copyright (C) 2021 The Qt Company Ltd.
+** Copyright (C) 2021 Intel Corporation.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtCore module of the Qt Toolkit.
@@ -43,6 +43,8 @@
 
 #include <QtCore/qfloat16.h>
 #include <QtCore/qglobal.h>
+
+#include <limits>
 
 // include stdlib.h and hope that it defines __GLIBC__ for glibc-based systems
 #include <stdlib.h>
