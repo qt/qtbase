f379938443544025511a8e60ce6034f71b19add8
Joerg Bornemann <joerg.bornemann@qt.io>
2021-01-20 12:00:44 +0100

    pro2cmake: Raise QT_REPO_MODULE_VERSION to 6.1.0
    Modules that are ported for dev should get this version now.
    
    Change-Id: I11d47504f163798d53c51e56ef978edaa711d93a
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>

--- util/cmake/pro2cmake.py
+++ util/cmake/pro2cmake.py
@@ -4349,7 +4349,7 @@ def create_top_level_cmake_conf():
     conf_file_name = ".cmake.conf"
     try:
         with open(conf_file_name, "x") as file:
-            file.write('set(QT_REPO_MODULE_VERSION "6.0.0")\n')
+            file.write('set(QT_REPO_MODULE_VERSION "6.1.0")\n')
     except FileExistsError:
         pass
 
