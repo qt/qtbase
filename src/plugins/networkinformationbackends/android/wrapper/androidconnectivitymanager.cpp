2d5506dd9ca7f1ee2e90c4b47eee3fb642dd4c60
Mårten Nordheim <marten.nordheim@qt.io>
2021-01-26 12:39:11 +0100

    QNetworkInformation backend for Android
    Based on the old bearer manager code!
    
    A lot of the old code went away though since it had been deprecated
    in the time since it was written.
    
    Task-number: QTBUG-86966
    Change-Id: I21a6db1d4ebd8367084355a8e3202f4c05d9dce5
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    (cherry picked from commit 8652493529a46a375c11bbaf16d2122ee8466c29)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/plugins/networkinformationbackends/android/wrapper/androidconnectivitymanager.cpp
@@ -0,0 +1,132 @@
+/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the plugins of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "androidconnectivitymanager.h"
+
+#include <QtCore/private/qjnihelpers_p.h>
+#include <QtCore/qjnienvironment.h>
+
+QT_BEGIN_NAMESPACE
+
+struct AndroidConnectivityManagerInstance
+{
+    AndroidConnectivityManagerInstance() : connManager(new AndroidConnectivityManager) { }
+    std::unique_ptr<AndroidConnectivityManager> connManager = nullptr;
+};
+Q_GLOBAL_STATIC(AndroidConnectivityManagerInstance, androidConnManagerInstance)
+
+static const char networkInformationClass[] =
+        "org/qtproject/qt/android/networkinformation/QtAndroidNetworkInformation";
+
+static void networkConnectivityChanged()
+{
+    Q_EMIT androidConnManagerInstance->connManager->connectivityChanged();
+}
+
+AndroidConnectivityManager::AndroidConnectivityManager()
+{
+    if (!registerNatives())
+        return;
+
+    m_connectivityManager = QJniObject::callStaticObjectMethod(
+            networkInformationClass, "getConnectivityManager",
+            "(Landroid/content/Context;)Landroid/net/ConnectivityManager;",
+            QtAndroidPrivate::context());
+    if (!m_connectivityManager.isValid())
+        return;
+
+    QJniObject::callStaticMethod<void>(networkInformationClass, "registerReceiver",
+                                       "(Landroid/content/Context;)V", QtAndroidPrivate::context());
+}
+
+AndroidConnectivityManager *AndroidConnectivityManager::getInstance()
+{
+    if (!androidConnManagerInstance())
+        return nullptr;
+    return androidConnManagerInstance->connManager->isValid()
+            ? androidConnManagerInstance->connManager.get()
+            : nullptr;
+}
+
+AndroidConnectivityManager::~AndroidConnectivityManager()
+{
+    QJniObject::callStaticMethod<void>(networkInformationClass, "unregisterReceiver",
+                                       "(Landroid/content/Context;)V", QtAndroidPrivate::context());
+}
+
+AndroidConnectivityManager::AndroidConnectivity AndroidConnectivityManager::networkConnectivity()
+{
+    QJniEnvironment env;
+    QJniObject networkReceiver(networkInformationClass);
+    jclass clazz = env->GetObjectClass(networkReceiver.object());
+    static const QByteArray functionSignature =
+            QByteArray(QByteArray("()L") + networkInformationClass + "$AndroidConnectivity;");
+    QJniObject enumObject =
+            QJniObject::callStaticObjectMethod(clazz, "state", functionSignature.data());
+    if (!enumObject.isValid())
+        return AndroidConnectivityManager::AndroidConnectivity::Unknown;
+
+    QJniObject enumName = enumObject.callObjectMethod<jstring>("name");
+    if (!enumName.isValid())
+        return AndroidConnectivityManager::AndroidConnectivity::Unknown;
+
+    QString name = enumName.toString();
+    if (name == u"Connected")
+        return AndroidConnectivity::Connected;
+    if (name == u"Disconnected")
+        return AndroidConnectivity::Disconnected;
+    return AndroidConnectivity::Unknown;
+}
+
+bool AndroidConnectivityManager::registerNatives()
+{
+    QJniEnvironment env;
+    QJniObject networkReceiver(networkInformationClass);
+    if (!networkReceiver.isValid())
+        return false;
+
+    jclass clazz = env->GetObjectClass(networkReceiver.object());
+    static JNINativeMethod method = { "connectivityChanged", "()V",
+                                      reinterpret_cast<void *>(networkConnectivityChanged) };
+    const bool ret = (env->RegisterNatives(clazz, &method, 1) == JNI_OK);
+    env->DeleteLocalRef(clazz);
+    return ret;
+}
+
+QT_END_NAMESPACE
