5c329f023a29f45a1e87c660554c49fee34dae4b
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-02-03 11:41:46 +0100

    Add "we mean it" warning to private header
    Fix warning from configure.
    
    Change-Id: I12def11a4effb7298ec0501cfac4ffd37b777ff6
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- src/gui/platform/darwin/qapplekeymapper_p.h
+++ src/gui/platform/darwin/qapplekeymapper_p.h
@@ -40,6 +40,17 @@
 #ifndef QAPPLEKEYMAPPER_H
 #define QAPPLEKEYMAPPER_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #ifdef Q_OS_MACOS
 #include <Carbon/Carbon.h>
 #endif
