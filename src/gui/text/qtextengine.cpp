94ce755dcd823678b0338f4bb4149fe4eeb844c2
Edward Welbourne <edward.welbourne@qt.io>
2021-02-08 12:13:13 +0100

    Fix handling of surrogates in QBidiAlgorithm
    Prior code was naively assuming the character after a high surrogate
    would necessarily be a low surrogate, which is buggy.
    Fixes oss-fuzz issue 29718.
    
    Change-Id: I10f023c4b5024a0d76fea0a3672001063591ec6d
    Reviewed-by: Konstantin Ritt <ritt.ks@gmail.com>
    Reviewed-by: Robert Löhning <robert.loehning@qt.io>
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>
    (cherry picked from commit aeeaab1a5ac0b4d91c9f9b542035b8970e4c61dd)

--- src/gui/text/qtextengine.cpp
+++ src/gui/text/qtextengine.cpp
@@ -258,7 +258,7 @@ struct QBidiAlgorithm {
         for (int i = 0; i < length; ++i) {
             int pos = i;
             uint uc = text[i].unicode();
-            if (QChar::isHighSurrogate(uc) && i < length - 1) {
+            if (QChar::isHighSurrogate(uc) && i < length - 1 && text[i + 1].isLowSurrogate()) {
                 ++i;
                 analysis[i].bidiDirection = QChar::DirNSM;
                 uc = QChar::surrogateToUcs4(ushort(uc), text[i].unicode());
