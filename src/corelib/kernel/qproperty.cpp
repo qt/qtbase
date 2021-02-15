52fc2539e01b0d8376721d9563a0cba52a899a9a
Fabian Kosmale <fabian.kosmale@qt.io>
2021-02-15 14:08:44 +0100

    Mark QPropertyAlias as internal
    That class was only really meant for the QML compiler, and it is dubious
    if event that one needs it. The current implementation is also broken.
    
    Change-Id: Ie40d282707f3fabc8079bee9e98f082aeb9d30b3
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 5481601debd6ce06bb8f0135e3ea4baf8a21cb3d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/kernel/qproperty.cpp
+++ src/corelib/kernel/qproperty.cpp
@@ -1126,6 +1126,8 @@ QString QPropertyBindingError::description() const
 /*!
   \class QPropertyAlias
   \inmodule QtCore
+  \internal
+
   \brief The QPropertyAlias class is a safe alias for a QProperty with same template parameter.
 
   \ingroup tools
