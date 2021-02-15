7846e2f40d7d2912e9040ebfd97d71aef976131e
Nico Vertriest <nico.vertriest@qt.io>
2021-02-15 08:36:33 +0100

    Doc: Fix warning on undocumented enum value
    Change-Id: Ia917b86a24bdb621b1040f1e1a9fe4946a1e57ae
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/corelib/global/qlibraryinfo.cpp
+++ src/corelib/global/qlibraryinfo.cpp
@@ -861,6 +861,7 @@ QStringList QLibraryInfo::platformPluginArguments(const QString &platformName)
     \value BinariesPath The path to installed Qt binaries (tools and applications).
     \value PluginsPath The path to installed Qt plugins.
     \value QmlImportsPath The path to installed QML extensions to import.
+    \value Qml2ImportsPath The path to installed QML extensions to import.
     \value ArchDataPath The path to general architecture-dependent Qt data.
     \value DataPath The path to general architecture-independent Qt data.
     \value TranslationsPath The path to translation information for Qt strings.
