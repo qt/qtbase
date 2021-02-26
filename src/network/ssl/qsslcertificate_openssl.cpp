f0367f87d89806b54fc5768ae59c0927e506b325
Timur Pocheptsov <timur.pocheptsov@qt.io>
2021-02-26 12:11:03 +0100

    QSsl: fix UB pointer use
    Weird macro had inverted notion of type safety: instead of casting
    parameters and return values (they are pointers) to work with generic
    OPENSSL_sk_xxx functions, it was ... casting a function pointer to an invalid
    type to get ... nothing, but UB. Home-brewed (un)'safestack'!!!
    
    Change-Id: Ib91a7ba4cd472f370836797e422456f91a4385b0
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 9fc2f1f076b953ff0979fb73ed6e70ac9bde278c)

--- src/network/ssl/qsslcertificate_openssl.cpp
+++ src/network/ssl/qsslcertificate_openssl.cpp
@@ -356,7 +356,7 @@ static QVariant x509UnknownExtensionToValue(X509_EXTENSION *ext)
         QVariantList list;
         bool isMap = false;
 
-        for (int j = 0; j < q_SKM_sk_num(CONF_VALUE, val); j++) {
+        for (int j = 0; j < q_SKM_sk_num(val); j++) {
             CONF_VALUE *nval = q_SKM_sk_value(CONF_VALUE, val, j);
             if (nval->name && nval->value) {
                 isMap = true;
@@ -428,7 +428,7 @@ static QVariant x509ExtensionToValue(X509_EXTENSION *ext)
                 return QVariant();
 
             QVariantMap result;
-            for (int i=0; i < q_SKM_sk_num(ACCESS_DESCRIPTION, info); i++) {
+            for (int i=0; i < q_SKM_sk_num(info); i++) {
                 ACCESS_DESCRIPTION *ad = q_SKM_sk_value(ACCESS_DESCRIPTION, info, i);
 
                 GENERAL_NAME *name = ad->location;
