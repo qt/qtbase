ba139603925453bf79994eca48b566d8f15b2af0
Friedemann Kleint <Friedemann.Kleint@qt.io>
2021-03-02 08:37:15 +0100

    Stabilize test QMenu::QTBUG_89082_actionTipsHide() on Windows
    Move the cursor out of the way to prevent it from interfering.
    Amends 3f3d5e6716d9130776b3613ccbd5595de7d4af8d.
    
    Task-number: QTBUG-89082
    Pick-to: 5.15
    Pick-to: 6.0
    Change-Id: I0efbe2b5618a04e92839083a1bd3383e05d6ff93
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- tests/auto/widgets/widgets/qmenu/tst_qmenu.cpp
+++ tests/auto/widgets/widgets/qmenu/tst_qmenu.cpp
@@ -1369,6 +1369,9 @@ void tst_QMenu::QTBUG_89082_actionTipsHide()
     widget.resize(300, 200);
     centerOnScreen(&widget);
     widget.show();
+#if QT_CONFIG(cursor)
+    QCursor::setPos(widget.screen()->availableGeometry().topLeft() + QPoint(10, 10));
+#endif
     widget.activateWindow();
     QVERIFY(QTest::qWaitForWindowExposed(&widget));
     menu->popup(widget.geometry().topRight() + QPoint(50, 0));
