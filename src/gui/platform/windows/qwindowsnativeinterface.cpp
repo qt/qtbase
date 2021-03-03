aac5b02e28440c93fd6c997fcf1cf6f217c5c1a5
Paul Wicking <paul.wicking@qt.io>
2021-03-03 18:39:40 +0000

    Doc: Use correct include for QWGLContext
    Task-number: QTBUG-91500
    Change-Id: If487a2d14dd61d127dc35aa039f9b71915128da1
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 15f934dcea2743ceb59d3c9ad840e2b00b39d043)
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/gui/platform/windows/qwindowsnativeinterface.cpp
+++ src/gui/platform/windows/qwindowsnativeinterface.cpp
@@ -53,7 +53,7 @@ using namespace QNativeInterface::Private;
 
 /*!
     \class QNativeInterface::QWGLContext
-    \inheaderfile qopenglcontext_platform.h
+    \inheaderfile QOpenGLContext
     \since 6.0
     \brief Native interface to a WGL context on Windows.
 
