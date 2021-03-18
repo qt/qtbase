c8989ca20fa5798f7853f07df6e150cc407c64a4
Tarja Sundqvist <tarja.sundqvist@qt.io>
2021-03-18 17:29:29 +0200

    Revert "Update commercial license headers to /util files"
    This reverts commit d7f95124da230cafe26896e537e3a3a1af1fd9bb.
    
    Revert of commercial license headers is required for
    Qt 5.15.3 opensource release.
    
    Task-number: QTBUG-91108
    Change-Id: Ifad15cc26055eed6c82e30b5b68bb33d070bae0e
    Reviewed-by: Akseli Salovaara <akseli.salovaara@qt.io>

--- util/includemocs/includemocs.pl
+++ util/includemocs/includemocs.pl
@@ -6,8 +6,7 @@
 ##
 ## This file is the build configuration utility of the Qt Toolkit.
 ##
-## $QT_BEGIN_LICENSE:COMM$
-##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
 ## Commercial License Usage
 ## Licensees holding valid commercial Qt licenses may use this file in
 ## accordance with the commercial license agreement provided with the
@@ -16,16 +15,17 @@
 ## and conditions see https://www.qt.io/terms-conditions. For further
 ## information use the contact form at https://www.qt.io/contact-us.
 ##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
 ## $QT_END_LICENSE$
 ##
-##
-##
-##
-##
-##
-##
-##
-############################################################################
+#############################################################################
 use strict;
 
 MAKEFILE: while ($_ = shift @ARGV) {
