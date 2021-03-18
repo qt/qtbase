cb29a8c2a5f019f915f576991901b045bf78ff5d
Tarja Sundqvist <tarja.sundqvist@qt.io>
2021-03-18 17:56:27 +0200

    Revert "Update commercial license headers"
    This reverts commit 81f474501aa0c7bfe2a206b303da022b72423e46.
    
    Revert of commercial license headers is required for
    Qt 5.15.3 opensource release.
    
    Task-number: QTBUG-91108
    Change-Id: I3c477161af0e2d166f282da344d028fdd4cfca53
    Reviewed-by: Jukka Jokiniva <jukka.jokiniva@qt.io>

--- src/tools/qlalr/compress.cpp
+++ src/tools/qlalr/compress.cpp
@@ -1,12 +1,11 @@
 /****************************************************************************
 **
-** Copyright (C) 2021 The Qt Company Ltd.
+** Copyright (C) 2016 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the test suite module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:COMM$
-**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
@@ -15,14 +14,15 @@
 ** and conditions see https://www.qt.io/terms-conditions. For further
 ** information use the contact form at https://www.qt.io/contact-us.
 **
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
 ** $QT_END_LICENSE$
-**
-**
-**
-**
-**
-**
-**
 **
 ****************************************************************************/
 
