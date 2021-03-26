qt/qtbase

commit e6ca200a3a0aecc2313becf959c1be4fe18bdfa1

Android: Fix constant resize of a large TextEdit on click
If there is a large (3/5 of the screen in portrait or 2/3
in landscape) TextEdit on the screen, and it gets focus
on click, it will be shrank to fit the screen. Next click on this
TextEdit will restore its normal height, the next will shrink.

Pick-to: 5.15
Fixes: QTBUG-91056
Change-Id: I3dbf085cbfdc2739d537a304c16e28c58a6e01ce
Reviewed-by: Rami Potinkara <rami.potinkara@qt.io>
Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>

src/android/jar/src/org/qtproject/qt/android/QtNative.java
@@ -57,6 +57,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ApplicationInfo;
 import android.content.UriPermission;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
@@ -67,6 +68,7 @@ import android.content.ClipboardManager.OnPrimaryClipChangedListener;
 import android.content.ClipData;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
+import android.util.DisplayMetrics;
 import android.view.ContextMenu;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -116,6 +118,7 @@ public class QtNative
     public static QtThread m_qtThread = new QtThread();
     private static HashMap<String, Uri> m_cachedUris = new HashMap<String, Uri>();
     private static ArrayList<String> m_knownDirs = new ArrayList<String>();
+    private static final int KEYBOARD_HEIGHT_THRESHOLD = 100;
 
     private static final Runnable runPendingCppRunnablesRunnable = new Runnable() {
         @Override
@@ -947,6 +950,17 @@ public class QtNative
                 updateWindow();
             }
         });
+    }
+
+    public static boolean isSoftwareKeyboardVisible()
+    {
+        Activity activity = QtNative.activity();
+        Rect r = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        final int kbHeight = metrics.heightPixels - r.bottom;
+        return (kbHeight >= KEYBOARD_HEIGHT_THRESHOLD);
     }
 
     private static void notifyAccessibilityLocationChange()

src/plugins/platforms/android/androidjniinput.cpp
@@ -58,7 +58,6 @@ using namespace QtAndroid;
 namespace QtAndroidInput
 {
     static bool m_ignoreMouseEvents = false;
-    static bool m_softwareKeyboardVisible = false;
     static QRect m_softwareKeyboardRect;
 
     static QList<QWindowSystemInterface::TouchPoint> m_touchPoints;
@@ -114,7 +113,7 @@ namespace QtAndroidInput
 
     bool isSoftwareKeyboardVisible()
     {
-        return m_softwareKeyboardVisible;
+        return QJniObject::callStaticMethod<jboolean>(applicationClass(), "isSoftwareKeyboardVisible");
     }
 
     QRect softwareKeyboardRect()
@@ -807,7 +806,6 @@ namespace QtAndroidInput
 
     static void keyboardVisibilityChanged(JNIEnv */*env*/, jobject /*thiz*/, jboolean visibility)
     {
-        m_softwareKeyboardVisible = visibility;
         if (!visibility)
             m_softwareKeyboardRect = QRect();
 

src/plugins/platforms/android/qandroidinputcontext.cpp
@@ -890,10 +890,11 @@ void QAndroidInputContext::showInputPanel()
         m_updateCursorPosConnection = connect(qGuiApp->focusObject(), SIGNAL(cursorPositionChanged()), this, SLOT(updateCursorPosition()));
 
     QRect rect = cursorRect();
-    QtAndroidInput::showSoftwareKeyboard(rect.left(), rect.top(), rect.width(), rect.height(),
-                                         screenInputItemRectangle().height(),
-                                         query->value(Qt::ImHints).toUInt(),
-                                         query->value(Qt::ImEnterKeyType).toUInt());
+    if (!isInputPanelVisible())
+        QtAndroidInput::showSoftwareKeyboard(rect.left(), rect.top(), rect.width(), rect.height(),
+                                             screenInputItemRectangle().height(),
+                                             query->value(Qt::ImHints).toUInt(),
+                                             query->value(Qt::ImEnterKeyType).toUInt());
 }
 
 QRect QAndroidInputContext::cursorRect()

commit e402e1103b874c0bf91a1bc754752ee73ffadf33

Android: fix for isSoftwareKeyboard method
isSoftwareKeyboard() method will rely on visibility flag from
activityDelegate. In such case it also need to consider that keyboard is
during the hiding.

Fix the regression related with commit:
e6ca200a3a0aecc2313becf959c1be4fe18bdfa1

Before this change Software Keyboard is not showing after changing the
focus. It is happening when Keyboard is already visible and focus is
changed to widget that also need the keyboard. In such case keyboard is
hiding (and NOT showing again).

Pick-to: 5.15
Fixes: QTBUG-92051
Change-Id: I6a5d07b25442429fda6e715d09ea104432c5ef50
Reviewed-by: Rami Potinkara <rami.potinkara@qt.io>
Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>

src/android/jar/src/org/qtproject/qt/android/QtActivityDelegate.java
@@ -209,6 +209,11 @@ public class QtActivityDelegate
         }
     }
 
+    public boolean isKeyboardVisible()
+    {
+        return m_keyboardIsVisible;
+    }
+
     // input method hints - must be kept in sync with QTDIR/src/corelib/global/qnamespace.h
     private final int ImhHiddenText = 0x1;
     private final int ImhSensitiveData = 0x2;
@@ -260,7 +265,7 @@ public class QtActivityDelegate
         if (m_keyboardIsVisible == visibility)
             return false;
         m_keyboardIsVisible = visibility;
-        QtNative.keyboardVisibilityChanged(m_keyboardIsVisible);
+        QtNative.keyboardVisibilityUpdated(m_keyboardIsVisible);
 
         if (visibility == false)
             updateFullScreen(); // Hiding the keyboard clears the immersive mode, so we need to set it again.

src/android/jar/src/org/qtproject/qt/android/QtNative.java
@@ -99,6 +99,7 @@ public class QtNative
     public static final String QtTAG = "Qt JAVA"; // string used for Log.x
     private static ArrayList<Runnable> m_lostActions = new ArrayList<Runnable>(); // a list containing all actions which could not be performed (e.g. the main activity is destroyed, etc.)
     private static boolean m_started = false;
+    private static boolean m_isKeyboardHiding = false;
     private static int m_displayMetricsScreenWidthPixels = 0;
     private static int m_displayMetricsScreenHeightPixels = 0;
     private static int m_displayMetricsAvailableLeftPixels = 0;
@@ -930,6 +931,7 @@ public class QtNative
 
     private static void hideSoftwareKeyboard()
     {
+        m_isKeyboardHiding = true;
         runAction(new Runnable() {
             @Override
             public void run() {
@@ -954,13 +956,7 @@ public class QtNative
 
     public static boolean isSoftwareKeyboardVisible()
     {
-        Activity activity = QtNative.activity();
-        Rect r = new Rect();
-        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
-        DisplayMetrics metrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        final int kbHeight = metrics.heightPixels - r.bottom;
-        return (kbHeight >= KEYBOARD_HEIGHT_THRESHOLD);
+        return m_activityDelegate.isKeyboardVisible() && !m_isKeyboardHiding;
     }
 
     private static void notifyAccessibilityLocationChange()
@@ -1325,6 +1321,12 @@ public class QtNative
                     m_activityDelegate.hideSplashScreen(duration);
             }
         });
+    }
+
+    public static void keyboardVisibilityUpdated(boolean visibility)
+    {
+        m_isKeyboardHiding = false;
+        keyboardVisibilityChanged(visibility);
     }
 
     private static String[] listAssetContent(android.content.res.AssetManager asset, String path) {