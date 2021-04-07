2c2b34997468a028de9e9c2de54b21c7e0757fbb
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-04-07 16:50:26 +0200

    Don't judge other people's writing systems
    Pick-to: 6.1 6.0 5.15
    Change-Id: Ifb8265bad4c8c96c6b20f957c376cec47e28f666
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/gui/text/qfontmetrics.cpp
+++ src/gui/text/qfontmetrics.cpp
@@ -261,9 +261,8 @@ bool QFontMetrics::operator ==(const QFontMetrics &other) const
     The ascent of a font is the distance from the baseline to the
     highest position characters extend to. In practice, some font
     designers break this rule, e.g. when they put more than one accent
-    on top of a character, or to accommodate an unusual character in
-    an exotic language, so it is possible (though rare) that this
-    value will be too small.
+    on top of a character, or to accommodate a certain character, so it
+    is possible (though rare) that this value will be too small.
 
     \sa descent()
 */
@@ -298,8 +297,8 @@ int QFontMetrics::capHeight() const
 
     The descent is the distance from the base line to the lowest point
     characters extend to. In practice, some font designers break this rule,
-    e.g. to accommodate an unusual character in an exotic language, so
-    it is possible (though rare) that this value will be too small.
+    e.g. to accommodate a certain character, so it is possible (though
+    rare) that this value will be too small.
 
     \sa ascent()
 */
@@ -1075,9 +1074,8 @@ bool QFontMetricsF::operator ==(const QFontMetricsF &other) const
     The ascent of a font is the distance from the baseline to the
     highest position characters extend to. In practice, some font
     designers break this rule, e.g. when they put more than one accent
-    on top of a character, or to accommodate an unusual character in
-    an exotic language, so it is possible (though rare) that this
-    value will be too small.
+    on top of a character, or to accommodate a certain character, so
+    it is possible (though rare) that this value will be too small.
 
     \sa descent()
 */
@@ -1113,8 +1111,8 @@ qreal QFontMetricsF::capHeight() const
     The descent is the distance from the base line to the lowest point
     characters extend to. (Note that this is different from X, which
     adds 1 pixel.) In practice, some font designers break this rule,
-    e.g. to accommodate an unusual character in an exotic language, so
-    it is possible (though rare) that this value will be too small.
+    e.g. to accommodate a certain character, so it is possible (though
+    rare) that this value will be too small.
 
     \sa ascent()
 */
