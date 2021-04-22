qt/qtbase

commit 661b586a69740bd9a1791622f8b238d290ebe00d

Make "qmake -qtconf my-qt.conf -query" possible
It's now possible to put the -qtconf argument in front of the -query
argument (and others).

Fixes: QTBUG-85136
Change-Id: I74b78f084acf264ef637ed2f6cb63b655b176056
Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

qmake/option.cpp
@@ -420,6 +420,11 @@ Option::init(int argc, char **argv)
                 Option::qmake_mode = Option::QMAKE_QUERY_PROPERTY;
             } else if (opt == "-makefile") {
                 Option::qmake_mode = Option::QMAKE_GENERATE_MAKEFILE;
+            } else if (opt == "-qtconf") {
+                // Move the argument following "-qtconf <file>" in front and check again.
+                if (args.length() >= 3)
+                    args.prepend(args.takeAt(2));
+                continue;
             } else {
                 break;
             }

commit 6b1a7341fed4b9456ea6bfa2de7412d45ef56c65

Fix infinite loop in qmake option parsing
If calling 'qmake -qtconf file' an infinite loop occurs.

Bug introduced in 661b586a69740bd9a1791622f8b238d290ebe00d

Pick-to: 6.1 6.1.0 6.0
Change-Id: I3ccc067570308227b605d700b41b299800aa872a
Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

qmake/option.cpp
@@ -399,10 +399,11 @@ Option::init(int argc, char **argv)
             } else if (opt == "-makefile") {
                 Option::qmake_mode = Option::QMAKE_GENERATE_MAKEFILE;
             } else if (opt == "-qtconf") {
-                // Move the argument following "-qtconf <file>" in front and check again.
-                if (args.length() >= 3)
+                if (args.length() >= 3) {
+                    // Move the argument following "-qtconf <file>" in front and check again.
                     args.prepend(args.takeAt(2));
-                continue;
+                    continue;
+                }
             } else {
                 break;
             }