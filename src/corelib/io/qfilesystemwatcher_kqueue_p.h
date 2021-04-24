3795e4dda55a71bcdab53a3f5a75a0dbd20e7e0e
Niclas Rosenvik <nros@netbsd.org>
2021-04-24 10:32:31 +0000

    Add override to addPaths and removePaths in kqueue filesystem watcher
    Add override to addPaths and removePaths in kqueue filesystem watcher
    Fixes build break in developer-build with gcc 10 on *BSD
    
    Change-Id: I7d91251d6bcb1614bb9f21c319abc9c2df681cda
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit 96f493f3b8ba823dcac8e8385ea1256a22c0a1cf)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/io/qfilesystemwatcher_kqueue_p.h
+++ src/corelib/io/qfilesystemwatcher_kqueue_p.h
@@ -71,8 +71,10 @@ public:
 
     static QKqueueFileSystemWatcherEngine *create(QObject *parent);
 
-    QStringList addPaths(const QStringList &paths, QStringList *files, QStringList *directories);
-    QStringList removePaths(const QStringList &paths, QStringList *files, QStringList *directories);
+    QStringList addPaths(const QStringList &paths, QStringList *files,
+                         QStringList *directories) override;
+    QStringList removePaths(const QStringList &paths, QStringList *files,
+                            QStringList *directories) override;
 
 private Q_SLOTS:
     void readFromKqueue();
