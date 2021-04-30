b0e96ee181527f319e6749abae478bcbe9e56474
Joerg Bornemann <joerg.bornemann@qt.io>
2021-04-30 17:09:18 +0200

    Update the comment in QtFinishPrlFile.cmake
    Clarify that OUT_FILE is not the final .prl anymore and where we get the
    path of the final .prl from.
    
    Change-Id: I4563c6ef1026bc0646e3ac9ab2fac49e426ca07f
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- cmake/QtFinishPrlFile.cmake
+++ cmake/QtFinishPrlFile.cmake
@@ -10,9 +10,11 @@
 # - Prepends '-l' to values that are not absolute paths, and don't start with a dash
 #   aka, '-lfoo', '-framework', '-pthread'.
 #
+# The path to the final .prl file is stored in the input file as assignment to FINAL_PRL_FILE_PATH.
+#
 # This file is to be used in CMake script mode with the following variables set:
-# IN_FILE: path to the preliminary .prl file
-# OUT_FILE: path to the final .prl file that's going to be installed
+# IN_FILE: path to the step 1 preliminary .prl file
+# OUT_FILE: path to the step 2 preliminary .prl file that is going to be created
 # QT_LIB_DIRS: list of paths where Qt libraries are located.
 #              This includes the install prefix and the current repo build dir.
 #              These paths get replaced with relocatable paths or linker / framework flags.
