db25a067b0e1b5905e9cd3316df45f2be1b44594
Joerg Bornemann <joerg.bornemann@qt.io>
2021-05-17 14:09:37 +0200

    Fix build of standalone painting examples
    Since commit f5c69a51956 the examples/widgets/painting examples could
    not be built standalone anymore.
    Move the find_package calls out of the examples/widgets/painting/shared
    subdirectory and add a .cmake file that can be included to use the
    library conveniently.
    
    Change-Id: I35cb5bd8419a67c782d109104c4a88d64477a8be
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

---
+++ examples/widgets/painting/shared/use_lib.cmake
@@ -0,0 +1,13 @@
+# Include this file in your example project to use the library defined in this directory.
+# This avoids find_package calls in a directory scope different from the directory scope of the
+# consuming target.
+
+if(NOT TARGET Qt::Widgets)
+    find_package(Qt6 REQUIRED COMPONENTS Widgets)
+endif()
+
+if(NOT TARGET Qt::OpenGL)
+    find_package(Qt6 OPTIONAL_COMPONENTS OpenGL)
+endif()
+
+add_subdirectory("${CMAKE_CURRENT_LIST_DIR}" painting_shared)
