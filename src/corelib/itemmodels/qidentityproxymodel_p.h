9f918e1356f3acb6769fb6155f471101f67180e2
Mitch Curtis <mitch.curtis@qt.io>
2021-06-02 13:48:03 +0200

    Add "We mean it" warning to qidentityproxymodel_p.h
    This was missed in b42e2d70fbda5afb462b869583b925ad5f1a5480.
    
    Pick-to: 6.2
    Change-Id: Id06bd880456534293cf6c0d215f794b6ad0552ca
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/corelib/itemmodels/qidentityproxymodel_p.h
+++ src/corelib/itemmodels/qidentityproxymodel_p.h
@@ -40,6 +40,18 @@
 #ifndef QIDENTITYPROXYMODEL_P_H
 #define QIDENTITYPROXYMODEL_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of QAbstractItemModel*.  This header file may change from version
+// to version without notice, or even be removed.
+//
+// We mean it.
+//
+//
+
 #include <QtCore/private/qabstractproxymodel_p.h>
 #include <QtCore/qidentityproxymodel.h>
 
