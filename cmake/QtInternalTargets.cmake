8b31d1475cd9e904765bbbe10cfe86f8905438cb
Andreas Buhr <andreas.buhr@qt.io>
2021-06-24 11:23:12 +0200

    fix warning in cborparser with gcc 11.1
    Change-Id: Ie8270f2b156b194dedd115a97b93f7f800af055e
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 3818046204164c4ec9851ef3043f1c1fc2932f72)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- cmake/QtInternalTargets.cmake
+++ cmake/QtInternalTargets.cmake
@@ -48,7 +48,8 @@ function(qt_internal_set_warnings_are_errors_flags target)
         if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "11.0.0" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11.2.0")
             # GCC 11.1 has a regression in the integrated preprocessor, so disable it as a workaround (QTBUG-93360)
             # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100796
-            list(APPEND flags -no-integrated-cpp)
+            # This in turn triggers a fallthrough warning in cborparser.c, so we disable this warning.
+            list(APPEND flags -no-integrated-cpp -Wno-implicit-fallthrough)
         endif()
 
         # Work-around for bug https://code.google.com/p/android/issues/detail?id=58135
