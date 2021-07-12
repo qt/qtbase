e9519d207e3c38ca779f7dcfc5e5a716ec041590
Ievgenii Meshcheriakov <ievgenii.meshcheriakov@qt.io>
2021-07-12 14:11:39 +0200

    locale_database: Use context manager interface to update source files
    Use context manager interface (with statement) to atomically update source
    files. This ensures that all files are properly closed and the temporary
    file is removed even in case of errors.
    
    Task-number: QTBUG-83488
    Pick-to: 6.2
    Change-Id: I18cd96f1d03e467dea6212f6576a41e65f414ce1
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- util/locale_database/localetools.py
+++ util/locale_database/localetools.py
@@ -37,6 +37,7 @@ Classes:
   SourceFileEditor -- adds standard prelude and tail handling to Transcriber.
 """
 
+from contextlib import ExitStack, contextmanager
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
@@ -69,45 +70,98 @@ def wrap_list(lst):
             yield head
     return ",\n".join(", ".join(x) for x in split(lst, 20))
 
-class Transcriber (object):
-    """Helper class to facilitate rewriting source files.
 
-    This class takes care of the temporary file manipulation. Derived
-    classes need to implement transcribing of the content, with
+@contextmanager
+def AtomicRenameTemporaryFile(originalLocation: Path, *, prefix: str, dir: Path):
+    """Context manager for safe file update via a temporary file.
+
+    Accepts path to the file to be updated. Yields a temporary file to the user
+    code, open for writing.
+
+    On success closes the temporary file and moves its content to the original
+    location. On error, removes temporary file, without disturbing the original.
+    """
+    tempFile = NamedTemporaryFile('w', prefix=prefix, dir=dir, delete=False)
+    try:
+        yield tempFile
+        tempFile.close()
+        # Move the modified file to the original location
+        Path(tempFile.name).rename(originalLocation)
+    except Exception:
+        # delete the temporary file in case of error
+        tempFile.close()
+        Path(tempFile.name).unlink()
+        raise
+
+
+class Transcriber:
+    """Context manager base-class to manage source file rewrites.
+
+    Derived classes need to implement transcribing of the content, with
     whatever modifications they may want.  Members reader and writer
     are exposed; use writer.write() to output to the new file; use
     reader.readline() or iterate reader to read the original.
 
-    Callers should call close() on success or cleanup() on failure (to
-    clear away the temporary file).
+    This class is intended to be used as context manager only (inside a
+    `with` statement).
+
+    Reimplement onEnter() to write any preamble the file may have,
+    onExit() to write any tail. The body of the with statement takes
+    care of anything in between, using methods provided by derived classes.
+
+    The data is written to a temporary file first. The temporary file data
+    is then moved to the original location if there were no errors. Otherwise
+    the temporary file is removed and the original is left unchanged.
     """
     def __init__(self, path: Path, temp_dir: Path):
-        # Open the old file
-        self.reader = open(path)
         self.path = path
-        # Create a temp file to write the new data into
-        self.writer = NamedTemporaryFile('w', prefix=path.name, dir=temp_dir, delete=False)
+        self.tempDir = temp_dir
 
-    def close(self) -> None:
-        self.reader.close()
-        self.writer.close()
+    def onEnter(self) -> None:
+        """
+        Called before transferring control to user code.
 
-        # Move the modified file to the original location
-        self.path.unlink()
-        Path(self.writer.name).rename(self.path)
+        This function can be overridden in derived classes to perform actions
+        before transferring control to the user code.
 
-        self.reader = self.writer = None
+        The default implementation does nothing.
+        """
+        pass
 
-    def cleanup(self) -> None:
-        if self.reader:
-            self.reader.close()
-            self.reader = None
+    def onExit(self) -> None:
+        """
+        Called after return from user code.
 
-        if self.writer:
-            self.writer.close()
-            # Remove temp-file:
-            Path(self.writer.name).unlink(missing_ok=True)
-            self.writer = None
+        This function can be overridden in derived classes to perform actions
+        after successful return from user code.
+
+        The default implementation does nothing.
+        """
+        pass
+
+    def __enter__(self):
+        with ExitStack() as resources:
+            # Create a temp file to write the new data into
+            self.writer = resources.enter_context(
+                AtomicRenameTemporaryFile(self.path, prefix=self.path.name, dir=self.tempDir))
+            # Open the old file
+            self.reader = resources.enter_context(open(self.path))
+
+            self.onEnter()
+
+            # Prevent resources from being closed on normal return from this
+            # method and make them available inside __exit__():
+            self.__resources = resources.pop_all()
+            return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None:
+            with self.__resources:
+               self.onExit()
+        else:
+            self.__resources.__exit__(exc_type, exc_value, traceback)
+
+        return False
 
 
 class SourceFileEditor (Transcriber):
@@ -121,41 +175,27 @@ class SourceFileEditor (Transcriber):
     the new version to replace it.
 
     This class takes care of transcribing the parts before and after
-    the generated content; on creation, an instance will copy the
-    preamble up to the start marker; its close() will skip over the
-    original's generated content and resume transcribing with the end
-    marker. Derived classes need only implement the generation of the
-    content in between.
+    the generated content; on entering the context, an instance will
+    copy the preamble up to the start marker; on exit from the context
+    it will skip over the original's generated content and resume
+    transcribing with the end marker.
 
-    Callers should call close() on success or cleanup() on failure (to
-    clear away the temporary file); see Transcriber.
+    This class is only intended to be used as a context manager:
+    see Transcriber. Derived classes implement suitable methods for use in
+    the body of the with statement, using self.writer to rewrite the part
+    of the file between the start and end markers.
     """
-    def __init__(self, path: Path, temp_dir: Path):
-        """Set up the source file editor.
-
-        Requires two arguments: the path to the source file to be read
-        and, on success, replaced with a new version; and the
-        directory in which to store the temporary file during the
-        rewrite."""
-        super().__init__(path, temp_dir)
-        self.__copyPrelude()
-
-    def close(self):
-        self.__copyTail()
-        super().close()
-
-    # Implementation details:
     GENERATED_BLOCK_START = '// GENERATED PART STARTS HERE'
     GENERATED_BLOCK_END = '// GENERATED PART ENDS HERE'
 
-    def __copyPrelude(self):
+    def onEnter(self) -> None:
         # Copy over the first non-generated section to the new file
         for line in self.reader:
             self.writer.write(line)
             if line.strip() == self.GENERATED_BLOCK_START:
                 break
 
-    def __copyTail(self):
+    def onExit(self) -> None:
         # Skip through the old generated data in the old file
         for line in self.reader:
             if line.strip() == self.GENERATED_BLOCK_END:
