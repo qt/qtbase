23a20fb293c5b3276a6aca77d77a886f3414548d
Li Xinwei <1326710505@qq.com>
2021-07-23 15:37:14 +0800

    CMake: Use namespaced variables in Qt6*Dependencies.cmake
    Change-Id: I166988020cfd9750a4d58e519742215d0c03ad3e
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit bd594f945773f713ca493fd7b040fde8a12bdf87)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- cmake/QtPublicDependencyHelpers.cmake
+++ cmake/QtPublicDependencyHelpers.cmake
@@ -2,18 +2,18 @@
 # contain preformed dependencies. See foreach block for reference.
 # The same applies for find_dependency_path_list.
 macro(_qt_internal_find_dependencies target_dep_list find_dependency_path_list)
-    foreach(target_dep IN LISTS ${target_dep_list})
-        list(GET target_dep 0 pkg)
-        list(GET target_dep 1 version)
+    foreach(__qt_target_dep IN LISTS ${target_dep_list})
+        list(GET __qt_target_dep 0 __qt_pkg)
+        list(GET __qt_target_dep 1 __qt_version)
 
-        if (NOT ${pkg}_FOUND)
-            set(pkg_names ${pkg})
-            if(pkg MATCHES "(.*)Private$")
-                set(pkg_names "${CMAKE_MATCH_1};${pkg}")
+        if (NOT ${__qt_pkg}_FOUND)
+            set(__qt_pkg_names ${__qt_pkg})
+            if(__qt_pkg MATCHES "(.*)Private$")
+                set(__qt_pkg_names "${CMAKE_MATCH_1};${__qt_pkg}")
             endif()
-            find_dependency(${pkg} ${version}
+            find_dependency(${__qt_pkg} ${__qt_version}
                 NAMES
-                    ${pkg_names}
+                    ${__qt_pkg_names}
                 PATHS
                     ${${find_dependency_path_list}}
                     ${_qt_additional_packages_prefix_path}
