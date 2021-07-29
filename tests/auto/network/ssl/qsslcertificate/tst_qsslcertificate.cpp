a1fbfabc662f69f5c0c948519f28be92917a76c6
Timur Pocheptsov <timur.pocheptsov@qt.io>
2021-07-29 06:13:53 +0200

    tst_QSslCertificate::verify - skip auto-test
    as a temporary fix for suddenly expired certificates situation (to
    be regenerated).
    
    Task-number: QTBUG-95429
    Change-Id: I00ad11cfd8824eeeffa2991dfcda6a7899726953
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 8d0e4a2e5c3627e2fd88f14ccc8b349668012685)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/network/ssl/qsslcertificate/tst_qsslcertificate.cpp
+++ tests/auto/network/ssl/qsslcertificate/tst_qsslcertificate.cpp
@@ -1061,6 +1061,7 @@ void tst_QSslCertificate::verify()
 {
     if (isNonOpenSslTls)
         QSKIP("Not implemented in SecureTransport or Schannel");
+    QSKIP("Expired certificates, regenerate first");
 
     QList<QSslError> errors;
     QList<QSslCertificate> toVerify;
