qt/qtbase

commit 9906cc57ed3eed64d534f43c677bb16e08561bb6

testlib: Add BXPASS and BXFAIL
Prioritize blacklisting over QEXPECT_FAIL so that a test that is
blacklisted no longer fails if QEXPECT_FAIL returns true unexpectedly. To
reflect this state properly, the two values of BXPASS and BXFAIL were
added to testlib's output.

[ChangeLog][Important Behavior Changes][QtTestLib] Blacklisting of tests
will be taken into account for XPASS and XFAIL. A blacklisted test that
causes an XPASS will no longer be a fail.

Task-number: QTBUG-72928
Change-Id: Ia2232fdc714d405fa3fd9aea6c89eb2836bc5950
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

src/testlib/qtaptestlogger.cpp
@@ -123,13 +123,15 @@ void QTapTestLogger::addIncident(IncidentTypes type, const char *description,
         return;
     }
 
-    bool ok = type == Pass || type == XPass || type == BlacklistedPass;
+    bool ok = type == Pass || type == XPass || type == BlacklistedPass || type == BlacklistedXPass;
 
     QTestCharBuffer directive;
-    if (type == XFail || type == XPass || type == BlacklistedFail || type == BlacklistedPass)
+    if (type == XFail || type == XPass || type == BlacklistedFail || type == BlacklistedPass
+            || type == BlacklistedXFail || type == BlacklistedXPass) {
         // We treat expected or blacklisted failures/passes as TODO-failures/passes,
         // which should be treated as soft issues by consumers. Not all do though :/
         QTest::qt_asprintf(&directive, " # TODO %s", description);
+    }
 
     int testNumber = QTestLog::totalCount();
     if (type == XFail) {

src/testlib/qabstracttestlogger_p.h
@@ -69,7 +69,9 @@ public:
         Fail,
         XPass,
         BlacklistedPass,
-        BlacklistedFail
+        BlacklistedFail,
+        BlacklistedXPass,
+        BlacklistedXFail
     };
 
     enum MessageTypes {

src/testlib/qappletestlogger.cpp
@@ -101,6 +101,10 @@ static IncidentClassification incidentTypeToClassification(QAbstractTestLogger::
         return IncidentClassification(QtWarningMsg, "bpass");
     case QAbstractTestLogger::BlacklistedFail:
         return IncidentClassification(QtInfoMsg, "bfail");
+    case QAbstractTestLogger::BlacklistedXPass:
+        return IncidentClassification(QtWarningMsg, "bxpass");
+    case QAbstractTestLogger::BlacklistedXFail:
+        return IncidentClassification(QtInfoMsg, "bxfail");
     }
     return IncidentClassification(QtFatalMsg, nullptr);
 }

src/testlib/qplaintestlogger.cpp
@@ -89,6 +89,10 @@ namespace QTest {
             return "BPASS  ";
         case QAbstractTestLogger::BlacklistedFail:
             return "BFAIL  ";
+        case QAbstractTestLogger::BlacklistedXPass:
+            return "BXPASS ";
+        case QAbstractTestLogger::BlacklistedXFail:
+            return "BXFAIL ";
         }
         return "??????";
     }

src/testlib/qteamcitylogger.cpp
@@ -66,6 +66,10 @@ namespace QTest {
             return "BPASS";
         case QAbstractTestLogger::BlacklistedFail:
             return "BFAIL";
+        case QAbstractTestLogger::BlacklistedXPass:
+            return "BXPASS";
+        case QAbstractTestLogger::BlacklistedXFail:
+            return "BXFAIL";
         }
         return "??????";
     }

src/testlib/qtestlog.cpp
@@ -455,6 +455,24 @@ void QTestLog::addBFail(const char *msg, const char *file, int line)
     QTest::TestLoggers::addIncident(QAbstractTestLogger::BlacklistedFail, msg, file, line);
 }
 
+void QTestLog::addBXPass(const char *msg, const char *file, int line)
+{
+    QTEST_ASSERT(msg);
+    QTEST_ASSERT(file);
+
+    ++QTest::blacklists;
+
+    QTest::TestLoggers::addIncident(QAbstractTestLogger::BlacklistedXPass, msg, file, line);
+}
+
+void QTestLog::addBXFail(const char *msg, const char *file, int line)
+{
+    QTEST_ASSERT(msg);
+    QTEST_ASSERT(file);
+
+    QTest::TestLoggers::addIncident(QAbstractTestLogger::BlacklistedXFail, msg, file, line);
+}
+
 void QTestLog::addSkip(const char *msg, const char *file, int line)
 {
     QTEST_ASSERT(msg);

src/testlib/qtestlog_p.h
@@ -80,6 +80,8 @@ public:
     static void addXPass(const char *msg, const char *file, int line);
     static void addBPass(const char *msg);
     static void addBFail(const char *msg, const char *file, int line);
+    static void addBXPass(const char *msg, const char *file, int line);
+    static void addBXFail(const char *msg, const char *file, int line);
     static void addSkip(const char *msg, const char *file, int line);
     static void addBenchmarkResult(const QBenchmarkResult &result);
 

src/testlib/qtestresult.cpp
@@ -218,17 +218,24 @@ static bool checkStatement(bool statement, const char *msg, const char *file, in
 {
     if (statement) {
         if (QTest::expectFailMode) {
-            QTestLog::addXPass(msg, file, line);
+            if (QTest::blacklistCurrentTest)
+                QTestLog::addBXPass(msg, file, line);
+            else
+                QTestLog::addXPass(msg, file, line);
+
+            QTest::failed = true;
             bool doContinue = (QTest::expectFailMode == QTest::Continue);
             clearExpectFail();
-            QTest::failed = true;
             return doContinue;
         }
         return true;
     }
 
     if (QTest::expectFailMode) {
-        QTestLog::addXFail(QTest::expectFailComment, file, line);
+        if (QTest::blacklistCurrentTest)
+            QTestLog::addBXFail(QTest::expectFailComment, file, line);
+        else
+            QTestLog::addXFail(QTest::expectFailComment, file, line);
         bool doContinue = (QTest::expectFailMode == QTest::Continue);
         clearExpectFail();
         return doContinue;

commit c95d7741b74bc617d86297dbfa5cd7d1df98f8c3

testlib: Don't report additional 'pass' test point for blacklisted XFail
Regression after 9906cc57ed3eed64d534f43c677bb16e08561bb6.

Pick-to: 6.2
Change-Id: I5566f70c66d248426c7a41b6de1cfb92f104cc64
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

src/testlib/qtaptestlogger.cpp
@@ -115,7 +115,7 @@ void QTapTestLogger::outputTestLine(bool ok, int testNumber, QTestCharBuffer &di
 void QTapTestLogger::addIncident(IncidentTypes type, const char *description,
                                    const char *file, int line)
 {
-    if (m_wasExpectedFail && type == Pass) {
+    if (m_wasExpectedFail && (type == Pass || type == BlacklistedPass)) {
         // XFail comes with a corresponding Pass incident, but we only want
         // to emit a single test point for it, so skip the this pass.
         return;
@@ -132,7 +132,7 @@ void QTapTestLogger::addIncident(IncidentTypes type, const char *description,
     }
 
     int testNumber = QTestLog::totalCount();
-    if (type == XFail) {
+    if (type == XFail || type == BlacklistedXFail) {
         // The global test counter hasn't been updated yet for XFail
         testNumber += 1;
     }
@@ -238,7 +238,7 @@ void QTapTestLogger::addIncident(IncidentTypes type, const char *description,
         outputString(YAML_INDENT "...\n");
     }
 
-    m_wasExpectedFail = type == XFail;
+    m_wasExpectedFail = (type == XFail || type == BlacklistedXFail);
 }
 
 void QTapTestLogger::addMessage(MessageTypes type, const QString &message,

tests/auto/testlib/selftests/expected_blacklisted.tap
@@ -15,14 +15,13 @@ not ok 4 - fail() # TODO 'false' returned FALSE. (This test should BFAIL)
   file: qtbase/tests/auto/testlib/selftests/blacklisted/tst_blacklisted.cpp
   line: 0
   ...
-not ok 5 - xfail() # TODO This test should BXFAIL then BPASS
+not ok 6 - xfail() # TODO This test should BXFAIL then BPASS
   ---
   # This test should BXFAIL then BPASS
   at: tst_Blacklisted::xfail() (qtbase/tests/auto/testlib/selftests/blacklisted/tst_blacklisted.cpp:0)
   file: qtbase/tests/auto/testlib/selftests/blacklisted/tst_blacklisted.cpp
   line: 0
   ...
-ok 6 - xfail() # TODO 
 ok 7 - xpass() # TODO 'true' returned TRUE unexpectedly. (This test should BXPASS)
 # This is a warning that should not appear in silent test output
 # This is an internal testlib warning that should not appear in silent test output