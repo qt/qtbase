7d85fa29dc82e594486676fe87c86850729a9038
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2021-08-06 14:23:08 +0200

    testlib: Remove unused QTestCoreElement and QTestElementAttribute types
    The "documentation" has also been removed for all types, as it didn't
    add anything, and maintaining it to be in sync with the values just
    adds overhead.
    
    Change-Id: Iab1dd7b9804c05559b4674342d6059b68ecf3ecf
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit f0757d2fcdf78e1c0573d8b8e1b3aca88dde6ad1)
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- src/testlib/qtestelementattribute.cpp
+++ src/testlib/qtestelementattribute.cpp
@@ -44,72 +44,6 @@
 
 QT_BEGIN_NAMESPACE
 
-/*! \enum QTest::AttributeIndex
-  This enum numbers the different tests.
-
-  \value AI_Undefined
-
-  \value AI_Name
-
-  \value AI_Result
-
-  \value AI_Tests
-
-  \value AI_Failures
-
-  \value AI_Errors
-
-  \value AI_Type
-
-  \value AI_Message
-
-  \value AI_PropertyValue
-
-  \value AI_QTestVersion
-
-  \value AI_QtVersion
-
-  \value AI_File
-
-  \value AI_Line
-
-  \value AI_Metric
-
-  \value AI_Tag
-
-  \value AI_Value
-
-  \value AI_Iterations
-
-  \value AI_Time
-
-  \value AI_Timestamp
-*/
-
-/*! \enum QTest::LogElementType
-  The enum specifies the kinds of test log messages.
-
-  \value LET_Undefined
-
-  \value LET_Property
-
-  \value LET_Properties
-
-  \value LET_Failure
-
-  \value LET_Message
-
-  \value LET_TestCase
-
-  \value LET_TestSuite
-
-  \value LET_Benchmark
-
-  \value LET_SystemError
-
-  \value LET_SystemOutput
-*/
-
 QTestElementAttribute::QTestElementAttribute() = default;
 
 QTestElementAttribute::~QTestElementAttribute()
@@ -127,21 +61,13 @@ const char *QTestElementAttribute::name() const
     const char *AttributeNames[] =
     {
         "name",
-        "result",
         "tests",
         "failures",
         "errors",
         "type",
         "message",
         "value",
-        "qtestversion",
-        "qtversion",
-        "file",
-        "line",
-        "metric",
-        "tag",
         "value",
-        "iterations",
         "time",
         "timestamp",
         "hostname",
