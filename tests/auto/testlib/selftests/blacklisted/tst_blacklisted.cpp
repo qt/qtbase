1bdc35bfe05ac5385278d95a052ac51c1bc7bba6
Edward Welbourne <edward.welbourne@qt.io>
2021-08-16 16:56:04 +0200

    Add more tests of blacklisting, combined with XPASS and XFAIL
    Include counting of test types, to catch two more cases where totals
    don't add up.
    
    Task-number: QTBUG-95661
    Change-Id: I9fe5424bc6652c61a065bf2889333e2ed9437c81
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- tests/auto/testlib/selftests/blacklisted/tst_blacklisted.cpp
+++ tests/auto/testlib/selftests/blacklisted/tst_blacklisted.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2021 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the test suite of the Qt Toolkit.
@@ -35,40 +35,103 @@ class tst_Blacklisted : public QObject
     Q_OBJECT
 
 private slots:
+    void init();
+    void cleanupTestCase();
     void pass();
     void skip();
     void fail();
     void xfail();
+    void xfailContinueSkip();
+    void xfailContinueFail();
     void xpass();
+    void xpassContinueSkip();
+    void xpassContinueFail();
+
+private:
+    int casesTested = 2;
+    // What the totals line's numbers *should* be:
+    int passed = 2, skipped = 0, blacklisted = 0;
+    // Total and passed get {init,cleanup}TestCase() in addition to the actual tests.
 };
+
+void tst_Blacklisted::init()
+{
+    ++casesTested;
+}
+
+void tst_Blacklisted::cleanupTestCase()
+{
+    qDebug("Totals should add up to %d: %d passed, 0 failed, %d skipped, %d blacklisted",
+           casesTested, passed, skipped, blacklisted);
+}
 
 // All the tests below have been blacklisted in blacklisted/BLACKLIST
 
 void tst_Blacklisted::pass()
 {
+    ++blacklisted;
+    qDebug("This test should BPASS");
     QVERIFY(true);
 }
 
 void tst_Blacklisted::skip()
 {
+    ++skipped;
     QSKIP("This test should SKIP");
 }
 
 void tst_Blacklisted::fail()
 {
+    ++blacklisted;
     QVERIFY2(false, "This test should BFAIL");
 }
 
 void tst_Blacklisted::xfail()
 {
+    ++blacklisted;
     QEXPECT_FAIL("", "This test should BXFAIL then BPASS", Abort);
     QVERIFY(false);
 }
 
+void tst_Blacklisted::xfailContinueSkip()
+{
+    ++skipped;
+    QEXPECT_FAIL("", "This test should BXFAIL then SKIP", Continue);
+    QVERIFY(false);
+    QSKIP("This skip should be seen and counted");
+}
+
+void tst_Blacklisted::xfailContinueFail()
+{
+    ++blacklisted;
+    QEXPECT_FAIL("", "This test should BXFAIL then BFAIL", Continue);
+    QVERIFY(false);
+    QFAIL("This fail should be seen and counted as blacklisted");
+}
+
 void tst_Blacklisted::xpass()
 {
+    ++blacklisted;
     QEXPECT_FAIL("", "This test should BXPASS", Abort);
     QVERIFY2(true, "This test should BXPASS");
+}
+
+void tst_Blacklisted::xpassContinueSkip()
+{
+    ++blacklisted;
+    QEXPECT_FAIL("", "This test should BXPASS then SKIP", Continue);
+    QVERIFY2(true, "This test should BXPASS then SKIP");
+    // FIXME QTBUG-95661: skip gets counted
+    QSKIP("This skip should be seen but not counted");
+}
+
+void tst_Blacklisted::xpassContinueFail()
+{
+    ++blacklisted;
+    QEXPECT_FAIL("", "This test should BXPASS then BFAIL", Continue);
+    QVERIFY2(true, "This test should BXPASS then BFAIL");
+    // FIXME QTBUG-95661: gets double-counted
+    QFAIL("This fail should be seen and not counted (due to prior XPASS)");
 }
 
 QTEST_MAIN(tst_Blacklisted)
