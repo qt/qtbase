a2abb0145174a8ed82572a06e537f550a6777b08
Luca Di Sera <luca.disera@qt.io>
2021-08-24 09:27:19 +0200

    Doc: Replace the example for QFileInfo::setFile
    The example in the documentation of `QFileInfo::setFile` made no use
    of `setFile` and only showed a use of `QDir::setCurrent`.
    
    The example was replaced with a new example showing how `setFile`
    changes the file that the information are retrieved from.
    
    The old example was moved under the documentation for
    `QDir::setCurrent` as it shows its working.
    
    Fixes: QTBUG-87128
    Pick-to: 6.2 6.1 5.15
    Change-Id: I8227876cfcb4d582040bda9b4b7f3f7debea1e07
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/corelib/io/qdir.cpp
+++ src/corelib/io/qdir.cpp
@@ -1942,6 +1942,8 @@ QFileInfoList QDir::drives()
     Returns \c true if the directory was successfully changed; otherwise
     returns \c false.
 
+    \snippet code/src_corelib_io_qdir.cpp 16
+
     \sa current(), currentPath(), home(), root(), temp()
 */
 bool QDir::setCurrent(const QString &path)
