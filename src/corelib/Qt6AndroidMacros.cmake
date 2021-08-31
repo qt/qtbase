daf64298ca6cae1243b7756d3b82ea24cbfee9cd
Craig Scott <craig.scott@qt.io>
2021-08-31 15:56:00 +1000

    Revert "Ensure versionless wrappers do not introduce a new variable scope"
    This reverts commit 08180e76e6116f0ef66476ca00b2b676b3aa50da.
    
    Macros add another level of escaping that functions do not. The
    conversion of the versionless wrappers to macros may alter the
    behavior, so revert that change.
    
    Task-number: QTBUG-96219
    Change-Id: Ic5dcff3081123d957888584ba1d76ae0580d9083
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit a37789e4931a85fce4fba9c9612ebb4020572d54)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/Qt6AndroidMacros.cmake
+++ src/corelib/Qt6AndroidMacros.cmake
@@ -262,9 +262,9 @@ function(qt6_android_generate_deployment_settings target)
 endfunction()
 
 if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
-    macro(qt_android_generate_deployment_settings)
+    function(qt_android_generate_deployment_settings)
         qt6_android_generate_deployment_settings(${ARGV})
-    endmacro()
+    endfunction()
 endif()
 
 function(qt6_android_apply_arch_suffix target)
@@ -277,9 +277,9 @@ function(qt6_android_apply_arch_suffix target)
 endfunction()
 
 if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
-    macro(qt_android_apply_arch_suffix)
+    function(qt_android_apply_arch_suffix)
         qt6_android_apply_arch_suffix(${ARGV})
-    endmacro()
+    endfunction()
 endif()
 
 # Add custom target to package the APK
@@ -397,7 +397,7 @@ function(_qt_internal_create_global_apk_all_target_if_needed)
 endfunction()
 
 if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
-    macro(qt_android_add_apk_target)
+    function(qt_android_add_apk_target)
         qt6_android_add_apk_target(${ARGV})
-    endmacro()
+    endfunction()
 endif()
