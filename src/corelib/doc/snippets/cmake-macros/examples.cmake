7b4219989a4f23e326254e0e4c9b7fe8bf10b4c2
Craig Scott <craig.scott@qt.io>
2021-09-08 18:00:55 +1000

    Remove unused qt5_... CMake command snippets
    Task-number: QTBUG-95712
    Change-Id: I92535fd96937638bb8997a63455ba5152055b72a
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 2ad35d133a8d71d4ce21e25df7b12b70cc2f8151)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/doc/snippets/cmake-macros/examples.cmake
+++ src/corelib/doc/snippets/cmake-macros/examples.cmake
@@ -1,20 +1,8 @@
-#! [qt5_wrap_cpp]
-set(SOURCES myapp.cpp main.cpp)
-qt5_wrap_cpp(SOURCES myapp.h)
-add_executable(myapp ${SOURCES})
-#! [qt5_wrap_cpp]
-
 #! [qt_wrap_cpp]
 set(SOURCES myapp.cpp main.cpp)
 qt_wrap_cpp(SOURCES myapp.h)
 add_executable(myapp ${SOURCES})
 #! [qt_wrap_cpp]
-
-#! [qt5_add_resources]
-set(SOURCES main.cpp)
-qt5_add_resources(SOURCES example.qrc)
-add_executable(myapp ${SOURCES})
-#! [qt5_add_resources]
 
 #! [qt_add_resources]
 set(SOURCES main.cpp)
@@ -29,46 +17,20 @@ qt_add_resources(myapp "images"
     FILES image1.png image2.png)
 #! [qt_add_resources_target]
 
-#! [qt5_add_big_resources]
-set(SOURCES main.cpp)
-qt5_add_big_resources(SOURCES big_resource.qrc)
-add_executable(myapp ${SOURCES})
-#! [qt5_add_big_resources]
-
 #! [qt_add_big_resources]
 set(SOURCES main.cpp)
 qt_add_big_resources(SOURCES big_resource.qrc)
 add_executable(myapp ${SOURCES})
 #! [qt_add_big_resources]
 
-#! [qt5_add_binary_resources]
-qt5_add_binary_resources(resources project.qrc OPTIONS -no-compress)
-add_dependencies(myapp resources)
-#! [qt5_add_binary_resources]
-
 #! [qt_add_binary_resources]
 qt_add_binary_resources(resources project.qrc OPTIONS -no-compress)
 add_dependencies(myapp resources)
 #! [qt_add_binary_resources]
 
-#! [qt5_generate_moc]
-qt5_generate_moc(main.cpp main.moc TARGET myapp)
-#! [qt5_generate_moc]
-
 #! [qt_generate_moc]
 qt_generate_moc(main.cpp main.moc TARGET myapp)
 #! [qt_generate_moc]
-
-#! [qt5_import_plugins]
-add_executable(myapp main.cpp)
-target_link_libraries(myapp Qt5::Gui Qt5::Sql)
-qt5_import_plugins(myapp
-    INCLUDE Qt5::QCocoaIntegrationPlugin
-    EXCLUDE Qt5::QMinimalIntegrationPlugin
-    INCLUDE_BY_TYPE imageformats Qt5::QGifPlugin Qt5::QJpegPlugin
-    EXCLUDE_BY_TYPE sqldrivers
-)
-#! [qt5_import_plugins]
 
 #! [qt_import_plugins]
 add_executable(myapp main.cpp)
