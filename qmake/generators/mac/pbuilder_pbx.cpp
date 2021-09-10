098883d0689de39ce79ebaa49f160d39b6adea73
Joerg Bornemann <joerg.bornemann@qt.io>
2021-09-10 10:01:31 +0200

    qmake/xcode: Do not create OBJECTS_DIR
    If we create OBJECTS_DIR at qmake time, Xcode will not consider this
    directory as created by the build system, and "xcodebuild --clean" will
    fail.
    
    Prevent qmake from creating that directory in the Xcode generator.
    
    Fixes: QTBUG-96305
    Change-Id: I874bf34a4289ce5f2d3e2ce070ffbe56d5368861
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit b5a23e88be0dd8cb6c66010c92c495c20c455325)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- qmake/generators/mac/pbuilder_pbx.cpp
+++ qmake/generators/mac/pbuilder_pbx.cpp
@@ -2073,4 +2073,10 @@ ProjectBuilderMakefileGenerator::writeSettings(const QString &var, const ProStri
     return ret;
 }
 
+bool
+ProjectBuilderMakefileGenerator::inhibitMakeDirOutPath(const ProKey &path) const
+{
+    return path == "OBJECTS_DIR";
+}
+
 QT_END_NAMESPACE
