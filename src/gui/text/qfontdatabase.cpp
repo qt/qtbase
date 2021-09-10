e0ad2cee55193696285cc84bf5c4922bb7247e9a
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2021-09-10 14:31:12 +0200

    Fix querying font aliases that share name with other fonts
    a332f3fabc29f796526202648eddf35a24f1cb67 disabled resolving all fonts
    on the system for every font lookup, which was a significant startup
    time improvement. But it also caused a regression: When a font has
    an alias which shares the name of a proper font, then this would
    not be resolved correctly.
    
    This is fairly typical on Windows/GDI due to backwards-compatibility.
    Instead of being collected under a shared typographical family, fonts
    are disambiguated by adding the style name to the family name. The
    proper typographical name is still available, but this is not
    enumerated by the system.
    
    So "Segoe UI" for instance, will be available as "Segoe UI",
    "Segoe UI Light", "Segoe UI Bold" etc.
    
    When we populate family aliases, we register that "Segoe UI Light"
    is actually "Segoe UI" with Light weight, and prior to
    a332f3fabc29f796526202648eddf35a24f1cb67 this would be done implicitly.
    
    But after the optimization, we would only populate family aliases once
    we stumbled over a font request for a non-existent font. For "Segoe UI",
    we would simply return the regular weight font as the best imperfect
    match.
    
    The fix is to populate font family aliases not only when the family is
    non-existent, but when the match is imperfect, e.g. if we are asking
    for a Light weight font and only finding a regular one. User code can
    still avoid this somewhat expensive operation by using the full
    family names on Windows.
    
    This also requires a fix to a test. When removeApplicationFont() is
    called, we invalidate the font database, so it will be reset to a state
    that does not contain the family aliases. Therefore we cannot guarantee
    that it is identical to what it was before the test started, since this
    depends on what has happened previously in the application.
    
    [ChangeLog][QtGui][Text] Fixed an issue where some font styles and weights
    would not be selectable. This was especially noticeable on Windows.
    
    Pick-to: 5.15 6.1 6.2
    Fixes: QTBUG-94835
    Change-Id: I892855edd1c8e3d3734aace396f6000d897d2ec4
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- src/gui/text/qfontdatabase.cpp
+++ src/gui/text/qfontdatabase.cpp
@@ -1051,9 +1051,13 @@ static bool matchFamilyName(const QString &familyName, QtFontFamily *f)
 
     Tries to find the best match for a given request and family/foundry
 */
-static int match(int script, const QFontDef &request,
-                 const QString &family_name, const QString &foundry_name,
-                 QtFontDesc *desc, const QList<int> &blacklistedFamilies)
+static int match(int script,
+                 const QFontDef &request,
+                 const QString &family_name,
+                 const QString &foundry_name,
+                 QtFontDesc *desc,
+                 const QList<int> &blacklistedFamilies,
+                 unsigned int *resultingScore = nullptr)
 {
     int result = -1;
 
@@ -1126,6 +1130,10 @@ static int match(int script, const QFontDef &request,
         if (newscore < 10) // xlfd instead of FT... just accept it
             break;
     }
+
+    if (resultingScore != nullptr)
+        *resultingScore = score;
+
     return result;
 }
 
@@ -2394,8 +2402,9 @@ QFontEngine *QFontDatabasePrivate::findFont(const QFontDef &request,
     parseFontName(requestFamily, foundry_name, family_name);
     QtFontDesc desc;
     QList<int> blackListed;
-    int index = match(multi ? QChar::Script_Common : script, request, family_name, foundry_name, &desc, blackListed);
-    if (index < 0 && QGuiApplicationPrivate::platformIntegration()->fontDatabase()->populateFamilyAliases(family_name)) {
+    unsigned int score = UINT_MAX;
+    int index = match(multi ? QChar::Script_Common : script, request, family_name, foundry_name, &desc, blackListed, &score);
+    if (score > 0 && QGuiApplicationPrivate::platformIntegration()->fontDatabase()->populateFamilyAliases(family_name)) {
         // We populated familiy aliases (e.g. localized families), so try again
         index = match(multi ? QChar::Script_Common : script, request, family_name, foundry_name, &desc, blackListed);
     }
