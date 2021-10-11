1e763219a8d7309ffb13f3fb1d19505a0239b8af
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2021-10-11 12:25:03 +0200

    freetype/no-fc: Disambiguate fonts with different widths
    When using the Freetype font database, we would ignore the
    typographical width of loaded fonts, which could make it
    impossible to select certain fonts in a family.
    
    Also contains 518790af79df339143820cd9d34a74c00db1ed7e and
    cde562cf6d65a3ad64fa6afd6014be74febdf912 squashed in.
    
    [ChangeLog][Freetype] Fixed a bug where fonts of different
    width within the same family would be unselectable if the
    Freetype font database (no-fontconfig configuration) was in
    use.
    
    Fixes: QTBUG-89640
    Change-Id: Ic9c17a7c3cc14eb7e942b05d6b21b7c1602816cf
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit e05e3c776256d35798f451f31cd71e809786ed78)
    (cherry picked from commit 518790af79df339143820cd9d34a74c00db1ed7e)
    (cherry picked from commit cde562cf6d65a3ad64fa6afd6014be74febdf912)

--- tests/auto/gui/text/qfontdatabase/tst_qfontdatabase.cpp
+++ tests/auto/gui/text/qfontdatabase/tst_qfontdatabase.cpp
@@ -33,6 +33,8 @@
 #include <qfontmetrics.h>
 #include <qtextlayout.h>
 #include <private/qrawfont_p.h>
+#include <private/qfont_p.h>
+#include <private/qfontengine_p.h>
 #include <qpa/qplatformfontdatabase.h>
 
 Q_LOGGING_CATEGORY(lcTests, "qt.text.tests")
@@ -411,7 +413,11 @@ void tst_QFontDatabase::condensedFontMatching()
     tfcByStyleName.setStyleName("Condensed");
 
 #ifdef Q_OS_WIN
-    QEXPECT_FAIL("","No matching of sub-family by stretch on Windows", Continue);
+    QFont f;
+    f.setStyleStrategy(QFont::NoFontMerging);
+    QFontPrivate *font_d = QFontPrivate::get(f);
+    if (font_d->engineForScript(QChar::Script_Common)->type() != QFontEngine::Freetype)
+        QEXPECT_FAIL("","No matching of sub-family by stretch on Windows", Continue);
 #endif
 
 #ifdef Q_OS_ANDROID
