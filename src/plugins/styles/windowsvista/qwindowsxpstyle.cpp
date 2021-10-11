30b63edd35390721c356dba89d6063b71a44f25f
Jonas Kvinge <jonas@jkvinge.net>
2021-10-11 18:55:28 +0200

    qwindowsxpstyle: Fix typos in documentation
    Change-Id: I5dc4649783b0d4c2b17e0555b98874b83a5b9f30
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 84d0d089a8e4b63257e2f11b9b00e3e42953ae81)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/styles/windowsvista/qwindowsxpstyle.cpp
+++ src/plugins/styles/windowsvista/qwindowsxpstyle.cpp
@@ -325,7 +325,7 @@ void QWindowsXPStylePrivate::cleanupVistaTreeViewTheming()
 
 /* \internal
     Closes all open theme data handles to ensure that we don't leak
-    resources, and that we don't refere to old handles when for
+    resources, and that we don't refer to old handles when for
     example the user changes the theme style.
 */
 void QWindowsXPStylePrivate::cleanupHandleMap()
@@ -572,7 +572,7 @@ bool QWindowsXPStylePrivate::hasAlphaChannel(const QRect &rect)
     The rule of thumb for premultiplied pixmaps is that the color
     values of a pixel can never be higher than the alpha values, so
     we use this to our advantage here, and fix all instances where
-    this occures.
+    this occurs.
 */
 bool QWindowsXPStylePrivate::fixAlphaChannel(const QRect &rect)
 {
@@ -600,7 +600,7 @@ bool QWindowsXPStylePrivate::fixAlphaChannel(const QRect &rect)
     Swaps the alpha values on certain pixels:
         0xFF?????? -> 0x00??????
         0x00?????? -> 0xFF??????
-    Used to determin the mask of a non-alpha transparent pixmap in
+    Used to determine the mask of a non-alpha transparent pixmap in
     the native doublebuffer, and swap the alphas so we may paint
     the image as a Premultiplied QImage with drawImage(), and obtain
     the mask transparency.
@@ -784,7 +784,7 @@ bool QWindowsXPStylePrivate::drawBackgroundDirectly(HDC dc, XPThemeData &themeDa
     It should only be used when the painteengine doesn't provide a proper
     HDC for direct painting (e.g. when doing a grabWidget(), painting to
     other pixmaps etc), or when special transformations are needed (e.g.
-    flips (horizonal mirroring only, vertical are handled by the theme
+    flips (horizontal mirroring only, vertical are handled by the theme
     engine).
 
     \a correctionFactor is an additional factor used to scale up controls
@@ -3957,7 +3957,7 @@ static QList<PropPair> all_props;
     directly.
     Since we cannot rely on the pixel data we get from Microsoft
     when drawing into the DIB section, we use this function to
-    see the actual data we got, and can determin the appropriate
+    see the actual data we got, and can determine the appropriate
     action.
 */
 void QWindowsXPStylePrivate::dumpNativeDIB(int w, int h)
