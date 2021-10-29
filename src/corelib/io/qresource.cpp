97fc812b325054bda60fce7856790ddb364cd9a8
Ivan Tkachenko <me@ratijas.tk>
2021-10-29 16:22:32 +0300

    Doc: Fix broken formatting
    Amends f25bc30d8d9d13fffd34213dfbf5e7373a18222a
    
    Change-Id: Ia42c7639e4919de3f995d771bfc7d8237a8773d8
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit a670839c61b6e7957bfacab0b51990f9ad37ffe1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/io/qresource.cpp
+++ src/corelib/io/qresource.cpp
@@ -635,7 +635,7 @@ bool QResource::isValid() const
     possible compression algorithm.
 
     If this function returns QResource::ZstdCompression, you need to use the
-    Zstandard library functions (\c{<zstd.h> header). Qt does not provide a
+    Zstandard library functions (\c{<zstd.h>} header). Qt does not provide a
     wrapper.
 
     See \l{http://facebook.github.io/zstd/zstd_manual.html}{Zstandard manual}.
