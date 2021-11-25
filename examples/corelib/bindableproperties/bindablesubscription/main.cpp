cee89e70a6011c3fcae29ad95d5fec4b2026d055
Sona Kurazyan <sona.kurazyan@qt.io>
2021-11-25 11:08:01 +0100

    Add example showing the benefits of using bindable properties
    Added two examples for modeling a subscription service: signal/slot
    connection-based and bindable property-based. The examples are based
    on the example from Fabian's Qt WS talk about the bindable properties.
    
    Pick-to: 6.2
    Task-number: QTBUG-97655
    Change-Id: I0345913b8b6e5c40b8477e128d36483598bdfcaa
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

---
+++ examples/corelib/bindableproperties/bindablesubscription/main.cpp
@@ -0,0 +1,116 @@
+/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "../shared/subscriptionwindow.h"
+#include "bindablesubscription.h"
+#include "bindableuser.h"
+
+#include <QApplication>
+#include <QButtonGroup>
+#include <QLabel>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QSpinBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    BindableUser user;
+    BindableSubscription subscription(&user);
+
+    SubscriptionWindow w;
+
+    // Initialize subscription data
+    QRadioButton *monthly = w.findChild<QRadioButton *>("btnMonthly");
+    QObject::connect(monthly, &QRadioButton::clicked, [&] {
+        subscription.setDuration(BindableSubscription::Monthly);
+    });
+    QRadioButton *quarterly = w.findChild<QRadioButton *>("btnQuarterly");
+    QObject::connect(quarterly, &QRadioButton::clicked, [&] {
+        subscription.setDuration(BindableSubscription::Quarterly);
+    });
+    QRadioButton *yearly = w.findChild<QRadioButton *>("btnYearly");
+    QObject::connect(yearly, &QRadioButton::clicked, [&] {
+        subscription.setDuration(BindableSubscription::Yearly);
+    });
+
+    // Initialize user data
+    QPushButton *germany = w.findChild<QPushButton *>("btnGermany");
+    QObject::connect(germany, &QPushButton::clicked, [&] {
+        user.setCountry(BindableUser::Germany);
+    });
+    QPushButton *finland = w.findChild<QPushButton *>("btnFinland");
+    QObject::connect(finland, &QPushButton::clicked, [&] {
+        user.setCountry(BindableUser::Finland);
+    });
+    QPushButton *norway = w.findChild<QPushButton *>("btnNorway");
+    QObject::connect(norway, &QPushButton::clicked, [&] {
+        user.setCountry(BindableUser::Norway);
+    });
+
+    QSpinBox *ageSpinBox = w.findChild<QSpinBox *>("ageSpinBox");
+    QObject::connect(ageSpinBox, &QSpinBox::valueChanged, [&](int value) {
+        user.setAge(value);
+    });
+
+    QLabel *priceDisplay = w.findChild<QLabel *>("priceDisplay");
+
+    // Track price changes
+    auto priceChangeHandler = subscription.bindablePrice().subscribe([&] {
+        priceDisplay->setText(QString::number(subscription.price()));
+    });
+
+    auto priceValidHandler = subscription.bindableIsValid().subscribe([&] {
+        priceDisplay->setEnabled(subscription.isValid());
+    });
+
+    w.show();
+    return a.exec();
+}
