20783c417020dcda5b9e7f2f9263bebbdb2752db
Joerg Bornemann <joerg.bornemann@qt.io>
2021-11-25 22:01:22 +0100

    CMake: Fix cross-compilation to Windows
    ...and potentially to macOS.
    
    This amends 3f56950862181f4d50f30d66f577c933795522c3.
    
    Change-Id: Idf2acded37b59994459bc80ecda5733232f721f6
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/tools/configure.cmake
+++ src/tools/configure.cmake
@@ -8,12 +8,14 @@ qt_feature("macdeployqt" PRIVATE
     SECTION "Deployment"
     LABEL "macOS deployment tool"
     PURPOSE "The Mac deployment tool automates the process of creating a deployable application bundle that contains the Qt libraries as private frameworks."
+    AUTODETECT CMAKE_HOST_APPLE
     CONDITION MACOS)
 
 qt_feature("windeployqt" PRIVATE
     SECTION "Deployment"
     LABEL "Windows deployment tool"
     PURPOSE "The Windows deployment tool is designed to automate the process of creating a deployable folder containing the Qt-related dependencies (libraries, QML imports, plugins, and translations) required to run the application from that folder. It creates a sandbox for Universal Windows Platform (UWP) or an installation tree for Windows desktop applications, which can be easily bundled into an installation package."
+    AUTODETECT CMAKE_HOST_WIN32
     CONDITION WIN32)
 
 qt_feature("qmake" PRIVATE
