66a79287f159d236090fd0092a81be133862bbdc
Marc Mutz <marc.mutz@qt.io>
2021-12-21 14:11:38 +0100

    [doc] QCoreApplication::installTranslator() doesn't take ownership
    Document it.
    
    Pick-to: 6.3 6.2 5.15
    Change-Id: I25d305945bf29348d6ea5756fbf80c418f812d0f
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/corelib/kernel/qcoreapplication.cpp
+++ src/corelib/kernel/qcoreapplication.cpp
@@ -2113,6 +2113,8 @@ void QCoreApplicationPrivate::quit()
 
     The function returns \c true on success and false on failure.
 
+    \note QCoreApplication does \e not take ownership of \a translationFile.
+
     \sa removeTranslator(), translate(), QTranslator::load(), {Dynamic Translation}
 */
 
