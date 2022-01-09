cbc19427e2a9f3ee221575e8f900daeefb1fe2b8
Thiago Macieira <thiago.macieira@intel.com>
2022-01-09 08:07:23 -0800

    QVariant: fix incorrect warning about duplicated alias where there isn't
    We were always matching a type name to itself because we forgot to
    advance the iterator after we did match. The issue was introduced in
    commit 46dc8e453ae1d0c1eb749cfebe686995f3a6cfd0.
    
    Fixes: QTBUG-99620
    Task-number: QTBUG-96916
    Change-Id: Ib42b3adc93bf4d43bd55fffd16c8a5df6306e404
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: Jonas Kvinge <jonas@jkvinge.net>
    (cherry picked from commit eabb3fd2f03f81f738a6128b7b09d92f0731522c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/kernel/qmetatype.cpp
+++ src/corelib/kernel/qmetatype.cpp
@@ -202,6 +202,7 @@ const char *QtMetaTypePrivate::typedefNameForType(const QtPrivate::QMetaTypeInte
         if (it.key() == officialName)
             continue;               // skip the official name
         name = it.key().constData();
+        ++it;
         break;
     }
 
