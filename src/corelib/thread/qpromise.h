a8794503ebdefec53a7a42479c477d04f0efa067
Sona Kurazyan <sona.kurazyan@qt.io>
2022-01-17 14:45:32 +0100

    Fix memory leaks when capturing a QFuture in its continuation
    Capturing a QFuture in the continuations attached to it results in
    memory leaks. QFuture's ref-counted data can only be deleted when the
    last copy referencing the data gets deleted. The saved continuation
    that keeps a copy of the future (as in case of the lambda capture) will
    prevent the data from being deleted. So we need to manually clean the
    continuation after it is run. But this doesn't solve the problem if the
    continuation isn't run. In that case, clean the continuation in the
    destructor of the associated QPromise.
    
    To avoid similar leaks, internally we should always create futures via
    QPromise, instead of the ref-counted QFutureInterface, so that the
    continuation is always cleaned in the destructor. Currently QFuture
    continuations and QtFuture::when* methods use QFutureInterface directly,
    which will be fixed by the follow-up commits.
    
    Fixes: QTBUG-99534
    Pick-to: 6.3 6.2
    Change-Id: Ic13e7dffd8cb25bd6b87e5416fe4d1a97af74c9b
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- src/corelib/thread/qpromise.h
+++ src/corelib/thread/qpromise.h
@@ -66,12 +66,16 @@ public:
     {
         const int state = d.loadState();
         // If QFutureInterface has no state, there is nothing to be done
-        if (state == static_cast<int>(QFutureInterfaceBase::State::NoState))
+        if (state == static_cast<int>(QFutureInterfaceBase::State::NoState)) {
+            d.cleanContinuation();
             return;
+        }
         // Otherwise, if computation is not finished at this point, cancel
         // potential waits
-        if (!(state & QFutureInterfaceBase::State::Finished))
+        if (!(state & QFutureInterfaceBase::State::Finished)) {
             d.cancelAndFinish(); // cancel and finalize the state
+            d.cleanContinuation();
+        }
     }
 
     // Core QPromise APIs
