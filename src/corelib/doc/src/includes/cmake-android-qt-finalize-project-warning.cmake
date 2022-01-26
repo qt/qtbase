87bcb9ca6ec883afb86165a282557014c9f06e41
Alexey Edelev <alexey.edelev@qt.io>
2022-01-26 12:26:06 +0100

    Add documentation for qt_finalize_project command
    Task-number: QTBUG-100207
    Change-Id: I902ae827efad422771494ae3ad2da48d48d5c9cb
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 63f79d7ffb22de89a2c6314caa5b5a1908590063)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/corelib/doc/src/includes/cmake-android-qt-finalize-project-warning.cmake
@@ -0,0 +1,3 @@
+\warning If your \e Android project is built using a CMake version lower than
+3.19, make sure that you call \l{qt_finalize_project}{qt6_finalize_project()} at
+the end of a top-level CMakeLists.txt.
