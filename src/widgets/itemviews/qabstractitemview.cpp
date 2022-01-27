2d4dccc1e10fd79f8c5517fe9b09129b4008f12d
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2022-01-27 09:47:59 +0100

    QAbstractItemView: do not access invalid model indices (2/N)
    Similar to the parent patch, the private selectAll() was doing
    two out of bounds accesses on an empty model. Guard it.
    
    Change-Id: If0f3ce1e6c44a152791313e47db79985e71ef955
    Reviewed-by: David Faure <david.faure@kdab.com>
    (cherry picked from commit 035babe5020cfeafdaf5e8590f4c5c3ac043a5bf)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/itemviews/qabstractitemview.cpp
+++ src/widgets/itemviews/qabstractitemview.cpp
@@ -4622,6 +4622,8 @@ void QAbstractItemViewPrivate::selectAll(QItemSelectionModel::SelectionFlags com
 {
     if (!selectionModel)
         return;
+    if (!model->hasChildren(root))
+        return;
 
     QItemSelection selection;
     QModelIndex tl = model->index(0, 0, root);
