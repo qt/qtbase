a5fa1ee82246a660be1e3e2026d766e03b051391
Sona Kurazyan <sona.kurazyan@qt.io>
2022-01-28 14:31:46 +0100

    QtFuture::connect: fix for signals with a single std::tuple argument
    If the signal passed to QtFuture::connect() takes multiple arguments,
    we need to wrap the arguments in a std::tuple when reporting the result.
    To detect this case we were checking if the result type of a QFuture
    returned by QtFuture::connect() is a std::tuple, but this was not
    correct: the result type could be a std::tuple also if the passed
    signal takes a single std::tuple argument. Instead, check if the signal
    takes more than one argument.
    
    As a drive-by modified the tst_QFuture::signalConnect to use const
    values for tuples used in multiple test-cases, to avoid repetition.
    
    Fixes: QTBUG-100071
    Change-Id: I1ce39cf87028f36ef94a9d1a4423b0c51473afd4
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit f2f5c7d2b71a93ff826e3731cbb80febe5c7b308)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/thread/qfuture_impl.h
+++ src/corelib/thread/qfuture_impl.h
@@ -279,17 +279,6 @@ template<class Class, class Callable>
 using EnableIfInvocable = std::enable_if_t<
         QtPrivate::ArgResolver<Callable>::template CanInvokeWithArgs<Class, Callable>>;
 
-template<class>
-struct isTuple : std::false_type
-{
-};
-template<class... T>
-struct isTuple<std::tuple<T...>> : std::true_type
-{
-};
-template<class T>
-inline constexpr bool isTupleV = isTuple<T>::value;
-
 template<class T>
 inline constexpr bool isQFutureV = false;
 
@@ -902,7 +891,7 @@ static QFuture<ArgsType<Signal>> connect(Sender *sender, Signal signal)
                     QObject::disconnect(connections->second);
                     promise.reportFinished();
                 });
-    } else if constexpr (QtPrivate::isTupleV<ArgsType>) {
+    } else if constexpr (QtPrivate::ArgResolver<Signal>::HasExtraArgs) {
         connections->first = QObject::connect(sender, signal, sender,
                                               [promise, connections](auto... values) mutable {
                                                   QObject::disconnect(connections->first);
