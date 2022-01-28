3e5aa0e15a51e9ead8a96820f779e15d4912fdec
Kai Köhne <kai.koehne@qt.io>
2022-01-28 11:08:14 +0100

    Widgets: Do not include qundogroup.h, qundostack.h in QtWidgets header
    The headers are already included in QtGui/QtGui. This also fixes
    an issue when compiling with the respective features disabled.
    
    Fixes: QTBUG-100290
    Change-Id: I5e7c6578952535315bacde218a26d2133c486b2c
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 604ace00957d7dfd8029932c529a59810485163e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/util/qundostack.h
+++ src/widgets/util/qundostack.h
@@ -37,6 +37,11 @@
 **
 ****************************************************************************/
 
+#if 0
+// syncqt: header is already part of QtGui/QtGui
+#pragma qt_no_master_include
+#endif
+
 #include <QtGui/QUndoStack>
 
 QT_BEGIN_NAMESPACE
