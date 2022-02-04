5d45c3b6a4bfc4fd9c2a542c9e2325e3f42261d2
Shawn Rutledge <shawn.rutledge@qt.io>
2022-02-04 08:58:03 +0100

    doc: clarify that passive-grab API is only for Qt Quick Pointer Handlers
    ...for now anyway. The plan is to have C++ API for custom handlers
    later on; and we'll see if we find any other "must-have" use cases.
    
    Task-number: QTBUG-68110
    Change-Id: I5e624a500af37797345d25bb5f54b146b13a45ca
    Reviewed-by: Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>
    (cherry picked from commit cf2ec5a5fccf9cd92ffead2b95af032283eecebc)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/kernel/qevent.cpp
+++ src/gui/kernel/qevent.cpp
@@ -398,7 +398,7 @@ void QPointerEvent::setExclusiveGrabber(const QEventPoint &point, QObject *exclu
     Returns the list of objects that have been requested to receive all
     future update events and the release event containing the given \a point.
 
-    It's mainly for use in Qt Quick at this time.
+    It's only for use by \l {Qt Quick Input Handlers}.
 
     \sa QPointerEvent::addPassiveGrabber()
 */
@@ -418,7 +418,7 @@ QList<QPointer<QObject> > QPointerEvent::passiveGrabbers(const QEventPoint &poin
     future update events and the release event containing the given \a point,
     regardless where else those events may be delivered.
 
-    It's mainly for use in Qt Quick at this time.
+    It's only for use by \l {Qt Quick Input Handlers}.
 
     Returns \c false if \a grabber was already added, \c true otherwise.
 */
@@ -433,7 +433,7 @@ bool QPointerEvent::addPassiveGrabber(const QEventPoint &point, QObject *grabber
     Removes the passive \a grabber from the given \a point if it was previously added.
     Returns \c true if it had been a passive grabber before, \c false if not.
 
-    It's mainly for use in Qt Quick at this time.
+    It's only for use by \l {Qt Quick Input Handlers}.
 
     \sa QPointerEvent::addPassiveGrabber()
 */
@@ -447,7 +447,7 @@ bool QPointerEvent::removePassiveGrabber(const QEventPoint &point, QObject *grab
 /*!
     Removes all passive grabbers from the given \a point.
 
-    It's mainly for use in Qt Quick at this time.
+    It's only for use by \l {Qt Quick Input Handlers}.
 
     \sa QPointerEvent::addPassiveGrabber()
 */
