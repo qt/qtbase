f71d54ee34931b5b3d2336954a76138c086aa94e
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2022-02-09 15:05:20 +0100

    Update docs for QSurface::MetalSurface to include iOS
    Pick-to: 6.3 6.2
    Change-Id: I67fc47b3dc33960e7dfa78890d16d1e7450e30ab
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/gui/kernel/qsurface.cpp
+++ src/gui/kernel/qsurface.cpp
@@ -84,11 +84,11 @@ QT_IMPL_METATYPE_EXTERN_TAGGED(QSurface*, QSurface_ptr)
     \value VulkanSurface The surface is a Vulkan compatible surface and can be used
     in conjunction with the Vulkan graphics API.
     \value MetalSurface The surface is a Metal compatible surface and can be used
-    in conjunction with Apple's Metal graphics API. This surface type is supported
-    on macOS only.
+    in conjunction with Apple's Metal graphics API. This surface type is only supported
+    on \macos and iOS.
     \value Direct3DSurface The surface is a Direct 3D 11 and 12 compatible
     surface and can be used in conjunction with the DXGI and Direct3D APIs. This
-    surface type is supported on Windows only.
+    surface type is only supported on Windows.
  */
 
 
