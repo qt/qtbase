dae858b7105ffa9c7dedca1d5b35c0f9e9d56436
Marc Mutz <marc.mutz@qt.io>
2022-02-15 10:32:47 +0100

    QtGui: establish removed_api.cpp for upcoming QT_GUI_REMOVED_SINCE
    Change-Id: I9715c11fc6431e7046193a1d68807155ad7bea05
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

---
+++ src/gui/compat/removed_api.cpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#define QT_GUI_BUILD_REMOVED_API
+
+#include "qtguiglobal.h"
+
+QT_USE_NAMESPACE
+
+#if QT_GUI_REMOVED_SINCE(6, 6)
+
+// #include "qotherheader.h"
+// // implement removed functions from qotherheader.h
+// order sections alphabetically
+
+#endif // QT_GUI_REMOVED_SINCE(6, 6)
+
+#if QT_GUI_REMOVED_SINCE(6, 7)
+
+// #include "qotherheader.h"
+// // implement removed functions from qotherheader.h
+// order sections alphabetically
+
+#endif // QT_GUI_REMOVED_SINCE(6, 7)
