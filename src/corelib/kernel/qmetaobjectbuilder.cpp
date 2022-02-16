qt/qtbase

commit 465701bb98f3c3454d15c22b8e38ab4ad8821dfc

QMetaPropertyBuilder: carry metatype
Properties of non-dynamic classes always have the metatype stored, so we
can make use of it. Moreover, when the builder is converted into a
metaobject, we need to resolve the metatype anyway.
As a driveby, add a dedicated metatype test to tst_qmetaobjectbuilder.

Change-Id: I7eea0cd8fc2da5d92c7fc803f05cd81e3a9d4cf4
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/corelib/kernel/qmetaobjectbuilder.cpp
@@ -152,10 +152,11 @@ class QMetaPropertyBuilderPrivate
 {
 public:
     QMetaPropertyBuilderPrivate
-            (const QByteArray& _name, const QByteArray& _type, int notifierIdx=-1,
+            (const QByteArray& _name, const QByteArray& _type, QMetaType _metaType, int notifierIdx=-1,
              int _revision = 0)
         : name(_name),
           type(QMetaObject::normalizedType(_type.constData())),
+          metaType(_metaType),
           flags(Readable | Writable | Scriptable), notifySignal(notifierIdx),
           revision(_revision)
     {
@@ -164,6 +165,7 @@ public:
 
     QByteArray name;
     QByteArray type;
+    QMetaType metaType;
     int flags;
     int notifySignal;
     int revision;
@@ -556,8 +558,16 @@ QMetaMethodBuilder QMetaObjectBuilder::addConstructor(const QMetaMethod &prototy
 QMetaPropertyBuilder QMetaObjectBuilder::addProperty(const QByteArray &name, const QByteArray &type,
                                                      int notifierId)
 {
+    return addProperty(name, type, QMetaType::fromName(name), notifierId);
+}
+
+/*!
+    \overload
+ */
+QMetaPropertyBuilder QMetaObjectBuilder::addProperty(const QByteArray &name, const QByteArray &type, QMetaType metaType, int notifierId)
+{
     int index = int(d->properties.size());
-    d->properties.push_back(QMetaPropertyBuilderPrivate(name, type, notifierId));
+    d->properties.push_back(QMetaPropertyBuilderPrivate(name, type, metaType, notifierId));
     return QMetaPropertyBuilder(this, index);
 }
 
@@ -571,7 +581,7 @@ QMetaPropertyBuilder QMetaObjectBuilder::addProperty(const QByteArray &name, con
 */
 QMetaPropertyBuilder QMetaObjectBuilder::addProperty(const QMetaProperty &prototype)
 {
-    QMetaPropertyBuilder property = addProperty(prototype.name(), prototype.typeName());
+    QMetaPropertyBuilder property = addProperty(prototype.name(), prototype.typeName(), prototype.metaType());
     property.setReadable(prototype.isReadable());
     property.setWritable(prototype.isWritable());
     property.setResettable(prototype.isResettable());

tests/auto/corelib/kernel/qmetaobjectbuilder/tst_qmetaobjectbuilder.cpp
@@ -64,6 +64,8 @@ private slots:
 
     void classNameFirstInStringData();
 
+    void propertyMetaType();
+
 private:
     static bool checkForSideEffects
         (const QMetaObjectBuilder& builder,
@@ -1685,6 +1687,24 @@ void tst_QMetaObjectBuilder::classNameFirstInStringData()
     QByteArray className(reinterpret_cast<const char *>(mo->d.stringdata) + offset, len);
     QCOMPARE(className, QByteArrayLiteral("TestClass"));
 
+    free(mo);
+}
+
+struct MyFoo {};
+
+void tst_QMetaObjectBuilder::propertyMetaType()
+{
+    QMetaType meta = QMetaType::fromType<MyFoo>();
+    auto metaId = meta.id();
+    QMetaObjectBuilder builder;
+    builder.setClassName("Test");
+    builder.addProperty("test", "MyFoo");
+    auto mo = builder.toMetaObject();
+
+    QMetaProperty metaProp = mo->property(mo->indexOfProperty("test"));
+    QCOMPARE(metaProp.typeName(), meta.name());
+    QCOMPARE(metaProp.typeId(), metaId);
+    QCOMPARE(metaProp.metaType(), meta);
     free(mo);
 }
 

src/corelib/kernel/qmetaobjectbuilder_p.h
@@ -125,6 +125,7 @@ public:
     QMetaMethodBuilder addConstructor(const QMetaMethod& prototype);
 
     QMetaPropertyBuilder addProperty(const QByteArray& name, const QByteArray& type, int notifierId=-1);
+    QMetaPropertyBuilder addProperty(const QByteArray& name, const QByteArray& type, QMetaType metaType, int notifierId=-1);
     QMetaPropertyBuilder addProperty(const QMetaProperty& prototype);
 
     QMetaEnumBuilder addEnumerator(const QByteArray& name);

commit 8de049389610037595e3b24ed660d4855aa460ce

QMetaObjectBuilder: fix addProperty() recording of the property type
Issue introduced by commit 465701bb98f3c3454d15c22b8e38ab4ad8821dfc.

[ChangeLog][QtCore][QMetaObjectBuilder] Fixed a bug that would cause
addProperty() to use the incorrect type for the property if the
property's name matched a valid type registered with QMetaType.

Pick-to: 6.2 6.3
Change-Id: Ic15405335d804bdea761fffd16d402f2c9611f30
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/corelib/kernel/qmetaobjectbuilder.cpp
@@ -558,7 +558,7 @@ QMetaMethodBuilder QMetaObjectBuilder::addConstructor(const QMetaMethod &prototy
 QMetaPropertyBuilder QMetaObjectBuilder::addProperty(const QByteArray &name, const QByteArray &type,
                                                      int notifierId)
 {
-    return addProperty(name, type, QMetaType::fromName(name), notifierId);
+    return addProperty(name, type, QMetaType::fromName(type), notifierId);
 }
 
 /*!

tests/auto/corelib/kernel/qmetaobjectbuilder/tst_qmetaobjectbuilder.cpp
@@ -1638,17 +1638,19 @@ void tst_QMetaObjectBuilder::classNameFirstInStringData()
 }
 
 struct MyFoo {};
+struct myParameter {};
 
 void tst_QMetaObjectBuilder::propertyMetaType()
 {
+    qRegisterMetaType<myParameter>();
     QMetaType meta = QMetaType::fromType<MyFoo>();
     auto metaId = meta.id();
     QMetaObjectBuilder builder;
     builder.setClassName("Test");
-    builder.addProperty("test", "MyFoo");
+    builder.addProperty("myParameter", "MyFoo");
     auto mo = builder.toMetaObject();
 
-    QMetaProperty metaProp = mo->property(mo->indexOfProperty("test"));
+    QMetaProperty metaProp = mo->property(mo->indexOfProperty("myParameter"));
     QCOMPARE(metaProp.typeName(), meta.name());
     QCOMPARE(metaProp.typeId(), metaId);
     QCOMPARE(metaProp.metaType(), meta);