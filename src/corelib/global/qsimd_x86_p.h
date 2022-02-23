71af0d7059d47a199e1d26de6573d6a5038caa30
Kai Köhne <kai.koehne@qt.io>
2022-02-23 12:34:11 +0100

    Re-add missing 'we mean it' header
    This got lost in commit b852584556bec3750bad7fac984b6fc5af4c870f .
    Also augment util/x86simdgen/header so that the header is
    automatically included when qsimd_x86_p.h is re-generated.
    
    Change-Id: I3e59b983f78b8c1aced3757e1aa5dceb6d653d97
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/global/qsimd_x86_p.h
+++ src/corelib/global/qsimd_x86_p.h
@@ -37,6 +37,17 @@
 **
 ****************************************************************************/
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 // This is a generated file. DO NOT EDIT.
 // Please see 3rdparty/x86simd_generate.pl
 #ifndef QSIMD_X86_P_H
