e00397efe39fcb86bca6b6e86034889bef7ba309
Marc Mutz <marc.mutz@qt.io>
2022-02-25 18:16:04 +0100

    Backport recent QFlags tests
    This is a backport of the tst_qflags parts of
    
    - 6f50e54138f9a2aa9ab6d84add449f197d545de3
      (QFlags: fix missing bitwise xor operators in QT_TYPESAFE_FLAGS builds)
    - 53890b5d3cd771d66d6b73940eb38e3c3fc6d60f
      (qtestmouse.h: compile with QT_TYPESAFE_FLAGS)
    
    These changes were originally only picked to 6.3 because it was
    thought only 6.3 was failing the tests. In order to check that this is
    indeed the case, and to help future cherry-picking, backport the
    tst_qflags additions to 6.2 and 5.15.
    
    Change-Id: Iecfa11a8a83388c60f659238a28a26142b579380
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
    (cherry picked from commit 4feaf095f44861fb93694483ac209b14974bf037)
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- tests/auto/corelib/global/qflags/tst_qflags.cpp
+++ tests/auto/corelib/global/qflags/tst_qflags.cpp
@@ -31,6 +31,8 @@ class tst_QFlags: public QObject
 {
     Q_OBJECT
 private slots:
+    void boolCasts() const;
+    void operators() const;
     void testFlag() const;
     void testFlagZeroFlag() const;
     void testFlagMultiBits() const;
@@ -41,6 +43,73 @@ private slots:
     void testSetFlags();
     void adl();
 };
+
+void tst_QFlags::boolCasts() const
+{
+    // This tests that the operator overloading is sufficient so that common
+    // idioms involving flags -> bool casts work as expected:
+
+    const Qt::Alignment nonNull = Qt::AlignCenter;
+    const Qt::Alignment null = {};
+
+    // basic premiss:
+    QVERIFY(bool(nonNull));
+    QVERIFY(!bool(null));
+
+    // The rest is just checking that stuff compiles:
+
+    // QVERIFY should compile:
+    QVERIFY(nonNull);
+    QVERIFY(!null);
+
+    // ifs should compile:
+    if (null) QFAIL("Can't contextually convert QFlags to bool!");
+    if (!nonNull) QFAIL("Missing operator! on QFlags (shouldn't be necessary).");
+
+    // ternary should compile:
+    QVERIFY(nonNull ? true : false);
+    QVERIFY(!null ? true : false);
+
+    // logical operators should compile:
+    QVERIFY(nonNull && true);
+    QVERIFY(nonNull || false);
+    QVERIFY(!null && true);
+    QVERIFY(!null || false);
+
+    // ... in both directions:
+    QVERIFY(true && nonNull);
+    QVERIFY(false || nonNull);
+    QVERIFY(true && !null);
+    QVERIFY(false || !null);
+
+    // ... and mixed:
+    QVERIFY(null || nonNull);
+    QVERIFY(!(null && nonNull));
+}
+
+void tst_QFlags::operators() const
+{
+#define CHECK(op, LHS, RHS, RES) \
+    do { \
+        using LFlags = QFlags<decltype(LHS)>; \
+        using RFlags = QFlags<decltype(RHS)>; \
+        QCOMPARE((LHS op RHS), (RES)); \
+        QCOMPARE((LFlags(LHS) op RHS), (RES)); \
+        QCOMPARE((LHS op RFlags(RHS)), (RES)); \
+        QCOMPARE((LFlags(LHS) op RFlags(RHS)), (RES)); \
+        QCOMPARE((LFlags(LHS) op ## = RHS), (RES)); \
+        QCOMPARE((LFlags(LHS) op ## = RFlags(RHS)), (RES)); \
+    } while (false)
+
+    CHECK(|, Qt::AlignHCenter, Qt::AlignVCenter, Qt::AlignCenter);
+    CHECK(|, Qt::AlignHCenter, Qt::AlignHCenter, Qt::AlignHCenter);
+    CHECK(&, Qt::AlignHCenter, Qt::AlignVCenter, Qt::Alignment());
+    CHECK(&, Qt::AlignHCenter, Qt::AlignHCenter, Qt::AlignHCenter);
+    CHECK(^, Qt::AlignHCenter, Qt::AlignVCenter, Qt::AlignCenter);
+    CHECK(^, Qt::AlignHCenter, Qt::AlignHCenter, Qt::Alignment());
+
+#undef CHECK
+}
 
 void tst_QFlags::testFlag() const
 {
