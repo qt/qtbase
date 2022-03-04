76016d99486778ae21b2d6eca242e46254239a6d
Mårten Nordheim <marten.nordheim@qt.io>
2022-03-04 13:47:27 +0100

    QPrintDialog/win: Fix use of deprecated QMessageBox::warning
    by using the new one
    
    Pick-to: 6.3 6.2
    Change-Id: I8779107079c4e3d4b1d48c05b6fbfef1b6e98b99
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/printsupport/dialogs/qprintdialog_win.cpp
+++ src/printsupport/dialogs/qprintdialog_win.cpp
@@ -261,9 +261,9 @@ int QPrintDialogPrivate::openWindowsPrintDialogModally()
         }
 
         if (!done) {
-            QMessageBox::warning(0, QPrintDialog::tr("Print"),
-                                 QPrintDialog::tr("The 'From' value cannot be greater than the 'To' value."),
-                                 QPrintDialog::tr("OK"));
+            QMessageBox::warning(nullptr,
+                                 QPrintDialog::tr("Print"),
+                                 QPrintDialog::tr("The 'From' value cannot be greater than the 'To' value."));
         }
     } while (!done);
 
