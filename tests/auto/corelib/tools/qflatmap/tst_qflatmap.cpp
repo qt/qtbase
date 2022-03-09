eded55cfbbb5816916fcef2000bc60b64246bd68
Marc Mutz <marc.mutz@qt.io>
2022-03-09 15:47:09 +0100

    tst_qflatmap: fix -Wunused-local-typedef (Clang)
    False Positive, of course, since it's used in dependent callee
    transparent_impl().
    
    Amends 58e8ae5605abefd0f579586a890dc13fb381b2a3.
    
    Pick-to: 6.3
    Change-Id: I7520fb7cf6bce28a5fd9381783ba349119309132
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/corelib/tools/qflatmap/tst_qflatmap.cpp
+++ tests/auto/corelib/tools/qflatmap/tst_qflatmap.cpp
@@ -511,7 +511,7 @@ void tst_QFlatMap::transparency_using()
 {
     struct StringViewCompare
     {
-        using is_transparent = void;
+        using is_transparent [[maybe_unused]] = void;
         bool operator()(QAnyStringView lhs, QAnyStringView rhs) const
         {
             return lhs < rhs;
