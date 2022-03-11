d5b3238def66419e1a79c79748251230d4c947cd
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2022-03-11 14:10:42 +0100

    tst_QLocalSocket: Output error when listening fails
    Pick-to: 6.3
    Change-Id: I742de5f522e4a28794abdee863eea2758536d392
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/auto/network/socket/qlocalsocket/tst_qlocalsocket.cpp
+++ tests/auto/network/socket/qlocalsocket/tst_qlocalsocket.cpp
@@ -326,7 +326,7 @@ void tst_QLocalSocket::listen()
     QFETCH(QString, name);
     QFETCH(bool, canListen);
     QFETCH(bool, close);
-    QVERIFY2((server.listen(name) == canListen), server.errorString().toLatin1().constData());
+    QVERIFY2((server.listen(name) == canListen), qUtf8Printable(server.errorString()));
 
     // test listening
     QCOMPARE(server.serverName(), name);
@@ -489,7 +489,7 @@ void tst_QLocalSocket::listenAndConnect()
 void tst_QLocalSocket::connectWithOpen()
 {
     LocalServer server;
-    QVERIFY(server.listen("tst_qlocalsocket"));
+    QVERIFY2(server.listen("tst_qlocalsocket"), qUtf8Printable(server.errorString()));
 
     LocalSocket socket;
     QSignalSpy spyAboutToClose(&socket, SIGNAL(aboutToClose()));
@@ -541,7 +541,7 @@ void tst_QLocalSocket::listenAndConnectAbstractNamespaceTrailingZeros()
         client_path.append(QChar('\0'));
     LocalServer server;
     server.setSocketOptions(QLocalServer::AbstractNamespaceOption);
-    QVERIFY(server.listen(server_path));
+    QVERIFY2(server.listen(server_path), qUtf8Printable(server.errorString()));
     QCOMPARE(server.fullServerName(), server_path);
 
     LocalSocket socket;
@@ -590,7 +590,7 @@ void tst_QLocalSocket::listenAndConnectAbstractNamespace()
 
     LocalServer server;
     server.setSocketOptions(serverOption);
-    QVERIFY(server.listen("tst_qlocalsocket"));
+    QVERIFY2(server.listen("tst_qlocalsocket"), qUtf8Printable(server.errorString()));
 
     LocalSocket socket;
     socket.setSocketOptions(socketOption);
@@ -771,7 +771,7 @@ void tst_QLocalSocket::readLine()
 
     const QString serverName = QLatin1String("tst_localsocket");
     LocalServer server;
-    QVERIFY(server.listen(serverName));
+    QVERIFY2(server.listen(serverName), qUtf8Printable(server.errorString()));
 
     LocalSocket client;
     client.connectToServer(serverName);
@@ -847,7 +847,7 @@ void tst_QLocalSocket::skip()
 
     const QString serverName = QLatin1String("tst_localsocket");
     LocalServer server;
-    QVERIFY(server.listen(serverName));
+    QVERIFY2(server.listen(serverName), qUtf8Printable(server.errorString()));
 
     LocalSocket client;
     client.connectToServer(serverName);
@@ -1039,7 +1039,7 @@ void tst_QLocalSocket::fullPath()
     QString path = "/tmp";
 #endif
     QString serverName = path + '/' + name;
-    QVERIFY2(server.listen(serverName), server.errorString().toLatin1().constData());
+    QVERIFY2(server.listen(serverName), qUtf8Printable(server.errorString()));
     QCOMPARE(server.serverName(), serverName);
     QCOMPARE(server.fullServerName(), serverName);
 
@@ -1070,7 +1070,7 @@ void tst_QLocalSocket::hitMaximumConnections()
     LocalServer server;
     QString name = "tst_localsocket";
     server.setMaxPendingConnections(max);
-    QVERIFY2(server.listen(name), server.errorString().toLatin1().constData());
+    QVERIFY2(server.listen(name), qUtf8Printable(server.errorString()));
     int connections = server.maxPendingConnections() + 1;
     QList<QLocalSocket*> sockets;
     for (int i = 0; i < connections; ++i) {
@@ -1131,7 +1131,7 @@ public:
         LocalServer server;
         server.setMaxPendingConnections(10);
         QVERIFY2(server.listen("qlocalsocket_threadtest"),
-                 server.errorString().toLatin1().constData());
+                 qUtf8Printable(server.errorString()));
         mutex.lock();
         wc.wakeAll();
         mutex.unlock();
@@ -1292,7 +1292,7 @@ void tst_QLocalSocket::waitForDisconnect()
 {
     QString name = "tst_localsocket";
     LocalServer server;
-    QVERIFY(server.listen(name));
+    QVERIFY2(server.listen(name), qUtf8Printable(server.errorString()));
     LocalSocket socket;
     socket.connectToServer(name);
     QVERIFY(socket.waitForConnected(3000));
@@ -1310,7 +1310,7 @@ void tst_QLocalSocket::waitForDisconnectByServer()
 {
     QString name = "tst_localsocket";
     LocalServer server;
-    QVERIFY(server.listen(name));
+    QVERIFY2(server.listen(name), qUtf8Printable(server.errorString()));
     LocalSocket socket;
     QSignalSpy spy(&socket, SIGNAL(disconnected()));
     QVERIFY(spy.isValid());
@@ -1329,7 +1329,7 @@ void tst_QLocalSocket::waitForReadyReadOnDisconnected()
 {
     QString name = "tst_localsocket";
     LocalServer server;
-    QVERIFY(server.listen(name));
+    QVERIFY2(server.listen(name), qUtf8Printable(server.errorString()));
     LocalSocket socket;
     connect(&socket, &QLocalSocket::readyRead, [&socket]() {
         QVERIFY(socket.getChar(nullptr));
@@ -1363,7 +1363,7 @@ void tst_QLocalSocket::delayedDisconnect()
 {
     QString name = "tst_localsocket";
     LocalServer server;
-    QVERIFY(server.listen(name));
+    QVERIFY2(server.listen(name), qUtf8Printable(server.errorString()));
     LocalSocket socket;
     socket.connectToServer(name);
     QVERIFY(socket.waitForConnected(3000));
@@ -1394,7 +1394,7 @@ void tst_QLocalSocket::removeServer()
     // this is a hostile takeover, but recovering from a crash results in the same
     QLocalServer server, server2;
     QVERIFY(QLocalServer::removeServer("cleanuptest"));
-    QVERIFY(server.listen("cleanuptest"));
+    QVERIFY2(server.listen("cleanuptest"), qUtf8Printable(server.errorString()));
 #ifndef Q_OS_WIN
     // on Windows, there can be several sockets listening on the same pipe
     // on Unix, there can only be one socket instance
@@ -1409,7 +1409,7 @@ void tst_QLocalSocket::recycleServer()
     QLocalServer server;
     QLocalSocket client;
 
-    QVERIFY(server.listen("recycletest1"));
+    QVERIFY2(server.listen("recycletest1"), qUtf8Printable(server.errorString()));
     client.connectToServer("recycletest1");
     QVERIFY(client.waitForConnected(201));
     QVERIFY(server.waitForNewConnection(201));
@@ -1419,7 +1419,7 @@ void tst_QLocalSocket::recycleServer()
     client.disconnectFromServer();
     qApp->processEvents();
 
-    QVERIFY(server.listen("recycletest2"));
+    QVERIFY2(server.listen("recycletest2"), qUtf8Printable(server.errorString()));
     client.connectToServer("recycletest2");
     QVERIFY(client.waitForConnected(202));
     QVERIFY(server.waitForNewConnection(202));
@@ -1433,7 +1433,7 @@ void tst_QLocalSocket::recycleClientSocket()
                                                   << "They haven't made it to a gig yet.";
     QLocalServer server;
     const QString serverName = QStringLiteral("recycleClientSocket");
-    QVERIFY(server.listen(serverName));
+    QVERIFY2(server.listen(serverName), qUtf8Printable(server.errorString()));
     QLocalSocket client;
     QSignalSpy clientReadyReadSpy(&client, SIGNAL(readyRead()));
     QSignalSpy clientErrorSpy(&client, SIGNAL(errorOccurred(QLocalSocket::LocalSocketError)));
@@ -1461,7 +1461,7 @@ void tst_QLocalSocket::multiConnect()
     QLocalSocket client2;
     QLocalSocket client3;
 
-    QVERIFY(server.listen("multiconnect"));
+    QVERIFY2(server.listen("multiconnect"), qUtf8Printable(server.errorString()));
 
     client1.connectToServer("multiconnect");
     client2.connectToServer("multiconnect");
@@ -1482,7 +1482,7 @@ void tst_QLocalSocket::multiConnect()
 void tst_QLocalSocket::writeOnlySocket()
 {
     QLocalServer server;
-    QVERIFY(server.listen("writeOnlySocket"));
+    QVERIFY2(server.listen("writeOnlySocket"), qUtf8Printable(server.errorString()));
 
     QLocalSocket client;
     client.connectToServer("writeOnlySocket", QIODevice::WriteOnly);
@@ -1516,7 +1516,7 @@ void tst_QLocalSocket::writeToClientAndDisconnect()
     QLocalSocket client;
     QSignalSpy readChannelFinishedSpy(&client, SIGNAL(readChannelFinished()));
 
-    QVERIFY(server.listen("writeAndDisconnectServer"));
+    QVERIFY2(server.listen("writeAndDisconnectServer"), qUtf8Printable(server.errorString()));
     client.connectToServer("writeAndDisconnectServer");
     QVERIFY(client.waitForConnected(200));
     QVERIFY(server.waitForNewConnection(200));
@@ -1541,7 +1541,7 @@ void tst_QLocalSocket::writeToClientAndDisconnect()
 void tst_QLocalSocket::writeToDisconnected()
 {
     QLocalServer server;
-    QVERIFY(server.listen("writeToDisconnected"));
+    QVERIFY2(server.listen("writeToDisconnected"), qUtf8Printable(server.errorString()));
 
     QLocalSocket client;
     QSignalSpy spyError(&client, SIGNAL(errorOccurred(QLocalSocket::LocalSocketError)));
@@ -1612,7 +1612,7 @@ public slots:
 void tst_QLocalSocket::bytesWrittenSignal()
 {
     QLocalServer server;
-    QVERIFY(server.listen("qlocalsocket_readyread"));
+    QVERIFY2(server.listen("qlocalsocket_readyread"), qUtf8Printable(server.errorString()));
     WriteThread writeThread;
     QSignalSpy receivedSpy(&writeThread, &WriteThread::bytesWrittenReceived);
     writeThread.start();
@@ -1633,7 +1633,7 @@ void tst_QLocalSocket::socketClosedSlot()
 void tst_QLocalSocket::syncDisconnectNotify()
 {
     QLocalServer server;
-    QVERIFY(server.listen("syncDisconnectNotify"));
+    QVERIFY2(server.listen("syncDisconnectNotify"), qUtf8Printable(server.errorString()));
     QLocalSocket client;
     connect(&client, &QLocalSocket::disconnected,
             this, &tst_QLocalSocket::socketClosedSlot);
@@ -1652,7 +1652,7 @@ void tst_QLocalSocket::syncDisconnectNotify()
 void tst_QLocalSocket::asyncDisconnectNotify()
 {
     QLocalServer server;
-    QVERIFY(server.listen("asyncDisconnectNotify"));
+    QVERIFY2(server.listen("asyncDisconnectNotify"), qUtf8Printable(server.errorString()));
     QLocalSocket client;
     QSignalSpy disconnectedSpy(&client, SIGNAL(disconnected()));
     QSignalSpy readChannelFinishedSpy(&client, SIGNAL(readChannelFinished()));
@@ -1707,7 +1707,7 @@ void tst_QLocalSocket::verifySocketOptions()
    QLocalServer::removeServer(service);
    QLocalServer server;
    server.setSocketOptions(opts);
-   QVERIFY2(server.listen(service), "service failed to start listening");
+   QVERIFY2(server.listen(service), qUtf8Printable(server.errorString()));
 
    // find the socket
    QString fullServerPath = QDir::cleanPath(QDir::tempPath());
@@ -1763,7 +1763,7 @@ void tst_QLocalSocket::verifyListenWithDescriptor()
     }
 
     QLocalServer server;
-    QVERIFY2(server.listen(listenSocket), "failed to start create QLocalServer with local socket");
+    QVERIFY2(server.listen(listenSocket), qUtf8Printable(server.errorString()));
 
 #if defined(Q_OS_LINUX) || defined(Q_OS_QNX)
     if (!bound) {
