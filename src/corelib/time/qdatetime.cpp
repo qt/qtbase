8377dedeecdcccd08cfdcba657dc6996cf8fda60
Marc Mutz <marc.mutz@qt.io>
2022-03-15 16:25:29 +0100

    QDateTime: future-proof swap()
    In Qt 7 32-bit builds, the ShortData will be larger than the
    d-pointer.
    
    So don't swap() the d-pointer, but a) delegate to Data to swap itself
    and b) swap the ShortData there, adding a static_assert in the .cpp
    file that triggers when the assumption that ShortData is always at
    least as large as a pointer is violated.
    
    Found while porting away from overly-generic qSwap(), so done that,
    too.
    
    Task-number: QTBUG-97601
    Change-Id: I5139da58d99e9491a582ff2cb2f817cd96952044
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 9f8da21239a4d9e22a2d5633920c36af4eb4a6c7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/time/qdatetime.cpp
+++ src/corelib/time/qdatetime.cpp
@@ -3488,6 +3488,7 @@ QDateTime::QDateTime() noexcept
     static_assert(sizeof(ShortData) == sizeof(qint64));
     static_assert(sizeof(Data) == sizeof(qint64));
 #endif
+    static_assert(sizeof(ShortData) >= sizeof(void*), "oops, Data::swap() is broken!");
 }
 
 /*!
