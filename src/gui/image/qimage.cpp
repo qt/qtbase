afb5e7d349cc19d7bf4e6037cc1dd7ab551a4d11
Allan Sandfeld Jensen <allan.jensen@qt.io>
2022-03-23 10:24:51 +0000

    Revert "QImage::operator==: Also compare the image colorspace"
    This reverts commit 019d0dd4b49d5a0e9cbb73829158e798ea25fd86.
    
    Reason for revert: Causes test failures, and better for just 6.4
    
    Change-Id: Iaf1dc18d6f347fd203b07253ad502b6060486157
    Reviewed-by: Albert Astals Cid <albert.astals.cid@kdab.com>
    (cherry picked from commit 136407e3eb88b5480cf5a37e9234e698ef6cff2d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/image/qimage.cpp
+++ src/gui/image/qimage.cpp
@@ -3961,7 +3961,7 @@ bool QImage::operator==(const QImage & i) const
         return false;
 
     // obviously different stuff?
-    if (i.d->height != d->height || i.d->width != d->width || i.d->format != d->format || i.d->colorSpace != d->colorSpace)
+    if (i.d->height != d->height || i.d->width != d->width || i.d->format != d->format)
         return false;
 
     if (d->format != Format_RGB32) {
