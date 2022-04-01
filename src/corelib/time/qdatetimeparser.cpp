d4800e542dd80ae1df89a7857c9fe65f40a2815c
Edward Welbourne <edward.welbourne@qt.io>
2022-04-01 12:07:10 +0200

    Fix assertion failure when parsing a doubly-invalid date-time text
    When the date-time string falls in a spring-forward (so is invalid)
    and one of the fields of the parsed string doesn't match the format
    it's meant to (e.g. a single-digit seconds field when format ss was
    specified), a check that the current fall-back date-time is between
    the minimum and maximum for the parser object failed, triggering an
    assertion.
    
    In any case, an invalid default-value wasn't useful to the code that
    parsed a single section of the date-time string, so brute-force the
    current value to a valid date-time (when possible) using the usual
    round-trip via milliseconds since the epoch.
    
    Added the test-case which first revealed the problem, plus a couple
    more informed by it, to exercise the same code-paths with fewer things
    failing.
    
    Fixes: QTBUG-102199
    Change-Id: I658308614505ef25f4c97d0de6148acb54a65a0f
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 9fee35a2eda68b5968d911d5e14d298b91418d2c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/time/qdatetimeparser.cpp
+++ src/corelib/time/qdatetimeparser.cpp
@@ -759,6 +759,11 @@ QDateTimeParser::parseSection(const QDateTime &currentValue, int sectionIndex, i
                          && m_text.at(offset) == QLatin1Char('-'));
     const int negativeYearOffset = negate ? 1 : 0;
 
+    // If the fields we've read thus far imply a time in a spring-forward,
+    // coerce to a nearby valid time:
+    const QDateTime defaultValue = currentValue.isValid() ? currentValue
+        : QDateTime::fromMSecsSinceEpoch(currentValue.toMSecsSinceEpoch());
+
     QStringView sectionTextRef =
             QStringView { m_text }.mid(offset + negativeYearOffset, sectionmaxsize);
 
@@ -794,7 +799,7 @@ QDateTimeParser::parseSection(const QDateTime &currentValue, int sectionIndex, i
             m_text.replace(offset, used, sectiontext.constData(), used);
         break; }
     case TimeZoneSection:
-        result = findTimeZone(sectionTextRef, currentValue,
+        result = findTimeZone(sectionTextRef, defaultValue,
                               absoluteMax(sectionIndex),
                               absoluteMin(sectionIndex));
         break;
@@ -806,7 +811,7 @@ QDateTimeParser::parseSection(const QDateTime &currentValue, int sectionIndex, i
             int num = 0, used = 0;
             if (sn.type == MonthSection) {
                 const QDate minDate = getMinimum().date();
-                const int year = currentValue.date().year(calendar);
+                const int year = defaultValue.date().year(calendar);
                 const int min = (year == minDate.year(calendar)) ? minDate.month(calendar) : 1;
                 num = findMonth(sectiontext.toLower(), min, sectionIndex, year, &sectiontext, &used);
             } else {
@@ -900,7 +905,7 @@ QDateTimeParser::parseSection(const QDateTime &currentValue, int sectionIndex, i
                     }
 
                 } else if (unfilled && (fi & (FixedWidth | Numeric)) == (FixedWidth | Numeric)) {
-                    if (skipToNextSection(sectionIndex, currentValue, digitsStr)) {
+                    if (skipToNextSection(sectionIndex, defaultValue, digitsStr)) {
                         const int missingZeroes = sectionmaxsize - digitsStr.size();
                         result = ParsedSection(Acceptable, last, sectionmaxsize, missingZeroes);
                         m_text.insert(offset, QString(missingZeroes, QLatin1Char('0')));
