974a7bd6e0ece921e699df6c2b346f944f723b83
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-04-12 02:55:23 +0400

    FileChooser portal: set current_name
    Fixes: QTBUG-100297
    Pick-to: 6.3 6.2
    Change-Id: I5d41e01aafeccce2d886debdb595b4c87b03a043
    Reviewed-by: Jan Grulich <jgrulich@redhat.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/plugins/platformthemes/xdgdesktopportal/qxdgdesktopportalfiledialog.cpp
+++ src/plugins/platformthemes/xdgdesktopportal/qxdgdesktopportalfiledialog.cpp
@@ -48,6 +48,7 @@
 
 #include <QEventLoop>
 #include <QFile>
+#include <QFileInfo>
 #include <QMetaType>
 #include <QMimeType>
 #include <QMimeDatabase>
@@ -199,8 +200,12 @@ void QXdgDesktopPortalFileDialog::openPortal()
         if (!d->directory.isEmpty())
             options.insert(QLatin1String("current_folder"), QFile::encodeName(d->directory).append('\0'));
 
-        if (!d->selectedFiles.isEmpty())
+        if (!d->selectedFiles.isEmpty()) {
+            // current_file for the file to be pre-selected, current_name for the file name to be pre-filled
+            // current_file accepts absolute path while current_name accepts just file name
             options.insert(QLatin1String("current_file"), QFile::encodeName(d->selectedFiles.first()).append('\0'));
+            options.insert(QLatin1String("current_name"), QFileInfo(d->selectedFiles.first()).fileName());
+        }
     }
 
     // Insert filters
