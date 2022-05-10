e26af754af4a9e19c95e9ebf3c6e53f71d7de75d
Antti Määttä <antti.maatta@qt.io>
2022-05-10 08:51:52 +0300

    Widgets: Use effect region bounds when drawing with repaint manager
    When drawing widgets with graphics effect using repaint manager,
    if we do not combine the effect region some child widgets are not
    affected by the graphics effect since they are not included in the dirty
    region. However always using region bounds breaks the referenced bug use case.
    
    Fixes: QTBUG-102374
    Change-Id: Iaf4eaba34db863500a0600b344e5062f2b36b9bf
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit a382134a40792068df56eec78f60be4c9acd0221)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/kernel/qwidget.cpp
+++ src/widgets/kernel/qwidget.cpp
@@ -5468,7 +5468,7 @@ void QWidgetPrivate::drawWidget(QPaintDevice *pdev, const QRegion &rgn, const QP
         QWidgetEffectSourcePrivate *sourced = static_cast<QWidgetEffectSourcePrivate *>
                                                          (source->d_func());
         if (!sourced->context) {
-            const QRegion effectRgn(rgn.boundingRect());
+            const QRegion effectRgn((flags & UseEffectRegionBounds) ? rgn.boundingRect() : rgn);
             QWidgetPaintContext context(pdev, effectRgn, offset, flags, sharedPainter, repaintManager);
             sourced->context = &context;
             if (!sharedPainter) {
@@ -5500,6 +5500,7 @@ void QWidgetPrivate::drawWidget(QPaintDevice *pdev, const QRegion &rgn, const QP
         }
     }
 #endif // QT_CONFIG(graphicseffect)
+    flags = flags & ~UseEffectRegionBounds;
 
     const bool alsoOnScreen = flags & DrawPaintOnScreen;
     const bool recursive = flags & DrawRecursive;
