bd8d3fb47ca685da9ce038bd6c6ee66e49ec9317
Morten Sørvig <morten.sorvig@qt.io>
2022-05-10 08:28:11 +0200

    wasm: set ASSERTIONS level to default (1)
    This removes one of the conditions which enables
    wasm-emscripten-finalize.
    
    Task-number: QTBUG-103352
    Change-Id: Id05db4b081dec360cdad2e611622e5baf09aeb23
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    Reviewed-by: David Skoland <david.skoland@qt.io>

--- cmake/QtWasmHelpers.cmake
+++ cmake/QtWasmHelpers.cmake
@@ -65,7 +65,6 @@ function (qt_internal_setup_wasm_target_properties wasmTarget)
     target_link_options("${wasmTarget}" INTERFACE $<$<CONFIG:Debug>:
         "SHELL:-s DEMANGLE_SUPPORT=1"
         "SHELL:-s GL_DEBUG=1"
-        "SHELL:-s ASSERTIONS=2"
         --profiling-funcs>)
 
     # target_link_options("${wasmTarget}" INTERFACE "SHELL:-s LIBRARY_DEBUG=1") # print out library calls, verbose
