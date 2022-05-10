40fc4e78269aa6c407bebf3396f5ac9c9b3b0391
Ivan Solovev <ivan.solovev@qt.io>
2022-05-10 16:01:48 +0200

    Android A11Y: handle LocationChanged event only for focused element
    LocationChanged event unconditionally triggered invalidateVirtualViewId
    call. That call results in TYPE_WINDOW_CONTENT_CHANGED Android event,
    which causes a lot of background processing.
    That is not correct, because LocationChanged event is generated by
    every accessible element, not only the one that has A11Y focus.
    
    This patch checks event->uniqueId(), and processes only events that
    come from the focused accessible element.
    
    Done-with: Mike Achtelik <mike.achtelik@gmail.com>
    Task-number: QTBUG-102594
    Change-Id: I6b941733c9d215fed5ee5a7aeeb5be234add9ebe
    Reviewed-by: Mike Achtelik <mike.achtelik@gmail.com>
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>
    (cherry picked from commit 0613146d210e494bf98e0e4de97e03fc5021736e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/android/androidjniaccessibility.cpp
+++ src/plugins/platforms/android/androidjniaccessibility.cpp
@@ -132,9 +132,9 @@ namespace QtAndroidAccessibility
         return iface;
     }
 
-    void notifyLocationChange()
+    void notifyLocationChange(uint accessibilityObjectId)
     {
-        QtAndroid::notifyAccessibilityLocationChange();
+        QtAndroid::notifyAccessibilityLocationChange(accessibilityObjectId);
     }
 
     static int parentId_helper(int objectId); // forward declaration
--- src/plugins/platforms/android/androidjnimain.h
+++ src/plugins/platforms/android/androidjnimain.h
@@ -100,7 +100,7 @@ namespace QtAndroid
     jobject createBitmap(int width, int height, QImage::Format format, JNIEnv *env);
     jobject createBitmapDrawable(jobject bitmap, JNIEnv *env = nullptr);
 
-    void notifyAccessibilityLocationChange();
+    void notifyAccessibilityLocationChange(uint accessibilityObjectId);
     void notifyObjectHide(uint accessibilityObjectId, uint parentObjectId);
     void notifyObjectFocus(uint accessibilityObjectId);
     void notifyValueChanged(uint accessibilityObjectId, jstring value);
