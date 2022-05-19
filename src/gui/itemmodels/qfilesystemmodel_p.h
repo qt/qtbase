b666113532a2535cbc8df5da2c4ae1e3e976ab7e
Thiago Macieira <thiago.macieira@intel.com>
2022-05-19 09:36:11 -0700

    QFileSystemModelPrivate: de-inline ctor and dtor
    This class is dllexport'ed and has members that refer to classes that
    are only forward-declared in the header (such as QRegularExpression,
    used in std::vector). GCC and MSVC appear to emit the destructor at the
    point where it is used, which I guess is the vtable, which probably
    means where the constructor was inlined because I don't see any virtual
    overrides. Clang (at least in the form of ICX) appears to emit it at the
    class declaration point.
    
    C:\Qt\qt-everywhere-src-6.3.0\qtbase\src\gui\itemmodels/qfilesystemmodel_p.h(94,20): note: in instantiation of member function 'std::vector<QRegularExpression>::~vector' requested here
    class Q_GUI_EXPORT QFileSystemModelPrivate : public QAbstractItemModelPrivate
                       ^
    C:\Qt\qt-everywhere-src-6.3.0\qtbase\src\gui\itemmodels/qfilesystemmodel_p.h(94,20): note: in implicit destructor for 'QFileSystemModelPrivate' first required here
    C:\Qt\qt-everywhere-src-6.3.0\qtbase\src\gui\itemmodels/qfilesystemmodel_p.h(94,7): note: due to 'QFileSystemModelPrivate' being dllexported
    
    Fix by defining both the default ctor and the dtor out-of-line.
    
    Task-number: QTBUG-45582
    Fixes: QTBUG-103605
    Change-Id: I77c8221eb2824c369feffffd16f08ee39004a825
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 845491ed3b38eb756cd73cf4161f440559c9a6a6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/itemmodels/qfilesystemmodel_p.h
+++ src/gui/itemmodels/qfilesystemmodel_p.h
@@ -215,7 +215,8 @@ public:
         bool isVisible = false;
     };
 
-    QFileSystemModelPrivate() = default;
+    QFileSystemModelPrivate();
+    ~QFileSystemModelPrivate();
     void init();
     /*
       \internal
