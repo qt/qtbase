2e5e79a1fee5b9e09f6f03f4691c708dae7dc7a0
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-06-06 20:57:22 +0400

    Add QXdgDesktopPortalFileDialog::useNativeFileDialog()
    Task-number: QTBUG-98988
    Change-Id: I39417f089d839a9af009791088bd20058532bd7a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 6f9f6849c53e057e8b4186db64a1a6b5e1f31eca)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platformthemes/xdgdesktopportal/qxdgdesktopportalfiledialog_p.h
+++ src/plugins/platformthemes/xdgdesktopportal/qxdgdesktopportalfiledialog_p.h
@@ -93,6 +93,7 @@ private Q_SLOTS:
 private:
     void initializeDialog();
     void openPortal();
+    bool useNativeFileDialog() const;
 
     QScopedPointer<QXdgDesktopPortalFileDialogPrivate> d_ptr;
 };
