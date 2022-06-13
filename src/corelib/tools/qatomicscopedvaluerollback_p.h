163734a8cf9777b7f4c99d4b03d19ce6de04295b
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-06-13 10:35:39 +0200

    Add "We mean it" warning to new private header
    Amends 5e48a51608485cbc87b68498fd54ab36f001d584, fixing warning
    from syncqt.
    
    Change-Id: I15d22a1bba865777c17180438b6e89ee9e1ae78b
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit e6da018c86d874f5f0a5549f97609bf456a6f0d3)

--- src/corelib/tools/qatomicscopedvaluerollback_p.h
+++ src/corelib/tools/qatomicscopedvaluerollback_p.h
@@ -4,6 +4,17 @@
 #ifndef QATOMICSCOPEDVALUEROLLBACK_P_H
 #define QATOMICSCOPEDVALUEROLLBACK_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of qapplication_*.cpp, qwidget*.cpp and qfiledialog.cpp.  This header
+// file may change from version to version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qglobal.h>
 #include <QtCore/qatomic.h>
 
