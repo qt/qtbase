c15714191cfe36de140b360f1eddb715c48f10d4
Fabian Kosmale <fabian.kosmale@qt.io>
2022-06-20 08:24:53 +0200

    tst_qthread: Fix compilation under ubsan
    We missed the terminating ";" in the QSKIP lines.
    Amends ea4d6b987ae10f1bb910081b523c3b22b11f7b64
    
    Pick-to: 6.3 6.4
    Change-Id: Ibda43b8a84230c243dbcc74e157f4c3f8ef3891d
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- tests/auto/corelib/thread/qthread/tst_qthread.cpp
+++ tests/auto/corelib/thread/qthread/tst_qthread.cpp
@@ -463,7 +463,7 @@ void tst_QThread::terminate()
     QSKIP("Thread termination is not supported on Android.");
 #endif
 #if defined(__SANITIZE_ADDRESS__) || __has_feature(address_sanitizer)
-    QSKIP("Thread termination might result in stack underflow address sanitizer errors.")
+    QSKIP("Thread termination might result in stack underflow address sanitizer errors.");
 #endif
 
     Terminate_Thread thread;
@@ -533,7 +533,7 @@ void tst_QThread::terminated()
     QSKIP("Thread termination is not supported on Android.");
 #endif
 #if defined(__SANITIZE_ADDRESS__) || __has_feature(address_sanitizer)
-    QSKIP("Thread termination might result in stack underflow address sanitizer errors.")
+    QSKIP("Thread termination might result in stack underflow address sanitizer errors.");
 #endif
 
     SignalRecorder recorder;
@@ -1748,7 +1748,7 @@ Q_SIGNALS:
 void tst_QThread::terminateAndPrematureDestruction()
 {
 #if defined(__SANITIZE_ADDRESS__) || __has_feature(address_sanitizer)
-    QSKIP("Thread termination might result in stack underflow address sanitizer errors.")
+    QSKIP("Thread termination might result in stack underflow address sanitizer errors.");
 #endif
 
     WaitToRun_Thread thread;
@@ -1768,7 +1768,7 @@ void tst_QThread::terminateAndPrematureDestruction()
 void tst_QThread::terminateAndDoubleDestruction()
 {
 #if defined(__SANITIZE_ADDRESS__) || __has_feature(address_sanitizer)
-    QSKIP("Thread termination might result in stack underflow address sanitizer errors.")
+    QSKIP("Thread termination might result in stack underflow address sanitizer errors.");
 #endif
 
     class ChildObject : public QObject
