36202da4a93f43348f36131fb5695edd7a0f708a
Sona Kurazyan <sona.kurazyan@qt.io>
2022-06-20 16:19:24 +0200

    Move QFutureInterfaceBase::cleanContinuation() to removed_api
    This method isn't used anymore, but we can't remove it entirely for BC
    reasons, because it was called from inline code.
    
    Change-Id: I9183c666c466030787ac7c2386706b50abf23eaa
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 73e1bc09e63394490d91a223b3479b46c1595d66)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/thread/qfutureinterface.cpp
+++ src/corelib/thread/qfutureinterface.cpp
@@ -829,18 +829,6 @@ void QFutureInterfaceBase::setContinuation(std::function<void(const QFutureInter
     }
 }
 
-void QFutureInterfaceBase::cleanContinuation()
-{
-    if (!d)
-        return;
-
-    // This is called when the associated QPromise is being destroyed.
-    // Clear the continuation, to make sure it doesn't keep any ref-counted
-    // copies of this, so that the allocated memory can be freed.
-    QMutexLocker lock(&d->continuationMutex);
-    d->continuation = nullptr;
-}
-
 void QFutureInterfaceBase::runContinuation() const
 {
     QMutexLocker lock(&d->continuationMutex);
