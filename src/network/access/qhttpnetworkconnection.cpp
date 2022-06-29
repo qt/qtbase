bf3afa7ba18bf8ce89290b3052c108d77890a51c
André Klitzing <aklitzing@gmail.com>
2022-06-29 09:59:08 +0200

    Provide more details of failed connection
    Otherwise the errorString from qtls_openssl is lost that
    would help a lot why a tls connection failed.
    
    "Error during SSL handshake: error:1414D17A:SSL routines:tls12_check_peer_sigalg:wrong curve"
    
    Change-Id: Iab74b73488219686e5926308db02bc4063818c9f
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 4fde012a90ff6f791c672b8e97bf592171514392)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/network/access/qhttpnetworkconnection.cpp
+++ src/network/access/qhttpnetworkconnection.cpp
@@ -893,6 +893,8 @@ QString QHttpNetworkConnectionPrivate::errorDetail(QNetworkReply::NetworkError e
         break;
     case QNetworkReply::SslHandshakeFailedError:
         errorString = QCoreApplication::translate("QHttp", "SSL handshake failed");
+        if (socket)
+            errorString += QStringLiteral(": ") + socket->errorString();
         break;
     case QNetworkReply::TooManyRedirectsError:
         errorString = QCoreApplication::translate("QHttp", "Too many redirects");
