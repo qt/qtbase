7fe74410754c88d461829a15171d8287482bc2ff
Alexandru Croitor <alexandru.croitor@qt.io>
2022-06-30 15:51:03 +0200

    CMake: Rewrite double-conversion find module
    Rename FindWrapDoubleConversion.cmake into
    FindWrapSystemDoubleConversion.cmake.
    Merge contents of Finddouble-conversion.cmake into the one above.
    This allows users to provide their own Finddouble-conversion.cmake
    file (Conan can do it).
    Don't mark the system package as required, because we have a bundled
    one too.
    Add link to upstream.
    Make sure to show either Config file or library path when one is
    found.
    
    Fixes: QTBUG-104541
    Change-Id: I9ea2330697c6fc280328849ca11522291c4073d8
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit a310319a0409c1faa1d2fc83c740e009f46dba84)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/configure.cmake
+++ src/corelib/configure.cmake
@@ -19,7 +19,9 @@ if((UNIX AND NOT QNX) OR QT_FIND_ALL_PACKAGES_ALWAYS)
     # offerings
     qt_find_package(WrapBacktrace PROVIDED_TARGETS WrapBacktrace::WrapBacktrace MODULE_NAME core QMAKE_LIB backtrace)
 endif()
-qt_find_package(WrapDoubleConversion PROVIDED_TARGETS WrapDoubleConversion::WrapDoubleConversion MODULE_NAME core QMAKE_LIB doubleconversion)
+qt_find_package(WrapSystemDoubleConversion
+                PROVIDED_TARGETS WrapSystemDoubleConversion::WrapSystemDoubleConversion
+                MODULE_NAME core QMAKE_LIB doubleconversion)
 qt_find_package(GLIB2 PROVIDED_TARGETS GLIB2::GLIB2 MODULE_NAME core QMAKE_LIB glib)
 qt_find_package(ICU COMPONENTS i18n uc data PROVIDED_TARGETS ICU::i18n ICU::uc ICU::data MODULE_NAME core QMAKE_LIB icu)
 if(QT_FEATURE_dlopen)
@@ -531,7 +533,7 @@ qt_feature("doubleconversion" PUBLIC PRIVATE
 qt_feature_definition("doubleconversion" "QT_NO_DOUBLECONVERSION" NEGATE VALUE "1")
 qt_feature("system-doubleconversion" PRIVATE
     LABEL "  Using system DoubleConversion"
-    CONDITION QT_FEATURE_doubleconversion AND WrapDoubleConversion_FOUND
+    CONDITION QT_FEATURE_doubleconversion AND WrapSystemDoubleConversion_FOUND
     ENABLE INPUT_doubleconversion STREQUAL 'system'
     DISABLE INPUT_doubleconversion STREQUAL 'qt'
 )
