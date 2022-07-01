e16f74247958329263b59fe1b9ad8bd2a25de564
Jaroslaw Kubik <jarek@froglogic.com>
2022-07-02 01:03:43 +0200

    Provide a native interface instance in minimal platform plugin
    The minimal platform plugin does not have any native APIs it could offer
    through that interface, but needs to provide an instance anyway. A lot
    of code within Qt assumes that a native interface is always available
    and does not check the pointer value, which leads to segfaults when used
    with the minimal plugin.
    
    Pick-to: 6.4 6.3 6.2 5.15
    Change-Id: I625bd95afd49872cff3a34b29ceb0ffbbcd39db7
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- src/plugins/platforms/minimal/qminimalintegration.cpp
+++ src/plugins/platforms/minimal/qminimalintegration.cpp
@@ -6,6 +6,7 @@
 
 #include <QtGui/private/qpixmap_raster_p.h>
 #include <QtGui/private/qguiapplication_p.h>
+#include <qpa/qplatformnativeinterface.h>
 #include <qpa/qplatformwindow.h>
 #include <qpa/qwindowsysteminterface.h>
 
@@ -155,6 +156,13 @@ QAbstractEventDispatcher *QMinimalIntegration::createEventDispatcher() const
 #else
     return createUnixEventDispatcher();
 #endif
+}
+
+QPlatformNativeInterface *QMinimalIntegration::nativeInterface() const
+{
+    if (!m_nativeInterface)
+        m_nativeInterface.reset(new QPlatformNativeInterface);
+    return m_nativeInterface.get();
 }
 
 QMinimalIntegration *QMinimalIntegration::instance()
