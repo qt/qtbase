f71aeea9329faf33d7161e6af88e2a94a35edb9f
Kai Köhne <kai.koehne@qt.io>
2022-07-12 16:07:23 +0200

    Doc: Hide weak overload template magic from documentation
    Hide the 'template magic' to implement Q_WEAK_OVERLOAD from the
    documentation. So far Q_WEAK_OVERLOAD void foo() lead to
    
      template <typename> void foo()
    
    in the generated documentation, which is arguably confusing to the
    uninitiated. And people interested in implementation details & exact
    overload resolution will arguably just read the .h files themselves.
    
    Fixes: QTBUG-104851
    Pick-to: 6.4 6.3 6.2 5.15
    Change-Id: I5e0b1b337b28e621e6a627241aa8037da0a879a7
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- src/corelib/global/qcompilerdetection.h
+++ src/corelib/global/qcompilerdetection.h
@@ -1027,7 +1027,11 @@
  * "Weak overloads" - makes an otherwise confliciting overload weaker
  * (by making it a template)
  */
-#define Q_WEAK_OVERLOAD template <typename = void>
+#ifndef Q_CLANG_QDOC
+#  define Q_WEAK_OVERLOAD template <typename = void>
+#else
+#  define Q_WEAK_OVERLOAD
+#endif
 
 /*
  * If one wants to add functions that use post-C++17 APIs, one needs to:
