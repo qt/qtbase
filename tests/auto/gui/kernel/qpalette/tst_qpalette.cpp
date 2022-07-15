6321e69024f6d1fa5ec4be9d7663f53c850e1ada
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-07-15 08:50:37 +0200

    QPalette: detach before modifying resolve mask when resolving
    Amends 1d961491d817490da156769ddce6fce48a0bce4a. We modify the resolve
    mask after making a shared copy of 'other', so we must detach. Call the
    setter designed for that purpose.
    
    Include the modified addition to the unit test from 7a5d7e8440a1e4beec
    which is an optimization that wasn't cherry-picked to 6.2.
    
    Task-number: QTBUG-98762
    Change-Id: I4f45223e74764a341378992172787fae73efb8b7
    Reviewed-by: JiDe Zhang <zhangjide@uniontech.com>
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit 37c829f511d67be1c226201b4bbbe73c53835078)
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- tests/auto/gui/kernel/qpalette/tst_qpalette.cpp
+++ tests/auto/gui/kernel/qpalette/tst_qpalette.cpp
@@ -122,6 +122,21 @@ void tst_QPalette::resolve()
 
     QVERIFY(p2ResolvedTo1 != p1);
     QVERIFY(p2ResolvedTo1 != p2);
+
+    QPalette p3;
+    // ensure the resolve mask is full
+    for (int r = 0; r < QPalette::NColorRoles; ++r)
+        p3.setBrush(QPalette::All, QPalette::ColorRole(r), Qt::red);
+    const QPalette::ResolveMask fullMask = p3.resolveMask();
+
+    QPalette p3ResolvedToP1 = p3.resolve(p1);
+    QVERIFY(!p3ResolvedToP1.isCopyOf(p3));
+
+    QPalette p4;
+    QCOMPARE(p4.resolveMask(), QPalette::ResolveMask{});
+    // resolve must detach even if p4 has no mask
+    p4 = p4.resolve(p3);
+    QCOMPARE(p3.resolveMask(), fullMask);
 }
 
 
