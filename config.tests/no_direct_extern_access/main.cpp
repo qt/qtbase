2221b25dac767ad2d3988af37e711c12e1ac9d78
Thiago Macieira <thiago.macieira@intel.com>
2022-07-18 11:01:18 -0700

    CMake: add one more test for no_direct_extern_access
    Found while compiling qtdeclarative tests:
    
    FAILED: qtdeclarative/tests/auto/qml/qmlcppcodegen/tst_qmlcppcodegen
    [...]
    ld: qtdeclarative/tests/auto/qml/qmlcppcodegen/data/TestTypes/libcodegen_test_moduleplugin.a(codegen_test_moduleplugin_TestTypesPlugin.cpp.o): non-canonical reference to canonical protected function `_Z28qml_register_types_TestTypesv' in qtdeclarative/tests/auto/qml/qmlcppcodegen/data/libcodegen_test_module.a(codegen_test_module_qmltyperegistrations.cpp.o)
    ld: failed to set dynamic section sizes: bad value
    collect2: error: ld returned 1 exit status
    ninja: build stopped: subcommand failed.
    
    See https://sourceware.org/bugzilla/show_bug.cgi?id=29377
    
    Pick-to: 6.4
    Change-Id: I3859764fed084846bcb0fffd1702fe6da341a9e3
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- config.tests/no_direct_extern_access/main.cpp
+++ config.tests/no_direct_extern_access/main.cpp
@@ -1,9 +1,17 @@
 // Copyright (C) 2022 Intel Corporation.
 // SPDX-License-Identifier: MIT
 
-// This is the test found in https://sourceware.org/bugzilla/show_bug.cgi?id=29087
+// This is combining the tests found in:
+// https://sourceware.org/bugzilla/show_bug.cgi?id=29087
+// https://sourceware.org/bugzilla/show_bug.cgi?id=29377
 
 #include "lib.h"
+
+extern void foo(); // other.cpp
+void (*get_foo())()
+{
+    return foo;
+}
 
 struct Local : S { };
 int main()
---
+++ config.tests/no_direct_extern_access/other.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2022 Intel Corporation.
+// SPDX-License-Identifier: MIT
+
+// This is the test found in https://sourceware.org/bugzilla/show_bug.cgi?id=29377
+
+__attribute__((visibility("protected"))) void foo()
+{
+}
