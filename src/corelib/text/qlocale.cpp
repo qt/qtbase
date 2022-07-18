11ecff9d4ba7a9b8ed176de713e7f80f05c690c9
Marc Mutz <marc.mutz@qt.io>
2022-07-18 17:20:04 +0200

    Port qt_repeatCount() to qsizetype
    While it's true that the typical user of the function won't expect
    more than half a dozen repeats, this function is fed with
    user-supplied input, so it could be asked to return in excess of
    INT_MAX matches. The truncation then means we're misreporting the
    number mod INT_MAX, which is as good as a random number and leads to
    false positive matches in users of the function.
    
    Just return the true result instead of a truncated one.
    
    Task-number: QTBUG-103531
    Change-Id: I5e3aa23dec873c6f9af255e90748fb38619d2f5d
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit d6b38be3dfa4579cbe99cc442452a45779a71e70)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qlocale.cpp
+++ src/corelib/text/qlocale.cpp
@@ -681,7 +681,7 @@ QString qt_readEscapedFormatString(QStringView format, int *idx)
     qt_repeatCount(u"aab"); // == 2
     \endcode
 */
-int qt_repeatCount(QStringView s)
+qsizetype qt_repeatCount(QStringView s)
 {
     if (s.isEmpty())
         return 0;
@@ -689,7 +689,7 @@ int qt_repeatCount(QStringView s)
     qsizetype j = 1;
     while (j < s.size() && s.at(j) == c)
         ++j;
-    return int(j);
+    return j;
 }
 
 static const QLocaleData *default_data = nullptr;
@@ -3358,7 +3358,7 @@ QString QCalendarBackend::dateTimeToString(QStringView format, const QDateTime &
         }
 
         const QChar c = format.at(i);
-        int repeat = qt_repeatCount(format.mid(i));
+        qsizetype repeat = qt_repeatCount(format.mid(i));
         bool used = false;
         if (formatDate) {
             switch (c.unicode()) {
