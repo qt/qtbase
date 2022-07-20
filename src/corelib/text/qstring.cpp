e08167d74d4f34e52ba8a8d5f7154b01a727c9d5
Marc Mutz <marc.mutz@qt.io>
2022-07-20 22:16:30 +0200

    Fix QString::arg() for format strings > 2Gi characters
    The getEscape function truncated the qsizetype position it got from
    the caller to an int, potentially parsing a previous escape sequence
    (or, more likely, garbage), by getting the position in the string
    wrong.
    
    Task-number: QTBUG-103531
    Change-Id: I3b921c0991d238bbacfe6699c56146fe032134df
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 841ff3b73f9951e49b6406007d76302b3f367f55)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qstring.cpp
+++ src/corelib/text/qstring.cpp
@@ -8364,7 +8364,7 @@ static inline char16_t to_unicode(const char c) { return QLatin1Char{c}.unicode(
 template <typename Char>
 static int getEscape(const Char *uc, qsizetype *pos, qsizetype len, int maxNumber = 999)
 {
-    int i = *pos;
+    qsizetype i = *pos;
     ++i;
     if (i < len && uc[i] == QLatin1Char('L'))
         ++i;
