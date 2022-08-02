936af3ef8a3e5c639554aeed4ad68f2e9b2bd99b
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2022-08-02 20:55:45 +0200

    macOS: Don't ifdef out code to resolve display name that might be used
    The call to displayName is not guarded by the same ifdef.
    
    Change-Id: I169777130ec316a2d2f837621280acb061072501
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    (cherry picked from commit b9a9edc52fd88745f6f45c76e48f1c2e32677e5c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/cocoa/qcocoascreen.mm
+++ src/plugins/platforms/cocoa/qcocoascreen.mm
@@ -219,7 +219,6 @@ QCocoaScreen::~QCocoaScreen()
          dispatch_release(m_displayLinkSource);
 }
 
-#if QT_MACOS_DEPLOYMENT_TARGET_BELOW(__MAC_10_15)
 static QString displayName(CGDirectDisplayID displayID)
 {
     QIOType<io_iterator_t> iterator;
@@ -251,7 +250,6 @@ static QString displayName(CGDirectDisplayID displayID)
 
     return QString();
 }
-#endif
 
 void QCocoaScreen::update(CGDirectDisplayID displayId)
 {
