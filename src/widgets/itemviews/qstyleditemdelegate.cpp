c53ae39af577df3cc978c41b1fc523c8d7821154
Paul Wicking <paul.wicking@qt.io>
2022-08-05 15:32:45 +0200

    Doc: Drop parentheses around \since command
    The closing parenthesis ends up in the output.
    As the fix to QDoc is rather involved, clean up the docs
    to fix the symptom.
    
    Fixes: QTBUG-105339
    Change-Id: I35a1e7615125781d950649213c08e5760b0235e7
    Reviewed-by: Nicholas Bennett <nicholas.bennett@qt.io>
    (cherry picked from commit 1a825d884907c7c30064b655bc1dda76d5009d87)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/itemviews/qstyleditemdelegate.cpp
+++ src/widgets/itemviews/qstyleditemdelegate.cpp
@@ -151,7 +151,7 @@ public:
     \row    \li \l Qt::AccessibleDescriptionRole \li QString
     \row    \li \l Qt::AccessibleTextRole \li QString
     \endomit
-    \row    \li \l Qt::BackgroundRole \li QBrush (\since 4.2)
+    \row    \li \l Qt::BackgroundRole \li QBrush \since 4.2
     \row    \li \l Qt::CheckStateRole \li Qt::CheckState
     \row    \li \l Qt::DecorationRole \li QIcon, QPixmap, QImage and QColor
     \row    \li \l Qt::DisplayRole \li QString and types with a string representation
@@ -162,7 +162,7 @@ public:
     \row    \li \l Qt::StatusTipRole \li
     \endomit
     \row    \li \l Qt::TextAlignmentRole \li Qt::Alignment
-    \row    \li \l Qt::ForegroundRole \li QBrush (\since 4.2)
+    \row    \li \l Qt::ForegroundRole \li QBrush \since 4.2
     \omit
     \row    \li \l Qt::ToolTipRole
     \row    \li \l Qt::WhatsThisRole
