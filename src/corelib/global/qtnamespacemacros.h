c834c2b91823116eddc9f96b68251446e210bf79
Sona Kurazyan <sona.kurazyan@qt.io>
2022-08-09 18:53:13 +0200

    Remove qtnamespacemacros.h and use qtconfigmacros.h instead
    Change-Id: Ibae6e6e255c1bb1ee52839a051d585de81833bf9
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 4842cc176881ae22e14ca193fba46c6a04d09530)

--- src/corelib/global/qtnamespacemacros.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2022 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-
-#ifndef QTNAMESPACEMACROS_H
-#define QTNAMESPACEMACROS_H
-
-#include <QtCore/qtconfigmacros.h>
-
-#endif /* QTNAMESPACEMACROS_H */
