62859ccaa666909c2db8ac4d0a46fa0f5163153f
Ivan Solovev <ivan.solovev@qt.io>
2022-08-09 12:40:05 +0200

    QtSql: establish removed_api.cpp for upcoming QT_SQL_REMOVED_SINCE
    Pick-to: 6.4
    Change-Id: I6614ed13deee733e0cc53b3d8bb6514b2c000a9b
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

---
+++ src/sql/compat/removed_api.cpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#define QT_SQL_BUILD_REMOVED_API
+
+#include "qtsqlglobal.h"
+
+QT_USE_NAMESPACE
+
+#if QT_SQL_REMOVED_SINCE(6, 4)
+
+// #include <qotherheader.h>
+// // implement removed functions from qotherheader.h
+// order sections alphabetically to reduce chances of merge conflicts
+
+#endif // QT_SQL_REMOVED_SINCE(6, 4)
+
+#if QT_SQL_REMOVED_SINCE(6, 5)
+
+// #include <qotherheader.h>
+// // implement removed functions from qotherheader.h
+// order sections alphabetically to reduce chances of merge conflicts
+
+#endif // QT_SQL_REMOVED_SINCE(6, 5)
