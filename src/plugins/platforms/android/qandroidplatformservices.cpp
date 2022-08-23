2f20a58dadd1d905cce96c6db54e1817f94225a6
Assam Boudjelthia <assam.boudjelthia@qt.io>
2022-08-23 16:50:45 +0300

    Android: properly retrieve mime type of uri to for openUrl()
    Retrieve the mime type of the url regardless of whether QFile::exists()
    returns true or false, because it is nonetheless required when calling
    openUrl().
    
    Fixes: QTBUG-47979
    Change-Id: Ia095b76d5d39addb0b115eb97ac6bbae0c18a21f
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Samuel Mira <samuel.mira@qt.io>
    Reviewed-by: Konstantin Ritt <ritt.ks@gmail.com>
    Reviewed-by: Rami Potinkara <rami.potinkara@qt.io>
    Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>
    (cherry picked from commit 6f418df9cc931032ffc32a15b6ff268452fe9f01)
    Reviewed-by: BogDan Vatra <bogdan@kdab.com>

--- src/plugins/platforms/android/qandroidplatformservices.cpp
+++ src/plugins/platforms/android/qandroidplatformservices.cpp
@@ -59,12 +59,13 @@ bool QAndroidPlatformServices::openUrl(const QUrl &theUrl)
     // if the file is local, we need to pass the MIME type, otherwise Android
     // does not start an Intent to view this file
     QLatin1String fileScheme("file");
-    if ((url.scheme().isEmpty() || url.scheme() == fileScheme) && QFile::exists(url.path())) {
-        // a real URL including the scheme is needed, else the Intent can not be started
+
+    // a real URL including the scheme is needed, else the Intent can not be started
+    if (url.scheme().isEmpty())
         url.setScheme(fileScheme);
-        QMimeDatabase mimeDb;
-        mime = mimeDb.mimeTypeForUrl(url).name();
-    }
+
+    if (url.scheme() == fileScheme)
+        mime = QMimeDatabase().mimeTypeForUrl(url).name();
 
     QJNIObjectPrivate urlString = QJNIObjectPrivate::fromString(url.toString());
     QJNIObjectPrivate mimeString = QJNIObjectPrivate::fromString(mime);
