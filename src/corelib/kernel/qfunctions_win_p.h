848e64ea8abb39228e9c446da19ff46c7f4d4023
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2022-08-25 09:36:25 +0200

    Add qfunctions_win_p.h header for Windows helper functions
    For now just includes qfunctions_winrt_p.h
    
    Once submodules have been moved over to qfunctions_win_p.h we can move
    non-WinRT specific content from qfunctions_winrt_p.h to _win_p.h.
    
    Change-Id: I467bb4991c67a8769b60b9cf9f26aa553c439b92
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

---
+++ src/corelib/kernel/qfunctions_win_p.h
@@ -0,0 +1,24 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QFUNCTIONS_WIN_P_H
+#define QFUNCTIONS_WIN_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#if defined(Q_OS_WIN)
+
+#include <QtCore/private/qfunctions_winrt_p.h>
+
+#endif // Q_OS_WIN
+
+#endif // QFUNCTIONS_WIN_P_H
