825ef509c5527a90c12b15537d4eaa382cd63f1a
Timur Pocheptsov <timur.pocheptsov@qt.io>
2022-08-29 13:02:58 +0200

    QHttp2Configuration: in QNAM, use old, higher values in flow control
    Initially, stream receive window's size was quite significant.
    At some point when dealing with a server that did not want
    our windows' sizes and was closing the connection (they only
    accept 64K) we reduced this size, which ended in a regression
    with download speed significantly throttled. We return the old
    values (or even more, presuming we have 10 multiplexed streams
    and not 100). And also making QNAM consistent with its documentation
    (it was not updated).
    
    [ChangeLog][QtNetwork] Stream receive window that HTTP/2 protocol in QNAM is using
    increased to 214748364 octets (from the previous 64K) not to throttle download
    speed. Clients, working with servers, not accepting such parameters,
    must set HTTP/2 configuration on their requests accordingly.
    
    Fixes: QTBUG-105043
    Change-Id: I252b6b5eefe92a7304dad15c67928d5a57d9597f
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 9540a4eb9071a17446c6b68e086bbea2b50d50f5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/network/access/http2/tst_http2.cpp
+++ tests/auto/network/access/http2/tst_http2.cpp
@@ -92,6 +92,7 @@ public slots:
     void init();
 private slots:
     // Tests:
+    void defaultQnamHttp2Configuration();
     void singleRequest_data();
     void singleRequest();
     void multipleRequests();
@@ -229,6 +230,12 @@ tst_Http2::~tst_Http2()
 void tst_Http2::init()
 {
     manager.reset(new QNetworkAccessManager);
+}
+
+void tst_Http2::defaultQnamHttp2Configuration()
+{
+    // The configuration we also implicitly use in QNAM.
+    QCOMPARE(qt_defaultH2Configuration(), QNetworkRequest().http2Configuration());
 }
 
 void tst_Http2::singleRequest_data()
