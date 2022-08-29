c5bb2f746b23a5055713647ec0962bdda58df869
Mike Trahearn <mtrahearn@topcon.com>
2022-08-29 17:18:06 +1000

    Fix the spelling of the word "combination"
    Change-Id: Ib6ad11d4fbbeefa280070125fd0ee6a64dababae
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 20242a254987414566ec852a7e05c19041b90024)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/kernel/qmetatype.cpp
+++ src/corelib/kernel/qmetatype.cpp
@@ -418,7 +418,7 @@ struct DefinedTypesFilter {
 
     If the type is a pointer type to a subclass of QObject, flags() contains
     QMetaType::PointerToQObject and this function returns the corresponding QMetaObject. This can
-    be used to in combinaison with QMetaObject::construct to create QObject of this type.
+    be used to in combination with QMetaObject::construct to create QObject of this type.
 
     If the type is a Q_GADGET, flags() contains QMetaType::IsGadget, and this function returns its
     QMetaObject.  This can be used to retrieve QMetaMethod and QMetaProperty and use them on a
