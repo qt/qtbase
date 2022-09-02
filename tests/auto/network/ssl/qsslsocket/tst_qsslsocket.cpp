ba32713e42d11ae2ef4a994011208fe82c852af0
Timur Pocheptsov <timur.pocheptsov@qt.io>
2022-09-02 13:50:49 +0200

    tst_QSslSocket::oldErrorsOnSocketReuse - make it work with OpenSSL v3
    The initial problem was in server not starting encryption, because
    its certificate was rejected by OpenSSL (v3) saying 'weak md'.
    After the certificate was replaced, we got another problem - due
    to the fixed TLS v.1 protocol for both client and server - handshake
    is interrupted early with 'tls v1 alert' and error message
    saying about not found signature algorithms. Don't fix the protocol
    version, use proper QSsl::SecureProtocols (default).
    
    Task-number: QTBUG-106018
    Task-number: QTBUG-95123
    Change-Id: I0f22f5304f4405933ad511195bc61c640518e7d8
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit a7657d3c5ff1aafe347209dc6b76bd6d127694d9)
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- tests/auto/network/ssl/qsslsocket/tst_qsslsocket.cpp
+++ tests/auto/network/ssl/qsslsocket/tst_qsslsocket.cpp
@@ -4538,13 +4538,15 @@ void tst_QSslSocket::oldErrorsOnSocketReuse()
     if (setProxy)
         return; // not relevant
     SslServer server;
-    server.protocol = QSsl::TlsV1_1;
+    if (!isTestingOpenSsl)
+        server.protocol = QSsl::TlsV1_1;
     server.m_certFile = testDataDir + "certs/fluke.cert";
     server.m_keyFile = testDataDir + "certs/fluke.key";
     QVERIFY(server.listen(QHostAddress::SpecialAddress::LocalHost));
 
     QSslSocket socket;
-    socket.setProtocol(QSsl::TlsV1_1);
+    if (!isTestingOpenSsl)
+        socket.setProtocol(QSsl::TlsV1_1);
     QList<QSslError> errorList;
     auto connection = connect(&socket, QOverload<const QList<QSslError> &>::of(&QSslSocket::sslErrors),
         [&socket, &errorList](const QList<QSslError> &errors) {
