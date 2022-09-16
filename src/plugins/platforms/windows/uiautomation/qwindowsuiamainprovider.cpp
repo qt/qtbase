6a9226b430d2bca99a20deed9a45eade930e270b
Jannis Xiong <fly2xj@msn.com>
2022-09-16 11:29:49 +0800

    Fix cache maybe invalid while the signal is actived from queue
    with default QObject::connect signal may active from next message loop. invalide cache will hit while accessibility interface is called from windows. Invalide cache will lead to a crash
    
    Fixes: QTBUG-106653
    Change-Id: I5359672bcd60ed6cfb2edf238645225164cb1b88
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
    (cherry picked from commit 80f44954f6872afb5aa37e6737c3e1ac68ad3577)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/windows/uiautomation/qwindowsuiamainprovider.cpp
+++ src/plugins/platforms/windows/uiautomation/qwindowsuiamainprovider.cpp
@@ -74,10 +74,13 @@ QT_BEGIN_NAMESPACE
 
 using namespace QWindowsUiAutomation;
 
+QMutex QWindowsUiaMainProvider::m_mutex;
 
 // Returns a cached instance of the provider for a specific accessible interface.
 QWindowsUiaMainProvider *QWindowsUiaMainProvider::providerForAccessible(QAccessibleInterface *accessible)
 {
+    QMutexLocker locker(&m_mutex);
+
     if (!accessible)
         return nullptr;
 
@@ -275,6 +278,8 @@ ULONG QWindowsUiaMainProvider::AddRef()
 
 ULONG STDMETHODCALLTYPE QWindowsUiaMainProvider::Release()
 {
+    QMutexLocker locker(&m_mutex);
+
     if (!--m_ref) {
         delete this;
         return 0;
