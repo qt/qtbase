26b13a26e16da7bbea6bc69e7ad8d0becbb89f6b
Alexey Edelev <alexey.edelev@qt.io>
2022-09-26 17:45:56 +0200

    Fix condition that runs the scanning of the Qml imports
    Scanning of the Qml imports should be run if either Qml root paths or
    .qrc files are specified for androiddeployqt.
    
    Task-number: QTBUG-106939
    Change-Id: I3229ec6d3f2ac17ef751b4a6ed74e8e98db7465e
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry-picked from commit 24f68a0deb7aea19d9d80391a35c76043ed7532e)
    Reviewed-by: Daniel Smith <Daniel.Smith@qt.io>

--- src/tools/androiddeployqt/main.cpp
+++ src/tools/androiddeployqt/main.cpp
@@ -2204,7 +2204,7 @@ bool readDependencies(Options *options)
         }
     }
 
-    if ((!options->rootPaths.empty() || options->qrcFiles.isEmpty()) &&
+    if ((!options->rootPaths.empty() || !options->qrcFiles.isEmpty()) &&
         !scanImports(options, &usedDependencies))
         return false;
 
