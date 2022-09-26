qt/qtbase

commit 9cd3ff2bdea0d5a009a8284288bbbdaaaec817be

macOS: Mark QFileDialog::DontConfirmOverwrite as unsupported
The native NSSavePanel does not have an option corresponding
to DontConfirmOverwrite. Qt has a workaround where
the save-file-name is temporarily given a prefix in
order do bypass the overwrite check, however this has
stopped working on recent versions of macOS. (Confirmed
on macOS 11)

Users who want to use DontConfirmOverwrite should use
Qt’s file dialog instead.

Fixes: QTBUG-39791
Pick-to: 5.15 6.0
Change-Id: Id3b9e6de72c2afc9526c96f1d9fdbce78db92aeb
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/widgets/dialogs/qfiledialog.cpp
@@ -226,6 +226,9 @@ Q_GLOBAL_STATIC(QUrl, lastVisitedDir)
     \value DontConfirmOverwrite Don't ask for confirmation if an
     existing file is selected.  By default confirmation is requested.
 
+    Note: This opption is not supported on macOS when using the
+    native file dialog.
+
     \value DontUseNativeDialog Don't use the native file dialog. By
     default, the native file dialog is used unless you use a subclass
     of QFileDialog that contains the Q_OBJECT macro, or the platform

commit 12ff1888b16d91db9227d856d38d9b5740f7e191

Doc: Fix typo in QFileDialog documentation
Fix typo introduced in 9cd3ff2bdea

Pick-to: 6.2 6.4
Change-Id: I248779c06fff81aaea1ef8f0a3a412cfb7cfc0bc
Reviewed-by: Alex Blasche <alexander.blasche@qt.io>

src/widgets/dialogs/qfiledialog.cpp
@@ -193,7 +193,7 @@ Q_GLOBAL_STATIC(QUrl, lastVisitedDir)
     \value DontConfirmOverwrite Don't ask for confirmation if an
     existing file is selected.  By default confirmation is requested.
 
-    Note: This opption is not supported on macOS when using the
+    Note: This option is not supported on macOS when using the
     native file dialog.
 
     \value DontUseNativeDialog Don't use the native file dialog. By