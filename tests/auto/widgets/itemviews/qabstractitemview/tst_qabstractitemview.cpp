a3080fcd31640127dbfa8d4cb135e3a615f86aab
Liang Qi <liang.qi@qt.io>
2022-10-04 11:41:45 +0200

    tests: skip two tests in tst_QAbstractItemView on Wayland
    QWindow::requestActivate() is not supported.
    
    Task-number: QTBUG-107153
    Change-Id: I9080fbb0ae0a604ad4a7ffa55ba3243d1cf96be8
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
    (cherry picked from commit 7d38320c4b634f140442bbec51ac45a140aafa03)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/widgets/itemviews/qabstractitemview/tst_qabstractitemview.cpp
+++ tests/auto/widgets/itemviews/qabstractitemview/tst_qabstractitemview.cpp
@@ -3103,6 +3103,9 @@ void tst_QAbstractItemView::mouseSelection()
 */
 void tst_QAbstractItemView::scrollerSmoothScroll()
 {
+    if (!QGuiApplicationPrivate::platformIntegration()->hasCapability(QPlatformIntegration::WindowActivation))
+        QSKIP("QWindow::requestActivate() is not supported.");
+
     QListWidget view;
     view.setAutoScroll(true);
     view.setVerticalScrollMode(QListView::ScrollPerPixel);
@@ -3172,6 +3175,9 @@ void tst_QAbstractItemView::inputMethodOpensEditor_data()
 
 void tst_QAbstractItemView::inputMethodOpensEditor()
 {
+    if (!QGuiApplicationPrivate::platformIntegration()->hasCapability(QPlatformIntegration::WindowActivation))
+        QSKIP("QWindow::requestActivate() is not supported.");
+
     QTableWidget tableWidget(50, 50);
     tableWidget.setEditTriggers(QAbstractItemView::AnyKeyPressed);
     for (int r = 0; r < 50; ++r) {
