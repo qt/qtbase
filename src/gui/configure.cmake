aeddd791f9f3dd852cefa4ddcc04b8dd4354e559
Eric Lemanissier <eric.lemanissier@gmail.com>
2022-10-05 08:42:56 +0000

    fix fontconfig detection
    the "official" variable name is Fontconfig_FOUND
    cf https://cmake.org/cmake/help/latest/module/FindFontconfig.html
    
    Change-Id: I40ec178a18baabe47d8d66845ff03add9efc0e51
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 539165dae99d996746801da1cd8a09267cda3c42)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/configure.cmake
+++ src/gui/configure.cmake
@@ -661,7 +661,7 @@ qt_feature("system-freetype" PRIVATE
 qt_feature("fontconfig" PUBLIC PRIVATE
     LABEL "Fontconfig"
     AUTODETECT NOT APPLE
-    CONDITION NOT WIN32 AND QT_FEATURE_system_freetype AND FONTCONFIG_FOUND
+    CONDITION NOT WIN32 AND QT_FEATURE_system_freetype AND Fontconfig_FOUND
 )
 qt_feature_definition("fontconfig" "QT_NO_FONTCONFIG" NEGATE VALUE "1")
 qt_feature("gbm"
