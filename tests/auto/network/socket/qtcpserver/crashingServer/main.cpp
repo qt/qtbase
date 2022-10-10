f7e0c2cd5e561621b49b633fb9c98c20965bf998
Thiago Macieira <thiago.macieira@intel.com>
2022-10-10 13:39:19 -0700

    tst_QTcpServer: use a random port number in addressReusable
    Just in case the same test is being run in parallel. We do that by
    creating a listening TCP server in the test process. This test is
    supposed to test the address reusability, so a clean close on a server
    that never accepted a connection should not cause reusability issues.
    
    Change-Id: I12a088d1ae424825abd3fffd171ccfb9fc5c09ee
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit a94731c2ad85f9dd40050a780f67c911bf12668e)
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- tests/auto/network/socket/qtcpserver/crashingServer/main.cpp
+++ tests/auto/network/socket/qtcpserver/crashingServer/main.cpp
@@ -43,16 +43,21 @@ int main(int argc, char *argv[])
     _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
 #endif
     QCoreApplication app(argc, argv);
+    if (argc < 1) {
+        fprintf(stderr, "Need a port number\n");
+        return 1;
+    }
 
+    int port = QByteArray(argv[1]).toInt();
     QTcpServer server;
-    if (!server.listen(QHostAddress::LocalHost, 49199)) {
+    if (!server.listen(QHostAddress::LocalHost, port)) {
         fprintf(stderr, "Failed to listen: %s\n", server.errorString().toLatin1().constData());
         if (server.serverError() == QTcpSocket::AddressInUseError) {
             // let's see if we can find the process that would be holding this
             // still open
 #ifdef Q_OS_LINUX
             static const char *ss_args[] = {
-                "ss", "-nap", "sport", "=", ":49199", nullptr
+                "ss", "-nap", "sport", "=", argv[1], nullptr
             };
             dup2(STDERR_FILENO, STDOUT_FILENO);
             execvp(ss_args[0], const_cast<char **>(ss_args));
