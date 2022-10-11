c849c48d19cc0b086f98688d760fa6e008adc50e
Thiago Macieira <thiago.macieira@intel.com>
2022-10-11 09:38:42 -0700

    Autotest/Unix: request zero-sized core dumps for crashing code
    Unix systems have got crash loggers in the past 15-20 years, notably
    macOS and Linux (abrtd, systemd-coredumpd, etc.). By setting the core
    dump limit to zero, those tools should be mostly inhibited from running
    and thus not interfere with the parent process' timeouts. Even for
    systems without core dump loggers, disabling the writing of a core dump
    to the filesystem should also help.
    
    Pick-to: 6.4
    Change-Id: I12a088d1ae424825abd3fffd171d112d0671effe
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- tests/auto/corelib/io/qprocess/testProcessCrash/main.cpp
+++ tests/auto/corelib/io/qprocess/testProcessCrash/main.cpp
@@ -2,6 +2,22 @@
 // Copyright (C) 2020 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
+#if __has_include(<sys/resource.h>)
+#  include <sys/resource.h>
+#  if defined(RLIMIT_CORE)
+static bool disableCoreDumps()
+{
+    // Unix: set our core dump limit to zero to request no dialogs.
+    if (struct rlimit rlim; getrlimit(RLIMIT_CORE, &rlim) == 0) {
+        rlim.rlim_cur = 0;
+        setrlimit(RLIMIT_CORE, &rlim);
+    }
+    return true;
+}
+static bool disabledCoreDumps = disableCoreDumps();
+#  endif // RLIMIT_CORE
+#endif // <sys/resource.h>
+
 void crashFallback(volatile int *ptr = nullptr)
 {
     *ptr = 0;
