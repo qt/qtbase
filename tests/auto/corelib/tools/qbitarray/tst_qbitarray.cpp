93503b0c525c1a2af5dcc0561562a239ec71d70c
Marc Mutz <marc.mutz@qt.io>
2022-10-17 08:00:15 +0200

    tst_Q{BitArray,ContiguousCache}: check not only count(), but size(), too
    Add at least a few, so size() isn't completely untested.
    
    Change-Id: I500d28f7efb30ab578808d8fefb6ea57949edc2e
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit f5205bd6a409d736942a4eed8869245cb9646d35)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/corelib/tools/qbitarray/tst_qbitarray.cpp
+++ tests/auto/corelib/tools/qbitarray/tst_qbitarray.cpp
@@ -143,6 +143,7 @@ void tst_QBitArray::countBits()
             bits.setBit(i);
     }
 
+    QCOMPARE(bits.size(), numBits);
     QCOMPARE(bits.count(), numBits);
     QCOMPARE(bits.count(true), onBits);
     QCOMPARE(bits.count(false), numBits - onBits);
