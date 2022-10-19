qt/qtbase

commit 8d4eb292b2e8fc14437db97febdc2eebe36ed3ce

QLibrary: Use QTaggedPointer for the did_load flag
This makes the size nicely aligned and conveniently intializes
everything to the right values.

Change-Id: Ibad2defbbd323fd5cdd4bed8374f40558d80acc0
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/plugin/qlibrary.cpp
@@ -825,9 +825,10 @@ bool QLibrary::load()
 {
     if (!d)
         return false;
-    if (did_load)
+    if (d.tag() == Loaded)
         return d->pHnd.loadRelaxed();
-    did_load = true;
+    else
+        d.setTag(Loaded);
     return d->load();
 }
 
@@ -848,8 +849,8 @@ bool QLibrary::load()
 */
 bool QLibrary::unload()
 {
-    if (did_load) {
-        did_load = false;
+    if (d.tag() == Loaded) {
+        d.setTag(NotLoaded);
         return d->unload();
     }
     return false;
@@ -869,8 +870,7 @@ bool QLibrary::isLoaded() const
 /*!
     Constructs a library with the given \a parent.
  */
-QLibrary::QLibrary(QObject *parent)
-    :QObject(parent), d(nullptr), did_load(false)
+QLibrary::QLibrary(QObject *parent) : QObject(parent)
 {
 }
 
@@ -884,8 +884,7 @@ QLibrary::QLibrary(QObject *parent)
     suffix in accordance with the platform, e.g. ".so" on Unix,
     ".dylib" on \macos and iOS, and ".dll" on Windows. (See \l{fileName}.)
  */
-QLibrary::QLibrary(const QString& fileName, QObject *parent)
-    :QObject(parent), d(nullptr), did_load(false)
+QLibrary::QLibrary(const QString& fileName, QObject *parent) : QObject(parent)
 {
     setFileName(fileName);
 }
@@ -901,8 +900,7 @@ QLibrary::QLibrary(const QString& fileName, QObject *parent)
     suffix in accordance with the platform, e.g. ".so" on Unix,
     ".dylib" on \macos and iOS, and ".dll" on Windows. (See \l{fileName}.)
 */
-QLibrary::QLibrary(const QString& fileName, int verNum, QObject *parent)
-    :QObject(parent), d(nullptr), did_load(false)
+QLibrary::QLibrary(const QString& fileName, int verNum, QObject *parent) : QObject(parent)
 {
     setFileNameAndVersion(fileName, verNum);
 }
@@ -918,7 +916,7 @@ QLibrary::QLibrary(const QString& fileName, int verNum, QObject *parent)
     ".dylib" on \macos and iOS, and ".dll" on Windows. (See \l{fileName}.)
  */
 QLibrary::QLibrary(const QString& fileName, const QString &version, QObject *parent)
-    :QObject(parent), d(nullptr), did_load(false)
+    : QObject(parent)
 {
     setFileNameAndVersion(fileName, version);
 }
@@ -965,8 +963,7 @@ void QLibrary::setFileName(const QString &fileName)
     if (d) {
         lh = d->loadHints();
         d->release();
-        d = nullptr;
-        did_load = false;
+        d = {};
     }
     d = QLibraryPrivate::findOrCreate(fileName, QString(), lh);
 }
@@ -995,8 +992,7 @@ void QLibrary::setFileNameAndVersion(const QString &fileName, int verNum)
     if (d) {
         lh = d->loadHints();
         d->release();
-        d = nullptr;
-        did_load = false;
+        d = {};
     }
     d = QLibraryPrivate::findOrCreate(fileName, verNum >= 0 ? QString::number(verNum) : QString(), lh);
 }
@@ -1016,8 +1012,7 @@ void QLibrary::setFileNameAndVersion(const QString &fileName, const QString &ver
     if (d) {
         lh = d->loadHints();
         d->release();
-        d = nullptr;
-        did_load = false;
+        d = {};
     }
     d = QLibraryPrivate::findOrCreate(fileName, version, lh);
 }

src/corelib/plugin/qlibrary.h
@@ -41,6 +41,7 @@
 #define QLIBRARY_H
 
 #include <QtCore/qobject.h>
+#include <QtCore/qtaggedpointer.h>
 
 QT_REQUIRE_CONFIG(library);
 
@@ -92,8 +93,12 @@ public:
     void setLoadHints(LoadHints hints);
     LoadHints loadHints() const;
 private:
-    QLibraryPrivate *d;
-    bool did_load;
+    enum LoadStatusTag {
+        NotLoaded,
+        Loaded
+    };
+
+    QTaggedPointer<QLibraryPrivate, LoadStatusTag> d = nullptr;
     Q_DISABLE_COPY(QLibrary)
 };
 

commit 823b7c7bce1aae5d413c9540c4916c7b1f393300

QLibrary: fix load() after a failed load()
Regression introduced by commit 8d4eb292b2e8fc14437db97febdc2eebe36ed3ce
in 6.0, when QTaggedPointer was introduced. We set the tag even when the
loading failed and failed to reset it because d = {} retains the tag.

Pick-to: 6.2 6.4
Fixes: QTBUG-103387
Change-Id: Ie4bb662dcb274440ab8bfffd170a07aa9c9ecfca
Reviewed-by: Marc Mutz <marc.mutz@qt.io>

src/corelib/plugin/qlibrary.cpp
@@ -776,9 +776,11 @@ bool QLibrary::load()
         return false;
     if (d.tag() == Loaded)
         return d->pHnd.loadRelaxed();
-    else
+    if (d->load()) {
         d.setTag(Loaded);
-    return d->load();
+        return true;
+    }
+    return false;
 }
 
 /*!

tests/auto/corelib/plugin/qlibrary/tst_qlibrary.cpp
@@ -90,10 +90,14 @@ private slots:
     void unload_data();
     void unload();
     void unload_after_implicit_load();
+    void setFilenameAfterFailedLoad();
+    void loadAfterFailedLoad();
     void isLibrary_data();
     void isLibrary();
     void version_data();
     void version();
+    void setFileNameAndVersionAfterFailedLoad_data() { version_data(); }
+    void setFileNameAndVersionAfterFailedLoad();
     void errorString_data();
     void errorString();
     void loadHints();
@@ -270,6 +274,76 @@ void tst_QLibrary::unload_after_implicit_load()
     QVERIFY(library.isLoaded());
     QVERIFY(library.unload());
     QCOMPARE(library.isLoaded(), false);
+}
+
+void tst_QLibrary::setFilenameAfterFailedLoad()
+{
+#if defined(Q_OS_WIN) || defined(Q_OS_ANDROID)
+    QSKIP("### FIXME: The helper libraries are currently messed up in the CMakeLists.txt");
+#endif
+
+    QLibrary library(directory + "/nolib");
+    QVERIFY(!library.load());
+    QVERIFY(!library.isLoaded());
+    QVERIFY(!library.load());
+    QVERIFY(!library.isLoaded());
+
+    library.setFileName(directory + "/mylib");
+    QVERIFY(library.load());
+    QVERIFY(library.isLoaded());
+    auto p = (VersionFunction)library.resolve("mylibversion");
+    QVERIFY(p);
+    QCOMPARE(p(), 2);
+    library.unload();
+}
+
+void tst_QLibrary::setFileNameAndVersionAfterFailedLoad()
+{
+    QLibrary library(directory + "/nolib");
+    QVERIFY(!library.load());
+    QVERIFY(!library.isLoaded());
+    QVERIFY(!library.load());
+    QVERIFY(!library.isLoaded());
+
+#if !defined(Q_OS_AIX) && !defined(Q_OS_WIN)
+    QFETCH(QString, lib);
+    QFETCH(int, loadversion);
+    QFETCH(int, resultversion);
+
+    library.setFileNameAndVersion(directory + '/' + lib, loadversion);
+    QVERIFY(library.load());
+    QVERIFY(library.isLoaded());
+    auto p = (VersionFunction)library.resolve("mylibversion");
+    QVERIFY(p);
+    QCOMPARE(p(), resultversion);
+    library.unload();
+#endif
+}
+
+void tst_QLibrary::loadAfterFailedLoad()
+{
+#if defined(Q_OS_WIN) || defined(Q_OS_ANDROID)
+    QSKIP("### FIXME: The helper libraries are currently messed up in the CMakeLists.txt");
+#endif
+
+    QTemporaryDir dir;
+    QLibrary library(dir.path() + "/mylib");
+    QVERIFY(!library.load());
+    QVERIFY(!library.isLoaded());
+    QVERIFY(!library.load());
+    QVERIFY(!library.isLoaded());
+
+    // now copy the actual lib file into our dir
+    QString actualLib = PREFIX "mylib" SUFFIX;
+    QVERIFY(QFile::copy(directory + '/' + actualLib, dir.filePath(actualLib)));
+
+    // try again, must succeed now
+    QVERIFY(library.load());
+    QVERIFY(library.isLoaded());
+    auto p = (VersionFunction)library.resolve("mylibversion");
+    QVERIFY(p);
+    QCOMPARE(p(), 2);
+    library.unload();
 }
 
 void tst_QLibrary::resolve_data()