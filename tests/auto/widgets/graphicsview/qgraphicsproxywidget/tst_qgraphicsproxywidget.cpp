d8c078c2b6135e1ccd4b4b676b0d229e23e22c11
Liang Qi <liang.qi@qt.io>
2022-10-20 11:33:39 +0200

    tests: skip tst_QGraphicsProxyWidget::scrollUpdate() on Wayland
    QWindow::requestActivate() is not supported.
    
    This function failed in test vm in coin manually very often.
    
    Task-number: QTBUG-107153
    Pick-to: 6.4 6.2
    Change-Id: I013651b0e5e7618c29742effd85a091ca95a7414
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

--- tests/auto/widgets/graphicsview/qgraphicsproxywidget/tst_qgraphicsproxywidget.cpp
+++ tests/auto/widgets/graphicsview/qgraphicsproxywidget/tst_qgraphicsproxywidget.cpp
@@ -1326,6 +1326,9 @@ static QList<QRect> rects(const QRegion &region)
 
 void tst_QGraphicsProxyWidget::scrollUpdate()
 {
+    if (!QGuiApplicationPrivate::platformIntegration()->hasCapability(QPlatformIntegration::WindowActivation))
+        QSKIP("QWindow::requestActivate() is not supported.");
+
     ScrollWidget *widget = new ScrollWidget;
 
     QGraphicsScene scene;
