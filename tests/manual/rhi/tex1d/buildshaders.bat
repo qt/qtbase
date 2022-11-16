427152c9b9e12d74fc5c7526148d09c5fba9263b
Laszlo Agocs <laszlo.agocs@qt.io>
2022-11-16 09:43:02 +0100

    rhi: tex1d manual test: Also build GLSL 120 shaders
    This way we can run the test on macOS with the version 2.1
    OpenGL context.
    
    Amends 85a1663eb15cd85f91fe1f6dd924588d9cc7c2a8
    
    Change-Id: I8ec122fefaab54b35613e226e3937f4b51a7ea5a
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- tests/manual/rhi/tex1d/buildshaders.bat
+++ tests/manual/rhi/tex1d/buildshaders.bat
@@ -1,2 +1,2 @@
-qsb --glsl "300 es,150" --hlsl 50 --msl 12 -c texture1d.vert -o texture1d.vert.qsb
-qsb --glsl "300 es,150" --hlsl 50 --msl 12 -c texture1d.frag -o texture1d.frag.qsb
+qsb --glsl "120,150,300 es" --hlsl 50 --msl 12 -c texture1d.vert -o texture1d.vert.qsb
+qsb --glsl "120,150,300 es" --hlsl 50 --msl 12 -c texture1d.frag -o texture1d.frag.qsb
