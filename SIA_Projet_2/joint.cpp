f63108872ab729fbb10bfdc0264b84fdef6580ef
Ahmed El Yakoubi <ahmed.el-yakoubi@grenoble-inp.org>
2022-12-12 16:11:31 +0100

    codebase

---
+++ SIA_Projet_2/joint.cpp
@@ -0,0 +1,63 @@
+#include "joint.h"
+#include <QtGui/QMatrix4x4>
+
+using namespace std;
+
+Joint* Joint::createFromFile(std::string fileName) {
+	Joint* root = NULL;
+	cout << "Loading from " << fileName << endl;
+
+	ifstream inputfile(fileName.data());
+	if(inputfile.good()) {
+		while(!inputfile.eof()) {
+			string buf;	
+			inputfile >> buf;
+			// TODO : construire la structure de données root à partir du fichier
+		}
+		inputfile.close();
+	} else {
+		std::cerr << "Failed to load the file " << fileName.data() << std::endl;
+		fflush(stdout);
+	}
+
+	cout << "file loaded" << endl;
+
+	return root;
+}
+
+void Joint::animate(int iframe) 
+{
+	// Update dofs :
+	_curTx = 0; _curTy = 0; _curTz = 0;
+	_curRx = 0; _curRy = 0; _curRz = 0;
+	for (unsigned int idof = 0 ; idof < _dofs.size() ; idof++) {
+		if(!_dofs[idof].name.compare("Xposition")) _curTx = _dofs[idof]._values[iframe];
+		if(!_dofs[idof].name.compare("Yposition")) _curTy = _dofs[idof]._values[iframe];
+		if(!_dofs[idof].name.compare("Zposition")) _curTz = _dofs[idof]._values[iframe];
+		if(!_dofs[idof].name.compare("Zrotation")) _curRz = _dofs[idof]._values[iframe];
+		if(!_dofs[idof].name.compare("Yrotation")) _curRy = _dofs[idof]._values[iframe];
+		if(!_dofs[idof].name.compare("Xrotation")) _curRx = _dofs[idof]._values[iframe];
+	}	
+	// Animate children :
+	for (unsigned int ichild = 0 ; ichild < _children.size() ; ichild++) {
+		_children[ichild]->animate(iframe);
+	}
+}
+
+
+void Joint::nbDofs() {
+	if (_dofs.empty()) return;
+
+	double tol = 1e-4;
+
+	int nbDofsR = -1;
+
+	// TODO :
+	cout << _name << " : " << nbDofsR << " degree(s) of freedom in rotation\n";
+
+	// Propagate to children :
+	for (unsigned int ichild = 0 ; ichild < _children.size() ; ichild++) {
+		_children[ichild]->nbDofs();
+	}
+
+}
---
+++ SIA_Projet_2/joint.h
@@ -0,0 +1,79 @@
+#ifndef _JOINT_H_
+#define _JOINT_H_
+
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+class AnimCurve {
+public :
+	AnimCurve() {};
+	~AnimCurve() {
+		_values.clear();
+	}
+public :
+	std::string name;					// name of dof
+	std::vector<double> _values;		// different keyframes = animation curve
+};
+
+
+enum RotateOrder {roXYZ=0, roYZX, roZXY, roXZY, roYXZ, roZYX};
+
+class Joint {
+public :
+	std::string _name;					// name of joint
+	double _offX;						// initial offset in X
+	double _offY;						// initial offset in Y
+	double _offZ;						// initial offset in Z
+	std::vector<AnimCurve> _dofs;		// keyframes : _animCurves[i][f] = i-th dof at frame f;
+	double _curTx;						// current value of translation on X
+	double _curTy;						// current value of translation on Y
+	double _curTz;						// current value of translation on Z
+	double _curRx;						// current value of rotation about X (deg)
+	double _curRy;						// current value of rotation about Y (deg)
+	double _curRz;						// current value of rotation about Z (deg)
+	int _rorder;						// order of euler angles to reconstruct rotation
+	std::vector<Joint*> _children;	// children of the current joint
+
+
+public :
+	// Constructor :
+	Joint() {};
+	// Destructor :
+	~Joint() {
+		_dofs.clear();
+		_children.clear();
+	}
+
+	// Create from data :
+	static Joint* create(std::string name, double offX, double offY, double offZ, Joint* parent) {
+		Joint* child = new Joint();
+		child->_name = name;
+		child->_offX = offX;
+		child->_offY = offY;
+		child->_offZ = offZ;
+		child->_curTx = 0;
+		child->_curTy = 0;
+		child->_curTz = 0;
+		child->_curRx = 0;
+		child->_curRy = 0;
+		child->_curRz = 0;
+		if(parent != NULL) {
+			parent->_children.push_back(child);
+		}
+		return child;
+	}
+
+	// Load from file (.bvh) :	
+	static Joint* createFromFile(std::string fileName);
+
+	void animate(int iframe=0);
+
+	// Analysis of degrees of freedom :
+	void nbDofs();
+};
+
+
+#endif
