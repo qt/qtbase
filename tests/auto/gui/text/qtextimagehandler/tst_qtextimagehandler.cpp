c35691cd1c19c0a6986c14b74bcb96e41fff16d5
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-12-21 14:03:12 +0100

    Enable and fix the test for QTextImageHandler
    Amends 52ce0c177e80c2d5b70b38d429abb3689b3da51e, which added the test
    without adding it to the parent directory.
    
    Refactor the test code to be data driven, add the image files as
    external test data files, and adjust the test code to find the files.
    
    Use the QTextImageFormat from the document rather than a manually
    crafted one, as otherwise we don't test a real usecase.
    
    This also makes the test more flexible for adding qrc, resources, and
    file URLs.
    
    Task-number: QTBUG-109212
    Change-Id: Id0771037b961d95ec3cadd0cd6467d2448f22884
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    Reviewed-by: Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
    (cherry picked from commit d8e213a9e6b3013f768cbf4d94878cd2a0f7bb79)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/gui/text/qtextimagehandler/tst_qtextimagehandler.cpp
+++ tests/auto/gui/text/qtextimagehandler/tst_qtextimagehandler.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 The Qt Company Ltd.
+// Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include <QTest>
@@ -17,6 +17,7 @@ private slots:
     void init();
     void cleanup();
     void cleanupTestCase();
+    void loadAtNImages_data();
     void loadAtNImages();
 };
 
@@ -36,24 +37,37 @@ void tst_QTextImageHandler::cleanupTestCase()
 {
 }
 
+void tst_QTextImageHandler::loadAtNImages_data()
+{
+    QTest::addColumn<QString>("imageFile");
+
+    QTest::addRow("file") << QFINDTESTDATA("data/image.png");
+}
+
 void tst_QTextImageHandler::loadAtNImages()
 {
+    QFETCH(QString, imageFile);
+
     QTextDocument doc;
     QTextCursor c(&doc);
-    c.insertHtml("<img src=\"data/image.png\">");
+    c.insertHtml("<img src=\"" + imageFile + "\">");
+    const auto formats = doc.allFormats();
+    const auto it = std::find_if(formats.begin(), formats.end(), [](const auto &format){
+        return format.objectType() == QTextFormat::ImageObject;
+    });
+    QVERIFY(it != formats.end());
+    const QTextImageFormat format = (*it).toImageFormat();
     QTextImageHandler handler;
-    QTextImageFormat fmt;
-    fmt.setName("data/image.png");
 
-    for (int i = 1; i < 3; ++i) {
+    for (const auto &dpr : {1, 2}) {
         QImage img(20, 20, QImage::Format_ARGB32_Premultiplied);
         img.fill(Qt::white);
-        img.setDevicePixelRatio(i);
+        img.setDevicePixelRatio(dpr);
         QPainter p(&img);
-        handler.drawObject(&p, QRect(0, 0, 20, 20), &doc, 0, fmt);
+        handler.drawObject(&p, QRect(0, 0, 20, 20), &doc, 0, format);
         p.end();
         QVERIFY(!img.isNull());
-        const auto expectedColor = i == 1 ? Qt::red : Qt::green;
+        const auto expectedColor = dpr == 1 ? Qt::red : Qt::green;
         QCOMPARE(img.pixelColor(0, 0), expectedColor);
     }
 }
