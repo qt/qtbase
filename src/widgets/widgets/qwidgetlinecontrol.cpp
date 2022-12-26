3c24bb026e57fc988d31a5bd22f4059fb4cb6c79
Lu YaNing <luyaning@uniontech.com>
2022-12-26 15:25:18 +0800

    QWidgetLineControl: Add missing emit keywords
    Add emit keyword to avoid misunderstanding
    
    Change-Id: Iec3a896b75769a0bbc791e186d335ed0f728434f
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>

--- src/widgets/widgets/qwidgetlinecontrol.cpp
+++ src/widgets/widgets/qwidgetlinecontrol.cpp
@@ -718,7 +718,7 @@ bool QWidgetLineControl::finishChange(int validateFromState, bool update, bool e
         emit selectionChanged();
     }
     if (m_cursor == m_lastCursorPos)
-        updateMicroFocus();
+        emit updateMicroFocus();
     emitCursorPositionChanged();
     return true;
 }
@@ -1395,7 +1395,7 @@ void QWidgetLineControl::emitCursorPositionChanged()
     if (m_cursor != m_lastCursorPos) {
         const int oldLast = m_lastCursorPos;
         m_lastCursorPos = m_cursor;
-        cursorPositionChanged(oldLast, m_cursor);
+        emit cursorPositionChanged(oldLast, m_cursor);
 #if QT_CONFIG(accessibility)
         // otherwise we send a selection update which includes the cursor
         if (!hasSelectedText()) {
