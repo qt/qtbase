085ecc2c9f5bdf609256be2e8811791198098520
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2023-01-02 15:40:19 +0100

    macOS: Ensure NSEvent processing during NSApp initialization
    After 27f08548131e385f3c0c1fd4721e5979d512effe we are ensuring NSApp
    initialization without relying on our own runloop sources. But stopping
    the app only happens after it has processed an NSEvent, such as a mouse
    move or key press, which in the case of an auto tests might not happen.
    
    To ensure that the [NSApp stop] has an effect we post a synthetic event,
    that has no other effect than waking up the runloop.
    
    Change-Id: Ifa8912a80f9deff62645cbef56c1b1230542f3e4
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    (cherry picked from commit 76cef0f1714c303aec2ee28996984babdd91d1b2)
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- src/plugins/platforms/cocoa/qcocoaeventdispatcher.mm
+++ src/plugins/platforms/cocoa/qcocoaeventdispatcher.mm
@@ -598,6 +598,7 @@ void QCocoaEventDispatcherPrivate::ensureNSAppInitialized()
     CFRunLoopPerformBlock(mainRunLoop(), kCFRunLoopCommonModes, ^{
         qCDebug(lcEventDispatcher) << "NSApplication has been initialized; Stopping NSApp";
         [NSApp stop:NSApp];
+        cancelWaitForMoreEvents(); // Post event that wakes up the runloop
     });
     [NSApp run];
     qCDebug(lcEventDispatcher) << "Finished ensuring NSApplication is initialized";
