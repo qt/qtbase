346dbc361dea4e946366e36abdb59c78f6b4c2e9
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2023-01-10 18:06:21 +0100

    QStyleHints: Fix Q_PROPERTY declaration for keyboardAutoRepeatRateF
    Change-Id: I21d3280176d6c52daab0b3abfdafb404d42db9d0
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 50218659a4488d6e5717ad358d289b6d5e79173b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/kernel/qstylehints.h
+++ src/gui/kernel/qstylehints.h
@@ -22,7 +22,7 @@ class Q_GUI_EXPORT QStyleHints : public QObject
 #if QT_DEPRECATED_SINCE(6, 5)
     Q_PROPERTY(int keyboardAutoRepeatRate READ keyboardAutoRepeatRate STORED false CONSTANT FINAL)
 #endif
-    Q_PROPERTY(int keyboardAutoRepeatRateF READ keyboardAutoRepeatRateF STORED false CONSTANT FINAL)
+    Q_PROPERTY(qreal keyboardAutoRepeatRateF READ keyboardAutoRepeatRateF STORED false CONSTANT FINAL)
     Q_PROPERTY(int keyboardInputInterval READ keyboardInputInterval
                NOTIFY keyboardInputIntervalChanged FINAL)
     Q_PROPERTY(int mouseDoubleClickInterval READ mouseDoubleClickInterval
