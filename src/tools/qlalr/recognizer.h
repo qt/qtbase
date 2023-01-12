d3596c07e1b5f94a3f329878bb1e70d597476151
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-01-12 14:34:59 +0100

    Add missing header guards
    Task-number: QTBUG-109394
    Change-Id: Idc5b0057d41186c8dfd7e752fe819ebebc6f8859
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit aa29b3ef523d3f9d3e52942464d82cd58283c783)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/tools/qlalr/recognizer.h
+++ src/tools/qlalr/recognizer.h
@@ -1,6 +1,9 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
+#ifndef RECOGNIZER_H
+#define RECOGNIZER_H
+
 #include "grammar_p.h"
 
 #include "lalr.h"
@@ -72,3 +75,5 @@ protected:
   QString _M_current_value;
   bool _M_no_lines;
 };
+
+#endif // RECOGNIZER_H
