da36ac3c35d50b5e7863923b8d9c626821c077f7
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-01-12 14:34:59 +0100

    Add missing header guards
    Task-number: QTBUG-109394
    Change-Id: Idc5b0057d41186c8dfd7e752fe819ebebc6f8859
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit aa29b3ef523d3f9d3e52942464d82cd58283c783)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/networkinformation/networklistmanager/qnetworklistmanagerevents.h
+++ src/plugins/networkinformation/networklistmanager/qnetworklistmanagerevents.h
@@ -1,6 +1,9 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
+#ifndef QNETWORKLISTMANAGEREVENTS_H
+#define QNETWORKLISTMANAGEREVENTS_H
+
 #include <QtNetwork/private/qtnetworkglobal_p.h>
 
 #include <QtNetwork/qnetworkinformation.h>
@@ -73,3 +76,5 @@ private:
 };
 
 QT_END_NAMESPACE
+
+#endif // QNETWORKLISTMANAGEREVENTS_H
