8a96dee4cb94b45ab5a7b182e66f2ce1e7a4cb81
Marc Mutz <marc.mutz@qt.io>
2023-01-16 11:36:35 +0100

    QWindowsMimeConverter: fix copyright year
    Noted in API review.
    
    Pick-to: 6.5
    Change-Id: I08a5cb5cec1d7a13d96c22b69c7999eccfb4f932
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/gui/platform/windows/qwindowsmimeconverter.h
+++ src/gui/platform/windows/qwindowsmimeconverter.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 The Qt Company Ltd.
+// Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QWINDOWSMIMECONVERTER_P_H
