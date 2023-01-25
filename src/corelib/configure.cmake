2d43775a4b698964a74bf91d381cf19fd080cbd3
Mikolaj Boc <mikolaj.boc@qt.io>
2023-01-25 11:26:13 +0100

    Disable the process feature on WASM
    Processes are unsupported. processenvironment is kept intact.
    
    Change-Id: Ic2023be408adff9f8b034e9184bed620397be9bf
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
    (cherry picked from commit 7d7de4b2e8e49b89d87478c4aeaec1ffbb0218b1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/configure.cmake
+++ src/corelib/configure.cmake
@@ -763,7 +763,13 @@ qt_feature("process" PUBLIC
     SECTION "File I/O"
     LABEL "QProcess"
     PURPOSE "Supports external process invocation."
-    CONDITION QT_FEATURE_processenvironment AND ( QT_FEATURE_thread OR NOT UNIX ) AND NOT UIKIT AND NOT INTEGRITY AND NOT VXWORKS AND NOT rtems
+    CONDITION QT_FEATURE_processenvironment
+              AND (QT_FEATURE_thread OR NOT UNIX)
+              AND NOT UIKIT
+              AND NOT INTEGRITY
+              AND NOT VXWORKS
+              AND NOT rtems
+              AND NOT WASM
 )
 qt_feature_definition("process" "QT_NO_PROCESS" NEGATE VALUE "1")
 qt_feature("processenvironment" PUBLIC
