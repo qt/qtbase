c6ba34c7f82653fffbc43bc9b4a93442c75359ae
Antti Määttä <antti.maatta@qt.io>
2023-01-27 08:15:39 +0200

    Trace: Autogenerate metadata in qtgui module
    Pick-to: 6.5
    Change-Id: I300ecb9d25f0ecb7208c643704adf66bdf0a4b56
    Reviewed-by: Antti Määttä <antti.maatta@qt.io>
    Reviewed-by: Janne Koskinen <janne.p.koskinen@qt.io>
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>

--- src/gui/image/qimage.cpp
+++ src/gui/image/qimage.cpp
@@ -68,51 +68,8 @@ Q_TRACE_PREFIX(qtgui,
 );
 
 Q_TRACE_METADATA(qtgui,
-"ENUM {" \
-"Format_Invalid," \
-"Format_Mono," \
-"Format_MonoLSB," \
-"Format_Indexed8," \
-"Format_RGB32," \
-"Format_ARGB32," \
-"Format_ARGB32_Premultiplied," \
-"Format_RGB16," \
-"Format_ARGB8565_Premultiplied," \
-"Format_RGB666," \
-"Format_ARGB6666_Premultiplied," \
-"Format_RGB555," \
-"Format_ARGB8555_Premultiplied," \
-"Format_RGB888," \
-"Format_RGB444," \
-"Format_ARGB4444_Premultiplied," \
-"Format_RGBX8888," \
-"Format_RGBA8888," \
-"Format_RGBA8888_Premultiplied," \
-"Format_BGR30," \
-"Format_A2BGR30_Premultiplied," \
-"Format_RGB30," \
-"Format_A2RGB30_Premultiplied," \
-"Format_Alpha8," \
-"Format_Grayscale8," \
-"Format_RGBX64," \
-"Format_RGBA64," \
-"Format_RGBA64_Premultiplied," \
-"Format_Grayscale16," \
-"Format_BGR888," \
-"Format_RGBX16FPx4," \
-"Format_RGBA16FPx4," \
-"Format_RGBA16FPx4_Premultiplied," \
-"Format_RGBX32FPx4," \
-"Format_RGBA32FPx4," \
-"Format_RGBA32FPx4_Premultiplied," \
-"} QImage::Format;" \
-"FLAGS {" \
-"DefaultDither = 0," \
-"OrderedAlphaDither = 4," \
-"DiffuseAlphaDither = 8," \
-"OrderedDither = 16," \
-"ThresholdDither = 32," \
-"} Qt::ImageConversionFlags;"
+"ENUM { } QImage::Format;" \
+"FLAGS { } Qt::ImageConversionFlags;"
 );
 
 Q_TRACE_PARAM_REPLACE(Qt::AspectRatioMode, int);
