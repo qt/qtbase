e2030e366b73f5af9e60c987e498f21f9c985f68
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2023-02-02 13:41:03 +0100

    iOS: Report PermissionStatus::Denied when detecting accuracy mismatch
    There's no way to upgrade an Approximate accuracy permission to Precise,
    so once we have a known status we report the lacking accuracy as Denied.
    
    Pick-to: 6.5
    Change-Id: Id49af825335ff09669aa7f2285caaa744a5a4d24
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- src/corelib/platform/darwin/qdarwinpermissionplugin_location.mm
+++ src/corelib/platform/darwin/qdarwinpermissionplugin_location.mm
@@ -116,7 +116,7 @@ struct PermissionRequest
         if (permission.accuracy() == QLocationPermission::Approximate)
             return Qt::PermissionStatus::Granted;
         else
-            return Qt::PermissionStatus::Denied; // FIXME: Verify
+            return Qt::PermissionStatus::Denied;
     }
 
     Q_UNREACHABLE();
