83aa3052058eeda1ad7da0f8db31d23bf1e207d3
Mikolaj Boc <mikolaj.boc@qt.io>
2023-02-02 08:46:31 +0100

    Support Qt::NoDropShadowWindowHint in QWasmWindow
    Change-Id: I37b8b4c4d6b717d5fcd32b0793c2209850747085
    Reviewed-by: Aleksandr Reviakin <aleksandr.reviakin@qt.io>
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
    (cherry picked from commit c4a350db349f65c405e3bfd834da86645599eaba)
    Reviewed-by: Mikołaj Boc <Mikolaj.Boc@qt.io>

--- src/plugins/platforms/wasm/qwasmwindow.cpp
+++ src/plugins/platforms/wasm/qwasmwindow.cpp
@@ -360,6 +360,7 @@ void QWasmWindow::setWindowFlags(Qt::WindowFlags flags)
 {
     m_flags = flags;
     dom::syncCSSClassWith(m_qtWindow, "has-title-bar", hasTitleBar());
+    dom::syncCSSClassWith(m_qtWindow, "has-shadow", !flags.testFlag(Qt::NoDropShadowWindowHint));
 }
 
 void QWasmWindow::setWindowState(Qt::WindowStates newState)
