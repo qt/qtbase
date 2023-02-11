73f33e4da002b5dc15ad903b0488a833a9a3e758
Volker Hilsheimer <volker.hilsheimer@qt.io>
2023-02-11 17:53:32 +0100

    QMenu: guard for destruction when emitting action signals
    If a slot connected to a QMenu-action destroys the QMenu, then
    we must not touch data members in subsequent code, and instead return
    immediately.
    
    We cannot use QBoolBlocker here, as that would reset the data
    member of QMenuPrivate even when trying to return early.
    
    Fixes: QTBUG-106718
    Change-Id: I6b5ea471b1bf1f9864e1384382100f8f6c01346f
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit 52ce4d2d29db8a44fa2fa817cab9ebe969db082e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/widgets/qmenu.cpp
+++ src/widgets/widgets/qmenu.cpp
@@ -1413,9 +1413,18 @@ bool QMenuPrivate::mouseEventTaken(QMouseEvent *e)
 void QMenuPrivate::activateCausedStack(const QList<QPointer<QWidget>> &causedStack, QAction *action,
                                        QAction::ActionEvent action_e, bool self)
 {
-    QBoolBlocker guard(activationRecursionGuard);
+    Q_Q(QMenu);
+    // can't use QBoolBlocker here
+    const bool activationRecursionGuardReset = activationRecursionGuard;
+    activationRecursionGuard = true;
+    QPointer<QMenu> guard(q);
     if (self)
         action->activate(action_e);
+    if (!guard)
+        return;
+    auto boolBlocker = qScopeGuard([this, activationRecursionGuardReset]{
+        activationRecursionGuard = activationRecursionGuardReset;
+    });
 
     for(int i = 0; i < causedStack.size(); ++i) {
         QPointer<QWidget> widget = causedStack.at(i);
@@ -1491,9 +1500,10 @@ void QMenuPrivate::activateAction(QAction *action, QAction::ActionEvent action_e
 #endif
     }
 
-
+    QPointer<QMenu> thisGuard(q);
     activateCausedStack(causedStack, action, action_e, self);
-
+    if (!thisGuard)
+        return;
 
     if (action_e == QAction::Hover) {
 #ifndef QT_NO_ACCESSIBILITY
