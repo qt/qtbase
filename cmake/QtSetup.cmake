0a27f4030e61590e74cfea27519823010b2c1cbe
Alexey Edelev <alexey.edelev@qt.io>
2023-02-20 08:58:55 +0000

    Revert "Avoid mapping BUILD_TESTING to QT_BUILD_TESTS"
    This reverts commit e3ecabad228369bfc6dc82085446d7acc05ef45a.
    
    Reason for revert: This breaks the CTest internal switches. So it's better to disable testing directly in submodule.
    
    Change-Id: Ia93dc3bb0a3a34021c8e2d6c3d292e3a4909bef5
    Reviewed-by: Amir Masoud Abdol <amir.abdol@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- cmake/QtSetup.cmake
+++ cmake/QtSetup.cmake
@@ -233,6 +233,7 @@ if(QT_BUILD_STANDALONE_TESTS)
     set(QT_BUILD_TESTS_BY_DEFAULT ON CACHE BOOL
         "Should tests be built as part of the default 'all' target." FORCE)
 endif()
+set(BUILD_TESTING ${QT_BUILD_TESTS} CACHE INTERNAL "")
 
 if (WASM)
     set(_qt_batch_tests ON)
