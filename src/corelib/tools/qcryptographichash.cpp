9a18ca59ffc93b6e4b18a64c624ab396e3976f60
Marc Mutz <marc.mutz@qt.io>
2023-02-23 14:44:49 +0100

    QCryptographicHash: move result.clear() to beginning of reset()
    ... from the end().
    
    It was lonely down there, and prone to be overlooked when performing
    early returns in the #ifdef'ery above, as witnessed by the early
    returns in the OpenSSL3 code.
    
    Amends 1fe74c3bd3f727551b8168ee9f3e642d42233d55, itself amending
    633c1365966eda89fc883c039683a727d0a0813e.
    
    Pick-to: 6.5
    Change-Id: I8c941ecb5c4755d8823b2161544cf6f7fe75a239
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/tools/qcryptographichash.cpp
+++ src/corelib/tools/qcryptographichash.cpp
@@ -464,6 +464,7 @@ QCryptographicHash::Algorithm QCryptographicHash::algorithm() const noexcept
 
 void QCryptographicHashPrivate::reset() noexcept
 {
+    result.clear();
 #ifdef USING_OPENSSL30
     if (method == QCryptographicHash::Blake2b_160 ||
         method == QCryptographicHash::Blake2b_256 ||
@@ -573,7 +574,6 @@ void QCryptographicHashPrivate::reset() noexcept
 #endif
     }
 #endif // !QT_CONFIG(opensslv30)
-    result.clear();
 }
 
 #if QT_DEPRECATED_SINCE(6, 4)
