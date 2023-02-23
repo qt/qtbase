d3786c0bcbff8f910fc285f8421e3d7ee9d09c47
Marc Mutz <marc.mutz@qt.io>
2023-02-23 15:00:42 +0100

    De-pessimize QCryptographicHash::reset() in OpenSSL3 mode
    Instead of going through the whole dance with provider loading,
    context creation etc, just call EVP_MD_CTX_reset() to reuse the
    already-setup context, if any.
    
    This makes QCryptographicHash::reset() adhere to its noexcept
    specification again (assuming EVP_MD_CTX_reset() doesn't allocate
    state, and, despite its non-void return type, cannot fail on a valid
    context), and should greatly improve the speed of reset(), addData(),
    resultView() cycles.
    
    Change-Id: I7c35b61cbeab1ffd6dd258e8389ea614d49e2e1e
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Jan Grulich <jgrulich@redhat.com>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 189444d8c4a1384479962e98e699bf1fd2908de8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/tools/qcryptographichash.cpp
+++ src/corelib/tools/qcryptographichash.cpp
@@ -491,6 +491,12 @@ void QCryptographicHashPrivate::reset() noexcept
         return;
     }
 
+    if (context && !initializationFailed) {
+        // everything already set up - just reset the context
+        EVP_MD_CTX_reset(context.get());
+        return;
+    }
+
     initializationFailed = true;
 
     if (method == QCryptographicHash::Md4) {
