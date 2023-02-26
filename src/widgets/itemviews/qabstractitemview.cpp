09bf5a439e62dbd9b634b912ba6895862e17105b
Volker Hilsheimer <volker.hilsheimer@qt.io>
2023-02-26 16:47:42 +0100

    QAbtractItemView: deselect before click opens editor
    A click on the selected item should deselect all other items before
    editing starts.
    
    Remove the part of the test case that assumes that we can have multiple
    items selected in ExtendedSelection mode, and click on an item to start
    editing while maintaining selection. That can never happen.
    
    Fixes: QTBUG-111131
    Change-Id: I0312eed4614502cfb77eca26d3f7615427493d7d
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit 3f2d02e2f4436ace5f0fbbf432878a963f5ee969)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/itemviews/qabstractitemview.cpp
+++ src/widgets/itemviews/qabstractitemview.cpp
@@ -1973,7 +1973,7 @@ void QAbstractItemView::mouseReleaseEvent(QMouseEvent *event)
 
     if (d->selectionModel && d->noSelectionOnMousePress) {
         d->noSelectionOnMousePress = false;
-        if (!edited && !d->pressClosedEditor)
+        if (!d->pressClosedEditor)
             d->selectionModel->select(index, selectionCommand(index, event));
     }
 
