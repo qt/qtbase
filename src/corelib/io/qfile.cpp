7a1c82aabd3c3db4bd9d4146d11c8e109f562a00
Assam Boudjelthia <assam.boudjelthia@qt.io>
2023-03-01 12:42:12 +0200

    Android: document limitation for QFile::copy() operation for content uri
    Document that the operation is not supported.
    
    Task-number: QTBUG-98974
    Change-Id: I1faacb7af7e11943d6da62313ed104fda063d30d
    Reviewed-by: Rami Potinkara <rami.potinkara@qt.io>
    Reviewed-by: Nicholas Bennett <nicholas.bennett@qt.io>
    (cherry picked from commit 7af39be05295c5743d04ef83468a0d250141b145)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/io/qfile.cpp
+++ src/corelib/io/qfile.cpp
@@ -835,6 +835,9 @@ QFile::link(const QString &fileName, const QString &linkName)
 
     \include qfile-copy.qdocinc
 
+    \note On Android, this operation is not yet supported for \c content
+    scheme URIs.
+
     \sa setFileName()
 */
 
@@ -942,6 +945,9 @@ QFile::copy(const QString &newName)
     Copies the file named \a fileName to \a newName.
 
     \include qfile-copy.qdocinc
+
+    \note On Android, this operation is not yet supported for \c content
+    scheme URIs.
 
     \sa rename()
 */
