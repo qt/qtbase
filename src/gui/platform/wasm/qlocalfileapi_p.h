0cefeb259350938fddb123ef46a9407aa70d51a9
Mikolaj Boc <mikolaj.boc@qt.io>
2023-03-03 11:21:30 +0100

    Replace Q_CORE_EXPORT with Q_AUTOTEST_EXPORT in qlocalfileapi_p.h
    The only consumer outside of gui is the autotest target
    
    Change-Id: I2c6b41029ed5c53a2fd5f31f542128616620ddcf
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

--- src/gui/platform/wasm/qlocalfileapi_p.h
+++ src/gui/platform/wasm/qlocalfileapi_p.h
@@ -24,7 +24,8 @@
 QT_BEGIN_NAMESPACE
 
 namespace LocalFileApi {
-class Q_CORE_EXPORT Type {
+class Q_AUTOTEST_EXPORT Type
+{
 public:
     class Accept {
     public:
@@ -80,8 +81,10 @@ private:
     std::optional<Accept> m_accept;
 };
 
-Q_CORE_EXPORT emscripten::val makeOpenFileOptions(const QStringList &filterList, bool acceptMultiple);
-Q_CORE_EXPORT emscripten::val makeSaveFileOptions(const QStringList &filterList, const std::string& suggestedName);
+Q_AUTOTEST_EXPORT emscripten::val makeOpenFileOptions(const QStringList &filterList,
+                                                      bool acceptMultiple);
+Q_AUTOTEST_EXPORT emscripten::val makeSaveFileOptions(const QStringList &filterList,
+                                                      const std::string &suggestedName);
 
 Q_AUTOTEST_EXPORT std::string makeFileInputAccept(const QStringList &filterList);
 
