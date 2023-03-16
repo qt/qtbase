b538a53a9d2a581c2fddf5dae0a504ef954c3641
Edward Welbourne <edward.welbourne@qt.io>
2023-03-16 17:02:06 +0100

    Silence a signed vs unsigned warning when char is signed
    Change-Id: I5f706f87f3a15f3723a2d4ce3044b84b924da3e5
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/io/qurlidna.cpp
+++ src/corelib/io/qurlidna.cpp
@@ -327,12 +327,12 @@ static bool lessThan(const QChar *a, int l, const char *c)
         return false;
 
     while (*c) {
-        if (uc == e || *uc != *c)
+        if (uc == e || *uc != static_cast<unsigned char>(*c))
             break;
         ++uc;
         ++c;
     }
-    return (uc == e ? *c : *uc < *c);
+    return uc == e ? *c : (*uc < static_cast<unsigned char>(*c));
 }
 
 static bool equal(const QChar *a, int l, const char *b)
