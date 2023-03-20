f4872d9091715f7ff4c7ff732cc09c7291dde465
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2023-03-20 15:39:55 +0100

    Guard QTabBar against nested event processing during moving tabs
    If moving tabs results in nested event processing by the user's
    code in response to the tab index changing, the mouse release
    event might have been processed already, and the pressed index
    is reset back to -1.
    
    Fixes: QTBUG-74478
    Change-Id: I9696b10a6218456ab381ffe3a65ea471cfa42413
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>
    (cherry picked from commit 6736118b8c1d9ca1517f0539bd93acc6f99732c1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/widgets/qtabbar.cpp
+++ src/widgets/widgets/qtabbar.cpp
@@ -2190,7 +2190,8 @@ void QTabBar::mouseMoveEvent(QMouseEvent *event)
                 }
             }
             // Buttons needs to follow the dragged tab
-            d->layoutTab(d->pressedIndex);
+            if (d->pressedIndex != -1)
+                d->layoutTab(d->pressedIndex);
 
             update();
         }
