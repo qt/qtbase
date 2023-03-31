2bffce5719c49e2aac0217f97cf48171683ee1ee
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2023-03-31 14:41:46 +0200

    Darwin: Document that permissions can only be requested in GUI apps
    To request them in QtCore only apps we need to use the CoreFoundation
    event dispatcher on the main thread, which is currently blocked by
    issues in dealing with deferred delete events.
    
    Pick-to: 6.5
    Change-Id: I9fe2575c2b3527b1b83fac208cfed6a0d7a8cb42
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>

--- src/corelib/kernel/qpermissions.cpp
+++ src/corelib/kernel/qpermissions.cpp
@@ -79,6 +79,9 @@ Q_LOGGING_CATEGORY(lcPermissions, "qt.permissions", QtWarningMsg);
     The use of the \c{QT_CONFIG(permissions)} macro ensures that the code
     will work as before on platforms where permissions are not available.
 
+    \note On \macOS and iOS permissions can currently only be requested for
+    GUI applications.
+
     \section2 Declaring Permissions
 
     Some platforms require that the permissions you request are declared
