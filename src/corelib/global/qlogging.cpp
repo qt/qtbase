1f154eec608db6caa027db534856f40d2c7ccf87
Assam Boudjelthia <assam.boudjelthia@qt.io>
2023-04-04 22:58:11 +0100

    Android: fix build error for Qt 5.15 with NDK r24+
    the call backtace() used when QLOGGING_HAVE_BACKTRACE is available
    only for Android API >= 33.
    
    See https://android.googlesource.com/toolchain/prebuilts/ndk/r24/+/refs/
    heads/master/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/
    execinfo.h#52
    
    Fixes: QTBUG-112504
    Change-Id: I442042de3d21600ff364dada0c1cca133e39f305
    Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/corelib/global/qlogging.cpp
+++ src/corelib/global/qlogging.cpp
@@ -106,6 +106,8 @@
 #    if __UCLIBC_HAS_BACKTRACE__
 #      define QLOGGING_HAVE_BACKTRACE
 #    endif
+#  elif defined(Q_OS_ANDROID) && __ANDROID_API__ < 33
+    // Android lacked backtrace logging until API level 33.
 #  elif (defined(__GLIBC__) && defined(__GLIBCXX__)) || (__has_include(<cxxabi.h>) && __has_include(<execinfo.h>))
 #    define QLOGGING_HAVE_BACKTRACE
 #  endif
