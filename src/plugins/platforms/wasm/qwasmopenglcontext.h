b5834e39d986e324ee8b7d0fbaa7ab2136ffd233
Amir Masoud Abdol <amir.abdol@qt.io>
2023-04-13 12:18:12 +0200

    Add the missing include guard
    Task-number: QTBUG-109394
    Change-Id: I6a46784fa65aecd48a7f59021c4942ed52321742
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit 4b00aaae9cbf30b4182281e9bb5299a009ff8f83)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/wasm/qwasmopenglcontext.h
+++ src/plugins/platforms/wasm/qwasmopenglcontext.h
@@ -1,6 +1,9 @@
 // Copyright (C) 2018 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
+#ifndef QWASMOPENGLCONTEXT_H
+#define QWASMOPENGLCONTEXT_H
+
 #include <qpa/qplatformopenglcontext.h>
 
 #include <emscripten.h>
@@ -39,3 +42,4 @@ private:
 
 QT_END_NAMESPACE
 
+#endif // QWASMOPENGLCONTEXT_H
