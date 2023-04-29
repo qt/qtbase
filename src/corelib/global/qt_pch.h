4fcb348154b72146b91049ea252575054b87806a
Yuhang Zhao <2546789017@qq.com>
2023-04-29 16:23:36 +0800

    fix wrong and strange indentation
    Change-Id: I803d85eb83ec8e6ea532095c34fa8b0ae104705d
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/global/qt_pch.h
+++ src/corelib/global/qt_pch.h
@@ -14,31 +14,31 @@
 // for rand_s, _CRT_RAND_S must be #defined before #including stdlib.h.
 // put it at the beginning so some indirect inclusion doesn't break it
 #ifndef _CRT_RAND_S
-#define _CRT_RAND_S
+#  define _CRT_RAND_S
 #endif
 #include <stdlib.h>
 #include <qglobal.h>
 #ifdef Q_OS_WIN
-# ifdef Q_CC_MINGW
+#  ifdef Q_CC_MINGW
 // <unistd.h> must be included before any other header pulls in <time.h>.
-#  include <unistd.h> // Define _POSIX_THREAD_SAFE_FUNCTIONS to obtain localtime_r()
-# endif
-# define _POSIX_
-# include <limits.h>
-# undef _POSIX_
-#    endif
-#    include <qcoreapplication.h>
-#    include <qcoreevent.h>
-#    include <qiodevice.h>
-#    include <qlist.h>
-#    include <qvariant.h> /* All moc generated code has this include */
-#    include <qobject.h>
-#    if QT_CONFIG(regularexpression)
-#        include <qregularexpression.h>
-#    endif
-#    include <qscopedpointer.h>
-#    include <qshareddata.h>
-#    include <qstring.h>
-#    include <qstringlist.h>
-#    include <qtimer.h>
+#    include <unistd.h> // Define _POSIX_THREAD_SAFE_FUNCTIONS to obtain localtime_r()
+#  endif // Q_CC_MINGW
+#  define _POSIX_
+#  include <limits.h>
+#  undef _POSIX_
+#endif // Q_OS_WIN
+#include <qcoreapplication.h>
+#include <qcoreevent.h>
+#include <qiodevice.h>
+#include <qlist.h>
+#include <qvariant.h> /* All moc generated code has this include */
+#include <qobject.h>
+#if QT_CONFIG(regularexpression)
+#  include <qregularexpression.h>
+#endif
+#include <qscopedpointer.h>
+#include <qshareddata.h>
+#include <qstring.h>
+#include <qstringlist.h>
+#include <qtimer.h>
 #endif
