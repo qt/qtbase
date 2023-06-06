3653256b7f069ee081183b634f69ca0b2110362e
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2023-06-07 00:47:27 +0200

    Deprecate some older Apple platform defines
    Clang's macro deprecation pragma was introduced in Clang 14,
    shipped with Xcode 14.0.
    
    We can't version check Q_CC_CLANG here, as qcompilerdetection.h
    hasn't been included at this point. Nor would it have helped much,
    as we would have had to do so for each individual define we wanted
    to deprecate, since mentioning an undefined define in the deprecation
    pragma is an error. And we can't wrap the pragma in another macro
    that conditionally expands to _Pragma either, as the input argument
    is a define that needs to be passed on as is, without expanding it.
    
    So the simplest way to support < Xcode 14 is just to silence
    the unknown pragma warning.
    
    Change-Id: Icf95042e6186deb212cd1793eb12ba32206ce34b
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 3c12c3d58c5e27a5792eb85de994539c17ef2c38)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/thread/qthread.h
+++ src/corelib/thread/qthread.h
@@ -158,7 +158,7 @@ inline Qt::HANDLE QThread::currentThreadId() noexcept
     // See https://akkadia.org/drepper/tls.pdf for x86 ABI
 #if defined(Q_PROCESSOR_X86_32) && ((defined(Q_OS_LINUX) && defined(__GLIBC__)) || defined(Q_OS_FREEBSD)) // x86 32-bit always uses GS
     __asm__("movl %%gs:%c1, %0" : "=r" (tid) : "i" (2 * sizeof(void*)) : );
-#elif defined(Q_PROCESSOR_X86_64) && defined(Q_OS_DARWIN64)
+#elif defined(Q_PROCESSOR_X86_64) && defined(Q_OS_DARWIN)
     // 64bit macOS uses GS, see https://github.com/apple/darwin-xnu/blob/master/libsyscall/os/tsd.h
     __asm__("movq %%gs:0, %0" : "=r" (tid) : : );
 #elif defined(Q_PROCESSOR_X86_64) && ((defined(Q_OS_LINUX) && defined(__GLIBC__)) || defined(Q_OS_FREEBSD))
