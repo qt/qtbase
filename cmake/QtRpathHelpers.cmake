604eb6f4cdf61bf02a2e6e2ec2bbb58d0567364c
Thiago Macieira <thiago.macieira@intel.com>
2023-06-06 09:48:39 -0700

    CMake: apply OpenBSD patch to enable $ORIGIN
    Found at https://github.com/openbsd/ports/blob/master/x11/qt6/qtbase/patches/patch-cmake_QtRpathHelpers_cmake
    
    There are a lot more patches there whose purpose I can't tell. This is
    the only CMake one that I do understand. They should upstream those
    changes to us with an explanation, instead of carrying patches...
    
    Found while researching if 9caac0f176040b4da48d3ea289683b0b082cf729 was
    still necessary for OpenBSD.
    
    Pick-to: 6.5 6.6
    Change-Id: I63b988479db546dabffcfffd17661fdd376bf8c8
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- cmake/QtRpathHelpers.cmake
+++ cmake/QtRpathHelpers.cmake
@@ -6,7 +6,7 @@
 function(qt_internal_get_relative_rpath_base_token out_var)
     if(APPLE)
         set(rpath_rel_base "@loader_path")
-    elseif(LINUX OR SOLARIS OR FREEBSD OR HURD)
+    elseif(LINUX OR SOLARIS OR FREEBSD OR HURD OR OPENBSD)
         set(rpath_rel_base "$ORIGIN")
     else()
         set(rpath_rel_base "NO_KNOWN_RPATH_REL_BASE")
