b2244a252ca36da3b566112464655a70e887f178
Marc Mutz <marc.mutz@qt.io>
2023-06-07 15:50:17 +0200

    qnetworkreplyfileimpl_p.h: fix header guard name
    It was missing the _P.
    
    Pick-to: 6.6 6.5 6.2 5.15
    Change-Id: Ic07b4145f06ba3b1bea52349cf9b3f8215f30313
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/network/access/qnetworkreplyfileimpl_p.h
+++ src/network/access/qnetworkreplyfileimpl_p.h
@@ -1,8 +1,8 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#ifndef QNETWORKREPLYFILEIMPL_H
-#define QNETWORKREPLYFILEIMPL_H
+#ifndef QNETWORKREPLYFILEIMPL_P_H
+#define QNETWORKREPLYFILEIMPL_P_H
 
 //
 //  W A R N I N G
@@ -65,4 +65,4 @@ QT_END_NAMESPACE
 QT_DECL_METATYPE_EXTERN_TAGGED(QNetworkRequest::KnownHeaders,
                                QNetworkRequest__KnownHeaders, Q_NETWORK_EXPORT)
 
-#endif // QNETWORKREPLYFILEIMPL_H
+#endif // QNETWORKREPLYFILEIMPL_P_H
