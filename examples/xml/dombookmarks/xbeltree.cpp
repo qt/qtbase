9e85c70b3758680fd8ef8ff433bcc32e1fb7e321
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-06-12 11:48:21 +0200

    Brush up the DOM bookmarks example
    - Use modern string literals (use QStringLiteral instead of
      QLatin1StringView for strings that go into the DOM API).
    - Use mime types in the file dialog handling
    - Streamline code
    - Remove mentions of SAX
    - Use per class includes
    - Do not use QObject::tr()
    - Use the configure system instead of QT_NO... defines
    - Fix some doc text typos
    
    Complements 3dd3268ded4dd74c64d7ec726fd534375ab9f018.
    
    Task-number: QTBUG-111974
    Change-Id: If0dc7b61e729d0a71f37743efc9b82e285d3f451
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 3a553507a134bee1562d34ebbf786a053d36fc05)

--- examples/xml/dombookmarks/xbeltree.cpp
+++ examples/xml/dombookmarks/xbeltree.cpp
@@ -1,31 +1,41 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-#include <QtWidgets>
-
 #include "xbeltree.h"
+
+#include <QHeaderView>
+#include <QMenu>
+#include <QMessageBox>
+
+#include <QDesktopServices>
+#include <QGuiApplication>
+#if QT_CONFIG(clipboard) && QT_CONFIG(contextmenu)
+#  include <QClipboard>
+#  include <QContextMenuEvent>
+#endif
+
+#include <QUrl>
+
+using namespace Qt::StringLiterals;
 
 enum { DomElementRole = Qt::UserRole + 1 };
 
 Q_DECLARE_METATYPE(QDomElement)
 
-static inline QString titleElement() { return QStringLiteral("title"); }
-static inline QString folderElement() { return QStringLiteral("folder"); }
-static inline QString bookmarkElement() { return QStringLiteral("bookmark"); }
+static const auto titleElement  = u"title"_s;
+static const auto folderElement = u"folder"_s;
+static const auto bookmarkElement = u"bookmark"_s;
 
-static inline QString versionAttribute() { return QStringLiteral("version"); }
-static inline QString hrefAttribute() { return QStringLiteral("href"); }
-static inline QString foldedAttribute() { return QStringLiteral("folded"); }
+static const auto versionAttribute = u"version"_s;
+static const auto hrefAttribute = u"href"_s;
+static const auto foldedAttribute = u"folded"_s;
 
 //! [0]
 XbelTree::XbelTree(QWidget *parent)
     : QTreeWidget(parent)
 {
-    QStringList labels;
-    labels << tr("Title") << tr("Location");
-
     header()->setSectionResizeMode(QHeaderView::Stretch);
-    setHeaderLabels(labels);
+    setHeaderLabels({tr("Title"), tr("Location")});
 
     folderIcon.addPixmap(style()->standardPixmap(QStyle::SP_DirClosedIcon),
                          QIcon::Normal, QIcon::Off);
@@ -35,7 +45,7 @@ XbelTree::XbelTree(QWidget *parent)
 }
 //! [0]
 
-#if !defined(QT_NO_CONTEXTMENU) && !defined(QT_NO_CLIPBOARD)
+#if QT_CONFIG(clipboard) && QT_CONFIG(contextmenu)
 void XbelTree::contextMenuEvent(QContextMenuEvent *event)
 {
     const QTreeWidgetItem *item = itemAt(event->pos());
@@ -51,7 +61,7 @@ void XbelTree::contextMenuEvent(QContextMenuEvent *event)
     else if (action == openAction)
         QDesktopServices::openUrl(QUrl(url));
 }
-#endif // !QT_NO_CONTEXTMENU && !QT_NO_CLIPBOARD
+#endif // QT_CONFIG(clipboard) && QT_CONFIG(contextmenu)
 
 //! [1]
 bool XbelTree::read(QIODevice *device)
@@ -72,8 +82,8 @@ bool XbelTree::read(QIODevice *device)
         QMessageBox::information(window(), tr("DOM Bookmarks"),
                                  tr("The file is not an XBEL file."));
         return false;
-    } else if (root.hasAttribute(versionAttribute())
-               && root.attribute(versionAttribute()) != QLatin1String("1.0")) {
+    } else if (root.hasAttribute(versionAttribute)
+               && root.attribute(versionAttribute) != "1.0"_L1) {
         QMessageBox::information(window(), tr("DOM Bookmarks"),
                                  tr("The file is not an XBEL version 1.0 "
                                     "file."));
@@ -84,10 +94,10 @@ bool XbelTree::read(QIODevice *device)
 
     disconnect(this, &QTreeWidget::itemChanged, this, &XbelTree::updateDomElement);
 
-    QDomElement child = root.firstChildElement(folderElement());
+    QDomElement child = root.firstChildElement(folderElement);
     while (!child.isNull()) {
         parseFolderElement(child);
-        child = child.nextSiblingElement(folderElement());
+        child = child.nextSiblingElement(folderElement);
     }
 
     connect(this, &QTreeWidget::itemChanged, this, &XbelTree::updateDomElement);
@@ -112,16 +122,16 @@ void XbelTree::updateDomElement(const QTreeWidgetItem *item, int column)
     QDomElement element = qvariant_cast<QDomElement>(item->data(0, DomElementRole));
     if (!element.isNull()) {
         if (column == 0) {
-            QDomElement oldTitleElement = element.firstChildElement(titleElement());
-            QDomElement newTitleElement = domDocument.createElement(titleElement());
+            QDomElement oldTitleElement = element.firstChildElement(titleElement);
+            QDomElement newTitleElement = domDocument.createElement(titleElement);
 
             QDomText newTitleText = domDocument.createTextNode(item->text(0));
             newTitleElement.appendChild(newTitleText);
 
             element.replaceChild(newTitleElement, oldTitleElement);
         } else {
-            if (element.tagName() == bookmarkElement())
-                element.setAttribute(hrefAttribute(), item->text(1));
+            if (element.tagName() == bookmarkElement)
+                element.setAttribute(hrefAttribute, item->text(1));
         }
     }
 }
@@ -132,35 +142,35 @@ void XbelTree::parseFolderElement(const QDomElement &element,
 {
     QTreeWidgetItem *item = createItem(element, parentItem);
 
-    QString title = element.firstChildElement(titleElement()).text();
+    QString title = element.firstChildElement(titleElement).text();
     if (title.isEmpty())
-        title = QObject::tr("Folder");
+        title = tr("Folder");
 
     item->setFlags(item->flags() | Qt::ItemIsEditable);
     item->setIcon(0, folderIcon);
     item->setText(0, title);
 
-    bool folded = (element.attribute(foldedAttribute()) != QLatin1String("no"));
+    bool folded = (element.attribute(foldedAttribute) != "no"_L1);
     item->setExpanded(!folded);
 
     constexpr char16_t midDot = u'\xB7';
     static const QString dots = QString(30, midDot);
     QDomElement child = element.firstChildElement();
     while (!child.isNull()) {
-        if (child.tagName() == folderElement()) {
+        if (child.tagName() == folderElement) {
             parseFolderElement(child, item);
-        } else if (child.tagName() == bookmarkElement()) {
+        } else if (child.tagName() == bookmarkElement) {
             QTreeWidgetItem *childItem = createItem(child, item);
 
-            QString title = child.firstChildElement(titleElement()).text();
+            QString title = child.firstChildElement(titleElement).text();
             if (title.isEmpty())
-                title = QObject::tr("Folder");
+                title = tr("Folder");
 
             childItem->setFlags(item->flags() | Qt::ItemIsEditable);
             childItem->setIcon(0, bookmarkIcon);
             childItem->setText(0, title);
-            childItem->setText(1, child.attribute(hrefAttribute()));
-        } else if (child.tagName() == QLatin1String("separator")) {
+            childItem->setText(1, child.attribute(hrefAttribute));
+        } else if (child.tagName() == "separator"_L1) {
             QTreeWidgetItem *childItem = createItem(child, item);
             childItem->setFlags(item->flags() & ~(Qt::ItemIsSelectable | Qt::ItemIsEditable));
             childItem->setText(0, dots);
