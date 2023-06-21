34a64c377187c93a32f5c105a69b5ccd6b9cc945
Morten Sørvig <morten.sorvig@qt.io>
2023-06-21 14:48:49 +0200

    QWizard: Support fractional DPR on Windows
    We were storing the DPR as int; change to qreal.
    
    Task-number: QTBUG-114175
    Change-Id: I7dc7df82f584cddbbb3f690f1df74e7a30369ab2
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit c0e48ed645f68e714e982064540d710676a0738b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/dialogs/qwizard_win.cpp
+++ src/widgets/dialogs/qwizard_win.cpp
@@ -33,7 +33,7 @@ Q_DECLARE_METATYPE(QMargins)
 
 QT_BEGIN_NAMESPACE
 
-int QVistaHelper::m_devicePixelRatio = 1;
+qreal QVistaHelper::m_devicePixelRatio = 1.0;
 
 /******************************************************************************
 ** QVistaBackButton
@@ -82,7 +82,7 @@ void QVistaBackButton::paintEvent(QPaintEvent *)
     RECT clipRect;
     int xoffset = origin.x() + QWidget::mapToParent(r.topLeft()).x() - 1;
     int yoffset = origin.y() + QWidget::mapToParent(r.topLeft()).y() - 1;
-    const int dpr = devicePixelRatio();
+    const qreal dpr = devicePixelRatio();
     const QRect rDp = QRect(r.topLeft() * dpr, r.size() * dpr);
     const int xoffsetDp = xoffset * dpr;
     const int yoffsetDp = yoffset * dpr;
