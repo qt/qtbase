7d5a61eb281f11c5aa80787a3f872786635edeef
Morten Sørvig <morten.sorvig@qt.io>
2023-06-21 14:48:49 +0200

    QWizard: Support fractional DPR on Windows
    We were storing the DPR as int; change to qreal.
    
    Task-number: QTBUG-114175
    Change-Id: I7dc7df82f584cddbbb3f690f1df74e7a30369ab2
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit c0e48ed645f68e714e982064540d710676a0738b)
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

--- src/widgets/dialogs/qwizard_win_p.h
+++ src/widgets/dialogs/qwizard_win_p.h
@@ -152,7 +152,7 @@ private:
 
     int titleBarOffset;  // Extra spacing above the text
     int iconSpacing;    // Space between button and icon
-    static int m_devicePixelRatio;
+    static qreal m_devicePixelRatio;
 };
 
 
