e70ef7cd57737ea23785e4e021d1956a74ea38a7
Axel Spoerl <axel.spoerl@qt.io>
2023-06-24 11:38:31 +0200

    QDialogButtonBox - code cleanup
    Replace const char * based connect statements by modern API and remove
    '_q_' slot prefixes.
    This requires explicit disconnection in the destructor -> add.
    Replace bool traps for layouting and removing buttons by enum classes.
    Replace if/elseif event handler by switch.
    Replace iterator typedef with auto.
    Encapsulate logic to ensure defaulting to first accept button in a
    member function for better readability.
    Remove dead code.
    Move private header declaration from cpp to a new _p.h.
    
    Task-number: QTBUG-114377
    Change-Id: I8a2bc355e3816c3c826c10cd96194c89bf0ae510
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit df735d794fd2e545c18b9e345e833422bcd64329)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/widgets/widgets/qdialogbuttonbox_p.h
@@ -0,0 +1,72 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QDIALOGBUTTONBOX_P_H
+#define QDIALOGBUTTONBOX_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <private/qwidget_p.h>
+#include <qdialogbuttonbox.h>
+
+QT_BEGIN_NAMESPACE
+class Q_AUTOTEST_EXPORT QDialogButtonBoxPrivate : public QWidgetPrivate
+{
+    Q_DECLARE_PUBLIC(QDialogButtonBox)
+
+public:
+    enum class RemoveRule {
+        KeepConnections,
+        Disconnect,
+    };
+    enum class LayoutRule {
+        DoLayout,
+        SkipLayout,
+    };
+
+    QDialogButtonBoxPrivate(Qt::Orientation orient);
+
+    QList<QAbstractButton *> buttonLists[QDialogButtonBox::NRoles];
+    QHash<QPushButton *, QDialogButtonBox::StandardButton> standardButtonHash;
+    QHash<QAbstractButton *, QDialogButtonBox::ButtonRole> hiddenButtons;
+
+    Qt::Orientation orientation;
+    QDialogButtonBox::ButtonLayout layoutPolicy;
+    QBoxLayout *buttonLayout;
+    bool center;
+    bool byPassEventFilter = false;
+
+    void createStandardButtons(QDialogButtonBox::StandardButtons buttons);
+
+    void removeButton(QAbstractButton *button, RemoveRule rule);
+    void layoutButtons();
+    void initLayout();
+    void resetLayout();
+    QPushButton *createButton(QDialogButtonBox::StandardButton button,
+                              LayoutRule layoutRule = LayoutRule::DoLayout);
+    void addButton(QAbstractButton *button, QDialogButtonBox::ButtonRole role,
+                   LayoutRule layoutRule = LayoutRule::DoLayout);
+    void handleButtonDestroyed();
+    void handleButtonClicked();
+    bool handleButtonShowAndHide(QAbstractButton *button, QEvent *event);
+    void addButtonsToLayout(const QList<QAbstractButton *> &buttonList, bool reverse);
+    void ensureFirstAcceptIsDefault();
+    void retranslateStrings();
+    void disconnectAll();
+    QList<QAbstractButton *> allButtons() const;
+    QList<QAbstractButton *> visibleButtons() const;
+    QDialogButtonBox::ButtonRole buttonRole(QAbstractButton *button) const;
+};
+
+QT_END_NAMESPACE
+
+#endif // QDIALOGBUTTONBOX_P_H
