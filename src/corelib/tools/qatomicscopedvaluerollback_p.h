ac328eec8d227958961ca29379ff0b869435aedc
Marc Mutz <marc.mutz@qt.io>
2023-07-07 07:48:40 +0200

    QAtomicScopedValueRollback: fix CTAD for Q(Basic)AtomicPointer
    We need deduction guides to turn the AtomicPointer template argument
    (the pointee) into a pointer:
    
        QAtomicPointer<int> → QAtomicScopedValueRollback<int*>
    
    Extend a test to cover pointers, too.
    
    Fixes: QTBUG-115105
    Change-Id: Ib416c6a43e4da480b707a0bf6a10d186bbaad163
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 97ec1d7d8ecb07be20cf43f475067faba04f5aa5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/tools/qatomicscopedvaluerollback_p.h
+++ src/corelib/tools/qatomicscopedvaluerollback_p.h
@@ -108,6 +108,13 @@ public:
     }
 };
 
+template <typename T>
+QAtomicScopedValueRollback(QBasicAtomicPointer<T> &)
+    -> QAtomicScopedValueRollback<T*>;
+template <typename T>
+QAtomicScopedValueRollback(QBasicAtomicPointer<T> &, std::memory_order)
+    -> QAtomicScopedValueRollback<T*>;
+
 QT_END_NAMESPACE
 
 #endif // QATOMICASCOPEDVALUEROLLBACK_P_H
