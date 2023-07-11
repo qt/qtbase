b386b5b00b42ebdc7489a4854b2e3f48aff1cdc7
Joerg Bornemann <joerg.bornemann@qt.io>
2023-07-11 14:17:51 +0200

    CMake: Fix error in qt_generate_deploy_app_script
    ...when using a Qt cross-built for embedded Linux and
    NO_UNSUPPORTED_PLATFORM_ERROR set.
    
    Attempting to install such a project will now give the following output:
    
    -- Skipping runtime deployment steps. Support for installing runtime
    dependencies is not implemented for this target platform (Linux, shared
    Qt libs, cross-compiled).
    
    Fixes: QTBUG-114069
    Change-Id: Idd2af2135d2ca3cc0e5eeafb7701e891f8a0cc25
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 7c82a49e6fc4ec606cffa4fa8d3ceb42c87439cd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/Qt6CoreMacros.cmake
+++ src/corelib/Qt6CoreMacros.cmake
@@ -3057,6 +3057,10 @@ function(qt6_generate_deploy_app_script)
         set(qt_build_type_string "static Qt libs")
     endif()
 
+    if(CMAKE_CROSSCOMPILING)
+        string(APPEND qt_build_type_string ", cross-compiled")
+    endif()
+
     set(generate_args
         TARGET ${arg_TARGET}
         OUTPUT_SCRIPT deploy_script
@@ -3102,7 +3106,8 @@ qt6_deploy_runtime_dependencies(
 ${common_deploy_args})
 ")
 
-    elseif(UNIX AND NOT APPLE AND NOT ANDROID AND QT6_IS_SHARED_LIBS_BUILD)
+    elseif(UNIX AND NOT APPLE AND NOT ANDROID AND QT6_IS_SHARED_LIBS_BUILD
+            AND NOT CMAKE_CROSSCOMPILING)
         qt6_generate_deploy_script(${generate_args}
             CONTENT "
 qt6_deploy_runtime_dependencies(
