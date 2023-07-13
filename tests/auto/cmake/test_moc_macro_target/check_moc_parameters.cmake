e4c396f254ac61630278b96a68057f263920a73f
Orkun Tokdemir <orkun.tokdemir@qt.io>
2023-07-13 15:30:09 +0200

    _qt_internal_create_moc_command: Fix genex parse
    The function was replacing the `>` character in generator expressions coming from `add_compile_definitions`. This was creating generator expression syntax errors. Discard generator expressions from character replacing.
    Add tests for the three cases.
    
    Amends 5bb745f62ba1aa63e4bbd107b279d96bd06dffda
    
    Fixes: QTBUG-111717
    Fixes: QTBUG-119716
    Change-Id: I694d2908738085fdf15112834f20183a9f393422
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit bc0c44d4cd634c574307920968f0ab1bf6cd8305)
    Reviewed-by: Orkun Tokdemir <orkun.tokdemir@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>

---
+++ tests/auto/cmake/test_moc_macro_target/check_moc_parameters.cmake
@@ -0,0 +1,15 @@
+
+function(check_parameters file_path)
+    file(READ ${file_path} file_content)
+    foreach(compile_option IN ITEMS "-DDEFINE_CMDLINE_SIGNAL" "-DMY_OPTION")
+        string(REGEX MATCHALL "${compile_option}" matches ${file_content})
+        list(LENGTH matches matches_length)
+        if(matches_length GREATER 1)
+            message(FATAL_ERROR "${compile_option} is defined multiple times in ${file_path}")
+        elseif(matches_length EQUAL 0)
+            message(FATAL_ERROR "${compile_option} is not defined in ${file_path}")
+        endif()
+    endforeach()
+endfunction()
+
+check_parameters(${PARAMETERS_FILE_PATH})
