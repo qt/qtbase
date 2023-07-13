e307f57e9daddb218e638061b94fff5506ef5004
Axel Spoerl <axel.spoerl@qt.io>
2023-07-13 15:48:46 +0200

    QWidget: Don't re-use backing store on Android
    QWidget re-uses backing stores created in QWidgetPrivate::create().
    The Android platform plugin creates a new platform window, when a
    widget becomes a toplevel window. When it is hidden, the platform
    window is deleted. The link between QAndroidPlatformWindow and its
    backing store is made in the constructor of
    QAndroidPlatformBackingstore. When a new QAndroidPlatformWindow is
    constructed and the backing store is re-used, there is no more link
    between platform window and platform backing store.
    This has lead to screen assets not being painted, when shown more than
    once.
    
    This patch forces QWidgetPrivate::create() to construct a new backing
    store on Android. This way, toplevel windows always have a backing
    store associated with it. It adds an assertion to
    QAndroidPlatformScreen::addWindow(). That will make e.g.
    tst_QWidget::visible() crash without the fix.
    
    Fixes: QTBUG-97482
    Change-Id: Ib1b172068b03549df161ab93ac24a273221d5423
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>
    (cherry picked from commit dbb072eb2838a04e89e34dad686394a496d5de87)
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- src/plugins/platforms/android/qandroidplatformscreen.cpp
+++ src/plugins/platforms/android/qandroidplatformscreen.cpp
@@ -79,7 +79,7 @@ public:
     }
 
 private:
-    QTime m_timer;
+    QElapsedTimer m_timer;
     QString m_msg;
 };
 
@@ -188,6 +188,7 @@ bool QAndroidPlatformScreen::event(QEvent *event)
 
 void QAndroidPlatformScreen::addWindow(QAndroidPlatformWindow *window)
 {
+    Q_ASSERT(window->backingStore());
     if (window->parent() && window->isRaster())
         return;
 
