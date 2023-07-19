1552c79d3187d291598b13085789ec6d874248f8
Laszlo Agocs <laszlo.agocs@qt.io>
2023-07-19 11:19:28 +0200

    rhi: Run cleanup callbacks before the pending deletes
    If a callback deleteLaters a QRhiResource that should still be
    handled and not left unreleased. Swap the order.
    
    Change-Id: I8419a28a9db5bb59f768ab5820dfaf593464d6d2
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>
    (cherry picked from commit d907f1aa92dbd4500a18db2c2492491c34c66087)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/rhi/qrhi.cpp
+++ src/gui/rhi/qrhi.cpp
@@ -5502,10 +5502,10 @@ QRhi::~QRhi()
     if (!d)
         return;
 
+    runCleanup();
+
     qDeleteAll(d->pendingDeleteResources);
     d->pendingDeleteResources.clear();
-
-    runCleanup();
 
     d->destroy();
     delete d;
