9d3c0491fd091e33229fd42fc2f5674ebdd62c9f
Thiago Macieira <thiago.macieira@intel.com>
2023-07-24 18:22:59 -0700

    QString: use equalStrings in operator== and operator!=
    QStringView already did since 6.0. I don't know how we missed this.
    
    Change-Id: I5acc02341c5940499682fffd1774f7c3a807317a
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 3c4c7860f08e45de2965ea544a8eb742549bb150)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qstring.h
+++ src/corelib/text/qstring.h
@@ -938,7 +938,7 @@ public:
     static QString number(double, char format='g', int precision=6);
 
     friend bool operator==(const QString &s1, const QString &s2) noexcept
-    { return (s1.size() == s2.size()) && QtPrivate::compareStrings(s1, s2, Qt::CaseSensitive) == 0; }
+    { return (s1.size() == s2.size()) && QtPrivate::equalStrings(s1, s2); }
     friend bool operator< (const QString &s1, const QString &s2) noexcept
     { return QtPrivate::compareStrings(s1, s2, Qt::CaseSensitive) < 0; }
     friend bool operator> (const QString &s1, const QString &s2) noexcept { return s2 < s1; }
@@ -947,7 +947,7 @@ public:
     friend bool operator>=(const QString &s1, const QString &s2) noexcept { return !(s1 < s2); }
 
     friend bool operator==(const QString &s1, QLatin1StringView s2) noexcept
-    { return (s1.size() == s2.size()) && QtPrivate::compareStrings(s1, s2, Qt::CaseSensitive) == 0; }
+    { return (s1.size() == s2.size()) && QtPrivate::equalStrings(s1, s2); }
     friend bool operator< (const QString &s1, QLatin1StringView s2) noexcept
     { return QtPrivate::compareStrings(s1, s2, Qt::CaseSensitive) < 0; }
     friend bool operator> (const QString &s1, QLatin1StringView s2) noexcept
