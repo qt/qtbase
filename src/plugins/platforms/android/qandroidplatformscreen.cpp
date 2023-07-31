qt/qtbase

commit dbb072eb2838a04e89e34dad686394a496d5de87

QWidget: Don't re-use backing store on Android
QWidget re-uses backing stores created in QWidgetPrivate::create().
The Android platform plugin creates a new platform window, when a
widget becomes a toplevel window. When it is hidden, the platform
window is deleted. The link between QAndroidPlatformWindow and its
backing store is made in the constructor of
QAndroidPlatformBackingstore. When a new QAndroidPlatformWindow is
constructed and the backing store is re-used, there is no more link
between platform window and platform backing store.
This has lead to screen assets not being painted, when shown more than
once.

This patch forces QWidgetPrivate::create() to construct a new backing
store on Android. This way, toplevel windows always have a backing
store associated with it. It adds an assertion to
QAndroidPlatformScreen::addWindow(). That will make e.g.
tst_QWidget::visible() crash without the fix.

Fixes: QTBUG-97482
Pick-to: 6.6 6.5 6.2
Change-Id: Ib1b172068b03549df161ab93ac24a273221d5423
Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>

src/plugins/platforms/android/qandroidplatformscreen.cpp
@@ -43,7 +43,7 @@ public:
     }
 
 private:
-    QTime m_timer;
+    QElapsedTimer m_timer;
     QString m_msg;
 };
 
@@ -172,6 +172,7 @@ bool QAndroidPlatformScreen::event(QEvent *event)
 
 void QAndroidPlatformScreen::addWindow(QAndroidPlatformWindow *window)
 {
+    Q_ASSERT(window->backingStore());
     if (window->parent() && window->isRaster())
         return;
 

src/widgets/kernel/qwidget.cpp
@@ -1329,7 +1329,13 @@ void QWidgetPrivate::create()
     }
 #endif
 
+    // Android doesn't allow to re-use the backing store.
+    // => force creation of a new one.
+#ifdef Q_OS_ANDROID
+    QBackingStore *store = nullptr;
+#else
     QBackingStore *store = q->backingStore();
+#endif
     usesRhiFlush = false;
 
     if (!store) {

commit 9a39ad8dfb4e6d1a179bd0fa38026886f8f7cb8e

Remove assertion in QAndroidPlatformScreen::addWindow()
QAndroidPlatformScreen::addWindow() asserted the existence of a backing
store, when a window is added. But QQuickWindow neither needs nor has a
backing store.

This patch removes the assertion, which was introduced in
dbb072eb2838a04e89e34dad686394a496d5de87

Task-number: QTBUG-97482
Pick-to: 6.6 6.5 6.2 5.15
Change-Id: Ifb43f83d39e16b972754e86d0ff9633f58dac3f8
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/plugins/platforms/android/qandroidplatformscreen.cpp
@@ -172,7 +172,6 @@ bool QAndroidPlatformScreen::event(QEvent *event)
 
 void QAndroidPlatformScreen::addWindow(QAndroidPlatformWindow *window)
 {
-    Q_ASSERT(window->backingStore());
     if (window->parent() && window->isRaster())
         return;
 