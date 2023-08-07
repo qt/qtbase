b7c6b670baf13cdf12b3ba93504e8df86e7b0af1
Marc Mutz <marc.mutz@qt.io>
2023-08-07 22:44:55 +0200

    tst_QMimeDatabase{Xml,Cache}: make the two siblings behave the same
    First include the common tst_qmimedatabase.cpp (and nothing else),
    then implement the differing
    tst_QMimeDatabase::initTestCaseInternal().
    
    This will allow adding #undef QT_NO_FOREACH to tst_qmimedatabase.cpp
    in the next step.
    
    Pick-to: 6.6 6.5
    Change-Id: Icc1890229e9443bd35c81d4f0440ba7df5da906c
    Reviewed-by: David Faure <david.faure@kdab.com>

--- tests/auto/corelib/mimetypes/qmimedatabase/qmimedatabase-cache/tst_qmimedatabase-cache.cpp
+++ tests/auto/corelib/mimetypes/qmimedatabase/qmimedatabase-cache/tst_qmimedatabase-cache.cpp
@@ -1,12 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include "../tst_qmimedatabase.h"
-#include <QDir>
-#include <QFile>
-#include <QTest>
-#include <qstandardpaths.h>
-
 #include "../tst_qmimedatabase.cpp"
 
 void tst_QMimeDatabase::initTestCaseInternal()
