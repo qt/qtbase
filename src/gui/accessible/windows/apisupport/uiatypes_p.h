ef1ceabe385db306ec9b3e52da7e7370191c0a26
Jan Arve Sæther <jan-arve.saether@qt.io>
2023-08-16 09:51:52 +0200

    Revert "Windows QPA: Add support to UiaRaiseNotificationEvent()"
    This reverts commit 566def740ec58e842e6bb37177f80e20aebaa245.
    
    The fix had some unwanted side-effects when the
    QAccessibleValueChangeEvent carried a value of type string. Only
    QComboBox uses QAccessibleValueChangeEvent in such a way.
    The consequence of the reverted patch was that it broke QComboBox
    so that the screen reader would read aloud the value change event
    regardless of the visibility or focus state of the QComboBox. (Thus, if
    you e.g. changed the QComboBox::currentIndex on a *hidden* combo box,
    the screen reader would still read aloud the event)
    This is also the root cause of what is described in QTBUG-93763.
    
    Also, due to the usage of NotificationProcessing_ImportantMostRecent
    the screen reader would treat it as such an important event that it
    would abort whatever it was currently speaking.
    
    In addition, the reverted change didn't fix any bugs and it failed to
    implement the suggested behavior in a correct way (as was described in
    QTBUG-75003 - albeit properly described after the change was merged).
    QTBUG-75003 has already been reopened due to this, and the change can
    therefore be reverted quite risk-free.
    
    Task-number: QTBUG-75003
    Task-number: QTBUG-93763
    Change-Id: Ib91872adc563c31534fe2b30fd9c447bfcca6b40
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 193fb8bb68e35f6ad25a7d62a6e321bd41ecf688)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/accessible/windows/apisupport/uiatypes_p.h
+++ src/gui/accessible/windows/apisupport/uiatypes_p.h
@@ -126,22 +126,6 @@ enum ExpandCollapseState {
     ExpandCollapseState_LeafNode          = 3
 };
 
-enum NotificationKind {
-    NotificationKind_ItemAdded       = 0,
-    NotificationKind_ItemRemoved     = 1,
-    NotificationKind_ActionCompleted = 2,
-    NotificationKind_ActionAborted   = 3,
-    NotificationKind_Other           = 4
-};
-
-enum NotificationProcessing {
-    NotificationProcessing_ImportantAll          = 0,
-    NotificationProcessing_ImportantMostRecent   = 1,
-    NotificationProcessing_All                   = 2,
-    NotificationProcessing_MostRecent            = 3,
-    NotificationProcessing_CurrentThenMostRecent = 4
-};
-
 struct UiaRect {
     double left;
     double top;
