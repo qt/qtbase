a88f26f7e4fad73d9f31e3b493091415d5f8a20d
Rym Bouabid <rym.bouabid@qt.io>
2023-09-06 14:20:24 +0200

    Revamp Mandelbrot example: Fix Includes
    Reorder the includes following the coding conventions.
    
    Remove unneeded includes.
    
    Add needed includes to avoid Transitive includes.
    
    QGestureEvent is passed by a pointer in the gestureEvent method in
    mandelbrotwidget.h. So, we do not really need to include the header.
    Instead, forward-declare QGestureEvent class in a namespace.
    
    Task-number: QTBUG-108861
    Pick-to: 6.5
    Change-Id: I48cfd663bf98350281ef7f31e6c0ef3e301a08ca
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit a4d7a4ccb39daa86ee2ef23b1408344822ae7c16)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/corelib/threads/mandelbrot/main.cpp
+++ examples/corelib/threads/mandelbrot/main.cpp
@@ -2,15 +2,12 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #include "mandelbrotwidget.h"
+#include "renderthread.h"
 
 #include <QApplication>
-
-#include <QScreen>
-
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 #include <QDebug>
-#include <QRect>
 
 using namespace Qt::StringLiterals;
 
