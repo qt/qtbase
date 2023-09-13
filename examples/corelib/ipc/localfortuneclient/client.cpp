49eb2d1d8e21c0748075021b655df8d094ef61d6
Rym Bouabid <rym.bouabid@qt.io>
2023-09-13 17:12:20 +0200

    Revamp Local Fortune Client Example: Fix include order
    Fix include order to respect the Coding Conventions.
    
    Include individual headers instead of "generic" headers.
    
    Remove QT_{BEGIN,END}_NAMESPACE because these are private Qt macros that
    should not be used in the examples.
    
    Task-number: QTBUG-117036
    Pick-to: 6.5
    Change-Id: I33f9c54098824bfcfacac7c2f624554e105a291a
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit b83736be05e8ba4bcc586ff276ce81f992e0ef76)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/corelib/ipc/localfortuneclient/client.cpp
+++ examples/corelib/ipc/localfortuneclient/client.cpp
@@ -1,10 +1,13 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-#include <QtWidgets>
-#include <QtNetwork>
-
 #include "client.h"
+
+#include <QDialogButtonBox>
+#include <QGridLayout>
+#include <QGuiApplication>
+#include <QMessageBox>
+#include <QTimer>
 
 Client::Client(QWidget *parent)
     : QDialog(parent),
