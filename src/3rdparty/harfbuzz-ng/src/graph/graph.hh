a7dc536786383f67e162add644e46b450559dbe0
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2023-09-18 13:34:02 +0200

    Upgrade Harfbuzz to version 8.2.0
    Task-number: QTBUG-117136
    Pick-to: 6.2.10
    Change-Id: I910c8bff2fe521aa02929b9000fa7f38192d1a51
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 08134e458d7fdd0b9565976046821b3903166dd6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5a6d960018c1aac70c46ef1f9481b19ce01734df)
    (cherry picked from commit 27aeaf01080f07ae770bfb60973bff906435849d)

--- src/3rdparty/harfbuzz-ng/src/graph/graph.hh
+++ src/3rdparty/harfbuzz-ng/src/graph/graph.hh
@@ -290,7 +290,7 @@ struct graph_t
 	new_parents.set (id_map[_.first], _.second);
       }
 
-      if (new_parents.in_error ())
+      if (parents.in_error() || new_parents.in_error ())
         return false;
 
       parents = std::move (new_parents);
@@ -310,8 +310,15 @@ struct graph_t
       if (parents.has (old_index, &pv))
       {
         unsigned v = *pv;
-	parents.set (new_index, v);
+	if (!parents.set (new_index, v))
+          incoming_edges_ -= v;
 	parents.del (old_index);
+
+        if (incoming_edges_ == 1)
+	{
+	  single_parent = *parents.keys ();
+	  parents.reset ();
+	}
       }
     }
 
@@ -465,6 +472,18 @@ struct graph_t
   bool operator== (const graph_t& other) const
   {
     return root ().equals (other.root (), *this, other, 0);
+  }
+
+  void print () const {
+    for (int i = vertices_.length - 1; i >= 0; i--)
+    {
+      const auto& v = vertices_[i];
+      printf("%d: %lu [", i, v.table_size());
+      for (const auto &l : v.obj.real_links) {
+        printf("%u, ", l.objidx);
+      }
+      printf("]\n");
+    }
   }
 
   // Sorts links of all objects in a consistent manner and zeroes all offsets.
