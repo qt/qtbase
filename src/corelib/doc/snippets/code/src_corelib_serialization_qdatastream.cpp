ab5d3a2958f839589072a4ea4b89ce105637713d
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2023-09-24 19:12:39 +0200

    QDataStream: Documentation fix
    Fix the code snippets to match the Qt coding style.
    
    Change-Id: Id65d2253e620d217fa3ada7b82e28f4939336543
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/corelib/doc/snippets/code/src_corelib_serialization_qdatastream.cpp
+++ src/corelib/doc/snippets/code/src_corelib_serialization_qdatastream.cpp
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 //! [0]
-    QDataStream &operator<<(QDataStream &, const QXxx &);
-    QDataStream &operator>>(QDataStream &, QXxx &);
+QDataStream &operator<<(QDataStream &, const QXxx &);
+QDataStream &operator>>(QDataStream &, QXxx &);
 //! [0]
 
 //! [1]
-    QDataStream & operator<< (QDataStream& stream, const QImage& image);
-    QDataStream & operator>> (QDataStream& stream, QImage& image);
+QDataStream &operator<<(QDataStream &stream, const QImage &image);
+QDataStream &operator>>(QDataStream &stream, QImage &image);
 //! [1]
