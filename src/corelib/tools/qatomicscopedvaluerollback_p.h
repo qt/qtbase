b9def99fa47f11af953065373acefb8147565bf3
Marc Mutz <marc.mutz@qt.io>
2023-09-29 15:13:00 +0200

    QAtomicScopedValueRollback: fix UB (passing rel/acq_rel to std::atomic::load())
    It's explicitly undefined behavior to pass release/acq_rel
    memory_order to load(), so don't.
    
    This is private API, so no ChangeLog needed.
    
    Reported-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Task-number: QTBUG-115107
    Change-Id: Iee119303d790c31937238ef92d900a25020e9713
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 5797f29e8c249e13da6285ae9b2d50fff4ffb505)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 401950a76f26f9827715eaf0df7fcc4076aaf151)
    (cherry picked from commit 8a6908e726161d1755c3019a634e988586a1494a)

--- src/corelib/tools/qatomicscopedvaluerollback_p.h
+++ src/corelib/tools/qatomicscopedvaluerollback_p.h
@@ -48,6 +48,24 @@ class [[nodiscard]] QAtomicScopedValueRollback
 #endif
         return std::memory_order_seq_cst;
     }
+
+    static constexpr std::memory_order load_part(std::memory_order mo) noexcept
+    {
+        switch (mo) {
+        case std::memory_order_relaxed:
+        case std::memory_order_release: return std::memory_order_relaxed;
+        case std::memory_order_consume: return std::memory_order_consume;
+        case std::memory_order_acquire:
+        case std::memory_order_acq_rel: return std::memory_order_acquire;
+        case std::memory_order_seq_cst: return std::memory_order_seq_cst;
+        }
+        // GCC 8.x does not treat __builtin_unreachable() as constexpr
+#if !defined(Q_CC_GNU_ONLY) || (Q_CC_GNU >= 900)
+        // NOLINTNEXTLINE(qt-use-unreachable-return): Triggers on Clang, breaking GCC 8
+        Q_UNREACHABLE();
+#endif
+        return std::memory_order_seq_cst;
+    }
 public:
     //
     // std::atomic:
@@ -55,7 +73,7 @@ public:
     explicit constexpr
     QAtomicScopedValueRollback(std::atomic<T> &var,
                                std::memory_order mo = std::memory_order_seq_cst)
-        : m_atomic(var), m_value(var.load(mo)), m_mo(mo) {}
+        : m_atomic(var), m_value(var.load(load_part(mo))), m_mo(mo) {}
 
     explicit constexpr
     QAtomicScopedValueRollback(std::atomic<T> &var, T value,
@@ -98,7 +116,7 @@ public:
 
     constexpr void commit()
     {
-        m_value = m_atomic.load(m_mo);
+        m_value = m_atomic.load(load_part(m_mo));
     }
 };
 
