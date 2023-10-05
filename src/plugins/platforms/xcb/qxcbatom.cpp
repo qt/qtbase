fba38be414eda0eda45c4f7b24a20d239255ca87
Liang Qi <liang.qi@qt.io>
2023-10-05 13:28:32 +0200

    xcb: set _KDE_NET_WM_DESKTOP_FILE and _GTK_APPLICATION_ID for window
    in top level, which are in used very common for KDE and GNOME.
    
    Pick-to: 6.2
    Fixes: QTBUG-117488
    Change-Id: I88fe7b4afe44e4ac8f07e60e990cbe68498e98d9
    Reviewed-by: Nicolas Fella <nicolas.fella@kdab.com>
    Reviewed-by: Ilya Fedin <fedin-ilja2010@ya.ru>
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit 70d3c15e8e9ef5965a242022e6681dd87dff5bd4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 51129d65b931a0a5dd235f7df26a1fe9f421ad26)

--- src/plugins/platforms/xcb/qxcbatom.cpp
+++ src/plugins/platforms/xcb/qxcbatom.cpp
@@ -111,6 +111,7 @@ static const char *xcb_atomnames = {
     "_NET_WM_WINDOW_TYPE_NORMAL\0"
     "_KDE_NET_WM_WINDOW_TYPE_OVERRIDE\0"
 
+    "_KDE_NET_WM_DESKTOP_FILE\0"
     "_KDE_NET_WM_FRAME_STRUT\0"
     "_NET_FRAME_EXTENTS\0"
 
@@ -196,6 +197,7 @@ static const char *xcb_atomnames = {
     "_COMPIZ_DECOR_REQUEST\0"
     "_COMPIZ_DECOR_DELETE_PIXMAP\0"
     "_COMPIZ_TOOLKIT_ACTION\0"
+    "_GTK_APPLICATION_ID\0"
     "_GTK_LOAD_ICONTHEMES\0"
     "AT_SPI_BUS\0"
     "EDID\0"
