f0c2310ab8efb255be2737d43b0ae15774e1d0dc
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-10-19 13:50:53 +0200

    uic: Prepare for encountering fully qualified enum values in new .ui files
    This means checks have to be rewritten using endsWith().
    
    It mainly affects the QSizePolicy handling whose values originate from
    different sources, including numbers. To address this, refactor the
    code to deal with unqualified enum value names and introduce a helper
    function that fully qualifies the enumeration when writing out.
    
    Task-number: PYSIDE-2492
    Task-number: PYSIDE-1735
    Task-number: QTBUG-118473
    Change-Id: Iccbb884777aa9c696fbf48f9d7329353ef945b0f
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>
    (cherry picked from commit 2e82796022b9eaeeb3a231713603c39ae7b2099d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2ee11d3e019cb4deb3b2af0212f6edb9f6d77543)
    (cherry picked from commit c3280b616f443ac9c295e5f9b634f2662cdaade5)

--- src/tools/uic/cpp/cppwriteinitialization.cpp
+++ src/tools/uic/cpp/cppwriteinitialization.cpp
@@ -100,7 +100,7 @@ namespace {
         bool isVspacer = false;
         if (const DomProperty *o = properties.value(QLatin1String("orientation"))) {
             const QString orientation = o->elementEnum();
-            if (orientation == QLatin1String("Qt::Vertical") || orientation == QLatin1String("Vertical"))
+            if (orientation.endsWith(QLatin1String("Vertical")))
                 isVspacer = true;
         }
         const QString horizType = isVspacer ? QLatin1String("QSizePolicy::Minimum") : sizeType;
@@ -1283,7 +1283,7 @@ void WriteInitialization::writeProperties(const QString &varName,
                     && m_uic->customWidgetsInfo()->extends(className, "Line")) {
             // Line support
             QString shape = QLatin1String("QFrame::HLine");
-            if (p->elementEnum() == QLatin1String("Qt::Vertical"))
+            if (p->elementEnum().endsWith(QLatin1String("Vertical")))
                 shape = QLatin1String("QFrame::VLine");
 
             m_output << m_indent << varName << language::derefPointer << "setFrameShape("
