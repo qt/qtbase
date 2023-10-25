f419a8d67f2487aa07496539a49c0aca7eab1c27
Volker Hilsheimer <volker.hilsheimer@qt.io>
2023-10-25 16:14:54 +0200

    Android: add an appless test that recreates QGuiApplication
    Make sure this works and doesn't cause any QJniEnvironment or other
    JNI object lifetime issues.
    
    Change-Id: I08b55982266dfb1821517449f6eb9f2796cea9b7
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>

---
+++ tests/auto/corelib/platform/android_appless/tst_android_appless.cpp
@@ -0,0 +1,53 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include <QTest>
+#include <QTimer>
+#include <QGuiApplication>
+#include <QWindow>
+
+using namespace Qt::StringLiterals;
+
+class tst_AndroidAppless : public QObject
+{
+    Q_OBJECT
+private slots:
+    void app_data();
+    void app();
+
+};
+
+#define CREATE_DUMMY_ARGC_ARGV() \
+    int argc = 1; \
+    char *argv[] = { const_cast<char *>("tst_android_appless") };
+
+
+void tst_AndroidAppless::app_data()
+{
+    QTest::addColumn<QString>("displayName");
+    QTest::addRow("one") << "The first QGuiApplication instance";
+    QTest::addRow("two") << "The second QGuiApplication instance";
+}
+
+void tst_AndroidAppless::app()
+{
+    QFETCH(const QString, displayName);
+    CREATE_DUMMY_ARGC_ARGV()
+
+    QGuiApplication app(argc, argv);
+    app.setApplicationDisplayName(displayName);
+
+    QWindow window;
+    window.show();
+
+    QTimer::singleShot(1000, &app, QGuiApplication::quit);
+
+    window.show();
+    app.exec();
+}
+
+#undef CREATE_DUMMY_ARGC_ARGV
+
+QTEST_APPLESS_MAIN(tst_AndroidAppless)
+#include "tst_android_appless.moc"
+
