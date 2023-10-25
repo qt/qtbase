69811de2f5ac17adb38df17f4ded96c75cd58f55
Axel Spoerl <axel.spoerl@qt.io>
2023-10-25 10:05:15 +0200

    qsystemdetection.h: Update WASM in comments
    WASM has been added as a definition.
    Add it in the comment section as well.
    
    Change-Id: Id86520cad4c12d83234cc71a00cd3e484e2895dc
    Reviewed-by: Piotr Wierciński <piotr.wiercinski@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit e3d34d2fdbd6e732eaa64a871931935b906d96ea)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 9254bc610ce4b8da74b611f5a54faf0161dd639f)

--- src/corelib/global/qsystemdetection.h
+++ src/corelib/global/qsystemdetection.h
@@ -42,6 +42,7 @@
      ANDROID  - Android platform
      HAIKU    - Haiku
      WEBOS    - LG WebOS
+     WASM     - WebAssembly
 
    The following operating systems have variants:
      LINUX    - both Q_OS_LINUX and Q_OS_ANDROID are defined when building for Android
