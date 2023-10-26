4af7cf13c9ecb2475d59968c9f21c6c85465e330
Jarkko Koivikko <jarkko.koivikko@code-q.fi>
2023-10-26 12:49:01 +0300

    printpreview: Avoid rendering artifacts while scrolling zoomed view
    Fixes: QTBUG-120107
    Pick-to: 6.5
    Change-Id: I4de062c9ded47cc911abc0aebf1b1b7f57ed7e09
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>
    (cherry picked from commit f83e14b13a32b2cddff45dfdbce8e819c02aa9ca)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit dfc5e3f215706bd2f83954e133db6328d6017038)

--- src/printsupport/widgets/qprintpreviewwidget.cpp
+++ src/printsupport/widgets/qprintpreviewwidget.cpp
@@ -25,7 +25,6 @@ public:
         qreal border = qMax(paperSize.height(), paperSize.width()) / 25;
         brect = QRectF(QPointF(-border, -border),
                        QSizeF(paperSize)+QSizeF(2*border, 2*border));
-        setCacheMode(DeviceCoordinateCache);
     }
 
     QRectF boundingRect() const override
