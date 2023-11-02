7f7fd8ce96fd99a381483f2266d701076d6af73a
Assam Boudjelthia <assam.boudjelthia@qt.io>
2023-11-03 00:24:43 +0200

    Android: add missing extras native methods impls and registration
    This should have been done when the extras where moved to qtbase,
    it went under the rug and was forgotten.
    
    Pick-to: 6.5 6.2
    Fixes: QTBUG-114971
    Change-Id: Id7831a8bab8bedc1f869b9fc8bfc72acc3cca7a2
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 510cc564c07d41c2eb1689f1928c0b77f652ceb5)
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- src/corelib/platform/android/qandroidextras.cpp
+++ src/corelib/platform/android/qandroidextras.cpp
@@ -672,6 +672,45 @@ QAndroidBinder* QAndroidService::onBind(const QAndroidIntent &/*intent*/)
     return nullptr;
 }
 
+static jboolean onTransact(JNIEnv */*env*/, jclass /*cls*/, jlong id, jint code, jobject data,
+                           jobject reply, jint flags)
+{
+    if (!id)
+        return false;
+
+    return reinterpret_cast<QAndroidBinder*>(id)->onTransact(
+            code, QAndroidParcel(data), QAndroidParcel(reply), QAndroidBinder::CallType(flags));
+}
+
+static void onServiceConnected(JNIEnv */*env*/, jclass /*cls*/, jlong id, jstring name,
+                               jobject service)
+{
+    if (!id)
+        return;
+
+    return reinterpret_cast<QAndroidServiceConnection *>(id)->onServiceConnected(
+            QJniObject(name).toString(), QAndroidBinder(service));
+}
+
+static void onServiceDisconnected(JNIEnv */*env*/, jclass /*cls*/, jlong id, jstring name)
+{
+    if (!id)
+        return;
+
+    return reinterpret_cast<QAndroidServiceConnection *>(id)->onServiceDisconnected(
+            QJniObject(name).toString());
+}
+
+bool QtAndroidPrivate::registerExtrasNatives(QJniEnvironment &env)
+{
+    static const JNINativeMethod methods[] = {
+        {"onTransact", "(JILandroid/os/Parcel;Landroid/os/Parcel;I)Z", (void *)onTransact},
+        {"onServiceConnected", "(JLjava/lang/String;Landroid/os/IBinder;)V", (void *)onServiceConnected},
+        {"onServiceDisconnected", "(JLjava/lang/String;)V", (void *)onServiceDisconnected}
+    };
+
+    return env.registerNativeMethods("org/qtproject/qt/android/extras/QtNative", methods, 3);
+}
 
 /*!
     \class QAndroidIntent
@@ -1171,7 +1210,7 @@ QtAndroidPrivate::checkPermission(const QString &permission)
     return QtFuture::makeReadyValueFuture(result);
 }
 
-bool QtAndroidPrivate::registerPermissionNatives()
+bool QtAndroidPrivate::registerPermissionNatives(QJniEnvironment &env)
 {
     if (QtAndroidPrivate::androidSdkVersion() < 23)
         return true;
@@ -1181,7 +1220,6 @@ bool QtAndroidPrivate::registerPermissionNatives()
          reinterpret_cast<void *>(sendRequestPermissionsResult)
         }};
 
-    QJniEnvironment env;
     return env.registerNativeMethods(qtNativeClassName, methods, 1);
 }
 
