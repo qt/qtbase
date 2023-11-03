6d0b5051a6d20c20fdf4bd199eb9239ce2d56c11
Topi Reinio <topi.reinio@qt.io>
2023-11-03 12:18:34 +0000

    Doc: Fix \fn template arguments for Qt Gui
    Upcoming changes to QDoc require accurate definition for
    template arguments in \fn commands.
    
    Task-number: QTBUG-118080
    Change-Id: I75f8956c5fc094402837f96bd4598b60e3a7c649
    Reviewed-by: Luca Di Sera <luca.disera@qt.io>

--- src/gui/math3d/qmatrix4x4.cpp
+++ src/gui/math3d/qmatrix4x4.cpp
@@ -100,7 +100,7 @@ QMatrix4x4::QMatrix4x4(const float *values)
 */
 
 /*!
-    \fn QGenericMatrix<N, M, float> QMatrix4x4::toGenericMatrix() const
+    \fn template <int N, int M> QGenericMatrix<N, M, float> QMatrix4x4::toGenericMatrix() const
 
     Constructs a NxM generic matrix from the left-most N columns and
     top-most M rows of this 4x4 matrix.  If N or M is greater than 4,
