d2b109801e119a062f4e070b08255b8006780851
Wladimir Leuschner <wladimir.leuschner@qt.io>
2023-11-06 16:19:57 +0100

    Restore WindowState when changing SurfaceType
    Restore the WindowState, when a Window has to be recreated due to
    changed SurfaceType.
    
    Fixes: QTBUG-118194
    Pick-to: 6.5
    Change-Id: I40ba552cda9dfc8ab543611595c068f80b8fa1d6
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit c88211d1e4ac12eb2ae4990703a4f73c7085d624)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/kernel/qwidget.cpp
+++ src/widgets/kernel/qwidget.cpp
@@ -10871,8 +10871,10 @@ void QWidget::setParent(QWidget *parent, Qt::WindowFlags f)
                     recreate = true;
             }
             if (recreate) {
+                auto oldState = d->windowHandle(QWidgetPrivate::WindowHandleMode::Closest)->windowState();
                 newtlw->destroy();
                 newtlw->create();
+                d->windowHandle(QWidgetPrivate::WindowHandleMode::Closest)->setWindowState(oldState);
             }
         }
     }
