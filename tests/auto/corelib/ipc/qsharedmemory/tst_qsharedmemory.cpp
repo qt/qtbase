qt/qtbase

commit 02c42b26e1ff94047657c4838128cb5b22d24d2b

QSharedMemory: fix semaphore creation in legacy mode
The legacy mode was using the new nativeKey to create a semaphore.
As a result, on macOS the name of the semaphore key file was the same
as the name of the shared memory key file.
This lead to a situation when the file was mistakenly deleted when
destroying any instance of the shared memory, not only the one that
created it.
Fix it by using QSystemSemaphore::legacyNativeKey() in legacy mode.

Add a test to verify that we cannot re-create a shared memory with the
same key. The test was failing on macOS without the fix.

Fixes: QTBUG-111855
Pick-to: 6.6
Change-Id: Ib0bc41791e889b1888bbb8aa9044c6b053b63a5a
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

tests/auto/corelib/ipc/qsharedmemory/tst_qsharedmemory.cpp
@@ -78,6 +78,9 @@ private slots:
     void uniqueKey_data();
     void uniqueKey();
 
+    // legacy
+    void createWithSameKey();
+
 protected:
     void remove(const QNativeIpcKey &key);
 
@@ -898,6 +901,29 @@ void tst_QSharedMemory::uniqueKey()
     QCOMPARE(keyEqual, setEqual);
     QCOMPARE(nativeEqual, setEqual);
 }
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+void tst_QSharedMemory::createWithSameKey()
+{
+    const QString key = u"legacy_key"_s;
+    const qsizetype sz = 100;
+    QSharedMemory mem1(key);
+    QVERIFY(mem1.create(sz));
+
+    {
+        QSharedMemory mem2(key);
+        QVERIFY(!mem2.create(sz));
+        QVERIFY(mem2.attach());
+    }
+    // and the second create() should fail as well, QTBUG-111855
+    {
+        QSharedMemory mem2(key);
+        QVERIFY(!mem2.create(sz));
+        QVERIFY(mem2.attach());
+    }
+}
+QT_WARNING_POP
 
 QTEST_MAIN(tst_QSharedMemory)
 #include "tst_qsharedmemory.moc"

src/corelib/ipc/qsharedmemory.cpp
@@ -258,7 +258,11 @@ bool QSharedMemoryPrivate::initKey(SemaphoreAccessMode mode)
     if (!cleanHandle())
         return false;
 #if QT_CONFIG(systemsemaphore)
-    systemSemaphore.setNativeKey(semaphoreNativeKey(), 1, mode);
+    const QString legacyKey = QNativeIpcKeyPrivate::legacyKey(nativeKey);
+    const QNativeIpcKey semKey = legacyKey.isEmpty()
+            ? semaphoreNativeKey()
+            : QSystemSemaphore::legacyNativeKey(legacyKey, nativeKey.type());
+    systemSemaphore.setNativeKey(semKey, 1, mode);
     if (systemSemaphore.error() != QSystemSemaphore::NoError) {
         QString function = "QSharedMemoryPrivate::initKey"_L1;
         errorString = QSharedMemory::tr("%1: unable to set key on lock (%2)")

commit fb1dcbfcba35fe436b7be1c851a129da9f98cb55

QSharedMemory: fix attach() -> create() for legacy SystemV mode
The attach() -> create() sequence was not covered by unit-tests in
qtbase, but this approach is used inside QSharedImageLoader.

It turns out that 02c42b26e1ff94047657c4838128cb5b22d24d2b broke this
usecase for legacy mode, and the create() triggered an assertion
in QSharedMemorySystemV::updateNativeKeyFile(). Fix it by clearing
the nativeKeyFile if ftok() call in QSharedMemorySystemV::handle()
fails().

Add unit-tests for the attach() -> create() scenario.
These tests revealed that this scenario fails also in non-legacy mode
for SystemV, so add QEXPECT_FAIL for these cases for now. This will
be addressed in a separate patch.

Pick-to: 6.6
Change-Id: If133fa56c82eba902374dc48d2757046b3d40baf
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

src/corelib/ipc/qsharedmemory_systemv.cpp
@@ -76,6 +76,7 @@ key_t QSharedMemorySystemV::handle(QSharedMemoryPrivate *self)
     unix_key = ftok(nativeKeyFile, int(self->nativeKey.type()));
     if (unix_key < 0) {
         self->setUnixErrorString("QSharedMemory::handle"_L1);
+        nativeKeyFile.clear();
         unix_key = 0;
     }
     return unix_key;

tests/auto/corelib/ipc/qsharedmemory/tst_qsharedmemory.cpp
@@ -56,6 +56,8 @@ private slots:
     void removeWhileAttached();
     void emptyMemory();
     void readOnly();
+    void attachBeforeCreate_data();
+    void attachBeforeCreate();
 
     // basics all together
     void simpleProducerConsumer_data();
@@ -544,6 +546,39 @@ void tst_QSharedMemory::readOnly()
     p.waitForFinished();
     QCOMPARE(p.error(), QProcess::Crashed);
 #endif
+}
+
+void tst_QSharedMemory::attachBeforeCreate_data()
+{
+    QTest::addColumn<bool>("legacy");
+
+    QTest::addRow("legacy") << true;
+    QTest::addRow("non-legacy") << false;
+}
+
+void tst_QSharedMemory::attachBeforeCreate()
+{
+    QFETCH_GLOBAL(const QNativeIpcKey::Type, keyType);
+    QFETCH(const bool, legacy);
+    const QString keyStr(u"test"_s);
+    QNativeIpcKey key;
+    if (legacy) {
+        key = QSharedMemory::legacyNativeKey(keyStr, keyType);
+        // same as rememberKey(), but with legacy
+        if (!keys.contains(key)) {
+            keys.append(key);
+            remove(key);
+        }
+    } else {
+        key = rememberKey(keyStr);
+    }
+    const qsizetype sz = 100;
+    QSharedMemory mem(key);
+    QVERIFY(!mem.attach());
+    // Fails for all SystemV backends now
+    if (!legacy && (keyType < QNativeIpcKey::Type::PosixRealtime))
+        QEXPECT_FAIL("", "Not fixed yet", Continue);
+    QVERIFY(mem.create(sz));
 }
 
 /*!
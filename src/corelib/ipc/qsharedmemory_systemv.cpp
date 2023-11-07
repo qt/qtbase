fb1dcbfcba35fe436b7be1c851a129da9f98cb55
Ivan Solovev <ivan.solovev@qt.io>
2023-11-07 11:11:59 +0100

    QSharedMemory: fix attach() -> create() for legacy SystemV mode
    The attach() -> create() sequence was not covered by unit-tests in
    qtbase, but this approach is used inside QSharedImageLoader.
    
    It turns out that 02c42b26e1ff94047657c4838128cb5b22d24d2b broke this
    usecase for legacy mode, and the create() triggered an assertion
    in QSharedMemorySystemV::updateNativeKeyFile(). Fix it by clearing
    the nativeKeyFile if ftok() call in QSharedMemorySystemV::handle()
    fails().
    
    Add unit-tests for the attach() -> create() scenario.
    These tests revealed that this scenario fails also in non-legacy mode
    for SystemV, so add QEXPECT_FAIL for these cases for now. This will
    be addressed in a separate patch.
    
    Pick-to: 6.6
    Change-Id: If133fa56c82eba902374dc48d2757046b3d40baf
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- src/corelib/ipc/qsharedmemory_systemv.cpp
+++ src/corelib/ipc/qsharedmemory_systemv.cpp
@@ -76,6 +76,7 @@ key_t QSharedMemorySystemV::handle(QSharedMemoryPrivate *self)
     unix_key = ftok(nativeKeyFile, int(self->nativeKey.type()));
     if (unix_key < 0) {
         self->setUnixErrorString("QSharedMemory::handle"_L1);
+        nativeKeyFile.clear();
         unix_key = 0;
     }
     return unix_key;
