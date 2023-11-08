36465be5c129623b171821aa5305497c1f2e6985
Jonas Karlsson <jonas.karlsson@qt.io>
2023-11-08 12:53:50 +0100

    QHeaderView: relayout on resetDefaultSectionSize
    We now call setDefaultSectionSize with the new default section size.
    This clamps the value to min/max section size and it will resize
    affected sections.
    
    Fixes: QTBUG-116013
    Change-Id: I39849aca8d0672629ce0b3ca244038c27e045d4b
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit a8df174369cecd90f14dac85bf162353b7cb25d1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 4253b2b6e70ce6f2a2b5b18d9a5a230e1aa01b5e)

--- src/widgets/itemviews/qheaderview_p.h
+++ src/widgets/itemviews/qheaderview_p.h
@@ -141,7 +141,7 @@ public:
 
     inline void setDefaultValues(Qt::Orientation o) {
         orientation = o;
-        updateDefaultSectionSizeFromStyle();
+        defaultSectionSize = getDefaultSectionSizeFromStyle();
         defaultAlignment = (o == Qt::Horizontal
                             ? Qt::Alignment(Qt::AlignCenter)
                             : Qt::AlignLeft|Qt::AlignVCenter);
@@ -309,7 +309,7 @@ public:
     void removeSectionsFromSectionItems(int start, int end);
     void resizeSectionItem(int visualIndex, int oldSize, int newSize);
     void setDefaultSectionSize(int size);
-    void updateDefaultSectionSizeFromStyle();
+    int getDefaultSectionSizeFromStyle() const;
     void recalcSectionStartPos() const; // not really const
 
     inline int headerLength() const { // for debugging
