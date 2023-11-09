4f05ffeb41e02d1958f2d16cacc3bc4e21dd9b5b
Edward Welbourne <edward.welbourne@qt.io>
2023-11-09 17:21:39 +0100

    Restore ASN.1 Element tests after move to plugin
    When SSL backends were broken out as plugins, various tests were
    suppressed since the code they test is now in a plugin, no longer part
    of the network libraries.
    
    The ASN.1 test is, however, fairly self-contained, so just compile it
    with the relevant plugin sources (which are likewise self-contained)
    and brute-force the paths to line up for the test.
    
    Task-number: QTBUG-46843
    Change-Id: I778489e68b7361a7fd55c88d2a35257ad6a58c46
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

--- tests/auto/network/ssl/qasn1element/tst_qasn1element.cpp
+++ tests/auto/network/ssl/qasn1element/tst_qasn1element.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2014 Jeremy Lainé <jeremy.laine@m4x.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include "private/qasn1element_p.h"
+#include "qasn1element_p.h"
 
 #include <QTest>
 
