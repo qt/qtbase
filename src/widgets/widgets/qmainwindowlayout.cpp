65f59d96c77f25648e512490d56831206bccd3c8
Axel Spoerl <axel.spoerl@qt.io>
2023-11-14 09:46:55 +0100

    QMainWindowLayout: remove redundant #ifdef'ry
    Remove #if QT_CONFIG(dockwidget) nested within each other.
    
    Pick-to: 6.5
    Change-Id: I6c662909676ffada3ac52e41a9c2d8b9fd491689
    Reviewed-by: David Faure <david.faure@kdab.com>
    (cherry picked from commit 0e435b7f13619106d61c9701e53803d3c488fea3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/widgets/qmainwindowlayout.cpp
+++ src/widgets/widgets/qmainwindowlayout.cpp
@@ -1507,7 +1507,6 @@ void QMainWindowLayout::showDockWidgets() const
 
 // Checks if QDockWidgetGroupWindow or QDockWidget can be plugged the area indicated by path.
 // Returns false if called with invalid widget type or if compiled without dockwidget support.
-#if QT_CONFIG(dockwidget)
 static bool isAreaAllowed(QWidget *widget, const QList<int> &path)
 {
     Q_ASSERT_X((path.size() > 1), "isAreaAllowed", "invalid path size");
@@ -1542,7 +1541,6 @@ static bool isAreaAllowed(QWidget *widget, const QList<int> &path)
     qCDebug(lcQpaDockWidgets) << "Docking requested for invalid widget type (coding error)." << widget << area;
     return false;
 }
-#endif
 
 void QMainWindowLayout::setCorner(Qt::Corner corner, Qt::DockWidgetArea area)
 {
@@ -1562,7 +1560,6 @@ Qt::DockWidgetArea QMainWindowLayout::corner(Qt::Corner corner) const
 // Returns the rectangle of a dockWidgetArea
 // if max is true, the maximum possible rectangle for dropping is returned
 // the current visible rectangle otherwise
-#if QT_CONFIG(dockwidget)
 QRect QMainWindowLayout::dockWidgetAreaRect(const Qt::DockWidgetArea area, DockWidgetAreaSize size) const
 {
     const QInternal::DockPosition dockPosition = toDockPos(area);
@@ -1578,7 +1575,6 @@ QRect QMainWindowLayout::dockWidgetAreaRect(const Qt::DockWidgetArea area, DockW
     // Return maximum or visible rectangle
     return (size == Maximum) ? dl.gapRect(dockPosition) : dl.docks[dockPosition].rect;
 }
-#endif
 
 void QMainWindowLayout::addDockWidget(Qt::DockWidgetArea area,
                                              QDockWidget *dockwidget,
