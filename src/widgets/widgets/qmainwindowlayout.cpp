d462c7b09c07b366ee5cea00116e32a4b7fedac6
Axel Spoerl <axel.spoerl@qt.io>
2023-11-20 10:11:14 +0100

    QMainWindowTabBar: make destructor public
    Was private by mistake => make it public.
    
    Pick-to: 6.6 6.5
    Change-Id: I6b07a19687ddf84e8456aa70bc34b1cc714a299e
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/widgets/widgets/qmainwindowlayout.cpp
+++ src/widgets/widgets/qmainwindowlayout.cpp
@@ -1865,9 +1865,9 @@ class QMainWindowTabBar : public QTabBar
     Q_OBJECT
     QMainWindow *mainWindow;
     QPointer<QDockWidget> draggingDock; // Currently dragging (detached) dock widget
-    ~QMainWindowTabBar();
 public:
     QMainWindowTabBar(QMainWindow *parent);
+    ~QMainWindowTabBar();
     QDockWidget *dockAt(int index) const;
     QList<QDockWidget *> dockWidgets() const;
 protected:
