16546515f3fa8d7669ba701bf507672918b741f2
Kai Köhne <kai.koehne@qt.io>
2023-12-03 16:58:38 +0100

    headersclean: Remove unneeded exceptions
    Change-Id: I431d530aa4d588aad841b80b13d5e0caa4c8b445
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>

--- cmake/QtHeadersClean.cmake
+++ cmake/QtHeadersClean.cmake
@@ -108,7 +108,6 @@ function(qt_internal_add_headersclean_target module_target module_headers)
             -Woverloaded-virtual -Wshadow -Wundef -Wfloat-equal
             -Wnon-virtual-dtor -Wpointer-arith -Wformat-security
             -Wchar-subscripts -Wold-style-cast
-            -Wno-long-long -Wno-variadic-macros
             -fno-operator-names)
 
         if(QT_FEATURE_reduce_relocations AND UNIX)
