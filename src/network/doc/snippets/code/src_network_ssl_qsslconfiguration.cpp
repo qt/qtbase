96539466b585fda9ed0016a19564d4766ceba74c
Timur Pocheptsov <timur.pocheptsov@qt.io>
2023-12-05 13:09:50 +0100

    QSslConfiguration: remove example
    It uses the ciphersuite name which smart tools one day will mark as
    weak (i.e. https://ciphersuite.info/cs/TLS_DHE_DSS_WITH_AES_256_CBC_SHA/
    in our example), names in the example are not in the standard notation,
    + it is and always was OpenSSL-specific example.
    
    Pick-to: 6.2
    Fixes: QTBUG-119464
    Change-Id: Ic6f095ff2c861582de756bf220e2e74106b88e83
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 66a5f9fdd40485a4e64d447f84046698b7cacae3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 68340898452f4a4c2f2d70bcb80b0f919cff57e7)

--- src/network/doc/snippets/code/src_network_ssl_qsslconfiguration.cpp
+++ src/network/doc/snippets/code/src_network_ssl_qsslconfiguration.cpp
@@ -6,10 +6,3 @@ QSslConfiguration config = sslSocket.sslConfiguration();
 config.setProtocol(QSsl::TlsV1_2);
 sslSocket.setSslConfiguration(config);
 //! [0]
-
-
-//! [1]
-QSslConfiguration tlsConfig = QSslConfiguration::defaultConfiguration();
-tlsConfig.setCiphers(QStringLiteral("DHE-RSA-AES256-SHA:DHE-DSS-AES256-SHA:AES256-SHA"));
-//! [1]
-
