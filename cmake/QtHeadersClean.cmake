qt/qtbase

commit 0f985e16ba741695182020f2c6a952f31e44c722

headersclean: Simplify logic for gcc only options
Oldest GCC version we nowadays support is GCC 9.

Change-Id: If35ef1aa7bf63da55404832107226e1fa139723e
Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

cmake/QtHeadersClean.cmake
@@ -121,13 +121,7 @@ function(qt_internal_add_headersclean_target module_target module_headers)
         endif()
 
         if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-            list(APPEND hcleanFLAGS -Wzero-as-null-pointer-constant)
-            if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 4.5)
-                list(APPEND hcleanFLAGS -Wdouble-promotion)
-            endif()
-            if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 4.9)
-                list(APPEND hcleanFLAGS -Wfloat-conversion)
-            endif()
+            list(APPEND hcleanFLAGS -Wdouble-promotion -Wfloat-conversion)
         endif()
 
         if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang|IntelLLVM")

commit c5864a96a6670d9cece5eaa18cab8c4c339f8706

headersclean: Re-add zero-as-null-pointer-constant warning
Amends 0f985e16ba741695182020f2c6a952f31e44c722, which accidentally
dropped this flag (duh!).

Change-Id: Ie772f9e524625edfcdcda4d73a9a712fcd77f8c8
Reviewed-by: Marc Mutz <marc.mutz@qt.io>

cmake/QtHeadersClean.cmake
@@ -120,7 +120,8 @@ function(qt_internal_add_headersclean_target module_target module_headers)
         endif()
 
         if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-            list(APPEND hcleanFLAGS -Wdouble-promotion -Wfloat-conversion)
+            list(APPEND hcleanFLAGS -Wzero-as-null-pointer-constant
+                -Wdouble-promotion -Wfloat-conversion)
         endif()
 
         if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang|IntelLLVM")