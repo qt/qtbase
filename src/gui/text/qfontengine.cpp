qt/qtbase

commit 58907dfa812de8ce6c63295589f644bdf4257d15

Try to match variant-selector font to preceding character
Variant-selectors are special unicode symbols which are used to
modify glyph selection for the preceding character. For instance,
a regular symbol could be turned into a color emoji using VS16,
the emoji variation selector. In order for this to work, however,
the font that handles the selector has to handle the full pair of
characters, so that it can apply the correct substitution rules.

One specific example of this was on macOS, where an airplane
symbol + VS16 would match the symbol to the default UI font but
the VS16 to the emoji font. Since there string provided for the
emoji font did not have any preceding character for VS16, we just
ignored it.

To improve on this, we now detect variation selectors that have
been matched to different font engines than the preceding
character. When such a case occurs, we check if the selector font
also supports the preceding character, and if it does, we keep
the pair together and use the same font for both.

[ChangeLog][QtGui][Text] Fix some cases where a variation-selector
character would be ignored in font selection and the correct
variant of a character would thus not be selected.

Task-number: QTBUG-108799
Change-Id: I9f427e0520e652ee2f24a4f7dc3c1957251e06bd
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/gui/text/qfontengine.cpp
@@ -1825,6 +1825,7 @@ bool QFontEngineMulti::stringToCMap(const QChar *str, int len,
     QStringIterator it(str, str + len);
 
     int lastFallback = -1;
+    char32_t previousUcs4 = 0;
     while (it.hasNext()) {
         const char32_t ucs4 = it.peekNext();
 
@@ -1886,10 +1887,35 @@ bool QFontEngineMulti::stringToCMap(const QChar *str, int len,
                     break;
                 }
             }
+
+            // For variant-selectors, they are modifiers to the previous character. If we
+            // end up with different font selections for the selector and the character it
+            // modifies, we try applying the selector font to the preceding character as well
+            const int variantSelectorBlock = 0xFE00;
+            if ((ucs4 & 0xFFF0) == variantSelectorBlock && glyph_pos > 0) {
+                int selectorFontEngine = glyphs->glyphs[glyph_pos] >> 24;
+                int precedingCharacterFontEngine = glyphs->glyphs[glyph_pos - 1] >> 24;
+
+                if (selectorFontEngine != precedingCharacterFontEngine) {
+                    QFontEngine *engine = m_engines.at(selectorFontEngine);
+                    glyph_t glyph = engine->glyphIndex(previousUcs4);
+                    if (glyph != 0) {
+                        glyphs->glyphs[glyph_pos - 1] = glyph;
+                        if (!(flags & GlyphIndicesOnly)) {
+                            QGlyphLayout g = glyphs->mid(glyph_pos - 1, 1);
+                            engine->recalcAdvances(&g, flags);
+                        }
+
+                        // set the high byte to indicate which engine the glyph came from
+                        glyphs->glyphs[glyph_pos - 1] |= (selectorFontEngine << 24);
+                    }
+                }
+            }
         }
 
         it.advance();
         ++glyph_pos;
+        previousUcs4 = ucs4;
     }
 
     *nglyphs = glyph_pos;

tests/auto/gui/text/qglyphrun/tst_qglyphrun.cpp
@@ -633,8 +633,12 @@ void tst_QGlyphRun::defaultIgnorables()
 
         bool hasFullMainFontRun = false;
         for (const QGlyphRun &run : runs) {
+            // QtsSpecialFont will be used for at least five characters: AA[...]111
+            // Depending on the font selected for the 0xFE0F variant selector, the
+            // third 'A' may be in QtsSpecialFont or in the fallback. This is platform-specific,
+            // so we accept either.
             if (run.rawFont().familyName() == QStringLiteral("QtsSpecialTestFont")
-                    && run.glyphIndexes().size() == 6) {
+                    && run.glyphIndexes().size() >= 5) {
                 hasFullMainFontRun = true;
                 break;
             }

tests/baseline/painting/scripts/text.qps
@@ -165,7 +165,7 @@ translate 0 75
 save
   setPen black
   setFont "sansserif" 16 normal
-  drawText 0 40 "e😃m😇o😍j😜i😸!"
+  drawText 0 40 "e😃m😇o😍j😜i😸!✈️"
 restore
 
 translate 0 75

commit 35266dc3080dc13641616ed20e257d7b4c57778c

Fix regression when selecting emojis using VS-16
This amends 58907dfa812de8ce6c63295589f644bdf4257d15,
which tried to make sure we select a color variant of a
character when combining it with the VS-16 symbol, the
emoji selector (and vice versa with VS-15).

This is something which is not well-handled in Qt, and
the original work-around assumed that any font supporting the
conversion of Unicode characters to color glyphs would include
the VS-16 character in its map. This was true for the symptomatic
font on macOS, so it solved the problem there.

However, if you manually select a color font and then request a color
glyph, the font system should just pass it on and accept this. No
need for any magic adaptation. Same if you select a non-color font
and use VS-15.

To avoid regression, we detect this case and disable the behavior
introduced in 58907dfa812de8ce6c63295589f644bdf4257d15 when the existing
font selection is already meeting the criteria.

This is admittedly very hacky, and the plan is to do a larger refactor
where sequences of emojis are separated out ahead of time so that the
font selection can pick from a curated emoji font list for these.

But to fix the immediate regression, this will basically just return
to previous behavior for some cases which are currently prone to break,
so despite the added layer of conditions, it does improve the situation
temporarily.

Pick-to: 6.5 6.6 6.7
Task-number: QTBUG-111801
Fixes: QTBUG-118642
Change-Id: I1a3ca711426a042b22003b887883646132335a9f
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/gui/text/qfontengine.cpp
@@ -1908,17 +1908,32 @@ bool QFontEngineMulti::stringToCMap(const QChar *str, int len,
                 int precedingCharacterFontEngine = glyphs->glyphs[glyph_pos - 1] >> 24;
 
                 if (selectorFontEngine != precedingCharacterFontEngine) {
-                    QFontEngine *engine = m_engines.at(selectorFontEngine);
-                    glyph_t glyph = engine->glyphIndex(previousUcs4);
-                    if (glyph != 0) {
-                        glyphs->glyphs[glyph_pos - 1] = glyph;
-                        if (!(flags & GlyphIndicesOnly)) {
-                            QGlyphLayout g = glyphs->mid(glyph_pos - 1, 1);
-                            engine->recalcAdvances(&g, flags);
-                        }
+                    // Emoji variant selectors are specially handled and should affect font
+                    // selection. If VS-16 is used, then this means we want to select a color
+                    // font. If the selected font is already a color font, we do not need search
+                    // again. If the VS-15 is used, then this means we want to select a non-color
+                    // font. If the selected font is not a color font, we don't do anything.
+                    const QFontEngine *selectedEngine = m_engines.at(precedingCharacterFontEngine);
+                    const bool colorFont = selectedEngine->isColorFont();
+                    const char32_t vs15 = 0xFE0E;
+                    const char32_t vs16 = 0xFE0F;
+                    bool adaptVariantSelector = ucs4 < vs15
+                                                || (ucs4 == vs15 && colorFont)
+                                                || (ucs4 == vs16 && !colorFont);
 
-                        // set the high byte to indicate which engine the glyph came from
-                        glyphs->glyphs[glyph_pos - 1] |= (selectorFontEngine << 24);
+                    if (adaptVariantSelector) {
+                        QFontEngine *engine = m_engines.at(selectorFontEngine);
+                        glyph_t glyph = engine->glyphIndex(previousUcs4);
+                        if (glyph != 0) {
+                            glyphs->glyphs[glyph_pos - 1] = glyph;
+                            if (!(flags & GlyphIndicesOnly)) {
+                                QGlyphLayout g = glyphs->mid(glyph_pos - 1, 1);
+                                engine->recalcAdvances(&g, flags);
+                            }
+
+                            // set the high byte to indicate which engine the glyph came from
+                            glyphs->glyphs[glyph_pos - 1] |= (selectorFontEngine << 24);
+                        }
                     }
                 }
             }

src/gui/text/qfontengine_p.h
@@ -147,6 +147,8 @@ public:
         return subPixelPositionFor(QFixedPoint(x, 0)).x;
     }
 
+    bool isColorFont() const { return glyphFormat == Format_ARGB; }
+
     virtual QFixed emSquareSize() const { return ascent(); }
 
     /* returns 0 as glyph index for non existent glyphs */