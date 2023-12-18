c745ed94a4d5002cce0e7a6867bf1efc20e8612d
Kai Köhne <kai.koehne@qt.io>
2023-12-18 09:07:33 +0100

    Doc: Fix linking to QFileDialog::NativeFileDialog
    Pick-to: 6.5
    Task-number: QTBUG-119551
    Change-Id: I1c698673873e5534b6d7867f8bc752115c254241
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Inkamari Harjula <inkamari.harjula@qt.io>
    (cherry picked from commit 1edc866878926d3fba31fc9ee0d9c9abc52cc8a2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 84365c0b60648a924d072a399a586d5d5b980006)

--- src/widgets/dialogs/qfiledialog.cpp
+++ src/widgets/dialogs/qfiledialog.cpp
@@ -702,7 +702,7 @@ bool QFileDialogPrivate::usingWidgets() const
     Sets the given \a option to be enabled if \a on is true; otherwise,
     clears the given \a option.
 
-    Options (particularly the DontUseNativeDialogs option) should be set
+    Options (particularly the \l DontUseNativeDialog option) should be set
     before changing dialog properties or showing the dialog.
 
     Setting options while the dialog is visible is not guaranteed to have
@@ -742,7 +742,7 @@ bool QFileDialog::testOption(Option option) const
 
     By default, all options are disabled.
 
-    Options (particularly the DontUseNativeDialogs option) should be set
+    Options (particularly the \l DontUseNativeDialog option) should be set
     before changing dialog properties or showing the dialog.
 
     Setting options while the dialog is visible is not guaranteed to have
