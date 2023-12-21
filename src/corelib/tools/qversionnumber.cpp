9b5a83109f7023df2a1b8445a63070b633b43e72
Marc Mutz <marc.mutz@qt.io>
2023-12-21 07:03:41 +0100

    [docs] Remove references to C++11 feature availability
    QVersionNumber, e.g., was added for Qt 5.6, the last Qt version that
    didn't require C++11. So it made sense that the original documentation
    stated that certain functions were only available in C++11 mode.
    
    But already Qt 5.7 required C++11, so these historical anecdotes are
    no longer pertient to today's Qt users, so remove them from the docs.
    
    Pick-to: 5.15
    Change-Id: I5c732d3b9b33e1fb6947eff4fac546476c8379f2
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 4f3142bfce0c64d023299f142d14e60ec577b698)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5b856ba18f0316e9886a0d1048c9e1bb5e2b9fa1)
    (cherry picked from commit e98f7b448dbe438c547f4d4d3647a3ef08a44bc4)
    (cherry picked from commit d2d88cfa2e018d034789303ed82af56a133d03e8)

--- src/corelib/tools/qversionnumber.cpp
+++ src/corelib/tools/qversionnumber.cpp
@@ -105,8 +105,6 @@ QT_BEGIN_NAMESPACE
     \fn QVersionNumber::QVersionNumber(QList<int> &&seg)
 
     Move-constructs a version number from the list of numbers contained in \a seg.
-
-    This constructor is only enabled if the compiler supports C++11 move semantics.
 */
 
 /*!
@@ -114,9 +112,6 @@ QT_BEGIN_NAMESPACE
 
     Construct a version number from the std::initializer_list specified by
     \a args.
-
-    This constructor is only enabled if the compiler supports C++11 initializer
-    lists.
 */
 
 /*!
