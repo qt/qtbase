47c5f6b4c2a66db7d169fed451f08af6fbe21c39
David Faure <david.faure@kdab.com>
2023-12-31 16:15:20 +0100

    Doc: fix typo in QGraphicsView::rubberBandRect() method documentation
    outise -> outside
    
    Pick-to: 6.7
    Change-Id: I143430b3c6661cf5259a09ffa64b74a220e2b979
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/widgets/graphicsview/qgraphicsview.cpp
+++ src/widgets/graphicsview/qgraphicsview.cpp
@@ -1559,7 +1559,7 @@ void QGraphicsView::setRubberBandSelectionMode(Qt::ItemSelectionMode mode)
    is currently doing an itemselection with rubber band. When the user is not using the
    rubber band this functions returns (a null) QRectF().
 
-   Notice that part of this QRect can be outise the visual viewport. It can e.g
+   Notice that part of this QRect can be outside the visual viewport. It can e.g
    contain negative values.
 
    \sa rubberBandSelectionMode, rubberBandChanged()
