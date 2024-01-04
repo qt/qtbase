c226f2a3a0fd8450bb63dec010797c2ede427bfc
MohammadHossein Qanbari <mohammad.qanbari@qt.io>
2024-01-04 14:03:46 +0100

    Convert Group Box Example to snippets and screenshots
    Remove the Group Box Example from the repository, eliminating the
    associated example codes, screenshot, and documentation file. The
    only relevant portion used in the QGroupBox class has been relocated
    to the existing snippet file. Additionally, all references to this
    example across other files have been removed.
    
    Fixes: QTBUG-119980
    Pick-to: 6.6
    Change-Id: I66f9dd9dab1fe64f2d20424af3acd135201827d2
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit f371f8a64dc76ec9ff07b666577b7ab47b69751d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/doc/snippets/code/src_gui_widgets_qgroupbox.cpp
+++ src/widgets/doc/snippets/code/src_gui_widgets_qgroupbox.cpp
@@ -4,3 +4,20 @@
 //! [0]
 g->setTitle("&User information");
 //! [0]
+
+//! [Set up QGroupBox with layout]
+QGroupBox *groupBox = new QGroupBox(tr("Group Box with Layout"));
+
+QRadioButton *radio1 = new QRadioButton(tr("&Radio button 1"));
+QRadioButton *radio2 = new QRadioButton(tr("R&adio button 2"));
+QRadioButton *radio3 = new QRadioButton(tr("Ra&dio button 3"));
+
+radio1->setChecked(true);
+
+QVBoxLayout *vbox = new QVBoxLayout;
+vbox->addWidget(radio1);
+vbox->addWidget(radio2);
+vbox->addWidget(radio3);
+vbox->addStretch(1);
+groupBox->setLayout(vbox);
+//! [Set up QGroupBox with layout]
