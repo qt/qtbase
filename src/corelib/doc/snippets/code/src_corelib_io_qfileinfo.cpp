e409ae4fa56a391f466876d15128be0295e32bbf
Ahmad Samir <a.samirh78@gmail.com>
2024-01-21 18:35:55 +0200

    QFileInfo: clarify the docs with respect to "file" == file system entry
    The word "file" could be slightly misleading in this context, because it
    refers to an entry on the file system, which could be a regular file, a
    dir, a special character device. Clarify the meaning by using "file
    system entry" instead of just "file".
    
    Fixes: QTBUG-120688
    Pick-to: 6.5
    Change-Id: Ic2a91cc74336a6718c13ad1ffd9f2c4001e5d63f
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 0211dfd92bcaa5a3a4c1ce94e13fcfd9c49e9c6d)
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    (cherry picked from commit cddf6498d3c9064a2d82e19013fb2a6e96db7b4c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/doc/snippets/code/src_corelib_io_qfileinfo.cpp
+++ src/corelib/doc/snippets/code/src_corelib_io_qfileinfo.cpp
@@ -104,3 +104,21 @@ if (fi.permission(QFile::WriteUser | QFile::ReadGroup))
 if (fi.permission(QFile::WriteGroup | QFile::WriteOther))
     qWarning("The group or others can change the file");
 //! [10]
+
+{
+//! [11]
+// Given a current working directory of "/home/user/Documents/memos/"
+QFileInfo info1(u"relativeFile"_s);
+qDebug() << info1.absolutePath(); // "/home/user/Documents/memos/"
+qDebug() << info1.baseName(); // "relativeFile"
+qDebug() << info1.absoluteDir(); // QDir(u"/home/user/Documents/memos"_s)
+qDebug() << info1.absoluteDir().path(); // "/home/user/Documents/memos"
+
+// A QFileInfo on a dir
+QFileInfo info2(u"/home/user/Documents/memos"_s);
+qDebug() << info2.absolutePath(); // "/home/user/Documents"
+qDebug() << info2.baseName(); // "memos"
+qDebug() << info2.absoluteDir(); // QDir(u"/home/user/Documents"_s)
+qDebug() << info2.absoluteDir().path(); // "/home/user/Documents"
+//! [11]
+}
