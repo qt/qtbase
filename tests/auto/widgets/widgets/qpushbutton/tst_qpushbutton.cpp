qt/qtbase

commit 7199498fb9cbfb5afc043ddb0e630c8ffad5240b

QPushButton: use QMenu::popup instead of exec to show menu
QMenu::exec opens a blocking loop, which is problematic for webassembly.
Replace with QMenu::popup, and reset the down-state of the button when
the menu is about to hide. QMenu emits aboutToHide immediately before
existing the event loop in the hideEvent override, so the timing is the
same.

Change-Id: Iccb418d10fcb25f6ad1f73f9cdce6ea6581bd73b
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/widgets/widgets/qpushbutton.cpp
@@ -575,17 +575,14 @@ void QPushButtonPrivate::_q_popupPressed()
 
     QPoint menuPos = adjustedMenuPosition();
 
-    QPointer<QPushButton> guard(q);
-    QMenuPrivate::get(menu)->causedPopup.widget = guard;
+    QMenuPrivate::get(menu)->causedPopup.widget = q;
 
     //Because of a delay in menu effects, we must keep track of the
     //menu visibility to avoid flicker on button release
     menuOpen = true;
-    menu->exec(menuPos);
-    if (guard) {
-        menuOpen = false;
-        q->setDown(false);
-    }
+    QObject::connect(menu, &QMenu::aboutToHide,
+                     q, [q]{ q->setDown(false); }, Qt::SingleShotConnection);
+    menu->popup(menuPos);
 }
 
 QPoint QPushButtonPrivate::adjustedMenuPosition()

tests/auto/widgets/widgets/qmenu/tst_qmenu.cpp
@@ -1143,14 +1143,18 @@ void tst_QMenu::pushButtonPopulateOnAboutToShow()
         QSKIP("Your window manager won't allow a window against the bottom of the screen");
     }
 
-    QTimer::singleShot(300, buttonMenu, SLOT(hide()));
     QTest::mouseClick(&b, Qt::LeftButton, Qt::NoModifier, b.rect().center());
+    QVERIFY(QTest::qWaitForWindowExposed(buttonMenu));
+    QTest::qWait(300);
+    buttonMenu->hide();
     QVERIFY2(!buttonMenu->geometry().intersects(b.geometry()), msgGeometryIntersects(buttonMenu->geometry(), b.geometry()));
 
     // note: we're assuming that, if we previously got the desired geometry, we'll get it here too
     b.move(10, screen.bottom()-buttonMenu->height()-5);
-    QTimer::singleShot(300, buttonMenu, SLOT(hide()));
     QTest::mouseClick(&b, Qt::LeftButton, Qt::NoModifier, b.rect().center());
+    QVERIFY(QTest::qWaitForWindowExposed(buttonMenu));
+    QTest::qWait(300);
+    buttonMenu->hide();
     QVERIFY2(!buttonMenu->geometry().intersects(b.geometry()), msgGeometryIntersects(buttonMenu->geometry(), b.geometry()));
 }
 

commit 343551ffae66048599e5360085d1b77f64b9336e

QPushButton with a menu remains in sunken state after menu closed
Regression fix, introduced in:
7199498fb9cbfb5afc043ddb0e630c8ffad5240b

QPushButton with a menu will have a sunken state, QStyle::State_Sunken,
when the menu has been closed. Expected behavior is that the sunken
style is to be removed when the menu is closed.

A boolean called "menuOpen" is never set to false after being set to
true in the popupPressed() method. When this boolean is true, a sunken
state is painted.

Fixes: QTBUG-120976
Pick-to: 6.6 6.7
Change-Id: I3e721da5dfbb6db200aa2de7366ac5dc73c873e0
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>

src/widgets/widgets/qpushbutton.cpp
@@ -582,7 +582,7 @@ void QPushButtonPrivate::popupPressed()
     //menu visibility to avoid flicker on button release
     menuOpen = true;
     QObject::connect(menu, &QMenu::aboutToHide,
-                     q, [q]{ q->setDown(false); }, Qt::SingleShotConnection);
+                     q, [q, this]{ menuOpen = false; q->setDown(false); }, Qt::SingleShotConnection);
     menu->popup(menuPos);
 }
 

tests/auto/widgets/widgets/qpushbutton/tst_qpushbutton.cpp
@@ -15,6 +15,7 @@
 #include <QGridLayout>
 #include <QStyleFactory>
 #include <QTabWidget>
+#include <QStyleOption>
 
 #include <private/qguiapplication_p.h>
 #include <qpa/qplatformtheme.h>
@@ -54,6 +55,7 @@ private slots:
     void hitButton();
     void iconOnlyStyleSheet();
     void mousePressAndMove();
+    void reactToMenuClosed();
 
 protected slots:
     void resetCounters();
@@ -758,6 +760,52 @@ void tst_QPushButton::mousePressAndMove()
     // should emit pressed signal when the mouse is dragged into of boundary
     QCOMPARE(pressSpy.size(), 2);
     QCOMPARE(releaseSpy.size(), 1);
+}
+
+/*
+    Test checking that a QPushButton with a QMenu has a sunken style only
+    when the menu is open
+    QTBUG-120976
+*/
+void tst_QPushButton::reactToMenuClosed()
+{
+    // create a subclass of QPushButton to expose the initStyleOption method
+    class PushButton : public QPushButton {
+    public:
+        virtual void initStyleOption(QStyleOptionButton *option) const override
+        {
+            QPushButton::initStyleOption(option);
+        }
+    };
+
+    PushButton button;
+    QStyleOptionButton opt;
+    QMenu menu;
+
+    // add a menu to the button
+    menu.addAction(tr("string"));
+    button.setMenu(&menu);
+
+    // give the button a size and show it
+    button.setGeometry(0, 0, 50, 50);
+    button.show();
+    QVERIFY(QTest::qWaitForWindowExposed(&button));
+
+    // click the button to open the menu
+    QTest::mouseClick(&button, Qt::LeftButton);
+
+    // check the menu is visible and the button style is sunken
+    QTRY_VERIFY(menu.isVisible());
+    button.initStyleOption(&opt);
+    QVERIFY(opt.state.testFlag(QStyle::StateFlag::State_Sunken));
+
+    // close the menu
+    menu.close();
+
+    // check the menu isn't visible and the style isn't sunken
+    QTRY_VERIFY(!menu.isVisible());
+    button.initStyleOption(&opt);
+    QVERIFY(!opt.state.testFlag(QStyle::StateFlag::State_Sunken));
 }
 
 QTEST_MAIN(tst_QPushButton)