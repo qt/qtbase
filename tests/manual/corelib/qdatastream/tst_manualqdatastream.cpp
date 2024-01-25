beebf6450c3720029ba5038b46c6db883504a142
Ivan Solovev <ivan.solovev@qt.io>
2024-01-25 11:28:06 +0100

    tst_manualqdatastream.cpp: update the license
    The file was added in fd48ce0b73c74dafd5db27bc1f2752ef665df7ef
    by a TQtC employee, so the header should mention The Qt Company Ltd.
    
    Pick-to: 6.7
    Change-Id: Ib191d2d63656c42f783db87de489814d387dfbca
    Reviewed-by: Øystein Heskestad <oystein.heskestad@qt.io>
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- tests/manual/corelib/qdatastream/tst_manualqdatastream.cpp
+++ tests/manual/corelib/qdatastream/tst_manualqdatastream.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2023 BlackBerry Limited. All rights reserved.
+// Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include <QDataStream>
