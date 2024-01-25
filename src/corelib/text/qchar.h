3fbe22c752b2d9b6ff8ecf760d237eaaca20ba00
Ievgenii Meshcheriakov <ievgenii.meshcheriakov@qt.io>
2024-01-25 12:18:48 +0100

    unicode: Import version 15.1 (UCD version 32)
    Add enumerator for the new Unicode version to QChar::UnicodeVersion.
    
    Remap new line breaking classes to their Unicode 15.0 values:
    * AK, AP and AS to AL,
    * VI and VF to CM.
    These are classes for new line breaking support for Indic scripts
    that require more work.
    
    Blacklist failing tests for now:
    * tst_QUrlUts46::idnaTestV2
    * tst_QTextBoundaryFinder::lineBoundariesDefault
    * tst_QTextBoundaryFinder::graphemeBoundariesDefault
    
    Regenerate the source files.
    
    Task-number: QTBUG-121529
    Change-Id: I869cc9fbaa53765d8ae6265c22cdbef9f19d05bf
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit bfd09ec38c43551db17bd01a0ad5fb161babd840)

--- src/corelib/text/qchar.h
+++ src/corelib/text/qchar.h
@@ -431,6 +431,7 @@ public:
         Unicode_13_0,
         Unicode_14_0,
         Unicode_15_0,
+        Unicode_15_1,
     };
 
     inline Category category() const noexcept { return QChar::category(ucs); }
