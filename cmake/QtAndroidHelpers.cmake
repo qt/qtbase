219a0aabc118916c3be16e647a2a1411805cb15a
Alexey Edelev <alexey.edelev@qt.io>
2024-01-29 13:27:28 +0000

    Revert "Add the support of the qt_import_plugins functionality to androiddeployqt"
    This reverts commit 6763644c3fc151d6e7716af08531a386557d5a88.
    
    Reason for revert: Breaks android automotive and blocks the release.
    
    Pick-to: 6.5
    Change-Id: I450dca047d7f105bd60bab3fb698806ef486e581
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>
    (cherry picked from commit de62bb90ec829bd88fa91c48c4061d61cb1f6a71)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- cmake/QtAndroidHelpers.cmake
+++ cmake/QtAndroidHelpers.cmake
@@ -253,8 +253,7 @@ function(qt_internal_android_dependencies target)
     # Module plugins
     if(module_plugin_types)
         foreach(plugin IN LISTS module_plugin_types)
-            string(APPEND file_contents
-                "<bundled file=\"${INSTALL_PLUGINSDIR}/${plugin}\" type=\"plugin_dir\"/>\n")
+            string(APPEND file_contents "<bundled file=\"${INSTALL_PLUGINSDIR}/${plugin}\" />\n")
         endforeach()
     endif()
 
