fb226262e8b371372fdc66b832b9eab24741f70d
Lucie Gérard <lucie.gerard@qt.io>
2024-02-02 16:19:27 +0100

    Change license for tools files
    According to QUIP-18 [1], all tools file should be
    LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: I89e08859871d29a9cf8c7a56b30b07b2c2f34b42
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

--- src/tools/qtpaths/qtpaths.cpp
+++ src/tools/qtpaths/qtpaths.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 Sune Vuorela <sune@kde.org>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include <QCoreApplication>
 #include <QCommandLineParser>
--- src/tools/tracegen/helpers.cpp
+++ src/tools/tracegen/helpers.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2017 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Rafael Roquetto <rafael.roquetto@kdab.com>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include "helpers.h"
 #include <qdebug.h>
