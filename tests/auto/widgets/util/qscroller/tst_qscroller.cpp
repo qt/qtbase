b3ae097a2c4a80092d217ff6a1d098dc924350d2
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2024-02-06 12:50:34 +0100

    tst_QScroller::scroll() remove QApplicationPrivate::setActiveWindow()
    2f6fe3a26843ff68c5d3f9af0a2fc3cce6caac22 has made calls to
    QApplicationPrivate::setActiveWindow() redundant.
    
    Remove redundant calls.
    
    Task-number: QTBUG-121488
    Change-Id: I3bfbad661ffc157777a1d598b1999b0d8625eec3
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

--- tests/auto/widgets/util/qscroller/tst_qscroller.cpp
+++ tests/auto/widgets/util/qscroller/tst_qscroller.cpp
@@ -355,7 +355,6 @@ void tst_QScroller::scroll()
     QScroller::grabGesture(sw.data(), QScroller::TouchGesture);
     sw->setGeometry(100, 100, 400, 300);
     sw->show();
-    QApplicationPrivate::setActiveWindow(sw.data());
     if (!QTest::qWaitForWindowExposed(sw.data()) || !QTest::qWaitForWindowActive(sw.data()))
         QSKIP("Failed to show and activate window");
 
