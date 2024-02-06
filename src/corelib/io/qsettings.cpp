1575ffbfdbb73a37ef1ce24b356a02a4b8bbd0c3
Tim Blechmann <tim@klingt.org>
2024-02-06 13:40:25 +0800

    corelib: settings - make qsettings.cpp unity buildable
    Both qsettings.cpp and qjsonparser.cpp defined Space in the
    global namespace. Hiding it in one of them in an implementation
    namespace resolves the name clash.
    
    Change-Id: I8dd1244f80d87f3908597ed82c8e41b49b7b916c
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit 4ff65f0e5615b1132ec13c6eeba3647162d8dd0f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit a4c158effe328dede9ddbe35bc59449a6d9cb55a)
    (cherry picked from commit d2e685e0b53fb8cd3d05a67b3512ba9eb6aa82d8)

--- src/corelib/io/qsettings.cpp
+++ src/corelib/io/qsettings.cpp
@@ -1494,6 +1494,8 @@ void QConfFileSettingsPrivate::syncConfFile(QConfFile *confFile)
     }
 }
 
+namespace SettingsImpl {
+
 enum { Space = 0x1, Special = 0x2 };
 
 static const char charTraits[256] =
@@ -1520,10 +1522,15 @@ static const char charTraits[256] =
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
+} // namespace SettingsImpl
+
+using SettingsImpl::charTraits;
+
 bool QConfFileSettingsPrivate::readIniLine(QByteArrayView data, qsizetype &dataPos,
                                            qsizetype &lineStart, qsizetype &lineLen,
                                            qsizetype &equalsPos)
 {
+    using namespace SettingsImpl;
     qsizetype dataLen = data.size();
     bool inQuotes = false;
 
