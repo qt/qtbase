2d6034a9c8d1e08d93c856efb99ca9f6a4d548c8
Thiago Macieira <thiago.macieira@intel.com>
2024-02-07 19:40:39 -0800

    Remove unnecessary #undef about q{Debug,Info,Warning,Critical,Fatal}
    The ones in qlogging.cpp were copied to it from qglobal.cpp in commit
    23688d8942895507f4bc1517dd3c161134f4f9d0. They existed in qglobal.cpp
    like in qdebug.cpp since the beginning of the public history. The one in
    qdebug.cpp already made no sense in Qt 4 times, because qdebug.cpp was a
    documentation-only source (no actual C++ code).
    
    The #undefs from qglobal.cpp hadn't been necessary since commit
    d394ca7f27197cfbfc28eb9a08eb0db261dd9d3d, the global functions qDebug(),
    qWarning(), qCritical(), etc. were replaced by member functions in
    QMessageLogger whose names did not match the macros.
    
    Change-Id: I50e2158aeade4256ad1dfffd17b1c62f23eca4db
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 5670d5f7e1ed65b6d6158b73edc43111d7b8ca53)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/io/qdebug.cpp
+++ src/corelib/io/qdebug.cpp
@@ -2,13 +2,6 @@
 // Copyright (C) 2016 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#ifdef QT_NO_DEBUG
-#undef QT_NO_DEBUG
-#endif
-#ifdef qDebug
-#undef qDebug
-#endif
-
 #include "qdebug.h"
 #include "private/qdebug_p.h"
 #include "qmetaobject.h"
