8a4728c1b93600bce8141315cff619e35d347e9e
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2024-02-09 13:28:20 +0100

    setFocus_complexTwoWidgets() remove setActiveWindow()
    2f6fe3a26843ff68c5d3f9af0a2fc3cce6caac22 has made calls to
    QApplicationPrivate::setActiveWindow() redundant.
    
    Remove redundant calls.
    
    Task-number: QTBUG-121488
    Change-Id: I362ddbc98f864b5bae37d6e98dc5d9a3144848b5
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

--- tests/auto/widgets/graphicsview/qgraphicsproxywidget/tst_qgraphicsproxywidget.cpp
+++ tests/auto/widgets/graphicsview/qgraphicsproxywidget/tst_qgraphicsproxywidget.cpp
@@ -2213,7 +2213,6 @@ void tst_QGraphicsProxyWidget::setFocus_complexTwoWidgets()
     window.setLayout(layout);
 
     window.show();
-    QApplicationPrivate::setActiveWindow(&window);
     window.activateWindow();
     QVERIFY(QTest::qWaitForWindowActive(&window));
     QCOMPARE(QApplication::activeWindow(), &window);
