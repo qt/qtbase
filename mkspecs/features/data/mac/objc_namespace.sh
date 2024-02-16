08b8f9dd2f3e792a6fb43b7e065a7123dff74313
Lucie Gérard <lucie.gerard@qt.io>
2024-02-16 10:01:53 +0100

    Correct license
    According to QUIP-18 [1], all build system files
    should be BSD-3-Clause.
    The files in this patch are part of the build system.
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: I9a79fb04971b117515ed16b3978435ad8ef0e31f
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- mkspecs/features/data/mac/objc_namespace.sh
+++ mkspecs/features/data/mac/objc_namespace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (C) 2017 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+# SPDX-License-Identifier: BSD-3-Clause
 
 script_argument_prefix="-Wobjc_namespace,--"
 
