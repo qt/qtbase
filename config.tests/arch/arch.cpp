cb85390fc2548d2cf0a3247013e6a5b1d14d2f40
Lucie Gérard <lucie.gerard@qt.io>
2024-02-19 12:29:13 +0100

    Correct license in config.test
    According to QUIP-18 [1], all build system files should be
    BSD-3-Clause
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Task-number: QTBUG-121787
    Change-Id: I06880d91e4c6f8d7a92d249f3d216b2bc82fdeed
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit d478ca528511fcaa95491bcedaa259b2f6c5e727)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- config.tests/arch/arch.cpp
+++ config.tests/arch/arch.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2016 Intel Corporation.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: BSD-3-Clause
 
 #define QGLOBAL_H
 #include "../../src/corelib/global/archdetect.cpp"
--- config.tests/x86_simd/main.cpp
+++ config.tests/x86_simd/main.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2017 Intel Corporation.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: BSD-3-Clause
 
 // All of our supported compilers support <immintrin.h>
 #include <immintrin.h>
