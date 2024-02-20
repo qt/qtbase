b5a0db53b5eac7c0be8bc99ba79097608bf3e71f
Volker Hilsheimer <volker.hilsheimer@qt.io>
2024-02-20 11:10:25 +0100

    QIcon: complete the documentation of ThemeIcons
    Add missing \since 6.7, and document the purpose of each value based
    on the description in the freedesktop specification.
    
    Pick-to: 6.7
    Change-Id: Id88438abf152c317600454d7137a19b13aa0a3cc
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/gui/image/qicon.cpp
+++ src/gui/image/qicon.cpp
@@ -1569,162 +1569,180 @@ static constexpr QLatin1StringView themeIconName(QIcon::ThemeIcon icon)
 
 /*!
     \enum QIcon::ThemeIcon
+    \since 6.7
 
     This enum provides access to icons that are provided by most
     icon theme implementations.
 
-    \value AddressBookNew
-    \value ApplicationExit
-    \value AppointmentNew
-    \value CallStart
-    \value CallStop
-    \value ContactNew
-    \value DocumentNew
-    \value DocumentOpen
-    \value DocumentOpenRecent
-    \value DocumentPageSetup
-    \value DocumentPrint
-    \value DocumentPrintPreview
-    \value DocumentProperties
-    \value DocumentRevert
-    \value DocumentSave
-    \value DocumentSaveAs
-    \value DocumentSend
-    \value EditClear
-    \value EditCopy
-    \value EditCut
-    \value EditDelete
-    \value EditFind
-    \value EditPaste
-    \value EditRedo
-    \value EditSelectAll
-    \value EditUndo
-    \value FolderNew
-    \value FormatIndentLess
-    \value FormatIndentMore
-    \value FormatJustifyCenter
-    \value FormatJustifyFill
-    \value FormatJustifyLeft
-    \value FormatJustifyRight
-    \value FormatTextDirectionLtr
-    \value FormatTextDirectionRtl
-    \value FormatTextBold
-    \value FormatTextItalic
-    \value FormatTextUnderline
-    \value FormatTextStrikethrough
-    \value GoDown
-    \value GoHome
-    \value GoNext
-    \value GoPrevious
-    \value GoUp
-    \value HelpAbout
-    \value HelpFaq
-    \value InsertImage
-    \value InsertLink
-    \value InsertText
-    \value ListAdd
-    \value ListRemove
-    \value MailForward
-    \value MailMarkImportant
-    \value MailMarkRead
-    \value MailMarkUnread
-    \value MailMessageNew
-    \value MailReplyAll
-    \value MailReplySender
-    \value MailSend
-    \value MediaEject
-    \value MediaPlaybackPause
-    \value MediaPlaybackStart
-    \value MediaPlaybackStop
-    \value MediaRecord
-    \value MediaSeekBackward
-    \value MediaSeekForward
-    \value MediaSkipBackward
-    \value MediaSkipForward
-    \value ObjectRotateLeft
-    \value ObjectRotateRight
-    \value ProcessStop
-    \value SystemLockScreen
-    \value SystemLogOut
-    \value SystemSearch
-    \value SystemReboot
-    \value SystemShutdown
-    \value ToolsCheckSpelling
-    \value ViewFullscreen
-    \value ViewRefresh
-    \value ViewRestore
-    \value WindowClose
-    \value WindowNew
-    \value ZoomFitBest
-    \value ZoomIn
-    \value ZoomOut
+    \value AddressBookNew       The icon for the action to create a new address book.
+    \value ApplicationExit      The icon for exiting an application.
+    \value AppointmentNew       The icon for the action to create a new appointment.
+    \value CallStart            The icon for initiating or accepting a call.
+    \value CallStop             The icon for stopping a current call.
+    \value ContactNew           The icon for the action to create a new contact.
+    \value DocumentNew          The icon for the action to create a new document.
+    \value DocumentOpen         The icon for the action to open a document.
+    \value DocumentOpenRecent   The icon for the action to open a document that was recently opened.
+    \value DocumentPageSetup    The icon for the \e{page setup} action.
+    \value DocumentPrint        The icon for the \e{print} action.
+    \value DocumentPrintPreview The icon for the \e{print preview} action.
+    \value DocumentProperties   The icon for the action to view the properties of a document.
+    \value DocumentRevert       The icon for the action of reverting to a previous version of a document.
+    \value DocumentSave         The icon for the \e{save} action.
+    \value DocumentSaveAs       The icon for the \e{save as} action.
+    \value DocumentSend         The icon for the \e{send} action.
+    \value EditClear            The icon for the \e{clear} action.
+    \value EditCopy             The icon for the \e{copy} action.
+    \value EditCut              The icon for the \e{cut} action.
+    \value EditDelete           The icon for the \e{delete} action.
+    \value EditFind             The icon for the \e{find} action.
+    \value EditPaste            The icon for the \e{paste} action.
+    \value EditRedo             The icon for the \e{redo} action.
+    \value EditSelectAll        The icon for the \e{select all} action.
+    \value EditUndo             The icon for the \e{undo} action.
+    \value FolderNew            The icon for creating a new folder.
+    \value FormatIndentLess     The icon for the \e{decrease indent formatting} action.
+    \value FormatIndentMore     The icon for the \e{increase indent formatting} action.
+    \value FormatJustifyCenter  The icon for the \e{center justification formatting} action.
+    \value FormatJustifyFill    The icon for the \e{fill justification formatting} action.
+    \value FormatJustifyLeft    The icon for the \e{left justification formatting} action.
+    \value FormatJustifyRight   The icon for the \e{right justification} action.
+    \value FormatTextDirectionLtr   The icon for the \e{left-to-right text formatting} action.
+    \value FormatTextDirectionRtl   The icon for the \e{right-to-left formatting} action.
+    \value FormatTextBold       The icon for the \e{bold text formatting} action.
+    \value FormatTextItalic     The icon for the \e{italic text formatting} action.
+    \value FormatTextUnderline  The icon for the \e{underlined text formatting} action.
+    \value FormatTextStrikethrough  The icon for the \e{strikethrough text formatting} action.
+    \value GoDown               The icon for the \e{go down in a list} action.
+    \value GoHome               The icon for the \e{go to home location} action.
+    \value GoNext               The icon for the \e{go to the next item in a list} action.
+    \value GoPrevious           The icon for the \e{go to the previous item in a list} action.
+    \value GoUp                 The icon for the \e{go up in a list} action.
+    \value HelpAbout            The icon for the \e{About} item in the Help menu.
+    \value HelpFaq              The icon for the \e{FAQ} item in the Help menu.
+    \value InsertImage          The icon for the \e{insert image} action of an application.
+    \value InsertLink           The icon for the \e{insert link} action of an application.
+    \value InsertText           The icon for the \e{insert text} action of an application.
+    \value ListAdd              The icon for the \e{add to list} action.
+    \value ListRemove           The icon for the \e{remove from list} action.
+    \value MailForward          The icon for the \e{forward} action.
+    \value MailMarkImportant    The icon for the \e{mark as important} action.
+    \value MailMarkRead         The icon for the \e{mark as read} action.
+    \value MailMarkUnread       The icon for the \e{mark as unread} action.
+    \value MailMessageNew       The icon for the \e{compose new mail} action.
+    \value MailReplyAll         The icon for the \e{reply to all} action.
+    \value MailReplySender      The icon for the \e{reply to sender} action.
+    \value MailSend             The icon for the \e{send} action.
+    \value MediaEject           The icon for the \e{eject} action of a media player or file manager.
+    \value MediaPlaybackPause   The icon for the \e{pause} action of a media player.
+    \value MediaPlaybackStart   The icon for the \e{start playback} action of a media player.
+    \value MediaPlaybackStop    The icon for the \e{stop} action of a media player.
+    \value MediaRecord          The icon for the \e{record} action of a media application.
+    \value MediaSeekBackward    The icon for the \e{seek backward} action of a media player.
+    \value MediaSeekForward     The icon for the \e{seek forward} action of a media player.
+    \value MediaSkipBackward    The icon for the \e{skip backward} action of a media player.
+    \value MediaSkipForward     The icon for the \e{skip forward} action of a media player.
+    \value ObjectRotateLeft     The icon for the \e{rotate left} action performed on an object.
+    \value ObjectRotateRight    The icon for the \e{rotate right} action performed on an object.
+    \value ProcessStop          The icon for the \e{stop action in applications with} actions that
+                                may take a while to process, such as web page loading in a browser.
+    \value SystemLockScreen     The icon for the \e{lock screen} action.
+    \value SystemLogOut         The icon for the \e{log out} action.
+    \value SystemSearch         The icon for the \e{search} action.
+    \value SystemReboot         The icon for the \e{reboot} action.
+    \value SystemShutdown       The icon for the \e{shutdown} action.
+    \value ToolsCheckSpelling   The icon for the \e{check spelling} action.
+    \value ViewFullscreen       The icon for the \e{fullscreen} action.
+    \value ViewRefresh          The icon for the \e{refresh} action.
+    \value ViewRestore          The icon for leaving the fullscreen view.
+    \value WindowClose          The icon for the \e{close window} action.
+    \value WindowNew            The icon for the \e{new window} action.
+    \value ZoomFitBest          The icon for the \e{best fit} action.
+    \value ZoomIn               The icon for the \e{zoom in} action.
+    \value ZoomOut              The icon for the \e{zoom out} action.
 
-    \value AudioCard
-    \value AudioInputMicrophone
-    \value Battery
-    \value CameraPhoto
-    \value CameraVideo
-    \value CameraWeb
-    \value Computer
-    \value DriveHarddisk
-    \value DriveOptical
-    \value InputGaming
-    \value InputKeyboard
-    \value InputMouse
-    \value InputTablet
-    \value MediaFlash
-    \value MediaOptical
-    \value MediaTape
-    \value MultimediaPlayer
-    \value NetworkWired
-    \value NetworkWireless
-    \value Phone
-    \value Printer
-    \value Scanner
-    \value VideoDisplay
+    \value AudioCard            The icon for the audio rendering device.
+    \value AudioInputMicrophone The icon for the microphone audio input device.
+    \value Battery              The icon for the system battery device.
+    \value CameraPhoto          The icon for a digital still camera devices.
+    \value CameraVideo          The icon for a video camera device.
+    \value CameraWeb            The icon for a web camera device.
+    \value Computer             The icon for the computing device as a whole.
+    \value DriveHarddisk        The icon for hard disk drives.
+    \value DriveOptical         The icon for optical media drives such as CD and DVD.
+    \value InputGaming          The icon for the gaming input device.
+    \value InputKeyboard        The icon for the keyboard input device.
+    \value InputMouse           The icon for the mousing input device.
+    \value InputTablet          The icon for graphics tablet input devices.
+    \value MediaFlash           The icon for flash media, such as a memory stick.
+    \value MediaOptical         The icon for physical optical media such as CD and DVD.
+    \value MediaTape            The icon for generic physical tape media.
+    \value MultimediaPlayer     The icon for generic multimedia playing devices.
+    \value NetworkWired         The icon for wired network connections.
+    \value NetworkWireless      The icon for wireless network connections.
+    \value Phone                The icon for phone devices.
+    \value Printer              The icon for a printer device.
+    \value Scanner              The icon for a scanner device.
+    \value VideoDisplay         The icon for the monitor that video gets displayed on.
 
-    \value AppointmentMissed
-    \value AppointmentSoon
-    \value AudioVolumeHigh
-    \value AudioVolumeLow
-    \value AudioVolumeMedium
-    \value AudioVolumeMuted
-    \value BatteryCaution
-    \value BatteryLow
-    \value DialogError
-    \value DialogInformation
-    \value DialogPassword
-    \value DialogQuestion
-    \value DialogWarning
-    \value FolderDragAccept
-    \value FolderOpen
-    \value FolderVisiting
-    \value ImageLoading
-    \value ImageMissing
-    \value MailAttachment
-    \value MailUnread
-    \value MailRead
-    \value MailReplied
-    \value MediaPlaylistRepeat
-    \value MediaPlaylistShuffle
-    \value NetworkOffline
-    \value PrinterPrinting
-    \value SecurityHigh
-    \value SecurityLow
-    \value SoftwareUpdateAvailable
-    \value SoftwareUpdateUrgent
-    \value SyncError
-    \value SyncSynchronizing
-    \value UserAvailable
-    \value UserOffline
-    \value WeatherClear
-    \value WeatherClearNight
-    \value WeatherFewClouds
-    \value WeatherFewCloudsNight
-    \value WeatherFog
-    \value WeatherShowers
-    \value WeatherSnow
-    \value WeatherStorm
+    \value AppointmentMissed    The icon for when an appointment was missed.
+    \value AppointmentSoon      The icon for when an appointment will occur soon.
+    \value AudioVolumeHigh      The icon used to indicate high audio volume.
+    \value AudioVolumeLow       The icon used to indicate low audio volume.
+    \value AudioVolumeMedium    The icon used to indicate medium audio volume.
+    \value AudioVolumeMuted     The icon used to indicate the muted state for audio playback.
+    \value BatteryCaution       The icon used when the battery is below 40%.
+    \value BatteryLow           The icon used when the battery is below 20%.
+    \value DialogError          The icon used when a dialog is opened to explain an error
+                                condition to the user.
+    \value DialogInformation    The icon used when a dialog is opened to give information to the
+                                user that may be pertinent to the requested action.
+    \value DialogPassword       The icon used when a dialog requesting the authentication
+                                credentials for a user is opened.
+    \value DialogQuestion       The icon used when a dialog is opened to ask a simple question
+                                to the user.
+    \value DialogWarning        The icon used when a dialog is opened to warn the user of
+                                impending issues with the requested action.
+    \value FolderDragAccept     The icon used for a folder while an acceptable object is being
+                                dragged onto it.
+    \value FolderOpen           The icon used for folders, while their contents are being displayed
+                                within the same window.
+    \value FolderVisiting       The icon used for folders, while their contents are being displayed
+                                in another window.
+    \value ImageLoading         The icon used while another image is being loaded.
+    \value ImageMissing         The icon used when another image could not be loaded.
+    \value MailAttachment       The icon for a message that contains attachments.
+    \value MailUnread           The icon for an unread message.
+    \value MailRead             The icon for a read message.
+    \value MailReplied          The icon for a message that has been replied to.
+    \value MediaPlaylistRepeat  The icon for the repeat mode of a media player.
+    \value MediaPlaylistShuffle The icon for the shuffle mode of a media player.
+    \value NetworkOffline       The icon used to indicate that the device is not connected to the
+                                network.
+    \value PrinterPrinting      The icon used while a print job is successfully being spooled to a
+                                printing device.
+    \value SecurityHigh         The icon used to indicate that the security level of an item is
+                                known to be high.
+    \value SecurityLow          The icon used to indicate that the security level of an item is
+                                known to be low.
+    \value SoftwareUpdateAvailable  The icon used to indicate that an update is available.
+    \value SoftwareUpdateUrgent The icon used to indicate that an urgent update is available.
+    \value SyncError            The icon used when an error occurs while attempting to synchronize
+                                data across devices.
+    \value SyncSynchronizing    The icon used while data is successfully synchronizing across
+                                devices.
+    \value UserAvailable        The icon used to indicate that a user is available.
+    \value UserOffline          The icon used to indicate that a user is not available.
+    \value WeatherClear         The icon used to indicate that the sky is clear.
+    \value WeatherClearNight    The icon used to indicate that the sky is clear
+                                during the night.
+    \value WeatherFewClouds     The icon used to indicate that the sky is partly cloudy.
+    \value WeatherFewCloudsNight    The icon used to indicate that the sky is partly cloudy
+                                during the night.
+    \value WeatherFog           The icon used to indicate that the weather is foggy.
+    \value WeatherShowers       The icon used to indicate that rain showers are occurring.
+    \value WeatherSnow          The icon used to indicate that snow is falling.
+    \value WeatherStorm         The icon used to indicate that the weather is stormy.
 
     \omitvalue NThemeIcons
 
