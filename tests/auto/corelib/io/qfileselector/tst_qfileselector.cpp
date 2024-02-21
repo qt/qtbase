1319101ecee04598056b1061c0a0e0b2c873c765
Even Oscar Andersen <even.oscar.andersen@qt.io>
2024-02-21 09:50:53 +0100

    wasm: Enable fileselector test
    Also add test files for emscripten platform to build
    
    Change-Id: Ief03e44a4e3f3339a65453469a7ae0a6969f2252
    Reviewed-by: Lorn Potter <lorn.potter@gmail.com>

--- tests/auto/corelib/io/qfileselector/tst_qfileselector.cpp
+++ tests/auto/corelib/io/qfileselector/tst_qfileselector.cpp
@@ -60,7 +60,7 @@ void tst_QFileSelector::basicTest_data()
     QString expectedPlatform1File(":/platforms");
     QString expectedPlatform2File(""); //Only the last selector
     QString expectedPlatform3File; // Only the first selector (the family)
-#if defined(Q_OS_UNIX) && !defined(Q_OS_ANDROID) && \
+#if defined(Q_OS_UNIX) && !defined(Q_OS_ANDROID) && !defined(Q_OS_WASM) && \
     !defined(Q_OS_DARWIN) && !defined(Q_OS_LINUX) && !defined(Q_OS_HAIKU) && !defined(Q_OS_QNX)
     /* We are only aware of specific unixes, and do not have test files for any of the others.
        However those unixes can get a selector added from the result of a uname call, so this will
