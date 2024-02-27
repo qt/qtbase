35114124d2e1ff1b81b8c300fb0e861190530c6b
Lucie Gérard <lucie.gerard@qt.io>
2024-02-27 11:03:26 +0100

    Correct doc snippet license
    All file under doc/snippet should be
    license as Documentation snippets
    and according to QUIP-18 [1]
    this is LicenseRef-Qt-Commercial OR BSD-3-Clause
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Task-number: QTBUG-121787
    Change-Id: I76eedfb6b15c4091f726a5652e3530001d7cdaf7
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 1556a3a5eb13b05ccff5fb551ae9d72edb267e8d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/dbus/doc/snippets/cmake/examples.cmake
+++ src/dbus/doc/snippets/cmake/examples.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #! [qt_add_dbus_adaptor]
 qt_add_dbus_adaptor(GENERATED_SOURCES org.example.chat.xml chat.h ChatMainWindow)
