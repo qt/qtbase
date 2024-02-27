d289d35183fc900f6c5b34813d9c9b9a03caae27
Lucie Gérard <lucie.gerard@qt.io>
2024-02-27 10:11:54 +0100

    Correct license for tools files
    According to QUIP-18 [1], all tools file should be
    LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Task-number: QTBUG-121787
    Pick-to: 6.7.0
    Change-Id: Icd5d5be2e04819617e68ff142924de1773bebbad
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    (cherry picked from commit 7b6289a0358274ec3c37cabd5b7d7212508a4e8a)
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- mkspecs/features/uikit/devices.py
+++ mkspecs/features/uikit/devices.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (C) 2017 The Qt Company Ltd.
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 from __future__ import print_function
 
