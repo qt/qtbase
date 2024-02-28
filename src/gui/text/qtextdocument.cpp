f0713117d6d0b77c5903ffe9ce3a7d5317021691
Shawn Rutledge <shawn.rutledge@qt.io>
2024-02-28 16:36:02 -0700

    QTextHtmlImporter: don't forget to appendBlock after block tag closed
    If we see a closing tag that really demands a new block after it,
    like </ul>, that needs to be done even if some ignorable whitespace
    and "inline" tags come after it. Don't get distracted by those.
    
    Also add a comment in QTextDocument::setHtml() to remind the reader that
    HTML parsing is a two-pass algorithm.
    
    Pick-to: 6.6
    Fixes: QTBUG-81662
    Change-Id: If723c9d3c211a684725055a06bcf87be4e38923a
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
    (cherry picked from commit 60aeeb0e92762d57c208e4212374d30be6490611)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/text/qtextdocument.cpp
+++ src/gui/text/qtextdocument.cpp
@@ -1296,6 +1296,8 @@ void QTextDocument::setHtml(const QString &html)
     d->enableUndoRedo(false);
     d->beginEditBlock();
     d->clear();
+    // ctor calls parse() to build up QTextHtmlParser::nodes list
+    // then import() populates the QTextDocument from those
     QTextHtmlImporter(this, html, QTextHtmlImporter::ImportToDocument).import();
     d->endEditBlock();
     d->enableUndoRedo(previousState);
