qt/qtbase

commit d4f38a363251fcb5cdfb24d7a1aceb7b630f7e8f

QDialogButtonBox: Fix focus chain and default button assignment
QDialogButtonBox::layoutButtons() rebuilds the focus chain based on the
button layout. It relied on the fact that the last button in the layout
would point back to the first button. That is not the case, if
- a focus frame gets inserted in the chain, and
- the default button is not at the layout's first (=left) position.
In that case, the chain stops at the button left of the default button.
The default button can not be reached by tabbing forward. Back-tabbing
still worked.

By not "closing" the focus chain, the focus proxy was set to the first
button found in the layout. That is wrong, whenever the default button
is not at the first layout position. When the box got focus for the
first time, pressing "Enter" could lead to a non-default button being
triggered. A Yes/No message box would have No as its default button.
On Linux, it would pop up with "No" being highlighted, but "Yes" having
focus. Pressing Enter would trigger Yes, instead of No.

tst_QMessageBox::staticSourceCompat() heuristically defines the
button expected to be triggered by Enter. On Linux/KDE, it would pass,
when the wrong button was fired.

=> Always "close" the focus chain, by linking the last and first
buttons.
=> Make the default button the button box's focus proxy.
=> Change tst_QMessageBox::staticSourceCompat() to always expect No
being fired by Enter in a Yes/No box.

[ChangeLog][QtWidgets][QDialogButtonBox] Default button becomes
focus proxy of a QDialogButtonBox. This ensures that Enter triggers
the default button, instead of the first button in the layout.

Fixes: QTBUG-118489
Pick-to: 6.7 6.6 6.5
Change-Id: Ic626d3d0fe7ba8b924c21734332e98532f11c80e
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/widgets/widgets/qdialogbuttonbox.cpp
@@ -282,16 +282,20 @@ void QDialogButtonBoxPrivate::layoutButtons()
         ++currentLayout;
     }
 
-    QWidget *lastWidget = nullptr;
-    q->setFocusProxy(nullptr);
+    QWidgetList layoutWidgets;
     for (int i = 0; i < buttonLayout->count(); ++i) {
-        QLayoutItem *item = buttonLayout->itemAt(i);
-        if (QWidget *widget = item->widget()) {
-            if (lastWidget)
-                QWidget::setTabOrder(lastWidget, widget);
-            else
-                q->setFocusProxy(widget);
-            lastWidget = widget;
+        if (auto *widget = buttonLayout->itemAt(i)->widget())
+            layoutWidgets << widget;
+    }
+
+    q->setFocusProxy(nullptr);
+    if (!layoutWidgets.isEmpty()) {
+        QWidget *prev = layoutWidgets.constLast();
+        for (QWidget *here : layoutWidgets) {
+            QWidget::setTabOrder(prev, here);
+            prev = here;
+            if (auto *pushButton = qobject_cast<QPushButton *>(prev); pushButton->isDefault())
+                q->setFocusProxy(pushButton);
         }
     }
 

src/widgets/dialogs/qmessagebox.cpp
@@ -1731,7 +1731,7 @@ static QMessageBox::StandardButton showNewMessageBox(QWidget *parent,
     // handles (Yes, No) and (Yes|Default, No)
     if (defaultButton && !(buttons & defaultButton)) {
         const int defaultButtons = defaultButton | QMessageBox::Default;
-        const int otherButtons = static_cast<int>(buttons);
+        const int otherButtons = buttons.toInt();
         const int ret = QMessageBoxPrivate::showOldMessageBox(parent, icon, title,
                                                               text, otherButtons,
                                                               defaultButtons, 0);

tests/auto/widgets/dialogs/qmessagebox/tst_qmessagebox.cpp
@@ -481,14 +481,7 @@ QT_WARNING_DISABLE_DEPRECATED
     ExecCloseHelper closeHelper;
     closeHelper.start(Qt::Key_Enter);
     ret = QMessageBox::information(nullptr, "title", "text", QMessageBox::Yes, QMessageBox::No);
-    int expectedButton = int(QMessageBox::Yes);
-    if (const QPlatformTheme *theme = QGuiApplicationPrivate::platformTheme()) {
-        const int dialogButtonBoxLayout = theme->themeHint(QPlatformTheme::DialogButtonBoxLayout).toInt();
-        if (dialogButtonBoxLayout == QDialogButtonBox::MacLayout
-            || dialogButtonBoxLayout == QDialogButtonBox::GnomeLayout)
-            expectedButton = int(QMessageBox::No);
-    }
-    COMPARE(ret, expectedButton);
+    COMPARE(ret, QMessageBox::No);
     QVERIFY(closeHelper.done());
 
     closeHelper.start(Qt::Key_Enter);

commit d9b63c90bf65d853fa74a1327ef7455e1b655fac

QDialogButtonBox: fix crash when adding a QToolButton
Introduced by commit d4f38a363251fcb5cdfb24d7a1aceb7b630f7e8f

Pick-to: 6.7 6.6 6.5
Change-Id: I99385249a454c75eeae13c1d17670b356427cb70
Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

src/widgets/widgets/qdialogbuttonbox.cpp
@@ -294,7 +294,7 @@ void QDialogButtonBoxPrivate::layoutButtons()
         for (QWidget *here : layoutWidgets) {
             QWidget::setTabOrder(prev, here);
             prev = here;
-            if (auto *pushButton = qobject_cast<QPushButton *>(prev); pushButton->isDefault())
+            if (auto *pushButton = qobject_cast<QPushButton *>(prev); pushButton && pushButton->isDefault())
                 q->setFocusProxy(pushButton);
         }
     }