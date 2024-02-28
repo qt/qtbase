c2333f312ff05d592e44e8b10591e3568b8d9a26
Lucie Gérard <lucie.gerard@qt.io>
2024-02-28 11:38:51 +0100

    Correct license for examples files
    Example takes precedent over build system file type.
    According to QUIP-18 [1], all examples file should be
    LicenseRef-Qt-Commercial OR BSD-3-Clause
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: Id348a89884bb309b96abb31077f14a51086b5d0c
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

--- examples/widgets/painting/shared/use_lib.cmake
+++ examples/widgets/painting/shared/use_lib.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 # Include this file in your example project to use the library defined in this directory.
 # This avoids find_package calls in a directory scope different from the directory scope of the
