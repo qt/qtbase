880d1aef99a6826c8dd690b13e1ca6ea5574f403
Edward Welbourne <edward.welbourne@qt.io>
2024-03-04 16:28:13 +0100

    Revise CLDR-generated data header files to use Unicode-3.0 for SPDX
    This is the correct license for the data they contain. The types that
    are used to package them can be made available under the same.
    
    Pick-to: 6.7 6.5
    Task-number: QTBUG-121653
    Change-Id: I7fb5f332f9e7f4f6db0f1f0c3964a36ce03bccb2
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/corelib/time/qromancalendar_data_p.h
+++ src/corelib/time/qromancalendar_data_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2019 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: Unicode-3.0
 
 #ifndef QROMANCALENDAR_DATA_P_H
 #define QROMANCALENDAR_DATA_P_H
--- src/corelib/time/qtimezoneprivate_data_p.h
+++ src/corelib/time/qtimezoneprivate_data_p.h
@@ -1,7 +1,6 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2013 John Layt <jlayt@kde.org>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-
+// SPDX-License-Identifier: Unicode-3.0
 
 #ifndef QTIMEZONEPRIVATE_DATA_P_H
 #define QTIMEZONEPRIVATE_DATA_P_H
