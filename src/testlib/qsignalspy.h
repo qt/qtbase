730cf4571423af3649bc2621bdc69467edf1adc5
Marc Mutz <marc.mutz@qt.io>
2024-03-15 17:12:19 +0100

    QSignalSpy: fix C'n'P mistake in a qWarning()
    The warning for the new-style signal constructor was copied from the
    old-style signal constructor, but not adjusted to its new home. The
    signal pointer passed here is not the signal "name", but a signal
    "pointer" (-to-member-function, but no need to go into that much
    detail).
    
    Amends 6fc7d76e7309c01a364b0f72d253735366674f29, but not picking to
    very strict LTS branches, just in case someone has a
    QTest::ignoreMsg() installed on it.
    
    Change-Id: Ia1f6b7001f38202ac72f9945c4a822d81562cdbf
    Reviewed-by: David Faure <david.faure@kdab.com>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 7565034aadf955537be908a94e4cef16096a041f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 24c2b8f8ab68c21953e28051bf402171ccb204be)
    (cherry picked from commit 783e33f7a9bfd7f93f1c6a6955506429099f07b9)

--- src/testlib/qsignalspy.h
+++ src/testlib/qsignalspy.h
@@ -62,7 +62,7 @@ public:
             return;
 
         if (!signal0) {
-            qWarning("QSignalSpy: Null signal name is not valid");
+            qWarning("QSignalSpy: Null signal pointer is not valid");
             return;
         }
 
