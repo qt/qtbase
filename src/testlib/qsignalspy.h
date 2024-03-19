49ecea1d31ae9f46cca4e077229c0a752bbdbcdf
Marc Mutz <marc.mutz@qt.io>
2024-03-19 08:00:27 +0100

    QSignalSpy: fix indexed loop (int instead of qsizetype)
    While a signal with more than 2Gi arguments is only a theoretical
    possibility, still use the correct index variable for the indexed loop
    over this QList<int>.
    
    Change-Id: I2ed33238c2cd9d2d1c39cd29c988a2adfd821897
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit a90c3cc3c7ed24c83b9ffa4020cb3652678b36bf)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2ec659b1758c76502b1914baaeeb01beeaade4e4)
    (cherry picked from commit 76ffd8b08c90e953dc9e7a6a6006e6f50396822d)

--- src/testlib/qsignalspy.h
+++ src/testlib/qsignalspy.h
@@ -178,7 +178,7 @@ private:
     {
         QList<QVariant> list;
         list.reserve(args.size());
-        for (int i = 0; i < args.size(); ++i) {
+        for (qsizetype i = 0; i < args.size(); ++i) {
             const QMetaType::Type type = static_cast<QMetaType::Type>(args.at(i));
             if (type == QMetaType::QVariant)
                 list << *reinterpret_cast<QVariant *>(a[i + 1]);
