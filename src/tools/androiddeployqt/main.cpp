045a4f67513743008cd75baaefc7ac8fd2c50249
Marc Mutz <marc.mutz@qt.io>
2024-03-28 10:11:07 +0100

    androiddeplyqt: fix more missing pclose() on early returns
    Found by Coverity.
    
    This code predates the move of androiddeployqt to qtbase.
    
    Pick-to: 6.2 5.15
    Coverity-Id: 378442
    Change-Id: Icc24918159132c55a3817eaf19c96ea212dfa6dc
    Reviewed-by: BogDan Vatra <bogdan@kdab.com>
    (cherry picked from commit db240d99cffbc3af2eb39a5f7d48e68e57b85271)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 76807bdfb754a1551a2ecf9977f4e08f944ff220)

--- src/tools/androiddeployqt/main.cpp
+++ src/tools/androiddeployqt/main.cpp
@@ -2205,6 +2205,7 @@ bool scanImports(Options *options, QSet<QString> *usedDependencies)
     QJsonDocument jsonDocument = QJsonDocument::fromJson(output);
     if (jsonDocument.isNull()) {
         fprintf(stderr, "Invalid json output from qmlimportscanner.\n");
+        pclose(qmlImportScannerCommand);
         return false;
     }
 
@@ -2213,6 +2214,7 @@ bool scanImports(Options *options, QSet<QString> *usedDependencies)
         QJsonValue value = jsonArray.at(i);
         if (!value.isObject()) {
             fprintf(stderr, "Invalid format of qmlimportscanner output.\n");
+            pclose(qmlImportScannerCommand);
             return false;
         }
 
@@ -2258,6 +2260,7 @@ bool scanImports(Options *options, QSet<QString> *usedDependencies)
 
             if (importPathOfThisImport.isEmpty()) {
                 fprintf(stderr, "Import found outside of import paths: %s.\n", qPrintable(info.absoluteFilePath()));
+                pclose(qmlImportScannerCommand);
                 return false;
             }
 
@@ -2325,6 +2328,7 @@ bool scanImports(Options *options, QSet<QString> *usedDependencies)
         }
     }
 
+    pclose(qmlImportScannerCommand);
     return true;
 }
 
