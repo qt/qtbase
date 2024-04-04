d6863a8028ce4f706fe7f426a8d5a5a8f6f182db
Piotr Wierciński <piotr.wiercinski@qt.io>
2024-04-04 10:20:59 +0000

    Revert "wasm: Proxy emscripten_fetch() to the main thread"
    This reverts commit f2f2b6ef18907a76461b54e110618e7840971fa7.
    
    Reason for revert: There are couple of issues introduced. Lets revert to base revision and go with fixes from there.
    
    Change-Id: I8341de42ef3e4a609bfbffdb763dd5c28794473c
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Even Oscar Andersen <even.oscar.andersen@qt.io>
    (cherry picked from commit 157c9eef48916c3e11f43ffb0fe8e8030bed8d06)
    Reviewed-by: Piotr Wierciński <piotr.wiercinski@qt.io>
    (cherry picked from commit bc9d2230d85a694a9a4a2b358e4a299960a51c2a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/kernel/qeventdispatcher_wasm_p.h
+++ src/corelib/kernel/qeventdispatcher_wasm_p.h
@@ -51,7 +51,6 @@ public:
     void wakeUp() override;
 
     static void runOnMainThread(std::function<void(void)> fn);
-    static void runOnMainThreadAsync(std::function<void(void)> fn);
     static void socketSelect(int timeout, int socket, bool waitForRead, bool waitForWrite,
                             bool *selectForRead, bool *selectForWrite, bool *socketDisconnect);
 protected:
@@ -88,6 +87,7 @@ private:
 
     static void run(std::function<void(void)> fn);
     static void runAsync(std::function<void(void)> fn);
+    static void runOnMainThreadAsync(std::function<void(void)> fn);
 
     static QEventDispatcherWasm *g_mainThreadEventDispatcher;
 
