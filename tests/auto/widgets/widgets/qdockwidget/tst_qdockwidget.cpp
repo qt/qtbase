70a9c2689f5417d655ab2c9c2fd01425e0a982e4
Axel Spoerl <axel.spoerl@qt.io>
2024-04-12 17:53:19 +0200

    Fix crash in QMainWindow:.tabifiedDockWidgets()
    QMainWindow::tabifiedDockWidgets() was refactored in Qt 6.6.3, because
    it returned wrong values. It now loops from 0 to
    QMainWindowTabBar::count() and uses QMainWindowTabBar::dockAt(), to
    get the dock widget pointer for an index.
    
    When a dock widget is removed from a floating tab, event processing is
    necessary for the item_list and QMainWindowTabBar::count() to get in
    sync. This case was overlooked in the refactoring. It can lead to
    dockAt() being called with an out-of-bounds index, which it asserted.
    The assertion triggered, when QMainWindow::removeDockWidget() and
    QMainWindow::tabifiedDockWidgets() were called right after each other,
    without processing events inbetween.
    
    QMainWindowTabBar::dockWidgets() doesn't add nullptr to the list, it
    returns.
    
    Therefore, return nullptr, when dockAt() is called out-of-bounds.
    Add test functionality in
    tst_QDockWidget::updateTabBarOnVisibilityChanged().
    
    Fixes: QTBUG-124262
    Change-Id: If66084b9f00b4e39f7a620da68df67c735029cf1
    (cherry picked from commit f97e60a4f0602e1478c888ff1043276966c93387)
    (cherry picked from commit 4e396bcdc442d5d6e5174a2d7a4073d929b8ee30)
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- tests/auto/widgets/widgets/qdockwidget/tst_qdockwidget.cpp
+++ tests/auto/widgets/widgets/qdockwidget/tst_qdockwidget.cpp
@@ -747,6 +747,9 @@ void tst_QDockWidget::updateTabBarOnVisibilityChanged()
     QCOMPARE(tabBar->currentIndex(), 0);
 
     QCOMPARE(mw.tabifiedDockWidgets(dw2), {dw3});
+
+    mw.removeDockWidget(dw3);
+    QCOMPARE(mw.tabifiedDockWidgets(dw2).count(), 0);
 }
 
 Q_DECLARE_METATYPE(Qt::DockWidgetArea)
