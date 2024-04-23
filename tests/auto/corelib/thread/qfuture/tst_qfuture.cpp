ec88e63f2a321ec010a7cbc5e5f48b22533a4c40
Tatiana Borisova <tatiana.borisova@qt.io>
2024-04-23 14:29:36 +0200

    QFuture::const_iterator: use modernize comparisons
    Replace class operators operator==(), operator!=() of
    QFuture::const_iterator: to friend method comparesEqual() and
    Q_DECLARE_EQUALITY_COMPARABLE macro.
    
    Task-number: QTBUG-120304
    Change-Id: Ifa264b83f4d5623db99820847ab9a800cca99be2
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- tests/auto/corelib/thread/qfuture/tst_qfuture.cpp
+++ tests/auto/corelib/thread/qfuture/tst_qfuture.cpp
@@ -17,6 +17,7 @@
 #include <private/qobject_p.h>
 
 #include <QTest>
+#include <QtTest/private/qcomparisontesthelper_p.h>
 #include <qfuture.h>
 #include <qfuturewatcher.h>
 #include <qresultstore.h>
@@ -161,6 +162,7 @@ class tst_QFuture: public QObject
 {
     Q_OBJECT
 private slots:
+    void compareCompiles();
     void resultStore();
     void future();
     void futureToVoid();
@@ -272,6 +274,12 @@ public:
 private:
     QtPrivate::ResultStoreBase &store;
 };
+
+void tst_QFuture::compareCompiles()
+{
+    QTestPrivate::testEqualityOperatorsCompile<QFuture<int>::const_iterator>();
+    QTestPrivate::testEqualityOperatorsCompile<QFuture<QString>::const_iterator>();
+}
 
 void tst_QFuture::resultStore()
 {
@@ -1359,16 +1367,16 @@ void tst_QFuture::iterators()
         QFuture<int>::const_iterator i1 = f.begin(), i2 = i1 + 1;
         QFuture<int>::const_iterator c1 = i1, c2 = c1 + 1;
 
-        QCOMPARE(i1, i1);
-        QCOMPARE(i1, c1);
-        QCOMPARE(c1, i1);
-        QCOMPARE(c1, c1);
-        QCOMPARE(i2, i2);
-        QCOMPARE(i2, c2);
-        QCOMPARE(c2, i2);
-        QCOMPARE(c2, c2);
-        QCOMPARE(1 + i1, i1 + 1);
-        QCOMPARE(1 + c1, c1 + 1);
+        QT_TEST_EQUALITY_OPS(i1, i1, true);
+        QT_TEST_EQUALITY_OPS(i1, c1, true);
+        QT_TEST_EQUALITY_OPS(c1, i1, true);
+        QT_TEST_EQUALITY_OPS(c1, c1, true);
+        QT_TEST_EQUALITY_OPS(i2, i2, true);
+        QT_TEST_EQUALITY_OPS(i2, c2, true);
+        QT_TEST_EQUALITY_OPS(c2, i2, true);
+        QT_TEST_EQUALITY_OPS(c2, c2, true);
+        QT_TEST_EQUALITY_OPS(1 + i1, i1 + 1, true);
+        QT_TEST_EQUALITY_OPS(1 + c1, c1 + 1, true);
 
         QVERIFY(i1 != i2);
         QVERIFY(i1 != c2);
