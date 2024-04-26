b562d2529ac0615ef09b868dda1801b5273c708f
Edward Welbourne <edward.welbourne@qt.io>
2024-04-26 14:47:45 +0200

    Remove old copyright notice from QTZP_data_p.h
    The SPDX header now makes this redundant.
    
    Pick-to: 6.5
    Task-number: QTBUG-121653
    Change-Id: I46624407ab268920b31238b26ca9cdc825dea84a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Lucie Gerard <lucie.gerard@qt.io>
    (cherry picked from commit f43ef0802abc47c60d617fd89be82a9bb45aee04)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/time/qtimezoneprivate_data_p.h
+++ src/corelib/time/qtimezoneprivate_data_p.h
@@ -65,27 +65,6 @@ struct QUtcData
     inline QByteArrayView id() const; // Space-joined list of IANA IDs
 };
 
-/*
-    COPYRIGHT AND PERMISSION NOTICE
-
-    Copyright © 1991-2012 Unicode, Inc. All rights reserved. Distributed under
-    the Terms of Use in http://www.unicode.org/copyright.html.
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of the Unicode data files and any associated documentation (the "Data
-    Files") or Unicode software and any associated documentation (the "Software")
-    to deal in the Data Files or Software without restriction, including without
-    limitation the rights to use, copy, modify, merge, publish, distribute, and/or
-    sell copies of the Data Files or Software, and to permit persons to whom the
-    Data Files or Software are furnished to do so, provided that (a) the above
-    copyright notice(s) and this permission notice appear with all copies of the
-    Data Files or Software, (b) both the above copyright notice(s) and this
-    permission notice appear in associated documentation, and (c) there is clear
-    notice in each modified Data File or in the Software as well as in the
-    documentation associated with the Data File(s) or Software that the data or
-    software has been modified.
-*/
-
 // GENERATED PART STARTS HERE
 
 /*
