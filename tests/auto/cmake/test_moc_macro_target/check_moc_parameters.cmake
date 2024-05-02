0f7a64a3adf018cb1c811753aba1b9d68990feea
Lucie Gérard <lucie.gerard@qt.io>
2024-05-02 09:58:22 +0200

    Add copyright and licensing to build system files missing it
    Task-number: QTBUG-124453
    Change-Id: Ibb6a0ab839a16ceef3c68861bac2f508ddb3d1ae
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- tests/auto/cmake/test_moc_macro_target/check_moc_parameters.cmake
+++ tests/auto/cmake/test_moc_macro_target/check_moc_parameters.cmake
@@ -1,4 +1,6 @@
 
+# Copyright (C) 2024 The Qt Company Ltd.
+# SPDX-License-Identifier: BSD-3-Clause
 function(check_parameters file_path)
     file(READ ${file_path} file_content)
     foreach(compile_option IN ITEMS "-DDEFINE_CMDLINE_SIGNAL" "-DMY_OPTION")
