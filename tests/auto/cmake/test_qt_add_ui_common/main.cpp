3114e0a72e52c8659cc12e5b527a181e174e5b94
Lucie Gérard <lucie.gerard@qt.io>
2024-05-08 10:26:14 +0200

    Correct license for test files
    According to QUIP-18 [1], all test files should be
    LicenseRef-Qt-Commercial OR GPL-3.0-only
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Task-number: QTBUG-121787
    Change-Id: I1da572cca6151d5fc6105f774731d11417025341
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- tests/auto/cmake/test_qt_add_ui_common/main.cpp
+++ tests/auto/cmake/test_qt_add_ui_common/main.cpp
@@ -1,4 +1,4 @@
 // Copyright (C) 2024 The Qt Company Ltd.
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 int main() { return 0; }
--- tests/auto/cmake/test_qt_add_ui_common/uic_test/mainwindow.cpp
+++ tests/auto/cmake/test_qt_add_ui_common/uic_test/mainwindow.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2023 The Qt Company Ltd.
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #include "../UicBuildFolderLeakageCommon/mainwindow.h"
 #include "sub1/sub2/sub3/ui_mainwindow.h"
