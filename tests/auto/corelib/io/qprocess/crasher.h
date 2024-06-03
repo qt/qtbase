ea588a8a2b080417a1602d4832d9a03b94111c5a
Oliver Wolff <oliver.wolff@qt.io>
2024-06-03 10:28:20 +0200

    tst_qprocess: Add crash helper for MSVC ARM64
    The ud2 intrinsic isn't available for ARM64
    
    Fixes: QTBUG-125436
    Pick-to: 6.7
    Change-Id: I3f71252c570cf9795de7907fea68f46ffc81211a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 1fd965cd2be5e73ab7ab8d1e9b44d77f32182744)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/corelib/io/qprocess/crasher.h
+++ tests/auto/corelib/io/qprocess/crasher.h
@@ -45,6 +45,8 @@ void crash()
 {
 #if defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_X64))
     __ud2();
+#elif defined(_MSC_VER) && defined(_M_ARM64)
+    __debugbreak();
 #elif __has_builtin(__builtin_trap)
     __builtin_trap();
 #elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
