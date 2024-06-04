8f9b0891f59dbc59d1295bf8eff361070bd01a9d
Łukasz Matysiak <lukasz.matysiak@qt.io>
2024-06-04 13:03:50 +0200

    Add vxworks to the list of keywords used to blacklist tests
    Right now vxworks is not on the list of keywords used to blacklist
    tests.
    Because of that, it's not possible to blacklist a test specifically on
    that platform.
    Solve the problem by extending the list.
    
    Pick-to: 6.7 6.8
    Task-number: QTBUG-115777
    Change-Id: I04d55eabc27f7554ea9e3fa030ff118a22a624b6
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/testlib/qtestblacklist.cpp
+++ src/testlib/qtestblacklist.cpp
@@ -127,6 +127,9 @@ static QSet<QByteArray> keywords()
 #ifdef Q_OS_QNX
             << "qnx"
 #endif
+#ifdef Q_OS_VXWORKS
+            << "vxworks"
+#endif
 #ifdef Q_OS_WEBOS
             << "webos"
 #endif
