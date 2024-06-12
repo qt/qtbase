756e4d463610c27818a86aa5a063f6dbdd623f57
Volker Hilsheimer <volker.hilsheimer@qt.io>
2024-06-12 16:40:14 +0200

    OpenGL functions: use removed_api to remove old version from API
    Implement them out-of-line in removed_api.cpp to call the GLsizeiptr
    overloads explicitly.
    
    Pick-to: 6.8
    Change-Id: I7a4b937c2c6f444f2dc8e874ab9e07ddd52437e9
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

---
+++ src/opengl/compat/removed_api.cpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#define QT_OPENGL_BUILD_REMOVED_API
+
+#include "qtopenglglobal.h"
+
+QT_USE_NAMESPACE
+
+
+#if QT_OPENGL_REMOVED_SINCE(6, 8)
+
+#include "qopenglfunctions_4_5_compatibility.h" // inline API
+#include "qopenglfunctions_4_5_core.h" // inline API
+
+// #include <qotherheader.h>
+// // implement removed functions from qotherheader.h
+// order sections alphabetically to reduce chances of merge conflicts
+
+#endif // QT_OPENGL_REMOVED_SINCE(6, 8)
+
+#if QT_OPENGL_REMOVED_SINCE(6, 9)
+
+// #include "qotherheader.h"
+// // implement removed functions from qotherheader.h
+// order sections alphabetically to reduce chances of merge conflicts
+
+#endif // QT_OPENGL_REMOVED_SINCE(6, 9)
