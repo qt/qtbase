d671e1af3b736ee7d866323246fc2190fc5e076a
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2024-06-12 18:52:12 +0200

    Determine Qt::AA_DontShowIconsInMenus default value based on platform
    Some platforms, such as macOS, do not natively have icons in menus,
    except in a few specific cases, so the default for AA_DontShowIconsInMenus
    and in turn QAction.iconVisibleInMenu should be to not show the icon.
    
    QActions with iconVisibleInMenu explicitly set to true will still show
    the icon, even if the user does not override AA_DontShowIconsInMenus.
    
    [ChangeLog][QtGui] The default value of Qt::AA_DontShowIconsInMenus is
    now determined based on the platform. On macOS icons will not show by
    default. To override, use QAction.iconVisibleInMenu for individual
    menu actions, or set Qt::AA_DontShowIconsInMenus to false.
    
    Pick-to: 6.8 6.7
    Change-Id: Ie4fb3e6618486233d64a7818be19a42ed9a2a852
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/gui/kernel/qplatformtheme.cpp
+++ src/gui/kernel/qplatformtheme.cpp
@@ -652,6 +652,8 @@ QVariant QPlatformTheme::defaultThemeHint(ThemeHint hint)
         return QVariant(QSize(16, 16));
     case UnderlineShortcut:
         return true;
+    case ShowIconsInMenus:
+        return true;
     }
 
     return QVariant();
