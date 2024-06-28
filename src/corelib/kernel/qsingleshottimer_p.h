e3da82d3b645260a314f60144f34ba446f1db3cf
Marc Mutz <marc.mutz@qt.io>
2024-06-28 10:18:20 +0200

    QSingleShotTimer: de-inline [1/4]: add .cpp file
    ... and split code between header and .cpp file. The goal here is to
    just sort the lines into header and .cpp, not to change them, so
    temporarily define inline away. This allows git to reliably track
    this as copy instead of independent changes. The inline define will be
    cleaned up in a follow-up patch; fixing inline and re-indenting would
    reduce the similarity score too much.
    
    Also includemocs and move qsingleshottimer* to the (lexicographically)
    correct position in CMakeLists.txt's SOURCES.
    
    Fixes -Wweak-vtable (and therefore prevents this class from being used
    outside QtCore (not exported atm)).
    
    Amends e0573e73645c0f57e46332a94160e28eb6c8ebac.
    
    Change-Id: Ia5304434fd1c9a56eb6f2f1988bd14a80ab61442
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 1ef5f50e28edd1bdfcb3828c3858640ba347b2bd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/kernel/qsingleshottimer_p.h
+++ src/corelib/kernel/qsingleshottimer_p.h
@@ -31,6 +31,7 @@ class QSingleShotTimer : public QObject
 
     Qt::TimerId timerId = Qt::TimerId::Invalid;
 
+#define inline
 public:
     // use the same duration type
     using Duration = QAbstractEventDispatcher::Duration;
@@ -63,78 +64,8 @@ Q_SIGNALS:
 
 private:
     inline void timerEvent(QTimerEvent *) override;
+#undef inline
 };
-
-QSingleShotTimer::QSingleShotTimer(Duration interval, Qt::TimerType timerType,
-                                   const QObject *r, const char *member)
-    : QObject(QAbstractEventDispatcher::instance())
-{
-    connect(this, SIGNAL(timeout()), r, member);
-    startTimerForReceiver(interval, timerType, r);
-}
-
-QSingleShotTimer::QSingleShotTimer(Duration interval, Qt::TimerType timerType,
-                                   const QObject *r, QtPrivate::QSlotObjectBase *slotObj)
-    : QObject(QAbstractEventDispatcher::instance())
-{
-    int signal_index = QMetaObjectPrivate::signalOffset(&staticMetaObject);
-    Q_ASSERT(QMetaObjectPrivate::signal(&staticMetaObject, signal_index).name() == "timeout");
-    QObjectPrivate::connectImpl(this, signal_index, r ? r : this, nullptr, slotObj,
-                                Qt::AutoConnection, nullptr, &staticMetaObject);
-
-    startTimerForReceiver(interval, timerType, r);
-}
-
-QSingleShotTimer::~QSingleShotTimer()
-{
-    if (timerId > Qt::TimerId::Invalid)
-        killTimer(timerId);
-}
-
-/*
-    Move the timer, and the dispatching and handling of the timer event, into
-    the same thread as where it will be handled, so that it fires reliably even
-    if the thread that set up the timer is busy.
-*/
-void QSingleShotTimer::startTimerForReceiver(Duration interval, Qt::TimerType timerType,
-                                             const QObject *receiver)
-{
-    if (receiver && receiver->thread() != thread()) {
-        // Avoid leaking the QSingleShotTimer instance in case the application exits before the
-        // timer fires
-        connect(QCoreApplication::instance(), &QCoreApplication::aboutToQuit, this,
-                &QObject::deleteLater);
-        setParent(nullptr);
-        moveToThread(receiver->thread());
-
-        QDeadlineTimer deadline(interval, timerType);
-        auto invokable = [this, deadline, timerType] {
-            if (deadline.hasExpired()) {
-                Q_EMIT timeout();
-            } else {
-                timerId = Qt::TimerId{startTimer(deadline.remainingTimeAsDuration(), timerType)};
-            }
-        };
-        QMetaObject::invokeMethod(this, invokable, Qt::QueuedConnection);
-    } else {
-        timerId = Qt::TimerId{startTimer(interval, timerType)};
-    }
-}
-
-void QSingleShotTimer::timerEvent(QTimerEvent *)
-{
-    // need to kill the timer _before_ we emit timeout() in case the
-    // slot connected to timeout calls processEvents()
-    if (timerId > Qt::TimerId::Invalid)
-        killTimer(std::exchange(timerId, Qt::TimerId::Invalid));
-
-    Q_EMIT timeout();
-
-    // we would like to use delete later here, but it feels like a
-    // waste to post a new event to handle this event, so we just unset the flag
-    // and explicitly delete...
-    delete this;
-}
 
 QT_END_NAMESPACE
 
