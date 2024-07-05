qt/qtbase

commit ae64c54f8cc2491988241a7433f5e8cfc6016ab4

configure: Fix --developer-build argument (with two hyphens)
We never properly handled double-hyphen arguments like --list-features.
However --developer-build worked by accident, because it did set
INPUT_developer_build implicitly.

Now, we don't automagically translate INPUT_foo to FEATURE_foo anymore,
and --developer-build stopped working.

Fix this by consistently handling -foo and --foo arguments.

Change-Id: Ibf32979b419c28e0a8e1f810f03ae3f295b14c69
Reviewed-by:  Alexey Edelev <alexey.edelev@qt.io>

cmake/QtProcessConfigureArgs.cmake
@@ -90,7 +90,11 @@ unset(device_options)
 unset(options_json_file)
 set_property(GLOBAL PROPERTY UNHANDLED_ARGS "")
 while(NOT "${configure_args}" STREQUAL "")
-    list(POP_FRONT configure_args arg)
+    list(POP_FRONT configure_args raw_arg)
+
+    # Condense '--foo-bar' arguments into '-foo-bar'.
+    string(REGEX REPLACE "^--([^-])" "-\\1" arg "${raw_arg}")
+
     if(arg STREQUAL "-cmake-generator")
         list(POP_FRONT configure_args generator)
     elseif(arg STREQUAL "-cmake-use-default-generator")
@@ -156,7 +160,7 @@ while(NOT "${configure_args}" STREQUAL "")
         list(POP_FRONT configure_args version)
         is_valid_qt_hex_version("${arg}" "${version}")
         push("-DQT_DISABLE_DEPRECATED_UP_TO=${version}")
-    elseif(arg STREQUAL "--")
+    elseif(raw_arg STREQUAL "--")
         # Everything after this argument will be passed to CMake verbatim.
         list(APPEND cmake_args "${configure_args}")
         break()

commit 76d5bceba7f1c553b02def689f2010db671ec60f

configure: Fix --foo=bar arguments
Commit ae64c54f8cc2491988241a7433f5e8cfc6016ab4 broke configure
arguments like
    --webengine-jumbo-build=20.

That commit transforms the argument to
    -webengine-jumbo-build=20
but single-hyphen arguments take values without an equal sign.

We need to store the unaltered argument (with the two hyphens) for
further handling instead of the single-hyphen variant.

Pick-to: 6.8
Fixes: QTBUG-126872
Change-Id: I243eb072dfe5535a648bd78bb3aeb3b9e0e4ede0
Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

cmake/QtProcessConfigureArgs.cmake
@@ -167,7 +167,7 @@ while(NOT "${configure_args}" STREQUAL "")
         list(APPEND cmake_args "${configure_args}")
         break()
     else()
-        set_property(GLOBAL APPEND PROPERTY UNHANDLED_ARGS "${arg}")
+        set_property(GLOBAL APPEND PROPERTY UNHANDLED_ARGS "${raw_arg}")
     endif()
 endwhile()
 