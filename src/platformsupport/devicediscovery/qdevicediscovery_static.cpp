37acc1ba2609738f3361113e1e6a813b3735e504
Volker Hilsheimer <volker.hilsheimer@qt.io>
2024-07-10 17:27:32 +0200

    Make logging category static
    Silence warnings when building for Android.
    
    Change-Id: I09e3a217535ccbf28fb84e60ae12b0d7049fe7e3
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

--- src/platformsupport/devicediscovery/qdevicediscovery_static.cpp
+++ src/platformsupport/devicediscovery/qdevicediscovery_static.cpp
@@ -49,7 +49,7 @@ QT_BEGIN_NAMESPACE
 
 using namespace Qt::StringLiterals;
 
-Q_LOGGING_CATEGORY(lcDD, "qt.qpa.input")
+Q_STATIC_LOGGING_CATEGORY(lcDD, "qt.qpa.input")
 
 QDeviceDiscovery *QDeviceDiscovery::create(QDeviceTypes types, QObject *parent)
 {
