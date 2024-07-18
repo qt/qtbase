54bdd3beb752613f38b73d1fbfc20424d04e8161
Marc Mutz <marc.mutz@qt.io>
2024-07-18 14:09:06 +0200

    Fix -Wformat issues in our headers
    Before we add __attribute__((format(printf))) to qsnprintf(), we need
    to make sure our code is clean.
    
    Manual conflict resolutions:
     - tracked changed from qtesttostring.h back to qtest.h,
       whence they came
    
    Pick-to: 6.5 6.2 5.15
    Task-number: QTBUG-127110
    Change-Id: I7411ff0036482ad68bd5867b624518f68f249229
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 63295f43e70955a194602a7acf39013f124e14b7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e4a03a4001daf9103baac513a6b0985e5578afec)

--- src/testlib/qtest.h
+++ src/testlib/qtest.h
@@ -122,7 +122,9 @@ template<> inline char *toString(const QChar &c)
 template<> inline char *toString(const QModelIndex &idx)
 {
     char msg[128];
-    qsnprintf(msg, sizeof(msg), "QModelIndex(%d,%d,%p,%p)", idx.row(), idx.column(), idx.internalPointer(), idx.model());
+    qsnprintf(msg, sizeof(msg), "QModelIndex(%d,%d,%p,%p)",
+              idx.row(), idx.column(), idx.internalPointer(),
+              static_cast<const void*>(idx.model()));
     return qstrdup(msg);
 }
 #endif
@@ -235,7 +237,8 @@ struct QCborValueFormatter
     {
         QScopedArrayPointer<char> hold(format(taggedValue));
         char *buf = new char[BufferLen];
-        qsnprintf(buf, BufferLen, "QCborValue(QCborTag(%llu), %s)", tag, hold.get());
+        qsnprintf(buf, BufferLen, "QCborValue(QCborTag(%llu), %s)",
+                  qToUnderlying(tag), hold.get());
         return buf;
     }
 
@@ -429,8 +432,9 @@ bool _q_compareSequence(ActualIterator actualIt, ActualIterator actualEnd,
 
     if (!isOk) {
         qsnprintf(msg, sizeof(msg), "Compared lists have different sizes.\n"
-                  "   Actual   (%s) size: %zd\n"
-                  "   Expected (%s) size: %zd", actual, actualSize,
+                      "   Actual   (%s) size: %lld\n"
+                      "   Expected (%s) size: %lld",
+                      actual, qlonglong(actualSize),
                   expected, expectedSize);
     }
 
@@ -440,9 +444,10 @@ bool _q_compareSequence(ActualIterator actualIt, ActualIterator actualEnd,
             char *val1 = toString(*actualIt);
             char *val2 = toString(*expectedIt);
 
-            qsnprintf(msg, sizeof(msg), "Compared lists differ at index %zd.\n"
+            qsnprintf(msg, sizeof(msg), "Compared lists differ at index %lld.\n"
                       "   Actual   (%s): %s\n"
-                      "   Expected (%s): %s", i, actual, val1 ? val1 : "<null>",
+                          "   Expected (%s): %s",
+                          qlonglong(i), actual, val1 ? val1 : "<null>",
                       expected, val2 ? val2 : "<null>");
             isOk = false;
 
