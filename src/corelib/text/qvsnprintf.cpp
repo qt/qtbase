4636c02129d0947c5d487911af87a65e4349d28e
Marc Mutz <marc.mutz@qt.io>
2024-07-18 11:25:15 +0200

    q(v)snprintf(): mark the functions as \obsolete
    We can't deprecate them, yet, because there are way too many users in
    Qt and 6.8 is too close by now, but we should give users a heads-up
    that better alternatives exist these days.
    
    Pick-to: 6.2 5.15
    Task-number: QTBUG-127110
    Change-Id: Ic14e9d12fc746fdc65d6c0006756c37339aa25f0
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 3f54c71b78e4ed4ab513190bbcda89ae27256377)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 6c21f8103b671d68d4181005453972fd35c3c872)
    (cherry picked from commit fce31df44a87a1f8382f4fc3e2078c88c21b09af)

--- src/corelib/text/qvsnprintf.cpp
+++ src/corelib/text/qvsnprintf.cpp
@@ -15,6 +15,10 @@ QT_BEGIN_NAMESPACE
 /*!
     \relates QByteArray
 
+    \obsolete
+
+    Use C++11's \c{std::vsnprintf()} from \c{<cstdio>} instead.
+
     A portable \c vsnprintf() function. Will call \c ::vsnprintf(), \c
     ::_vsnprintf(), or \c ::vsnprintf_s depending on the system, or
     fall back to an internal version.
@@ -66,6 +70,10 @@ int qvsnprintf(char *str, size_t n, const char *fmt, va_list ap)
 /*!
     \target bytearray-qsnprintf
     \relates QByteArray
+
+    \obsolete
+
+    Use C++11's \c{std::snprintf()} from \c{<cstdio>} instead.
 
     A portable snprintf() function, calls qvsnprintf.
 
