29df33a1a480b9d6358268564c58b8a773dd5fc7
Mate Barany <mate.barany@qt.io>
2024-07-18 17:45:30 +0200

    Remove default case from QNetworkReplyHttpImplPrivate::convert
    QNetworkRequest::Priority has three possible values and all of them
    are handled already by the switch. The default label also disables
    -Wswitch, so remove it.
    
    Pick-to: 6.7 6.5
    Change-Id: I971a41ef9cbce237f5ff6ae5d28408cbf612eae3
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 645f954d358117ebf1130ac9adc29b0835dad9d4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/network/access/qnetworkreplyhttpimpl.cpp
+++ src/network/access/qnetworkreplyhttpimpl.cpp
@@ -620,9 +620,9 @@ QHttpNetworkRequest::Priority QNetworkReplyHttpImplPrivate::convert(QNetworkRequ
     case QNetworkRequest::HighPriority:
         return QHttpNetworkRequest::HighPriority;
     case QNetworkRequest::NormalPriority:
-    default:
         return QHttpNetworkRequest::NormalPriority;
     }
+    Q_UNREACHABLE_RETURN(QHttpNetworkRequest::NormalPriority);
 }
 
 void QNetworkReplyHttpImplPrivate::postRequest(const QNetworkRequest &newHttpRequest)
