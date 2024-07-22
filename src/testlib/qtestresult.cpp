6ab9d8f7bcf06d9858feb752d49e82edd9a01046
Marc Mutz <marc.mutz@qt.io>
2024-07-22 13:58:49 +0200

    QTest::formatFailMessage(): fix use of std::mbstowcs()
    This function is not re-entrant, and while we can probably rule out
    that our test macros are being executed concurrently, we can't rule
    out that other parts of the test concurrently execute std::mbstowcs().
    
    Use the re-entrant version and also fix the format string (%* requires
    int, not size_t).
    
    Saturation is ok here, because, while std::mbsrtowcs() will happily
    return a value > maxMsgLen when dst == nullptr and the string is
    longer, we will never actually print more than maxMsgLen of it.
    
    As a drive-by, port to std and adjust the QNX comments to describe the
    quirky QNX behavior, if, indeed, still present in the <cwchar>
    version, better. This author had to go back in the git history to
    figure out what it wanted to tell him.
    
    Amends 2d8028d696a86102a7753f9d59fb41f4170181a8 (mbstowcs) and
    d040681b6f3d03b349e9b9487fe89a611d03ee3c (%*).
    
    Manual conflict resolutions:
      - replaced C++17 std::clamp() with Qt qBound()
    
    Change-Id: I6215713c643647727f73f4e2f2a7ac34204af40d
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 1579b18dfa49252318b4a2b3cfa83a4ce325ba0c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8777e13b5a344425a8dd85620f8d2b06d9723c4d)
    (cherry picked from commit 37b050a19510ae42648aafc4244f2ebaddd99b34)
    (cherry picked from commit 1b618f77cdca86e97ff9f437335623bc1e4f6e23)
    (cherry picked from commit 1b2ec1e7e35afdb8d5b1ebd2602aece1350aa7a6)
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/testlib/qtestresult.cpp
+++ src/testlib/qtestresult.cpp
@@ -47,6 +47,10 @@
 #include <QtTest/qtestcase.h>
 #include <QtTest/qtestassert.h>
 
+#include <algorithm>
+#include <climits>
+#include <cwchar>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -267,14 +271,26 @@ bool QTestResult::verify(bool statement, const char *statementStr,
     return checkStatement(statement, msg, file, line);
 }
 
+static int approx_wide_len(const char *s)
+{
+    std::mbstate_t state = {};
+    // QNX might stop at max when dst == nullptr, so pass INT_MAX,
+    // being the largest value this function will return:
+    constexpr size_t max = INT_MAX;
+    auto r = std::mbsrtowcs(nullptr, &s, max, &state);
+    if (r == size_t(-1)) // encoding error, fall back to strlen()
+        r = strlen(s); // `s` was not advanced since `dst == nullptr`
+    return int(qBound(size_t(0), r, max));
+}
+
 // Overload to format failures for "const char *" - no need to strdup().
 void formatFailMessage(char *msg, size_t maxMsgLen,
                        const char *failureMsg,
                        const char *val1S, const char *val2S,
                        const char *actual, const char *expected)
 {
-    size_t len1 = mbstowcs(nullptr, actual, maxMsgLen);    // Last parameter is not ignored on QNX
-    size_t len2 = mbstowcs(nullptr, expected, maxMsgLen);  // (result is never larger than this).
+    const auto len1 = approx_wide_len(actual);
+    const auto len2 = approx_wide_len(expected);
     qsnprintf(msg, maxMsgLen, "%s\n   Actual   (%s)%*s %s\n   Expected (%s)%*s %s",
               failureMsg,
               actual, qMax(len1, len2) - len1 + 1, ":", val1S ? val1S : "<null>",
