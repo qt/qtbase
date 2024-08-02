5fe247df3993d56fe130f3189cb06348fe2aeaea
Orkun Tokdemir <orkun.tokdemir@qt.io>
2024-08-02 13:44:15 +0200

    Doc: Use qt_add_ui instead of qt6_add_ui
    Task-number: QTBUG-126463
    Change-Id: Ib5d207e72c0de436b4daec29efd8baec8a998103
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 4f39704602de001db4c9e82fb0708ee1b2713e38)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/doc/snippets/cmake-macros/examples.cpp
+++ src/widgets/doc/snippets/cmake-macros/examples.cpp
@@ -2,25 +2,25 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 
-//! [qt6_add_ui_1]
+//! [qt_add_ui_1]
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-//! [qt6_add_ui_1]
+//! [qt_add_ui_1]
 
-//! [qt6_add_ui_2]
+//! [qt_add_ui_2]
 #include "mainwindow.h"
 #include "src/files/ui_mainwindow.h"
-//! [qt6_add_ui_2]
+//! [qt_add_ui_2]
 
-//! [qt6_add_ui_3_1]
+//! [qt_add_ui_3_1]
 #include "src/files/ui_widget1.h"
-//! [qt6_add_ui_3_1]
+//! [qt_add_ui_3_1]
 
-//! [qt6_add_ui_3_2]
+//! [qt_add_ui_3_2]
 #include "src/files/ui_widget2.h"
-//! [qt6_add_ui_3_2]
+//! [qt_add_ui_3_2]
 
-//! [qt6_add_ui_5]
+//! [qt_add_ui_5]
 #include "src/files_1/ui_widget1.h"
 #include "src/files_2/ui_widget2.h"
-//! [qt6_add_ui_5]
+//! [qt_add_ui_5]
