e0db6b1dafc57fb860139c6e78bab603a129acd9
Mitch Curtis <mitch.curtis@qt.io>
2024-08-02 13:36:05 +0800

    Add Qt 7 TODO for printing Qt Core QEvent subclasses with operator<<
    We can't move the operator<< in src/gui/kernel/qevent.h to Core in Qt 6,
    so add a TODO as a reminder to do it in Qt 7.
    
    Task-number: QTBUG-127680
    Change-Id: Ib469b7b95a54e885dbceea0363287904535485ff
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/gui/kernel/qevent.h
+++ src/gui/kernel/qevent.h
@@ -910,6 +910,7 @@ private:
 };
 
 #ifndef QT_NO_DEBUG_STREAM
+// ### Qt 7: move this to Qt Core and add a hook to allow GUI events to be printed: QTBUG-127680
 Q_GUI_EXPORT QDebug operator<<(QDebug, const QEvent *);
 #endif
 
