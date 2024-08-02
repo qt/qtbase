4f39704602de001db4c9e82fb0708ee1b2713e38
Orkun Tokdemir <orkun.tokdemir@qt.io>
2024-08-02 13:44:15 +0200

    Doc: Use qt_add_ui instead of qt6_add_ui
    Pick-to: 6.8
    Task-number: QTBUG-126463
    Change-Id: Ib5d207e72c0de436b4daec29efd8baec8a998103
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/widgets/doc/snippets/cmake-macros/examples.cmake
+++ src/widgets/doc/snippets/cmake-macros/examples.cmake
@@ -7,29 +7,29 @@ qt_wrap_ui(SOURCES mainwindow.ui)
 qt_add_executable(myapp ${SOURCES})
 #! [qt_wrap_ui]
 
-#! [qt6_add_ui_1]
+#! [qt_add_ui_1]
 qt_add_executable(myapp mainwindow.cpp main.cpp)
-qt6_add_ui(myapp SOURCES mainwindow.ui)
-#! [qt6_add_ui_1]
+qt_add_ui(myapp SOURCES mainwindow.ui)
+#! [qt_add_ui_1]
 
-#! [qt6_add_ui_2]
+#! [qt_add_ui_2]
 qt_add_executable(myapp mainwindow.cpp main.cpp)
-qt6_add_ui(myapp INCLUDE_PREFIX "src/files" SOURCES mainwindow.ui)
-#! [qt6_add_ui_2]
+qt_add_ui(myapp INCLUDE_PREFIX "src/files" SOURCES mainwindow.ui)
+#! [qt_add_ui_2]
 
-#! [qt6_add_ui_3]
+#! [qt_add_ui_3]
 qt_add_executable(myapp widget1.cpp widget2.cpp main.cpp)
-qt6_add_ui(myapp INCLUDE_PREFIX "src/files" SOURCES widget1.ui widget2.ui)
-#! [qt6_add_ui_3]
+qt_add_ui(myapp INCLUDE_PREFIX "src/files" SOURCES widget1.ui widget2.ui)
+#! [qt_add_ui_3]
 
-#! [qt6_add_ui_4]
+#! [qt_add_ui_4]
 qt_add_executable(myapp widget1.cpp widget2.cpp main.cpp)
-qt6_add_ui(myapp INCLUDE_PREFIX "src/files"
+qt_add_ui(myapp INCLUDE_PREFIX "src/files"
                  SOURCES "my_ui_files_1/widget1.ui" "my_ui_files_2/widget2.ui")
-#! [qt6_add_ui_4]
+#! [qt_add_ui_4]
 
-#! [qt6_add_ui_5]
+#! [qt_add_ui_5]
 qt_add_executable(myapp widget1.cpp widget2.cpp main.cpp)
-qt6_add_ui(myapp INCLUDE_PREFIX "src/files_1" SOURCES "my_ui_files/widget1.ui")
-qt6_add_ui(myapp INCLUDE_PREFIX "src/files_2" SOURCES "my_ui_files/widget2.ui")
-#! [qt6_add_ui_5]
+qt_add_ui(myapp INCLUDE_PREFIX "src/files_1" SOURCES "my_ui_files/widget1.ui")
+qt_add_ui(myapp INCLUDE_PREFIX "src/files_2" SOURCES "my_ui_files/widget2.ui")
+#! [qt_add_ui_5]
