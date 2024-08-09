bc4552963345def94645b74cfd1a7d047e56137b
Lucie Gérard <lucie.gerard@qt.io>
2024-08-09 16:20:58 +0200

    Correct license for module and plugin file
    According to QUIP-18 [1], all module and
    plugin files should be
    LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
    or
    LicenseRef-Qt-Commercial OR GPL-3.0-only
    
    Also, LGPL and non LGPL should not be mixed in a given directory
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.8
    Task-number: QTBUG-121787
    Change-Id: I3c0d98490a9d0bb45b9801d004e23e7e22ebaa34
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/corelib/platform/windows/qbstr_p.h
+++ src/corelib/platform/windows/qbstr_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #ifndef QBSTR_P_H
 #define QBSTR_P_H
