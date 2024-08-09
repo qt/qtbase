58631ae5d46ec4ca9e094a323a1701427c39fc80
Lucie Gérard <lucie.gerard@qt.io>
2024-08-09 14:53:07 +0200

    Correct tests license
    According to QUIP-18 [1], all test files should be
    LicenseRef-Qt-Commercial OR GPL-3.0-only
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Task-number: QTBUG-121787
    Change-Id: Ib37614d55b4d86630d727395c8389a223719ae52
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>

--- tests/auto/corelib/global/q20/utility/tst_q20_utility.cpp
+++ tests/auto/corelib/global/q20/utility/tst_q20_utility.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #include <QTest>
 #include <QtCore/q20utility.h>
