qt/qtbase

commit 4b755bc11a8eadd156c65b7474c11e3ce822c6f1

Teach Qt::compareThreeWay() to support native float16 types
Provide a custom variable template to detect float types and
specialize it for QtPrivate::NativeFloat16Type.
This will allow to enable three-way comparison for qfloat16.

Task-number: QTBUG-104113
Change-Id: Id12c42c86f8dc9e233fe39776b0f0e28088de9e1
Reviewed-by: Marc Mutz <marc.mutz@qt.io>

src/corelib/global/qcomparehelpers.h
@@ -341,6 +341,14 @@ template <typename T>
 constexpr bool IsIntegralType_v = std::numeric_limits<std::remove_const_t<T>>::is_specialized
                                   && std::numeric_limits<std::remove_const_t<T>>::is_integer;
 
+template <typename T>
+constexpr bool IsFloatType_v = std::is_floating_point_v<T>;
+
+#if QFLOAT16_IS_NATIVE
+template <>
+constexpr bool IsFloatType_v<QtPrivate::NativeFloat16Type> = true;
+#endif
+
 } // namespace QtPrivate
 
 namespace Qt {
@@ -353,14 +361,14 @@ using if_integral =
 
 template <typename T, typename U>
 using if_floating_point =
-        std::enable_if_t<std::conjunction_v<std::is_floating_point<std::remove_reference_t<T>>,
-                                            std::is_floating_point<std::remove_reference_t<U>>>,
+        std::enable_if_t<QtPrivate::IsFloatType_v<std::remove_reference_t<T>>
+                           && QtPrivate::IsFloatType_v<std::remove_reference_t<U>>,
                          bool>;
 
 template <typename T, typename U>
 using if_integral_and_floating_point =
         std::enable_if_t<QtPrivate::IsIntegralType_v<std::remove_reference_t<T>>
-                           && std::is_floating_point_v<std::remove_reference_t<U>>,
+                           && QtPrivate::IsFloatType_v<std::remove_reference_t<U>>,
                          bool>;
 
 template <typename T, typename U>

tests/auto/corelib/global/qcomparehelpers/tst_qcomparehelpers.cpp
@@ -663,12 +663,30 @@ void tst_QCompareHelpers::builtinOrder()
     TEST_BUILTIN(qint64, std::float16_t)
     TEST_BUILTIN(uint, std::float16_t)
 #endif
-
     TEST_BUILTIN(long double, long double)
     TEST_BUILTIN(float, long double)
     TEST_BUILTIN(double, long double)
     TEST_BUILTIN(quint64, long double)
     TEST_BUILTIN(ushort, long double)
+
+#if QFLOAT16_IS_NATIVE
+    {
+        // Cannot use TEST_BUILTIN here, because std::numeric_limits are not defined
+        // for QtPrivate::NativeFloat16Type.
+        const QtPrivate::NativeFloat16Type smaller{1.0};
+        const QtPrivate::NativeFloat16Type bigger{2.0};
+        // native vs native
+        QCOMPARE_EQ(Qt::compareThreeWay(smaller, smaller), Qt::partial_ordering::equivalent);
+        QCOMPARE_EQ(Qt::compareThreeWay(smaller, bigger), Qt::partial_ordering::less);
+        QCOMPARE_EQ(Qt::compareThreeWay(bigger, smaller), Qt::partial_ordering::greater);
+        // native vs float
+        QCOMPARE_EQ(Qt::compareThreeWay(smaller, 1.0f), Qt::partial_ordering::equivalent);
+        QCOMPARE_EQ(Qt::compareThreeWay(1.0f, bigger), Qt::partial_ordering::less);
+        QCOMPARE_EQ(Qt::compareThreeWay(bigger, 1.0f), Qt::partial_ordering::greater);
+        const auto floatNaN = std::numeric_limits<float>::quiet_NaN();
+        QCOMPARE_EQ(Qt::compareThreeWay(bigger, floatNaN), Qt::partial_ordering::unordered);
+    }
+#endif
 
     QCOMPARE_EQ(Qt::compareThreeWay(TestEnum::Smaller, TestEnum::Bigger),
                 Qt::strong_ordering::less);

commit a06f3e823bea0361125a10dd361f6fd50a4041cc

Fix ODR violation for IsFloatType_v
With recent GCC 15 trunk, I started to see:
```
ld: .../kwalletentry.cc.o:(.rodata+0x0): multiple definition of `QtPrivate::IsFloatType_v<_Float16>';
	src/runtime/kwalletd/backend/CMakeFiles/KF6WalletBackend.dir/cbc.cc.o:(.rodata+0x0): first defined here
```

The issue is that constexpr is only implicitly inline for functions or
static data members [0], so the constexpr IsFloatType_v specialization
here causes an ODR violation.

Explicitly mark the specialization as inline constexpr.

[0] http://eel.is/c++draft/dcl.constexpr#1.sentence-3

Fixes: 4b755bc11a8eadd156c65b7474c11e3ce822c6f1
Pick-to: 6.8 6.7
Change-Id: Ie9257138f6d1218ca0a91f5d114aab2483cb275b
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/global/qcomparehelpers.h
@@ -774,7 +774,7 @@ constexpr bool IsFloatType_v = std::is_floating_point_v<T>;
 
 #if QFLOAT16_IS_NATIVE
 template <>
-constexpr bool IsFloatType_v<QtPrivate::NativeFloat16Type> = true;
+inline constexpr bool IsFloatType_v<QtPrivate::NativeFloat16Type> = true;
 #endif
 
 } // namespace QtPrivate