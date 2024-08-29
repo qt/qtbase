6d5e39f4756945b8ada74d745bd949856c1b17f9
Marc Mutz <marc.mutz@qt.io>
2024-08-29 15:02:23 +0200

    Make Container::max_size() non-static member functions
    ... as is required by the standard.
    
    We have already found that QAnyStringView::max_size() can also not be
    static (because the size depends on the charset of the contained
    data), so we have a second reason (besides STL compatibility) to
    change the current static into non-static max_size() member functions:
    Qt consistency.
    
    Found in API-Review.
    
    Amends 7ce6920aacfcba485cd8017e01c6aeb324292e75.
    
    Pick-to: 6.8
    Fixes: QTBUG-128450
    Change-Id: Id5ee3fec82bc557bd83b6843838ccdd753442cd0
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- src/corelib/text/qstring.cpp
+++ src/corelib/text/qstring.cpp
@@ -6412,7 +6412,7 @@ QString& QString::fill(QChar ch, qsizetype size)
 */
 
 /*!
-    \fn qsizetype QString::max_size()
+    \fn qsizetype QString::max_size() const
     \fn qsizetype QString::maxSize()
     \since 6.8
 
