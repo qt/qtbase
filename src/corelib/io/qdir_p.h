0314491abac092b20ebefc05e2e9f27fd038fc38
Thiago Macieira <thiago.macieira@intel.com>
2024-09-03 18:21:29 -0500

    QDir: change qt_{cleanPath,normalizePathSegments} to returning bool
    Since qt_normalizePathSegments very often modifies the path, pass that
    as pointer, and return a boolean with whether the path is attempting to
    go up above the root.
    
    Pick-to: 6.8
    Change-Id: I851fcb94db4606a6bd97fffd81910930dea8222a
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: David Faure <david.faure@kdab.com>

--- src/corelib/io/qdir_p.h
+++ src/corelib/io/qdir_p.h
@@ -80,8 +80,7 @@ public:
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(QDirPrivate::PathNormalizations)
 
-Q_AUTOTEST_EXPORT QString qt_normalizePathSegments(const QString &name, QDirPrivate::PathNormalizations flags, bool *ok = nullptr);
-bool qt_normalizePathSegments(QString *path, QDirPrivate::PathNormalizations flags);
+Q_AUTOTEST_EXPORT bool qt_normalizePathSegments(QString *path, QDirPrivate::PathNormalizations flags);
 
 QT_END_NAMESPACE
 
