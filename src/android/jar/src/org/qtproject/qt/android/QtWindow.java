cc0629a4877ff7609094a29e4a18252c0de057b0
Petri Virkkunen <petri.virkkunen@qt.io>
2024-09-26 11:11:01 +0300

    Android: Use main thread to add QtEditText to view hierarchy
    QtWindow is adding QtEditText to its view hierarchy in qtMainThread,
    instead of the main thread. This causes crashes.
    
    Fixes: QTBUG-129201
    Pick-to: 6.8
    Change-Id: I10a638d281198a82edc7a81e23182b0c66e83a0b
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

--- src/android/jar/src/org/qtproject/qt/android/QtWindow.java
+++ src/android/jar/src/org/qtproject/qt/android/QtWindow.java
@@ -46,9 +46,12 @@ class QtWindow extends QtLayout implements QtSurfaceInterface {
         if (!isForeignWindow && context instanceof Activity) {
             // TODO QTBUG-122552 - Service keyboard input not implemented
             m_editText = new QtEditText(context, listener);
-            addView(m_editText, new QtLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                                                      ViewGroup.LayoutParams.WRAP_CONTENT));
             m_editText.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
+            QtNative.runAction(() -> {
+                addView(m_editText,
+                        new QtLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                                                  ViewGroup.LayoutParams.WRAP_CONTENT));
+            });
         } else {
             m_editText = null;
         }
