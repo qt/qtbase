qt/qtbase

commit 08c6d5c3fdfe9ad40053451ab3a9eb0492e273da

Core: Clean up qstorageinfo_linux{_p.h|.cpp}
There is exactly one function the autotest wants to use:
doParseMountInfo(). Instead of making the whole code inline, export this
one function as Q_AUTOTEST_EXPORT. In turn, take the logging category
out of line.

Change-Id: Idf7a410d75f6b2c90bb021b326e3da43a9a57415
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/io/qstorageinfo_linux_p.h
@@ -20,137 +20,11 @@
 
 #include "qstorageinfo_p.h"
 
-#include <QtCore/qsystemdetection.h>
-#include <QtCore/private/qlocale_tools_p.h>
-
 #include <sys/sysmacros.h> // makedev()
 
 QT_BEGIN_NAMESPACE
 
-using namespace Qt::StringLiterals;
 using MountInfo = QStorageInfoPrivate::MountInfo;
-
-static const char MountInfoPath[] = "/proc/self/mountinfo";
-
-static std::optional<dev_t> deviceNumber(QByteArrayView devno)
-{
-    // major:minor
-    auto it = devno.cbegin();
-    auto r = qstrntoll(it, devno.size(), 10);
-    if (!r.ok())
-        return std::nullopt;
-    int rdevmajor = int(r.result);
-    it += r.used;
-
-    if (*it != ':')
-        return std::nullopt;
-
-    r = qstrntoll(++it, devno.size() - r.used + 1, 10);
-    if (!r.ok())
-        return std::nullopt;
-
-    return makedev(rdevmajor, r.result);
-}
-
-// Helper function to parse paths that the kernel inserts escape sequences
-// for.
-static QByteArray parseMangledPath(QByteArrayView path)
-{
-    // The kernel escapes with octal the following characters:
-    //  space ' ', tab '\t', backslash '\\', and newline '\n'
-    // See:
-    // https://codebrowser.dev/linux/linux/fs/proc_namespace.c.html#show_mountinfo
-    // https://codebrowser.dev/linux/linux/fs/seq_file.c.html#mangle_path
-
-    QByteArray ret(path.size(), '\0');
-    char *dst = ret.data();
-    const char *src = path.data();
-    const char *srcEnd = path.data() + path.size();
-    while (src != srcEnd) {
-        switch (*src) {
-        case ' ': // Shouldn't happen
-            return {};
-
-        case '\\': {
-            // It always uses exactly three octal characters.
-            ++src;
-            char c = (*src++ - '0') << 6;
-            c |= (*src++ - '0') << 3;
-            c |= (*src++ - '0');
-            *dst++ = c;
-            break;
-        }
-
-        default:
-            *dst++ = *src++;
-            break;
-        }
-    }
-    // If "path" contains any of the characters this method is demangling,
-    // "ret" would be oversized with extra '\0' characters at the end.
-    ret.resize(dst - ret.data());
-    return ret;
-}
-
-// Indexes into the "fields" std::array in parseMountInfo()
-static constexpr short MountId = 0;
-// static constexpr short ParentId = 1;
-static constexpr short DevNo = 2;
-static constexpr short FsRoot = 3;
-static constexpr short MountPoint = 4;
-static constexpr short MountOptions = 5;
-// static constexpr short OptionalFields = 6;
-// static constexpr short Separator = 7;
-static constexpr short FsType = 8;
-static constexpr short MountSource = 9;
-static constexpr short SuperOptions = 10;
-static constexpr short FieldCount = 11;
-
-// Splits a line from /proc/self/mountinfo into fields; fields are separated
-// by a single space.
-static void tokenizeLine(std::array<QByteArrayView, FieldCount> &fields, QByteArrayView line)
-{
-    size_t fieldIndex = 0;
-    qsizetype from = 0;
-    const char *begin = line.data();
-    const qsizetype len = line.size();
-    qsizetype spaceIndex = -1;
-    while ((spaceIndex = line.indexOf(' ', from)) != -1 && fieldIndex < FieldCount) {
-        fields[fieldIndex] = QByteArrayView{begin + from, begin + spaceIndex};
-        from = spaceIndex;
-
-        // Skip "OptionalFields" and Separator fields
-        if (fieldIndex == MountOptions) {
-            static constexpr char separatorField[] = " - ";
-            const qsizetype sepIndex = line.indexOf(separatorField, from);
-            if (sepIndex == -1) {
-                qCWarning(lcStorageInfo,
-                          "Malformed line (missing '-' separator field) while parsing '%s':\n%s",
-                          MountInfoPath, line.constData());
-                fields.fill({});
-                return;
-            }
-
-            from = sepIndex + strlen(separatorField);
-            // Continue parsing at FsType field
-            fieldIndex = FsType;
-            continue;
-        }
-
-        if (from + 1 < len)
-            ++from; // Skip the space at spaceIndex
-
-        ++fieldIndex;
-    }
-
-    // Currently we don't use the last field, so just check the index
-    if (fieldIndex != SuperOptions) {
-        qCInfo(lcStorageInfo,
-               "Expected %d fields while parsing line from '%s', but found %zu instead:\n%.*s",
-               FieldCount, MountInfoPath, fieldIndex, int(line.size()), line.data());
-        fields.fill({});
-    }
-}
 
 // parseMountInfo() is called from:
 // - QStorageInfoPrivate::initRootPath(), where a list of all mounted volumes is needed
@@ -161,90 +35,8 @@ enum class FilterMountInfo {
     Filtered,
 };
 
-[[maybe_unused]] static std::vector<MountInfo>
-doParseMountInfo(const QByteArray &mountinfo, FilterMountInfo filter = FilterMountInfo::All)
-{
-    // https://www.kernel.org/doc/Documentation/filesystems/proc.txt:
-    // 36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue
-    // (1)(2)(3)   (4)   (5)      (6)      (7)   (8) (9)   (10)         (11)
-
-    auto it = mountinfo.cbegin();
-    const auto end = mountinfo.cend();
-    auto nextLine = [&it, &end]() -> QByteArrayView {
-        auto nIt = std::find(it, end, '\n');
-        if (nIt != end) {
-            QByteArrayView ba(it, nIt);
-            it = ++nIt; // Advance
-            return ba;
-        }
-        return {};
-    };
-
-    std::vector<MountInfo> infos;
-    std::array<QByteArrayView, FieldCount> fields;
-    QByteArrayView line;
-
-    auto checkField = [&line](QByteArrayView field) {
-        if (field.isEmpty()) {
-            qDebug("Failed to parse line from %s:\n%.*s", MountInfoPath, int(line.size()),
-                   line.data());
-            return false;
-        }
-        return true;
-    };
-
-    // mountinfo has a stable format, no empty lines
-    while (!(line = nextLine()).isEmpty()) {
-        fields.fill({});
-        tokenizeLine(fields, line);
-
-        MountInfo info;
-        if (auto r = qstrntoll(fields[MountId].data(), fields[MountId].size(), 10); r.ok()) {
-            info.mntid = r.result;
-        } else {
-            checkField({});
-            continue;
-        }
-
-        QByteArray mountP = parseMangledPath(fields[MountPoint]);
-        if (!checkField(mountP))
-            continue;
-        info.mountPoint = QFile::decodeName(mountP);
-
-        if (!checkField(fields[FsType]))
-            continue;
-        info.fsType = fields[FsType].toByteArray();
-
-        if (filter == FilterMountInfo::Filtered && !shouldIncludeFs(info.mountPoint, info.fsType))
-            continue;
-
-        std::optional<dev_t> devno = deviceNumber(fields[DevNo]);
-        if (!devno) {
-            checkField({});
-            continue;
-        }
-        info.stDev = *devno;
-
-        QByteArrayView fsRootView = fields[FsRoot];
-        if (!checkField(fsRootView))
-            continue;
-
-        // If the filesystem root is "/" -- it's not a *sub*-volume/bind-mount,
-        // in that case we leave info.fsRoot empty
-        if (fsRootView != "/") {
-            info.fsRoot = parseMangledPath(fsRootView);
-            if (!checkField(info.fsRoot))
-                continue;
-        }
-
-        info.device = parseMangledPath(fields[MountSource]);
-        if (!checkField(info.device))
-            continue;
-
-        infos.push_back(std::move(info));
-    }
-    return infos;
-}
+Q_AUTOTEST_EXPORT std::vector<MountInfo> doParseMountInfo(
+        const QByteArray &mountinfo, FilterMountInfo filter = FilterMountInfo::All);
 
 QT_END_NAMESPACE
 

tests/auto/corelib/io/qstorageinfo/tst_qstorageinfo.cpp
@@ -24,6 +24,8 @@
 #  include "../../../../../src/corelib/io/qstorageinfo_linux_p.h"
 #endif
 
+using namespace Qt::StringLiterals;
+
 class tst_QStorageInfo : public QObject
 {
     Q_OBJECT

src/corelib/io/qstorageinfo.cpp
@@ -9,6 +9,8 @@
 
 QT_BEGIN_NAMESPACE
 
+Q_LOGGING_CATEGORY(lcStorageInfo, "qt.core.qstorageinfo", QtWarningMsg)
+
 QT_IMPL_METATYPE_EXTERN(QStorageInfo)
 
 /*!

src/corelib/io/qstorageinfo_linux.cpp
@@ -6,9 +6,12 @@
 
 #include "qstorageinfo_linux_p.h"
 
-#include "qdirlisting.h"
 #include <private/qcore_unix_p.h>
+#include <private/qlocale_tools_p.h>
 #include <private/qtools_p.h>
+
+#include <QtCore/qdirlisting.h>
+#include <QtCore/qsystemdetection.h>
 
 #include <q20memory.h>
 
@@ -39,6 +42,212 @@
 QT_BEGIN_NAMESPACE
 
 using namespace Qt::StringLiterals;
+
+static const char MountInfoPath[] = "/proc/self/mountinfo";
+
+static std::optional<dev_t> deviceNumber(QByteArrayView devno)
+{
+    // major:minor
+    auto it = devno.cbegin();
+    auto r = qstrntoll(it, devno.size(), 10);
+    if (!r.ok())
+        return std::nullopt;
+    int rdevmajor = int(r.result);
+    it += r.used;
+
+    if (*it != ':')
+        return std::nullopt;
+
+    r = qstrntoll(++it, devno.size() - r.used + 1, 10);
+    if (!r.ok())
+        return std::nullopt;
+
+    return makedev(rdevmajor, r.result);
+}
+
+// Helper function to parse paths that the kernel inserts escape sequences
+// for.
+static QByteArray parseMangledPath(QByteArrayView path)
+{
+    // The kernel escapes with octal the following characters:
+    //  space ' ', tab '\t', backslash '\\', and newline '\n'
+    // See:
+    // https://codebrowser.dev/linux/linux/fs/proc_namespace.c.html#show_mountinfo
+    // https://codebrowser.dev/linux/linux/fs/seq_file.c.html#mangle_path
+
+    QByteArray ret(path.size(), '\0');
+    char *dst = ret.data();
+    const char *src = path.data();
+    const char *srcEnd = path.data() + path.size();
+    while (src != srcEnd) {
+        switch (*src) {
+        case ' ': // Shouldn't happen
+            return {};
+
+        case '\\': {
+            // It always uses exactly three octal characters.
+            ++src;
+            char c = (*src++ - '0') << 6;
+            c |= (*src++ - '0') << 3;
+            c |= (*src++ - '0');
+            *dst++ = c;
+            break;
+        }
+
+        default:
+            *dst++ = *src++;
+            break;
+        }
+    }
+    // If "path" contains any of the characters this method is demangling,
+    // "ret" would be oversized with extra '\0' characters at the end.
+    ret.resize(dst - ret.data());
+    return ret;
+}
+
+// Indexes into the "fields" std::array in parseMountInfo()
+static constexpr short MountId = 0;
+// static constexpr short ParentId = 1;
+static constexpr short DevNo = 2;
+static constexpr short FsRoot = 3;
+static constexpr short MountPoint = 4;
+static constexpr short MountOptions = 5;
+// static constexpr short OptionalFields = 6;
+// static constexpr short Separator = 7;
+static constexpr short FsType = 8;
+static constexpr short MountSource = 9;
+static constexpr short SuperOptions = 10;
+static constexpr short FieldCount = 11;
+
+// Splits a line from /proc/self/mountinfo into fields; fields are separated
+// by a single space.
+static void tokenizeLine(std::array<QByteArrayView, FieldCount> &fields, QByteArrayView line)
+{
+    size_t fieldIndex = 0;
+    qsizetype from = 0;
+    const char *begin = line.data();
+    const qsizetype len = line.size();
+    qsizetype spaceIndex = -1;
+    while ((spaceIndex = line.indexOf(' ', from)) != -1 && fieldIndex < FieldCount) {
+        fields[fieldIndex] = QByteArrayView{begin + from, begin + spaceIndex};
+        from = spaceIndex;
+
+        // Skip "OptionalFields" and Separator fields
+        if (fieldIndex == MountOptions) {
+            static constexpr char separatorField[] = " - ";
+            const qsizetype sepIndex = line.indexOf(separatorField, from);
+            if (sepIndex == -1) {
+                qCWarning(lcStorageInfo,
+                          "Malformed line (missing '-' separator field) while parsing '%s':\n%s",
+                          MountInfoPath, line.constData());
+                fields.fill({});
+                return;
+            }
+
+            from = sepIndex + strlen(separatorField);
+            // Continue parsing at FsType field
+            fieldIndex = FsType;
+            continue;
+        }
+
+        if (from + 1 < len)
+            ++from; // Skip the space at spaceIndex
+
+        ++fieldIndex;
+    }
+
+    // Currently we don't use the last field, so just check the index
+    if (fieldIndex != SuperOptions) {
+        qCInfo(lcStorageInfo,
+               "Expected %d fields while parsing line from '%s', but found %zu instead:\n%.*s",
+               FieldCount, MountInfoPath, fieldIndex, int(line.size()), line.data());
+        fields.fill({});
+    }
+}
+
+std::vector<MountInfo> doParseMountInfo(const QByteArray &mountinfo, FilterMountInfo filter)
+{
+    // https://www.kernel.org/doc/Documentation/filesystems/proc.txt:
+    // 36 35 98:0 /mnt1 /mnt2 rw,noatime master:1 - ext3 /dev/root rw,errors=continue
+    // (1)(2)(3)   (4)   (5)      (6)      (7)   (8) (9)   (10)         (11)
+
+    auto it = mountinfo.cbegin();
+    const auto end = mountinfo.cend();
+    auto nextLine = [&it, &end]() -> QByteArrayView {
+        auto nIt = std::find(it, end, '\n');
+        if (nIt != end) {
+            QByteArrayView ba(it, nIt);
+            it = ++nIt; // Advance
+            return ba;
+        }
+        return {};
+    };
+
+    std::vector<MountInfo> infos;
+    std::array<QByteArrayView, FieldCount> fields;
+    QByteArrayView line;
+
+    auto checkField = [&line](QByteArrayView field) {
+        if (field.isEmpty()) {
+            qDebug("Failed to parse line from %s:\n%.*s", MountInfoPath, int(line.size()),
+                   line.data());
+            return false;
+        }
+        return true;
+    };
+
+    // mountinfo has a stable format, no empty lines
+    while (!(line = nextLine()).isEmpty()) {
+        fields.fill({});
+        tokenizeLine(fields, line);
+
+        MountInfo info;
+        if (auto r = qstrntoll(fields[MountId].data(), fields[MountId].size(), 10); r.ok()) {
+            info.mntid = r.result;
+        } else {
+            checkField({});
+            continue;
+        }
+
+        QByteArray mountP = parseMangledPath(fields[MountPoint]);
+        if (!checkField(mountP))
+            continue;
+        info.mountPoint = QFile::decodeName(mountP);
+
+        if (!checkField(fields[FsType]))
+            continue;
+        info.fsType = fields[FsType].toByteArray();
+
+        if (filter == FilterMountInfo::Filtered && !shouldIncludeFs(info.mountPoint, info.fsType))
+            continue;
+
+        std::optional<dev_t> devno = deviceNumber(fields[DevNo]);
+        if (!devno) {
+            checkField({});
+            continue;
+        }
+        info.stDev = *devno;
+
+        QByteArrayView fsRootView = fields[FsRoot];
+        if (!checkField(fsRootView))
+            continue;
+
+        // If the filesystem root is "/" -- it's not a *sub*-volume/bind-mount,
+        // in that case we leave info.fsRoot empty
+        if (fsRootView != "/") {
+            info.fsRoot = parseMangledPath(fsRootView);
+            if (!checkField(info.fsRoot))
+                continue;
+        }
+
+        info.device = parseMangledPath(fields[MountSource]);
+        if (!checkField(info.device))
+            continue;
+
+        infos.push_back(std::move(info));
+    }
+    return infos;
+}
 
 namespace {
 struct AutoFileDescriptor

src/corelib/io/qstorageinfo_p.h
@@ -27,7 +27,7 @@
 
 QT_BEGIN_NAMESPACE
 
-inline Q_LOGGING_CATEGORY(lcStorageInfo, "qt.core.qstorageinfo", QtWarningMsg)
+Q_DECLARE_LOGGING_CATEGORY(lcStorageInfo)
 
 class QStorageInfoPrivate : public QSharedData
 {

commit 80d1787759e6bea318c7e39aaea7380b45d84a41

QStorageInfo/Linux: Add missing static for doParseMountInfo()
Amends 08c6d5c3fdfe9ad40053451ab3a9eb0492e273da, which accidentally made
this an public, non-namespaced symbol out of QtCore.

Pick-to: 6.8
Change-Id: I3217dee5b44730440f22fffd11a6888c4ee76e45
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/corelib/io/qstorageinfo_linux_p.h
@@ -35,7 +35,12 @@ enum class FilterMountInfo {
     Filtered,
 };
 
-Q_AUTOTEST_EXPORT std::vector<MountInfo> doParseMountInfo(
+#ifdef QT_BUILD_INTERNAL
+Q_AUTOTEST_EXPORT
+#else
+static
+#endif
+std::vector<MountInfo> doParseMountInfo(
         const QByteArray &mountinfo, FilterMountInfo filter = FilterMountInfo::All);
 
 QT_END_NAMESPACE

tests/auto/corelib/io/qstorageinfo/tst_qstorageinfo.cpp
@@ -22,7 +22,7 @@
 
 #include "../../../../manual/qstorageinfo/printvolumes.cpp"
 
-#ifdef Q_OS_LINUX
+#if defined(Q_OS_LINUX) && defined(QT_BUILD_INTERNAL)
 #  include "../../../../../src/corelib/io/qstorageinfo_linux_p.h"
 #endif
 