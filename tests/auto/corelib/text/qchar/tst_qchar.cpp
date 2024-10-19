167b04aac89488a5f2005bd1f4dfde354f43c6a6
Marc Mutz <marc.mutz@qt.io>
2024-10-19 10:53:36 +0200

    tst_QChar: run it also under QT_RESTRICTED_CAST_FROM_ASCII
    Like QString, QChar also changes API when said macro is defined.
    
    So, like tst_QString from 8420d3e0b205d0ef2367224afd3040c68e28bcc9
    onwards, also compile tst_QChar under it, separately.
    
    Pick-to: 6.8 6.5
    Change-Id: I81d25d5bfae78b1a11d47e49aa618d079c4b09b7
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- tests/auto/corelib/text/qchar/tst_qchar.cpp
+++ tests/auto/corelib/text/qchar/tst_qchar.cpp
@@ -33,7 +33,11 @@ static_assert(explicitly<char32_t>);
 //
 // Conversion from others
 //
+#if defined(QT_RESTRICTED_CAST_FROM_ASCII)
+static_assert(disabled<uchar>);
+#else
 static_assert(explicitly<uchar>);
+#endif
 static_assert(implicitly<short>);
 static_assert(implicitly<ushort>);
 static_assert(explicitly<int>);
