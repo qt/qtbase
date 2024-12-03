51c8dbabab1927593eefc653f42685340b082c64
Joerg Bornemann <joerg.bornemann@qt.io>
2024-12-03 15:51:53 +0100

    CMake: Check minimum MSVC version when configuring Qt
    MSVC 2019 support was dropped from Qt 6.8. Bail out early if someone
    wants to build Qt with it.
    
    One can opt out of the version check by setting the CMake variable
    QT_NO_MSVC_MIN_VERSION_CHECK to ON.
    
    Change-Id: Ia1f5668e1cddcd0c9f0a8d50482fb50d0c5afe7e
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit 9a409295c7cfe74b4fb6b1892f4ff86d4f3c23f3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- cmake/QtBuildHelpers.cmake
+++ cmake/QtBuildHelpers.cmake
@@ -213,6 +213,7 @@ function(qt_internal_get_qt_build_private_helpers out_var)
         QtToolchainHelpers
         QtUnityBuildHelpers
         QtWasmHelpers
+        QtWindowsHelpers
         QtWrapperScriptHelpers
         PARENT_SCOPE
     )
@@ -444,6 +445,7 @@ macro(qt_internal_setup_build_and_global_variables)
 
     qt_internal_check_macos_host_version()
     _qt_internal_check_apple_sdk_and_xcode_versions()
+    qt_internal_check_msvc_versions()
     qt_internal_check_host_path_set_for_cross_compiling()
     qt_internal_setup_android_platform_specifics()
     qt_internal_setup_find_host_info_package()
