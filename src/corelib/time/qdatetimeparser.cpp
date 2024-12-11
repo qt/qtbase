329c83457667c0fed2980ee9e86001f5fb383816
Edward Welbourne <edward.welbourne@qt.io>
2024-12-11 15:36:44 +0100

    Fix assertion failure on parsing Feb 29 in a non-leap year
    If there's no way to resolve an actual date with the data parsed, then
    the date-text given is invalid, so don't try to fix it up.
    
    On picking to 6.5, switch to QString("...") rather than u"..."_s for
    consistency with other tests (the u"..."_s form is available but not
    previously in use by tst_qdate.cpp) and remove the base year parameter
    (added in 6.7).
    
    Fixes: QTBUG-132115
    Change-Id: Ic6821bd01394d4dba1be1d25806c372800f8176b
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 72519aeb237a4085aeb6290b0b4088c690fad106)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e2a0ef30da08ed46ea90e2c7aa297c8f8faf0dbb)
    (cherry picked from commit 8a63c9b43841ee3e9a69926d2ccf32c9f242a65b)
    Reviewed-by: Magdalena Stojek <magdalena.stojek@qt.io>

--- src/corelib/time/qdatetimeparser.cpp
+++ src/corelib/time/qdatetimeparser.cpp
@@ -1312,7 +1312,9 @@ QDateTimeParser::scanString(const QDateTime &defaultValue, bool fixup) const
         if (year % 100 != year2digits && (isSet & YearSection2Digits)) {
             const QDate date = actualDate(isSet, calendar,
                                           year, year2digits, month, day, dayofweek);
-            if (!(isSet & YearSection)) {
+            if (!date.isValid()) {
+                state = Invalid;
+            } else if (!(isSet & YearSection)) {
                 year = date.year();
             } else {
                 conflicts = true;
