0ce8e1db2bae8dc5da1b4ee3ae55fc5115efa491
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2024-12-12 15:41:57 +0100

    Fixup minimum MSVC version check when configuring Qt
    The message displayed to the user says they need "at least Visual Studio
    2019", but in reality the minimum is now 2022.
    
    Amends 9a409295c7cfe74b4fb6b1892f4ff86d4f3c23f3
    
    Pick-to: 6.8
    Change-Id: Idaedd72d114b994fddd2ba7574fb7a45ca0a3e85
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 652250295c7f9812b49c522ab2fbe518ac9690e4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- cmake/QtWindowsHelpers.cmake
+++ cmake/QtWindowsHelpers.cmake
@@ -8,7 +8,7 @@ function(qt_internal_check_msvc_versions)
     set(min_msvc_version "1930")
     if(MSVC_VERSION VERSION_LESS min_msvc_version)
         message(FATAL_ERROR
-            "Qt requires at least Visual Studio 2019 (MSVC ${min_msvc_version} or newer), "
+            "Qt requires at least Visual Studio 2022 (MSVC ${min_msvc_version} or newer), "
             "you're building against version ${MSVC_VERSION}. "
             "You can turn off this version check by setting QT_NO_MSVC_MIN_VERSION_CHECK to ON."
         )
