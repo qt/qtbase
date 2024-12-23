712192b9cdf519ce7daf08beb2f2b1f0eb2e9d5e
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2024-12-23 18:23:03 +0100

    Windows11Style: adjust size of flat buttons
    Flat buttons were larger than normal buttons - therefore also add
    marginsRemove(QMargins(2,2,2,2) for the flat case too.
    
    Task-number: QTBUG-132433
    Change-Id: Id010d45bf85c5d7d09ff8db8bda44b1be7d85297
    Reviewed-by: Wladimir Leuschner <wladimir.leuschner@qt.io>
    (cherry picked from commit a930fb758d71ef6bf353e8fd8078de2335c32085)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b7e0a7bfdebfcce46084164c971ad6ab9363184f)

--- src/plugins/styles/modernwindows/qwindows11style.cpp
+++ src/plugins/styles/modernwindows/qwindows11style.cpp
@@ -1414,8 +1414,9 @@ void QWindows11Style::drawControl(ControlElement element, const QStyleOption *op
         break;
     case CE_PushButtonBevel:
         if (const QStyleOptionButton *btn = qstyleoption_cast<const QStyleOptionButton *>(option))  {
+            QRectF rect = btn->rect.marginsRemoved(QMargins(2, 2, 2, 2));
+            painter->setPen(Qt::NoPen);
             if (btn->features.testFlag(QStyleOptionButton::Flat)) {
-                painter->setPen(Qt::NoPen);
                 painter->setBrush(btn->palette.button());
                 painter->drawRoundedRect(rect, secondLevelRoundingRadius, secondLevelRoundingRadius);
                 if (flags & (State_Sunken | State_On)) {
@@ -1426,8 +1427,6 @@ void QWindows11Style::drawControl(ControlElement element, const QStyleOption *op
                 }
                 painter->drawRoundedRect(rect, secondLevelRoundingRadius, secondLevelRoundingRadius);
             } else {
-                QRectF rect = btn->rect.marginsRemoved(QMargins(2,2,2,2));
-                painter->setPen(Qt::NoPen);
                 if (flags & (State_Sunken))
                     painter->setBrush(flags & State_On ? option->palette.accent().color().lighter(120) : WINUI3Colors[colorSchemeIndex][controlFillTertiary]);
                 else if (flags & State_MouseOver)
