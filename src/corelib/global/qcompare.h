fa8effc7da2417f6ccaca923bd1b2c376f63ac38
Alexandru Croitor <alexandru.croitor@qt.io>
2025-01-07 18:35:31 +0100

    Silence qcompare.h "zero as null pointer constant" warnings on macOS
    When using qmake to build a user project on macOS, the Qt headers are
    included with the -I option, rather than the -isystem option. This
    means that the compiler will not silence warnings that are generated
    while parsing Qt headers.
    
    If the user project compiles their code with the
    -Wzero-as-null-pointer-constant compiler option, this leads to
    warnings like:
    
    /QtCore.framework/Headers/qcompare.h:226:78:
     warning: zero as null pointer constant
         [-Wzero-as-null-pointer-constant]
      friend constexpr bool is_eq  (partial_ordering o) noexcept
      { return o == 0; }
                    ^
                    nullptr
    
    Fixing qmake to use -isystem instead of -I is not trivial.
    We already silence these warnings for gcc in the header file.
    Do it for clang as well.
    
    Amends bdd41f491c0f85ae0897a1c7372c5ecda62a5aab
    
    Pick-to: 6.8
    Fixes: QTBUG-132581
    Change-Id: I7883248b7be548580a03333e76620ac10f57733a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 0711eb60716b53dc86cacb2fc19b72aa4ffee3d0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/global/qcompare.h
+++ src/corelib/global/qcompare.h
@@ -232,6 +232,7 @@ private:
     QT_WARNING_PUSH
     // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100903
     QT_WARNING_DISABLE_GCC("-Wzero-as-null-pointer-constant")
+    QT_WARNING_DISABLE_CLANG("-Wzero-as-null-pointer-constant")
     friend constexpr bool is_eq  (partial_ordering o) noexcept { return o == 0; }
     friend constexpr bool is_neq (partial_ordering o) noexcept { return o != 0; }
     friend constexpr bool is_lt  (partial_ordering o) noexcept { return o <  0; }
@@ -417,6 +418,7 @@ private:
     QT_WARNING_PUSH
     // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100903
     QT_WARNING_DISABLE_GCC("-Wzero-as-null-pointer-constant")
+    QT_WARNING_DISABLE_CLANG("-Wzero-as-null-pointer-constant")
     friend constexpr bool is_eq  (weak_ordering o) noexcept { return o == 0; }
     friend constexpr bool is_neq (weak_ordering o) noexcept { return o != 0; }
     friend constexpr bool is_lt  (weak_ordering o) noexcept { return o <  0; }
@@ -612,6 +614,7 @@ public:
     QT_WARNING_PUSH
     // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100903
     QT_WARNING_DISABLE_GCC("-Wzero-as-null-pointer-constant")
+    QT_WARNING_DISABLE_CLANG("-Wzero-as-null-pointer-constant")
     friend constexpr bool is_eq  (strong_ordering o) noexcept { return o == 0; }
     friend constexpr bool is_neq (strong_ordering o) noexcept { return o != 0; }
     friend constexpr bool is_lt  (strong_ordering o) noexcept { return o <  0; }
@@ -862,6 +865,7 @@ private:
     QT_WARNING_PUSH
     // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100903
     QT_WARNING_DISABLE_GCC("-Wzero-as-null-pointer-constant")
+    QT_WARNING_DISABLE_CLANG("-Wzero-as-null-pointer-constant")
     friend constexpr bool is_eq  (QPartialOrdering o) noexcept { return o == 0; }
     friend constexpr bool is_neq (QPartialOrdering o) noexcept { return o != 0; }
     friend constexpr bool is_lt  (QPartialOrdering o) noexcept { return o <  0; }
