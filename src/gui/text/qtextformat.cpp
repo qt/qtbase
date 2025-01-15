d5094535ba4f99a4a48e2418879c1de51317ad11
Alexei Cazacov <alexei.cazacov@qt.io>
2025-01-15 17:04:36 +0200

    Docs: Describe QTextFormat::TextUnderlineStyle property
    This commit describes the QTextFormat::TextUnderlineStyle,
    TextVerticalAlignment, and TextOutline properties and their allowed
    values
    
    Task-number: QTBUG-126954
    Change-Id: Ib961331d9a06edb2bedf0a6b93afebef0cd467cb
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit 0ec4447d7a535e1d593f3a5db94f0dcd98fe61ec)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 813ea954b3e383681c16b90476a81b9d17fb1566)

--- src/gui/text/qtextformat.cpp
+++ src/gui/text/qtextformat.cpp
@@ -658,9 +658,11 @@ Q_GUI_EXPORT QDataStream &operator>>(QDataStream &stream, QTextTableCellFormat &
     \omitvalue LastFontProperty
 
     \value TextUnderlineColor      Specifies the color to draw underlines, overlines and strikeouts.
-    \value TextVerticalAlignment
-    \value TextOutline
-    \value TextUnderlineStyle
+    \value TextVerticalAlignment   Specifies the type of text vertical alignment according to
+                                   the values of the QTextCharFormat::VerticalAlignment enum.
+    \value TextOutline             Specifies a \l QPen used to draw the text outline.
+    \value TextUnderlineStyle      Specifies the style of text underline according to
+                                   the values of the QTextCharFormat::UnderlineStyle enum.
     \value TextToolTip Specifies the (optional) tool tip to be displayed for a fragment of text.
     \value TextSuperScriptBaseline Specifies the baseline (in % of height) of superscript texts.
     \value TextSubScriptBaseline   Specifies the baseline (in % of height) of subscript texts.
