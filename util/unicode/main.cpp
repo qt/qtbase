67500054d2e45f676cd7a891ac54d124f9ecced9
Mårten Nordheim <marten.nordheim@qt.io>
2025-01-17 13:13:47 +0100

    Unicode tool: print values using portable types
    ssize_t is not universal; fails to compile in Windows.
    
    Task-number: QTBUG-132902
    Change-Id: I4b8f45cba32202329ac085c7caa0a8c19a11c621
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit f7d0366207709ec0dc33f5d10d21b8f3c9d6534f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- util/unicode/main.cpp
+++ util/unicode/main.cpp
@@ -2883,7 +2883,7 @@ static void computeUniqueProperties()
         }
         d.propertyIndex = index;
     }
-    qDebug("    %zd unique unicode properties found", ssize_t(uniqueProperties.size()));
+    qDebug("    %" PRIdQSIZETYPE " unique unicode properties found", uniqueProperties.size());
 }
 
 struct UniqueBlock {
@@ -3205,7 +3205,7 @@ static QByteArray createSpecialCaseMap()
     out += QByteArray::number(maxN);
     out += ";\n\n";
 
-    qDebug("    memory usage: %zd bytes", ssize_t(specialCaseMap.size() * sizeof(unsigned short)));
+    qDebug("    memory usage: %zu bytes", specialCaseMap.size() * sizeof(unsigned short));
 
     return out;
 }
