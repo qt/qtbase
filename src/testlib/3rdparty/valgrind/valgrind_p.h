9ed5d93c77f335cd58ae448bd7ce8ebb2c50fa74
Edward Welbourne <edward.welbourne@qt.io>
2025-01-17 17:09:10 +0100

    QtTest: Update valgrind (fatuously) to v3.24.0
    Nothing has changed in the headers we use, aside from the version
    number, since v3.23.0, so we may as well declare ourselves up to
    date.
    
    [ChangeLog][Third-Party Code][QtTest] Valgrind headers are up to date
    with Valgrind v3.24.0.
    
    Pick-to: 6.8
    Task-number: QTBUG-132871
    Change-Id: Ib2ad8d73e662535a014d241b46cc50214b45f50b
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    (cherry picked from commit 549bab4150b4652bb8b6577c737e49ae03f6348f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/testlib/3rdparty/valgrind/valgrind_p.h
+++ src/testlib/3rdparty/valgrind/valgrind_p.h
@@ -89,7 +89,7 @@
         || (__VALGRIND_MAJOR__ == 3 && __VALGRIND_MINOR__ >= 6))
 */
 #define __VALGRIND_MAJOR__    3
-#define __VALGRIND_MINOR__    23
+#define __VALGRIND_MINOR__    24
 
 
 #include <stdarg.h>
