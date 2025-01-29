34f9106f3cfc6adefce94c14eeb26ebfd7ec9d37
Liang Qi <liang.qi@qt.io>
2025-01-29 13:27:55 +0100

    Fix dangling references - GCC 15
    This amends 8a6750475410629741de0417c28f574c25c65338 and
    18def77d27f88ce26b6af29fe56a80429fed555d.
    
    Change-Id: I31068527458da3d25211743fb64e2624039bd78b
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 9379b95b533216593d4d37c0cbf70f91d1ce037a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 98065efdfa6b5f31796579101c8f8d9b83459103)

--- src/tools/qtpaths/qtpaths.cpp
+++ src/tools/qtpaths/qtpaths.cpp
@@ -252,7 +252,7 @@ int main(int argc, char **argv)
     }
 
     QT_WARNING_PUSH
-#if defined(Q_CC_GNU_ONLY) && Q_CC_GNU >= 1300 && Q_CC_GNU < 1500
+#if defined(Q_CC_GNU_ONLY) && Q_CC_GNU >= 1300 && Q_CC_GNU < 1600
     QT_WARNING_DISABLE_GCC("-Wdangling-reference")
 #endif
     if (parser.isSet(display)) {
