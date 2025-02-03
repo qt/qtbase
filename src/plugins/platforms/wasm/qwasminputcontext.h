b6faeb8142439a289805e14f1a3bc91282244692
Even Oscar Andersen <even.oscar.andersen@qt.io>
2025-02-03 11:50:03 +0100

    wasm: Truncate input field length to 1px, fix object selection
    Having the input field length too wide causes the view to scroll
    to contain it. Since it is not visible, setting it to 1px width and
    height should work.
    
    Further, input field contents where set on the showInputPanel
    call. At this point in time we do not necessarily have an updated
    focus object. Hence the input contents is now set on the
    setFocusObject call.
    
    Ignore input on showInputPanel/hideInputPanel. This reflects
    the current design where showInputPanel do not act unless
    inputMethodAccepted(), and hideInputPanel do not act if
    m_focusObject.
    
    Still hideInputPanel used to set m_usingTextInput to false,
    this behavior is not kept.
    
    Fixes: QTBUG-132057
    Change-Id: I2d7088def8060df267c5da9e045824decd913faa
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
    (cherry picked from commit 7dd94d74e4497647ff62c85460643487baf372bc)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 43eb0dbd38e74158e7eb36a28cb416b357445d6b)

--- src/plugins/platforms/wasm/qwasminputcontext.h
+++ src/plugins/platforms/wasm/qwasminputcontext.h
@@ -39,15 +39,17 @@ public:
 
     void insertText(QString inputStr, bool replace = false);
 
-    bool usingTextInput() { return m_usingTextInput; }
-    void setUsingTextInput(bool enable) { m_usingTextInput = enable; }
+    bool usingTextInput() const { return m_inputMethodAccepted; }
     void setFocusObject(QObject *object) override;
+
+private:
+    void setInputText();
 
 private:
     QString m_preeditString;
     int m_replaceSize = 0;
 
-    bool m_usingTextInput = false;
+    bool m_inputMethodAccepted = false;
     QObject *m_focusObject = nullptr;
 };
 
