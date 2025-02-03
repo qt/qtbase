ed8023d4805b827bbb32d82f34272b8fe6137ce6
Liang Qi <liang.qi@qt.io>
2025-02-03 14:46:20 +0100

    xcb: call qGuiApp->exit() on xcb connection error
    Fixes: QTBUG-130741
    Change-Id: I67eff8e58d4a4a8d18fb49699111f72ed9100480
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit facf2c17707ac4827e624fb6e9546278aef0c38d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b33813534e48e488e53eef73ab84dae7af0f76f5)
    (cherry picked from commit fbd9b9b43bc9f6e0196d3777c57a520bec1bcd98)

--- src/plugins/platforms/xcb/qxcbconnection.cpp
+++ src/plugins/platforms/xcb/qxcbconnection.cpp
@@ -1068,7 +1068,8 @@ void QXcbConnection::processXcbEvents(QEventLoop::ProcessEventsFlags flags)
     int connection_error = xcb_connection_has_error(xcb_connection());
     if (connection_error) {
         qWarning("The X11 connection broke (error %d). Did the X11 server die?", connection_error);
-        exit(1);
+        qGuiApp->exit(connection_error);
+        return;
     }
 
     m_eventQueue->flushBufferedEvents();
