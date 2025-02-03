8f057b5be46243040712d41b2e8035de2f91a231
Lauri Pohjanheimo <lauri.pohjanheimo@qt.io>
2025-02-03 16:30:13 +0200

    QRhi test renderToTextureArrayOfTexturedQuad unblacklisted
    On CI an Android emulator with SwiftShader software graphics
    implementation is used. SwiftShader implementation is unable
    to emulate this OpenGL functionality.
    
    Blacklisting removed. QSKIP the test if it is run in Android platform,
    OpenGL and SwiftShader sw graphics implementation is found.
    
    Task-number: QTQAINFRA-6330
    Change-Id: Ib0832f35b63540d57a273fc6dba1e310de917439
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry picked from commit 935d1d633c8f32a34592976525a70adfd26aab01)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/gui/rhi/qrhi/tst_qrhi.cpp
+++ tests/auto/gui/rhi/qrhi/tst_qrhi.cpp
@@ -2468,6 +2468,11 @@ void tst_QRhi::renderToTextureArrayOfTexturedQuad()
     if (!rhi)
         QSKIP("QRhi could not be created, skipping testing rendering");
 
+    if (isAndroidOpenGLSwiftShader(impl, rhi.get())) {
+        QSKIP("SwiftShader software acceleration is used which does not support this OpenGLES "
+              "feature. See QTBUG-132934");
+    }
+
     QImage inputImage;
     inputImage.load(QLatin1String(":/data/qt256.png"));
     QVERIFY(!inputImage.isNull());
