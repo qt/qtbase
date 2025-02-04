a35a46a4d7a6685a2dd7542d7e86ab1f1cd8f006
Edward Welbourne <edward.welbourne@qt.io>
2025-02-04 12:09:12 +0100

    Fix UB in QTextStreamPrivate::putNumber()
    Negating a negative value may attempt to negate the minimal value for
    the signed type, whose only set bit is its sign; this is UB. So don't
    do that. Since the non-decimal branch of the code just prepends
    locale.negativeSign(), and we're passing -1 as width parameter to the
    QLocalePrivate formatters (so there's no zero-padding, whose size
    would be reduced to make space for a sign), we can treat decimal the
    same as all other bases. This, furthermore, simplifies the code.
    
    In the process, I noticed (because a test only done for decimal
    failed) that if QTextStream::ForceSign is set, this code-path for
    negatives would prepend the minus sign before a plus sign supplied by
    QLocaleData. Skip the flag to include sign, for negative input, since
    we'll be including a negative sign anyway. Remove the QEXPECT_FAIL()
    from the test I've added for this in a preparatory commit.
    
    Purge one now-redundant comment and revise some others. Expand on why
    we need to hack octal zero to display two zeros. Add a second test row
    to keep the lonely zero-row company in the test for that.
    
    On picking to 5.15, adapt to retain old QLatin{String,Char} usage,
    matching the prior code, rather than user-defined literals.
    
    Fixes: QTBUG-133269
    Change-Id: I35c9bdf34b812cff578de9b0a2570a60e6145c80
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 44ddb34da07f7fdb2214cb39b56cf114d4aa209e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 05b91b119523a8cc4f8134bd997b35ac02b73ddf)
    (cherry picked from commit 05151832074e13c1575808ddea49fb40bf170451)
    (cherry picked from commit 9e7e3be21b366971bfddd7f766760f91fa4b9940)

--- tests/auto/corelib/serialization/qtextstream/tst_qtextstream.cpp
+++ tests/auto/corelib/serialization/qtextstream/tst_qtextstream.cpp
@@ -1150,6 +1150,7 @@ void tst_QTextStream::octTest_data()
     QTest::addColumn<QByteArray>("data");
 
     QTest::newRow("0") << 0 << QByteArray("00");
+    QTest::newRow("40") << 40 << QByteArray("050");
 }
 
 // ------------------------------------------------------------------------------
@@ -2742,7 +2743,6 @@ void tst_QTextStream::manipulators()
     stream << textData;
     stream.flush();
 
-    QEXPECT_FAIL("hex-negative", "Discovered while fixing QTBUG-133269", Continue);
     QCOMPARE(buffer.data(), result);
 }
 
