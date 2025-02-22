a2c00903506ac1e53c9f4e5fc3e6098519d8bfd4
Milian Wolff <milian.wolff@kdab.com>
2025-02-22 15:30:56 +0100

    Add a benchmark for QTimeZone::utc
    On my machine this gives me:
    
    ```
    ********* Start testing of tst_QTimeZone *********
    Config: Using QtTest library 6.10.0, Qt 6.10.0 (x86_64-little_endian-lp64 shared (dynamic) release build; by GCC 14.2.1 20250207), arch unknown
    PASS   : tst_QTimeZone::initTestCase()
    PASS   : tst_QTimeZone::utc()
    RESULT : tst_QTimeZone::utc():
         358.686871 nsecs per iteration (total: 358,686,513, iterations: 999999)
         896.524312 CPU cycles per iteration, 2,5 GHz (total: 896,523,416, iterations: 999999)
         2,227.000427 instructions per iteration, 2,484 instr/cycle (total: 2,226,998,200, iterations: 999999)
         560.000375 branch instructions per iteration, 1,56 G/sec (total: 559,999,815, iterations: 999999)
    PASS   : tst_QTimeZone::cleanupTestCase()
    Totals: 3 passed, 0 failed, 0 skipped, 0 blacklisted, 374ms
    ********* Finished testing of tst_QTimeZone *********
    ```
    
    Profiling shows some quite unexpected code paths that
    I will try to optimize in follow-up patches. Note that
    this function can be called frequently when deserializing
    QDateTime over a QDataStream e.g. - I have stumbled over
    it while profiling some KDE PIM code in akonadi.
    
    Pick-to: 5.15
    Change-Id: I7439df53ae8512c766f63cb4b0d4f33d14aa3a01
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 29c7beac2e0065cc0d03b9db6a701f7c690c6f56)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit c4618d6020b1ab568f40338fb715142d8fe01445)
    (cherry picked from commit fd8b35a121c939e99d64adfb2a08870915802858)
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- tests/benchmarks/corelib/time/qtimezone/tst_bench_qtimezone.cpp
+++ tests/benchmarks/corelib/time/qtimezone/tst_bench_qtimezone.cpp
@@ -17,6 +17,7 @@ class tst_QTimeZone : public QObject
 private Q_SLOTS:
     void isTimeZoneIdAvailable();
     void systemTimeZone();
+    void utc();
     void zoneByName_data();
     void zoneByName();
     void displayName_data();
@@ -68,6 +69,13 @@ void tst_QTimeZone::systemTimeZone()
 {
     QBENCHMARK {
         QTimeZone::systemTimeZone();
+    }
+}
+
+void tst_QTimeZone::utc()
+{
+    QBENCHMARK {
+        QTimeZone::utc();
     }
 }
 
