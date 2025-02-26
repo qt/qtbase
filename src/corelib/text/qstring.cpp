988e6e4ea360cf367ebe762cb13b30f844746735
Marc Mutz <marc.mutz@qt.io>
2025-02-26 09:57:48 +0000

    Revert "QString::replace: fix a performance regression"
    This reverts commit 315210de916d060c044c01e53ff249d676122b1b.
    
    The change was not correct. If the newly-inserted reserve() call
    actually grows the string, and `after` aliased a part of the old
    string, it will now reference deleted data, as the q_points_into_range
    check now comes too late.
    
    Change-Id: I2e016b8b90f74126dfc7126800b7b7fde96a091e
    Pick-to: 6.7
    Task-number: QTBUG-127549
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
    (cherry picked from commit d1539331dd813c83916a323f6cdc7fd6aacb0043)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 887c7a4b89e8d0ce7ff4fa5df1b5fa422e9a7d7e)

--- src/corelib/text/qstring.cpp
+++ src/corelib/text/qstring.cpp
@@ -3763,12 +3763,10 @@ static void replace_helper(QString &str, QSpan<size_t> indices, qsizetype blen, 
     const qsizetype oldSize = str.data_ptr().size;
     const qsizetype adjust = indices.size() * (after.size() - blen);
     const qsizetype newSize = oldSize + adjust;
-    if (str.data_ptr().needsDetach()) {
+    if (str.data_ptr().needsDetach() || needsReallocate(str, newSize)) {
         replace_with_copy(str, indices, blen, after);
         return;
     }
-
-    str.reserve(newSize);
 
     if (QtPrivate::q_points_into_range(after.begin(), str))
         // Copy after if it lies inside our own d.b area (which we could
