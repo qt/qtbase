f0a4af6557e5dc719018f05784037fce473048fc
Marc Mutz <marc.mutz@qt.io>
2025-03-03 10:53:04 +0100

    QWidget: init `data` in ctor, not Private::init()
    This should fix Coverity's hallucination that QWidget::data is used
    uninitialized, or, after 58b9250aea0b1b41c8cbd1033149371a93a5b2c1,
    dereferenced while it's still nullptr.
    
    Coverity-Id: 475552
    Coverity-Id: 475551
    Coverity-Id: 475550
    Coverity-Id: 475549
    Coverity-Id: 396828
    Pick-to: 6.5
    Change-Id: I49705b8bc44082fc68a59c2c98c4e37a6d841b19
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit bcd29a9eab00c77bc402b9d4242f097d19c92044)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e2129f6548621699eb31918d7d1f64c12c8c8b40)

--- src/widgets/kernel/qwidget.cpp
+++ src/widgets/kernel/qwidget.cpp
@@ -862,6 +862,7 @@ QWidget::QWidget(QWidget *parent, Qt::WindowFlags f)
 */
 QWidget::QWidget(QWidgetPrivate &dd, QWidget* parent, Qt::WindowFlags f)
     : QObject(dd, nullptr), QPaintDevice()
+    , data(&dd.data)
 {
     Q_D(QWidget);
     QT_TRY {
@@ -944,8 +945,6 @@ void QWidgetPrivate::init(QWidget *parentWidget, Qt::WindowFlags f)
     Q_ASSERT(allWidgets);
     if (allWidgets)
         allWidgets->insert(q);
-
-    q->data = &data;
 
 #if QT_CONFIG(thread)
     if (!parent) {
