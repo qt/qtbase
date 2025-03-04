850d1b7cc6d9ffffd557d63d8c854680ad885fda
Marc Mutz <marc.mutz@qt.io>
2025-03-04 14:59:48 +0100

    QMovableArrayOps::Inserter cleanup [2/3]: mark most data members const
    This makes explicit that the only thing that should be changing is
    displaceFrom, which tracks the hole-filling process.
    
    Requires rewriting of the dtor a bit, to skip the nInserts
    modification.
    
    Pick-to: 6.5
    Change-Id: I1f0ce17cbeb171704f789624ad1be6ef70e58245
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    (cherry picked from commit 75df3a250d84a7e2680a8f229d11eec20b2d3ff0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e1d021301146df04eac8ba2cb11b507b7bc6947b)

--- src/corelib/tools/qarraydataops.h
+++ src/corelib/tools/qarraydataops.h
@@ -661,11 +661,11 @@ public:
 
     struct Inserter
     {
-        QArrayDataPointer<T> *data;
+        QArrayDataPointer<T> * const data;
         T *displaceFrom;
-        T *displaceTo;
-        qsizetype nInserts = 0;
-        size_t bytes;
+        T * const displaceTo;
+        const qsizetype nInserts = 0;
+        const size_t bytes;
 
         void verifyPost()
         { Q_ASSERT(displaceFrom == displaceTo); }
@@ -680,13 +680,14 @@ public:
             ::memmove(static_cast<void *>(displaceTo), static_cast<void *>(displaceFrom), bytes);
         }
         ~Inserter() {
+            auto inserts = nInserts;
             if constexpr (!std::is_nothrow_copy_constructible_v<T>) {
                 if (displaceFrom != displaceTo) {
                     ::memmove(static_cast<void *>(displaceFrom), static_cast<void *>(displaceTo), bytes);
-                    nInserts -= qAbs(displaceFrom - displaceTo);
+                    inserts -= qAbs(displaceFrom - displaceTo);
                 }
             }
-            data->size += nInserts;
+            data->size += inserts;
         }
         Q_DISABLE_COPY(Inserter)
 
