robertpelloni/bobui

CBOR/JSON: fix crash when comparing strings with different length
Amends 8e5ce9cd369230256045864d6fad38dbd8bee413, which introduced the
QtPrivate::equalStrings() call. At that time, equalStrings() had already
required equal lengths (see 1560e0161af70b5cf88a70e55c0b502612d433cd),
so no excuse.

[ChangeLog][QtCore][QCborMap and QJsonObject] Fixed bug that could
result in a crash or failing to find a entry in the map/object with non-
ASCII keys.

Pick-to: 6.9 6.8
Fixes: QTBUG-133744
Change-Id: I6b0f8b0a2e47d3ef905afffda6c4c079814a0914
Reviewed-by: Marc Mutz <marc.mutz@qt.io>

tests/auto/corelib/serialization/json/tst_qtjson.cpp
@@ -41,6 +41,7 @@ private Q_SLOTS:
     void testObjectTakeDetach();
     void testObjectSmallKeys();
     void testObjectInsertCopies();
+    void testObjectInsertNonAscii();
     void testArraySimple();
     void testArrayInsertCopies();
     void testValueObject();
@@ -730,6 +731,22 @@ void tst_QtJson::testObjectInsertCopies()
         QCOMPARE(obj.value("prop2"), "TEST");
         QCOMPARE(obj2.size(), 1);
         QCOMPARE(obj2.value("value"), "TEST");
+    }
+}
+
+void tst_QtJson::testObjectInsertNonAscii()
+{
+    {
+        QJsonObject myObject;
+        myObject.insert("k♭", "First key");
+        myObject.insert("a", "Second key");
+        QCOMPARE(myObject.begin().keyView(), "a");
+    }
+    {
+        QJsonObject myObject;
+        myObject.insert("a", "Second key");
+        myObject.insert("k♭", "First key");
+        QCOMPARE(myObject.begin().keyView(), "a");
     }
 }
 

qt/qtbase

Manual conflict resolution for 6.9:
- Port from keyView() to key(), because the former is a 6.10+ feature.
Pick-to: 6.8
(cherry picked from commit 54daec43a041cb69cff31cbfd1dd0b7127e8ba87)

tests/auto/corelib/serialization/json/tst_qtjson.cpp
@@ -41,6 +41,7 @@ private Q_SLOTS:
     void testObjectTakeDetach();
     void testObjectSmallKeys();
     void testObjectInsertCopies();
+    void testObjectInsertNonAscii();
     void testArraySimple();
     void testArrayInsertCopies();
     void testValueObject();
@@ -730,6 +731,22 @@ void tst_QtJson::testObjectInsertCopies()
         QCOMPARE(obj.value("prop2"), "TEST");
         QCOMPARE(obj2.size(), 1);
         QCOMPARE(obj2.value("value"), "TEST");
+    }
+}
+
+void tst_QtJson::testObjectInsertNonAscii()
+{
+    {
+        QJsonObject myObject;
+        myObject.insert("k♭", "First key");
+        myObject.insert("a", "Second key");
+        QCOMPARE(myObject.begin().key(), "a");
+    }
+    {
+        QJsonObject myObject;
+        myObject.insert("a", "Second key");
+        myObject.insert("k♭", "First key");
+        QCOMPARE(myObject.begin().key(), "a");
     }
 }
 