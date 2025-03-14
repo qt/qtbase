ab3e4d3a5b77ed8fd0aa127989a379e360f8fc1b
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2025-03-14 15:31:48 +0100

    Fix calls to non-started QElapsedTimer functions
    It's illegal to call elapsed() or restart() (sic) on a non-started
    QElapsedTimer, so don't do that. In QNAM code, this has been observed by
    adding assertions into QElapsedTimer, which then make network tests
    crash. (While I am unable to run the network testsuite, this is
    reproducible locally by running tst_QNetworkDiskCache::setCookieHeader).
    
    The QTextStream test also calls restart() without a start(), but then it
    calls elapsed()+start() which is the whole point of restart(), so amend
    accordingly.
    
    Pick-to: 6.5
    Change-Id: I5cfe308f64b631b68b92843b409777a6b9176828
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 585471b138b195cb9020200cae760f119903df85)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 06b02f003e3955b28c2d9bdc7ab250aea703bb7f)

--- src/network/access/qnetworkreplyhttpimpl.cpp
+++ src/network/access/qnetworkreplyhttpimpl.cpp
@@ -1167,7 +1167,8 @@ void QNetworkReplyHttpImplPrivate::replyDownloadData(QByteArray d)
     emit q->readyRead();
     // emit readyRead before downloadProgress in case this will cause events to be
     // processed and we get into a recursive call (as in QProgressDialog).
-    if (downloadProgressSignalChoke.elapsed() >= progressSignalInterval
+    if (downloadProgressSignalChoke.isValid() &&
+        downloadProgressSignalChoke.elapsed() >= progressSignalInterval
         && (!decompressHelper.isValid() || decompressHelper.isCountingBytes())) {
         downloadProgressSignalChoke.restart();
         emit q->downloadProgress(bytesDownloaded, totalSizeOpt.value_or(-1));
@@ -1500,7 +1501,8 @@ void QNetworkReplyHttpImplPrivate::replyDownloadProgressSlot(qint64 bytesReceive
     // processed and we get into a recursive call (as in QProgressDialog).
     if (bytesDownloaded > 0)
         emit q->readyRead();
-    if (downloadProgressSignalChoke.elapsed() >= progressSignalInterval) {
+    if (downloadProgressSignalChoke.isValid() &&
+        downloadProgressSignalChoke.elapsed() >= progressSignalInterval) {
         downloadProgressSignalChoke.restart();
         emit q->downloadProgress(bytesDownloaded, bytesTotal);
     }
@@ -1937,7 +1939,8 @@ void QNetworkReplyHttpImplPrivate::_q_cacheLoadReadyRead()
         // This readyRead() goes to the user. The user then may or may not read() anything.
         emit q->readyRead();
 
-        if (downloadProgressSignalChoke.elapsed() >= progressSignalInterval) {
+        if (downloadProgressSignalChoke.isValid() &&
+            downloadProgressSignalChoke.elapsed() >= progressSignalInterval) {
             downloadProgressSignalChoke.restart();
             emit q->downloadProgress(bytesDownloaded, totalSizeOpt.value_or(-1));
         }
