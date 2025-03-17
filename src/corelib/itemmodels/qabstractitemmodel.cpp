14a070972736a5ef03a004dde55b6d62c9535835
Marc Mutz <marc.mutz@qt.io>
2025-03-17 16:00:43 +0100

    QFileSystemModel: memoize roleNames()
    The return value of this function is constant and some users call this
    function often (an earlier version of QGenericItemModel did, possibly
    QML does?), so memoize the result of the function instead of
    re-creating the QHash on every call.
    
    Use QAbstractItemModelPrivate::defaultRoleNames() so we don't need
    an instance to get the base class' contents. Add a comment to keep
    the two in sync.
    
    As a drive-by, port from the QByteArrayLiteral macro to _ba UDLs.
    
    Amends 32b586864e3a4398da38c045f4ac0823c3dc3c57.
    
    Change-Id: If00b51e60930c974e6f8f928711dc78ba1f42b93
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit fde232c855d59009503fe6dbaadd10346ab0830a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 34c5b311fc6a8cb4c3d736a9bb41e78f5200bd2f)

--- src/corelib/itemmodels/qabstractitemmodel.cpp
+++ src/corelib/itemmodels/qabstractitemmodel.cpp
@@ -2654,6 +2654,8 @@ QSize QAbstractItemModel::span(const QModelIndex &) const
 */
 QHash<int,QByteArray> QAbstractItemModel::roleNames() const
 {
+    // if the return value ever becomes dependent on *this, also change the following overrides:
+    // - QFileSystemModel
     return QAbstractItemModelPrivate::defaultRoleNames();
 }
 
