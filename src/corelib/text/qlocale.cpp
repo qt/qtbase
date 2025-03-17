5d90dea2a02d3ede82848e57a1b8ed7f17f884f0
Edward Welbourne <edward.welbourne@qt.io>
2025-03-17 17:59:40 +0100

    Correct case of QLocale::toString(double, ...)'s exponent separator
    The function follows various libc functions in forcing the caller to
    decide the case of the exponent separator, rather than allowing them
    to opt for the case the locale normally uses. At Qt 6 I changed the
    code reading from CLDR to not force the case, but didn't change
    toString() to force lower when that was requested. Since the function
    is documented to use lower-case when that's what it asks for, change
    it to do what it says in its documentation. A caller who wants the
    locale-appropriate case is thus obliged to call exponential() and scan
    it for lower- or upper-case letters and select the right one of 'e'
    and 'E' (or 'g' and 'G') to match.
    
    This partially reverts some test changes made in
    commit d5bb8d5150498dc059e8c17f224c66fb1a6bcf32 and expands the
    QLocale tests to include ones matching those it changed. Fixed some
    tests of QDoubleValidator that previously turned 'e' in the input into
    'E' in the output; roughly alternate between changing the input to
    match the output and the other way round, to be sure we exercise both
    branches of the test.
    
    Pick-to: 6.5
    Task-number: QTBUG-134785
    Fixes: QTBUG-134768
    Change-Id: I5cffbd772a166efab9f7a5145289771c16be1658
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 05c8a48612be38a3c50bb5ed9daf1adfc221b9f2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 31d64b725d966c11529eb07357847af40b4153c4)

--- src/corelib/text/qlocale.cpp
+++ src/corelib/text/qlocale.cpp
@@ -3945,7 +3945,9 @@ QString QLocaleData::doubleToString(double d, int precision, DoubleForm form,
         }
     }
 
-    return prefix + (flags & CapitalEorX ? std::move(numStr).toUpper() : numStr);
+    return prefix + (flags & CapitalEorX
+                     ? std::move(numStr).toUpper()
+                     : std::move(numStr).toLower());
 }
 
 QString QLocaleData::decimalForm(QString &&digits, int decpt, int precision,
