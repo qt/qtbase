9c79fbf25195139c72e6fd4bd0d965c8b38ccbf0
Marc Mutz <marc.mutz@qt.io>
2025-03-24 15:39:44 +0100

    tst_QGraphicsScene: fix memleaks in selectionChanged()/removeItem()
    A QGraphicsScene owns the items added to it, but if we later
    removeItem() one again, then we're also responsible for freeing the
    item. The test functions didn't do that, so we leaked the item.
    
    In order to not change the rest of the functions by deleting the item
    earlier than it had been before, install a qScopeGuard() to delete it,
    right after we removed it from the scene again.
    
    Amends the start of the public history.
    
    Change-Id: I8ab50626a0b5d52825bb329233ab8efe6c1291dc
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 3651769d52108b01b142a3bcaae530e949eafa68)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b859f192e9d6a4f988c7536c9238d0af91ca1360)
    (cherry picked from commit 456556684b818d47a066e14258153b1ffa70c32a)
    (cherry picked from commit ab33aa3dea596e4a2ec3b776833bb98e5798a3d7)
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- tests/auto/widgets/graphicsview/qgraphicsscene/tst_qgraphicsscene.cpp
+++ tests/auto/widgets/graphicsview/qgraphicsscene/tst_qgraphicsscene.cpp
@@ -44,6 +44,7 @@
 
 #include <QtCore/QDebug>
 #include <QtCore/QLoggingCategory>
+#include <QtCore/qscopeguard.h>
 
 #include <private/qgraphicsscene_p.h>
 #include <private/qgraphicssceneindex_p.h>
@@ -1002,6 +1003,7 @@ void tst_QGraphicsScene::selectionChanged()
     QCOMPARE(spy.count(), 3); // the grandchild was added, so the selection changed once
 
     scene.removeItem(parentItem);
+    const auto reaper = qScopeGuard([=] { delete parentItem; });
     QCOMPARE(spy.count(), 4); // the grandchild was removed, so the selection changed
 
     rect->setSelected(true);
@@ -1295,6 +1297,7 @@ void tst_QGraphicsScene::removeItem()
     item2->setSelected(true);
     QVERIFY(scene.selectedItems().contains(item2));
     scene.removeItem(item2);
+    const auto reaper = qScopeGuard([=] { delete item2; });
     QVERIFY(scene.selectedItems().isEmpty());
 
     // Check that we are in a state that can receive paint events
