d30642aea54fea036bfe167fb0214b9d14216467
Edward Welbourne <edward.welbourne@qt.io>
2025-03-27 12:01:04 +0100

    Purge dangling uses of QDateTime deprecated time-spec changes
    QDateTime::{to,set}TimeSpec and QTimeZone::setOffsetFromUtc()
    shouldn't be referenced in docs aside from their own; they are
    deprecated from 6.9.
    
    Fixes: QTBUG-135238
    Change-Id: I835cfaa888dd3079f96719cd1c413dc7c2796fdb
    Reviewed-by: Magdalena Stojek <magdalena.stojek@qt.io>
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit 8c943392ae2ae554e496f11d9a3dd6c2c2e762e3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b546ab5af43d2522bba3a5d71af87dda34ad5dea)

--- src/corelib/time/qdatetime.cpp
+++ src/corelib/time/qdatetime.cpp
@@ -4145,7 +4145,7 @@ QTime QDateTime::time() const
     details of that time zone). Equivalent to
     \c{timeRepresentation().timeSpec()}.
 
-    \sa setTimeSpec(), timeRepresentation(), date(), time()
+    \sa setTimeZone(), timeRepresentation(), date(), time()
 */
 
 Qt::TimeSpec QDateTime::timeSpec() const
@@ -5322,7 +5322,7 @@ Qt::weak_ordering compareThreeWay(const QDateTime &lhs, const QDateTime &rhs)
     Returns the system clock's current datetime, using the time representation
     described by \a zone. If \a zone is omitted, local time is used.
 
-    \sa currentDateTimeUtc(), QDate::currentDate(), QTime::currentTime(), toTimeSpec()
+    \sa currentDateTimeUtc(), QDate::currentDate(), QTime::currentTime(), toTimeZone()
 */
 
 /*!
@@ -5341,7 +5341,7 @@ QDateTime QDateTime::currentDateTime()
 
     Equivalent to \c{currentDateTime(QTimeZone::UTC)}.
 
-    \sa currentDateTime(), QDate::currentDate(), QTime::currentTime(), toTimeSpec()
+    \sa currentDateTime(), QDate::currentDate(), QTime::currentTime(), toTimeZone()
 */
 
 QDateTime QDateTime::currentDateTimeUtc()
@@ -5358,7 +5358,7 @@ QDateTime QDateTime::currentDateTimeUtc()
     This number is like the POSIX time_t variable, but expressed in milliseconds
     instead of seconds.
 
-    \sa currentDateTime(), currentDateTimeUtc(), toTimeSpec()
+    \sa currentDateTime(), currentDateTimeUtc(), toTimeZone()
 */
 
 /*!
