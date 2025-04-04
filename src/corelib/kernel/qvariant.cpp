79e3416bb08166cf27aaddaf8edcaf7652e77efa
Thiago Macieira <thiago.macieira@intel.com>
2025-04-04 08:24:04 -0700

    QMetaType/QVariant: update docs that canConvert() operates on types
    Not on values. That is, it indicates whether there is a conversion path
    from origin type to the destination type, not that the conversion will
    succeed when attempted. For example:
    
      QObject *o = new QAbstractItemModel;
      QVariant variant = QVariant::fromValue(o);
      qDebug() << variant.canConvert<QAbstractItemModel*>();
    
    This conversion *will* succeed for this particular value of a QObject*,
    so canConvert() must return true for conversions between QObject* and
    QAbstractItemModel*, despite not all such conversions being possible.
    
    This is also done in preparation of changes being done to the conversion
    routines that may cause some FP->integer conversions to fail depending
    on the value of the FP.
    
    Fixes: QTBUG-135619
    Task-number: QTBUG-135285
    Pick-to: 6.8
    Change-Id: I138e34fb61a8b8772c8bfffdf75787a2e007d847
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
    Reviewed-by: Lars Knoll <lars@knoll.priv.no>
    (cherry picked from commit 7cf49085eb47ae660d05e8579c08a3ad3be75021)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/kernel/qvariant.cpp
+++ src/corelib/kernel/qvariant.cpp
@@ -2068,6 +2068,10 @@ QVariantList QVariant::toList() const
     type, \a type. Such casting is done automatically when calling the
     toInt(), toBool(), ... methods.
 
+    Note this function operates only on the variant's type, not the contents.
+    It indicates whether there is a conversion path from this variant to \a
+    type, not that the conversion will succeed when attempted.
+
     \sa QMetaType::canConvert()
 */
 
