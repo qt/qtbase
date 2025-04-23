bc90fbb51efc4479df6da9aa019b3f6a4a291058
Marc Mutz <marc.mutz@qt.io>
2025-04-23 13:42:42 +0200

    Mark QXmlUtils as security-critical
    QXmlUtils are used by QXmlStream and QDom, both of which are (or ought
    soon to be) marked as security-critical. The component is clearly a
    data-parser, too, with the same input as QXmlStream and QDom, so has to
    be security-critical, too.
    
    The header file contains only declarations, so it gets the default
    score:significant.
    
    Amends 8df072fc8006510c9b743e8ffedaaf51a876883a.
    
    QUIP: 23
    Task-number: QTBUG-135194
    Change-Id: I415486dbba0c748e6af561ea4f098ea42c4b1830
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 046e6c5701da96dd64b613c2bc9cfd245c83b1a5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d15f3c7815881531ce02478c879af120095fe525)
    (cherry picked from commit af2bc3b9f8a104624998249e0966bd3a76d5e4e9)

--- src/corelib/serialization/qxmlutils_p.h
+++ src/corelib/serialization/qxmlutils_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:header-decls-only
 
 #ifndef QXMLUTILS_P_H
 #define QXMLUTILS_P_H
