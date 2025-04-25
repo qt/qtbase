f813496afc06ac981a491572fc177c5a05e46b1f
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-04-25 23:15:41 +0200

    Windows11Style: draw item text unconditionally
    5d590a38d96206bcf5ad6ffe2b156223dc909fd7 made the drawing of the
    itemview text dependent whether there is an editor or not. This is in
    contrast to all other styles and also no longer needed because now the
    editor properly paints it's background.
    
    Pick-to: 6.9 6.8
    Fixes: QTBUG-136217
    Task-number: QTBUG-119501
    Change-Id: I84f64e71010d1a3d77949dee78426565252bed34
    Reviewed-by: Wladimir Leuschner <wladimir.leuschner@qt.io>
    (cherry picked from commit 21f3364c06f9ea03be7c29de90b4fbf9eb26425b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/styles/modernwindows/qwindows11style.cpp
+++ src/plugins/styles/modernwindows/qwindows11style.cpp
@@ -1659,7 +1659,7 @@ void QWindows11Style::drawControl(ControlElement element, const QStyleOption *op
     }
     case CE_ItemViewItem: {
         if (const QStyleOptionViewItem *vopt = qstyleoption_cast<const QStyleOptionViewItem *>(option)) {
-            if (const QAbstractItemView *view = qobject_cast<const QAbstractItemView *>(widget)) {
+            if (qobject_cast<const QAbstractItemView *>(widget)) {
                 QRect checkRect = proxy()->subElementRect(SE_ItemViewItemCheckIndicator, vopt, widget);
                 QRect iconRect = proxy()->subElementRect(SE_ItemViewItemDecoration, vopt, widget);
                 QRect textRect = proxy()->subElementRect(SE_ItemViewItemText, vopt, widget);
@@ -1761,10 +1761,10 @@ void QWindows11Style::drawControl(ControlElement element, const QStyleOption *op
                 QIcon::State state = vopt->state & QStyle::State_Open ? QIcon::On : QIcon::Off;
                 vopt->icon.paint(painter, iconRect, vopt->decorationAlignment, mode, state);
 
-                if (!view || !view->isPersistentEditorOpen(vopt->index)) {
-                    painter->setPen(highlightCurrent && highContrastTheme ? vopt->palette.base().color() : option->palette.text().color());
-                    d->viewItemDrawText(painter, vopt, textRect);
-                }
+                painter->setPen(highlightCurrent && highContrastTheme ? vopt->palette.base().color()
+                                                                      : vopt->palette.text().color());
+                d->viewItemDrawText(painter, vopt, textRect);
+
                 // paint a vertical marker for QListView
                 if (vopt->state & State_Selected) {
                     if (const QListView *lv = qobject_cast<const QListView *>(widget);
