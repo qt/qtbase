4169839f3d939c2bcdd09adeb1125ba133bca026
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-05-04 15:51:23 +0200

    QHeaderView: fix logic in setOffset()
    Fix the logic in setOffset() which was accidentally reversed during
    fixing of UB during signed overflow.
    
    Amends 49fcac99deea390901000a74deea1c0c690b6ae2.
    
    Coverity-Id: 479557
    Fixes: QTBUG-136362
    Fixes: QTBUG-136477
    Change-Id: Ifc9c80346355b0a9e516effe2ff8b60b1966d67a
    Reviewed-by: Thorbjørn Lund Martsum <tmartsum@gmail.com>
    (cherry picked from commit e808b38d4bb894f42b7dfe52f1ef67a0d48c23ff)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 1da71e5e7d099b429802da67feb405214afa3b38)
    (cherry picked from commit d47b1255eb24efc1638c12ff182acc4dfa122f2a)

--- src/widgets/itemviews/qheaderview.cpp
+++ src/widgets/itemviews/qheaderview.cpp
@@ -447,7 +447,7 @@ void QHeaderView::setOffset(int newOffset)
     const int ndelta = qt_saturate<int>(d->headerOffset - qint64{newOffset});
     d->headerOffset = newOffset;
     if (d->orientation == Qt::Horizontal) {
-        if (isLeftToRight()) {
+        if (isRightToLeft()) {
             if (int r; !qMulOverflow<-1>(ndelta, &r))
                 d->viewport->scroll(r, 0);
             else
