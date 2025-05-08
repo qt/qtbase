e516876a47ed175f31cb7121b0146134613b78ae
Matthias Rauter <matthias.rauter@qt.io>
2025-05-08 15:43:02 +0200

    Mark src/corelib/ipc as security:significant
    Pick-to: 6.9 6.8
    Fixes: QTBUG-135188
    Change-Id: I8865fe8ffbc22e30218441ec64157d9bafccdd35
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit e186e86599b542930b773bb9eb0f25dfd3dc1857)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/ipc/qsystemsemaphore_posix.cpp
+++ src/corelib/ipc/qsystemsemaphore_posix.cpp
@@ -3,6 +3,7 @@
 // Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Tobias Koenig <tobias.koenig@kdab.com>
 // Copyright (C) 2022 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "qsystemsemaphore.h"
 #include "qsystemsemaphore_p.h"
