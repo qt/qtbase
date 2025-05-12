cd4b84f9b71a75c9ad1edeb2c53453533d80bf1a
Matthias Rauter <matthias.rauter@qt.io>
2025-05-12 11:02:32 +0200

    Mark src/corelib/global with security score
    Most of the files are unmarked and will therefore fall back to their
    default value of significant.
    
    qendian{_p.h,.h,.cpp} were considered to be critical due to their
    role in parsers, but ultimately kept at significant.
    
    qlogging{.h,.cpp} were considered to be critical because they work
    with raw string data that might come directly from untrusted sources.
    However, untrusted string data is forwarded to QString, and string
    parsing is only performed on trusted strings. Therefore, the security
    scores are kept at significant.
    
    qlibraryinfo.cpp is marked critical because it influence paths from
    which libraries and executables are loaded.
    
    qnumeric{.h,.cpp} are marked critical because they handle edge-cases
    that might lead to undefined behaviour if not handled correctly. At the
    current point we assume that various data-parsers rely on the correct
    behavior of these functions and therefore want to treat them as
    critical.
    
    qrandom{.h,.cpp} are marked critical because they are related to
    cryptography. Some functionality is described as cryptographically safe
    in the documentation. While we forward the complex parts to the OS, it
    seems prudent to be extra cautious here.
    
    QUIP: 23
    Pick-to: 6.8
    Fixes: QTBUG-135186
    Change-Id: I037379fff3a72c36b840d042252391417de33f5c
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit a671b3b1c88729b6b69c55746116781e2e13b7a1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 499ea7a4bef80291d8394691c392a29c92c76545)

--- src/corelib/global/qnumeric.h
+++ src/corelib/global/qnumeric.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parser
 
 #ifndef QNUMERIC_H
 #define QNUMERIC_H
--- src/corelib/global/qrandom.cpp
+++ src/corelib/global/qrandom.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2021 Intel Corporation.
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:cryptography
 
 // for rand_s
 #define _CRT_RAND_S
