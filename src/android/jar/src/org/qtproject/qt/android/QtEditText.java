1a4a7b0d04ea29467560609ec6d333cee997820d
Jani Korteniemi <jani.korteniemi@qt.io>
2025-05-13 13:26:07 +0300

    Mark qtbase/src/android files as security sensitive
    QtApkFileEngine.java
    -File/Data parsing
    
    QtClipboardManager.java
    -Text/Url parsing
    
    QtEditText.java
    -Password input type
    
    Fixes: QTBUG-136730
    Task-number: QTBUG-135178
    Change-Id: Ib2b574825e1f4acdc96c7bc781dbae00a274bd50
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry picked from commit 30f5ad0329867d266529a4e88db287f3a5d49749)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 3938b87d9151cf248856cdd20ea7b0caa5c5ae17)

--- src/android/jar/src/org/qtproject/qt/android/QtEditText.java
+++ src/android/jar/src/org/qtproject/qt/android/QtEditText.java
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2012 BogDan Vatra <bogdan@kde.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:password-input-type
 
 package org.qtproject.qt.android;
 
