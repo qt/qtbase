6ef8da2c41fc478af465a8e40fba2d40b612097b
Karim Pinter <karim.pinter@qt.io>
2025-05-13 16:45:59 +0300

    Remove VxWorks DKM check from QThreads
    Qt doesn't support VxWorks DKM since 4.8, removing DKM specific check,
    so it is the same as in Qt for VxWorks 5.15.
    
    Pick-to: 6.8
    Task-number: QTBUG-136722
    Change-Id: I86cf54e8eadbb78365b7c58cb72b0675714fb37a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 312215ba87b00b0065babe1cf190980e2d9dbc4a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/thread/qthread_unix.cpp
+++ src/corelib/thread/qthread_unix.cpp
@@ -718,7 +718,7 @@ static bool calculateUnixPriority(int priority, int *sched_policy, int *sched_pr
 
     int prio_min;
     int prio_max;
-#if defined(Q_OS_VXWORKS) && defined(VXWORKS_DKM)
+#if defined(Q_OS_VXWORKS)
     // for other scheduling policies than SCHED_RR or SCHED_FIFO
     prio_min = SCHED_FIFO_LOW_PRI;
     prio_max = SCHED_FIFO_HIGH_PRI;
