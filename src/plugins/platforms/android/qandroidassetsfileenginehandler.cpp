61d14ea26724d0da49bb75c9f403f6f0f6c3dbd4
Lauri Pohjanheimo <lauri.pohjanheimo@qt.io>
2025-05-16 12:41:03 +0300

    Mark files as security sensitive
    androidcontentfileengine.cpp
    qandroidapkfileengine.cpp,
    qandroidassetsfileenginehandler.cpp and
    extract.cpp marked
    Reasons: Data serialization, filename parsing
    
    Task-number: QTBUG-136818
    Task-number: QTBUG-135178
    Task-number: QTBUG-136816
    Pick-to: 6.8
    Change-Id: Ib277a04cc00dc0762feed17a7f185aa5d19942dc
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry picked from commit a6caa394ba49cb58cc07613f9a5fc6bfb5975e3b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/android/qandroidassetsfileenginehandler.cpp
+++ src/plugins/platforms/android/qandroidassetsfileenginehandler.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2012 BogDan Vatra <bogdan@kde.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:file-handling
 
 #include "androidjnimain.h"
 #include "qandroidassetsfileenginehandler.h"
