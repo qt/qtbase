6920b9ee302ba3f4043a95d0289736d711c2d698
Piotr Wiercinski <piotr.wiercinski@qt.io>
2025-05-21 18:21:53 +0200

    Mark platform/wasm/qstdweb as security sensitive
    Fixes: QTBUG-137066
    Pick-to: 6.9 6.8
    Change-Id: Ic70020d3781cf02a22812a3eb0769b1f2c5ca70f
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 639bbc79790a9898fd2f31497c35ed52cb5f90b7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/platform/wasm/qstdweb.cpp
+++ src/corelib/platform/wasm/qstdweb.cpp
@@ -1,5 +1,6 @@
-// Copyright (C) 2019 The Qt Company Ltd.
+// Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parser
 
 #include "qstdweb_p.h"
 #include "qwasmsuspendresumecontrol_p.h"
