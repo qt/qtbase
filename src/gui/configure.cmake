5bc06ecb87225c06176f4e5496f4f7ff0d28f41d
Liang Qi <liang.qi@qt.io>
2025-05-27 16:07:19 +0200

    Reapply: xcb: add xcb-util dependency for xcb-image
    xcb-image includes xcb_aux.h, which is part of xcb-util.
    
    0.3.8 is enough for the dependency, it also make Debian 10
    (Buster) satisfied. xcb-image is from xcb-util-image, which
    should follow same version number as xcb-util. Not sure how
    Debian 10(Buster) got xcb-image 0.3.9 with xcb-util 0.3.8
    combination.
    
    See also https://lists.x.org/archives/xorg-announce/2012-May/date.html#1965 .
    
    This reverts 92a57e0b016f811876ec1d62328ca8edfe452a3a .
    
    Fixes: QTBUG-86287
    Pick-to: 6.8
    Change-Id: I838b1e06a25fa1e45470466de17cbb6b6da851a4
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit ac2a38d80cfd043edc1a0b0434112af008bdabb9)
    Reviewed-by: Liang Qi <liang.qi@qt.io>
    (cherry picked from commit 3e9319325eabec978f508da896a83ef2964e599f)

--- src/gui/configure.cmake
+++ src/gui/configure.cmake
@@ -90,9 +90,13 @@ if((X11_SUPPORTED) OR QT_FIND_ALL_PACKAGES_ALWAYS)
 endif()
 qt_add_qmake_lib_dependency(xcb_icccm xcb)
 if((X11_SUPPORTED) OR QT_FIND_ALL_PACKAGES_ALWAYS)
+    qt_find_package(XCB 0.3.8 COMPONENTS UTIL PROVIDED_TARGETS XCB::UTIL MODULE_NAME gui QMAKE_LIB xcb_util)
+endif()
+qt_add_qmake_lib_dependency(xcb_util xcb)
+if((X11_SUPPORTED) OR QT_FIND_ALL_PACKAGES_ALWAYS)
     qt_find_package(XCB 0.3.9 COMPONENTS IMAGE PROVIDED_TARGETS XCB::IMAGE MODULE_NAME gui QMAKE_LIB xcb_image)
 endif()
-qt_add_qmake_lib_dependency(xcb_image xcb_shm xcb)
+qt_add_qmake_lib_dependency(xcb_image xcb_shm xcb_util xcb)
 if((X11_SUPPORTED) OR QT_FIND_ALL_PACKAGES_ALWAYS)
     qt_find_package(XCB 0.3.9 COMPONENTS KEYSYMS PROVIDED_TARGETS XCB::KEYSYMS MODULE_NAME gui QMAKE_LIB xcb_keysyms)
 endif()
@@ -525,6 +529,7 @@ qt_config_compile_test(xcb_syslibs
     LIBRARIES
         XCB::CURSOR
         XCB::ICCCM
+        XCB::UTIL
         XCB::IMAGE
         XCB::KEYSYMS
         XCB::RANDR
@@ -540,6 +545,7 @@ qt_config_compile_test(xcb_syslibs
 "// xkb.h is using a variable called 'explicit', which is a reserved keyword in C++
 #define explicit dont_use_cxx_explicit
 #include <xcb/xcb.h>
+#include <xcb/xcb_util.h>
 #include <xcb/xcb_image.h>
 #include <xcb/xcb_keysyms.h>
 #include <xcb/xcb_cursor.h>
