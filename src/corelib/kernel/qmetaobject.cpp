668ee8888571d07dbb960095018adbb7c7739cd8
Marc Mutz <marc.mutz@qt.io>
2025-06-05 17:06:15 +0200

    QMetaObject: cut out the `m` middle-man in indexOf*() methods
    It's the same as `this`.
    
    Amends e4f86012360b20938be8f34e6966a577dae6c049.
    
    Manual conflict resolution for 6.9:
    - deal with the absence of INDEXOF_COMPAT() in this branch.
    
    Reported-by: Ahmad Samir <a.samir78@gmail.com>
    Change-Id: I43fb23f8995c300b7982ffbb03ea1ed3ff55a122
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit f08898ffdbb7f5ffa710512efbf1f6b6a5ba7962)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e2c41eb66dc036202e17dce10732be47b75d00be)
    (cherry picked from commit 2429401d95898682716ebd01a79258859b309c4c)
    (cherry picked from commit d346af6760a4f8577fd9006337f25731a550124d)

--- src/corelib/kernel/qmetaobject.cpp
+++ src/corelib/kernel/qmetaobject.cpp
@@ -736,8 +736,7 @@ static int indexOfMethod_helper(const QMetaObject *m, const char *method)
 
 int QMetaObject::indexOfMethod(const char *method) const
 {
-    const QMetaObject *m = this;
-    int i = indexOfMethod_helper(m, method);
+    int i = indexOfMethod_helper(this, method);
     return i;
 }
 
@@ -810,8 +809,7 @@ static int indexOfSignal_helper(const QMetaObject *m, const char *signal)
 
 int QMetaObject::indexOfSignal(const char *signal) const
 {
-    const QMetaObject *m = this;
-    int i = indexOfSignal_helper(m, signal);
+    int i = indexOfSignal_helper(this, signal);
     return i;
 }
 
@@ -866,8 +864,7 @@ static int indexOfSlot_helper(const QMetaObject *m, const char *slot)
 
 int QMetaObject::indexOfSlot(const char *slot) const
 {
-    const QMetaObject *m = this;
-    int i = indexOfSlot_helper(m, slot);
+    int i = indexOfSlot_helper(this, slot);
     return i;
 }
 
