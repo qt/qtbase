5f8ca97f51d41cd313f028f25f2cb8c10922dfb0
Matthias Rauter <matthias.rauter@qt.io>
2025-06-10 16:38:07 +0200

    Make include guard match its filename
    Change-Id: I57af3c237bddffc424cab734faa30deacaa9bcdb
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit dd81b421270a390aff6e686677575ed6d017cd9d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/ipc/qtipccommon.h
+++ src/corelib/ipc/qtipccommon.h
@@ -1,8 +1,8 @@
 // Copyright (C) 2022 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#ifndef QNATIVEIPCKEY_H
-#define QNATIVEIPCKEY_H
+#ifndef QTIPCCOMMON_H
+#define QTIPCCOMMON_H
 
 #include <QtCore/qglobal.h>
 #include <QtCore/qtcore-config.h>
@@ -207,4 +207,4 @@ QT_END_NAMESPACE
 #endif // QT_CONFIG(sharedmemory) || QT_CONFIG(systemsemaphore)
 
 
-#endif // QNATIVEIPCKEY_H
+#endif // QTIPCCOMMON_H
