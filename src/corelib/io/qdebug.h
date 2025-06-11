64ce22c61e96e753a44175a002e532ed84fe27e8
Marc Mutz <marc.mutz@qt.io>
2025-06-11 19:00:57 +0200

    qdebug.h: remove duplicate includes
    Added by 51f702d4029148570d255daef80d071d98cf1e16 w/o rationale.
    
    Found in API-review.
    
    Change-Id: Ia0afd3d3aaf7a8bc392333b8359ce5b5496bec7f
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit f1e1a933a610a71d93d888cc911187b80ea02e9c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/io/qdebug.h
+++ src/corelib/io/qdebug.h
@@ -26,14 +26,11 @@
 #include <map>
 #include <memory>
 #include <optional>
-#include <set>
 #include <string>
 #include <string_view>
 #include <set>
 #include <tuple>
 #include <QtCore/q20type_traits.h>
-#include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <unordered_map>
 #include <unordered_set>
