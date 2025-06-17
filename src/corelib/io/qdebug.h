0c171b9ec45a2d9bcd8967c57f92c4416a151b2a
Matthias Rauter <matthias.rauter@qt.io>
2025-06-17 14:48:13 +0200

    Mark QDebug as security-critical
    QDebug can be used to print strings from untrusted sources. Since
    QDebug modifies these strings to improve their appearance, it parses
    their contents as part of the formatting process. This behavior can
    be exploited by attackers to trigger bugs in QDebug, making it a
    potential attack vector. Therefore, mark it as security-critical.
    
    Header and implementation files are marked.
    
    QUIP: 23
    Pick-to: 6.8
    Task-number: QTBUG-135187
    Change-Id: I3d751f196a110354ef6c81aa9d2af5e2c88a9b6a
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit d9a0cbc36c14b58c567731e820f6ea83c2c4da36)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit ac1d232cbad7598c7a0537dfa9dc6fa378b413e3)

--- src/corelib/io/qdebug.h
+++ src/corelib/io/qdebug.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2016 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parsing
 
 #ifndef QDEBUG_H
 #define QDEBUG_H
