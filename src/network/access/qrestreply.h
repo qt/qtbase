255cbf7827f8fcabc1d91035fb30041cc7a87603
Mårten Nordheim <marten.nordheim@qt.io>
2025-06-19 13:41:16 +0200

    CRA[network/access]: Mark the rest significant
    The rest was handled separately.
    
    Fixes: QTBUG-135582
    Pick-to: 6.8
    Change-Id: Ibabde62b1e66e364c9ba508a5a97d4830d7fcc69
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit f0fcd396c57107a8fb4bca12a014107b6b09f5d0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8ce3517a946b49c6724b4f81a0c45137827249d2)

--- src/network/access/qrestreply.h
+++ src/network/access/qrestreply.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QRESTREPLY_H
 #define QRESTREPLY_H
