b9f7f190d480777fdeb00a3c86b9d4bacf81beec
Mårten Nordheim <marten.nordheim@qt.io>
2025-06-19 13:42:01 +0200

    CRA[network/kernel]: Mark the rest significant
    The rest was handled separately.
    
    Fixes: QTBUG-135584
    Pick-to: 6.9 6.8
    Change-Id: I302f9f381712e5f65cc4b7354bfb441361c615e7
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit c0ec9dcee71a7f8dbb9f024e68a382f2e9f32e97)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/network/kernel/qdnslookup.h
+++ src/network/kernel/qdnslookup.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2012 Jeremy Lainé <jeremy.laine@m4x.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QDNSLOOKUP_H
 #define QDNSLOOKUP_H
