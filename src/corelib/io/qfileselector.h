e96fa69938735c271225e866963693c1edb3b794
Matthias Rauter <matthias.rauter@qt.io>
2025-06-20 09:04:37 +0200

    Mark QFileSelector as security critical
    QFileSelector inspects the paths to find the corresponding files.
    Further there is the risk of exposing files of the resource system or
    choosing files outside of the resource system by accident.
    
    QUIP: 23
    Pick-to: 6.9 6.8
    Task-number: QTBUG-135187
    Change-Id: I6e0783da8bc08102df7cc108389db33940ffa4a6
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit cca658d4821b6d7378df13c29d1dab53c44359ac)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/io/qfileselector.h
+++ src/corelib/io/qfileselector.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2013 BlackBerry Limited. All rights reserved.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QFILESELECTOR_H
 #define QFILESELECTOR_H
