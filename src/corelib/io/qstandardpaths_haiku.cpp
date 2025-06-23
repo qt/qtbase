594012d43992da611218a22c9ab88753f0b092e9
Matthias Rauter <matthias.rauter@qt.io>
2025-06-23 12:12:09 +0200

    Mark QStandardPath implementation security critical
    The QStandardPaths class provides access to OS-specific standard
    locations on the filesystem, and also allows to search for files
    within the defined directories. Other parts of the application
    should be able to rely on the results returned by this class. Bugs
    in this code can lead to severe issues like overwriting of user files,
    reading configs from an incorrect place, or loading an incorrect
    external code for execution. Therefore, the respective files are
    marked security-critical.
    
    QUIP: 23
    Pick-to: 6.9 6.8
    Task-number: QTBUG-135187
    Change-Id: Id1a8189f2a7cde4a760446428551f2fe682d4073
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit f10e599e3f6e5c77890281b44fad996542d60fca)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/io/qstandardpaths_haiku.cpp
+++ src/corelib/io/qstandardpaths_haiku.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Tobias Koenig <tobias.koenig@kdab.com>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:provides-trusted-directory-paths
 
 #include "qstandardpaths.h"
 
