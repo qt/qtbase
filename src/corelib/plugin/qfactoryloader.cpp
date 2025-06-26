7854a7d74fe77eda51692c1a507bb5f0f263cd2d
Marc Mutz <marc.mutz@qt.io>
2025-06-26 11:55:14 +0200

    QFactoryLoader: reserve() two QLists
    Either of them may see additional append()s after the first loop, but
    we can at least skip the first few reallocations by reserving
    d->library.size().
    
    Change-Id: I1c2ccdc47444657957dd593a76d75fe210536b5b
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit afae4957d675c733cd06185194f72624096b29bd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 9e7b15aaeb3665ee47599ce2523fad43e7b3cb80)

--- src/corelib/plugin/qfactoryloader.cpp
+++ src/corelib/plugin/qfactoryloader.cpp
@@ -505,6 +505,7 @@ QFactoryLoader::MetaDataList QFactoryLoader::metaData() const
     QList<QPluginParsedMetaData> metaData;
 #if QT_CONFIG(library)
     QMutexLocker locker(&d->mutex);
+    metaData.reserve(qsizetype(d->libraries.size()));
     for (const auto &library : d->libraries)
         metaData.append(library->metaData);
     locker.unlock();
@@ -531,6 +532,7 @@ QList<QCborArray> QFactoryLoader::metaDataKeys() const
     QList<QCborArray> metaData;
 #if QT_CONFIG(library)
     QMutexLocker locker(&d->mutex);
+    metaData.reserve(qsizetype(d->libraries.size()));
     for (const auto &library : d->libraries) {
         const QCborValue md = library->metaData.value(QtPluginMetaDataKeys::MetaData);
         metaData.append(md["Keys"_L1].toArray());
