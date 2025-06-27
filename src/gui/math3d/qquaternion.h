c6ca7e80d35bd92ffdb63f230f12ad18849fcd59
Marc Mutz <marc.mutz@qt.io>
2025-06-27 11:42:14 +0200

    QQuaternion: fix incorrect Qt 7 export on inline function
    The fromEulerAngles(QVector3D) function is and will stay inline, as
    it's just forwarding to the (float, float, float) overload of the same
    name. So it needn't be, and shan't, either, be exported come Qt 7.
    
    Remove the QT7_ONLY export.
    
    Partially reverts / amends f9f1272e7c438b79591032877f2b386af8085c3f.
    
    Pick-to: 6.8
    Change-Id: I8ed0054ca89ab04ef0b06bea953473e91789fda6
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit e8f596785e28e3bfbe0d47a329a445bc29415488)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 564851b55f23a0d1659b03dbce27e9a9ae5afee0)

--- src/gui/math3d/qquaternion.h
+++ src/gui/math3d/qquaternion.h
@@ -109,7 +109,7 @@ QT_WARNING_POP
 
 #ifndef QT_NO_VECTOR3D
     inline QVector3D toEulerAngles() const;
-    QT7_ONLY(Q_GUI_EXPORT) static inline QQuaternion fromEulerAngles(const QVector3D &eulerAngles);
+    static inline QQuaternion fromEulerAngles(const QVector3D &eulerAngles);
 #endif
     QT7_ONLY(Q_GUI_EXPORT) void getEulerAngles(float *pitch, float *yaw, float *roll) const;
     QT7_ONLY(Q_GUI_EXPORT) static QQuaternion fromEulerAngles(float pitch, float yaw, float roll);
