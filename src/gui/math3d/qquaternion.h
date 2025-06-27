aaabcbad0a553b432c9b64e0d97ed51c9f0bbb42
Marc Mutz <marc.mutz@qt.io>
2025-06-27 14:36:22 +0200

    QQuaternion: rename the fromEulerAngles() argument
    The eulerAngles name will clash with a function I intend to add, of
    the same name, probably triggering -Wshadow. Besides, this fixes long
    lines and isn't any less readable.
    
    Picking all the way since it's 100% risk-free.
    
    Change-Id: Iea552c6cad4ad20164cf0cc462cb1dcb2bc752c0
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit eb98bb80162cdf3656c9839bae5ee126f4ff00a8)
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit b5eb2c657d6e1bdae755389767e9083e6f7aed70)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit a88b8012da7e72bbbe95549eb5613fe8681a1744)
    (cherry picked from commit 64eb432796bfd7959c5c991b1eff73c0a85c2c2b)

--- src/gui/math3d/qquaternion.h
+++ src/gui/math3d/qquaternion.h
@@ -130,7 +130,7 @@ QT_WARNING_POP
 
 #ifndef QT_NO_VECTOR3D
     inline QVector3D toEulerAngles() const;
-    static inline QQuaternion fromEulerAngles(const QVector3D &eulerAngles);
+    static inline QQuaternion fromEulerAngles(const QVector3D &angles);
 #endif
     void getEulerAngles(float *pitch, float *yaw, float *roll) const;
     static QQuaternion fromEulerAngles(float pitch, float yaw, float roll);
@@ -341,9 +341,9 @@ QVector3D QQuaternion::toEulerAngles() const
     return QVector3D(pitch, yaw, roll);
 }
 
-QQuaternion QQuaternion::fromEulerAngles(const QVector3D &eulerAngles)
+QQuaternion QQuaternion::fromEulerAngles(const QVector3D &angles)
 {
-    return QQuaternion::fromEulerAngles(eulerAngles.x(), eulerAngles.y(), eulerAngles.z());
+    return QQuaternion::fromEulerAngles(angles.x(), angles.y(), angles.z());
 }
 
 #endif // QT_NO_VECTOR3D
