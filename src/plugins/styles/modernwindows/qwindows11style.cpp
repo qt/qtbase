9fe2ea40140c0383d9a40a710a4ff1b82e0a7d73
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-07-10 18:26:04 +0200

    Windows11Style: don't modify palette for QScrollBar/QMenu/QComboBox
    Setting the background color to Qt::transparent is no longer needed and
    just causes problems in other places.
    
    Amends 2ec4da62b2020affc2687a2348470401fbd43bdb.
    
    Pick-to: 6.8
    Fixes: QTBUG-138401
    Task-number: QTBUG-138094
    Change-Id: I4ccad488204d03c870dbb61386157aa130ca78b3
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit d48185433af1f69a388e28f0078224a82702be6c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8455467c472ce9438c4baf0c8db8ea7b1c1d54d3)

--- src/plugins/styles/modernwindows/qwindows11style.cpp
+++ src/plugins/styles/modernwindows/qwindows11style.cpp
@@ -2248,9 +2248,6 @@ void QWindows11Style::polish(QWidget* widget)
         widget->setWindowFlag(Qt::NoDropShadowWindowHint);
         widget->setAttribute(Qt::WA_RightToLeft, layoutDirection);
         widget->setAttribute(Qt::WA_WState_Created, wasCreated);
-        auto pal = widget->palette();
-        pal.setColor(widget->backgroundRole(), Qt::transparent);
-        widget->setPalette(pal);
         if (!isScrollBar) {
             bool inGraphicsView = widget->graphicsProxyWidget() != nullptr;
             if (!inGraphicsView && comboBoxContainer && comboBoxContainer->parentWidget())
