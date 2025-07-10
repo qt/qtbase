e25577e6f4aee12e493740b6cfc8b5fb0d219c83
Marc Mutz <marc.mutz@qt.io>
2025-07-10 12:52:57 +0200

    QTextStream: use write(QChar) more
    Replace write(&ch, 1) calls with write(ch) ones.
    
    The QChar overload, added in 31c6d9f04ba80e6776ae01cb0588e6bd228f7184,
    is safer, because it has less preconditions. With QTextStream being
    security-critical, we should target to use safer functions to do the
    same work, even if it introduces a bit of git history churn.
    
    The call from a different TU (qdebug.cpp) means we need to remove the
    lying inline keywords from both declaration and definition of
    write(QChar), because GCC -Werrors on them.
    
    As a drive-by, remove a use of QLatin1Char, in preparation of moving
    from QChar to char16_t, eventually.
    
    Pick-to: 6.8
    Change-Id: I97b4c8c68a4ee0d4b821c4d2ec4a9dd6f44dceb0
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit b9f6a1b113d8f26210da5c3194f0f790883761fe)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 742cd7e8fd19862a06df15ba9ef7894ce946e578)

--- src/corelib/serialization/qtextstream.cpp
+++ src/corelib/serialization/qtextstream.cpp
@@ -705,7 +705,7 @@ void QTextStreamPrivate::write(const QChar *data, qsizetype len)
 /*!
     \internal
 */
-inline void QTextStreamPrivate::write(QChar ch)
+void QTextStreamPrivate::write(QChar ch)
 {
     if (string) {
         // ### What about seek()??
@@ -839,7 +839,7 @@ void QTextStreamPrivate::putString(const QChar *data, qsizetype len, bool number
             const QChar sign = len > 0 ? data[0] : QChar();
             if (sign == locale.negativeSign() || sign == locale.positiveSign()) {
                 // write the sign before the padding, then skip it later
-                write(&sign, 1);
+                write(sign);
                 ++data;
                 --len;
             }
@@ -868,7 +868,7 @@ void QTextStreamPrivate::putString(QLatin1StringView data, bool number)
             const QChar sign = data.size() > 0 ? QLatin1Char(*data.data()) : QChar();
             if (sign == locale.negativeSign() || sign == locale.positiveSign()) {
                 // write the sign before the padding, then skip it later
-                write(&sign, 1);
+                write(sign);
                 data = QLatin1StringView(data.data() + 1, data.size() - 1);
             }
         }
