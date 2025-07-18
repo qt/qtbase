32fd1dc89a22e237aa5393b5efc6238c71d7b740
Marc Mutz <marc.mutz@qt.io>
2025-07-18 21:56:45 +0200

    tst_QLocale: check codeToScript() can find LastScript
    It can't...
    
    Filed QTBUG-138566 to track the issue.
    
    Amends the start of the public history.
    
    Task-number: QTBUG-138566
    Task-number: QTBUG-17105
    Change-Id: I84444044b6f82dcec620e9a0e7f803870b7a3ed5
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 7196859101e99b1234c015e9b30569d76836f2e4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 96a1057b2dd8b3f1c0ecdc80b70a41b7760df050)
    (cherry picked from commit dc18d32eb7b2c4623098bd8a2977086ecf520867)
    (cherry picked from commit 6a5abfb0880a8a66a4d0f248f5ac2dedc5a5d79c)

--- tests/auto/corelib/text/qlocale/tst_qlocale.cpp
+++ tests/auto/corelib/text/qlocale/tst_qlocale.cpp
@@ -4340,6 +4340,10 @@ void tst_QLocale::codeToLcs()
     QCOMPARE(QLocale::codeToScript(QString()), QLocale::AnyScript);
     QCOMPARE(QLocale::codeToScript(QString("Zzzz")), QLocale::AnyScript);
     QCOMPARE(QLocale::codeToScript(QString("Hans")), QLocale::SimplifiedHanScript);
+    // ensure we can find the last script, too:
+    QEXPECT_FAIL("", "QTBUG-138566", Continue);
+    QCOMPARE(QLocale::codeToScript(QLocale::scriptToCode(QLocale::LastScript)),
+             QLocale::LastScript);
 }
 
 QTEST_MAIN(tst_QLocale)
