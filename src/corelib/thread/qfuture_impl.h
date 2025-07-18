c24b01fe95889d2172a4a8b91a4051e6d21bb8dd
Matthias Rauter <matthias.rauter@qt.io>
2025-07-18 12:57:56 +0200

    Mark all files in corelib/thread security significant
    None of the files in this folder handle untrusted inputs, implement a
    protocol or cryptographic logic or execute external code.
    
    While bugs in threading, atomics, and locks might lead to security
    issues, there is no elevated risk and exposure of these classes and
    methods. Therefore they are marked significant.
    
    Pick-to: 6.9 6.8
    Fixes: QTBUG-135196
    Change-Id: If336b6f8a920b3d41b4c4e57f1bff36e2b392739
    Reviewed-by: Dimitrios Apostolou <jimis@qt.io>
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit a795b442638f35faab15c1b6d06d268948865f56)

--- src/corelib/thread/qfuture_impl.h
+++ src/corelib/thread/qfuture_impl.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2020 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QFUTURE_H
 #error Do not include qfuture_impl.h directly
--- src/corelib/thread/qresultstore.h
+++ src/corelib/thread/qresultstore.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2020 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QTCORE_RESULTSTORE_H
 #define QTCORE_RESULTSTORE_H
