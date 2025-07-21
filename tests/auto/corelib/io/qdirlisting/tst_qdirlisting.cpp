d7efdd01ae8d9f870473c0ef574a59c75ca822a9
Ahmad Samir <a.samirh78@gmail.com>
2025-07-22 01:37:45 +0300

    QDirListing: add tests for name filter consisting of "."
    Add the tests a previous patch added to QDirIterator to QDirListing,
    too. This tests the legacy filters code path in QDirListing.
    
    Pick-to: 6.8
    Change-Id: Ie44a6cd3766fa6aba737d0850de1e6d1c66b8924
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 16c6021d0dac2771adb5e28097ae89f671b66bc5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 145e21b9a23a1354a4049cae3d19c44172bae2fe)

--- tests/auto/corelib/io/qdirlisting/tst_qdirlisting.cpp
+++ tests/auto/corelib/io/qdirlisting/tst_qdirlisting.cpp
@@ -82,6 +82,8 @@ private slots:
     void longPath();
     void dirorder();
     void relativePaths();
+    void dotNameFilters_data();
+    void dotNameFilters();
 #if defined(Q_OS_WIN)
     void uncPaths_data();
     void uncPaths();
@@ -665,6 +667,81 @@ void tst_QDirListing::relativePaths()
 {
     for (const auto &dirEntry : QDirListing(u"*"_s, ItFlag::Recursive))
         QCOMPARE(dirEntry.filePath(), QDir::cleanPath(dirEntry.filePath()));
+}
+
+void tst_QDirListing::dotNameFilters_data()
+{
+    QTest::addColumn<QStringList>("nameFilters");
+    QTest::addColumn<QDirListing::IteratorFlags>("flags");
+    QTest::addColumn<QDir::Filters>("dirFilters");
+    QTest::addColumn<QStringList>("expected");
+
+    using F = QDirListing::IteratorFlag;
+
+    QTest::newRow("dirLister-default-flags")
+        << QStringList{u"."_s}
+        << QDirListing::IteratorFlags{}
+        << QDir::Filters(QDir::NoFilter)
+        << QStringList{};
+
+    QTest::newRow("dirLister-IncludeDotAndDotDot")
+        << QStringList{u"."_s}
+        << QDirListing::IteratorFlags(F::IncludeDotAndDotDot)
+        << QDir::Filters(QDir::NoFilter)
+        << QStringList{u"."_s};
+
+    QTest::newRow("dirLister-IncludeDotAndDotDot-glob-everything")
+        << QStringList{u".*"_s}
+        << QDirListing::IteratorFlags(F::IncludeDotAndDotDot)
+        << QDir::Filters(QDir::NoFilter)
+        << QStringList{u"."_s, u".."_s};
+
+    // Test legacy filters code path
+    QTest::newRow("legacy-NoDotAndDotDot")
+        << QStringList{u"."_s}
+        << QDirListing::IteratorFlags{}
+        << QDir::Filters(QDir::AllEntries | QDir::NoDotAndDotDot)
+        << QStringList{};
+
+    QTest::newRow("legacy-default-dirfilters")
+        << QStringList{u"."_s}
+        << QDirListing::IteratorFlags{}
+        << QDir::Filters(QDir::AllEntries)
+        << QStringList{u"."_s};
+
+    QTest::newRow("legacy-glob-everything")
+        << QStringList{u".*"_s}
+        << QDirListing::IteratorFlags{}
+        << QDir::Filters(QDir::AllEntries)
+        << QStringList{u"."_s, u".."_s};
+}
+
+void tst_QDirListing::dotNameFilters()
+{
+    QFETCH(QStringList, nameFilters);
+    QFETCH(QDirListing::IteratorFlags, flags);
+    QFETCH(QDir::Filters, dirFilters);
+    QFETCH(QStringList, expected);
+
+    const auto dirPath = u"empty"_s;
+    QVERIFY(QFileInfo(dirPath).isDir());
+
+    if (dirFilters == QDir::NoFilter) {
+        QStringList entries;
+        for (const auto &dirEntry : QDirListing(dirPath, nameFilters, flags))
+            entries.append(dirEntry.fileName());
+        QEXPECT_FAIL("dirLister-IncludeDotAndDotDot",
+                     "QDirListing ignores name filters for '.' and '..' when IncludeDotAndDotDot "
+                     "is set",
+                     Continue);
+        entries.sort();
+        QCOMPARE_EQ(entries, expected);
+    } else {
+        // legacy filters code path
+        QStringList entries = QDir(dirPath).entryList(nameFilters, dirFilters);
+        entries.sort();
+        QCOMPARE_EQ(entries, expected);
+    }
 }
 
 #if defined(Q_OS_WIN)
