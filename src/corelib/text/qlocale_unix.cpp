9d825e973a124c8d5ce199e5c7ed4f9785006737
Thiago Macieira <thiago.macieira@intel.com>
2025-07-22 08:17:03 -0700

    QLocale/Unix: remove unnecessary #endif/#ifdef
    It's the same thing. Amends a0c4a712263dbacd2c8d95da64e00bd213d05cbf
    ("Inital port of QSystemLocale on BlackBerry 10") from Qt 5.2 which
    removed a bunch of older QNX classes.
    
    Change-Id: Idd39bc44956bd82356b6fffdf63dbdc775bb7fa3
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit eaef0e4671baaf006fa910a689d31a40963b552a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 729c7d40509781e7481725817499c21e91708379)
    (cherry picked from commit 9e139676d05cce17e1d266832f8c91034e267f62)
    (cherry picked from commit 12ee7d91e7bdafde0837faec0a63403fa7aa6957)

--- src/corelib/text/qlocale_unix.cpp
+++ src/corelib/text/qlocale_unix.cpp
@@ -75,10 +75,6 @@ void QSystemLocaleData::readEnvironment()
 
 Q_GLOBAL_STATIC(QSystemLocaleData, qSystemLocaleData)
 
-#endif
-
-#ifndef QT_NO_SYSTEMLOCALE
-
 static bool contradicts(QStringView maybe, const QString &known)
 {
     if (maybe.isEmpty())
