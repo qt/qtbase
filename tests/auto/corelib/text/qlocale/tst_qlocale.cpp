aa65c29627dcd352eaa1bc122cb95d95d4247086
Marc Mutz <marc.mutz@qt.io>
2025-07-24 12:44:51 +0200

    tst_QLocale: replace complicated ifdefery with macro
    This stops the shotgun surgery necessary when the condition changes,
    by centralizing the condition and assigning a symbolic name to the
    feature is represents. The more so as this list has evolved over the
    lifetime of the still-active branches: First it was only ICU, then
    Windows was added, and then Mac.
    
    Task-number: QTBUG-2163
    Pick-to: 6.9 6.8 6.5
    Change-Id: I0fa604f9726a9fcf12b90655a5621b4169a7199b
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit c376f2f40e772e1a86ba61ea8609fbac8c1ad28d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/corelib/text/qlocale/tst_qlocale.cpp
+++ tests/auto/corelib/text/qlocale/tst_qlocale.cpp
@@ -32,6 +32,14 @@ using namespace Qt::StringLiterals;
 
 Q_DECLARE_METATYPE(QLocale::FormatType)
 
+// These platforms implement a locale-dependent case conversion
+// (the others fall back to QString::toUpper/toLower()):
+#if QT_CONFIG(icu) || defined(Q_OS_WIN) || defined(Q_OS_APPLE)
+#  define QT_HAS_LOCALE_CASE_CONVERSION 1
+#else
+#  define QT_HAS_LOCALE_CASE_CONVERSION 0
+#endif
+
 class tst_QLocale : public QObject
 {
     Q_OBJECT
@@ -172,7 +180,7 @@ private slots:
     void codeToLang_data();
     void codeToLang();
 
-#if QT_CONFIG(icu) || defined(Q_OS_WIN) || defined(Q_OS_APPLE)
+#if QT_HAS_LOCALE_CASE_CONVERSION
     void toLowerUpper_data();
     void toLowerUpper();
 
@@ -5556,7 +5564,7 @@ void tst_QLocale::codeToLcs()
              QLocale::LastScript);
 }
 
-#if QT_CONFIG(icu) || defined(Q_OS_WIN) || defined(Q_OS_APPLE)
+#if QT_HAS_LOCALE_CASE_CONVERSION
 void tst_QLocale::toLowerUpper_data()
 {
     QTest::addColumn<QLocale>("locale");
@@ -5651,7 +5659,7 @@ void tst_QLocale::toLowerUpperEszett()
     QCOMPARE(QLocale(u"de_DE"_s).toUpper(eszettLowerString), eszettUpperString);
     QCOMPARE(QLocale(u"tr_TR"_s).toUpper(eszettLowerString), eszettUpperString);
 }
-#endif
+#endif // QT_HAS_LOCALE_CASE_CONVERSION
 
 QTEST_MAIN(tst_QLocale)
 #include "tst_qlocale.moc"
