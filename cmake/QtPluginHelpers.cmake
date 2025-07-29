99d20128add4b4d2272b638b31bee28662e58e5c
Tim Blechmann <tim.blechmann@qt.io>
2025-07-29 08:47:03 +0800

    CMake: add missing headers to project files
    Making the code a little more IDE friendly by populating IDE projects.
    
    Pick-to: 6.8
    Change-Id: I7071e1072d5dec710d633078f567c282c75f212b
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit d7097efcb8d01e1cc639727a67ee4fac916f8363)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 262f92b30a915f70c23d773bbae74dbf73a93858)

--- cmake/QtPluginHelpers.cmake
+++ cmake/QtPluginHelpers.cmake
@@ -549,6 +549,7 @@ function(qt_internal_add_darwin_permission_plugin permission)
         DEFAULT_IF FALSE
         SOURCES
             ${permission_source_file}
+            platform/darwin/qdarwinpermissionplugin_p_p.h
         DEFINES
             QT_DARWIN_PERMISSION_PLUGIN=${permission}
         LIBRARIES
