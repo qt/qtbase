914290a787e186dd9e7bc4375b7941cd16365481
Marc Mutz <marc.mutz@qt.io>
2025-07-29 22:05:40 +0200

    QLocale/Win: remove unused variable
    It was declared, but never used. Remove it.
    
    Amends fcd2a219c4c222309152f1ea2a3124ddc17d4ea5, which removed the
    last user of the variable.
    
    Change-Id: Ica20677284a828a77785b6a668c58797e02d7bed
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 8a6c40ffc34fef54f75a45ec2e19bf2991e4f08a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit cead8a0655ef0cb9a65a300fa908451903921205)
    (cherry picked from commit e3e1c09e24018f410126f4bf8efd330b893bf8e9)
    (cherry picked from commit 8edc9fb02a1d420c9f4a100c70f25674742a805e)

--- src/corelib/text/qlocale_win.cpp
+++ src/corelib/text/qlocale_win.cpp
@@ -1164,7 +1164,6 @@ static QString winIso3116CtryName(LCID id)
 
 static QByteArray getWinLocaleName(LCID id)
 {
-    QByteArray result;
     if (id == LOCALE_USER_DEFAULT) {
         const auto [name, lcid] = scanLangEnv();
         if (!name.isEmpty())
