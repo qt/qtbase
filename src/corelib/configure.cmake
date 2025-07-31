b51a88daa0d931d8c13e6ddb7fe5a6530b6ae380
Karim Pinter <karim.pinter@qt.io>
2025-07-31 16:21:24 +0300

    Set exit on poll error by default on VxWorks
    On Qt 5.15 for VxWorks, exit on poll error was on by default. This
    change brings it to 6.x.
    
    Task-number: QTBUG-134546
    Pick-to: 6.8
    Change-Id: I660c17f4433cc0c77b8d8b6da80a6961c2cdfc98
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 0c848b70b0e171f82d9059fbbd7f4c08dd07e13e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 118a74cd805b0e56a85690d64f8e32945ab53e1c)

--- src/corelib/configure.cmake
+++ src/corelib/configure.cmake
@@ -1175,7 +1175,7 @@ qt_feature("cborstreamwriter" PUBLIC
 )
 qt_feature("poll-exit-on-error" PRIVATE
     LABEL "Poll exit on error"
-    AUTODETECT OFF
+    AUTODETECT VXWORKS
     CONDITION UNIX
     PURPOSE "Exit on error instead of just printing the error code and continue."
 )
