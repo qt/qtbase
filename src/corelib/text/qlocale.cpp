9714b8daf26c1d559e771fd20b992e82ce9e503d
Marc Mutz <marc.mutz@qt.io>
2025-08-02 06:33:44 +0200

    QLocale: add an easy missing std::move()
    Coverity complained, and it's trivial to see, that a move is missing
    in the assignment of local 'converted' to 'digits'. It's trivial
    because the scope of 'converted' ends right afterwards.
    
    Coverity complains about more missing moves in qlocale.cpp, but the
    others are more complicated, so need a separate patch.
    
    Amends ed2b110b6add650954dc102a0317c14ff826c677.
    
    Coverity-Id: 427079
    Change-Id: Ic124b2c9d9318cc7194d2f2affc8bd522be53fc7
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit fe278b9551db6ef18a88f08822a43d2e2d81374d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2680b043d2b03afb92644779ed96c48ab776ed1e)
    (cherry picked from commit cdadcbc69988d654a74f86f6cebfe2433a7bd10d)
    (cherry picked from commit 42158e24c8e68ad4b2954d51d23959f8e671bd99)

--- src/corelib/text/qlocale.cpp
+++ src/corelib/text/qlocale.cpp
@@ -3611,7 +3611,7 @@ QString QLocaleData::doubleToString(double d, int precision, DoubleForm form,
                 converted.append(QChar::highSurrogate(digit));
                 converted.append(QChar::lowSurrogate(digit));
             }
-            digits = converted;
+            digits = std::move(converted);
         } else {
             Q_ASSERT(zero.size() == 1);
             Q_ASSERT(!zero.at(0).isSurrogate());
