a3895644feeda742a242f7fd829145436a2bcad7
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-08-02 15:58:12 +0200

    QWindows11/VistaStyle: remove special handling buttons in QInputDialog
    The special handling for the QDialogBox inside the QInputDialog did not
    do anything since Qt 4.5.0 when the object name 'qt_inputdlg_buttonbox'
    was no longer set to the QDialogButtonBox anymore. Therefore we don't
    need a special handling for this anymore here. It's also not used in
    any other style.
    
    Pick-to: 6.8
    Change-Id: I94dc797ca602b8f53b58a65f7fca6d0f07d709a5
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 24c8695a10c377f592bba41403734cb336ec7319)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 6ea1f6318fa498cdba2ed7f7345904160c17f13c)

--- src/plugins/styles/modernwindows/qwindowsvistastyle.cpp
+++ src/plugins/styles/modernwindows/qwindowsvistastyle.cpp
@@ -2041,10 +2041,6 @@ void QWindowsVistaStyle::drawPrimitive(PrimitiveElement element, const QStyleOpt
         const QDialogButtonBox *buttonBox = nullptr;
         if (qobject_cast<const QMessageBox *> (widget))
             buttonBox = widget->findChild<const QDialogButtonBox *>(QLatin1String("qt_msgbox_buttonbox"));
-#if QT_CONFIG(inputdialog)
-        else if (qobject_cast<const QInputDialog *> (widget))
-            buttonBox = widget->findChild<const QDialogButtonBox *>(QLatin1String("qt_inputdlg_buttonbox"));
-#endif // QT_CONFIG(inputdialog)
         if (buttonBox) {
             //draw white panel part
             QWindowsThemeData theme(widget, painter,
@@ -4669,16 +4665,6 @@ void QWindowsVistaStyle::polish(QWidget *widget)
                 buttonBox->setContentsMargins(0, 9, 0, 0);
 #endif
         }
-#if QT_CONFIG(inputdialog)
-        else if (qobject_cast<QInputDialog *> (widget)) {
-            widget->setAttribute(Qt::WA_StyledBackground);
-#if QT_CONFIG(dialogbuttonbox)
-            QDialogButtonBox *buttonBox = widget->findChild<QDialogButtonBox *>(QLatin1String("qt_inputdlg_buttonbox"));
-            if (buttonBox)
-                buttonBox->setContentsMargins(0, 9, 0, 0);
-#endif
-        }
-#endif // QT_CONFIG(inputdialog)
 }
 
 /*!
@@ -4750,16 +4736,6 @@ void QWindowsVistaStyle::unpolish(QWidget *widget)
                 buttonBox->setContentsMargins(0, 0, 0, 0);
 #endif
         }
-#if QT_CONFIG(inputdialog)
-        else if (qobject_cast<QInputDialog *> (widget)) {
-            widget->setAttribute(Qt::WA_StyledBackground, false);
-#if QT_CONFIG(dialogbuttonbox)
-            QDialogButtonBox *buttonBox = widget->findChild<QDialogButtonBox *>(QLatin1String("qt_inputdlg_buttonbox"));
-            if (buttonBox)
-                buttonBox->setContentsMargins(0, 0, 0, 0);
-#endif
-        }
-#endif // QT_CONFIG(inputdialog)
         else if (QTreeView *tree = qobject_cast<QTreeView *> (widget)) {
             tree->viewport()->setAttribute(Qt::WA_Hover, false);
         }
