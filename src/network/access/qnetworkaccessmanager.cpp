4ca25af6de294fbccd0d353706cec331b7184333
Mate Barany <mate.barany@qt.io>
2025-08-07 15:36:25 +0200

    Fix some minor grammar mistakes in the documentation of QNAM
    Change-Id: I45efad0c2484e9eb5c45dffea4e1d5a8c0e2ccaa
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>
    (cherry picked from commit b0a01e3239c5ded21df77c587758c2beeb3ab3db)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/network/access/qnetworkaccessmanager.cpp
+++ src/network/access/qnetworkaccessmanager.cpp
@@ -187,7 +187,7 @@ static void ensureInitialized()
 
     Once a QNetworkAccessManager object has been created, the application can
     use it to send requests over the network. A group of standard functions
-    are supplied that take a request and optional data, and each return a
+    is supplied that take a request and optional data, and each returns a
     QNetworkReply object. The returned object is used to obtain any data
     returned in response to the corresponding request.
 
