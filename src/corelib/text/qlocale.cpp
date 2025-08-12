dbab124b80ae2195928994fc9335270d8e466dc2
Edward Welbourne <edward.welbourne@qt.io>
2025-08-12 15:54:44 +0200

    Simplify QLocaleData::unsLongLongToString()'s handling of zero
    QLocaleData::longLongToString() doesn't bother special-casing zero as
    parameter, so don't bother doing so here, either. The optimization of
    the short-cut is rendered moot by applyIntegerFormatting() in any
    case, so would only make a difference in the unpadded, unjustified
    case, and even in that case the user who cares about such an
    optimisation can do better, after all, by doing it themselves with
    value ? locale.toString(value) : locale.zeroDigit(). The
    simplification incidentally saves an unintended copy of zeroDigit()
    flagged by Coverity.
    
    Amends commit 19ed60d703f9071d183760a8ce1c50009c780bf8
    
    Pick-to: 6.9 6.8 6.5
    Coverity-Id: 424819
    Change-Id: I61593d7d0c19ed492929b913f4040ea6e708276a
    Reviewed-by: Mate Barany <mate.barany@qt.io>
    (cherry picked from commit 8c886b7e6a441a0bd17a2c68dc509adf1d343c3f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qlocale.cpp
+++ src/corelib/text/qlocale.cpp
@@ -4303,9 +4303,7 @@ QString QLocaleData::longLongToString(qlonglong n, int precision,
 QString QLocaleData::unsLongLongToString(qulonglong l, int precision,
                                          int base, int width, unsigned flags) const
 {
-    const QString zero = zeroDigit();
-    QString resultZero = base == 10 ? zero : QStringLiteral("0");
-    return applyIntegerFormatting(l ? qulltoa(l, base, zero) : resultZero,
+    return applyIntegerFormatting(qulltoa(l, base, zeroDigit()),
                                   false, precision, base, width, flags);
 }
 
