94319d72c58aea8f6ad3777253656401a1cf3e7b
Ahmad Samir <a.samirh78@gmail.com>
2025-08-15 23:55:15 +0300

    QLocale: add a `Q` prefix to struct ParsingResult
    A type defined in a header (including private headers) should either be
    in a namespace or have a Q prefix.
    
    Fix the issue by moving the struct into QLocaleData.
    
    Amends 67129ef9cd946837cec6118c9e6e5a03b7eae342.
    
    Pick-to: 6.8
    Change-Id: I153678bc3d38134dcbbeba507c13008bf672a04d
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit 1cdb5178b9db59f3aa5c0af8f30ad25bc6620021)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 98aa34d251426e0c1e5cbe6496d1a7e5b75aab45)

--- src/corelib/text/qlocale_p.h
+++ src/corelib/text/qlocale_p.h
@@ -227,18 +227,6 @@ Q_DECLARE_TYPEINFO(QLocaleId, Q_PRIMITIVE_TYPE);
 
 using CharBuff = QVarLengthArray<char, 256>;
 
-struct ParsingResult
-{
-    enum State { // A duplicate of QValidator::State
-        Invalid,
-        Intermediate,
-        Acceptable
-    };
-
-    State state = Invalid;
-    CharBuff buff;
-};
-
 struct QLocaleData
 {
 public:
@@ -280,6 +268,18 @@ public:
         int higher = 0; // Each group between separators.
         int least = 0; // Least significant, when any separators appear.
         bool isValid() const { return least > 0 && higher > first && first > 0; }
+    };
+
+    struct ParsingResult
+    {
+        enum State { // A duplicate of QValidator::State
+            Invalid,
+            Intermediate,
+            Acceptable,
+        };
+
+        State state = Invalid;
+        CharBuff buff;
     };
 
 private:
