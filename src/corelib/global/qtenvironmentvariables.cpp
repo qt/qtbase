1813058b76918813309a84b886cec89ae9ba8b68
Marc Mutz <marc.mutz@qt.io>
2025-08-18 07:29:01 +0200

    qTzName(): Fix #endif comment
    Since eedb715ece5beabd1db731d0d003cf8112360f7c, the #ifdef is for
    _UCRT instead of Q_OS_WIN, so let the endif comment reflect that.
    
    Amends said commit.
    
    Pick-to: 6.9 6.8
    Change-Id: I7f9b488ed923744267a1107826ab05553f3d0267
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
    (cherry picked from commit 70433155f56b68ed2789a10dfe554edb428a0b43)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/global/qtenvironmentvariables.cpp
+++ src/corelib/global/qtenvironmentvariables.cpp
@@ -420,7 +420,7 @@ QString qTzName(int dstIndex)
         if (ok)
             memcpy(name, src, size + 1);
     }
-#endif // Q_OS_WIN
+#endif // _UCRT
     return ok ? QString::fromLocal8Bit(name, qsizetype(size)) : QString();
 }
 
