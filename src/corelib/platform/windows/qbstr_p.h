a13a857599d796e9dac4c7d0c5b3d5bbd67a42e8
Oliver Wolff <oliver.wolff@qt.io>
2025-08-19 09:08:39 +0200

    CRA review of src/corelib/platform/windows
    None of these files parse data or implement a protocol. While COM can be
    used to run "external code" none of the files in questions directly
    enables that so the security flags all can be "default".
    
    As a drive by: Fix minor comment
    
    Fixes: QTBUG-137067
    Fixes: QTBUG-135192
    Pick-to: 6.10 6.9 6.8
    Change-Id: I02772a94d27a4bc5d7bd3049f2f8d4bf4b049f4a
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit 4dcdb60056833ac42a3bc14804bdc03733f647fe)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/platform/windows/qbstr_p.h
+++ src/corelib/platform/windows/qbstr_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QBSTR_P_H
 #define QBSTR_P_H
@@ -141,4 +142,4 @@ QT_END_NAMESPACE
 
 #endif // Q_OS_WIN
 
-#endif // QCOMPTR_P_H
+#endif // QBSTR_P_H
