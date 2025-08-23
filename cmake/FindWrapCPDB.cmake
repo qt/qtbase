0dfcc864b2e143220a18f24397c993011c0545b8
Abrar Nasir Jaffari <abrarnasirjaffari@gmail.com>
2025-08-23 08:50:41 +0000

    Fix CPDB build issues: remove missing source file and add WrapCPDB target

--- cmake/FindWrapCPDB.cmake
+++ cmake/FindWrapCPDB.cmake
@@ -1,25 +1,7 @@
-# Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: BSD-3-Clause
-
-if (TARGET WrapCPDB::WrapCPDB)
-    set(WrapCPDB_FOUND ON)
-    return()
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(CPDB REQUIRED cpdb-libs-common)
+if(CPDB_FOUND)
+    add_library(WrapCPDB::WrapCPDB INTERFACE IMPORTED)
+    target_include_directories(WrapCPDB::WrapCPDB INTERFACE ${CPDB_INCLUDE_DIRS})
+    target_link_libraries(WrapCPDB::WrapCPDB INTERFACE ${CPDB_LIBRARIES})
 endif()
-
-find_package(PkgConfig QUIET)
-if (PKG_CONFIG_FOUND)
-    pkg_check_modules(CPDB QUIET cpdb-frontend)
-    if (CPDB_FOUND)
-        add_library(WrapCPDB::WrapCPDB INTERFACE IMPORTED)
-        target_compile_definitions(WrapCPDB::WrapCPDB INTERFACE -DQT_NO_KEYWORDS)
-        set_target_properties(WrapCPDB::WrapCPDB PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${CPDB_INCLUDE_DIRS}")
-        set_target_properties(WrapCPDB::WrapCPDB PROPERTIES
-            INTERFACE_LINK_LIBRARIES "${CPDB_LIBRARIES}")
-    endif()
-endif()
-
-find_package_handle_standard_args(WrapCPDB
-                                  REQUIRED_VARS CPDB_INCLUDE_DIRS CPDB_LIBRARIES
-                                  VERSION_VAR CPDB_VERSION)
-
