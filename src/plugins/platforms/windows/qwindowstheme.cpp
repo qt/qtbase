3d7e1c146262da658a788724bde90a9f46c7e23e
Zhao Yuhang <2546789017@qq.com>
2025-08-29 14:53:06 +0800

    Windows QPA: remove unnecessary WinRT usage
    We have QOperatingSystemVersion to retrieve and compare
    the current OS version, which is accurate and reliable,
    I see no absolute reason to use WinRT APIs to do this
    job, and WinRT APIs may throw in some environments, so
    let's use our own facility.
    
    Amends commit 081062ea5e396d9d251956eee412156f2319b816
    
    Task-number: QTBUG-135964
    Pick-to: 6.8 6.5
    Change-Id: I80df22c2ffde3c8e7d724fe03a684021ea6c5094
    Reviewed-by: Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
    (cherry picked from commit 5868f8a1d4bcbba89c6323d6562d9cf509b5be0b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b787756d42e08cc7c49f89448b0fcafd52864b96)

--- src/plugins/platforms/windows/qwindowstheme.cpp
+++ src/plugins/platforms/windows/qwindowstheme.cpp
@@ -49,7 +49,6 @@
 #   include <QtCore/private/qt_winrtbase_p.h>
 
 #   include <winrt/Windows.UI.ViewManagement.h>
-#   include <winrt/Windows.System.Profile.h>
 #endif // QT_CONFIG(cpp_winrt)
 
 QT_BEGIN_NAMESPACE
@@ -279,19 +278,6 @@ static QColor placeHolderColor(QColor textColor)
     return textColor;
 }
 
-static inline bool isWindows11() {
-    bool retVal = false;
-#if QT_CONFIG(cpp_winrt)
-    auto versionStr = winrt::Windows::System::Profile::AnalyticsInfo::VersionInfo().DeviceFamilyVersion();
-    uint64_t version = std::stoull(std::wstring(versionStr));
-    uint32_t major = (version >> 48) & 0xFFFF;
-    uint32_t minor = (version >> 32) & 0xFFFF;
-    uint32_t build = (version >> 16) & 0xFFFF;
-    retVal = (major >= 10 && minor >= 0 and build >= 22000);
-#endif
-    return retVal;
-}
-
 /*
     This is used when the theme is light mode, and when the theme is dark but the
     application doesn't support dark mode. In the latter case, we need to check.
@@ -418,9 +404,10 @@ void QWindowsTheme::populateDarkSystemBasePalette(QPalette &result)
 static inline QPalette toolTipPalette(const QPalette &systemPalette, bool light, bool highContrastEnabled)
 {
     QPalette result(systemPalette);
-    const QColor tipBgColor = highContrastEnabled ? (isWindows11() ? getSysColor(COLOR_WINDOW) : getSysColor(COLOR_BTNFACE)) :
+    static const bool isWindows11 = QOperatingSystemVersion::current() >= QOperatingSystemVersion::Windows11;
+    const QColor tipBgColor = highContrastEnabled ? (isWindows11 ? getSysColor(COLOR_WINDOW) : getSysColor(COLOR_BTNFACE)) :
                                     (light ? getSysColor(COLOR_INFOBK) : systemPalette.button().color());
-    const QColor tipTextColor = highContrastEnabled ? (isWindows11() ? getSysColor(COLOR_WINDOWTEXT) : getSysColor(COLOR_BTNTEXT)) :
+    const QColor tipTextColor = highContrastEnabled ? (isWindows11 ? getSysColor(COLOR_WINDOWTEXT) : getSysColor(COLOR_BTNTEXT)) :
                                     (light ? getSysColor(COLOR_INFOTEXT) : systemPalette.buttonText().color().darker(120));
 
     result.setColor(QPalette::All, QPalette::Button, tipBgColor);
