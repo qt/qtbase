1ce1ab2ce0f2701070f3c510d8310c754d335dfd
Marc Mutz <marc.mutz@qt.io>
2025-09-04 17:13:30 +0200

    QDataStream: fix -Werror,-Wshorten-64-to-32 on 32-bit Clang
    On 32-bit platforms qsizetype is int, so assigning a qint64 to it
    narrows.
    
    It's benign, because we perform the overflow check in the next line,
    but that doesn't help -Werror.
    
    Fix by making the narrowing explicit. As a drive-by, mark `n` const.
    
    Amends fd48ce0b73c74dafd5db27bc1f2752ef665df7ef.
    
    Reported-by: Karim Pinter <karim.pinter@qt.io>
    Pick-to: 6.8
    Change-Id: I14d95c54c501df14a0b76d11262f22605fbb583b
    Reviewed-by: Øystein Heskestad <oystein.heskestad@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit d812e32bcba17f4be5a54c48651c3068487478f7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit a0f09867f7f09fa43edfc87f8de48f6bf765002c)

--- src/corelib/serialization/qdatastream.h
+++ src/corelib/serialization/qdatastream.h
@@ -295,7 +295,7 @@ QDataStream &readArrayBasedContainer(QDataStream &s, Container &c)
 
     c.clear();
     qint64 size = QDataStream::readQSizeType(s);
-    qsizetype n = size;
+    const auto n = qsizetype(size);
     if (size != n || size < 0) {
         s.setStatus(QDataStream::SizeLimitExceeded);
         return s;
@@ -321,7 +321,7 @@ QDataStream &readListBasedContainer(QDataStream &s, Container &c)
 
     c.clear();
     qint64 size = QDataStream::readQSizeType(s);
-    qsizetype n = size;
+    const auto n = qsizetype(size);
     if (size != n || size < 0) {
         s.setStatus(QDataStream::SizeLimitExceeded);
         return s;
@@ -346,7 +346,7 @@ QDataStream &readAssociativeContainer(QDataStream &s, Container &c)
 
     c.clear();
     qint64 size = QDataStream::readQSizeType(s);
-    qsizetype n = size;
+    const auto n = qsizetype(size);
     if (size != n || size < 0) {
         s.setStatus(QDataStream::SizeLimitExceeded);
         return s;
