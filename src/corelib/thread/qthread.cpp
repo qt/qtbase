03a05b6181d7cc9c8a1f781e350cde505e815620
Morten Sørvig <morten.sorvig@qt.io>
2025-09-09 14:16:32 +0200

    no-thread: include setStatusAndClearList() in build
    Fix build error on single-threaded webassembly:
    
      undefined symbol: QtPrivate::BindingStatusOrList::
      setStatusAndClearList(QBindingStatus*)
    
    Fixes: QTBUG-139990
    Change-Id: Iabeb4c0f94a9ec8374ceb9419f0acacd02711729
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>
    (cherry picked from commit af66bdfbc3ea741819fa5c5855c9ab6889ec4864)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 36cacc0e955e5c8e558affea947dc32cb87ad784)

--- src/corelib/thread/qthread.cpp
+++ src/corelib/thread/qthread.cpp
@@ -620,6 +620,8 @@ QThread::QualityOfService QThread::serviceLevel() const
     return d->serviceLevel;
 }
 
+#endif
+
 /*!
     \internal
     Transitions BindingStatusOrList to the binding status state. If we had a list of
@@ -636,6 +638,8 @@ void QtPrivate::BindingStatusOrList::setStatusAndClearList(QBindingStatus *statu
     // synchronizes-with the load-acquire in bindingStatus():
     data.store(encodeBindingStatus(status), std::memory_order_release);
 }
+
+#if QT_CONFIG(thread)
 
 /*!
     Enters the event loop and waits until exit() is called, returning the value
