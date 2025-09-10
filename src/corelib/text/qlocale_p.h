0196d03fb8a67e2379f729907d42a51831a67e84
Edward Welbourne <edward.welbourne@qt.io>
2025-09-10 15:22:53 +0200

    Move CharBuff back into QLocaleData
    This reverts commit 69d3e96e02d4726d79aaf2d82acd30a903aa5a74, and
    adjusted users that have since been added.
    
    Its name doesn't start with Q so it shouldn't be in the public
    namespace, even if it is in a private header.
    
    The reason for the move out of QLocaleData disappeared with the
    recent commit 1cdb5178b9db59f3aa5c0af8f30ad25bc6620021, which moved
    ParsingResult into QLocaleData for the same reason.
    
    Pick-to: 6.9 6.8
    Change-Id: Icd3eac41ef7654024224dd1d55a5922f7f598a36
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit a865a146bead3526dc8df5badf35aa2b55bcd614)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qlocale_p.h
+++ src/corelib/text/qlocale_p.h
@@ -243,9 +243,6 @@ struct QLocaleId
 };
 Q_DECLARE_TYPEINFO(QLocaleId, Q_PRIMITIVE_TYPE);
 
-
-using CharBuff = QVarLengthArray<char, 256>;
-
 struct QLocaleData
 {
 public:
@@ -288,6 +285,8 @@ public:
         int least = 0; // Least significant, when any separators appear.
         bool isValid() const { return least > 0 && higher > first && first > 0; }
     };
+
+    using CharBuff = QVarLengthArray<char, 256>;
 
     struct ParsingResult
     {
