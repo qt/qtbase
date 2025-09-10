6149e2081cf578fba35e49e84b2bd3439f349c63
Marc Mutz <marc.mutz@qt.io>
2025-09-10 10:41:44 +0200

    Mark string collation classes as security-critical
    QString and QByteArray are critical, too, and not because of their
    ownership semantics, but because of the algorithms operating on
    them. Collation is one of those algorithms.
    
    The headers are not decls-only, but they only contain trivial
    implementation (like (QChar*, int) -> QStringView forwarders, or the
    move SMFs or swap()), so are significant.
    
    QUIP: 23
    Task-number: QTBUG-135195
    Pick-to: 6.8
    Change-Id: I2e3418fd74b12cbfa297576f25a37ec2ea050902
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit c7b8563758a3f3d29cde317ace3f4d296bca5a9e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qcollator_p.h
+++ src/corelib/text/qcollator_p.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2013 Aleix Pol Gonzalez <aleixpol@kde.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:trivial-impl-only
 
 #ifndef QCOLLATOR_P_H
 #define QCOLLATOR_P_H
--- src/corelib/text/qcollator_posix.cpp
+++ src/corelib/text/qcollator_posix.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2013 Aleix Pol Gonzalez <aleixpol@kde.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parser
 
 #include "qcollator_p.h"
 #include "qstringlist.h"
