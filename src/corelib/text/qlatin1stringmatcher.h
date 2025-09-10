58d5b0b7210224ace314f78a56b01b64a91b0b82
Marc Mutz <marc.mutz@qt.io>
2025-09-10 14:08:58 +0200

    Mark Boyer-Moore searchers as security-critical
    QString and QByteArray are critical, too, and not because of their
    ownership semantics, but because of the algorithms operating on
    them. Boyer-Moore searching is one of those algorithms.
    
    The QStringMatcher header is not decls-only, but it only contains
    trivial implementation (like (QChar*, int) -> QStringView forwarders),
    so is sensitive.
    
    The QLatin1StringMatcher and QByteArrayMatchers contain half of the
    algorithm in the header, though, to support calculating the BM tables
    at compile-time, so they are security-critical.
    
    Task-nunber: QTBUG-135195
    Pick-to: 6.10.0 6.9 6.8
    Change-Id: Ia0e32262a7b80462d681d1e688dc5153de136e7a
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 9e479614d95a0859491ef8cbcc79a3a92f18a649)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qlatin1stringmatcher.h
+++ src/corelib/text/qlatin1stringmatcher.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parser
 
 #ifndef QLATIN1STRINGMATCHER_H
 #define QLATIN1STRINGMATCHER_H
--- src/corelib/text/qstringmatcher.cpp
+++ src/corelib/text/qstringmatcher.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2020 The Qt Company Ltd.
 // Copyright (C) 2019 Mail.ru Group.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parser
 
 #include "qstringmatcher.h"
 
