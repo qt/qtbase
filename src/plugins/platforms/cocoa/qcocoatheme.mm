0b96e511483e39bf530a0db836757f02fa6b580f
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2025-09-11 15:15:09 +0200

    macOS: Show icons in menus on macOS 26 and above
    As this is the new system behavior.
    
    Pick-to: 6.8 6.5
    Change-Id: Ie33bc70a627133459e26411f4492ea0fe1f07ca4
    Reviewed-by: Timur Pocheptsov <timur.pocheptsov@qt.io>
    (cherry picked from commit 08a17345db77589fc5a1c451df2795071b5bd3e9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d1641d016eb9dcc12f5a9941f6777d891979158b)
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

--- src/plugins/platforms/cocoa/qcocoatheme.mm
+++ src/plugins/platforms/cocoa/qcocoatheme.mm
@@ -465,7 +465,7 @@ QVariant QCocoaTheme::themeHint(ThemeHint hint) const
     case QPlatformTheme::KeyboardAutoRepeatRate:
         return 1.0 / NSEvent.keyRepeatInterval;
     case QPlatformTheme::ShowIconsInMenus:
-        return false;
+        return QOperatingSystemVersion::current() >= QOperatingSystemVersion::MacOSTahoe;
     default:
         break;
     }
