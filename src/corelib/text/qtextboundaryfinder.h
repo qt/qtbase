1cd9b6676e2e927a40d09cffeddd59a76089b286
Marc Mutz <marc.mutz@qt.io>
2025-09-11 12:59:58 +0200

    Mark qtextboundaryfinder.h as trivial-impl-only
    The implementation was already marked as security-critical in
    8df072fc8006510c9b743e8ffedaaf51a876883a.
    
    Task-number: QTBUG-135195
    Change-Id: I62787b7fefcc1904730e9834c6a571eb1bfc380a
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 8a5b5584006b21cdefd931c7f173ac546a89a100)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 68025817c31da558b57ebb1ffd85b470ac7adf2f)

--- src/corelib/text/qtextboundaryfinder.h
+++ src/corelib/text/qtextboundaryfinder.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:sensitive reason:trivial-impl-only
 
 #ifndef QTEXTBOUNDARYFINDER_H
 #define QTEXTBOUNDARYFINDER_H
