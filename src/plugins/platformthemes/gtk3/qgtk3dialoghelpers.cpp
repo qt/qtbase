2cc8a2cdec03cf30b0f5736ecccafba805b31172
Piotr Wiercinski <piotr.wiercinski@qt.io>
2025-09-15 16:49:34 +0200

    Assess security level for files under qtbase/src/plugins/platformthemes
    Assign the default level to all files in the directory.
    
    Fixes: QTBUG-135726
    Pick-to: 6.9 6.8
    Change-Id: I3a585710e61f29d99a460f6335e541203b721ce2
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>
    (cherry picked from commit 8033a313ff28ce2a8e69bd2f6e2d946d291acf5e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platformthemes/gtk3/qgtk3dialoghelpers.cpp
+++ src/plugins/platformthemes/gtk3/qgtk3dialoghelpers.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #undef QT_NO_FOREACH // this file contains unported legacy Q_FOREACH uses
 
