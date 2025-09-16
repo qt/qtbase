e852557a56f227b7bf02b3a6a6ad162e282bfb02
Volker Hilsheimer <volker.hilsheimer@qt.io>
2025-09-16 15:48:01 +0200

    Add security tags to the Qt Sql module
    Tag code that constructs SQL statements, parses results, or deals with
    user credentials as security critical. This is the driver code, the
    implementation of QSqlResult, QSqlTableModel, and the QSqlDatabase
    implementation (which deals with, and optionally stores, credentials).
    
    The rest of the code is not critical and gets the default tag.
    
    Fixes: QTBUG-135591
    Pick-to: 6.8
    Change-Id: I18fb565fd27ed8d1c9c1f3a1f572816b718eb3e8
    Reviewed-by: Mate Barany <mate.barany@qt.io>
    (cherry picked from commit 55f46569f3e9688c2a07973923bfda68aa2b0c3e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 9e6078294e48b3271284210ab4000256847c588c)

--- src/plugins/sqldrivers/mimer/qsql_mimer.h
+++ src/plugins/sqldrivers/mimer/qsql_mimer.h
@@ -1,6 +1,8 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // Copyright (C) 2022 Mimer Information Technology
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
 #ifndef QSQL_MIMER_H
 #define QSQL_MIMER_H
 
