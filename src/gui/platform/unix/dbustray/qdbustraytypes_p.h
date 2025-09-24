584b4b693bf5503fecc6b66c563b6e7ec2256884
Liang Qi <liang.qi@qt.io>
2025-09-24 06:59:52 +0200

    CRA review: gui/platform/unix/dbustray/*
    The files that we maintain by hand are marked as significant.
    * D-Bus is security critical, but the Qt DBus Tray implementation
    is merely a user, and does not do any parsing or communication on its own.
    
    QUIP: 23
    Task-number: QTBUG-134573
    Pick-to: 6.8
    Change-Id: I91d58c1598bb92bedfe613294897233073edc4f2
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 79219c95de4e201f773b64ceeae1ecc007e7335c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/platform/unix/dbustray/qdbustraytypes_p.h
+++ src/gui/platform/unix/dbustray/qdbustraytypes_p.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2009 Marco Martin <notmart@gmail.com>
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QDBUSTRAYTYPES_P_H
 #define QDBUSTRAYTYPES_P_H
