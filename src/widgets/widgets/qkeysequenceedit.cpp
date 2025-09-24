15f2095d1c8d0cc7803b32d32c230639871afb8e
Jan Arve Sæther <jan-arve.saether@qt.io>
2025-09-24 18:35:19 +0200

    Add security headers to qtbase/src/widgets/widgets
    criticals:
    
    qcalendarwidget.cpp:    QCalendarDateValidator::setFormat()
    qdatetimeedit.cpp:      QDateTimeEdit::setDisplayFormat()
    qlcdnumber.cpp:         int2string()
    qspinbox.cpp:           QDoubleSpinBoxPrivate::validateAndInterpret()
    qwidgetlinecontrol.cpp: QWidgetLineControl::updateDisplayText()
    
    QUIP: 23
    Fixes: QTBUG-135744
    Pick-to: 6.8
    Change-Id: I9e8bdc7bf953c8f1b589464935af99f3f8393ce3
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 3f9569c505c0949efee336a83cb247e4e5a4658c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/widgets/qkeysequenceedit.cpp
+++ src/widgets/widgets/qkeysequenceedit.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2013 Ivan Komissarov.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "qkeysequenceedit.h"
 #include "qkeysequenceedit_p.h"
--- src/widgets/widgets/qkeysequenceedit_p.h
+++ src/widgets/widgets/qkeysequenceedit_p.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2013 Ivan Komissarov.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QKEYSEQUENCEEDIT_P_H
 #define QKEYSEQUENCEEDIT_P_H
