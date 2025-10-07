4960f0912a258ee04a9685513416c8dd4b3443c2
Ahmad Samir <a.samirh78@gmail.com>
2025-10-07 19:31:44 +0300

    qiostextinputoverlay plugin: port away from QPair
    Pick-to: 6.8 6.5
    Task-number: QTBUG-115841
    Change-Id: I0dbad5d7ec6dec6bbd5de40bb4872a0de54da3d1
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 804d13e80fb9b34c11569c98bda05d89398980ca)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/ios/qiostextinputoverlay.mm
+++ src/plugins/platforms/ios/qiostextinputoverlay.mm
@@ -17,7 +17,9 @@
 #include "qioswindow.h"
 #include "quiview.h"
 
-typedef QPair<int, int> SelectionPair;
+#include <utility> // for std::pair
+
+typedef std::pair<int, int> SelectionPair;
 typedef void (^Block)(void);
 
 static const CGFloat kKnobWidth = 10;
@@ -33,7 +35,7 @@ static SelectionPair querySelection()
     QGuiApplication::sendEvent(QGuiApplication::focusObject(), &query);
     int anchorPos = query.value(Qt::ImAnchorPosition).toInt();
     int cursorPos = query.value(Qt::ImCursorPosition).toInt();
-    return qMakePair(anchorPos, cursorPos);
+    return {anchorPos, cursorPos};
 }
 
 static bool hasSelection()
