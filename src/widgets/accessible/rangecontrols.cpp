37ad27a593b59f60a5c6e2377224abecea705e9e
Morten Sørvig <morten.sorvig@qt.io>
2025-10-09 21:37:05 +0200

    a11y: use correct property for minimumStepSize()
    QSpinBox has singleStep, not stepSize.
    
    Pick-to: 6.8
    Change-Id: Ib3911f45594d967f7b80e221bdc374b84f31eff7
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
    (cherry picked from commit b0e116152edd438adb54b5647a0811daf4d118fa)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 7e89b78cd4c641f82f0bb162ae43a8e2ee7f97f3)

--- src/widgets/accessible/rangecontrols.cpp
+++ src/widgets/accessible/rangecontrols.cpp
@@ -115,7 +115,7 @@ QVariant QAccessibleAbstractSpinBox::minimumValue() const
 
 QVariant QAccessibleAbstractSpinBox::minimumStepSize() const
 {
-    return abstractSpinBox()->property("stepSize");
+    return abstractSpinBox()->property("singleStep");
 }
 
 void QAccessibleAbstractSpinBox::addSelection(int startOffset, int endOffset)
