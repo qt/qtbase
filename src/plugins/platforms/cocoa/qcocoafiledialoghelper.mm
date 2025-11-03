253fd0a95f7962ede524ff1d6ddf5c3778191785
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2025-11-03 18:09:19 +0100

    macOS: NFC normalize selected file in QNSOpenSavePanelDelegate
    Apple systems with HFS+ were traditionally storing file names in NFD
    normalized form, while Qt prefers to operate in NFC, as that is more
    likely to match what the user types with the keyboard.
    
    Nowadays APFS is truly normalization preserving, in that it stores
    the filename in whatever normalization the filename was written as,
    but this still means we might encounter filenames in NFD form, so
    we keep normalizing file paths to ensure Qt and user code can do
    file name comparisons without needing to deal with potentially
    different normalizations.
    
    Pick-to: 6.8 6.5
    Task-number: QTBUG-122624
    Change-Id: Iaed71c8194b44c82b21a0bb3ff34fdd81c8325bd
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit bb8f10af794001ecad0db5c3b3120c677edad218)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/cocoa/qcocoafiledialoghelper.mm
+++ src/plugins/platforms/cocoa/qcocoafiledialoghelper.mm
@@ -490,7 +490,7 @@ typedef QSharedPointer<QFileDialogOptions> SharedPointerFileDialogOptions;
         return;
 
     if (m_panel.visible) {
-        const QString selection = QString::fromNSString(m_panel.URL.path);
+        const QString selection = QString::fromNSString(m_panel.URL.path).normalized(QString::NormalizationForm_C);
         if (selection != m_currentSelection) {
             m_currentSelection = selection;
             emit m_helper->currentChanged(QUrl::fromLocalFile(selection));
