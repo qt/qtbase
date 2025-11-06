ba310cbdda05af7ec771e5ed1631d29dd809a334
Thierry Bastian <thierryb@filewave.com>
2025-11-06 12:08:37 +0100

    Fix build with MSVC2022 17.14.19
    When building it errors out with:
    C:/gitlab/qt/qtbase/src/gui/text/qcssparser.cpp(121): error C2872: 'Font': ambiguous symbol
    C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\comdef.h(489): note: could be 'Font'
    
    Because comdef.h has a Font class.
    
    Fixes: QTBUG-141755
    Fixes: QTBUG-146146
    Pick-to: 6.12 6.11 6.8
    Change-Id: I7d86ee8bd367c120cf32d5c8142a2aaebef347cd
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>

--- src/gui/text/qcssparser.cpp
+++ src/gui/text/qcssparser.cpp
@@ -117,7 +117,7 @@ static constexpr QCssKnownValue properties[] = {
     { "bottom", Bottom },
     { "color", Color },
     { "float", Float },
-    { "font", Font },
+    { "font", Property::Font },
     { "font-family", FontFamily },
     { "font-kerning", FontKerning },
     { "font-size", FontSize },
@@ -405,7 +405,7 @@ static quint64 findKnownValue(const QString &name, const C &arr)
 static inline bool isInheritable(Property propertyId)
 {
     switch (propertyId) {
-    case Font:
+    case Property::Font:
     case FontKerning:
     case FontFamily:
     case FontSize:
