6f243b2e1dc940a52cde52827d89b3dccb8ac53c
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-11-07 21:58:36 +0100

    QStyleSheetStyle: don't call base implementation for PE_Widget
    Don't call the base implementation for PE_Widget for an
    QAbstractScrollArea when we have rules for it to avoid overriding of the
    style paintings by the base class.
    This was not visible until the windows11 style reimplemented the drawing
    for PE_Widget with 072bf40fe9597bdd910bb0c1e6a754c1a468b844.
    
    Pick-to: 6.8
    Fixes: QTBUG-141819
    Change-Id: I6e90dc6cb3b1a7151815d57d790c67317a97d589
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 65ffcb48421c8a75f7a088b74aadef2ad557802b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/styles/qstylesheetstyle.cpp
+++ src/widgets/styles/qstylesheetstyle.cpp
@@ -4661,7 +4661,14 @@ void QStyleSheetStyle::drawPrimitive(PrimitiveElement pe, const QStyleOption *op
 #if QT_CONFIG(scrollarea)
         if (const QAbstractScrollArea *sa = qobject_cast<const QAbstractScrollArea *>(w)) {
             const QAbstractScrollAreaPrivate *sap = sa->d_func();
-            rule.drawBackground(p, opt->rect, sap->contentsOffset());
+            bool callBaseClass = true;
+            if (rule.hasBackground()) {
+                if (rule.baseStyleCanDraw())
+                    baseStyle()->drawPrimitive(pe, opt, p, w);
+                else
+                    rule.drawBackground(p, opt->rect, sap->contentsOffset());
+                callBaseClass = false;
+            }
             if (rule.hasBorder()) {
                 QRect brect = rule.borderRect(opt->rect);
                 if (styleHint(QStyle::SH_ScrollView_FrameOnlyAroundContents, opt, w)) {
@@ -4670,7 +4677,10 @@ void QStyleSheetStyle::drawPrimitive(PrimitiveElement pe, const QStyleOption *op
                     brect = QStyle::visualRect(opt->direction, brect, r);
                 }
                 rule.drawBorder(p, brect);
+                callBaseClass = false;
             }
+            if (!callBaseClass)
+                return;
             break;
         }
 #endif
