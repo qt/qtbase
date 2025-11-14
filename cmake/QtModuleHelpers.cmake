de98a464783f647698257cc99312d782cf8d51c2
Alexandru Croitor <alexandru.croitor@qt.io>
2025-11-14 21:16:07 +0100

    CMake: Add options to skip generation of some module files
    Some qt_internal_add_module calls create INTERFACE libraries just to
    carry information like recorded feature values.
    
    These libraries should not have additional files like pkg-config
    files, qmake .pri and .prl files, or module json files.
    
    Add a few new options to qt_internal_add_module to allow disabling the
    generation of these files on a per-module basis.
    
    Pick-to: 6.8
    Change-Id: I471a8a71fe5a94235dcb0ec9efc69ef14658a1ac
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 983a6b9525f4d59881b8b935f8f28047e280fd47)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- cmake/QtModuleHelpers.cmake
+++ cmake/QtModuleHelpers.cmake
@@ -19,6 +19,9 @@ macro(qt_internal_get_internal_add_module_keywords option_args single_args multi
         NO_HEADERSCLEAN_CHECK
         NO_GENERATE_CPP_EXPORTS
         NO_UNITY_BUILD
+        NO_PACKAGE_CONFIG_FILE
+        NO_MODULE_JSON_FILE
+        NO_QMAKE_SUPPORT_FILES
         ${__qt_internal_sbom_optional_args}
     )
     set(${single_args}
@@ -1017,7 +1020,9 @@ set(QT_ALLOW_MISSING_TOOLS_PACKAGES TRUE)")
         unset(arg_NO_PRIVATE_MODULE)
     endif()
 
-    qt_describe_module(${target})
+    if(NOT arg_NO_MODULE_JSON_FILE)
+        qt_describe_module(${target})
+    endif()
 
     if(QT_GENERATE_SBOM)
         set(sbom_args "")
@@ -1072,7 +1077,19 @@ set(QT_ALLOW_MISSING_TOOLS_PACKAGES TRUE)")
         qt_internal_extend_qt_entity_sbom(${target} ${sbom_args})
     endif()
 
-    qt_add_list_file_finalizer(qt_finalize_module ${target} ${arg_INTERNAL_MODULE} ${arg_NO_PRIVATE_MODULE})
+    if(arg_NO_PACKAGE_CONFIG_FILE)
+        set_target_properties("${target}" PROPERTIES _qt_no_package_config_file TRUE)
+    endif()
+
+    if(arg_NO_QMAKE_SUPPORT_FILES)
+        set_target_properties("${target}" PROPERTIES _qt_no_qmake_support_files TRUE)
+    endif()
+
+    qt_add_list_file_finalizer(qt_finalize_module
+        ${target}
+        ${arg_INTERNAL_MODULE}
+        ${arg_NO_PRIVATE_MODULE}
+    )
 endfunction()
 
 # Write and install the basic Qt6Foo and Qt6FooPrivate packages.
@@ -1270,9 +1287,18 @@ function(qt_finalize_module target)
     )
 
     qt_finalize_framework_headers_copy(${target})
-    qt_generate_prl_file(${target} "${INSTALL_LIBDIR}")
-    qt_generate_module_pri_file("${target}" ${ARGN})
-    qt_internal_generate_pkg_config_file(${target})
+
+    get_target_property(no_qmake_support_files "${target}" _qt_no_qmake_support_files)
+    if(NOT no_qmake_support_files)
+        qt_generate_prl_file(${target} "${INSTALL_LIBDIR}")
+        qt_generate_module_pri_file("${target}" ${ARGN})
+    endif()
+
+    get_target_property(no_package_config_file "${target}" _qt_no_package_config_file)
+    if(NOT no_package_config_file)
+        qt_internal_generate_pkg_config_file(${target})
+    endif()
+
     qt_internal_apply_apple_privacy_manifest(${target})
     _qt_internal_finalize_sbom(${target})
 endfunction()
