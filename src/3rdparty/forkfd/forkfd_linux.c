097542cd2cc4340bebac0c287fd75365c57d8afa
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2025-11-14 17:34:52 +0100

    forkfd: Add support for Linux SPARC
    Both SPARC and SPARC64 aren't CLONE_BACKWARDS architectures.
    
    Pick-to: 6.8 6.5
    Signed-off-by: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Change-Id: Ied2cba4aef923149a5d3fffd678e65e624116f6c
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit a1394b18f68e5338ca3c41f5b9ac0f69fd014ff0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/3rdparty/forkfd/forkfd_linux.c
+++ src/3rdparty/forkfd/forkfd_linux.c
@@ -85,7 +85,7 @@ static int sys_clone(unsigned long cloneflags, int *ptid)
 #elif defined(__arc__) || defined(__arm__) || defined(__aarch64__) || defined(__mips__) || \
     defined(__nds32__) || defined(__hppa__) || defined(__powerpc__) || defined(__i386__) || \
     defined(__x86_64__) || defined(__xtensa__) || defined(__alpha__) || defined(__riscv) || \
-    defined(__loongarch__)
+    defined(__loongarch__) || defined(__sparc__)
     /* ctid and newtls are inverted on CONFIG_CLONE_BACKWARDS architectures,
      * but since both values are 0, there's no harm. */
     return syscall(__NR_clone, cloneflags, child_stack, ptid, ctid, newtls);
