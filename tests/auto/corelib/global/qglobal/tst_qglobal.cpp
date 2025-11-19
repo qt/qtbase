1050c5388aa5138d1df6bc86a138533659dd8e3c
Marc Mutz <marc.mutz@qt.io>
2025-11-19 16:54:59 +0100

    Add macro warning suppression to QT_WARNING_DISABLE_DEPRECATED
    When deprecating macros using #pragma (clang) deprecated(), Clang and
    MSVC use a different warning flag than for your usual deprecated
    declaration. In order to suppress those in a cross-platform way, add
    them to the existing QT_WARNING_DISABLE_DEPRECATED category.
    
    Add a check to tst_QGlobal that can also be used to test other
    suppressions: compile a specific TU always with -Werror. The test
    succeeds if the suppression masks the warnings that would otherwise
    have fell prey of -Werror.
    
    Pick to all active branches, to allow cherry-picking new code that
    uses these to enjoy warning suppression in the target branches,
    too. We also have other warning suppression macros that could be
    tested similarly.
    
    Task-number: QTBUG-99060
    Pick-to: 6.11 6.10 6.8 6.5
    Change-Id: I28186e6aaace8fb297a85d27cd41e7a979c32422
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- tests/auto/corelib/global/qglobal/tst_qglobal.cpp
+++ tests/auto/corelib/global/qglobal/tst_qglobal.cpp
@@ -91,6 +91,7 @@ class tst_QGlobal: public QObject
 private slots:
     void accessibleStateBC();
     void cMode();
+    void warnings() { extern void warnings_linker_hook(); warnings_linker_hook(); }
     void qIsNull();
     void for_each();
     void qassert();
---
+++ tests/auto/corelib/global/qglobal/warnings.cpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <QtCore/qobject.h>
+
+namespace DisableDeprecatedMacro {
+class Object : public QObject
+{
+    Q_OBJECT
+public:
+    using QObject::QObject;
+    enum E { A, B, C, D };
+    enum F { None = 0x0, One = 0x1, Two = 0x2, Three = 0x4 };
+    Q_DECLARE_FLAGS(Flags, F)
+    QT_WARNING_PUSH
+    QT_WARNING_DISABLE_DEPRECATED
+    Q_ENUMS(E)
+    Q_FLAGS(Flags)
+    QT_WARNING_POP
+};
+} // namespace DisableDeprecatedMacro
+
+extern void warnings_linker_hook()
+{
+    [[maybe_unused]] DisableDeprecatedMacro::Object o;
+}
+
+#include "warnings.moc"
