2d9bcfd3e32c523c1235b0c3e50368b630802c0a
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-11-23 13:45:45 +0100

    Windows11Style: adjust drawing focus line
    The focus line for an edit widget is 2 px width. This means we must
    adjust the rect to not draw outside the rect.
    
    Pick-to: 6.10
    Change-Id: I6e48b6ada7762c2a70ab50dcef70afcc421ee421
    Reviewed-by: Wladimir Leuschner <wladimir.leuschner@qt.io>
    (cherry picked from commit ff93e89e1e931fee20e872a39004e0f5302f79ca)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/styles/modernwindows/qwindows11style.cpp
+++ src/plugins/styles/modernwindows/qwindows11style.cpp
@@ -3123,15 +3123,20 @@ void QWindows11Style::drawLineEditFrame(QPainter *p, const QRectF &rect, const Q
     if (!isEditable || StyleOptionHelper::isDisabled(o))
         return;
 
-    QPainterStateGuard psg(p);
-    p->setClipRect(rect.marginsRemoved(QMarginsF(0, rect.height() - 0.5, 0, -1)));
     const bool hasFocus = o->state & State_HasFocus;
     const auto underlineCol = hasFocus
             ? o->palette.color(QPalette::Accent)
             : colorSchemeIndex == 0 ? QColor(0x80, 0x80, 0x80)
                                     : QColor(0xa0, 0xa0, 0xa0);
-    const auto penUnderline = QPen(underlineCol, hasFocus ? 2 : 1);
-    drawRoundedRect(p, rect, penUnderline, Qt::NoBrush);
+    QPainterStateGuard psg(p);
+    if (hasFocus) {
+        p->setClipRect(rect.marginsRemoved(QMarginsF(0, rect.height() - 1.5, 0, -1)));
+        const QRectF r(rect.top(), rect.left(), rect.width(), rect.height() - 1);
+        drawRoundedRect(p, r, QPen(underlineCol, 2), Qt::NoBrush);
+    } else {
+        p->setClipRect(rect.marginsRemoved(QMarginsF(0, rect.height() - 0.5, 0, -1)));
+        drawRoundedRect(p, rect, underlineCol, Qt::NoBrush);
+    }
 }
 
 QColor QWindows11Style::winUI3Color(enum WINUI3Color col) const
