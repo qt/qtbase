d54dd369f1d841e773fe9be0e4be09b96b502773
Morten Sørvig <morten.sorvig@qt.io>
2025-11-26 16:08:04 +0100

    Add check for -Wcharacter-conversion support
    Use __has_warning() to check if -Wcharacter-conversion
    is supported before disabling it, in order to avoid
    "unknown warning" warnings.
    
    This is/was a problem when building with Emscripten 4.0.7,
    which uses a git snapshot of Clang 21 which does not
    support the warning.
    
    Amends f20f12baab1f40ec0aad2635186e7d270139509b.
    
    Change-Id: I4c08e69013b36b87709295d9a3a79d8986078ef1
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit dd4864fbbda4b23ef93d154c18babb4b7ede9af9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/global/qcompilerdetection.h
+++ src/corelib/global/qcompilerdetection.h
@@ -1446,7 +1446,7 @@ QT_WARNING_DISABLE_MSVC(4355) /* 'this' : used in base member initializer list *
 QT_WARNING_DISABLE_MSVC(4710) /* function not inlined */
 QT_WARNING_DISABLE_MSVC(4530) /* C++ exception handler used, but unwind semantics are not enabled. Specify /EHsc */
 #  elif defined(Q_CC_CLANG_ONLY)
-#    if Q_CC_CLANG >= 2100
+#    if __has_warning("-Wcharacter-conversion")
         QT_WARNING_DISABLE_CLANG("-Wcharacter-conversion") /* until https://github.com/llvm/llvm-project/issues/163719 is fixed */
 #    endif
 #  elif defined(Q_CC_BOR)
