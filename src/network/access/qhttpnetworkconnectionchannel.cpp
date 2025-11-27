06c15bfc543867ec075eb8b1eda5728c04453fc9
Mårten Nordheim <marten.nordheim@qt.io>
2025-11-27 13:04:40 +0100

    Http: properly handle a connection: close state when server disconnects
    We already somewhat do this, but possibly due to version differences the
    path in _q_error is not reached on certain Windows systems.
    
    Add the same path checking for 'no content-length specified' in the
    slot to handle the socket disconnected() signal, though unlike the
    _q_error path we don't need to do a queued invoke - the socket is
    definitely already disconnected.
    
    Pick-to: 6.8
    Change-Id: I65bcab18bd3de80c26a1a5f88e8ffd2840cfa291
    Reviewed-by: Øystein Heskestad <oystein.heskestad@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>
    (cherry picked from commit 949bebaec69d908ca842e8ef7d3e3800d8c2f7b5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/network/access/qhttpnetworkconnectionchannel.cpp
+++ src/network/access/qhttpnetworkconnectionchannel.cpp
@@ -855,6 +855,10 @@ void QHttpNetworkConnectionChannel::_q_disconnected()
             state = QHttpNetworkConnectionChannel::ReadingState;
             _q_receiveReply();
         }
+    } else if (reply && reply->contentLength() == -1 && !reply->d_func()->isChunked()) {
+        // There was no content-length header and it's not chunked encoding,
+        // so this is a valid way to have the connection closed by the server
+        _q_receiveReply();
     } else if (state == QHttpNetworkConnectionChannel::IdleState && resendCurrent) {
         // re-sending request because the socket was in ClosingState
         QMetaObject::invokeMethod(connection, "_q_startNextRequest", Qt::QueuedConnection);
