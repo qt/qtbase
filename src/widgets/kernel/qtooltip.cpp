cb2f8195e1a85ab3576191db6dc04ec35116dd54
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2025-12-05 16:54:15 +0100

    QToolTip: misc cleanup
    Remove some unneeded headers and avoid two useless conversions from/to
    QPoint(F).
    
    Pick-to: 6.8
    Change-Id: Ieae6df74e4bd2bdba9498f86ec490520408e4c20
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit d195c219961faf944f5f5914f85abfddc39ec8ba)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 521abd25399f64e455d3d378b72582341e08b571)

--- src/widgets/kernel/qtooltip.cpp
+++ src/widgets/kernel/qtooltip.cpp
@@ -6,27 +6,21 @@
 
 #include <qapplication.h>
 #include <qevent.h>
-#include <qpointer.h>
 #include <qstyle.h>
 #include <qstyleoption.h>
 #include <qstylepainter.h>
 #if QT_CONFIG(effects)
 #include <private/qeffects_p.h>
 #endif
-#include <qtextdocument.h>
-#include <qdebug.h>
 #include <qpa/qplatformscreen.h>
 #include <qpa/qplatformcursor.h>
 #if QT_CONFIG(style_stylesheet)
 #include <private/qstylesheetstyle_p.h>
 #endif
 
-#include <qlabel.h>
 #include <QtWidgets/private/qlabel_p.h>
 #include <QtGui/private/qhighdpiscaling_p.h>
 #include <qtooltip.h>
-
-#include <QtCore/qbasictimer.h>
 #include <QtWidgets/private/qtooltip_p.h>
 
 QT_BEGIN_NAMESPACE
@@ -188,10 +182,10 @@ void QTipLabel::resizeEvent(QResizeEvent *e)
 void QTipLabel::mouseMoveEvent(QMouseEvent *e)
 {
     if (!rect.isNull()) {
-        QPoint pos = e->globalPosition().toPoint();
+        QPointF pos = e->globalPosition();
         if (widget)
             pos = widget->mapFromGlobal(pos);
-        if (!rect.contains(pos))
+        if (!rect.contains(pos.toPoint()))
             hideTip();
     }
     QLabel::mouseMoveEvent(e);
