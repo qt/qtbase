20356361bf1f6504af2892e9496bdcbc9d2cf908
Oliver Wolff <oliver.wolff@qt.io>
2025-12-11 15:33:33 +0100

    QUIP 23: Add security marks for pointer helpers in src/corelib/tools
    These files are significant. There is no parsing or cryptagraphy
    involved in any of these.
    
    QUIP: 23
    Task-number: QTBUG-135198
    Pick-to: 6.10 6.9 6.8
    Change-Id: I8bd44b1ccde973bd339fa4cbb55dc97f509419ca
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit e1d3328a0afdd4322aa6c930620b47c50dd506c8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/tools/qsharedpointer.cpp
+++ src/corelib/tools/qsharedpointer.cpp
@@ -2,6 +2,7 @@
 // Copyright (C) 2020 Intel Corporation.
 // Copyright (C) 2019 Klarälvdalens Datakonsult AB.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "qsharedpointer.h"
 
