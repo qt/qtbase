d47100c5382a88d30b0fb150da31dee48ab8fd76
Oliver Wolff <oliver.wolff@qt.io>
2025-12-11 10:25:45 +0100

    QUIP 23: Add security marks for container related files in src/corelib/tools
    All of these container related classes are marked significant.
    
    Even though QHash uses AES, the source code specifically mentions that
    this usage is not cryptography.
    
    While some of the container functions access data (for example in
    remove_if or count) these functions do not parse data. They only check
    for equality.
    
    QUIP: 23
    Task-number: QTBUG-135198
    Pick-to: 6.10 6.9 6.8
    Change-Id: I84764b75676eb786f03af4e68f84ac4a9d9886eb
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit 3146a5b8ebfb37e736789ae45aea5f2eae4b0c32)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/tools/qarraydata.cpp
+++ src/corelib/tools/qarraydata.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // Copyright (C) 2016 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include <QtCore/qalloc.h>
 #include <QtCore/qarraydata.h>
--- src/corelib/tools/qhash.cpp
+++ src/corelib/tools/qhash.cpp
@@ -2,6 +2,7 @@
 // Copyright (C) 2021 Intel Corporation.
 // Copyright (C) 2012 Giuseppe D'Angelo <dangelog@gmail.com>.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 // for rand_s, _CRT_RAND_S must be #defined before #including stdlib.h.
 // put it at the beginning so some indirect inclusion doesn't break it
