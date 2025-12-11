b4df296aab9debddd7380a4e3774b855d2898757
Oliver Wolff <oliver.wolff@qt.io>
2025-12-11 10:25:45 +0100

    QUIP 23: Add security marks for container related files in src/corelib/tools
    All of these container related classes are marked significant.
    
    Even though QHash uses AES, the source code specifically mentions that
    this usage is not cryptography.
    
    While some of the container functions access data (for example in
    remove_if or count) these functions do not parse data. They only check
    for equality.
    
    QUIP: 23
    Task-number: QTBUG-135198
    Pick-to: 6.9 6.8
    Change-Id: I84764b75676eb786f03af4e68f84ac4a9d9886eb
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit 3146a5b8ebfb37e736789ae45aea5f2eae4b0c32)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit d47100c5382a88d30b0fb150da31dee48ab8fd76)

--- src/corelib/tools/qhashfunctions.h
+++ src/corelib/tools/qhashfunctions.h
@@ -2,6 +2,7 @@
 // Copyright (C) 2015 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Marc Mutz <marc.mutz@kdab.com>
 // Copyright (C) 2024 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QHASHFUNCTIONS_H
 #define QHASHFUNCTIONS_H
--- src/corelib/tools/qmap.h
+++ src/corelib/tools/qmap.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2020 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QMAP_H
 #define QMAP_H
