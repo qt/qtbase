7443777005414d558c1417f2d3dd1559b8fa0686
Oliver Wolff <oliver.wolff@qt.io>
2025-12-12 10:44:10 +0100

    QUIP 23: Add security marks for crypto related files in src/corelib/tools
    These files do have a connection to cryptographic functionality so mark
    these as such.
    
    QUIP: 23
    Task-number: QTBUG-135198
    Pick-to: 6.9 6.8
    Change-Id: I4456f2aa9586369386d6cb672222b789a3620e33
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit 174bf5017acf703325d91552b8fd39b1f436fe92)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5508416d1c2928000d840da312a2ecee292d1bbc)

--- src/corelib/tools/qcryptographichash.h
+++ src/corelib/tools/qcryptographichash.h
@@ -2,6 +2,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2013 Richard J. Moore <rich@kde.org>.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:cryptography
 
 #ifndef QCRYPTOGRAPHICHASH_H
 #define QCRYPTOGRAPHICHASH_H
