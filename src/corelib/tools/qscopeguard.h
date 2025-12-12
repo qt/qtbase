d999b25a9782614cd05b09c6657fa946f22ae3e7
Oliver Wolff <oliver.wolff@qt.io>
2025-12-12 10:46:18 +0100

    QUIP 23: Add security marks for misc files in src/corelib/tools
    This commit covers files in src/corelib/tools which are not covered by
    the other category commits.
    
    Most of the files are significant because they do not involve data
    parsing or cryptography. The exceptions are qversionnumber and
    qtyperevision because they offer data parsing from strings.
    
    QUIP: 23
    Task-number: QTBUG-135198
    Pick-to: 6.9 6.8
    Change-Id: If1c31c1216e49ea6fde0a767f096da367b55491b
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit f6a1e0449f6645d814bfb076df68c6e6dbb87457)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 9aa932cd70506018d2a560916cf894505889f7e8)

--- src/corelib/tools/qscopeguard.h
+++ src/corelib/tools/qscopeguard.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Sérgio Martins <sergio.martins@kdab.com>
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QSCOPEGUARD_H
 #define QSCOPEGUARD_H
--- src/corelib/tools/qshareddata_impl.h
+++ src/corelib/tools/qshareddata_impl.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2020 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #if 0
 #pragma qt_sync_skip_header_check
