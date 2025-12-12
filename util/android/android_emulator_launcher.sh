f9e9e1f1a566511d663b32e422f7b054713e5112
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-12-12 21:46:57 +0200

    Replace the commercial SPDX identifier with an LGPL SPDX identifier
    Replaced the commercial SPDX license identified with an LGPL
    SPDX identifier. This is needed for the Qt 6.5 opensource releases.
    
    Change-Id: Ib2ee0e0e9be9a4bcfed90ae864af1b9b6f71a0a7
    Reviewed-by: Tarja Sundqvist <tarja.sundqvist@qt.io>

--- util/android/android_emulator_launcher.sh
+++ util/android/android_emulator_launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (C) 2024 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 # This util launches the Android emulator and ensures it doesn't stuck/freeze
 # by detecting that and restarting it
