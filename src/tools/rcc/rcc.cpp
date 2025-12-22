c56536af6b32566916422699fd2d597156e1ef6b
Marc Mutz <marc.mutz@qt.io>
2025-12-22 07:26:36 +0100

    rcc: add another missing std::move()
    Coverity complained that the 'pwd' variable could have been moved into
    the third 'interpretResourceFile' parameter, but was instead copied.
    
    It is correct. Add the std::move(), which doesn't impair readability,
    because it appears near the end of the scope in which 'pwd' is
    defined.
    
    Amends the start of the public history.
    
    Coverity-Id: 427453
    Pick-to: 6.8
    Change-Id: I292a33f2a561d61635ba34876abd8e9b21d839f7
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit 15fd587515535ce93928e802c8ce3bcfa6f23247)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 98c0011c23d26f24cd5ce0a2d74c36ab854bf994)

--- src/tools/rcc/rcc.cpp
+++ src/tools/rcc/rcc.cpp
@@ -817,7 +817,7 @@ bool RCCResourceLibrary::readFiles(bool listMode, QIODevice &errorDevice)
             m_errorDevice->write(msg.toUtf8());
         }
 
-        if (!interpretResourceFile(&fileIn, fname, pwd, listMode))
+        if (!interpretResourceFile(&fileIn, fname, std::move(pwd), listMode))
             return false;
     }
     return true;
