328fb6f6698e89dce0e089f10c8c6800edf03b10
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2026-01-03 14:08:05 +0100

    QCommonStyle: correct subControlRect(CC_GroupBox) for rect.top != 0
    QCommonStyle::subControlRect(CC_GroupBox) is setting an offset to the
    top coordinate without honoring the previous top which lead to a wrong
    coordinate when top is != 0.
    
    Pick-to: 6.10
    Fixes: QTBUG-34103
    Change-Id: I4ea4307eda0f651cd9af8f01a366f443e798fc82
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>
    (cherry picked from commit c8cdde6dd3b245dd0ab15229af243a0928994f05)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/widgets/styles/qcommonstyle.cpp
+++ src/widgets/styles/qcommonstyle.cpp
@@ -4359,7 +4359,7 @@ QRect QCommonStyle::subControlRect(ComplexControl cc, const QStyleOptionComplex 
                 }
 
                 QRect frameRect = groupBox->rect;
-                frameRect.setTop(topMargin);
+                frameRect.setTop(frameRect.top() + topMargin);
 
                 if (sc == SC_GroupBoxFrame) {
                     ret = frameRect;
