1552fef04f2c72088585b4560ea138a6741fbfc9
Morten Sørvig <morten.sorvig@qt.io>
2026-01-05 16:16:31 +0100

    wasm: don't add empty url lists to QMimeData
    Doing so will make QMimeData claim it supports the
    "text/uri-list" data type, even if it does not actually
    contain any URLs
    
    Change-Id: I637398cb0a0a871d2199de8ed19655b3ee4f3c40
    Reviewed-by: Lorn Potter <lorn.potter@qt.io>
    (cherry picked from commit e21f1898539268b50f5158a49e6e658fe07fbeb6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/wasm/qwasmdom.cpp
+++ src/plugins/platforms/wasm/qwasmdom.cpp
@@ -251,7 +251,9 @@ QMimeData *DataTransfer::toMimeDataPreview()
             data->setData(itemMimeType, QByteArray());
         }
     }
-    data->setUrls(uriList);
+    if (!uriList.isEmpty())
+        data->setUrls(uriList);
+
     return data;
 }
 
