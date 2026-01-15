0fda451034b86e6605450253445ff7184fd3e8cb
Dennis Oberst <dennis.oberst@qt.io>
2026-01-15 11:18:40 +0100

    Update tika-mimetypes.xml from upstream and improve tooling
    Update the Tika MIME types definition file to version
    408c26e1e03e018a623e732dff6fb047a2fb8e19.
    
    Fix the 'sed' command in 'update_tika_from_upstream.sh' to work
    with both GNU and BSD versions.
    
    Fix 'process_tika_mimetypes.py' to handle skipping of
    'minShouldMatch=' correctly, only commenting out the matching XML
    block. Previously the script skipped to the next '</magic>', which
    didn't work for cases like the image/bmp magic rules with nested
    minShouldMatch elements:
    
      <mime-type type="image/bmp">
        ~~~
        <magic priority="50">
          <match value="BM" type="string" offset="0">
            <match value="0x0100" type="string" offset="26">
              <match minShouldMatch="2">
                <match minShouldMatch="1">
                  <match value="0x0000" type="string" offset="28"/>
                  ~~~
                </match>
                <match value="0x00000000" type="string" offset="30"/>
              </match>
            </match>
          </match> -> previously skipped until here, which is invalid XML
        </magic>
      </mime-type>
    
    [ChangeLog][Third-Party Code] Updated TIKA mimetypes from upstream
    
    Pick-to: 6.8 6.5
    Task-number: QTBUG-143128
    Change-Id: I2855f58c433b4fbf17a229074d9c1fc362f399a6
    Reviewed-by: David Faure <david.faure@kdab.com>
    (cherry picked from commit 86127bba7fb5502510947942e1d43fac689802f3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit bd6714f2c75cf641d22aedf0ab87a902cc424f5d)

--- src/corelib/mimetypes/3rdparty/update_tika_from_upstream.sh
+++ src/corelib/mimetypes/3rdparty/update_tika_from_upstream.sh
@@ -19,9 +19,18 @@ wget "$downloadURL" -O tika-mimetypes.xml.orig
 
 # Update attribution file
 jsonfile="qt_attribution.json"
-sed -i -E "s/(\"Version\":\s+\")[a-f0-9]+(\")/\1$sha1\2/" "$jsonfile"
-sed -i -E "s|(https://github.com/apache/tika/blob/)[a-f0-9]+(/tika-core/src/main/resources/org/apache/tika/mime/tika-mimetypes.xml)|\1$sha1\2|" "$jsonfile"
+
+sed_inplace() {
+    if sed --version 2>/dev/null | grep -q GNU; then
+        sed -i "$@" # GNU - Linux
+    else
+        sed -i '' "$@" # BSD - macOS
+    fi
+}
+
+sed_inplace -E 's/("Version":[[:space:]]+")[a-f0-9]+(")/\1'"$sha1"'\2/' "$jsonfile"
+sed_inplace -E 's|(https://github.com/apache/tika/blob/)[a-f0-9]+(/tika-core/src/main/resources/org/apache/tika/mime/tika-mimetypes.xml)|\1'"$sha1"'\2|' "$jsonfile"
 year=$(date +%Y)
-sed -i -E "s/(\"Copyright\":\s+\"Copyright )[0-9]{4}/\1$year/" "$jsonfile"
+sed_inplace -E 's/("Copyright":[[:space:]]+"Copyright )[0-9]{4}/\1'"$year"'/' "$jsonfile"
 
 echo "Remember to run tst_QMimeDatabase now, at least to update the total mimetype count"
