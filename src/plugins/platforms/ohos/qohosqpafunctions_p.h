c114af68cd1fdeb98f0675b0fba25c3427660472
Bartlomiej Neumann <bartlomiej.neumann@qt.io>
2026-01-15 14:24:17 +0100

    ohos: Add functions to set AudioStreamUsage property
    This will serve as the main way of actually setting the properties.
    The AudioStreamUsage enum is based on the ::OH_AudioStream_Usage.
    
    Change-Id: I80bd8ea77cd2e5d673f5e5b2df4b86da30030f04
    Reviewed-by: Liang Qi <liang.qi@qt.io>
    (cherry picked from commit ffac05d506ceea936af9b0b9b7e126f58c7949ff)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/ohos/qohosqpafunctions_p.h
+++ src/plugins/platforms/ohos/qohosqpafunctions_p.h
@@ -61,6 +61,23 @@ public:
         Mismatch,
     };
 
+    enum class AudioStreamUsage {
+        Unknown,
+        Music,
+        VoiceCommunication,
+        VoiceAssistant,
+        Alarm,
+        VoiceMessage,
+        Ringtone,
+        Notification,
+        Accessibility,
+        Movie,
+        Game,
+        Audiobook,
+        Navigation,
+        VideoCommunication,
+    };
+
     struct AbilityOnContinueRequest
     {
         int sourceApplicationVersionCode;
@@ -342,6 +359,9 @@ public:
 
     virtual QObject *getActiveWindowOrNull() const = 0;
 
+    virtual void setAudioStreamUsageHintProperty(QObject *qObject, AudioStreamUsage usage) = 0;
+    virtual QOhosOptional<AudioStreamUsage> tryGetAudioStreamUsageHintProperty(QObject *qObject) = 0;
+
 protected:
     QOhosQpaFunctions();
 };
@@ -351,5 +371,7 @@ QOhosQpaFunctions &getQOhosQpaFunctions();
 }
 
 QT_END_NAMESPACE
+
+Q_DECLARE_METATYPE(QT_PREPEND_NAMESPACE(QtOhos::QOhosQpaFunctions::AudioStreamUsage));
 
 #endif // QOHOSQPAFUNCTIONS_H
