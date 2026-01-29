59842a4af4ccff9e9ccc2777e8ff09b9457d3621
Assam Boudjelthia <assam.boudjelthia@qt.io>
2026-01-29 17:18:04 +0200

    CMake Android Deployment: get rid of invalid dependency warnings
    Avoid adding invalid plugin types into the xml dependencies files.
    This fixes the warnings similar to:
    
    CMake Warning at
    qtbase/lib/cmake/Qt6Core/Qt6AndroidGradleHelpers.cmake:1146 (message):
      Qt Android deployment: dependency 'qtbase/plugins/generic' is missing
    for Qt6::Gui.
    Call Stack (most recent call first):
      qtbase/lib/cmake/Qt6Core/Qt6AndroidGradleHelpers.cmake:1277
    (_qt_internal_android_get_dependency_abs_path)
      qtbase/lib/cmake/Qt6Core/Qt6AndroidGradleHelpers.cmake:250
    (_qt_internal_android_copy_qt_dependencies)
      qtbase/lib/cmake/Qt6Core/Qt6AndroidMacros.cmake:1703
    (_qt_internal_android_prepare_gradle_build)
      qtbase/lib/cmake/Qt6Core/Qt6CoreMacros.cmake:818
    (_qt_internal_android_executable_finalizer)
      qtbase/lib/cmake/Qt6Core/Qt6CoreMacros.cmake:818 (cmake_language)
      qtbase/lib/cmake/Qt6Core/Qt6CoreMacros.cmake:896
    (_qt_internal_finalize_executable)
      qtbase/lib/cmake/Qt6Core/Qt6CoreMacros.cmake:865:EVAL:1
    (qt6_finalize_target)
      CMakeLists.txt:DEFERRED
    
    Task-number: QTBUG-137529
    Change-Id: Iba90b490cf8141873813ac78c5f06f005af8685e
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- cmake/QtAndroidHelpers.cmake
+++ cmake/QtAndroidHelpers.cmake
@@ -288,8 +288,12 @@ function(qt_internal_android_dependencies target)
     qt_internal_android_dependencies_content(${target} file_contents)
 
     # Get plugins from the module's plugin types and get their dependencies
+    set(available_plugin_types "")
     foreach(plugin ${QT_KNOWN_PLUGINS})
         get_target_property(iter_known_plugin_type ${plugin} QT_PLUGIN_TYPE)
+        if(iter_known_plugin_type)
+            list(APPEND available_plugin_types "${iter_known_plugin_type}")
+        endif()
         foreach(plugin_type ${module_plugin_types})
             if (plugin_type STREQUAL iter_known_plugin_type)
                 qt_internal_android_dependencies_content(${plugin} plugin_file_contents)
@@ -297,6 +301,7 @@ function(qt_internal_android_dependencies target)
             endif()
         endforeach()
     endforeach()
+    list(REMOVE_DUPLICATES available_plugin_types)
 
     if ((NOT module_plugin_types)
         AND (NOT file_contents))
@@ -316,9 +321,11 @@ function(qt_internal_android_dependencies target)
 
     # Module plugins
     if(module_plugin_types)
-        foreach(plugin IN LISTS module_plugin_types)
-            string(APPEND file_contents
-                "<bundled file=\"${INSTALL_PLUGINSDIR}/${plugin}\" type=\"plugin_dir\"/>\n")
+        foreach(plugin_type IN LISTS module_plugin_types)
+            if(NOT available_plugin_types OR plugin_type IN_LIST available_plugin_types)
+                string(APPEND file_contents
+                    "<bundled file=\"${INSTALL_PLUGINSDIR}/${plugin_type}\" type=\"plugin_dir\"/>\n")
+            endif()
         endforeach()
     endif()
 
