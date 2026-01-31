bc60fa052b6163bcf444dab027bd6c1e717c9845
Even Oscar Andersen <even.oscar.andersen@qt.io>
2026-01-31 08:24:28 +0100

    wasm: drag&drop - Set QMimeData in onNativeDragEnter
    Some users might look at the mimedata before accepting events.
    In particular the draggabletext example does this.
    
    Also reduce the size of the QImage/QPixmap used to
    generate the dragimage for text, to avoid running
    out of memory.
    
    Task-number: QTBUG-143194
    Pick-to: 6.11
    Change-Id: Id81b76ab51542243593a2fe6de7d4604382509d1
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>

--- src/plugins/platforms/wasm/qwasmdrag.cpp
+++ src/plugins/platforms/wasm/qwasmdrag.cpp
@@ -265,7 +265,7 @@ void QWasmDrag::onNativeDragEnter(DragEvent *event)
 
     m_isInEnterDrag = true;
     QDrag *drag = new QDrag(this);
-    drag->setMimeData(new QMimeData());
+    drag->setMimeData(event->dataTransfer.toMimeDataPreview());
     drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction);
     m_isInEnterDrag = false;
 }
@@ -336,17 +336,21 @@ void QWasmDrag::DragState::DragImage::generateDragImageFromText(const QMimeData 
 
     QRect bounds;
     {
-        QPixmap image(QSize(1000,1000));
-        QPainter painter(&image);
-        bounds = painter.boundingRect(0, 0, 1000, 1000, 0, text);
+        QPixmap image(QSize(200,200));
+        if (!image.isNull()) {
+            QPainter painter(&image);
+            bounds = painter.boundingRect(0, 0, 200, 200, 0, text);
+        }
     }
     QImage image(bounds.size(), QImage::Format_RGBA8888);
-    QPainter painter(&image);
-    painter.fillRect(bounds, QColor(255,255,255, 255)); // Transparency does not work very well :-(
-    painter.setPen(Qt::black);
-    painter.drawText(bounds, text);
+    if (!image.isNull()) {
+        QPainter painter(&image);
+        painter.fillRect(bounds, QColor(255,255,255, 255)); // Transparency does not work very well :-(
+        painter.setPen(Qt::black);
+        painter.drawText(bounds, text);
 
-    m_pixmap = QPixmap::fromImage(image);
+        m_pixmap = QPixmap::fromImage(image);
+    }
     m_imageDomElement = dragImageElement;
 }
 
