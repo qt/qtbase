70de14c9b1820a62a68fd89696e1af832943d90d
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-06 16:43:58 +0100

    QUIP-23: mark remaining Android QPA sources with default header
    Amends 8c626ad667ef50a610acc04403a908fbd068a654, which classified some
    files parsing data as critical. We generally trust the underlying
    platform, and these remaining sources don't implement interfaces through
    which data crosses a trust boundary.
    
    Mark them with the default classification.
    
    Pick-to: 6.10 6.8
    Task-number: QTBUG-135178
    Change-Id: I6f8a47d6aec49cd7212858bbd5625a0a9d897903
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry picked from commit 231a437fcd53523f09ec6ae323ca63da73c41600)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/android/qandroidplatformmenu.cpp
+++ src/plugins/platforms/android/qandroidplatformmenu.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2012 BogDan Vatra <bogdan@kde.org>
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #include "androidjnimenu.h"
 #include "qandroidplatformmenu.h"
