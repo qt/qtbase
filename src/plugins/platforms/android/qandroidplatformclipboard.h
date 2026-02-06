231a437fcd53523f09ec6ae323ca63da73c41600
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-06 16:43:58 +0100

    QUIP-23: mark remaining Android QPA sources with default header
    Amends 8c626ad667ef50a610acc04403a908fbd068a654, which classified some
    files parsing data as critical. We generally trust the underlying
    platform, and these remaining sources don't implement interfaces through
    which data crosses a trust boundary.
    
    Mark them with the default classification.
    
    Pick-to: 6.11 6.10 6.8
    Task-number: QTBUG-135178
    Change-Id: I6f8a47d6aec49cd7212858bbd5625a0a9d897903
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

--- src/plugins/platforms/android/qandroidplatformclipboard.h
+++ src/plugins/platforms/android/qandroidplatformclipboard.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // Copyright (C) 2012 BogDan Vatra <bogdan@kde.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QANDROIDPLATFORMCLIPBOARD_H
 #define QANDROIDPLATFORMCLIPBOARD_H
