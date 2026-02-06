4e38e266eca1dc2283edf5b7a3b8a4a8bcfe8514
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-06 15:59:46 +0100

    QUIP-23: add default security header to Direct2D QPA sources
    QPA plugins generally deal with data from provided by the system, which
    is at least as trusted as the Qt components processing the data. This
    particular plugin is anyway not dealing with settings files or clipboard
    data.
    
    Pick-to: 6.8
    Fixes: QTBUG-135707
    Change-Id: I871c7eaeece27aa3ee3c6cd4e838422553220f49
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
    (cherry picked from commit 6fb701751134673471bfa4b0268d93ddc3c107bc)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 231949ed3751b31dbff3f5b6a56bce1609b181e5)

--- src/plugins/platforms/direct2d/qwindowsdirect2dhelpers.h
+++ src/plugins/platforms/direct2d/qwindowsdirect2dhelpers.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QWINDOWSDIRECT2DHELPERS_H
 #define QWINDOWSDIRECT2DHELPERS_H
