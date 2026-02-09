2c332f650812283fba6ad324453895140d7f73a6
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-09 18:09:42 +0100

    QUIP-23: add default security header to RTOS QPA sources
    QPA plugins generally deal with data provided by the system, which
    is at least as trusted as the Qt components processing the data. This
    includes settings files and databases, and clipboard or drag'n'drop
    content.
    
    Pick-to: 6.11 6.8
    Change-Id: Ic785ed81750a76f903cb7d857ee9ef5c77a56c91
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/plugins/platforms/qnx/qqnxforeignwindow.h
+++ src/plugins/platforms/qnx/qqnxforeignwindow.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2018 QNX Software Systems. All rights reserved.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QQNXFOREIGNWINDOW_H
 #define QQNXFOREIGNWINDOW_H
--- src/plugins/platforms/qnx/qqnxscreen.cpp
+++ src/plugins/platforms/qnx/qqnxscreen.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2011 - 2013 BlackBerry Limited. All rights reserved.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #undef QT_NO_FOREACH // this file contains unported legacy Q_FOREACH uses
 
