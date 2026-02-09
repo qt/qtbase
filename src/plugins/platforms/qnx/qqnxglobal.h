25b0ded9109cb0a26d7585f6ba1127da2dcc2c10
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-09 18:09:42 +0100

    QUIP-23: add default security header to RTOS QPA sources
    QPA plugins generally deal with data provided by the system, which
    is at least as trusted as the Qt components processing the data. This
    includes settings files and databases, and clipboard or drag'n'drop
    content.
    
    Pick-to: 6.8
    Change-Id: Ic785ed81750a76f903cb7d857ee9ef5c77a56c91
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 2c332f650812283fba6ad324453895140d7f73a6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/qnx/qqnxglobal.h
+++ src/plugins/platforms/qnx/qqnxglobal.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2011 - 2014 BlackBerry Limited. All rights reserved.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QQNXGLOBAL_H
 #define QQNXGLOBAL_H
--- src/plugins/platforms/qnx/qqnxinputcontext_imf.h
+++ src/plugins/platforms/qnx/qqnxinputcontext_imf.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2013 BlackBerry Limited. All rights reserved.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QQNXINPUTCONTEXT_H
 #define QQNXINPUTCONTEXT_H
