84b56f7ad37a7c29f4f93bb17db4cf8dbf353d8b
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-09 16:21:46 +0100

    QUIP-23: add default security header to WASM QPA sources
    QPA plugins generally deal with data provided by the system, which
    is at least as trusted as the Qt components processing the data. This
    includes settings files and databases, and clipboard or drag'n'drop
    content.
    
    With WebAssembly, Qt is additionally running in a sandbox, where the
    exchange of data is further limited and controlled by the operator of
    the system.
    
    Pick-to: 6.10 6.8
    Change-Id: Icd5dab283b1b64de4a8c1880c77402fede0ca37a
    Reviewed-by: Piotr Wierciński <piotr.wiercinski@qt.io>
    (cherry picked from commit 12c277b1489daee1fb406ef7bc34556767cae2dd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/wasm/qwasmplatform.h
+++ src/plugins/platforms/wasm/qwasmplatform.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QWASMPLATFORM_H
 #define QWASMPLATFORM_H
--- src/plugins/platforms/wasm/qwasmwindowtreenode.h
+++ src/plugins/platforms/wasm/qwasmwindowtreenode.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QWASMWINDOWTREENODE_H
 #define QWASMWINDOWTREENODE_H
