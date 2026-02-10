c74a16b3fdfcaebeb5f22a997ccdd0c6173eddfd
Assam Boudjelthia <assam.boudjelthia@qt.io>
2026-02-10 13:06:57 +0200

    Android: Add extern "C" to JNI_OnLoad to prevent name mangling
    While not causing issues today, name mangling could lead to a problem
    in the future.
    
    Pick-to: 6.11
    Change-Id: I221afa02e4a4ba62172932f57bc57dcc7a91dd69
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/plugins/platforms/android/androidjnimain.cpp
+++ src/plugins/platforms/android/androidjnimain.cpp
@@ -889,7 +889,7 @@ static bool initJavaReferences(QJniEnvironment &env)
 
 QT_END_NAMESPACE
 
-Q_DECL_EXPORT jint JNICALL JNI_OnLoad(JavaVM */*vm*/, void */*reserved*/)
+extern "C" Q_DECL_EXPORT jint JNICALL JNI_OnLoad(JavaVM */*vm*/, void */*reserved*/)
 {
     static bool initialized = false;
     if (initialized)
