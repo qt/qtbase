7fb13ef6172e750cf453f8445f4729c1532f2ef1
Edward Welbourne <edward.welbourne@qt.io>
2026-02-12 11:48:43 +0100

    Link QBA::number(double, ...) docs directly to QLocale::toString()
    It previously delegated to QString::number(double, ...), which
    delegated to a {Number Formats} section of its \class doc (whose title
    was actually {Number formats} so a case-sensitive search didn't find
    it), which in turn delegated to QLocale::toString(double, ...).
    Skip the intermediate steps and cut to the chase.
    
    Change-Id: Ibadc0874bc9d63ca4b06a01cc2f0cede6d8e21d5
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>
    (cherry picked from commit 73c8b52d8034a567fd77eea8e09a44bd09bcc4c0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/text/qbytearray.cpp
+++ src/corelib/text/qbytearray.cpp
@@ -4405,7 +4405,7 @@ QByteArray QByteArray::number(qulonglong n, int base)
 
     Returns a byte array containing a string representing \a n, with a given \a
     format and \a precision, with the same meanings as for \l
-    {QString::number(double, char, int)}. For example:
+    {QLocale::toString(double, char, int)}. For example:
 
     \snippet code/src_corelib_text_qbytearray.cpp 42
 
