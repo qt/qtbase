5d0a01e1f16374172e7dcfb7e33768b9cda10cb8
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2026-02-15 22:18:45 +0100

    QImageReader: add a round-trip test for invalid QColorSpaces
    Make sure we don't regress this by adding a test: load an invalid
    profile, use it in an image, save the image, and load it back; check
    that we get back the same (invalid) ICC profile.
    
    The profile itself comes from the sRGB-v2-micro.icc file available in
    [1]. It has been made invalid by changing one byte:
    
      $ diff <(od -x sRGB-v2-micro.icc) <(od -x test_invalid_icc.icc)
      9c9
      < 0000200 0000 0900 6564 6373 0000 f000 0000 5f00
      ---
      > 0000200 0000 0900 6564 6373 0000 0000 0000 5f00
    
    Thanks to J.D. Purcell in the bugreport for providing the file.
    
    [1] https://github.com/saucecontrol/Compact-ICC-Profiles (CC0 license),
        as of commit bdd84663061bc4ae95ca70decff54f581e27f702.
    
    Task-number: QTBUG-125246
    Pick-to: 6.11 6.10 6.8
    Change-Id: I2cbfff0e76036029bc26f3ff33ededf99509d747
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- tests/auto/gui/image/qimagereader/tst_qimagereader.cpp
+++ tests/auto/gui/image/qimagereader/tst_qimagereader.cpp
@@ -158,6 +158,9 @@ private slots:
 
     void autoTransformSizes();
 
+    void invalidColorspaceRoundTrip_data();
+    void invalidColorspaceRoundTrip();
+
 private:
     QString prefix;
     QTemporaryDir m_temporaryDir;
@@ -2226,7 +2229,56 @@ void tst_QImageReader::autoTransformSizes()
     QCOMPARE(r2.size(), untransformedSize);
     QCOMPARE(r2.effectiveSize(), transformedSize);
     QCOMPARE(r2.read().size(), transformedSize);
+}
 
+static bool formatSupportsIccProfiles(QByteArrayView format)
+{
+    return format.compare("png", Qt::CaseInsensitive) == 0 ||
+           format.compare("jpeg", Qt::CaseInsensitive) == 0 ||
+           format.compare("tiff", Qt::CaseInsensitive) == 0;
+}
+
+void tst_QImageReader::invalidColorspaceRoundTrip_data()
+{
+    QTest::addColumn<QByteArray>("imageFormat");
+    for (const QByteArray &format : QImageWriter::supportedImageFormats()) {
+        if (formatSupportsIccProfiles(format))
+            QTest::addRow("%s", format.constData()) << format;
+    }
+}
+
+void tst_QImageReader::invalidColorspaceRoundTrip()
+{
+    QFile iccProfileFile(QFINDTESTDATA("images/test_invalid_icc.icc"));
+    QVERIFY(iccProfileFile.open(QFile::ReadOnly));
+
+    const QByteArray iccProfile = iccProfileFile.readAll();
+    const QColorSpace colorSpace = QColorSpace::fromIccProfile(iccProfile);
+    QVERIFY(!colorSpace.isValid());
+    QCOMPARE(colorSpace.iccProfile(), iccProfile);
+
+    constexpr QSize IMAGE_SIZE = {32, 32};
+
+    QImage image(IMAGE_SIZE, QImage::Format_RGB32);
+    image.fill(Qt::black);
+    image.setColorSpace(colorSpace);
+    QCOMPARE(image.colorSpace().iccProfile(), iccProfile);
+
+    QTemporaryDir dir;
+    QVERIFY(dir.isValid());
+    const QString imagePath = dir.filePath("image");
+
+    QFETCH(QByteArray, imageFormat);
+
+    QImageWriter writer(imagePath, imageFormat);
+    QVERIFY(writer.write(image));
+
+    QImageReader reader(imagePath, imageFormat);
+    QImage result;
+    QVERIFY(reader.read(&result));
+
+    QCOMPARE(result.size(), IMAGE_SIZE);
+    QCOMPARE(result.colorSpace().iccProfile(), iccProfile);
 }
 
 QTEST_MAIN(tst_QImageReader)
