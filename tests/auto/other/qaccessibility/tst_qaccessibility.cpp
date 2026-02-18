robertpelloni/bobui

commit 888af1ad02af0eab10322a23b032872d72b59fb5

a11y: Notify about property/state changes of focused item view item
Item view item data for various roles are relevant
for the properties and states reported for the
corresponding accessible objects on the accessibility
layer, see the a11y implementation in QAccessibleTableCell.

Therefore, changes to the relevant role data
should also be reported as corresponding state/property
changes on the a11y layer.

Implement sending corresponding accessibility
events from QAbstractItemView::dataChanged.
While the roles param in the QAbstractItemModel::dataChanged
signal that this slot is connected to is optional,
only handle roles that are explicitly passed for now,
which covers the case of interactively modifying items.
Properly handling the case of an empty list of roles
indicating that all roles should be considered as
(potentially) having been modified would require to
keep track of previous values to be able to only send
notifications about those that actually changed.
(One way to do that for the current item could be to
remember the values in QAbstractItemView::currentChanged
and then compare whether those have changed in the
meantime.)
Always sending accessibility events even if no change
actually occured would be incorrect and could e.g. result
in confusing/misleading screen reader announcements.

For now, in order to avoid potential negative performance
implications when doing that for all items, restrict
this to the case where (only) the current item's data
has changed, which in particular covers the case where
a user interactively modifies that item.
(If the need arises, this can be further extended as
needed in the future.)

This e.g. makes the Orca screen reader on Linux
announce changes to the checkbox state for the
QTBUG-141856 sample program when toggling it using
the space key, and results in the state getting
updated as expected in Accerciser [1]. The accessible
name shown in Accerciser also gets updated as expected

src/widgets/itemviews/qabstractitemview.cpp
@@ -172,6 +172,43 @@ void QAbstractItemViewPrivate::checkMouseMove(const QPersistentModelIndex &index
     }
 }
 
+#if QT_CONFIG(accessibility)
+void QAbstractItemViewPrivate::updateItemAccessibility(const QModelIndex &index,
+                                                       const QList<int> &roles)
+{
+    Q_Q(QAbstractItemView);
+
+    if (!QAccessible::isActive())
+        return;
+
+    const int childIndex = accessibleChildIndex(index);
+    if (childIndex < 0)
+        return;
+
+    // see QAccessibleTableCell for how role data are mapped to the a11y layer
+
+    for (int role : roles) {
+        if (role == Qt::AccessibleTextRole
+            || (role == Qt::DisplayRole
+                && index.data(Qt::AccessibleTextRole).toString().isEmpty())) {
+            QAccessibleEvent event(q, QAccessible::NameChanged);
+            event.setChild(childIndex);
+            QAccessible::updateAccessibility(&event);
+        } else if (role == Qt::AccessibleDescriptionRole) {
+            QAccessibleEvent event(q, QAccessible::DescriptionChanged);
+            event.setChild(childIndex);
+            QAccessible::updateAccessibility(&event);
+        } else if (role == Qt::CheckStateRole) {
+            QAccessible::State state;
+            state.checked = true;
+            QAccessibleStateChangeEvent event(q, state);
+            event.setChild(childIndex);
+            QAccessible::updateAccessibility(&event);
+        }
+    }
+}
+#endif
+
 #if QT_CONFIG(gestures) && QT_CONFIG(scroller)
 
 // stores and restores the selection and current item when flicking
@@ -3495,6 +3532,10 @@ void QAbstractItemView::dataChanged(const QModelIndex &topLeft, const QModelInde
         accessibleEvent.setLastRow(bottomRight.row());
         accessibleEvent.setLastColumn(bottomRight.column());
         QAccessible::updateAccessibility(&accessibleEvent);
+
+        // send accessibility events as needed when current item is modified
+        if (topLeft == bottomRight && topLeft == currentIndex())
+            d->updateItemAccessibility(topLeft, roles);
     }
 #endif
     d->updateGeometry();

src/widgets/itemviews/qabstractitemview_p.h
@@ -280,6 +280,10 @@ public:
     }
 #endif
 
+#if QT_CONFIG(accessibility)
+    void updateItemAccessibility(const QModelIndex &index, const QList<int> &roles);
+#endif
+
     // reimplemented from QAbstractScrollAreaPrivate
     QPoint contentsOffset() const override {
         Q_Q(const QAbstractItemView);

qt/qtbase

commit 162f6bfee56866ff32fd6c7a11f392b008b5d3f1

QAbstractItemView a11y: Handle invalid model indexes
This change fixes a segmentation fault that happens when
`dataChanged()` is emitted with invalid indexes.
Arguably the application has a logic bug if this ever happens,
(and that's why a nice `qWarning()` is emitted), however
the library should still not turn such a logic bug into a
segmentation fault.

This is a regression since commit 888af1ad02af0eab10322a23b032872d72b59fb5

Before that commit only a warning message would be printed, after
that commit the whole application crashes.

Fixes: QTBUG-143781
Pick-to: 6.11
Change-Id: Iaa268c4b925329a15146b959fd690f57270655c9
Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

src/testlib/qtestaccessible.h
@@ -130,6 +130,13 @@ public:
         }
         return false;
     }
+    static bool containsEventOfType(QAccessible::Event evtype) {
+        for (const QAccessibleEvent *ev : std::as_const(eventList())) {
+            if (ev->type() == evtype)
+                return true;
+        }
+        return false;
+    }
     static void setUpdateHandler(std::function<void(QAccessibleEvent *event)> updateHandler)
     {
         Q_ASSERT_X(updateHandler, __FUNCTION__, "Update handler cannot be nullptr");

src/widgets/itemviews/qabstractitemview.cpp
@@ -3534,7 +3534,7 @@ void QAbstractItemView::dataChanged(const QModelIndex &topLeft, const QModelInde
         QAccessible::updateAccessibility(&accessibleEvent);
 
         // send accessibility events as needed when current item is modified
-        if (topLeft == bottomRight && topLeft == currentIndex())
+        if (topLeft.isValid() && topLeft == bottomRight && topLeft == currentIndex())
             d->updateItemAccessibility(topLeft, roles);
     }
 #endif

src/widgets/itemviews/qlistview.cpp
@@ -1963,6 +1963,7 @@ bool QListViewPrivate::dropOn(QDropEvent *event, int *dropRow, int *dropCol, QMo
 int QListViewPrivate::accessibleChildIndex(const QModelIndex &index) const
 {
     Q_Q(const QListView);
+    Q_ASSERT(index.isValid());
     return q->visualIndex(index);
 }
 #endif

src/widgets/itemviews/qtableview_p.h
@@ -144,6 +144,7 @@ public:
 #if QT_CONFIG(accessibility)
     inline int accessibleChildIndex(const QModelIndex &index) const override
     {
+        Q_ASSERT(index.isValid());
         const int vHeader = verticalHeader ? 1 : 0;
         return (index.row() + (horizontalHeader ? 1 : 0)) * (index.model()->columnCount() + vHeader)
             + index.column() + vHeader;

src/widgets/itemviews/qtreeview.cpp
@@ -4085,6 +4085,7 @@ void QTreeViewPrivate::sortIndicatorChanged(int column, Qt::SortOrder order)
 int QTreeViewPrivate::accessibleChildIndex(const QModelIndex &index) const
 {
     Q_Q(const QTreeView);
+    Q_ASSERT(index.isValid());
 
     // Note that this will include the header, even if its hidden.
     return (q->visualIndex(index) + (q->header() ? 1 : 0)) * index.model()->columnCount() + index.column();

tests/auto/other/qaccessibility/tst_qaccessibility.cpp
@@ -238,6 +238,8 @@ private slots:
     void widgetLocaleTest();
     void noInterfacesBeforeSetActive();
     void parentChangedEvent();
+    void itemViewEmittingDataChangeNoSegFault_data();
+    void itemViewEmittingDataChangeNoSegFault();
 
 protected slots:
     void onClicked();
@@ -5038,6 +5040,68 @@ void tst_QAccessibility::parentChangedEvent()
         QAccessibleEvent parentChangedEvent(&w, QAccessible::ParentChanged);
         QVERIFY(QTestAccessibility::containsEvent(&parentChangedEvent));
     }
+}
+
+void tst_QAccessibility::itemViewEmittingDataChangeNoSegFault_data()
+{
+    QTest::addColumn<int>("dummy");
+    QTest::addRow("QListView");
+    QTest::addRow("QTreeView");
+    QTest::addRow("QTableView");
+    QTest::addRow("QColumnView");
+}
+
+QAbstractItemView *createView(const char *viewName)
+{
+    if (0 == strcmp(viewName, "QListView"))
+        return new QListView();
+    if (0 == strcmp(viewName, "QTreeView"))
+        return new QTreeView();
+    if (0 == strcmp(viewName, "QTableView"))
+        return new QTableView();
+    if (0 == strcmp(viewName, "QColumnView"))
+        return new QColumnView();
+    return nullptr;
+}
+
+class InvalidDataChangeSignalEmittingModel : public QStandardItemModel
+{
+    Q_OBJECT
+public:
+    using QStandardItemModel::QStandardItemModel;
+    inline void triggerEmittingInvalidDataChangeSignal() { emit dataChanged({}, {}); }
+};
+
+void tst_QAccessibility::itemViewEmittingDataChangeNoSegFault()
+{
+    // Tests for QTBUG-143781, views shouldn't segfault if they get a dataChanged() signal with an invalid index
+    QVERIFY(QAccessible::isActive()); // This is set high in initTestCase()
+    auto clearRemainingEvents = qScopeGuard([] {
+        // I don't care about warnings for left over events
+        // This has to execute after destructors execute for the views I create
+        QTestAccessibility::clearEvents();
+    });
+    const QScopedPointer<QAbstractItemView> view(createView(QTest::currentDataTag()));
+    QVERIFY(view);
+
+    InvalidDataChangeSignalEmittingModel model(1, 1);
+    model.setData(model.index(0, 0), u"Initial"_s);
+
+    view->setModel(&model);
+    view->show();
+
+    QTestAccessibility::clearEvents();
+    model.triggerEmittingInvalidDataChangeSignal();
+    // If we got to here without segmentation fault, we're good!
+
+    QVERIFY(!QTestAccessibility::containsEventOfType(QAccessible::NameChanged));
+    QVERIFY(!QTestAccessibility::containsEventOfType(QAccessible::DescriptionChanged));
+    QVERIFY(!QTestAccessibility::containsEventOfType(QAccessible::StateChanged));
+
+    view->setCurrentIndex(model.index(0, 0)); // This is necessary for NameChanged events to be emitted later
+    QTestAccessibility::clearEvents();
+    model.setData(model.index(0, 0), u"Modified"_s);
+    QVERIFY(QTestAccessibility::containsEventOfType(QAccessible::NameChanged));
 }
 
 QTEST_MAIN(tst_QAccessibility)