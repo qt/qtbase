d0fe6859921f451db64b19624f0b9d5470382bf4
Arkadiusz Zarębski <arkadiusz.zarebski@qt.io>
2026-02-19 23:32:15 +0100

    Revert "ohos: emit only wheel events with NoScrollPhase"
    * The issue
    
    We introduced a hack to fix QTFOROH-1685 ticket in the commit
    "ohos: emit only wheel events with NoScrollPhase".
    
    The mentioned ticket was about double Qt::ScrollBegin and Qt::ScrollEnd
    events. Without that fix, if User used mouse wheel, events order was:
     - wheel event: Qt::ScrollBegin
     - wheel event: Qt::ScrollBegin
     - wheel event: Qt::ScrollUpdate
     - ...
     - wheel event: Qt::ScrollUpdate
     - wheel event: Qt::ScrollEnd
     - wheel event: Qt::ScrollEnd
    
    This behavior was considered incorrect and the accepted fix filtered
    Qt::ScrollBegin and Qt::ScrollEnd events while changing the phase to
    Qt::NoScrollPhase.
    
    However it seemed to fix the issue (the behavior is the same as macOS
    or windows) it changed Qt behavior.
    
    * Analysis
    
    While reading code and comments in:
    
        void QWindowSystemInterface::handleWheelEvent(...)
    
    User should expect multiple Qt::ScrollBegin and Qt::ScrollEnd wheel
    events as visible here:
    
        void QWindowSystemInterface::handleWheelEvent(...)
        {
            // Qt 4 sends two separate wheel events for horizontal and vertical
            // deltas. For Qt 5 we want to send the deltas in one event, but at the
            // same time preserve source and behavior compatibility with Qt 4.
            ...
            // Both horizontal and vertical deltas: Send two wheel events.
            // The first event contains the Qt 5 pixel and angle delta as points,
            // and in addition the Qt 4 compatibility vertical angle delta.
            e = new QWindowSystemInterfacePrivate::WheelEvent(window, timestamp, QHighDpi::fromNativeLocalPosition(local, window), QHighDpi::fromNativePixels(global, window), pixelDelta, angleDelta, angleDelta.y(), Qt::Vertical, mods, phase, source, invertedScrolling);
            QWindowSystemInterfacePrivate::handleWindowSystemEvent(e);
    
            // The second event contains null pixel and angle points and the
            // Qt 4 compatibility horizontal angle delta.
            e = new QWindowSystemInterfacePrivate::WheelEvent(window, timestamp, QHighDpi::fromNativeLocalPosition(local, window), QHighDpi::fromNativePixels(global, window), QPoint(), QPoint(), angleDelta.x(), Qt::Horizontal, mods, phase, source, invertedScrolling);
            QWindowSystemInterfacePrivate::handleWindowSystemEvent(e);
        }
    
    Change-Id: Idd16c6e2bd9d0f3f0ba766d301d8a2a71f7ae0b2
    Reviewed-by: SanthoshKumar Selvaraj <santhosh.kumar.selvaraj@qt.io>
    (cherry picked from commit 0e9fbfed42c9a8c51d21067c8f1d2b8e1381ceae)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/ohos/qohosinputmethodeventhandler.cpp
+++ src/plugins/platforms/ohos/qohosinputmethodeventhandler.cpp
@@ -71,6 +71,25 @@ QOhosOptional<QEventPoint::State> tryMapXComponentTouchEventTypeToQt(::OH_Native
     return makeEmptyQOhosOptional();
 }
 
+Qt::ScrollPhase convertArkUiAxisEventActionToQtScrollPhase(int32_t arkUiAxisEventAction)
+{
+    switch (arkUiAxisEventAction) {
+    case UI_AXIS_EVENT_ACTION_NONE:
+        return Qt::NoScrollPhase;
+    case UI_AXIS_EVENT_ACTION_BEGIN:
+        return Qt::ScrollBegin;
+    case UI_AXIS_EVENT_ACTION_UPDATE:
+        return Qt::ScrollUpdate;
+    case UI_AXIS_EVENT_ACTION_END:
+        return Qt::ScrollEnd;
+    case UI_AXIS_EVENT_ACTION_CANCEL:
+        return Qt::ScrollEnd;
+    default:
+        qOhosReportFatalErrorAndAbort(
+            "Received unsupported UI_AXIS_EVENT_ACTION: %d", arkUiAxisEventAction);
+    }
+}
+
 QPointF calculateTouchPointNormalPosition(QWindow *targetWindow, const QPointF &clickPoint)
 {
     auto *platformScreen = static_cast<QOhosPlatformScreen *>(targetWindow->screen()->handle());
@@ -256,9 +275,6 @@ void QOhosInputMethodEventHandler::onMouseWheelEvent(const QOhosWheelEvent &even
     QPoint pixelDelta;
     QPoint angleDelta;
 
-    if (event.axisAction != UI_AXIS_EVENT_ACTION_UPDATE)
-        return;
-
     static_cast<QOhosPlatformTheme *>(QGuiApplicationPrivate::platformTheme())->setWheelScrollLines(
         static_cast<int>(event.wheelScrollLines));
 
@@ -288,6 +304,7 @@ void QOhosInputMethodEventHandler::onMouseWheelEvent(const QOhosWheelEvent &even
         return;
     }
 
+    Qt::ScrollPhase phase = convertArkUiAxisEventActionToQtScrollPhase(event.axisAction);
     Qt::MouseEventSource source = event.eventToolType == ::UI_INPUT_EVENT_TOOL_TYPE_TOUCHPAD
         ? Qt::MouseEventSynthesizedBySystem
         : Qt::MouseEventNotSynthesized;
@@ -301,7 +318,7 @@ void QOhosInputMethodEventHandler::onMouseWheelEvent(const QOhosWheelEvent &even
         pixelDelta,
         angleDelta,
         convertOhosToQtKeyboardModifiers(event.modifiers),
-        Qt::NoScrollPhase,
+        phase,
         source,
         inverted);
 }
