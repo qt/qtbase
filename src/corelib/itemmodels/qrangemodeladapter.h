d9e64e57eca2cda6eff172e822d70c7e5eb5fb77
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-02-26 22:53:22 +0100

    QRMA: allow index() with an empty path
    It should return the root index of the model (the invalid index, or the
    index of the item this is a sub-tree of) instead of asserting. Otherwise
    we have to unnecessarily handle cases where rows are inserted into the
    top level of a tree by span (as we'd ask for the parent of the first
    slice of the span, which would be empty).
    
    Addresses issues discovered when increasing test coverage and building
    example.
    
    Pick-to: 6.11.0
    Change-Id: I623bffbbf6f140a70e53de8fbefe82157f1a6b2d
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit 0bebea4073dd681eafea13569b635197723068b1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/itemmodels/qrangemodeladapter.h
+++ src/corelib/itemmodels/qrangemodeladapter.h
@@ -1143,7 +1143,6 @@ public:
     template <typename I = Impl, if_tree<I> = true>
     QModelIndex index(QSpan<const int> path, int col) const
     {
-        Q_PRE(path.size());
         QModelIndex result = storage.root();
         auto count = path.size();
         for (const int r : path) {
