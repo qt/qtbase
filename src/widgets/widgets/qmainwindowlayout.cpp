c229c132aedcb40b8af4f8328e1aa93532689be5
Anna Wojciechowska <anna.wojciechowska@qt.io>
2026-02-26 14:36:01 +0100

    Fix memory leak in QDockWidgetGroupWindowItem
    When one floating QDockWidget is dragged over another while
    QMainWindow::GroupedDragging is enabled, the hover logic incorrectly
    restores a previously saved state. In the hover function, a
    QDockWidgetGroupWindow temporarily wraps the dock widget. Calling
    setCurrentHoveredFloat(...) attempts to restore savedState by
    assignment. However, the wrapping must be undone manually, including
    memory management.
    
    This fix clears savedState before calling setCurrentHoveredFloat(...),
    making restore a no-op.
    
    Testing:
    An autotest would require introducing a static counter to track
    constructor/destructor calls, which is considered intrusive.
    
    Fixes: QTBUG-143754
    Pick-to: 6.8 6.5
    Change-Id: I1aa2aa7b2a0f224dfcbad98268be0e1d18cd8471
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit 73dd5bdef21b61fc36baf7a5fad06e265a93b457)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 1e2cebf4e985585466cdaccb1a22fc1440e00f6c)

--- src/widgets/widgets/qmainwindowlayout.cpp
+++ src/widgets/widgets/qmainwindowlayout.cpp
@@ -3119,6 +3119,7 @@ void QMainWindowLayout::hover(QLayoutItem *hoverTarget,
             auto *groupWindow = qobject_cast<QDockWidgetGroupWindow *>(w);
             Q_ASSERT(groupWindow);
             if (groupWindow->hover(hoverTarget, groupWindow->mapFromGlobal(mousePos))) {
+                savedState.clear();
                 setCurrentHoveredFloat(groupWindow);
                 applyState(layoutState); // update the tabbars
             }
