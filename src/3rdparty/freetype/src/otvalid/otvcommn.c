37624c637a9fd28b8a6744d8c900a53b4abe11ed
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2026-03-04 12:25:55 +0100

    Update Freetype to 2.14.2
    [ChangeLog][Third-Party Code] Updated bundled Freetype to
    2.14.2.
    
    Fixes: QTBUG-144722
    Change-Id: I5d8b74ccab6added61dfe7f840ca23e62fcd0058
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>
    (cherry picked from commit 0300b5c25f6848ae71716b8fd67dd7f974a45fa5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b913cfdb541f81e568031a34ddc0eab6c69c9be2)
    (cherry picked from commit d03dfaac53cfa0e0bee93c337aa4424f65876b17)

--- src/3rdparty/freetype/src/otvalid/otvcommn.c
+++ src/3rdparty/freetype/src/otvalid/otvcommn.c
@@ -390,7 +390,7 @@
   }
 
 
-  /* uses valid->lookup_count */
+  /* sets otvalid->lookup_count */
 
   FT_LOCAL_DEF( void )
   otv_LookupList_validate( FT_Bytes       table,
@@ -488,7 +488,7 @@
 
     OTV_TRACE(( " (FeatureCount = %u)\n", FeatureCount ));
 
-    OTV_LIMIT_CHECK( FeatureCount * 2 );
+    OTV_LIMIT_CHECK( FeatureCount * 6 );
 
     otvalid->lookup_count = otv_LookupList_get_count( lookups );
 
@@ -846,7 +846,7 @@
   }
 
 
-  /* sets otvalid->extra1 (valid->lookup_count) */
+  /* sets otvalid->extra1 (otvalid->lookup_count) */
 
   FT_LOCAL_DEF( void )
   otv_u_O_O_x_Onx( FT_Bytes       table,
@@ -874,7 +874,7 @@
     OTV_LIMIT_CHECK( ClassSetCount * 2 );
 
     otvalid->nesting_level++;
-    func          = otvalid->func[otvalid->nesting_level];
+    func            = otvalid->func[otvalid->nesting_level];
     otvalid->extra1 = otvalid->lookup_count;
 
     for ( ; ClassSetCount > 0; ClassSetCount-- )
@@ -931,7 +931,7 @@
   }
 
 
-  /* sets otvalid->extra1 (valid->lookup_count)    */
+  /* sets otvalid->extra1 (otvalid->lookup_count)    */
 
   FT_LOCAL_DEF( void )
   otv_u_O_O_O_O_x_Onx( FT_Bytes       table,
@@ -966,7 +966,7 @@
     OTV_LIMIT_CHECK( ChainClassSetCount * 2 );
 
     otvalid->nesting_level++;
-    func          = otvalid->func[otvalid->nesting_level];
+    func            = otvalid->func[otvalid->nesting_level];
     otvalid->extra1 = otvalid->lookup_count;
 
     for ( ; ChainClassSetCount > 0; ChainClassSetCount-- )
