6135b06559e2bc7075891bd904b52299a6916845
Marc Mutz <marc.mutz@qt.io>
2026-03-11 14:42:57 +0100

    QTreeWidget: don't pass const Connection to disconnect()
    That overload will be deprecated, because the function modifies the
    argument.
    
    Amends c55a7e4167c3174120f1fb32c7e7d1756cc3960d.
    
    Pick-to: 6.11 6.10 6.8
    Task-number: QTBUG-144929
    Change-Id: If38a0a257ce3e4df697db369e63461c9e6a0e79a
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

--- src/widgets/itemviews/qtreewidget.cpp
+++ src/widgets/itemviews/qtreewidget.cpp
@@ -2292,7 +2292,7 @@ QDataStream &operator>>(QDataStream &in, QTreeWidgetItem &item)
 
 void QTreeWidgetPrivate::clearConnections()
 {
-    for (const QMetaObject::Connection &connection : connections)
+    for (QMetaObject::Connection &connection : connections)
         QObject::disconnect(connection);
 }
 
