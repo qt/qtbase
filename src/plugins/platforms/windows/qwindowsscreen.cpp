adb2b1b76cc93da016af85d0261ded6aef592f3e
Vladimir Belyavsky <belyavskyv@gmail.com>
2026-03-13 16:29:17 +0300

    Windows QPA: Suppress misleading warnings for virtual displays
    When querying display information for virtual screens or disconnected
    displays, DisplayConfigGetDeviceInfo and SetupDiOpenDeviceInterfaceW
    can fail with various error codes. These failures are expected and
    not actual errors.
    
    The warnings were particularly confusing because they appeared during
    normal operations like:
    - Starting applications when no physical monitors are connected
    - Hot-plugging/unplugging external displays
    - Using virtual display adapters
    
    When Qt tries to query device information for virtual rendering
    surfaces, the first call (GET_SOURCE_NAME) may succeed, but the
    second call (GET_TARGET_NAME) fails because there's no physical
    output connected to the virtual screen.
    
    Changed qCWarning to qCDebug for these cases, so the messages only
    appear when qt.qpa.screen debug logging is explicitly enabled.
    
    Fixes: QTBUG-115751
    Task-number: QTBUG-133050
    Change-Id: Ie636bca71abc7365ac97754b0bbc1ea6d668396c
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
    (cherry picked from commit 1988980f2ae9fd9a452fd213487f370ba4af6e29)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/windows/qwindowsscreen.cpp
+++ src/plugins/platforms/windows/qwindowsscreen.cpp
@@ -162,12 +162,14 @@ static void setMonitorDataFromSetupApi(QWindowsScreenData &data,
         // The first element in the clone group is the main monitor.
         deviceName.header.adapterId = pathGroup[0].targetInfo.adapterId;
         deviceName.header.id = pathGroup[0].targetInfo.id;
-        if (DisplayConfigGetDeviceInfo(&deviceName.header) == ERROR_SUCCESS) {
+        const LONG result = DisplayConfigGetDeviceInfo(&deviceName.header);
+        if (result == ERROR_SUCCESS) {
             data.devicePath = QString::fromWCharArray(deviceName.monitorDevicePath);
         } else {
-            qCWarning(lcQpaScreen)
+            // This can fail for virtual screens or disconnected displays - not an error
+            qCDebug(lcQpaScreen)
                     << u"Unable to get device information for %1:"_s.arg(pathGroup[0].targetInfo.id)
-                    << QSystemError::windowsString();
+                    << QSystemError::windowsString(result);
         }
     }
 
@@ -183,10 +185,12 @@ static void setMonitorDataFromSetupApi(QWindowsScreenData &data,
         deviceName.header.size = sizeof(DISPLAYCONFIG_TARGET_DEVICE_NAME);
         deviceName.header.adapterId = path.targetInfo.adapterId;
         deviceName.header.id = path.targetInfo.id;
-        if (DisplayConfigGetDeviceInfo(&deviceName.header) != ERROR_SUCCESS) {
-            qCWarning(lcQpaScreen)
+        const LONG result = DisplayConfigGetDeviceInfo(&deviceName.header);
+        if (result != ERROR_SUCCESS) {
+            // This can fail for virtual screens (WinDisc) or disconnected displays - not an error
+            qCDebug(lcQpaScreen)
                     << u"Unable to get device information for %1:"_s.arg(path.targetInfo.id)
-                    << QSystemError::windowsString();
+                    << QSystemError::windowsString(result);
             continue;
         }
 
@@ -205,7 +209,8 @@ static void setMonitorDataFromSetupApi(QWindowsScreenData &data,
 
         if (!SetupDiOpenDeviceInterfaceW(devInfo.get(), deviceName.monitorDevicePath, DIODI_NO_ADD,
                                          &deviceInterfaceData)) {
-            qCWarning(lcQpaScreen)
+            // This can fail for virtual screens with no physical target - not an error
+            qCDebug(lcQpaScreen)
                     << u"Unable to open monitor interface to %1:"_s.arg(data.deviceName)
                     << QSystemError::windowsString();
             continue;
