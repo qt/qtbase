ce139c3233f33c92c46629df6a189972a07a27e7
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2026-03-22 21:04:43 +0100

    QWhatsThis: fix positioning on second monitor
    When the whatsthis label does not fit on the screen, it gets
    repositioned. This worked well as long as the screens topLeft is at 0/0.
    In other cases (e.g. in a multi-monitor setup) the calculation did not
    honored the topLeft position and therefore the placement was wrong.
    
    Fixes: QTBUG-145104
    Pick-to: 6.11 6.10 6.8
    Change-Id: I756350f720c9330c97ffabad9d755f1417bff600
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/widgets/kernel/qwhatsthis.cpp
+++ src/widgets/kernel/qwhatsthis.cpp
@@ -556,10 +556,9 @@ void QWhatsThisPrivate::say(QWidget * widget, const QString &text, int x, int y)
 
     // squeeze it in if that would result in part of what's this
     // being only partially visible
-    if (x + w  + shadowWidth > sx+screenRect.width()) {
-        x = (widget ? qMin(screenRect.width(), pos.x() + widget->width())
-                    : screenRect.width())
-            - w;
+    if (x + w  + shadowWidth > screenRect.right()) {
+        x = widget ? qMin(screenRect.right(), pos.x() + widget->width()) : screenRect.right();
+        x -=w;
     }
 
     if (x < sx)
@@ -568,17 +567,16 @@ void QWhatsThisPrivate::say(QWidget * widget, const QString &text, int x, int y)
     if (widget && h > widget->height() + 16) {
         y = pos.y() + widget->height() + 2; // below, two pixels spacing
         // what's this is above or below, wherever there's most space
-        if (y + h + 10 > sy + screenRect.height())
+        if (y + h + 10 > screenRect.bottom())
             y = pos.y() + 2 - shadowWidth - h; // above, overlap
     }
     y = y + 2;
 
     // squeeze it in if that would result in part of what's this
     // being only partially visible
-    if (y + h + shadowWidth > sy + screenRect.height()) {
-        y = (widget ? qMin(screenRect.height(), pos.y() + widget->height())
-                    : screenRect.height())
-            - h;
+    if (y + h + shadowWidth > screenRect.bottom()) {
+        y = widget ? qMin(screenRect.bottom(), pos.y() + widget->height()) : screenRect.bottom();
+        y -= h;
     }
     if (y < sy)
         y = sy;
