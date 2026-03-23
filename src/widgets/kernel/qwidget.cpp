8c91ccc8b5e989be3802b71df2c6ca5b3ee518aa
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2026-03-23 16:51:04 +0100

    Mask by Qt::WindowType_Mask when checking for Qt::WindowType::Desktop
    We can't use QFlags::testFlag() for the window type bits of
    Qt::WindowFlags, as that would wrongly match for `Qt::Window |
    Qt::SubWindow`.
    
    The window type part of Qt::WindowFlags is strictly speaking not meant
    to be compared with `f & Qt::Foo`, or even `(f & Qt::Foo) == Qt::Foo`,
    as they are distinct values, not flags that can be combined. Though in
    some cases we do use the latter form, for example for catching sub
    windows, as the semantics of `Qt::Window | Qt::SubWindow` are pretty
    clear from the user's point of view (a window that is a sub window), and
    we don't want to force users to unset the Qt::Window bit explicitly.
    
    Amends d928f2e19da22fd866503f480b90338da4613389.
    
    Pick-to: 6.11
    Change-Id: I8e6aacb4fa88b1721218a9e7b2805e62cd767e74
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

--- src/widgets/kernel/qwidget.cpp
+++ src/widgets/kernel/qwidget.cpp
@@ -942,8 +942,9 @@ void QWidgetPrivate::init(QWidget *parentWidget, Qt::WindowFlags f)
 
 #if QT_DEPRECATED_SINCE(6, 11)
     QT_IGNORE_DEPRECATIONS(
-    Q_ASSERT_X(!f.testFlag(Qt::WindowType::Desktop), Q_FUNC_INFO, "Qt::WindowType::Desktop is not allowed.");
-    if (f.testFlag(Qt::WindowType::Desktop))
+    const bool isDesktopWindow = (f & Qt::WindowType_Mask) == Qt::WindowType::Desktop;
+    Q_ASSERT_X(!isDesktopWindow, Q_FUNC_INFO, "Qt::WindowType::Desktop is not allowed.");
+    if (isDesktopWindow)
         f.setFlag(Qt::WindowType::Desktop, false);
     )
 #endif
@@ -10655,7 +10656,7 @@ void QWidgetPrivate::setWindowFlags(Qt::WindowFlags flags)
     Q_Q(QWidget);
 #if QT_DEPRECATED_SINCE(6, 11)
     QT_IGNORE_DEPRECATIONS(
-    if (flags.testFlag(Qt::WindowType::Desktop)) {
+    if ((flags & Qt::WindowType_Mask) == Qt::WindowType::Desktop) {
         qWarning() << "Qt::WindowType::Desktop has been deprecated in Qt 6. Ignoring.";
         flags.setFlag(Qt::WindowType::Desktop, false);
     }
