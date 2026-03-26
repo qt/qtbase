fa96ccf4a3a8e4064ce9c3c8812c9ef4a2cd0bbf
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2026-03-26 14:25:10 +0100

    Stabilize tst_QTreeview::spanningItems
    On certain platforms the default minimumSectionSize is relatively
    large. This prevent some column's size change to be ignored, because
    the requested size would fall below the enforced minimum.
    
    Set a small minimumSectionSize to ensure that changes are being called
    as supposed by tests.
    
    Pick-to: 6.11 6.10 6.8
    Change-Id: Id4d3a45d08f4412df3b057498c3ae27f25fa4df8
    Reviewed-by: SanthoshKumar Selvaraj <santhosh.kumar.selvaraj@qt.io>

--- tests/auto/widgets/itemviews/qtreeview/tst_qtreeview.cpp
+++ tests/auto/widgets/itemviews/qtreeview/tst_qtreeview.cpp
@@ -2435,6 +2435,7 @@ void tst_QTreeView::spanningItems()
     QtTestModel model(10, 10);
     model.onlyValidCalls = true;
     QTreeView view;
+    view.header()->setMinimumSectionSize(20);
     view.setModel(&model);
     view.show();
     QVERIFY(QTest::qWaitForWindowExposed(&view));
