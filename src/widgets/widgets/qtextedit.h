4b99b9740b5550cc4c6071e1feaf913fcd3bbbdc
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2026-04-08 12:47:53 +0200

    QTextEdit: add anchorAt(const QPointF)
    Add a new overload anchorAt() which takes a QPointF instead QPoint as
    the underlying function QWidgetTextControl::anchorAt already takes a
    QPointF.
    
    [ChangeLog][QtWidgets][QTextEdit] QTextEdit gained a new overload for
    anchorAt() which takes a QPointF.
    
    Change-Id: Icbf881d235892554e4327f0056d4c06aeaaf2996
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

--- src/widgets/widgets/qtextedit.h
+++ src/widgets/widgets/qtextedit.h
@@ -158,7 +158,10 @@ public:
     QRect cursorRect(const QTextCursor &cursor) const;
     QRect cursorRect() const;
 
-    QString anchorAt(const QPoint& pos) const;
+    // ### Qt7: remove
+    QT_WIDGETS_INLINE_SINCE(6, 12)
+    QString anchorAt(const QPoint &pos) const;
+    QString anchorAt(QPointF pos) const;
 
     bool overwriteMode() const;
     void setOverwriteMode(bool overwrite);
@@ -293,6 +296,10 @@ private:
 };
 
 #if QT_WIDGETS_INLINE_IMPL_SINCE(6, 12)
+QString QTextEdit::anchorAt(const QPoint &pos) const
+{
+    return anchorAt(QPointF(pos));
+}
 QTextCursor QTextEdit::cursorForPosition(const QPoint &pos) const
 {
     return cursorForPosition(QPointF(pos));
