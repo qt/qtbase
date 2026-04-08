5096a970117b338332c4dc0d6bb23c8b7bdbecc3
Miika Pernu <miika.pernu@qt.io>
2026-04-08 09:16:43 +0900

    Revert: QHeaderView: adjust autoscroll to QAbstractItemView changes
    This change reverts e3b6bea5fbbc7f4be8730573b78d86d95245c1c4. The change
    was originally done to amend an issue in QHeaderView scroll direction
    caused by 882a4df4fdd2a5952a605ca783142f46d9a92d5b. Unfortunately,
    882a4df4fdd2a5952a605ca783142f46d9a92d5b turned out to be a bigger break
    than a fix so both should be reverted.
    
    Pick-to: 6.8 6.11
    Change-Id: Ib404cf2aa365a2e58cb8a7bfa39cb9c3158935eb
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/widgets/itemviews/qheaderview.cpp
+++ src/widgets/itemviews/qheaderview.cpp
@@ -2711,8 +2711,7 @@ void QHeaderView::mouseMoveEvent(QMouseEvent *e)
         }
         case QHeaderViewPrivate::MoveSection: {
             if (d->shouldAutoScroll(e->position().toPoint())) {
-                d->draggedPosition = e->pos();
-                d->draggedPositionOffset = d->offset();
+                d->draggedPosition = e->pos() + d->offset();
                 d->startAutoScroll();
             }
             if (qAbs(pos - d->firstPos) >= QApplication::startDragDistance()
