574e3633e9acbc3af76f1f689f135ab9a67ede30
Thiago Macieira <thiago.macieira@intel.com>
2026-04-15 19:41:39 -0700

    QCoreApplication: fix the instance() data race in Qt 6 for C++20
    This requires C++20 because we can't change the type of
    QCoreApplication::self. Strictly speaking, this also requires that
    QtCore be compiled as C++20, which we don't allow people to do. So this
    isn't really a fix.
    
    Complements commit 7d92ecd9173a5ce4af8eb6ac05d6e7648d433db4.
    
    Pick-to: 6.11
    Change-Id: I426ea2ecb86ecf2bf6f9fffd6d873728843d067a
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/kernel/qcoreapplication.cpp
+++ src/corelib/kernel/qcoreapplication.cpp
@@ -725,6 +725,10 @@ void QCoreApplicationPrivate::initLocale()
     QGuiApplication/QApplication) instance.
 
     If no instance has been allocated, \nullptr is returned.
+
+    \b{Data race notice}: this function should not be used to test for the
+    existence of the QCoreApplication instance from auxiliary threads, unless
+    the caller code is compiled as C++20.
 */
 
 /*!
@@ -799,7 +803,11 @@ void Q_TRACE_INSTRUMENT(qtcore) QCoreApplicationPrivate::init()
 
     Q_ASSERT_X(!QCoreApplication::self, "QCoreApplication", "there should be only one application object");
 #if QT_VERSION < QT_VERSION_CHECK(7, 0, 0)
+#  ifdef __cpp_lib_atomic_ref
+    std::atomic_ref(QCoreApplication::self).store(q, std::memory_order_relaxed);
+#  else
     QCoreApplication::self = q;
+#  endif
     g_self.storeRelaxed(q);
 #else
     QCoreApplication::self.storeRelaxed(q);
@@ -907,7 +915,11 @@ QCoreApplication::~QCoreApplication()
     qt_call_post_routines();
 
 #if QT_VERSION < QT_VERSION_CHECK(7, 0, 0)
+#  ifdef __cpp_lib_atomic_ref
+    std::atomic_ref(self).store(nullptr, std::memory_order_relaxed);
+#  else
     self = nullptr;
+#  endif
     g_self.storeRelaxed(nullptr);
 #else
     self.storeRelaxed(nullptr);
