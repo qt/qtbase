81ca5895f55e7334d9220fd38265764ae2783a76
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2026-04-21 14:26:27 +0200

    Upgrade Harfbuzz to 14.2.0
    [ChangeLog][Third-Party Code] Upgraded Harfbuzz to version 14.2.0
    
    Pick-to: 6.11 6.8 6.5 5.15
    Task-number: QTBUG-145919
    Change-Id: I0043fdd75b5e9988b12ac5c3d53e64512e4aa110
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>

---
+++ src/3rdparty/harfbuzz-ng/src/harfbuzz-world.cc
@@ -0,0 +1,169 @@
+#ifdef HB_FEATURES_H
+#include HB_FEATURES_H
+#endif
+
+/* Core library. */
+#include "OT/Var/VARC/VARC.cc"
+#include "hb-aat-layout.cc"
+#include "hb-aat-map.cc"
+#include "hb-blob.cc"
+#include "hb-buffer-serialize.cc"
+#include "hb-buffer-verify.cc"
+#include "hb-buffer.cc"
+#include "hb-common.cc"
+#include "hb-draw.cc"
+#include "hb-face-builder.cc"
+#include "hb-face.cc"
+#include "hb-fallback-shape.cc"
+#include "hb-font.cc"
+#include "hb-map.cc"
+#include "hb-number.cc"
+#include "hb-ot-cff1-table.cc"
+#include "hb-ot-cff2-table.cc"
+#include "hb-ot-color.cc"
+#include "hb-ot-face.cc"
+#include "hb-ot-font.cc"
+#include "hb-ot-layout.cc"
+#include "hb-ot-map.cc"
+#include "hb-ot-math.cc"
+#include "hb-ot-meta.cc"
+#include "hb-ot-metrics.cc"
+#include "hb-ot-name.cc"
+#include "hb-ot-shape-fallback.cc"
+#include "hb-ot-shape-normalize.cc"
+#include "hb-ot-shape.cc"
+#include "hb-ot-shaper-arabic.cc"
+#include "hb-ot-shaper-default.cc"
+#include "hb-ot-shaper-hangul.cc"
+#include "hb-ot-shaper-hebrew.cc"
+#include "hb-ot-shaper-indic-table.cc"
+#include "hb-ot-shaper-indic.cc"
+#include "hb-ot-shaper-khmer.cc"
+#include "hb-ot-shaper-myanmar.cc"
+#include "hb-ot-shaper-syllabic.cc"
+#include "hb-ot-shaper-thai.cc"
+#include "hb-ot-shaper-use.cc"
+#include "hb-ot-shaper-vowel-constraints.cc"
+#include "hb-ot-tag.cc"
+#include "hb-ot-var.cc"
+#include "hb-outline.cc"
+#include "hb-paint-bounded.cc"
+#include "hb-paint-extents.cc"
+#include "hb-paint.cc"
+#include "hb-set.cc"
+#include "hb-shape-plan.cc"
+#include "hb-shape.cc"
+#include "hb-shaper.cc"
+#include "hb-static.cc"
+#include "hb-style.cc"
+#include "hb-ucd.cc"
+#include "hb-unicode.cc"
+
+#ifdef HB_HAS_CAIRO
+#include "hb-cairo-utils.cc"
+#include "hb-cairo.cc"
+#include "hb-static.cc"
+#endif
+
+#ifdef HB_HAS_CORETEXT
+#define HAVE_CORETEXT 1
+#include "hb-coretext-font.cc"
+#include "hb-coretext-shape.cc"
+#include "hb-coretext.cc"
+#endif
+
+#ifdef HB_HAS_DIRECTWRITE
+#define HAVE_DIRECTWRITE 1
+#include "hb-directwrite-font.cc"
+#include "hb-directwrite-shape.cc"
+#include "hb-directwrite.cc"
+#endif
+
+#ifdef HB_HAS_FREETYPE
+#define HAVE_FREETYPE 1
+#include "hb-ft.cc"
+#endif
+
+#ifdef HB_HAS_GDI
+#ifndef HAVE_GDI
+#define HAVE_GDI 1
+#include "hb-gdi.cc"
+#endif
+#endif
+
+#ifdef HB_HAS_GLIB
+#define HAVE_GLIB 1
+#include "hb-glib.cc"
+#endif
+
+#ifdef HB_HAS_GRAPHITE
+#define HAVE_GRAPHITE 1
+#include "hb-graphite2.cc"
+#endif
+
+#ifdef HB_HAS_GPU
+#include "hb-gpu-draw.cc"
+#include "hb-gpu-paint.cc"
+#include "hb-gpu.cc"
+#include "hb-static.cc"
+#endif
+
+#ifdef HB_HAS_ICU
+#include "hb-icu.cc"
+#endif
+
+#ifdef HB_HAS_RASTER
+#include "hb-raster-draw.cc"
+#include "hb-raster-image.cc"
+#include "hb-raster-paint.cc"
+#include "hb-raster.cc"
+#include "hb-static.cc"
+#endif
+
+#ifdef HB_HAS_SUBSET
+#include "graph/gsubgpos-context.cc"
+#include "hb-number.cc"
+#include "hb-ot-cff1-table.cc"
+#include "hb-ot-cff2-table.cc"
+#include "hb-static.cc"
+#include "hb-subset-cff-common.cc"
+#include "hb-subset-cff1.cc"
+#include "hb-subset-cff2-to-cff1.cc"
+#include "hb-subset-cff2.cc"
+#include "hb-subset-input.cc"
+#include "hb-subset-instancer-iup.cc"
+#include "hb-subset-instancer-solver.cc"
+#include "hb-subset-plan-layout.cc"
+#include "hb-subset-plan-var.cc"
+#include "hb-subset-plan.cc"
+#include "hb-subset-serialize.cc"
+#include "hb-subset-table-cff.cc"
+#include "hb-subset-table-color.cc"
+#include "hb-subset-table-layout.cc"
+#include "hb-subset-table-other.cc"
+#include "hb-subset-table-var.cc"
+#include "hb-subset.cc"
+#endif
+
+#ifdef HB_HAS_UNISCRIBE
+#define HAVE_UNISCRIBE 1
+#define HAVE_GDI 1
+#include "hb-uniscribe.cc"
+#endif
+
+#ifdef HB_HAS_VECTOR
+#include "hb-static.cc"
+#include "hb-vector-draw.cc"
+#include "hb-vector-paint-pdf.cc"
+#include "hb-vector-paint-svg.cc"
+#include "hb-vector-paint.cc"
+#include "hb-vector-path.cc"
+#include "hb-vector.cc"
+#include "hb-zlib.cc"
+#endif
+
+#ifdef HB_HAS_WASM
+#define HAVE_WASM 1
+#include "hb-wasm-api.cc"
+#include "hb-wasm-shape.cc"
+#endif
---
+++ src/3rdparty/harfbuzz-ng/src/hb-gpu-draw-fragment.glsl
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2026  Behdad Esfahbod
+ *
+ *  This is part of HarfBuzz, a text shaping library.
+ *
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the
+ * above copyright notice and the following two paragraphs appear in
+ * all copies of this software.
+ *
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN
+ * IF THE COPYRIGHT HOLDER HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * THE COPYRIGHT HOLDER SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ * ON AN "AS IS" BASIS, AND THE COPYRIGHT HOLDER HAS NO OBLIGATION TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+
+/* Draw-renderer fragment shader entry.  The heavy lifting (Slug
+ * coverage, MSAA, ppem, stem darkening) lives in the shared
+ * hb-gpu-fragment.glsl that must be prepended to this source;
+ * this file only adds the thin hb_gpu_draw() wrapper that lifts
+ * pixelsPerEm out of fwidth() at uniform control flow before
+ * calling the shared _hb_gpu_slug(). */
+
+
+float hb_gpu_draw (vec2 renderCoord, uint glyphLoc_)
+{
+  vec2 pixelsPerEm = 1.0 / fwidth (renderCoord);
+  return _hb_gpu_slug (renderCoord, pixelsPerEm, glyphLoc_);
+}
