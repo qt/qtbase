0c47a97de3f687db1e2bb403d6f59f8c4e9a5453
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2026-05-05 15:59:16 +0200

    iOS: Remove dead and unneeded code for plugin registration
    qt_registerPlatformPlugin was no longer used after
    880db10f553d1190ab79f3dd45a89bfa28e5c84b.
    
    We don't need the explicit Q_IMPORT_PLUGIN either nowadays,
    as qmake handles it via qt.prf, and CMake via
    __qt_internal_add_static_plugin_init_object_library.
    
    Change-Id: I8342f1ffdf5ae15e4b261b92e4a4c8180eb121ca
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/plugins/platforms/ios/plugin.mm
+++ src/plugins/platforms/ios/plugin.mm
@@ -32,9 +32,3 @@ QPlatformIntegration * QIOSIntegrationPlugin::create(const QString& system, cons
 QT_END_NAMESPACE
 
 #include "plugin.moc"
-
-// Dummy function that we explicitly tell the linker to look for,
-// so that the plugin's static initializer is included and run.
-extern "C" void qt_registerPlatformPlugin() {}
-
-Q_IMPORT_PLUGIN(QIOSIntegrationPlugin)
