edabab0199632245040a2a33846dc3e077f46dd5
Marc Mutz <marc.mutz@qt.io>
2026-05-06 10:17:17 +0200

    qlalr: regenerate files [2/2]: parser
    Add a script to regenerate qlalr from lalr.g and run it.
    
    The security score is copied from make-xml-parser.sh.
    
    The lack of changes to generated files shows that input and
    output are in sync again, after the handful of fixes merged
    as prequel commits.
    
    Found that the output of qlalr depends on the host platform and
    filed QTBUG-146645 to keep track of the issue.
    
    Pick-to: 6.11 6.8
    Change-Id: Iab561319510cfab7112a78e1f16065584f5be06b
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

---
+++ src/tools/qlalr/generate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (C) 2026 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+# Qt-Security score:insignificant reason:build-tool-containing-no-compiled-source
+
+# run this script in the qtbase srcdir with qlalr in PATH
+
+function msg() {
+    echo "$@" 1>&2
+}
+
+function die() {
+    msg "$@"
+    exit 1
+}
+
+cd src/tools/qlalr || die "not in qtbase toplevel srcdir?"
+exec qlalr \
+     --qt \
+     --no-debug \
+     --no-lines \
+     lalr.g
