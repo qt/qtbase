df5421bd77713db7c91431bec15dd3a03138ff00
Thiago Macieira <thiago.macieira@intel.com>
2026-05-06 09:52:14 -0700

    tst_qfile: make invalidFile() Windows-specific
    Only Windows has limitations in what characters can be part of a file
    name. Other OSes may have filesystem-specific limitations (e.g., on
    Microsoft filesystems), but we don't have a good way to find them on a
    unit test and it's not guaranteed to fail anyway.
    
    Everywhere else, any character other than the null character is valid in
    a file path. The test was attempting to use a trailing "/" in the path,
    which caused open(2) to fail with EISDIR as opposed to being an invalid
    file name. We have tests for testing opening directory, so leave that
    one.
    
    Fixes: QTBUG-146578
    Change-Id: I2330f2f024adbe9ffc8afffd8a2d3c1c0d540280
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    (cherry picked from commit 8210fda3ff984686b7736069079995073c07f81b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit f582fac2cc91c34d3fe8089abdbc5952622f4a47)

--- tests/auto/corelib/io/qfile/tst_qfile.cpp
+++ tests/auto/corelib/io/qfile/tst_qfile.cpp
@@ -287,8 +287,11 @@ private slots:
     void openDirectory();
     void writeNothing();
 
+#ifdef Q_OS_WIN
+    // All file paths are potentially valid, outside of Windows
     void invalidFile_data();
     void invalidFile();
+#endif
 
     void reuseQFile();
 
@@ -1235,19 +1238,11 @@ static inline QChar invalidDriveLetter()
     return QChar();
 }
 
-#endif // Q_OS_WIN
 
 void tst_QFile::invalidFile_data()
 {
     QTest::addColumn<QString>("fileName");
 
-#if defined(Q_OS_WASM)
-    QSKIP("No invalid files on wasm");
-#endif
-
-#if !defined(Q_OS_WIN)
-    QTest::newRow( "x11" ) << QString( "qwe//" );
-#else
     QTest::newRow( "colon2" ) << invalidDriveLetter() + QString::fromLatin1(":ail:invalid");
     QTest::newRow( "colon3" ) << QString( ":failinvalid" );
     QTest::newRow( "forwardslash" ) << QString( "fail/invalid" );
@@ -1257,7 +1252,6 @@ void tst_QFile::invalidFile_data()
     QTest::newRow( "lt" ) << QString( "fail<invalid" );
     QTest::newRow( "gt" ) << QString( "fail>invalid" );
     QTest::newRow( "pipe" ) << QString( "fail|invalid" );
-#endif
 }
 void tst_QFile::invalidFile()
 {
@@ -1265,6 +1259,7 @@ void tst_QFile::invalidFile()
     QFile f( fileName );
     QVERIFY2( !f.open( QIODevice::ReadWrite ), qPrintable(fileName) );
 }
+#endif // Q_OS_WIN
 
 void tst_QFile::createFile()
 {
