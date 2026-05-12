3929e670ba17e3d61c7ffca8fe0083871665182c
Edward Welbourne <edward.welbourne@qt.io>
2026-05-12 19:50:03 +0200

    Correct a QDate::fromString() test so its text and format match
    The test was written to test that doubled single-quotes in the format
    string get read as a single quote to match in the parsed text. It was
    expected to fail, because this documented behavior is not actually
    supported, so we failed to notice that it would have failed even if it
    were supported.  The quote was at the front of the month in the text
    to parse, which also had a four-digit year, but the format string had
    a two-digit year and its doubled single-quote was just before the
    year. Correct this so that when the new date-time parser fixes it we
    can actually remove the expected-fail marker and close the bug.
    
    Corrects commit 96e031edd74e8e1eaea79d95320637eb27e8e2ba (since which
    I've tidied up the relevant tests at least once without noticing this
    error).
    
    Pick-to: 6.11 6.8
    Task-number: QTBUG-110669
    Change-Id: Idbb57001d5d2898249a46d4df0f00c05e1a6cd74
    Reviewed-by: Mate Barany <mate.barany@qt.io>

--- tests/auto/corelib/time/qdate/tst_qdate.cpp
+++ tests/auto/corelib/time/qdate/tst_qdate.cpp
@@ -1483,7 +1483,7 @@ void tst_QDate::fromStringFormat_data()
     QTest::newRow("quoted-MM")
             << u"21-MM05-2006"_s << u"dd-'MM'MM-yyyy"_s << 1900 << QDate(2006, 5, 21);
     QTest::newRow("quotes-empty")
-            << u"21-'05-2006"_s << u"dd-MM-''yy"_s << 1900 << QDate(2006, 5, 21);
+            << u"21-'05-2006"_s << u"dd-''MM-yyyy"_s << 1900 << QDate(2006, 5, 21);
     QTest::newRow("quote-verbose")
             << u"On day 22 of the Gregorian month named April in the Common Era year 2026"_s
             << u"'On day' dd 'of the' 'Gregorian month named' MMMM 'in the' 'Common Era year' yyyy"_s
