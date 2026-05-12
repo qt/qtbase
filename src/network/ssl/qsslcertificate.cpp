8017f8fc90407058222e5a948bc1348c8e83bc6c
Mårten Nordheim <marten.nordheim@qt.io>
2026-05-12 14:58:58 +0200

    QSslCertificate: Add missing braces for if-body
    I was randomly looking at the patch that introduced this and realized it
    doesn't have braces around the if-body. It is clearly meant to - it
    should be possible to use the fixed-string version - but it was missed
    and no one has complained in the time since.
    
    Probably people are not using QSslCertificate while disabling
    regularexpression.
    
    Amends 49f143e19ca11ef48260a3aaaa4ddbe490cf81a
    
    Pick-to: 6.11 6.8
    Change-Id: I6522fea48997e6bb6626a061a706acfc2f89be7a
    Reviewed-by: Samuel Gaist <samuel.gaist@idiap.ch>
    Reviewed-by: Mate Barany <mate.barany@qt.io>

--- src/network/ssl/qsslcertificate.cpp
+++ src/network/ssl/qsslcertificate.cpp
@@ -654,9 +654,10 @@ QList<QSslCertificate> QSslCertificate::fromPath(const QString &path,
     else if (syntax == PatternSyntax::RegularExpression)
         pos = sourcePath.indexOf(QRegularExpression("[\\$\\(\\)\\*\\+\\.\\?\\[\\]\\^\\{\\}\\|]"_L1));
 #else
-    if (syntax == PatternSyntax::Wildcard || syntax == PatternSyntax::RegularExpression)
+    if (syntax == PatternSyntax::Wildcard || syntax == PatternSyntax::RegularExpression) {
         qWarning("Regular expression support is disabled in this build. Only fixed string can be searched");
         return QList<QSslCertificate>();
+    }
 #endif
 
     if (pos != -1) {
