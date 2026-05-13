0af5003d17aacb29e2ed35e55aa696295864c128
Marc Mutz <marc.mutz@qt.io>
2026-05-13 18:06:55 +0200

    QMatrix4x4: deduplicate code between two ctors
    The QGenericMatrix<N,M,float> ctor was performing exactly the same
    operations as the (const float *, int cols, int rows) one, except the
    former is inline and the latter is out-of-line.
    
    While an inline function is more likely to be optimized properly,
    incl. strength-reducing the multiplication in the inner loop, we can
    do these optimizations to the out-of-line function manually, and
    reduce code duplication (both source and executable) with very little
    performance overhead. There's only one in-tree user of this contructor
    (from QMatrix3x3), so I think the advantages outweight the
    disadvantages here.
    
    Amends the start of the public history, which, of course, didn't have
    constructor delegation available, as this was a C++11 addition.
    
    Pick-to: 6.11
    Change-Id: Ieb237961d1f664f633fd576985b9376cd9225ed0
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 44d63c50836a4752b1ff3b9015d5f31528b74b79)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/math3d/qmatrix4x4.h
+++ src/gui/math3d/qmatrix4x4.h
@@ -36,7 +36,8 @@ public:
                       float m41, float m42, float m43, float m44);
 
     template <int N, int M>
-    explicit QMatrix4x4(const QGenericMatrix<N, M, float>& matrix);
+    explicit QMatrix4x4(const QGenericMatrix<N, M, float>& matrix)
+        : QMatrix4x4(matrix.data(), N, M) {}
 
     QMatrix4x4(const float *values, int cols, int rows);
     QMatrix4x4(const QTransform& transform);
@@ -207,24 +208,6 @@ inline QMatrix4x4::QMatrix4x4
     m[1][0] = m12; m[1][1] = m22; m[1][2] = m32; m[1][3] = m42;
     m[2][0] = m13; m[2][1] = m23; m[2][2] = m33; m[2][3] = m43;
     m[3][0] = m14; m[3][1] = m24; m[3][2] = m34; m[3][3] = m44;
-    flagBits = General;
-}
-
-template <int N, int M>
-Q_INLINE_TEMPLATE QMatrix4x4::QMatrix4x4
-    (const QGenericMatrix<N, M, float>& matrix)
-{
-    const float *values = matrix.constData();
-    for (int matrixCol = 0; matrixCol < 4; ++matrixCol) {
-        for (int matrixRow = 0; matrixRow < 4; ++matrixRow) {
-            if (matrixCol < N && matrixRow < M)
-                m[matrixCol][matrixRow] = values[matrixCol * M + matrixRow];
-            else if (matrixCol == matrixRow)
-                m[matrixCol][matrixRow] = 1.0f;
-            else
-                m[matrixCol][matrixRow] = 0.0f;
-        }
-    }
     flagBits = General;
 }
 
