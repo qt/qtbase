85dfd81b31d32fd4de11f49e0de110385f740ef3
Tarja Sundqvist <tarja.sundqvist@qt.io>
2026-06-02 15:35:15 +0300

    Revert "Update bundled libpng to version 1.6.48"
    This reverts commit a24f6dafe6adba28e203f7156b3ce59ff757fe29.
    
    Revert is needed as the commit causes conflicts on
    tqtc/lts-6.5-opensource branch when merging Qt 6.5.10 content.
    In Qt 6.5.10 opensource, the libpng 1.6.50 will be included via
    f8c09cbe6b1fb40634c252518ba7ea3933210407.
    
    Change-Id: I709f57d2b9ca12064b4994241675a67fa5c5f49f
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

--- src/3rdparty/libpng/pngstruct.h
+++ src/3rdparty/libpng/pngstruct.h
@@ -1,6 +1,6 @@
-/* pngstruct.h - internal structures for libpng
+/* pngstruct.h - header file for PNG reference library
  *
- * Copyright (c) 2018-2025 Cosmin Truta
+ * Copyright (c) 2018-2022 Cosmin Truta
  * Copyright (c) 1998-2002,2004,2006-2018 Glenn Randers-Pehrson
  * Copyright (c) 1996-1997 Andreas Dilger
  * Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.
@@ -10,9 +10,11 @@
  * and license in png.h
  */
 
-#ifndef PNGPRIV_H
-#  error This file must not be included by applications; please include <png.h>
-#endif
+/* The structure that holds the information to read and write PNG files.
+ * The only people who need to care about what is inside of this are the
+ * people who will be modifying the library for their own special needs.
+ * It should NOT be accessed directly by an application.
+ */
 
 #ifndef PNGSTRUCT_H
 #define PNGSTRUCT_H
