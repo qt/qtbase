ac873bcc233fd2868937935303ef97bf42ab4f24
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-06-03 08:59:15 +0200

    Widgets snippets: Update QTreeWidget minimal example
    Pick-to: 6.12 6.11
    Task-number: PYSIDE-3362
    Change-Id: I5fd9e784d7290900dffec34b6f360be1e17c6b07
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/widgets/doc/snippets/code/src_gui_itemviews_qtreewidget.cpp
+++ src/widgets/doc/snippets/code/src_gui_itemviews_qtreewidget.cpp
@@ -1,11 +1,18 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-//! [0]
-QTreeWidget *treeWidget = new QTreeWidget();
-treeWidget->setColumnCount(1);
-QList<QTreeWidgetItem *> items;
-for (int i = 0; i < 10; ++i)
-    items.append(new QTreeWidgetItem(static_cast<QTreeWidget *>(nullptr), QStringList(QString("item: %1").arg(i))));
-treeWidget->insertTopLevelItems(0, items);
-//! [0]
+#include <qtreewidget.h>
+
+class TreeMainWindow :public QWidget // Provide tr()
+{
+public:
+    TreeMainWindow()
+    {
+        //! [0]
+        auto *treeWidget = new QTreeWidget();
+        treeWidget->setColumnCount(1);
+        for (int i = 0; i < 10; ++i)
+            new QTreeWidgetItem(treeWidget, { tr("item: %1").arg(i) });
+        //! [0]
+    }
+};
