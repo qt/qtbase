d709684ade2141816606a1a7789ace887a8745f5
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2026-06-05 14:06:58 +0200

    Mark qtwaylandscanner as security critical
    This is used to generate server code which interfaces with
    clients, so it is outside the trust boundary.
    
    Pick-to: 6.11
    Change-Id: Icfd2f8f8779e761f99c751294720ff497651511c
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
    (cherry picked from commit cd260c782a819fcd551b3cc102c59d22f0ab4c60)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/tools/qtwaylandscanner/qtwaylandscanner.cpp
+++ src/tools/qtwaylandscanner/qtwaylandscanner.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:critical reason:network-protocol
 
 #include <QCoreApplication>
 #include <QFile>
