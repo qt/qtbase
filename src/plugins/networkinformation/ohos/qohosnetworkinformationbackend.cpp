59f7385edc1841535333f8396204770268337eee
Dawid Śliwa <dawid.sliwa@qt.io>
2026-06-07 14:33:07 +0200

    networkinformation/ohos: report reachability and transport medium
    Drive the backend from the @ohos.net.connection data source: install a
    connectivity-changed notifier and map the default network's state to
    QNetworkInformation. Reachability follows the network capabilities -
    Online when the connection is VALIDATED, Local while connectivity is
    still being checked (we have a network but internet is unconfirmed),
    Site when the network is internet-capable but not validated (a captive
    portal or otherwise limited connection), Local for a network with no
    internet capability, and Disconnected when there is no default network.
    The transport medium maps NetBearType (cellular/wifi/ethernet/bluetooth)
    to QNetworkInformation::TransportMedium.
    
    The initial state is computed at construction; later
    netAvailable/netCapabilitiesChange/netLost/netUnavailable events update
    it on the Qt thread.
    
    Task-number: QTBUG-147287
    Task-number: QTBUG-146821
    Pick-to: 6.12
    Change-Id: I267a134c4413e78d2ee8755a82d4463adba36077
    Reviewed-by: SanthoshKumar Selvaraj <santhosh.kumar.selvaraj@qt.io>
    Reviewed-by: Zbigniew Chyla <zbigniew.chyla@qt.io>

--- src/plugins/networkinformation/ohos/qohosnetworkinformationbackend.cpp
+++ src/plugins/networkinformation/ohos/qohosnetworkinformationbackend.cpp
@@ -3,7 +3,9 @@
 // Qt-Security score:significant reason:default
 
 #include <QtCore/qglobal.h>
+#include <QtCore/qloggingcategory.h>
 #include <QtNetwork/private/qnetworkinformation_p.h>
+#include <qohosnetconnection_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -30,6 +32,10 @@ public:
     static QNetworkInformation::Features featuresSupportedStatic();
 
 private:
+    void updateConnectivity();
+
+    QOhosSupplier<QtOhosNetConnection::NetState> m_netStateSupplier;
+
     Q_DISABLE_COPY_MOVE(QOhosNetworkInformationBackend)
 };
 
@@ -50,6 +56,39 @@ private:
     Q_DISABLE_COPY_MOVE(QOhosNetworkInformationBackendFactory)
 };
 
+QNetworkInformation::TransportMedium mapTransportMedium(QtOhosNetConnection::NetBearType bearerType)
+{
+    switch (bearerType) {
+    case QtOhosNetConnection::NetBearType::BEARER_CELLULAR:
+        return QNetworkInformation::TransportMedium::Cellular;
+    case QtOhosNetConnection::NetBearType::BEARER_WIFI:
+        return QNetworkInformation::TransportMedium::WiFi;
+    case QtOhosNetConnection::NetBearType::BEARER_BLUETOOTH:
+        return QNetworkInformation::TransportMedium::Bluetooth;
+    case QtOhosNetConnection::NetBearType::BEARER_ETHERNET:
+        return QNetworkInformation::TransportMedium::Ethernet;
+    case QtOhosNetConnection::NetBearType::BEARER_VPN:
+        return QNetworkInformation::TransportMedium::Unknown;
+    }
+
+    return QNetworkInformation::TransportMedium::Unknown;
+}
+
+QNetworkInformation::Reachability mapReachability(QtOhosNetConnection::NetworkReachability reachability)
+{
+    switch (reachability) {
+    case QtOhosNetConnection::NetworkReachability::Disconnected:
+        return QNetworkInformation::Reachability::Disconnected;
+    case QtOhosNetConnection::NetworkReachability::Local:
+        return QNetworkInformation::Reachability::Local;
+    case QtOhosNetConnection::NetworkReachability::Site:
+        return QNetworkInformation::Reachability::Site;
+    case QtOhosNetConnection::NetworkReachability::Online:
+        return QNetworkInformation::Reachability::Online;
+    }
+    return QNetworkInformation::Reachability::Unknown;
+}
+
 QString QOhosNetworkInformationBackend::name() const
 {
     return backendName();
@@ -62,10 +101,33 @@ QNetworkInformation::Features QOhosNetworkInformationBackend::featuresSupported(
 
 QNetworkInformation::Features QOhosNetworkInformationBackend::featuresSupportedStatic()
 {
-    return {};
+    return QNetworkInformation::Feature::Reachability
+        | QNetworkInformation::Feature::TransportMedium;
 }
 
-QOhosNetworkInformationBackend::QOhosNetworkInformationBackend() = default;
+QOhosNetworkInformationBackend::QOhosNetworkInformationBackend()
+    : m_netStateSupplier(QtOhosNetConnection::makeOhosNetStateDataSource(
+          [this](const QtOhosNetConnection::NetState &) {
+              updateConnectivity();
+          }))
+{
+    updateConnectivity();
+}
+
+void QOhosNetworkInformationBackend::updateConnectivity()
+{
+    const auto state = m_netStateSupplier();
+
+    setReachability(mapReachability(state.reachability));
+
+    auto transport = state.transport
+        ? mapTransportMedium(*state.transport)
+        : QNetworkInformation::TransportMedium::Unknown;
+    setTransportMedium(transport);
+
+    qCDebug(lcNetInfoOhos) << "updated reachability:" << reachability()
+        << "transport:" << transportMedium();
+}
 
 QString QOhosNetworkInformationBackendFactory::name() const
 {
