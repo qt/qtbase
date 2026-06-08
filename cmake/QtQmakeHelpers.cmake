1c55f381456c0b8812ff29beb6e34f281cff8fa1
Alexandru Croitor <alexandru.croitor@qt.io>
2026-06-08 18:23:44 +0200

    CMake: Fix drive letter extraction for qmake install prefix on Windows
    Starting with CMake 4.0+, the get_filename_component(REALPATH)
    function does not prepend a drive-letter on Windows for a drive-less
    path like '/Users/qt/work/install' unless the input path actually
    exists on disk.
    
    We relied on the drive-letter being prepended for such an install
    prefix because it's hard-coded into qmake for static builds which are
    not relocatable.
    
    To make the behavior work for both old and new CMake versions, replace
    the get_filename_component call with a new function that extracts the
    drive-letter depending on the CMake version.
    
    For >3.20, use cmake_path(GET + ROOT_NAME)
    For <3.20, use execute_process + cmd.exe + %CD% variable.
    
    Pick-to: 6.8
    Fixes: QTBUG-147326
    Change-Id: I2e3217a172084e6e8d7f9f5ae833b4b02bf051ac
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 94f8b34d76430e5506467ae3e1326d8a76132c8a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit c47432236587de2717930bd542eac74d3ab65c5f)

--- cmake/QtQmakeHelpers.cmake
+++ cmake/QtQmakeHelpers.cmake
@@ -48,16 +48,6 @@ function(qt_generate_qconfig_cpp in_file out_file)
          "${lib_location_absolute_path}" "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}")
     set(QT_CONFIGURE_LIBLOCATION_TO_PREFIX_PATH "${from_lib_location_to_prefix}")
 
-    # Ensure Windows drive letter is prepended to the install prefix hardcoded
-    # into qconfig.cpp, otherwise qmake can't find Qt modules in a static Qt
-    # build if there's no qt.conf. Mostly relevant for CI.
-    # Given input like
-    #    \work/qt/install
-    # or
-    #    \work\qt\install
-    # Expected output is something like
-    #   C:/work/qt/install
-    # so it includes a drive letter and forward slashes.
     if(QT_FEATURE_relocatable)
         # A relocatable Qt does not need a hardcoded prefix path.
         # This makes reproducible builds a closer reality, because we don't embed a CI path
@@ -65,10 +55,24 @@ function(qt_generate_qconfig_cpp in_file out_file)
         set(QT_CONFIGURE_PREFIX_PATH_STR "")
     else()
         set(QT_CONFIGURE_PREFIX_PATH_STR "${QT_BUILD_INTERNALS_RELOCATABLE_INSTALL_PREFIX}")
+
+        # Ensure a Windows drive letter is prepended to the install prefix hardcoded
+        # in qconfig.cpp, otherwise qmake can't find Qt modules in a static Qt
+        # build if there's no qt.conf. Mostly relevant for Qt's CI.
+        # Given input like
+        #    \work/qt/install
+        # or
+        #    \work\qt\install
+        # Expected output is something like
+        #   C:/work/qt/install
+        # so it includes a drive letter and forward slashes.
         if(CMAKE_HOST_WIN32)
-            get_filename_component(
-                QT_CONFIGURE_PREFIX_PATH_STR
-                "${QT_CONFIGURE_PREFIX_PATH_STR}" REALPATH)
+            file(TO_CMAKE_PATH "${QT_CONFIGURE_PREFIX_PATH_STR}" QT_CONFIGURE_PREFIX_PATH_STR)
+            # Only prepend the drive letter if not present.
+            if(QT_CONFIGURE_PREFIX_PATH_STR MATCHES "^/")
+                _qt_internal_get_current_build_dir_drive_letter_on_windows(drive_letter)
+                string(PREPEND QT_CONFIGURE_PREFIX_PATH_STR "${drive_letter}")
+            endif()
         endif()
     endif()
 
