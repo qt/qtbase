7e16efc816a97958bf11b9bf50225d614404846e
Karim Pinter <karim.pinter@qt.io>
2026-06-09 13:04:51 +0300

    VxWorks 26.03 fixes tst_QFile::openDirectory() fail on CI
    Using VxWorks 24.03 and 25.03 caused tst_QFile::openDirectory() to fail,
    on 26.03 it works, the QEXPECT_FAIL macro has been removed.
    
    Fixes: QTBUG-130074
    Change-Id: I225c9ab4a4688bd9f8e4445714b5fea266de55e9
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from commit edd0a78a48b23b2371b1d053405ef1fb4f6c1110)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/corelib/io/qfile/tst_qfile.cpp
+++ tests/auto/corelib/io/qfile/tst_qfile.cpp
@@ -3731,9 +3731,6 @@ void tst_QFile::openDirectory()
     f1.close();
     QVERIFY(!f1.open(QIODevice::ReadOnly|QIODevice::Unbuffered));
     f1.close();
-#if defined(Q_OS_VXWORKS)
-    QEXPECT_FAIL("", "QTBUG-130074: On VxWorks directories are always openable in a write mode", Abort);
-#endif
     QVERIFY(!f1.open(QIODevice::ReadWrite));
     f1.close();
     QVERIFY(!f1.open(QIODevice::WriteOnly));
