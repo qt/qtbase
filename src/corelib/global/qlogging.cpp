8ccc78834c94e859c368695ce7975a59de8d08bb
Marc Mutz <marc.mutz@qt.io>
2026-06-10 03:34:40 +0200

    Revert "qlogging: replace heap buffer with stack in win_outputDebugString_helper"
    This reverts commit 8bbdeaef85ead900d394f14a0fc4a4e9792982d2 (6.10).
    
    Reason for the revert: this function was flagged as exceeding 20KiB
    of stack use by -Wframe-larger-than=20KiB. It was also not protected
    against -ftrivial-auto-var-init initializing the stack buffer,
    contrary to what the reverted commit's message claimed. The heap
    buffer didn't have that problem, and no-one complained about the heap
    use for 14 Qt releases (since 5.14), so the risk is squarely on
    keeping the stack buffer, which, on Windows, uses 1/16th of total
    available stack, so revert to using the heap.
    
    An arguably more correct fix for this function would be to do the
    chunking inside the QString message, strtok()-style, not copy KiBs
    over KiBs of data just to null-terminate chunks, so this buffer, stack
    or heap, would be gone. But there is no consensus among QtCore devs at
    this point, so at least revert to the old status quo.
    
    Change-Id: Iec1191edb3988b4d43500969aef41f0789b818e5
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Aurélien Brooke <aurelien@bahiasoft.fr>
    (cherry picked from commit e315140ceb5931c3a8eb27161c2cef420eb9cec9)
    (cherry picked from commit c4f8b0da17bc0f0c961ac2a51749600559644d97)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/corelib/global/qlogging.cpp
+++ src/corelib/global/qlogging.cpp
@@ -2007,7 +2007,7 @@ static void win_outputDebugString_helper(const QString &message)
     if (message.length() <= maxOutputStringLength) {
         OutputDebugString(reinterpret_cast<const wchar_t *>(message.utf16()));
     } else {
-        wchar_t messagePart[maxOutputStringLength + 1];
+        wchar_t *messagePart = new wchar_t[maxOutputStringLength + 1];
         for (qsizetype i = 0; i < message.length(); i += maxOutputStringLength) {
             const qsizetype length = qMin(message.length() - i, maxOutputStringLength);
             const qsizetype len = QStringView{message}.mid(i, length).toWCharArray(messagePart);
@@ -2015,6 +2015,7 @@ static void win_outputDebugString_helper(const QString &message)
             messagePart[len] = 0;
             OutputDebugString(messagePart);
         }
+        delete[] messagePart;
     }
 }
 
