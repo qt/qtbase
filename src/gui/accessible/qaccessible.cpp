471f43dd14dabd45b4ff2987289bd842fa4ada54
Kai Koehne <kai.koehne@qt.io>
2026-06-15 09:19:18 +0000

    Gui: Add \since 6.12 to QAccessible::Event::ScrollingPositionChanged
    Amends documentation introduced in ce460b99e9bd6969203a381dd0ff42056b3fca42.
    
    Change-Id: If211c04905dae15d912855d39abbfbb472aa33db
    Reviewed-by: MohammadHossein Qanbari <mohammad.qanbari@qt.io>
    (cherry picked from commit c773e71aa9d85571d07d474c0ff8326f5e2c3ae0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/accessible/qaccessible.cpp
+++ src/gui/accessible/qaccessible.cpp
@@ -236,7 +236,8 @@ Q_STATIC_LOGGING_CATEGORY(lcAccessibilityCore, "qt.accessibility.core");
     \value ScrollingStart                   A scrollbar scroll operation is about to start; this may
                                             be caused by a mouse press on the slider handle, for
                                             example.
-    \value ScrollingPositionChanged         The position changed during a scroll operation.
+    \value [since 6.12] ScrollingPositionChanged
+                                            The position changed during a scroll operation.
     \value SectionChanged
     \value SelectionAdd                     An item has been added to the selection in an item view.
     \value SelectionRemove                  An item has been removed from an item view selection.
