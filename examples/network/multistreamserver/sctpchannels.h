6bba4a457214e3dc1cd6eaccd5d2d431d8423c9b
Joerg Bornemann <joerg.bornemann@qt.io>
2026-06-17 12:17:44 +0200

    Make Qt Network examples self-contained
    The multistreamserver and multistreamclient examples shared a
    sctpchannels.h through a sibling "shared" directory. This violates
    QUIP-13 and prevents Qt Creator and the VS Code extension from copying
    an example out of the example tree.
    
    The shared header is a tiny channel-id enum. Copy it into each example
    so both build standalone from their own directory, and drop the
    CMakeLists.txt.shared junction workaround that no longer helps.
    
    Fixes: QTBUG-147527
    Change-Id: I4984e1563fa1648a8fdd9f8c47c2569c0488d53e
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    (cherry picked from commit 388d42bb981ea6fa6d81fd19741831ae4581def1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2d0c19aac49ce90bcb406dce13aaf9a19836e59c)

---
+++ examples/network/multistreamserver/sctpchannels.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2016 Alex Trotsenko <alex1973tr@gmail.com>
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#ifndef SCTPCHANNELS_H
+#define SCTPCHANNELS_H
+
+enum SctpChannels {
+    Movie = 0,
+    Time = 1,
+    Chat = 2,
+
+    NumberOfChannels = 3
+};
+
+#endif
