qt/qtbase

commit 4fc36aa558b7b79bdf1171cd49c22398e885b1dd

iOS: Enable building platform plugin as shared library
The main wrapper logic has been split off and moved into the entrypoint
library, which is still static, and should only be enabled for the main
executable.

The iOS platform plugin and optional photo picker plugin are now built
as shared libraries if the Qt build is configured as such, allowing
them to be shared between a main application executable and bundled
application extensions, without bloating the app bundle.

This patch also helps for the use-case of merging a statically
built Qt into a single shared library, which will no longer cause
linker errors due to undefined _main symbol, as we can safely
link in the static platform plugin without bringing in the main
wrapper logic that now lives in the entrypoint library.

visionOS still requires static Qt builds, as the Swift entrypoint
machinery has not yet been untangled.

Task-number: QTBUG-85974
Task-number: QTBUG-62812
Task-number: QTBUG-107857
Task-number: QTBUG-42937
Task-number: QTBUG-140035
Task-number: QTBUG-139942
Change-Id: Ief0861c631dc49a4683ca25836d9e68899758a25
Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/plugins/platforms/ios/qiosintegration.mm
@@ -17,6 +17,7 @@
 #include "qiostheme.h"
 #include "qiosservices.h"
 #include "qiosoptionalplugininterface.h"
+#include "qiosapplicationdelegate.h"
 
 #if defined(Q_OS_VISIONOS)
 #include "qiosswiftintegration.h"
@@ -112,6 +113,12 @@ void QIOSIntegration::initialize()
     qsizetype size = QList<QPluginParsedMetaData>(m_optionalPlugins->metaData()).size();
     for (qsizetype i = 0; i < size; ++i)
         qobject_cast<QIosOptionalPluginInterface *>(m_optionalPlugins->instance(i))->initPlugin();
+
+    if (QIOSEventDispatcher::isQtApplication()) {
+        qCDebug(lcQpaWindowScene) << "Replacing application delegate"
+            << UIApplication.sharedApplication.delegate << "with QIOSApplicationDelegate";
+        UIApplication.sharedApplication.delegate = [QIOSApplicationDelegate new];
+    }
 }
 
 QIOSIntegration::~QIOSIntegration()

cmake/QtAutoDetectHelpers.cmake
@@ -412,7 +412,7 @@ function(qt_auto_detect_apple)
     endif()
 
     if(CMAKE_SYSTEM_NAME STREQUAL iOS OR CMAKE_SYSTEM_NAME STREQUAL visionOS)
-        if(NOT DEFINED BUILD_SHARED_LIBS)
+        if(NOT DEFINED BUILD_SHARED_LIBS OR CMAKE_SYSTEM_NAME STREQUAL visionOS)
             qt_internal_ensure_static_qt_config()
         endif()
 

src/corelib/CMakeLists.txt
@@ -1688,6 +1688,10 @@ if(APPLE AND QT_FEATURE_framework AND QT_FEATURE_separate_debug_info)
     )
 endif()
 
+qt_internal_extend_target(Core CONDITION UIKIT
+    SOURCES platform/ios/qiosrunloopintegration_p.h
+)
+
 if(IOS)
     qt_internal_set_apple_privacy_manifest(Core
         "${CMAKE_CURRENT_SOURCE_DIR}/platform/ios/PrivacyInfo.xcprivacy")

src/corelib/platform/ios/qiosrunloopintegration_p.h
@@ -1,0 +1,28 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
+#ifndef QIOSRUNLOOPINTEGRATION_P_H
+#define QIOSRUNLOOPINTEGRATION_P_H
+
+#import <UIKit/UIApplication.h>
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. This header file may change
+// from version to version without notice, or even be removed.
+//
+// We mean it.
+//
+
+@interface UIApplication (QtRunLoopIntegration)
++ (bool)qt_rootLevelRunLoopIntegration;
++ (bool)qt_eventDispatcherEnteredProcessEvents;
++ (void)qt_eventDispatcherInterruptEventLoopExec;
++ (bool)qt_applicationAboutToTerminate;
++ (char)qt_applicationWillTerminateExitCode;
+@end
+
+#endif // QIOSRUNLOOPINTEGRATION_P_H

src/entrypoint/CMakeLists.txt
@@ -59,7 +59,7 @@ set_target_properties(EntryPointPrivate PROPERTIES
     INTERFACE_COMPILE_DEFINITIONS ""
 )
 
-if(WIN32)
+if(WIN32 OR UIKIT)
     # Not all platforms require the static library
     set(using_entrypoint_library "yes")
 endif()
@@ -69,6 +69,7 @@ if(using_entrypoint_library)
     qt_internal_add_cmake_library(EntryPointImplementation STATIC
         INCLUDE_DIRECTORIES
            $<TARGET_PROPERTY:Qt::Core,INTERFACE_INCLUDE_DIRECTORIES>
+           $<TARGET_PROPERTY:Qt::CorePrivate,INTERFACE_INCLUDE_DIRECTORIES>
     )
 
     list(APPEND export_targets EntryPointImplementation)
@@ -113,6 +114,12 @@ if(WIN32)
 endif()
 
 if(UIKIT)
+    qt_internal_extend_target(EntryPointImplementation
+        SOURCES qtentrypoint_ios.mm
+        LIBRARIES
+          ${FWFoundation}
+          ${FWUIKit}
+    )
     set_target_properties(EntryPointPrivate PROPERTIES
         INTERFACE_LINK_OPTIONS "-Wl,-e,_qt_main_wrapper"
     )

src/plugins/platforms/ios/CMakeLists.txt
@@ -20,7 +20,6 @@ endif()
 
 qt_internal_add_plugin(QIOSIntegrationPlugin
     OUTPUT_NAME qios
-    STATIC # Force static, even in shared builds
     DEFAULT_IF "ios" IN_LIST QT_QPA_PLATFORMS
     PLUGIN_TYPE platforms
     SOURCES

src/plugins/platforms/ios/optional/nsphotolibrarysupport/CMakeLists.txt
@@ -7,7 +7,6 @@
 
 qt_internal_add_plugin(QIosOptionalPlugin_NSPhotoLibraryPlugin
     OUTPUT_NAME qiosnsphotolibrarysupport
-    STATIC # Force static, even in shared builds
     PLUGIN_TYPE platforms/darwin
     CLASS_NAME QIosOptionalPlugin_NSPhotoLibrary
     DEFAULT_IF FALSE

commit cf0c7d2000ada263143d6f6f6200e496b308a85d

visionOS: Don't override application delegate with QIOSApplicationDelegate
The issue was introduced in 4fc36aa558b7b79bdf1171cd49c22398e885b1dd,
which now creates an application delegate in
QIOSIntegration::initialize() implicitly (for isQtApplication),
this causes the application delegate for the  Swift entry point to be
replaced, which means the necessary code for setting up the immersive
space in QtQuick3D never happens and the application appears to hang.

Pick-to: 6.12
Fixes: QTBUG-147559
Change-Id: I33966ce56957a2d23358fb8c079273cd2ac402e2
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/plugins/platforms/ios/qiosintegration.mm
@@ -104,11 +104,15 @@ void QIOSIntegration::initialize()
     for (qsizetype i = 0; i < size; ++i)
         qobject_cast<QIosOptionalPluginInterface *>(m_optionalPlugins->instance(i))->initPlugin();
 
+    // On visionOS the Swift entry point is used, which sets up the application
+    // delegate itself, so we don't want to install the QIOSApplicationDelegate
+#if !defined(Q_OS_VISIONOS)
     if (QIOSEventDispatcher::isQtApplication()) {
         qCDebug(lcQpaWindowScene) << "Replacing application delegate"
             << UIApplication.sharedApplication.delegate << "with QIOSApplicationDelegate";
         UIApplication.sharedApplication.delegate = [QIOSApplicationDelegate new];
     }
+#endif
 }
 
 QIOSIntegration::~QIOSIntegration()