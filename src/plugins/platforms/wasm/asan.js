e896f6510cff4f1f0137e2303fefc7cc2f5a3be9
Even Oscar Andersen <even.oscar.andersen@qt.io>
2026-06-21 09:51:39 +0200

    wasm: drop sourcemap for sanitizer, add ubsan options variable
    We cannot use -gsource-map - we run out of memory.
    Fix this by replacing it with -g3
    
    Module.UBSAN_OPTIONS was missing.
    Fix this by adding the variable.
    
    sanitize-recover argument was incomplete.
    Fix this by changing to -sanitize-recover=all
    
    Change-Id: I44feff820c4e4cbc76f8a28e5678aaaa693a09fd
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/plugins/platforms/wasm/asan.js
+++ src/plugins/platforms/wasm/asan.js
@@ -1,2 +1,3 @@
 // In particular log_path is important, otherwise there will not be any output
-Module.ASAN_OPTIONS='verbosity=1:log_path=stdout:halt_on_error=0:debug=1:check_initialization_order=1:detect_stack_use_after_return=1:print_full_thread_history=1:strict_string_checks=1:detect_invalid_pointer_pairs=5:symbolize=1:fast_unwind_on_malloc=0';
+Module.ASAN_OPTIONS= 'log_path=stdout:abort_on_error=0:halt_on_error=0:print_stacktrace=1:keep_going=1:verbosity=1:debug=1:check_initialization_order=1:detect_stack_use_after_return=1:detect_stack_use_after_scope=1:print_full_thread_history=1:strict_string_checks=1:detect_invalid_pointer_pairs=5:symbolize=1:fast_unwind_on_malloc=0';
+Module.UBSAN_OPTIONS='log_path=stdout:abort_on_error=0:halt_on_error=0:print_stacktrace=1:keep_going=1';
