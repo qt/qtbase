qt/qtbase

commit b0f495735c30e400c9749c8b9f9823dbb56dfbd9

QMetaType: remove legacy automatic name registrations for QObject-like
This is wrong for cv-qualified T, because T::staticMetaObject is the
same variable as the non-cv-qualified one, but isn't qualified. That
causes a registration of `const MyObject *` to also register `MyObject
*` with the same metatype.

Even where it's right, it's unnecessary because we already have the
names. This code was mostly added in 2012 for Qt 5.0; the only one that
is still necessary of this lot is the case of pointer-to-gadget that
registers the gadget itself.

This removes the call to qRegisterNormalizedMetaTypeImplementation(),
which has side-effects, but none of the types this matched are
sequential containers, associative containers, pairs, smart pointers or
QFuture.

Fixes: QTBUG-147588
Pick-to: 6.12 6.11 6.8
Change-Id: I50b2a3e916ef23f21ebdfffdaf4d90317668802a
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/corelib/kernel/qmetatype.h
@@ -1227,10 +1227,7 @@ namespace QtPrivate
 } // namespace QtPrivate
 
 template <typename T, int =
-    QtPrivate::IsPointerToTypeDerivedFromQObject<T>::Value ? QMetaType::PointerToQObject :
-    QtPrivate::IsRealGadget<T>::value                      ? QMetaType::IsGadget :
-    QtPrivate::IsPointerToGadgetHelper<T>::IsRealGadget    ? QMetaType::PointerToGadget :
-    QtPrivate::IsQEnumHelper<T>::Value                     ? QMetaType::IsEnumeration : 0>
+    QtPrivate::IsPointerToGadgetHelper<T>::IsRealGadget    ? QMetaType::PointerToGadget : 0>
 struct QMetaTypeIdQObject
 {
     enum {
@@ -1420,47 +1417,6 @@ inline int qRegisterMetaType(QMetaType meta)
 
 #ifndef QT_NO_QOBJECT
 template <typename T>
-struct QMetaTypeIdQObject<T*, QMetaType::PointerToQObject>
-{
-    enum {
-        Defined = 1
-    };
-
-    static int qt_metatype_id()
-    {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *const cName = T::staticMetaObject.className();
-        QByteArray typeName;
-        typeName.reserve(strlen(cName) + 1);
-        typeName.append(cName).append('*');
-        const int newId = qRegisterNormalizedMetaType<T *>(typeName);
-        metatype_id.storeRelease(newId);
-        return newId;
-    }
-};
-
-template <typename T>
-struct QMetaTypeIdQObject<T, QMetaType::IsGadget>
-{
-    enum {
-        Defined = std::is_default_constructible<T>::value
-    };
-
-    static int qt_metatype_id()
-    {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *const cName = T::staticMetaObject.className();
-        const int newId = qRegisterNormalizedMetaType<T>(cName);
-        metatype_id.storeRelease(newId);
-        return newId;
-    }
-};
-
-template <typename T>
 struct QMetaTypeIdQObject<T*, QMetaType::PointerToGadget>
 {
     enum {
@@ -1469,39 +1425,7 @@ struct QMetaTypeIdQObject<T*, QMetaType::PointerToGadget>
 
     static int qt_metatype_id()
     {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *const cName = T::staticMetaObject.className();
-        QByteArray typeName;
-        typeName.reserve(strlen(cName) + 1);
-        typeName.append(cName).append('*');
-        const int newId = qRegisterNormalizedMetaType<T *>(typeName);
-        metatype_id.storeRelease(newId);
-        return newId;
-    }
-};
-
-template <typename T>
-struct QMetaTypeIdQObject<T, QMetaType::IsEnumeration>
-{
-    enum {
-        Defined = 1
-    };
-
-    static int qt_metatype_id()
-    {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *eName = qt_getEnumName(T());
-        const char *cName = qt_getEnumMetaObject(T())->className();
-        QByteArray typeName;
-        typeName.reserve(strlen(cName) + 2 + strlen(eName));
-        typeName.append(cName).append("::").append(eName);
-        const int newId = qRegisterNormalizedMetaType<T>(typeName);
-        metatype_id.storeRelease(newId);
-        return newId;
+        return QMetaType::fromType<T *>().id();
     }
 };
 #endif

tests/auto/corelib/kernel/qmetatype/tst_qmetatype.cpp
@@ -2445,6 +2445,32 @@ void tst_QMetaType::metaObject()
     QCOMPARE(!!(mt.flags() & QMetaType::PointerToQObject), isQObjectPtr);
 }
 
+class QObjectTypeRegisteredAsConst : public QObject
+{
+    Q_OBJECT
+};
+void tst_QMetaType::constQObjectPointer()
+{
+    static bool alreadyRun = false;
+
+    // Register first as const and ensure that the non-const did not get
+    // registered as the same metatype (see QTBUG-147588).
+    QMetaType cmt = QMetaType::fromType<const QObjectTypeRegisteredAsConst *>();
+    int cid = cmt.id();
+    QCOMPARE_NE(cid, -1);
+    QCOMPARE(cmt.name(), "const QObjectTypeRegisteredAsConst*");
+
+    if (!alreadyRun) {
+        // the non-const should not have got registered
+        QVERIFY(!QMetaType::fromName("QObjectTypeRegisteredAsConst *").isValid());
+    }
+
+    QMetaType mt = QMetaType::fromType<QObjectTypeRegisteredAsConst *>();
+    int id = mt.id();
+    alreadyRun = true;
+    QCOMPARE_NE(id, cid);
+}
+
 #define METATYPE_ID_FUNCTION(Type, MetaTypeId, Name) \
   case ::qMetaTypeId< Name >(): metaType = MetaTypeIdStruct<MetaTypeId>::Value; break;
 

tests/auto/corelib/kernel/qmetatype/tst_qmetatype.h
@@ -92,6 +92,7 @@ private slots:
     void saveAndLoadCustom();
     void metaObject_data();
     void metaObject();
+    void constQObjectPointer();
     void constexprMetaTypeIds();
 
     // tst_qmetatype2.cpp

commit d71357367a103bf355531b2359fbdb09abbc5a79

QMetaType: Restore converter registration
The changes in b0f495735c30e400c9749c8b9f9823dbb56dfbd9 broke the
automatic registration of converter functions.
Fix this by broadening the (compile time) condition in the
SequentialValueTypeIsMetaType and AssociativeValueTypeIsMetaType
templates.
The same fix is also needed for std::pair.

Pick-to: 6.12
Change-Id: Ib71fd0ade0d3ea571a060738995444663d786a2f
Reviewed-by: Andreas Hartmetz <ahartmetz@gmail.com>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/corelib/kernel/qmetatype.h
@@ -1118,6 +1118,23 @@ namespace QtPrivate
         enum { Value = false };
     };
 
+    /* Used to check whether we need to register a converter function
+       for associative and sequential containers.
+       The rule is that for any registered container (template), we register
+       the conversion function if the element type is "defined" in the sense
+       of QMetaTypeId2, or if it's a pointer to a QObject subclass or a
+       default-constructible gadget (for the rare non-default constructible gadgets,
+       we can't create the converter.
+    */
+    template<typename T>
+    struct IsContainerElementWhichNeedsConverter
+    {
+        static constexpr bool value =
+                QMetaTypeId2<T>::Defined
+                || IsPointerToTypeDerivedFromQObject<T>::Value
+                || (IsRealGadget<T>::value && std::is_default_constructible_v<T>);
+    };
+
     template<typename T, bool = QtPrivate::IsSequentialContainer<T>::Value>
     struct SequentialContainerTransformationHelper
     {
@@ -1132,7 +1149,7 @@ namespace QtPrivate
         }
     };
 
-    template<typename T, bool = QMetaTypeId2<typename T::value_type>::Defined>
+    template<typename T, bool = IsContainerElementWhichNeedsConverter<typename T::value_type>::value>
     struct SequentialValueTypeIsMetaType
     {
         static bool registerConverter()
@@ -1165,7 +1182,7 @@ namespace QtPrivate
         }
     };
 
-    template<typename T, bool = QMetaTypeId2<typename T::key_type>::Defined>
+    template<typename T, bool = IsContainerElementWhichNeedsConverter<typename T::key_type>::value>
     struct AssociativeKeyTypeIsMetaType
     {
         static bool registerConverter()
@@ -1179,7 +1196,7 @@ namespace QtPrivate
         }
     };
 
-    template<typename T, bool = QMetaTypeId2<typename T::mapped_type>::Defined>
+    template<typename T, bool = IsContainerElementWhichNeedsConverter<typename T::mapped_type>::value>
     struct AssociativeMappedTypeIsMetaType
     {
         static bool registerConverter()
@@ -1198,8 +1215,8 @@ namespace QtPrivate
     {
     };
 
-    template<typename T, bool = QMetaTypeId2<typename T::first_type>::Defined
-                                && QMetaTypeId2<typename T::second_type>::Defined>
+    template<typename T, bool = IsContainerElementWhichNeedsConverter<typename T::first_type>::value
+                                && IsContainerElementWhichNeedsConverter<typename T::second_type>::value>
     struct IsMetaTypePair
     {
         static bool registerConverter()
@@ -1543,7 +1560,7 @@ template <typename T> \
 struct QMetaTypeId< SINGLE_ARG_TEMPLATE<T> > \
 { \
     enum { \
-        Defined = QMetaTypeId2<T>::Defined \
+        Defined = QtPrivate::IsContainerElementWhichNeedsConverter<T>::value \
     }; \
     static int qt_metatype_id() \
     { \
@@ -1571,7 +1588,8 @@ template<typename T, typename U> \
 struct QMetaTypeId< DOUBLE_ARG_TEMPLATE<T, U> > \
 { \
     enum { \
-        Defined = QMetaTypeId2<T>::Defined && QMetaTypeId2<U>::Defined \
+        Defined = QtPrivate::IsContainerElementWhichNeedsConverter<T>::value \
+            && QtPrivate::IsContainerElementWhichNeedsConverter<U>::value \
     }; \
     static int qt_metatype_id() \
     { \

tests/auto/corelib/kernel/qmetatype/tst_qmetatype.cpp
@@ -2005,6 +2005,65 @@ void tst_QMetaType::enumAndFlagNamesAreRegisteredLookupByName()
     }
 }
 
+void tst_QMetaType::containerOfConvertibleMetaTypeIsIterable()
+{
+    // Containers whose value/key/mapped type is not a compile-time
+    // QMetaTypeId2 type (no Q_DECLARE_METATYPE) must still be viewable as a
+    // QMetaSequence / QMetaAssociation, otherwise they cannot be iterated when
+    // only the metatype is known (e.g. when exposed to QML). This covers the
+    // element kinds that are treated as covertible metatypes without a
+    // declaration: QObject-derived pointers and default-constructible gadgets.
+    const auto sequenceCanConvert = [](QMetaType container) {
+        container.registerType(); // register the container and its converters
+        const QMetaType iterable = QMetaType::fromType<QIterable<QMetaSequence>>();
+        return QMetaType::canConvert(container, iterable)
+                && QMetaType::canView(container, iterable);
+    };
+    const auto associationCanConvert = [](QMetaType container) {
+        container.registerType();
+        const QMetaType iterable = QMetaType::fromType<QIterable<QMetaAssociation>>();
+        return QMetaType::canConvert(container, iterable);
+    };
+    // A std::pair gets a converter to the pair/variant
+    // interface registered under the same condition.
+    const auto pairCanConvert = [](QMetaType pair) {
+        pair.registerType();
+        return QMetaType::canConvert(
+                pair, QMetaType::fromType<QtMetaTypePrivate::QPairVariantInterfaceImpl>());
+    };
+
+    // Sequential containers.
+    // QObject-derived pointers without Q_DECLARE_METATYPE.
+    QVERIFY(sequenceCanConvert(QMetaType::fromType<QList<Derived *>>()));
+    QVERIFY(sequenceCanConvert(QMetaType::fromType<QList<CustomQObject *>>()));
+    QVERIFY(sequenceCanConvert(QMetaType::fromType<std::vector<Derived *>>()));
+    // Default-constructible gadget by value.
+    QVERIFY(sequenceCanConvert(QMetaType::fromType<QList<CustomGadget>>()));
+    // Controls: kinds that were never affected.
+    QVERIFY(sequenceCanConvert(QMetaType::fromType<QList<CustomGadget *>>()));   // pointer to gadget
+    QVERIFY(sequenceCanConvert(QMetaType::fromType<QList<CustomObject *>>()));   // Q_DECLARE_METATYPE'd
+    QVERIFY(sequenceCanConvert(QMetaType::fromType<QList<QObject *>>()));        // built-in
+    // Negative control: a gadget that is not default-constructible was never
+    // an implicit metatype and must stay non-iterable.
+    QVERIFY(!sequenceCanConvert(QMetaType::fromType<QList<CustomGadget_NonDefaultConstructible>>()));
+
+    // Associative containers (key and mapped covered independently).
+    QVERIFY(associationCanConvert(QMetaType::fromType<QMap<QString, Derived *>>()));   // mapped: QObject ptr
+    QVERIFY(associationCanConvert(QMetaType::fromType<QMap<Derived *, QString>>()));   // key: QObject ptr
+    QVERIFY(associationCanConvert(QMetaType::fromType<QHash<QString, CustomGadget>>())); // mapped: gadget
+    QVERIFY(associationCanConvert(QMetaType::fromType<QMap<QString, CustomObject *>>())); // control
+    QVERIFY(!associationCanConvert(
+            QMetaType::fromType<QMap<QString, CustomGadget_NonDefaultConstructible>>()));
+
+    // std::pair, covering first and second independently.
+    QVERIFY(pairCanConvert(QMetaType::fromType<std::pair<Derived *, int>>()));        // first: QObject ptr
+    QVERIFY(pairCanConvert(QMetaType::fromType<std::pair<int, Derived *>>()));        // second: QObject ptr
+    QVERIFY(pairCanConvert(QMetaType::fromType<std::pair<CustomGadget, int>>()));     // first: gadget
+    QVERIFY(pairCanConvert(QMetaType::fromType<std::pair<int, CustomObject *>>()));   // control
+    QVERIFY(!pairCanConvert(
+            QMetaType::fromType<std::pair<CustomGadget_NonDefaultConstructible, int>>()));
+}
+
 void tst_QMetaType::isRegisteredStaticLess_data()
 {
     isRegistered_data();

tests/auto/corelib/kernel/qmetatype/tst_qmetatype.h
@@ -86,6 +86,7 @@ private slots:
     void isNotRegistered();
     void isEnum();
     void enumAndFlagNamesAreRegisteredLookupByName();
+    void containerOfConvertibleMetaTypeIsIterable();
     void underlyingType_data();
     void underlyingType();
     void automaticTemplateRegistration_1();