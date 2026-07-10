50843f58cbc886ac03ac95bfd323d58d7ed22c72
Zbigniew Chyla <zbigniew.chyla@qt.io>
2026-07-10 09:49:44 +0200

    ohos: AppKit: add window utils API
    Expose the OpenHarmony window hints to applications through
    QtOhosAppKit::Window in qohoswindowutils.h. The functions set the dynamic
    properties defined by the QtGui-private window-hint contract; the platform
    plugin reads them. QWindow and QWidget overloads are provided so a hint can
    be applied to a widget before its window exists.
    
    The function names and signatures mirror the Qt5 qtohosextras QtOhosExtras
    window API so migrating an application is a matter of changing the namespace
    qualifier (QtOhosExtras -> QtOhosAppKit::Window), and the documented
    OpenHarmony limitations are carried over. Render-fit is intentionally not
    exposed here: like Qt5, it is only for other Qt modules (via the private
    QOhosWindowHints API).
    
    Task-number: QTBUG-147194
    Change-Id: I4523e23ce12d5acf084c196db70b80a3419291e5
    Reviewed-by: SanthoshKumar Selvaraj <santhosh.kumar.selvaraj@qt.io>
    (cherry picked from commit c4a9a476ff4f1d58aff762ecfd88109575b9f48c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/plugins/platforms/ohos/ohosappkit/qohoswindowutils.h
@@ -0,0 +1,41 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QOHOSWINDOWUTILS_H
+#define QOHOSWINDOWUTILS_H
+
+#include <QtGui/qcolor.h>
+#include <QtGui/qwindow.h>
+#include <QtOhosAppKit/qtohosappkitglobal.h>
+#include <QtWidgets/qwidget.h>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtOhosAppKit {
+
+namespace Window {
+
+Q_OHOSAPPKIT_EXPORT void setShowWindowAsFloatWindowHint(QWindow *window, bool showAsFloatWindow);
+Q_OHOSAPPKIT_EXPORT void setShowWindowAsFloatWindowHint(QWidget *widget, bool showAsFloatWindow);
+
+Q_OHOSAPPKIT_EXPORT void setWindowPrivacyMode(QWindow *window, bool privacyModeEnabled);
+
+Q_OHOSAPPKIT_EXPORT void setSurfaceBackgroundColor(QWindow *window, const QColor &color);
+Q_OHOSAPPKIT_EXPORT void setSurfaceBackgroundColor(QWidget *widget, const QColor &color);
+
+Q_OHOSAPPKIT_EXPORT void setWindowCornerRadius(QWindow *window, double radius);
+Q_OHOSAPPKIT_EXPORT void setWindowCornerRadius(QWidget *widget, double radius);
+
+Q_OHOSAPPKIT_EXPORT void setWindowKeepScreenOn(QWindow *window, bool keepScreenOn);
+Q_OHOSAPPKIT_EXPORT void setWindowKeepScreenOn(QWidget *widget, bool keepScreenOn);
+
+Q_OHOSAPPKIT_EXPORT void setWindowDragResizable(QWindow *window, bool dragResizable);
+Q_OHOSAPPKIT_EXPORT void setWindowDragResizable(QWidget *widget, bool dragResizable);
+
+}
+
+}
+
+QT_END_NAMESPACE
+
+#endif // QOHOSWINDOWUTILS_H
