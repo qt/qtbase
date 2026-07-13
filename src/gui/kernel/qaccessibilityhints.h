363bca2882559de5650d41a18cc09a4012516993
Volker Hilsheimer <volker.hilsheimer@qt.io>
2026-07-13 09:52:45 +0200

    QAccessibilityHints: Remove FINAL from new motionPreference property
    New properties cannot be made FINAL without potentially breaking
    existing code.
    
    Amends 444b9f854e0a7aca1c066cb91d66b8f307d9a2bc to address header
    review comment.
    
    Pick-to: 6.12
    Task-number: QTBUG-146286
    Change-Id: I148f9655ab2c97684efc2b1e309c2488a0c31aa9
    Reviewed-by: MohammadHossein Qanbari <mohammad.qanbari@qt.io>

--- src/gui/kernel/qaccessibilityhints.h
+++ src/gui/kernel/qaccessibilityhints.h
@@ -17,7 +17,7 @@ class Q_GUI_EXPORT QAccessibilityHints : public QObject
     Q_OBJECT
     Q_DECLARE_PRIVATE(QAccessibilityHints)
     Q_PROPERTY(Qt::ContrastPreference contrastPreference READ contrastPreference NOTIFY contrastPreferenceChanged FINAL REVISION(6, 10))
-    Q_PROPERTY(Qt::MotionPreference motionPreference READ motionPreference NOTIFY motionPreferenceChanged FINAL REVISION(6, 12))
+    Q_PROPERTY(Qt::MotionPreference motionPreference READ motionPreference NOTIFY motionPreferenceChanged REVISION(6, 12))
 
 public:
     explicit QAccessibilityHints(QObject *parent = nullptr);
