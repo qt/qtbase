d68d099acfdc3de37774f3564335d3495930e49d
Assam Boudjelthia <assam.boudjelthia@qt.io>
2026-07-18 00:04:02 +0300

    Android: init both QPA stop and exit cleanup semaphores
    The check joined the two sem_init() calls with &&, so when the
    first succeeded the second was never evaluated, leaving
    m_stopQtSemaphore uninitialised, and the error was reported only
    if both failed. Use || so both initialise and either failure is
    caught.
    
    Fixes: QTBUG-148435
    Pick-to: 6.11 6.8
    Change-Id: I7bc6d14ca4f5f7418f7b20f5d4480ddad27bf8dd
    Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>
    (cherry picked from commit 45f2ff50da64b0f544961dba7e09c3419b127990)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/android/androidjnimain.cpp
+++ src/plugins/platforms/android/androidjnimain.cpp
@@ -408,7 +408,7 @@ static bool initAndroidQpaPlugin(JNIEnv *jenv, jobject object)
 
     m_backendRegister = new AndroidBackendRegister();
 
-    if (sem_init(&m_exitSemaphore, 0, 0) == -1 && sem_init(&m_stopQtSemaphore, 0, 0) == -1) {
+    if (sem_init(&m_exitSemaphore, 0, 0) == -1 || sem_init(&m_stopQtSemaphore, 0, 0) == -1) {
         qCritical() << "Failed to init Qt application cleanup semaphores";
         return false;
     }
