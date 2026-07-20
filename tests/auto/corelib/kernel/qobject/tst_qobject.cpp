qt/qtbase

commit b0f495735c30e400c9749c8b9f9823dbb56dfbd9

QMetaType: remove legacy automatic name registrations for QObject-like
This is wrong for cv-qualified T, because T::staticMetaObject is the
same variable as the non-cv-qualified one, but isn't qualified. That
causes a registration of `const MyObject *` to also register `MyObject
*` with the same metatype.

Even where it's right, it's unnecessary because we already have the
names. This code was mostly added in 2012 for Qt 5.0; the only one that
is still necessary of this lot is the case of pointer-to-gadget that
registers the gadget itself.

This removes the call to qRegisterNormalizedMetaTypeImplementation(),
which has side-effects, but none of the types this matched are
sequential containers, associative containers, pairs, smart pointers or
QFuture.

Fixes: QTBUG-147588
Pick-to: 6.12 6.11 6.8
Change-Id: I50b2a3e916ef23f21ebdfffdaf4d90317668802a
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/corelib/kernel/qmetatype.h
@@ -1227,10 +1227,7 @@ namespace QtPrivate
 } // namespace QtPrivate
 
 template <typename T, int =
-    QtPrivate::IsPointerToTypeDerivedFromQObject<T>::Value ? QMetaType::PointerToQObject :
-    QtPrivate::IsRealGadget<T>::value                      ? QMetaType::IsGadget :
-    QtPrivate::IsPointerToGadgetHelper<T>::IsRealGadget    ? QMetaType::PointerToGadget :
-    QtPrivate::IsQEnumHelper<T>::Value                     ? QMetaType::IsEnumeration : 0>
+    QtPrivate::IsPointerToGadgetHelper<T>::IsRealGadget    ? QMetaType::PointerToGadget : 0>
 struct QMetaTypeIdQObject
 {
     enum {
@@ -1420,47 +1417,6 @@ inline int qRegisterMetaType(QMetaType meta)
 
 #ifndef QT_NO_QOBJECT
 template <typename T>
-struct QMetaTypeIdQObject<T*, QMetaType::PointerToQObject>
-{
-    enum {
-        Defined = 1
-    };
-
-    static int qt_metatype_id()
-    {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *const cName = T::staticMetaObject.className();
-        QByteArray typeName;
-        typeName.reserve(strlen(cName) + 1);
-        typeName.append(cName).append('*');
-        const int newId = qRegisterNormalizedMetaType<T *>(typeName);
-        metatype_id.storeRelease(newId);
-        return newId;
-    }
-};
-
-template <typename T>
-struct QMetaTypeIdQObject<T, QMetaType::IsGadget>
-{
-    enum {
-        Defined = std::is_default_constructible<T>::value
-    };
-
-    static int qt_metatype_id()
-    {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *const cName = T::staticMetaObject.className();
-        const int newId = qRegisterNormalizedMetaType<T>(cName);
-        metatype_id.storeRelease(newId);
-        return newId;
-    }
-};
-
-template <typename T>
 struct QMetaTypeIdQObject<T*, QMetaType::PointerToGadget>
 {
     enum {
@@ -1469,39 +1425,7 @@ struct QMetaTypeIdQObject<T*, QMetaType::PointerToGadget>
 
     static int qt_metatype_id()
     {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *const cName = T::staticMetaObject.className();
-        QByteArray typeName;
-        typeName.reserve(strlen(cName) + 1);
-        typeName.append(cName).append('*');
-        const int newId = qRegisterNormalizedMetaType<T *>(typeName);
-        metatype_id.storeRelease(newId);
-        return newId;
-    }
-};
-
-template <typename T>
-struct QMetaTypeIdQObject<T, QMetaType::IsEnumeration>
-{
-    enum {
-        Defined = 1
-    };
-
-    static int qt_metatype_id()
-    {
-        Q_CONSTINIT static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);
-        if (const int id = metatype_id.loadAcquire())
-            return id;
-        const char *eName = qt_getEnumName(T());
-        const char *cName = qt_getEnumMetaObject(T())->className();
-        QByteArray typeName;
-        typeName.reserve(strlen(cName) + 2 + strlen(eName));
-        typeName.append(cName).append("::").append(eName);
-        const int newId = qRegisterNormalizedMetaType<T>(typeName);
-        metatype_id.storeRelease(newId);
-        return newId;
+        return QMetaType::fromType<T *>().id();
     }
 };
 #endif

tests/auto/corelib/kernel/qmetatype/tst_qmetatype.cpp
@@ -2445,6 +2445,32 @@ void tst_QMetaType::metaObject()
     QCOMPARE(!!(mt.flags() & QMetaType::PointerToQObject), isQObjectPtr);
 }
 
+class QObjectTypeRegisteredAsConst : public QObject
+{
+    Q_OBJECT
+};
+void tst_QMetaType::constQObjectPointer()
+{
+    static bool alreadyRun = false;
+
+    // Register first as const and ensure that the non-const did not get
+    // registered as the same metatype (see QTBUG-147588).
+    QMetaType cmt = QMetaType::fromType<const QObjectTypeRegisteredAsConst *>();
+    int cid = cmt.id();
+    QCOMPARE_NE(cid, -1);
+    QCOMPARE(cmt.name(), "const QObjectTypeRegisteredAsConst*");
+
+    if (!alreadyRun) {
+        // the non-const should not have got registered
+        QVERIFY(!QMetaType::fromName("QObjectTypeRegisteredAsConst *").isValid());
+    }
+
+    QMetaType mt = QMetaType::fromType<QObjectTypeRegisteredAsConst *>();
+    int id = mt.id();
+    alreadyRun = true;
+    QCOMPARE_NE(id, cid);
+}
+
 #define METATYPE_ID_FUNCTION(Type, MetaTypeId, Name) \
   case ::qMetaTypeId< Name >(): metaType = MetaTypeIdStruct<MetaTypeId>::Value; break;
 

tests/auto/corelib/kernel/qmetatype/tst_qmetatype.h
@@ -92,6 +92,7 @@ private slots:
     void saveAndLoadCustom();
     void metaObject_data();
     void metaObject();
+    void constQObjectPointer();
     void constexprMetaTypeIds();
 
     // tst_qmetatype2.cpp

commit 3bb40a9706d51ef0582e44827dc47bc6232211b6

QObject: restore registering some types during connect()
Amends commit b0f495735c30e400c9749c8b9f9823dbb56dfbd9, which introduced
this regression. That was intended to fix a problem with *what* we
registered, not *whether* we registered. However, by removing the
   { Defined = 1 }
from the QMetaTypeId2 expansion, that caused this old Qt 5.0 code to
fail to register the types. It was fine in Qt 5.x because types required
Q_DECLARE_METATYPE.

In Qt 6, types don't need to be declared (except for a few corner cases
that we haven't yet properly fixed), but this code was requiring them to
be so. This commit updates it to query qmetatype.h, using the same
qTryMetaTypeInterfaceForType() that moc-generated code does.

Pick-to: 6.12
Task-number: QTBUG-147588
Fixes: QTBUG-132826
Change-Id: I11e523a3c42ba0b81f35fffd048c20fb2f924f3e
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/corelib/kernel/qmetatype.h
@@ -363,19 +363,31 @@ To convertImplicit(const From& from)
     template <> struct IsPointerDeclaredOpaque<void *>       : std::true_type {};
     template <> struct IsPointerDeclaredOpaque<const void *> : std::true_type {};
 
+    template <typename T, typename UniqueType = void> constexpr inline
+    bool TypeIsCompleteOrVoid = is_complete<T, UniqueType>::value || std::is_void_v<T>;
+    template <typename T> constexpr inline
+    bool PointerIsSuitableForMetaTypeHelper =
+            IsPointerDeclaredOpaque<T>::value
+            || is_complete<std::remove_pointer_t<T>>::value;
+    template <typename T, typename UniqueType = void> constexpr inline
+    bool TypeIsSuitableForMetaType =
+            TypeIsCompleteOrVoid<T, UniqueType>
+            && !std::is_reference_v<T>
+            && (!std::is_pointer_v<T> || PointerIsSuitableForMetaTypeHelper<T>);
+
     template <typename X> static constexpr bool checkTypeIsSuitableForMetaType()
     {
         using T = typename MetatypeDecay<X>::type;
-        static_assert(is_complete<T, void>::value || std::is_void_v<T>,
-                "Meta Types must be fully defined");
+        static_assert(TypeIsCompleteOrVoid<T>, "Meta Types must be fully defined");
         static_assert(!std::is_reference_v<T>,
                 "Meta Types cannot be non-const references or rvalue references.");
-        if constexpr (std::is_pointer_v<T> && !IsPointerDeclaredOpaque<T>::value) {
-            using Pointed = std::remove_pointer_t<T>;
-            static_assert(is_complete<Pointed, void>::value,
+        if constexpr (std::is_pointer_v<T>) {
+            static_assert(PointerIsSuitableForMetaTypeHelper<T>,
                     "Pointer Meta Types must either point to fully-defined types "
                     "or be declared with Q_DECLARE_OPAQUE_POINTER(T *)");
         }
+        // final check
+        static_assert(TypeIsSuitableForMetaType<T>);
         return true;
     }
 }  // namespace QtPrivate

src/corelib/kernel/qobject_impl.h
@@ -4,6 +4,7 @@
 
 #ifndef QOBJECT_H
 #error Do not include qobject_impl.h directly
+#include <QtCore/qobjectdefs_impl.h>
 #endif
 
 #if 0
@@ -11,31 +12,46 @@
 #pragma qt_sync_stop_processing
 #endif
 
+#include <QtCore/qmetatype.h>
+
 QT_BEGIN_NAMESPACE
 
 
 namespace QtPrivate {
+    namespace { struct UniqueType; }
     /*
         Logic to statically generate the array of qMetaTypeId
         ConnectionTypes<FunctionPointer<Signal>::Arguments>::types() returns an array
         of int that is suitable for the types arguments of the connection functions.
 
-        The array only exist of all the types are declared as a metatype
-        (detected using the TypesAreDeclaredMetaType helper struct)
-        If one of the type is not declared, the function return 0 and the signal
-        cannot be used in queued connection.
+        The array only exist if all the types are valid for QMetaType, detected
+        using qTryMetaTypeInterfaceForType(). If any type is not valid at this
+        point (non-const references and forward-declared types), the function
+        returns nullptr. If the issue is a forward-declared type, the function
+        can be used in a queued connection if the type is registered elsewhere
+        before the signal is emitted. If the type is a non-const reference, it
+        cannot be use in queued connections at all.
     */
-    template <typename ArgList> struct TypesAreDeclaredMetaType { enum { Value = false }; };
-    template <> struct TypesAreDeclaredMetaType<List<>> { enum { Value = true }; };
-    template <typename Arg, typename... Tail> struct TypesAreDeclaredMetaType<List<Arg, Tail...> >
-    { enum { Value = QMetaTypeId2<Arg>::Defined && TypesAreDeclaredMetaType<List<Tail...>>::Value }; };
+    template <typename... Args> struct ConnectionTypesHelper
+    {
+        static const int *types()
+        {
+            if constexpr ((TypeIsSuitableForMetaType<Args, UniqueType> && ...)) {
+                static const int t[] = { qMetaTypeId<Args>()..., 0 };
+                return t;
+            } else {
+                return nullptr;
+            }
+        }
+    };
 
-    template <typename ArgList, bool Declared = TypesAreDeclaredMetaType<ArgList>::Value > struct ConnectionTypes
+    template <typename ArgList> struct ConnectionTypes;
+    template <> struct ConnectionTypes<List<>>
     { static const int *types() { return nullptr; } };
-    template <> struct ConnectionTypes<List<>, true>
-    { static const int *types() { return nullptr; } };
-    template <typename... Args> struct ConnectionTypes<List<Args...>, true>
-    { static const int *types() { static const int t[sizeof...(Args) + 1] = { (QtPrivate::QMetaTypeIdHelper<Args>::qt_metatype_id())..., 0 }; return t; } };
+    template <typename... Args> struct ConnectionTypes<List<Args...>>
+            : public ConnectionTypesHelper<typename MetatypeDecay<Args>::type...>
+    {
+    };
 }
 
 

tests/auto/corelib/kernel/qobject/tst_qobject.cpp
@@ -128,6 +128,11 @@ private slots:
     void connectManyArguments();
     void connectForwardDeclare();
     void connectNoDefaultConstructorArg();
+    void connectRegistersGadgetType();
+    void connectRegistersQEnumType();
+    void connectRegistersQObjectPointerType();
+    void connectRegistersQObjectConstPointerType();
+    void connectRegistersOtherType();
     void returnValue_data();
     void returnValue();
     void returnValue2_data();
@@ -5847,6 +5852,96 @@ void tst_QObject::connectNoDefaultConstructorArg()
     NoDefaultContructorArguments ob;
     // it should compile
     QVERIFY(connect(&ob, &NoDefaultContructorArguments::mySignal, &ob, &NoDefaultContructorArguments::mySlot, Qt::QueuedConnection));
+}
+
+struct GadgetForMetaTypeRegistration
+{
+    Q_GADGET
+};
+class OtherClassForMetaTypeRegistration : public QObject
+{
+    Q_OBJECT
+};
+class ClassForMetaTypeRegistration : public QObject
+{
+    Q_OBJECT
+public:
+    struct OtherType {};    // plain type
+    enum MyEnum {};
+    Q_ENUM(MyEnum)
+signals:
+    void signalOtherType(OtherType);
+    void signalGadgetType(GadgetForMetaTypeRegistration);
+    void signalQEnumType(MyEnum);
+    void signalQObjectPointerType(ClassForMetaTypeRegistration *);
+    void signalQObjectConstPointerType(const OtherClassForMetaTypeRegistration *);
+public slots:
+    void slot() { receivedSignalIdx = senderSignalIndex(); QTestEventLoop::instance().exitLoop(); }
+
+public:
+    int receivedSignalIdx = -1;
+
+    template <typename T> static q20::type_identity<T>
+    extractParameterFromSignal(void (ClassForMetaTypeRegistration::*)(T));
+    template <auto Signal> using ParameterFromSignal =
+        typename decltype(extractParameterFromSignal(Signal))::type;
+};
+
+template <auto Signal> static void connectRegistersType()
+{
+    static bool alreadyRunOnce = false;
+    if (alreadyRunOnce)
+        QSKIP("Test can only be run once (type gets registered)");
+
+    // extract the signal parameter type
+    using ParameterType = ClassForMetaTypeRegistration::ParameterFromSignal<Signal>;
+
+    // verify it's not registered
+    constexpr QMetaType mt = QMetaType::fromType<ParameterType>();
+    QVERIFY(!mt.isRegistered());
+    QVERIFY(!QMetaType::fromName(mt.name()).isValid());
+
+    // connect; this should register
+    ClassForMetaTypeRegistration obj;
+    QMetaObject::Connection conn =
+            QObject::connect(&obj, Signal, &obj, &ClassForMetaTypeRegistration::slot,
+                             Qt::QueuedConnection);
+    alreadyRunOnce = true;
+    QVERIFY(conn);
+    QVERIFY(QMetaType::fromName(mt.name()).isValid());
+
+    emit (obj.*Signal)(ParameterType{});
+    QTestEventLoop::instance().enterLoop(1);
+    QVERIFY(!QTestEventLoop::instance().timeout());
+
+    // sanity check that the signal was emitted properly and the slot was run
+    QMetaMethod mm = QMetaMethod::fromSignal(Signal);
+    QCOMPARE(obj.receivedSignalIdx, mm.methodIndex());
+}
+
+void tst_QObject::connectRegistersGadgetType()
+{
+    connectRegistersType<&ClassForMetaTypeRegistration::signalGadgetType>();
+}
+
+void tst_QObject::connectRegistersQEnumType()
+{
+    connectRegistersType<&ClassForMetaTypeRegistration::signalQEnumType>();
+}
+
+void tst_QObject::connectRegistersQObjectPointerType()
+{
+    connectRegistersType<&ClassForMetaTypeRegistration::signalQObjectPointerType>();
+}
+
+void tst_QObject::connectRegistersQObjectConstPointerType()
+{
+    connectRegistersType<&ClassForMetaTypeRegistration::signalQObjectConstPointerType>();
+}
+
+void tst_QObject::connectRegistersOtherType()
+{
+    connectRegistersType<&ClassForMetaTypeRegistration::signalOtherType>();
 }
 
 struct MoveOnly