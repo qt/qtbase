5cc4be4392ef5cff369faa51e4346ba0f017e1e7
Shawn Rutledge <shawn.rutledge@qt.io>
2026-07-24 15:23:48 +0200

    HTML export: emit the title attribute for table cells
    A QTextTableCellFormat is a QTextCharFormat and can carry a tool tip
    (toolTip() / TextToolTip), which the HTML importer sets from a <td>/<th>
    title attribute. The exporter wrote colspan, rowspan, background and cell
    styling but not the tool tip, so a cell title was dropped on export and did
    not round-trip. Emit it as the cell's title attribute, matching the anchor,
    image and span cases.
    
    Add a toHtml() data row for a table cell with a tool tip.
    
    Fixes: QTBUG-148550
    Change-Id: Ie94e635922aec210bf0f17f97f552563c414c387
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>
    (cherry picked from commit 3d732f70bc5b0f7fa5e14a570e7762a5e50fd74d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/gui/text/qtextdocument.cpp
+++ src/gui/text/qtextdocument.cpp
@@ -3514,6 +3514,9 @@ void QTextHtmlExporter::emitTable(const QTextTable *table)
             const QTextTableCellFormat cellFormat = cell.format().toTableCellFormat();
             emitBackgroundAttribute(cellFormat);
 
+            if (!cellFormat.toolTip().isEmpty())
+                emitAttribute("title", cellFormat.toolTip());
+
             QTextCharFormat oldDefaultCharFormat = defaultCharFormat;
 
             QTextCharFormat::VerticalAlignment valign = cellFormat.verticalAlignment();
