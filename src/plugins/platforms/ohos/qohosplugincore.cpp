6a5150332f06d13f95292b7ebcc0ebfefedaa206
Zbigniew Chyla <zbigniew.chyla@qt.io>
2026-07-27 07:00:54 +0200

    ohos: abort instead of throwing on JS task failure
    invokeInJsThreadAndWaitForContinue() catches an exception thrown by a
    task running in the JS thread and rethrows it as std::runtime_error in
    the calling thread, which is the Qt thread. Qt code does not use
    exceptions, so nothing there catches it: QEventLoop::exec() only prints
    its generic "Qt has caught an exception thrown from an event handler"
    warning and rethrows, and the process dies in std::terminate() with the
    message going to stderr instead of the plugin's log.
    
    Report the failure with qOhosReportFatalErrorAndAbort() instead. The
    process still dies, but the message goes through the plugin's logging
    with the caller context and the JS-side message included, and the crash
    is recorded in the calling thread, so it shows the Qt code that entered
    the blocking call.
    
    This is part of restricting the plugin to throwing Napi::Error only.
    Napi::Error needs a Napi::Env, which is available in the JS thread
    only, so that restriction is what makes it verifiable by inspection
    that no exception escapes into Qt code.
    
    Change-Id: I8d666c84512a723388b0621549661f0a7935ecdd
    Task-number: QTBUG-147393
    Reviewed-by: SanthoshKumar Selvaraj <santhosh.kumar.selvaraj@qt.io>
    (cherry picked from commit aa5c01b8a50ed8235ac4d6b0267fd0d993aaddaf)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/platforms/ohos/qohosplugincore.cpp
+++ src/plugins/platforms/ohos/qohosplugincore.cpp
@@ -15,7 +15,6 @@
 #include <optional>
 #include <qohosutils.h>
 #include <render/qohosjswindowregistry.h>
-#include <stdexcept>
 #include <tuple>
 #include <typeindex>
 #include <utility>
@@ -982,8 +981,6 @@ void invokeInJsThreadAndWaitForContinue(
     std::function<void(JsState &, QOhosTaskPromise<>)> &&task,
     std::string callerContextName)
 {
-    using namespace std::string_literals;
-
     const auto funcInfo = Q_FUNC_INFO;
 
     struct JsTaskCompletionState
@@ -1059,11 +1056,11 @@ void invokeInJsThreadAndWaitForContinue(
         taskReadyFuture.wait();
     }
 
-    if (completionState->jsThreadExceptionMsg.has_value()) {
-        throw std::runtime_error(
-            "Got exception from task invoked in JS thread"s
-            + " (caller: \""s + callerContextName + "\"): "
-            + completionState->jsThreadExceptionMsg.value());
+    if (completionState->jsThreadExceptionMsg) {
+        qOhosReportFatalErrorAndAbort(
+            "%s: exception from task invoked in JS thread (caller: \"%s\"): %s",
+            Q_FUNC_INFO, callerContextName.c_str(),
+            completionState->jsThreadExceptionMsg.value().c_str());
     }
 }
 
