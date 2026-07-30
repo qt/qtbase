2228be5e8d325514a0541bd34932a205cc391be1
Axel Spoerl <axel.spoerl@qt.io>
2026-07-30 11:41:08 +0200

    Fix flakiness in tst_QWindow::testInputEvents()
    The test function processed one event before testing with QCOMPARE.
    When another system event gets in the way, the pending input event
    doesn't get delivered and the test starts to flake.
    
    Remove QCoreApplication::processEvent() calls and substitute
    QCOMPARE with QTRY_COMPARE, which processes events until the predicate
    yields true.
    
    Fixes: QTBUG-136999
    Pick-to: 6.11 6.8 6.5
    Change-Id: I8c6cc29da565aef9f15261d4f5f66961ec89d82e
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit b687b72dbffc3cc60db6ffe3d55ec7b08b30efe2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/gui/kernel/qwindow/tst_qwindow.cpp
+++ tests/auto/gui/kernel/qwindow/tst_qwindow.cpp
@@ -1363,16 +1363,14 @@ void tst_QWindow::testInputEvents()
     QVERIFY(QTestPrivate::ensurePositionTopLeft(&window));
 
     QTest::keyClick(&window, Qt::Key_A, Qt::NoModifier);
-    QCoreApplication::processEvents();
-    QCOMPARE(window.keyPressCode, int(Qt::Key_A));
-    QCOMPARE(window.keyReleaseCode, int(Qt::Key_A));
+    QTRY_COMPARE(window.keyPressCode, int(Qt::Key_A));
+    QTRY_COMPARE(window.keyReleaseCode, int(Qt::Key_A));
 
     QPointF local(12, 34);
     QTest::mouseClick(&window, Qt::LeftButton, Qt::NoModifier, local.toPoint());
-    QCoreApplication::processEvents();
-    QCOMPARE(window.mousePressButton, int(Qt::LeftButton));
-    QCOMPARE(window.mouseReleaseButton, int(Qt::LeftButton));
-    QCOMPARE(window.mousePressLocalPos, local);
+    QTRY_COMPARE(window.mousePressButton, int(Qt::LeftButton));
+    QTRY_COMPARE(window.mouseReleaseButton, int(Qt::LeftButton));
+    QTRY_COMPARE(window.mousePressLocalPos, local);
 
     QList<QWindowSystemInterface::TouchPoint> points;
     QWindowSystemInterface::TouchPoint tp1, tp2;
@@ -1387,7 +1385,6 @@ void tst_QWindow::testInputEvents()
     points[0].state = QEventPoint::State::Released;
     points[1].state = QEventPoint::State::Released;
     QWindowSystemInterface::handleTouchEvent(&window, touchDevice, points);
-    QCoreApplication::processEvents();
     QTRY_COMPARE(window.touchPressedCount, 2);
     QTRY_COMPARE(window.touchReleasedCount, 2);
 
@@ -1397,15 +1394,13 @@ void tst_QWindow::testInputEvents()
     const QPointF nonWindowGlobal(window.geometry().topRight() + QPoint(200, 50)); // not inside the window
     const QPointF deviceNonWindowGlobal = QHighDpi::toNativePixels(nonWindowGlobal, window.screen());
     simulateMouseClick(nullptr, deviceNonWindowGlobal, deviceNonWindowGlobal);
-    QCoreApplication::processEvents();
-    QCOMPARE(window.mousePressButton, 0);
-    QCOMPARE(window.mouseReleaseButton, 0);
+    QTRY_COMPARE(window.mousePressButton, 0);
+    QTRY_COMPARE(window.mouseReleaseButton, 0);
     const QPointF windowGlobal = window.mapToGlobal(local.toPoint());
     const QPointF deviceWindowGlobal = QHighDpi::toNativePixels(windowGlobal, window.screen());
     simulateMouseClick(nullptr, deviceWindowGlobal, deviceWindowGlobal);
-    QCoreApplication::processEvents();
-    QCOMPARE(window.mousePressButton, int(Qt::LeftButton));
-    QCOMPARE(window.mouseReleaseButton, int(Qt::LeftButton));
+    QTRY_COMPARE(window.mousePressButton, int(Qt::LeftButton));
+    QTRY_COMPARE(window.mouseReleaseButton, int(Qt::LeftButton));
 
     const qreal fuzz = 1.0 / QHighDpiScaling::factor(&window);
     QTRY_VERIFY2(qFuzzyComparePositionF(window.mousePressScreenPos, windowGlobal, fuzz),
