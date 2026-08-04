368a41aa8cade472291a48fa7012cf6f51f9ad86
Oliver Wolff <oliver.wolff@qt.io>
2026-08-04 13:33:16 +0200

    rcc: Add security markers
    While rcc itself is a build tool and thus marked as "insignificant",
    rcc.h as well as rcc.cpp are also duplicated in designer's sources. Due
    to this special case, these files are marked "significant".
    
    QUIP: 23
    Pick-to: 6.8
    Task-number: QTBUG-135588
    Change-Id: Id768d7884e7a19697999b8af14fc37fa396554f8
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: hjk <hjk@qt.io>
    (cherry picked from commit fda3da529b933afd8f5c9b885d6e27ec74d5a836)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e09bb1b1a85ff6346ec5d50c5a1c278a14c9d1e6)

--- src/tools/rcc/rcc.cpp
+++ src/tools/rcc/rcc.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2018 The Qt Company Ltd.
 // Copyright (C) 2018 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:significant reason:default
 
 #include "rcc.h"
 
