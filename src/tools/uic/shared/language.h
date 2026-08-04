9471ea7f36d8f8d2e39b98751cf62b181d1e1922
Oliver Wolff <oliver.wolff@qt.io>
2026-08-04 13:43:44 +0200

    uic: Add security markers
    uic is a build tool, so its sources are marked "insignificant". The
    exception are ui4.h and ui4.cpp, which implement the .ui XML parser and
    are generated by qttools/src/designer/data/generate_ui.py from ui4.xsd.
    The same generator emits qttools/src/designer/src/lib/uilib/ui4{.cpp,_p.h},
    which is compiled into Qt UI Tools and parses .ui files at application
    runtime. Due to this special case, these files are marked "significant".
    
    QUIP: 23
    Pick-to: 6.11 6.8
    Task-number: QTBUG-135588
    Change-Id: Ic1b58462a18490f8aaccd8652acdb510d40f7e74
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit 03929e70406bd5475d870e8f49ed445b33b484b9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/tools/uic/shared/language.h
+++ src/tools/uic/shared/language.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2018 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:insignificant reason:build-tool
 
 #ifndef LANGUAGE_H
 #define LANGUAGE_H
