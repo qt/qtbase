c272119847220dcf239ca8a9908867376d6dca30
Linus Jahn <lnj@kaidan.im>
2026-08-05 17:10:01 +0200

    QDnsLookup/Android: add missing ChangeLog entry
    The entry was forgotten in 185e70dbebd09ccb472690b5bb09ce263261bb8d.
    
    [ChangeLog][QtNetwork][QDnsLookup] QDnsLookup is now implemented on
    Android, using the platform's DnsResolver. It requires Android 10 (API
    level 29); isProtocolSupported() reports whether it is available.
    
    Change-Id: I8307b8ecb79bb1cdb5c454f42918cded6adb4aa2
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/network/kernel/qdnslookup_unix.cpp
+++ src/network/kernel/qdnslookup_unix.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2012 Jeremy Lainé <jeremy.laine@m4x.org>
 // Copyright (C) 2023 Intel Corporation.
+// Copyright (C) 2026 Linus Jahn <lnj@kaidan.im>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 // Qt-Security score:critical reason:data-parser
 
