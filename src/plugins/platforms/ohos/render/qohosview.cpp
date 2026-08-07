03f69e05e41847e152f33d5a22020cfe2391c27e
Arkadiusz Zarębski <arkadiusz.zarebski@qt.io>
2026-08-07 10:49:37 +0200

    ohos: Route minimize() through flushedWindowProxyOrNull()
    minimize() hits the same race as maximize() and setFullScreen(). move()
    to another screen only queues the position update, while minimize() is
    synchronous, so it runs before the queued move is applied and the window
    is minimized on the screen it was created on instead of the target one.
    
    Fixes: QTBUG-148549
    Pick-to: 6.12
    Change-Id: I71f8c8a4deb5d7e3b89a9a7dc61a613c32c1b868
    Reviewed-by: SanthoshKumar Selvaraj <santhosh.kumar.selvaraj@qt.io>

--- src/plugins/platforms/ohos/render/qohosview.cpp
+++ src/plugins/platforms/ohos/render/qohosview.cpp
@@ -1053,8 +1053,9 @@ void QOhosView::recover()
 
 void QOhosView::minimize()
 {
-    if (m_ohosWindowProxy != nullptr)
-        m_ohosWindowProxy->minimize();
+    auto *windowProxy = flushedWindowProxyOrNull();
+    if (windowProxy != nullptr)
+        windowProxy->minimize();
 }
 
 void QOhosView::requestActivate()
