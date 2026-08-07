2ed6c208f34d0e7e4b406536abf20273a5b65ef7
Andy Nichols <andy.nichols@qt.io>
2026-08-07 13:06:55 +0200

    rhi autotest: Bake a GLSL ES variant of indirect_draw_args.comp
    The checked-in indirect_draw_args.comp.qsb was generated without a
    GLSL ES target, so it only contains SPIR-V, GLSL 430, HLSL and MSL.
    QRhiGles2::shaderSource() only ever looks up GlslEs variants when
    running on OpenGL ES, finds nothing, and so
    indexedIndirectMultiDrawFromCompute fails when creating the compute
    pipeline. Every other compute shader in the test data already ships
    an ES variant.
    
    Regenerate with 310 es added, matching storagebuffer.comp.qsb, and fix
    the buildshaders.bat line that produced the blob so it does not get
    dropped again.
    
    Task-number: QTBUG-148258
    Change-Id: I66c95887324fa1a99cef32bb8a58237208de60b5
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit 8471696f6eca5d47489adeca001b3063de826b72)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/gui/rhi/qrhi/data/buildshaders.bat
+++ tests/auto/gui/rhi/qrhi/data/buildshaders.bat
@@ -37,7 +37,7 @@ qsb --glsl "300 es,330" --hlsl 61 -c --msl 12 multiview.frag -o multiview.frag.q
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o samesrb_1.vert.qsb samesrb_1.vert
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o samesrb_2.vert.qsb samesrb_2.vert
 qsb --glsl "150,120,100 es" --hlsl 50 --msl 12 -o samesrb.frag.qsb samesrb.frag
-qsb --glsl 430 --hlsl 50 --msl 12 -o indirect_draw_args.comp.qsb indirect_draw_args.comp
+qsb --glsl 310es,430 --hlsl 50 --msl 12 -o indirect_draw_args.comp.qsb indirect_draw_args.comp
 qsb --glsl "150,130,300 es" --hlsl 50 --msl 12 -o fullscreenquad.vert.qsb fullscreenquad.vert
 qsb --glsl "150,130,300 es" --hlsl 50 --msl 12 -o fullscreenquad_color.frag.qsb fullscreenquad_color.frag
 qsb --glsl "420,430,460" -o indirect_multi_draw_sdp.vert.qsb indirect_multi_draw_sdp.vert
