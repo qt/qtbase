robertpelloni/bobui

commit cceae37a137ecd7c5a1ecd3b40f7c46e7fabd884

windeployqt: add PeHeaderInfo helper class
Clean up utils files and have code covering that area in a dedicated
helper class.

Preparation for upcoming caching of this data.

Change-Id: I698321d65c8e07ed7166af01bddb3e678c36f2ad
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/tools/windeployqt/peheaderinfo.cpp
@@ -1,0 +1,277 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include "peheaderinfo.h"
+
+#include <QtCore/private/qsystemerror_p.h>
+#include <QString>
+
+#if defined(Q_OS_WIN)
+#  include <QtCore/qt_windows.h>
+#  include <QtCore/private/qsystemerror_p.h>
+#  include <shlwapi.h>
+#  include <delayimp.h>
+#endif  // Q_OS_WIN
+
+QT_BEGIN_NAMESPACE
+
+using namespace Qt::StringLiterals;
+
+PeHeaderInfo::PeHeaderInfo(const QString fileName)
+{
+    mFileHandle = CreateFile(reinterpret_cast<const WCHAR*>(fileName.utf16()), GENERIC_READ, FILE_SHARE_READ, NULL,
+                             OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (mFileHandle == INVALID_HANDLE_VALUE || mFileHandle == NULL) {
+        mErrorMessage = QString::fromLatin1("Cannot open '%1': %2")
+        .arg(fileName, QSystemError::windowsString());
+        return;
+    }
+
+    mFileMapHandle = CreateFileMapping(mFileHandle, NULL, PAGE_READONLY, 0, 0, NULL);
+    if (mFileMapHandle == NULL) {
+        mErrorMessage = QString::fromLatin1("Cannot create file mapping of '%1': %2")
+        .arg(fileName, QSystemError::windowsString());
+        return;
+    }
+
+    mFileMemory = MapViewOfFile(mFileMapHandle, FILE_MAP_READ, 0, 0, 0);
+    if (!mFileMemory) {
+        mErrorMessage = QString::fromLatin1("Cannot map '%1': %2")
+        .arg(fileName, QSystemError::windowsString());
+        return;
+    }
+
+    mNtHeaders = getNtHeader();
+    if (!mNtHeaders)
+        return;
+
+    mValid = true;
+}
+
+PeHeaderInfo::~PeHeaderInfo()
+{
+    if (mFileMemory)
+        UnmapViewOfFile(mFileMemory);
+
+    if (mFileMapHandle != NULL)
+        CloseHandle(mFileMapHandle);
+
+    if (mFileHandle != NULL && mFileHandle != INVALID_HANDLE_VALUE)
+        CloseHandle(mFileHandle);
+}
+
+bool PeHeaderInfo::isValid()
+{
+    return mValid;
+}
+
+QString PeHeaderInfo::errorMessage()
+{
+    return mErrorMessage;
+}
+
+unsigned int PeHeaderInfo::wordSize()
+{
+    if (!isValid())
+        return 0;
+
+    return ntHeaderWordSize(mNtHeaders);
+}
+
+unsigned int PeHeaderInfo::machineArch()
+{
+    if (!isValid())
+        return 0;
+
+    return mNtHeaders->FileHeader.Machine;
+}
+
+QStringList PeHeaderInfo::dependentLibs()
+{
+    if (!isValid())
+        return QStringList();
+
+    if (!mDependentLibs.isEmpty())
+        return mDependentLibs;
+
+    if (wordSize() == 32) {
+        mDependentLibs
+                = determineDependentLibs(reinterpret_cast<const IMAGE_NT_HEADERS32 *>(mNtHeaders));
+    } else {
+        mDependentLibs
+                = determineDependentLibs(reinterpret_cast<const IMAGE_NT_HEADERS64 *>(mNtHeaders));
+    }
+    return mDependentLibs;
+}
+
+bool PeHeaderInfo::isDebug()
+{
+    if (!mIsDebug.has_value()) {
+        QStringList dependents = dependentLibs();
+        if (wordSize() == 32) {
+            mIsDebug = determineDebug(reinterpret_cast<const IMAGE_NT_HEADERS32 *>(mNtHeaders));
+        } else {
+            mIsDebug = determineDebug(reinterpret_cast<const IMAGE_NT_HEADERS64 *>(mNtHeaders));
+        }
+    }
+
+    return mIsDebug.value();
+}
+
+IMAGE_NT_HEADERS *PeHeaderInfo::getNtHeader()
+{
+    IMAGE_DOS_HEADER *dosHeader = static_cast<PIMAGE_DOS_HEADER>(mFileMemory);
+    // Check DOS header consistency
+    if (IsBadReadPtr(dosHeader, sizeof(IMAGE_DOS_HEADER))
+        || dosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
+        mErrorMessage = QString::fromLatin1("DOS header check failed.");
+        return 0;
+    }
+    // Retrieve NT header
+    char *ntHeaderC = static_cast<char *>(mFileMemory) + dosHeader->e_lfanew;
+    IMAGE_NT_HEADERS *ntHeaders = reinterpret_cast<IMAGE_NT_HEADERS *>(ntHeaderC);
+    // check NT header consistency
+    if (IsBadReadPtr(ntHeaders, sizeof(ntHeaders->Signature))
+        || ntHeaders->Signature != IMAGE_NT_SIGNATURE
+        || IsBadReadPtr(&ntHeaders->FileHeader, sizeof(IMAGE_FILE_HEADER))) {
+        mErrorMessage = QString::fromLatin1("NT header check failed.");
+        return 0;
+    }
+    // Check magic
+    if (!ntHeaderWordSize(ntHeaders)) {
+        mErrorMessage = QString::fromLatin1("NT header check failed; magic %1 is invalid.").arg(ntHeaders->OptionalHeader.Magic);
+        return 0;
+    }
+    // Check section headers
+    IMAGE_SECTION_HEADER *sectionHeaders = IMAGE_FIRST_SECTION(ntHeaders);
+    if (IsBadReadPtr(sectionHeaders,
+                     ntHeaders->FileHeader.NumberOfSections * sizeof(IMAGE_SECTION_HEADER))) {
+        mErrorMessage = QString::fromLatin1("NT header section header check failed.");
+        return 0;
+    }
+    return ntHeaders;
+}
+
+QString PeHeaderInfo::stringFromRvaPtr(const void *rvaPtr)
+{
+    return QString::fromLocal8Bit(static_cast<const char *>(rvaPtr));
+}
+
+PeHeaderInfo::MsvcDebugRuntimeResult PeHeaderInfo::checkMsvcDebugRuntime()
+{
+    for (const QString &lib : mDependentLibs) {
+        qsizetype pos = 0;
+        if (lib.startsWith("MSVCR"_L1, Qt::CaseInsensitive)
+            || lib.startsWith("MSVCP"_L1, Qt::CaseInsensitive)
+            || lib.startsWith("VCRUNTIME"_L1, Qt::CaseInsensitive)
+            || lib.startsWith("VCCORLIB"_L1, Qt::CaseInsensitive)
+            || lib.startsWith("CONCRT"_L1, Qt::CaseInsensitive)
+            || lib.startsWith("UCRTBASE"_L1, Qt::CaseInsensitive)) {
+            qsizetype lastDotPos = lib.lastIndexOf(u'.');
+            pos = -1 == lastDotPos ? 0 : lastDotPos - 1;
+        }
+
+        if (pos > 0) {
+            const auto removeExtraSuffix = [&lib, &pos](const QString &suffix) -> void {
+                if (lib.contains(suffix, Qt::CaseInsensitive))
+                    pos -= suffix.size();
+            };
+            removeExtraSuffix("_app"_L1);
+            removeExtraSuffix("_atomic_wait"_L1);
+            removeExtraSuffix("_codecvt_ids"_L1);
+        }
+
+        if (pos)
+            return lib.at(pos).toLower() == u'd' ? MsvcDebugRuntime : MsvcReleaseRuntime;
+    }
+    return NoMsvcRuntime;
+}
+
+template <class ImageNtHeader>
+QStringList PeHeaderInfo::readImportSections(const ImageNtHeader *ntHeaders)
+{
+    // Get import directory entry RVA and read out
+    const DWORD importsStartRVA = mNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+    if (!importsStartRVA) {
+        mErrorMessage = QString::fromLatin1("Failed to find IMAGE_DIRECTORY_ENTRY_IMPORT entry.");
+        return QStringList();
+    }
+    const IMAGE_IMPORT_DESCRIPTOR *importDesc = static_cast<const IMAGE_IMPORT_DESCRIPTOR *>(rvaToPtr(importsStartRVA, ntHeaders, mFileMemory));
+    if (!importDesc) {
+        mErrorMessage = QString::fromLatin1("Failed to find IMAGE_IMPORT_DESCRIPTOR entry.");
+        return QStringList();
+    }
+    QStringList result;
+    for ( ; importDesc->Name; ++importDesc)
+        result.push_back(stringFromRvaPtr(rvaToPtr(importDesc->Name, ntHeaders, mFileMemory)));
+
+           // Read delay-loaded DLLs, see http://msdn.microsoft.com/en-us/magazine/cc301808.aspx .
+           // Check on grAttr bit 1 whether this is the format using RVA's > VS 6
+    if (const DWORD delayedImportsStartRVA = mNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT].VirtualAddress) {
+        const ImgDelayDescr *delayedImportDesc = static_cast<const ImgDelayDescr *>(rvaToPtr(delayedImportsStartRVA, ntHeaders, mFileMemory));
+        for ( ; delayedImportDesc->rvaDLLName && (delayedImportDesc->grAttrs & 1); ++delayedImportDesc)
+            result.push_back(stringFromRvaPtr(rvaToPtr(delayedImportDesc->rvaDLLName, ntHeaders, mFileMemory)));
+    }
+
+    return result;
+}
+
+template <class ImageNtHeader>
+QStringList PeHeaderInfo::determineDependentLibs(const ImageNtHeader *nth)
+{
+    return readImportSections(nth);
+}
+
+template <class ImageNtHeader>
+bool PeHeaderInfo::determineDebug(const ImageNtHeader *nth)
+{
+    if (nth->FileHeader.Characteristics & IMAGE_FILE_DEBUG_STRIPPED)
+        return false;
+
+    if (mDependentLibs.isEmpty())
+        mDependentLibs = determineDependentLibs(nth);
+
+    const bool hasDebugEntry = mNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DEBUG].Size;
+    // When an MSVC debug entry is present, check whether the debug runtime
+    // is actually used to detect -release / -force-debug-info builds.
+    const MsvcDebugRuntimeResult msvcrt = checkMsvcDebugRuntime();
+    if (msvcrt == NoMsvcRuntime)
+        return hasDebugEntry;
+    else
+        return hasDebugEntry && msvcrt == MsvcDebugRuntime;
+}
+
+template <class ImageNtHeader>
+const IMAGE_SECTION_HEADER *PeHeaderInfo::findSectionHeader(DWORD rva, const ImageNtHeader *nTHeader)
+{
+    const IMAGE_SECTION_HEADER *section = IMAGE_FIRST_SECTION(nTHeader);
+    const IMAGE_SECTION_HEADER *sectionEnd = section + mNtHeaders->FileHeader.NumberOfSections;
+    for ( ; section < sectionEnd; ++section)
+        if (rva >= section->VirtualAddress && rva < (section->VirtualAddress + section->Misc.VirtualSize))
+            return section;
+    return 0;
+}
+
+template <class ImageNtHeader>
+const void *PeHeaderInfo::rvaToPtr(DWORD rva, const ImageNtHeader *nTHeader, const void *imageBase)
+{
+    const IMAGE_SECTION_HEADER *sectionHdr = findSectionHeader(rva, nTHeader);
+    if (!sectionHdr)
+        return 0;
+    const DWORD delta = sectionHdr->VirtualAddress - sectionHdr->PointerToRawData;
+    return static_cast<const char *>(imageBase) + rva - delta;
+}
+
+template <class ImageNtHeader>
+unsigned int PeHeaderInfo::ntHeaderWordSize(const ImageNtHeader *header)
+{
+    // defines IMAGE_NT_OPTIONAL_HDR32_MAGIC, IMAGE_NT_OPTIONAL_HDR64_MAGIC
+    enum { imageNtOptionlHeader32Magic = 0x10b, imageNtOptionlHeader64Magic = 0x20b };
+    if (header->OptionalHeader.Magic == imageNtOptionlHeader32Magic)
+        return 32;
+    if (header->OptionalHeader.Magic == imageNtOptionlHeader64Magic)
+        return 64;
+    return 0;
+}
+
+QT_END_NAMESPACE

src/tools/windeployqt/peheaderinfo.h
@@ -1,0 +1,75 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef PEHEADERINFO_H
+#define PEHEADERINFO_H
+
+#include <qt_windows.h>
+
+#include <QString>
+#include <QStringList>
+
+QT_BEGIN_NAMESPACE
+
+class PeHeaderInfo {
+public:
+    PeHeaderInfo(const QString fileName);
+
+    ~PeHeaderInfo();
+
+    bool isValid();
+
+    QString errorMessage();
+
+    unsigned wordSize();
+
+    unsigned int machineArch();
+
+    QStringList dependentLibs();
+
+    bool isDebug();
+
+private:
+    bool mValid = false;
+    QString mErrorMessage;
+
+    QStringList mDependentLibs;
+    std::optional<bool> mIsDebug;
+
+    HANDLE mFileHandle = NULL;
+    HANDLE mFileMapHandle = NULL;
+    void *mFileMemory = nullptr;
+    IMAGE_NT_HEADERS *mNtHeaders = nullptr;
+
+    IMAGE_NT_HEADERS *getNtHeader();
+    template <class ImageNtHeader>
+    QStringList readImportSections(const ImageNtHeader *ntHeaders);
+    template <class ImageNtHeader>
+    QStringList determineDependentLibs(const ImageNtHeader *nth);
+    template <class ImageNtHeader>
+    bool determineDebug(const ImageNtHeader *nth);
+
+    static QString stringFromRvaPtr(const void *rvaPtr);
+
+    // Helper for reading out PE executable files: Find a section header for an RVA
+    // (IMAGE_NT_HEADERS64, IMAGE_NT_HEADERS32).
+    template <class ImageNtHeader>
+    const IMAGE_SECTION_HEADER *findSectionHeader(DWORD rva, const ImageNtHeader *nTHeader);
+
+    // Helper for reading out PE executable files: convert RVA to pointer (IMAGE_NT_HEADERS64, IMAGE_NT_HEADERS32).
+    template <class ImageNtHeader>
+    const void *rvaToPtr(DWORD rva, const ImageNtHeader *nTHeader, const void *imageBase);
+
+    template <class ImageNtHeader>
+    unsigned ntHeaderWordSize(const ImageNtHeader *header);
+
+    // Check for MSCV runtime (MSVCP90D.dll/MSVCP90.dll, MSVCP120D.dll/MSVCP120.dll,
+    // VCRUNTIME140D.DLL/VCRUNTIME140.DLL (VS2015) or msvcp120d_app.dll/msvcp120_app.dll).
+    enum MsvcDebugRuntimeResult { MsvcDebugRuntime, MsvcReleaseRuntime, NoMsvcRuntime };
+
+    MsvcDebugRuntimeResult checkMsvcDebugRuntime();
+};
+
+QT_END_NAMESPACE
+
+#endif // PEHEADERINFO_H

src/tools/windeployqt/CMakeLists.txt
@@ -16,6 +16,7 @@ qt_internal_add_tool(${target_name}
         qtmoduleinfo.cpp qtmoduleinfo.h
         qtplugininfo.cpp qtplugininfo.h
         utils.cpp utils.h
+        peheaderinfo.cpp peheaderinfo.h
         main.cpp
     DEFINES
         QT_NO_CAST_FROM_ASCII
@@ -25,6 +26,7 @@ qt_internal_add_tool(${target_name}
     LIBRARIES
         Qt::CorePrivate
 )
+
 qt_internal_return_unless_building_tools()
 
 qt_internal_extend_target(${target_name} CONDITION WIN32

qt/qtbase

commit 1f83c2cb3e88dc6926e936be563ec7c5c8554cbb

windeployqt: Fix nt header usage for "mixed deployment" 32 bit targets
mNtHeaders is always host dependent. If the target architecture is
different from the host the deployment might fail. One symptom would be
that windeployqt will not be able to find dependencies of 32 bit
binaries and misdetect them as release builds.

In some of the structures that are used, the member offset is
architecture dependent. For example DataDirectory has an offset of 96
in IMAGE_OPTIONAL_HEADER32 and 112 in IMAGE_OPTIONAL_HEADER64.

The fix is to use the templated function parameters. These are target
architecture dependent and thus contain the right information.

While at it the functions were changed to static functions so that the
breakage of using mNtHeaders cannot be reintroduced by accident.

The remaining usages of mNtHeaders are fine as the offsets that are
obtained there are arch independent or 0.

The patch amends cceae37a137 which introduced the issue.

Pick-to: 6.12 6.11
Change-Id: Icfc81a42c086d73090401d843801d1ae27722a59
Reviewed-by: Wladimir Leuschner <wladimir.leuschner@qt.io>

src/tools/windeployqt/peheaderinfo.cpp
@@ -193,7 +193,7 @@ template <class ImageNtHeader>
 QStringList PeHeaderInfo::readImportSections(const ImageNtHeader *ntHeaders)
 {
     // Get import directory entry RVA and read out
-    const DWORD importsStartRVA = mNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+    const DWORD importsStartRVA = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
     if (!importsStartRVA) {
         mErrorMessage = QString::fromLatin1("Failed to find IMAGE_DIRECTORY_ENTRY_IMPORT entry.");
         return QStringList();
@@ -207,9 +207,9 @@ QStringList PeHeaderInfo::readImportSections(const ImageNtHeader *ntHeaders)
     for ( ; importDesc->Name; ++importDesc)
         result.push_back(stringFromRvaPtr(rvaToPtr(importDesc->Name, ntHeaders, mFileMemory)));
 
-           // Read delay-loaded DLLs, see http://msdn.microsoft.com/en-us/magazine/cc301808.aspx .
-           // Check on grAttr bit 1 whether this is the format using RVA's > VS 6
-    if (const DWORD delayedImportsStartRVA = mNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT].VirtualAddress) {
+    // Read delay-loaded DLLs, see http://msdn.microsoft.com/en-us/magazine/cc301808.aspx .
+    // Check on grAttr bit 1 whether this is the format using RVA's > VS 6
+    if (const DWORD delayedImportsStartRVA = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT].VirtualAddress) {
         const ImgDelayDescr *delayedImportDesc = static_cast<const ImgDelayDescr *>(rvaToPtr(delayedImportsStartRVA, ntHeaders, mFileMemory));
         for ( ; delayedImportDesc->rvaDLLName && (delayedImportDesc->grAttrs & 1); ++delayedImportDesc)
             result.push_back(stringFromRvaPtr(rvaToPtr(delayedImportDesc->rvaDLLName, ntHeaders, mFileMemory)));
@@ -233,7 +233,7 @@ bool PeHeaderInfo::determineDebug(const ImageNtHeader *nth)
     if (mDependentLibs.isEmpty())
         mDependentLibs = determineDependentLibs(nth);
 
-    const bool hasDebugEntry = mNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DEBUG].Size;
+    const bool hasDebugEntry = nth->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DEBUG].Size;
     // When an MSVC debug entry is present, check whether the debug runtime
     // is actually used to detect -release / -force-debug-info builds.
     const MsvcDebugRuntimeResult msvcrt = checkMsvcDebugRuntime();
@@ -247,7 +247,7 @@ template <class ImageNtHeader>
 const IMAGE_SECTION_HEADER *PeHeaderInfo::findSectionHeader(DWORD rva, const ImageNtHeader *nTHeader)
 {
     const IMAGE_SECTION_HEADER *section = IMAGE_FIRST_SECTION(nTHeader);
-    const IMAGE_SECTION_HEADER *sectionEnd = section + mNtHeaders->FileHeader.NumberOfSections;
+    const IMAGE_SECTION_HEADER *sectionEnd = section + nTHeader->FileHeader.NumberOfSections;
     for ( ; section < sectionEnd; ++section)
         if (rva >= section->VirtualAddress && rva < (section->VirtualAddress + section->Misc.VirtualSize))
             return section;

src/tools/windeployqt/peheaderinfo.h
@@ -58,14 +58,14 @@ private:
     // Helper for reading out PE executable files: Find a section header for an RVA
     // (IMAGE_NT_HEADERS64, IMAGE_NT_HEADERS32).
     template <class ImageNtHeader>
-    const IMAGE_SECTION_HEADER *findSectionHeader(DWORD rva, const ImageNtHeader *nTHeader);
+    static const IMAGE_SECTION_HEADER *findSectionHeader(DWORD rva, const ImageNtHeader *nTHeader);
 
     // Helper for reading out PE executable files: convert RVA to pointer (IMAGE_NT_HEADERS64, IMAGE_NT_HEADERS32).
     template <class ImageNtHeader>
-    const void *rvaToPtr(DWORD rva, const ImageNtHeader *nTHeader, const void *imageBase);
+    static const void *rvaToPtr(DWORD rva, const ImageNtHeader *nTHeader, const void *imageBase);
 
     template <class ImageNtHeader>
-    unsigned ntHeaderWordSize(const ImageNtHeader *header);
+    static unsigned ntHeaderWordSize(const ImageNtHeader *header);
 
     // Check for MSCV runtime (MSVCP90D.dll/MSVCP90.dll, MSVCP120D.dll/MSVCP120.dll,
     // VCRUNTIME140D.DLL/VCRUNTIME140.DLL (VS2015) or msvcp120d_app.dll/msvcp120_app.dll).