532c6aaea6b755dd7a62f124a7f59120589258a4
Jonas Kvinge <jonas@jkvinge.net>
2026-08-27 13:21:53 +0200

    QHttp2Connection: Check that socket is still open
    Stop reading if the socket was closed while reading.
    Fixes issues with servers closing the connection early causing
    "QIODevice::read (QSslSocket): device not open" errors.
    
    Fixes: QTBUG-149562
    Pick-to: 6.11
    Change-Id: Ifac3c5b013a96712251e135b9567f791f67fa9a6
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 2f63206bdbefbd8d8facc09a56772a75c8996275)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/network/access/qhttp2connection.cpp
+++ src/network/access/qhttp2connection.cpp
@@ -1207,6 +1207,8 @@ void QHttp2Connection::handleReadyRead()
         return;
 
     QIODevice *socket = getSocket();
+    if (!socket->isOpen())
+        return;
 
     qCDebug(qHttp2ConnectionLog, "[%p] Receiving data, %lld bytes available", this,
             socket->bytesAvailable());
@@ -1215,7 +1217,7 @@ void QHttp2Connection::handleReadyRead()
     if (!m_prefaceSent)
         return;
 
-    while (!m_connectionAborted) {
+    while (!m_connectionAborted && socket->isOpen()) {
         const auto result = frameReader.read(*socket);
         if (result != FrameStatus::goodFrame)
             qCDebug(qHttp2ConnectionLog, "[%p] Tried to read frame, got %d", this, int(result));
