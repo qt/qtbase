bd3a461924e052ceb7d789d4712441aad7853900
Laszlo Agocs <laszlo.agocs@qt.io>
2026-08-31 12:35:06 +0200

    rhi: vulkan: Wait on the queue, not the device
    QRhi creates and uses a single queue, so vkDeviceWaitIdle is equivalent
    to vkQueueWaitIdle(gfxQueue) for a device created by us. With an
    imported device it is not: vkDeviceWaitIdle requires external
    synchronization of all queues of the device, which breaks applications
    that submit on their own queue from another thread. finish() already
    waits on the queue only.
    
    Task-number: QTBUG-109307
    Pick-to: 6.8
    Change-Id: I51774edf71e859b74c6cd81f05e586e762d63e99
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>
    (cherry picked from commit 1c5333bac6a83904d8f04fd135dd0f998e0f7ffd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit f1b357cf26f62bdd7502577a2b36e64ce12e0628)

--- src/gui/rhi/qrhivulkan.cpp
+++ src/gui/rhi/qrhivulkan.cpp
@@ -1146,7 +1146,7 @@ void QRhiVulkan::destroy()
         return;
 
     if (!deviceLost)
-        df->vkDeviceWaitIdle(dev);
+        df->vkQueueWaitIdle(gfxQueue);
 
     executeDeferredReleases(true);
     finishActiveReadbacks(true);
@@ -2237,7 +2237,8 @@ bool QRhiVulkan::recreateSwapChain(QRhiSwapChain *swapChain)
         return false;
     }
 
-    df->vkDeviceWaitIdle(dev);
+    if (!deviceLost)
+        df->vkQueueWaitIdle(gfxQueue);
 
     if (!vkCreateSwapchainKHR) {
         vkCreateSwapchainKHR = reinterpret_cast<PFN_vkCreateSwapchainKHR>(f->vkGetDeviceProcAddr(dev, "vkCreateSwapchainKHR"));
@@ -2532,7 +2533,7 @@ void QRhiVulkan::releaseSwapChainResources(QRhiSwapChain *swapChain)
         return;
 
     if (!deviceLost)
-        df->vkDeviceWaitIdle(dev);
+        df->vkQueueWaitIdle(gfxQueue);
 
     for (int i = 0; i < QVK_FRAMES_IN_FLIGHT; ++i) {
         QVkSwapChain::FrameResources &frame(swapChainD->frameRes[i]);
