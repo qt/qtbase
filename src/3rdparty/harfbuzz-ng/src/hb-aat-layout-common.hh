9deebde7bac59f68dca49324e81dbf6641e10a29
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2026-09-01 12:54:04 +0200

    Upgrade Harfbuzz to 14.4.0
    [ChangeLog][Third-Party Code] Upgraded Harfbuzz to version 14.4.0
    
    Pick-to: 6.8 6.5 5.15
    Task-number: QTBUG-149810
    Change-Id: I578c052b230bcbe7b9e7a180573bfeb11955dea9
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>
    (cherry picked from commit 8984248703fc11e6e84b24b4b03ca93cf25458bc)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 96851e7a2d66b7843415dd2b752e0b6a81cdc416)

--- src/3rdparty/harfbuzz-ng/src/hb-aat-layout-common.hh
+++ src/3rdparty/harfbuzz-ng/src/hb-aat-layout-common.hh
@@ -49,6 +49,17 @@ struct ankr;
 
 using hb_aat_class_cache_t = hb_ot_layout_mapping_cache_t;
 
+/* The state-machine subtables' transition functions are called once per
+ * transition from the drive loop, but are big enough that the compiler
+ * declines to inline them, leaving a call in the hottest loop of AAT
+ * shaping.  Forcing them inline is worth 3-10% instructions on
+ * morx/kerx-heavy fonts, for about 16KB of code. */
+#ifndef HB_OPTIMIZE_SIZE
+#define HB_AAT_TRANSITION_INLINE HB_ALWAYS_INLINE
+#else
+#define HB_AAT_TRANSITION_INLINE
+#endif
+
 struct hb_aat_scratch_t
 {
   hb_aat_scratch_t () = default;
@@ -182,20 +193,26 @@ struct hb_aat_apply_context_t :
       buffer_glyph_set->add_array (glyphs, count);
     for (unsigned int i = 0; i < count; i++)
     {
+      bool at_end_of_text = buffer->idx == buffer->len;
+      if (at_end_of_text)
+	if (unlikely (!buffer->output_glyph (glyphs[i]))) return false;
+
+      hb_glyph_info_t &info = at_end_of_text ? buffer->prev () : buffer->cur ();
       if (glyphs[i] == DELETED_GLYPH)
       {
         buffer->scratch_flags |= HB_BUFFER_SCRATCH_FLAG_AAT_HAS_DELETED;
-	_hb_glyph_info_set_aat_deleted (&buffer->cur());
+	_hb_glyph_info_set_aat_deleted (&info);
       }
       else
       {
 #ifndef HB_NO_OT_LAYOUT
 	if (has_glyph_classes)
-	  _hb_glyph_info_set_glyph_props (&buffer->cur(),
+	  _hb_glyph_info_set_glyph_props (&info,
 					  gdef.get_glyph_props (glyphs[i]));
 #endif
       }
-      if (unlikely (!buffer->output_glyph (glyphs[i]))) return false;
+      if (!at_end_of_text)
+	if (unlikely (!buffer->output_glyph (glyphs[i]))) return false;
     }
     return true;
   }
--- src/3rdparty/harfbuzz-ng/src/hb-ot-cff1-table.hh
+++ src/3rdparty/harfbuzz-ng/src/hb-ot-cff1-table.hh
@@ -327,7 +327,7 @@ struct Charset0
 
   void collect_glyph_to_sid_map (glyph_to_sid_map_t *mapping, unsigned int num_glyphs) const
   {
-    mapping->resize_dirty  (num_glyphs);
+    if (!mapping->resize_dirty  (num_glyphs)) return;
     for (hb_codepoint_t gid = 1; gid < num_glyphs; gid++)
       mapping->arrayZ[gid] = {sids[gid - 1], gid};
   }
@@ -427,10 +427,12 @@ struct Charset1_2 {
 
   void collect_glyph_to_sid_map (glyph_to_sid_map_t *mapping, unsigned int num_glyphs) const
   {
-    mapping->resize_dirty  (num_glyphs);
+    if (!mapping->resize_dirty  (num_glyphs)) return;
+
     hb_codepoint_t gid = 1;
     if (gid >= num_glyphs)
       return;
+
     for (unsigned i = 0;; i++)
     {
       hb_codepoint_t sid = ranges[i].first;
@@ -771,8 +773,7 @@ struct cff1_top_dict_opset_t : top_dict_opset_t<cff1_top_dict_val_t>
 {
   static void process_op (op_code_t op, cff1_top_dict_interp_env_t& env, cff1_top_dict_values_t& dictval)
   {
-    cff1_top_dict_val_t  val;
-    val.last_arg_offset = (env.last_offset-1) - dictval.opStart;  /* offset to the last argument */
+    cff1_top_dict_val_t  val {};
 
     switch (op) {
       case OpCode_version:
@@ -816,6 +817,9 @@ struct cff1_top_dict_opset_t : top_dict_opset_t<cff1_top_dict_val_t>
 	dictval.nameSIDs[name_dict_values_t::ordering] = env.argStack.pop_uint ();
 	dictval.nameSIDs[name_dict_values_t::registry] = env.argStack.pop_uint ();
 	env.clear_args ();
+	if (unlikely (env.in_error ())) return;
+	assert (env.last_offset > dictval.opStart);
+	val.last_arg_offset = (env.last_offset - 1) - dictval.opStart;  /* offset to the last argument */
 	break;
 
       case OpCode_Encoding:
@@ -1464,8 +1468,8 @@ struct cff1
       return true;
     }
 
-    HB_INTERNAL bool get_extents (hb_font_t *font, hb_codepoint_t glyph, hb_glyph_extents_t *extents) const;
-    HB_INTERNAL bool get_path (hb_font_t *font, hb_codepoint_t glyph, hb_draw_session_t &draw_session) const;
+    HB_INTERNAL bool get_extents (hb_font_t *font, hb_codepoint_t glyph, hb_glyph_extents_t *extents, int64_t *budget = nullptr) const;
+    HB_INTERNAL bool get_path (hb_font_t *font, hb_codepoint_t glyph, hb_draw_session_t &draw_session, int64_t *budget = nullptr) const;
 
     private:
     struct gname_t
