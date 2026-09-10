b1655b02b5d87f6e0a4a339c01a5e297b1aff393
Marc Mutz <marc.mutz@qt.io>
2026-09-10 17:19:34 +0200

    QXmlStreamReader: fix parameter entities leaking from one doc to the next
    QXmlStreamReader failed to clear the parameterEntityHash on clear(),
    leaking parameter entities from one doc to the next.
    
    Clear it.
    
    Amends the start of the public history.
    
    [ChangeLog][QtCore][QXmlStreamReader] Fixed bugs that caused entities
    and parameter entities to leak from document to document when the
    QXmlStreamReader instance was reused (clear(), setDevice(), ...). A
    backwards-compatible work-around is to use QXmlStreamReader instances
    for only one XML document each, and not across mulitple documents.
    
    Like the other ones, not picking to ESM branches, as the work-around
    is trivial: use a new reader for each XML document.
    
    Pick-to: 6.12 6.12.0 6.11 6.11.3 6.8
    Fixes: QTBUG-150262
    Change-Id: I763665f423c2dd34ee62cb95b2afd4dcba7a6300
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- tests/auto/corelib/serialization/qxmlstream/tst_qxmlstream.cpp
+++ tests/auto/corelib/serialization/qxmlstream/tst_qxmlstream.cpp
@@ -2615,7 +2615,6 @@ void tst_QXmlStream::clearReallyResets() const
 
     r.addData(xml2);
 
-    QEXPECT_FAIL("parameter-entities-dont-leak-across-documents", "QTBUG-150262", Continue);
     QCOMPARE(readAllText(r), readAllText(fresh));
     QCOMPARE(r.error(), fresh.error());
     QCOMPARE(r.errorString(), fresh.errorString());
