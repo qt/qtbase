b1a3653f7250b9d4eb977e3a072b9323831fb27a
Marc Mutz <marc.mutz@qt.io>
2026-09-11 18:24:58 +0200

    tst_QXmlStream: Extract Method dumpTokenStream()
    And add per-TokenType customization points to it.
    
    This is needed for tests of security fixes, so picking to all active
    branches.
    
    Pick-to: 6.12.0 6.11 6.11.3 6.8 6.8.9 6.5 5.15
    Change-Id: If318c2c2f72c64f1cdf59558d5f6c273f67995ac
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    Reviewed-by: Magdalena Stojek <magdalena.stojek@qt.io>
    (cherry picked from commit 18d109184ee3ae561d870da8a8b5fa66abed0027)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/corelib/serialization/qxmlstream/tst_qxmlstream.cpp
+++ tests/auto/corelib/serialization/qxmlstream/tst_qxmlstream.cpp
@@ -19,6 +19,8 @@
 
 #include "qc14n.h"
 
+#include <QtCore/qxpfunctional.h>
+#include <map>
 #include <variant>
 
 using namespace Qt::StringLiterals;
@@ -661,7 +663,14 @@ private slots:
     void readElementTextDeepNesting() const;
 
 private:
+    using Actions = std::map<
+            QXmlStreamReader::TokenType,
+            qxp::function_ref<void(QXmlStreamReader &) const>
+        >;
+
     static QByteArray readFile(const QString &filename);
+    static QByteArray dumpTokenStream(QXmlStreamReader &r, Actions actions = {});
+
     void entityExpansionLimitImpl(const QString &xml, QXmlStreamEntityResolver *resolver = nullptr) const;
 
     QTemporaryDir m_tempDir;
@@ -878,6 +887,11 @@ QByteArray tst_QXmlStream::readFile(const QString &filename)
     QXmlStreamReader reader;
 
     reader.setDevice(&file);
+    return dumpTokenStream(reader);
+}
+
+QByteArray tst_QXmlStream::dumpTokenStream(QXmlStreamReader &reader, Actions actions)
+{
     QByteArray outarray;
     QTextStream writer(&outarray);
     // We always want UTF-8, and not what the system picks up.
@@ -885,6 +899,10 @@ QByteArray tst_QXmlStream::readFile(const QString &filename)
 
     while (!reader.atEnd()) {
         const auto tok = reader.readNext();
+
+        if (const auto it = actions.find(tok); it != actions.end())
+            it->second(reader);
+
         writer << reader.tokenString() << '(';
 
         // Accessors shared by several token types; asked after the switch so
