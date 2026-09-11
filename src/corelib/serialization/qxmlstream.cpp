7da91eacb93da1559252688424d887baccf5f08c
Marc Mutz <marc.mutz@qt.io>
2026-09-11 10:53:56 +0200

    QXmlStreamReader: clear entityReferenceStack in init()
    The consequence of not doing that is that if the previous parse failed
    in an entity expansion, and the stack isn't empty, the entityLength
    for the following parse is never reset to zero, because that is done
    only when stack is emptied. If elements from a previous parse remain,
    that never happens.
    
    Besides, the stack contain references into the entityHash and
    parameterEntityHash containers, the corresponding entries of which
    might not exist anymore.
    
    Also reserve(16) the stack, like all the others, to avoid the five
    reallocations at low sizes: 0 → 1 → 2 → 4 → 8 → 16.
    
    Amends the start of the public history, but the introduction of
    entityExpansionLimit (fd4be84d23a0db4186cb42e736a9de3af722c7f7; 5.15)
    exacerbated this.
    
    Not picking to ESM branches, since the work-around is so easy: just
    don't use a QXmlStreamReader instance for more than one
    document. Always use a fresh one.
    
    Changelog will be on the QTBUG-150215 fix in a follow-up.
    
    Fixes: QTBUG-150216
    Pick-to: 6.12 6.12.0 6.11 6.11.3 6.8
    Change-Id: I7eebad1c6dc76f811eac05a77f00f13902e91d15
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- src/corelib/serialization/qxmlstream.cpp
+++ src/corelib/serialization/qxmlstream.cpp
@@ -1089,6 +1089,8 @@ void QXmlStreamReaderPrivate::init()
     decoder = QStringDecoder();
     attributeStack.clear();
     attributeStack.reserve(16);
+    entityReferenceStack.clear();
+    entityReferenceStack.reserve(16);
     entityParser.reset();
     hasCheckedStartDocument = false;
     normalizeLiterals = false;
