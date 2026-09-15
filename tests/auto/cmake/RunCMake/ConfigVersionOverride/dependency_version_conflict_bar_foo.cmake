f6bdd926180b0f24f0ce0b270b498483980018f0
Michal Klocek <michal.klocek@qt.io>
2026-09-15 16:41:49 +0200

    CMake: Improve qt config override handling
    Make find_package calls not throwing errors from override handling. We
    should print just the warning and let the find_package call itself to
    'throw' the error when required. (namely when REQUIRED is set as
    argument and returned version is not fine.) Simplify override handling
    and use override only when version being asked is greater then qt build
    version, as this the case when we require overriding.
    
    In case override files are inconsistent or not provided simply return qt
    config version. Make sure if we bail out we load base file.
    
    Pick-to: 6.12
    Change-Id: I4cdc46399639a088b18b8656edde5ab2804ca039
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/auto/cmake/RunCMake/ConfigVersionOverride/dependency_version_conflict_bar_foo.cmake
+++ tests/auto/cmake/RunCMake/ConfigVersionOverride/dependency_version_conflict_bar_foo.cmake
@@ -16,6 +16,6 @@ set(PACKAGE_VERSION_COMPATIBLE TRUE)
 
 write_module_dependency(Foo ${mock_pkg_name}Bar "1.2.3")
 
-find_package(${mock_pkg_name} 6.140 REQUIRED COMPONENTS Bar Foo
+find_package(${mock_pkg_name} 6.140 COMPONENTS Bar Foo
     PATHS "${mock_pkg_dir}" NO_DEFAULT_PATH
 )
--- tests/auto/cmake/RunCMake/ConfigVersionOverride/override_files_conflict.cmake
+++ tests/auto/cmake/RunCMake/ConfigVersionOverride/override_files_conflict.cmake
@@ -4,16 +4,25 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 setup_mock_package("1.2.3")
 setup_mock_module_package(Foo "6.140.0")
+setup_mock_module_package(Boo "6.130.0")
 
-write_override_file(Foo "Override_0000_first.cmake" [[
+write_override_file(Foo "Override_0000_test.cmake" [[
 set(PACKAGE_VERSION "6.140")
 set(PACKAGE_VERSION_COMPATIBLE TRUE)
 ]])
-write_override_file(Foo "Override_0001_second.cmake" [[
+
+write_override_file(Boo "Override_0000_test.cmake" [[
 set(PACKAGE_VERSION "6.130")
 set(PACKAGE_VERSION_COMPATIBLE TRUE)
 ]])
 
-find_package(${mock_pkg_name} 6.140 COMPONENTS Foo
+find_package(${mock_pkg_name} 6.140 COMPONENTS Foo Boo QUIET
     PATHS "${mock_pkg_dir}" NO_DEFAULT_PATH
 )
+
+if(${mock_pkg_name}_FOUND)
+    message(FATAL_ERROR
+        "Expected ${mock_pkg_name} to not be found: Foo and Bar override files missmatch"
+    )
+endif()
+
