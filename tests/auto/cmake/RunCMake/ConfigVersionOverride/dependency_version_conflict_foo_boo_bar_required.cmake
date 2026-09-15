e183912de30065799c09ee66b9bb2c73539fd617
Michal Klocek <michal.klocek@qt.io>
2026-09-15 16:41:49 +0200

    CMake: Improve qt config override handling
    Make find_package calls not throwing errors from override handling. We
    should print just the warning and let the find_package call itself to
    'throw' the error when required. (namely when REQUIRED is set as
    argument and returned version is not fine.) Simplify override handling
    and use override only when version being asked is greater then qt build
    version, as this the case when we require overriding.
    
    In case override files are inconsistent or not provided simply return qt
    config version. Make sure if we bail out we load base file.
    
    Change-Id: I4cdc46399639a088b18b8656edde5ab2804ca039
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit f6bdd926180b0f24f0ce0b270b498483980018f0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/cmake/RunCMake/ConfigVersionOverride/dependency_version_conflict_foo_boo_bar_required.cmake
+++ tests/auto/cmake/RunCMake/ConfigVersionOverride/dependency_version_conflict_foo_boo_bar_required.cmake
@@ -22,7 +22,6 @@ set(PACKAGE_VERSION_COMPATIBLE TRUE)
 set(PACKAGE_VERSION_EXACT TRUE)
 ]])
 
-
 write_module_dependency(Foo ${mock_pkg_name}Bar "1.2.3")
 
 
@@ -34,6 +33,17 @@ if(NOT ${mock_pkg_name}_FOUND)
     message(FATAL_ERROR "Expected ${mock_pkg_name} to be found via version override")
 endif()
 
-find_package(${mock_pkg_name} 6.140 COMPONENTS Foo Boo Bar
+write_override_file(Foo "Override_0000_test.cmake" [[
+set(PACKAGE_VERSION "6.140")
+set(PACKAGE_VERSION_COMPATIBLE TRUE)
+]])
+write_module_dependency(Foo ${mock_pkg_name}Bar "1.2.3")
+
+# the previous find package resolves TestPkgBar as found due to internal dependency,so
+# the find_package below would not even try to look 'again' for Bar, prevent that
+# so the find_package fails.
+set(TestPkgBar_FOUND FALSE)
+
+find_package(${mock_pkg_name} 6.140 REQUIRED COMPONENTS Foo Boo Bar
     PATHS "${mock_pkg_dir}" NO_DEFAULT_PATH
 )
