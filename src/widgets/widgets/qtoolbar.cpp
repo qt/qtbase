83d48568fb594a1da1bc1c7022a92e00226cfa79
Axel Spoerl <axel.spoerl@qt.io>
2026-09-16 14:47:35 +0200

    Clear saved state in QToolBarPrivate::endDrag()
    Saved state was not cleared when a tool bar was dragged.
    This caused an early return in QMainWindowLayout::setGeometry() and
    subsequently in main window children not receiving resize events.
    That is a regression, introduced by
    1047809eb9bda7970a3b3ed889b2dfc2130937b2.
    
    Clear saved state in QDockWidgetPrivate::startDrag() and endDrag().
    
    The behavior can't be autotested, because a gradual resize of the
    main window is prone to flakiness.
    
    Fixes: QTBUG-150328
    Pick-to: 6.12 6.11 6.8
    Change-Id: I365780be73309c4ff24381428838baa3adf7de21
    Reviewed-by: Christian Ehrlicher <ch.ehrlicher@gmx.de>

--- src/widgets/widgets/qtoolbar.cpp
+++ src/widgets/widgets/qtoolbar.cpp
@@ -211,7 +211,7 @@ void QToolBarPrivate::endDrag()
 
         if (!layout->plug(state->widgetItem)) {
             if (q->isFloatable()) {
-                layout->restore(QInternal::KeepSavedState);
+                layout->restore(QInternal::ClearSavedState);
                 setWindowState(true); // gets rid of the X11BypassWindowManager window flag
                                       // and activates the resizer
                 q->activateWindow();
